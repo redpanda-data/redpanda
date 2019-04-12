655e76c0b6b92f1ffbe3bbd606309ac6d08f361a
Duarte Nunes <duarte@vectorized.io>
2019-04-12 16:45:13 -0300

    tools/verify_git_cfg.sh: Don't force globals
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

--- tools/verify_git_cfg.sh
+++ tools/verify_git_cfg.sh
@@ -4,19 +4,19 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${script_dir}/base_script.sh"
 
 if [[ "$(git config format.signOff)" != "yes" ]]; then
-    fatal "Please set auto signOff in Git with 'git config --global format.signOff yes'"
+    fatal "Please set auto signOff in Git with 'git config format.signOff yes'"
 else 
     log "Autmatic sign off checked"
 fi
 
 if [[ "$(git config user.name)" == "" ]]; then
-    fatal "please set user name with 'git config --global user.name <user_name>'"
+    fatal "please set user name with 'git config user.name <user_name>'"
 else 
     debug 'User name checked'
 fi
 
 if [[ "$(git config user.email)" == "" ]]; then
-    fatal "Please set user email with 'git config --global user.email <email>'"
+    fatal "Please set user email with 'git config user.email <email>'"
 else 
     debug "User email checked"
 fi
