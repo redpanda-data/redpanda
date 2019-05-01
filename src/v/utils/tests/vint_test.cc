0bc5430925ae29220565796af4b6801d89680d87
Duarte Nunes <duarte@vectorized.io>
2019-05-01 21:29:03 +0100

    utils: Add variable integer serialization
    Needed for the Kafka API, which encodes variable-length integers using
    the Google Protobuf format.
    
    Integers are zig-zag encoded and then the format describe in [1] is
    applied.
    
    Tests: unit(vint_test)
    
    [1] https://developers.google.com/protocol-buffers/docs/encoding?csw=1#varints
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>
    Message-Id: <20190501202903.19948-1-duarte@vectorized.io>

---
+++ src/v/utils/tests/vint_test.cc
@@ -0,0 +1,48 @@
+#define BOOST_TEST_MODULE utils
+
+#include "utils/vint.h"
+
+#include "bytes.h"
+
+#include <boost/test/unit_test.hpp>
+
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <random>
+
+namespace {
+
+std::mt19937 random_generator() {
+    std::random_device rd;
+    // In case of errors, replace the seed with a fixed value to get a deterministic run.
+    auto seed = rd();
+    std::cout << "Random seed: " << seed << "\n";
+    return std::mt19937(seed);
+}
+
+template<typename Integer>
+void check_roundtrip_sweep(std::size_t count) {
+    auto verify_round_trip = [] (Integer value) {
+        static bytes encoding_buffer(bytes::initialized_later(), sizeof(Integer));
+        const auto size = internal::vint_base<Integer>::serialize(value, encoding_buffer.begin());
+        const auto view = bytes_view(encoding_buffer.data(), size);
+        const auto deserialized = internal::vint_base<Integer>::deserialize(view);
+        BOOST_REQUIRE_EQUAL(deserialized, value);
+    };
+    std::uniform_int_distribution<Integer> distribution;
+    auto rng = random_generator();
+    for (std::size_t i = 0; i < count; ++i) {
+        verify_round_trip(distribution(rng));
+    }
+}
+
+}
+
+BOOST_AUTO_TEST_CASE(sanity_signed_sweep_32) {
+    check_roundtrip_sweep<int32_t>(100'000);
+}
+
+BOOST_AUTO_TEST_CASE(sanity_signed_sweep_64) {
+    check_roundtrip_sweep<int64_t>(100'000);
+}
---
+++ src/v/utils/vint.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "bytes.h"
+
+#include <cstdint>
+#include <type_traits>
+
+using vint_size_type = bytes::size_type;
+
+namespace internal {
+
+template<typename ValueType>
+class vint_base final {
+    static_assert(std::is_signed_v<ValueType>);
+    using UValueType = std::make_unsigned_t<ValueType>;
+    static constexpr uint8_t more_bytes = 128;
+
+public:
+    static constexpr UValueType encode_zigzag(ValueType n) noexcept {
+        // The right shift has to be arithmetic and not logical.
+        return (static_cast<UValueType>(n) << 1)
+            ^ static_cast<UValueType>(n >> std::numeric_limits<ValueType>::digits);
+    }
+
+    static constexpr ValueType decode_zigzag(UValueType n) noexcept {
+        return static_cast<std::make_signed_t<ValueType>>((n >> 1) ^ -(n & 1));
+    }
+
+    static vint_size_type serialize(ValueType value, bytes::iterator out) noexcept {
+        auto encode = encode_zigzag(value);
+        vint_size_type size = 1;
+        while (encode >= more_bytes) {
+            *out++ = static_cast<int8_t>(encode | more_bytes);
+            encode >>= 7;
+            ++size;
+        }
+        *out++ = static_cast<int8_t>(encode);
+        return size;
+    }
+
+    static ValueType deserialize(bytes_view v) noexcept {
+        UValueType result = 0;
+        uint8_t shift = 0;
+        for (auto* src = v.begin(); src != v.end(); ++src) {
+            uint64_t byte = *src;
+            if (byte & more_bytes) {
+                result |= (byte & (more_bytes - 1)) << shift;
+            } else {
+                result |= byte << shift;
+                break;
+            }
+            shift = std::min(std::numeric_limits<ValueType>::digits, shift + 7);
+        }
+        return decode_zigzag(result);
+    }
+};
+
+}
+
+using vint = internal::vint_base<int32_t>;
+using vlong = internal::vint_base<int64_t>;
