6229d859ce6d63a1f12dfc1a1b2f73569ccf9a20
Michal Maslanka <michal@vectorized.io>
2019-05-03 11:14:03 +0200

    build: Fixed set_option script to prevent warnings during build configuration
    According to CMake docs set() functions has the following order of args
    
    set(<variable> <value>... CACHE <type> <docstring> [FORCE])
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>
    Message-Id: <20190503091402.24789-1-michal@vectorized.io>

--- cmake/set_option.cmake
+++ cmake/set_option.cmake
@@ -2,5 +2,5 @@
 # https://github.com/edsiper/cmake-options
 #
 macro(RP_SET_OPTION option value)
-  set(${option} ${value} CACHE "" INTERNAL FORCE)
+  set(${option} ${value} CACHE INTERNAL "" FORCE)
 endmacro()
