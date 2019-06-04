d9a946009af3fffe73fe7397bbc3c36729607037
Michal Maslanka <michal@vectorized.io>
2019-06-04 13:39:07 +0200

    rpk: moved tuners factory from tune command
    In order to make it available to use in other places
    TunerFactroy struct was moved from tune.go to tuners package.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>
    Message-Id: <20190604113907.12932-1-michal@vectorized.io>

--- src/go/pkg/cli/cmd/tune.go
+++ src/go/pkg/cli/cmd/tune.go
@@ -3,70 +3,23 @@ package cmd
 import (
 	"errors"
 	"fmt"
-	"runtime"
 	"strings"
-	"vectorized/os"
-	"vectorized/tuners"
-	"vectorized/tuners/cpu"
-	"vectorized/tuners/disk"
-	"vectorized/tuners/hwloc"
-	"vectorized/tuners/irq"
-	"vectorized/tuners/network"
+	"vectorized/tuners/factory"
 	"vectorized/utils"
 
-	"github.com/safchain/ethtool"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 )
 
-type tunerParams struct {
-	mode         string
-	cpuMask      string
-	thingsToTune []string
-	disks        []string
-	directories  []string
-	nic          string
-}
-
-type tunersFactory struct {
-	fs                afero.Fs
-	irqDeviceInfo     irq.DeviceInfo
-	cpuMasks          irq.CpuMasks
-	irqBalanceService irq.BalanceService
-	irqProcFile       irq.ProcFile
-	diskInfoProvider  disk.InfoProvider
-	proc              os.Proc
-	grub              os.Grub
-	tuners            map[string]func(*tunersFactory, *tunerParams) tuners.Tunable
-}
-
 func NewTuneCommand() *cobra.Command {
-
 	fs := afero.OsFs{}
-	irqProcFile := irq.NewProcFile(fs)
-	proc := os.NewProc()
-	tunerFactory := tunersFactory{
-		tuners: map[string]func(*tunersFactory, *tunerParams) tuners.Tunable{
-			"disk_irq":   (*tunersFactory).newDiskIrqTuner,
-			"disk_sched": (*tunersFactory).newDiskSchedulerTuner,
-			"net":        (*tunersFactory).newNetworkTuner,
-			"cpu":        (*tunersFactory).newCpuTuner,
-		},
-		fs:                fs,
-		irqProcFile:       irqProcFile,
-		irqDeviceInfo:     irq.NewDeviceInfo(fs, irqProcFile),
-		cpuMasks:          irq.NewCpuMasks(fs, hwloc.NewHwLocCmd(proc)),
-		irqBalanceService: irq.NewBalanceService(fs, proc),
-		diskInfoProvider:  disk.NewDiskInfoProvider(fs, proc),
-		grub:              os.NewGrub(os.NewCommands(proc), proc, fs),
-		proc:              proc,
-	}
-	tunerParams := tunerParams{}
+	tunerFactory := factory.NewTunersFactory(fs)
+	tunerParams := factory.TunerParams{}
 	command := &cobra.Command{
 		Use: "tune <list_of_elements_to_tune>",
 		Short: `Sets the OS parameters to tune system performance
-		available tuners: ` + fmt.Sprintf("%#q", tunerFactory.getTunerNames()),
+		available tuners: ` + fmt.Sprintf("%#q", tunerFactory.AvailableTuners()),
 		Long: "In order to get more information about the tuner run: " +
 			"rpk tune <tuner_name> --help",
 		Args: func(cmd *cobra.Command, args []string) error {
@@ -74,31 +27,31 @@ func NewTuneCommand() *cobra.Command {
 				return errors.New("requires the list of elements to tune")
 			}
 			for _, toTune := range strings.Split(args[0], ",") {
-				if tunerFactory.tuners[toTune] == nil {
+				if !tunerFactory.IsTunerAvailable(toTune) {
 					return fmt.Errorf("invalid element to tune '%s' "+
 						"only %s are supported",
-						args[0], tunerFactory.getTunerNames())
+						args[0], tunerFactory.AvailableTuners())
 				}
 			}
 			return nil
 		},
 		RunE: func(ccmd *cobra.Command, args []string) error {
-			return tune(strings.Split(args[0], ","), &tunerFactory, &tunerParams)
+			return tune(strings.Split(args[0], ","), tunerFactory, &tunerParams)
 		},
 	}
-	command.Flags().StringVarP(&tunerParams.mode,
+	command.Flags().StringVarP(&tunerParams.Mode,
 		"mode", "m", "",
 		"Operation Mode: one of: [sq, sq_split, mq]")
-	command.Flags().StringVarP(&tunerParams.cpuMask,
+	command.Flags().StringVarP(&tunerParams.CpuMask,
 		"cpu-mask", "c",
 		"all", "CPU Mask (32-bit hexadecimal integer))")
-	command.Flags().StringSliceVarP(&tunerParams.disks,
+	command.Flags().StringSliceVarP(&tunerParams.Disks,
 		"disks", "d",
 		[]string{}, "Lists of devices to tune f.e. 'sda1'")
-	command.Flags().StringVarP(&tunerParams.nic,
+	command.Flags().StringVarP(&tunerParams.Nic,
 		"nic", "n",
 		"eth0", "Network Interface Controller to tune")
-	command.Flags().StringSliceVarP(&tunerParams.directories,
+	command.Flags().StringSliceVarP(&tunerParams.Directories,
 		"dirs", "r",
 		[]string{}, "List of *data* directories. or places to store data."+
 			" i.e.: '/var/vectorized/redpanda/',"+
@@ -140,77 +93,13 @@ func newHelpCommand() *cobra.Command {
 	}
 }
 
-func (factory *tunersFactory) getTunerNames() []string {
-	var keys []string
-	for key := range factory.tuners {
-		keys = append(keys, key)
-	}
-	return keys
-}
-
-func (factory *tunersFactory) newTuner(
-	tunerName string, tunerParams *tunerParams,
-) tuners.Tunable {
-	return factory.tuners[tunerName](factory, tunerParams)
-}
-
-func (factory *tunersFactory) newDiskIrqTuner(
-	params *tunerParams,
-) tuners.Tunable {
-
-	return disk.NewDiskIrqTuner(
-		irq.ModeFromString(params.mode),
-		params.cpuMask,
-		params.directories,
-		params.disks,
-		factory.irqDeviceInfo,
-		factory.cpuMasks,
-		factory.irqBalanceService,
-		factory.irqProcFile,
-		factory.diskInfoProvider,
-		runtime.NumCPU(),
-	)
-}
-
-func (factory *tunersFactory) newDiskSchedulerTuner(
-	params *tunerParams,
-) tuners.Tunable {
-	return disk.NewSchedulerTuner(
-		params.directories,
-		params.disks,
-		factory.diskInfoProvider,
-		factory.fs)
-}
-
-func (factory *tunersFactory) newNetworkTuner(
-	params *tunerParams,
-) tuners.Tunable {
-	return network.NewNetTuner(
-		irq.ModeFromString(params.mode),
-		params.cpuMask,
-		params.nic,
-		factory.fs,
-		factory.irqDeviceInfo,
-		factory.cpuMasks,
-		factory.irqBalanceService,
-		factory.irqProcFile,
-		&ethtool.Ethtool{},
-	)
-}
-
-func (factory *tunersFactory) newCpuTuner(params *tunerParams) tuners.Tunable {
-	return cpu.NewCpuTuner(
-		factory.cpuMasks,
-		factory.grub,
-		factory.fs,
-	)
-}
-
 func tune(
-	elementsToTune []string, tunersMap *tunersFactory, params *tunerParams,
+	elementsToTune []string,
+	factory factory.TunersFactory,
+	params *factory.TunerParams,
 ) error {
 	for _, tunerName := range elementsToTune {
-		tuner := tunersMap.newTuner(tunerName, params)
+		tuner := factory.CreateTuner(tunerName, params)
 		if supported, reason := tuner.CheckIfSupported(); supported == true {
 			log.Infof("Running '%s' tuner...", tunerName)
 			err := tuner.Tune()
---
+++ src/go/pkg/tuners/factory/factory.go
@@ -0,0 +1,135 @@
+package factory
+
+import (
+	"runtime"
+	"vectorized/os"
+	"vectorized/tuners"
+	"vectorized/tuners/cpu"
+	"vectorized/tuners/disk"
+	"vectorized/tuners/hwloc"
+	"vectorized/tuners/irq"
+	"vectorized/tuners/network"
+
+	"github.com/safchain/ethtool"
+	"github.com/spf13/afero"
+)
+
+type TunerParams struct {
+	Mode        string
+	CpuMask     string
+	Disks       []string
+	Directories []string
+	Nic         string
+}
+
+type TunersFactory interface {
+	CreateTuner(tunerType string, params *TunerParams) tuners.Tunable
+	AvailableTuners() []string
+	IsTunerAvailable(tuner string) bool
+}
+
+type tunersFactory struct {
+	fs                afero.Fs
+	irqDeviceInfo     irq.DeviceInfo
+	cpuMasks          irq.CpuMasks
+	irqBalanceService irq.BalanceService
+	irqProcFile       irq.ProcFile
+	diskInfoProvider  disk.InfoProvider
+	proc              os.Proc
+	grub              os.Grub
+	tuners            map[string]func(*tunersFactory, *TunerParams) tuners.Tunable
+}
+
+func NewTunersFactory(fs afero.Fs) TunersFactory {
+	irqProcFile := irq.NewProcFile(fs)
+	proc := os.NewProc()
+	return &tunersFactory{
+		tuners: map[string]func(*tunersFactory, *TunerParams) tuners.Tunable{
+			"disk_irq":   (*tunersFactory).newDiskIrqTuner,
+			"disk_sched": (*tunersFactory).newDiskSchedulerTuner,
+			"net":        (*tunersFactory).newNetworkTuner,
+			"cpu":        (*tunersFactory).newCpuTuner,
+		},
+		fs:                fs,
+		irqProcFile:       irqProcFile,
+		irqDeviceInfo:     irq.NewDeviceInfo(fs, irqProcFile),
+		cpuMasks:          irq.NewCpuMasks(fs, hwloc.NewHwLocCmd(proc)),
+		irqBalanceService: irq.NewBalanceService(fs, proc),
+		diskInfoProvider:  disk.NewDiskInfoProvider(fs, proc),
+		grub:              os.NewGrub(os.NewCommands(proc), proc, fs),
+		proc:              proc,
+	}
+}
+
+func (factory *tunersFactory) AvailableTuners() []string {
+	var keys []string
+	for key := range factory.tuners {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func (factory *tunersFactory) IsTunerAvailable(tuner string) bool {
+	if factory.tuners[tuner] != nil {
+		return true
+	}
+	return false
+}
+
+func (factory *tunersFactory) CreateTuner(
+	tunerName string, tunerParams *TunerParams,
+) tuners.Tunable {
+	return factory.tuners[tunerName](factory, tunerParams)
+}
+
+func (factory *tunersFactory) newDiskIrqTuner(
+	params *TunerParams,
+) tuners.Tunable {
+
+	return disk.NewDiskIrqTuner(
+		irq.ModeFromString(params.Mode),
+		params.CpuMask,
+		params.Directories,
+		params.Disks,
+		factory.irqDeviceInfo,
+		factory.cpuMasks,
+		factory.irqBalanceService,
+		factory.irqProcFile,
+		factory.diskInfoProvider,
+		runtime.NumCPU(),
+	)
+}
+
+func (factory *tunersFactory) newDiskSchedulerTuner(
+	params *TunerParams,
+) tuners.Tunable {
+	return disk.NewSchedulerTuner(
+		params.Directories,
+		params.Disks,
+		factory.diskInfoProvider,
+		factory.fs)
+}
+
+func (factory *tunersFactory) newNetworkTuner(
+	params *TunerParams,
+) tuners.Tunable {
+	return network.NewNetTuner(
+		irq.ModeFromString(params.Mode),
+		params.CpuMask,
+		params.Nic,
+		factory.fs,
+		factory.irqDeviceInfo,
+		factory.cpuMasks,
+		factory.irqBalanceService,
+		factory.irqProcFile,
+		&ethtool.Ethtool{},
+	)
+}
+
+func (factory *tunersFactory) newCpuTuner(params *TunerParams) tuners.Tunable {
+	return cpu.NewCpuTuner(
+		factory.cpuMasks,
+		factory.grub,
+		factory.fs,
+	)
+}
