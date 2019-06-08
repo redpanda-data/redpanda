1889f5b328f1a4e6fa24c4dcf66dbdc791392bc1
Duarte Nunes <duarte@vectorized.io>
2019-06-07 22:09:03 -0300

    redpanda/kafka: Implement the ApiVersions API
    The ApiVersions API is the first API call a client will usually make,
    and it allows clients to negotiate with the broker which protocol
    version to use.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

---
+++ src/v/redpanda/kafka/requests/api_versions_request.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "redpanda/kafka/requests/headers.h"
+#include "redpanda/kafka/requests/request_context.h"
+#include "redpanda/kafka/requests/response.h"
+
+#include <seastar/core/future.hh>
+
+namespace kafka::requests {
+
+class api_versions_request final {
+    static constexpr api_version v0 = api_version(0);
+
+public:
+    static constexpr api_key key = api_key(18);
+    static constexpr api_version min_supported = v0;
+    static constexpr api_version max_supported = api_version(2);
+
+    static seastar::future<response_ptr> process(request_context&);
+};
+
+} // namespace kafka::requests
