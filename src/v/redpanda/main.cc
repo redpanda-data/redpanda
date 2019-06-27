7c19ed5783cc1d5c26ca0a7eb1d887a0ca0ca3b9
Duarte Nunes <duarte@vectorized.io>
2019-06-27 09:17:06 -0300

    redpanda/main: Don't use at_exit tasks
    These are going to be deprecated and should be avoided. Use the defer
    mechanism instead.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

--- src/v/redpanda/main.cc
+++ src/v/redpanda/main.cc
@@ -3,8 +3,13 @@
 #include "syschecks/syschecks.h"
 
 #include <seastar/core/app-template.hh>
+#include <seastar/core/condition-variable.hh>
+#include <seastar/core/future.hh>
 #include <seastar/core/prometheus.hh>
+#include <seastar/core/reactor.hh>
+#include <seastar/core/sharded.hh>
 #include <seastar/core/thread.hh>
+#include <seastar/util/defer.hh>
 
 #include <smf/histogram_seastar_utils.h>
 #include <smf/log.h>
@@ -25,14 +30,46 @@
 
 using namespace std::chrono_literals;
 
+class stop_signal {
+    void signaled() {
+        _caught = true;
+        _cond.broadcast();
+    }
+
+public:
+    stop_signal() {
+        seastar::engine().handle_signal(SIGINT, [this] { signaled(); });
+        seastar::engine().handle_signal(SIGTERM, [this] { signaled(); });
+    }
+
+    ~stop_signal() {
+        // There's no way to unregister a handler yet, so register a no-op
+        // handler instead.
+        seastar::engine().handle_signal(SIGINT, [] {});
+        seastar::engine().handle_signal(SIGTERM, [] {});
+    }
+
+    seastar::future<> wait() {
+        return _cond.wait([this] { return _caught; });
+    }
+
+    bool stopping() const {
+        return _caught;
+    }
+
+private:
+    bool _caught = false;
+    seastar::condition_variable _cond;
+};
+
 seastar::future<> check_environment(const redpanda_cfg& c);
 
 void register_service(
   smf::rpc_server& s,
-  seastar::distributed<write_ahead_log>* log,
+  seastar::sharded<write_ahead_log>* log,
   const redpanda_cfg* c);
 
-void rpc_at_exit(seastar::distributed<smf::rpc_server>& rpc, seastar::sstring);
+void stop_rpc(seastar::sharded<smf::rpc_server>& rpc, seastar::sstring);
 bool hydrate_cfg(redpanda_cfg& c, std::string filename);
 
 int main(int argc, char** argv, char** env) {
@@ -40,8 +77,6 @@ int main(int argc, char** argv, char** env) {
     syschecks::initialize_intrinsics();
     namespace po = boost::program_options; // NOLINT
     std::setvbuf(stdout, nullptr, _IOLBF, 1024);
-    seastar::distributed<smf::rpc_server> rpc;
-    seastar::distributed<write_ahead_log> log;
     seastar::app_template::config app_cfg;
     app_cfg.name = "Redpanda";
     app_cfg.default_task_quota = 500us;
@@ -59,7 +94,7 @@ int main(int argc, char** argv, char** env) {
         smf::app_run_log_level(seastar::log_level::trace);
 #endif
         return seastar::async([&] {
-            seastar::engine().at_exit([&log] { return log.stop(); });
+            ::stop_signal stop_signal;
             auto&& config = app.configuration();
             LOG_THROW_IF(
               !config.count("redpanda-cfg"), "Missing redpanda-cfg flag");
@@ -69,35 +104,40 @@ int main(int argc, char** argv, char** env) {
               "Could not find `redpanda` section in: {}",
               config["redpanda-cfg"].as<std::string>());
             LOG_INFO("Configuration: {}", global_cfg);
-            rpc_at_exit(rpc, global_cfg.directory);
             check_environment(global_cfg).get();
+            static seastar::sharded<write_ahead_log> log;
             log.start(global_cfg.wal_cfg()).get();
+            auto stop_log = seastar::defer([] { log.stop().get(); });
             log.invoke_on_all(&write_ahead_log::open).get();
             log.invoke_on_all(&write_ahead_log::index).get();
+            static seastar::sharded<smf::rpc_server> rpc;
             rpc.start(global_cfg.rpc_cfg()).get();
+            auto stop_rcp = seastar::defer([&] {
+              stop_rpc(rpc, global_cfg.directory);
+            });
             rpc.invoke_on_all([&](smf::rpc_server& s) {
                 register_service(s, &log, &global_cfg);
             }).get();
             rpc.invoke_on_all(&smf::rpc_server::start).get();
+            stop_signal.wait().get();
+            LOG_INFO("Stopping...");
         });
     });
     return 0;
 }
 
-void rpc_at_exit(
-  seastar::distributed<smf::rpc_server>& rpc, seastar::sstring directory) {
-    seastar::engine().at_exit([&rpc] { return rpc.stop(); });
-    seastar::engine().at_exit([&rpc, directory] {
-        return rpc
-          .map_reduce(
-            smf::unique_histogram_adder(), &smf::rpc_server::copy_histogram)
-          .then([directory](auto h) {
-              const seastar::sstring histogram_dir = fmt::format(
-                "{}/redpanda.latencies.hgrm", directory);
-              return smf::histogram_seastar_utils::write(
-                histogram_dir, std::move(h));
-          });
-    });
+// Called in the context of a seastar::thread.
+void stop_rpc(
+  seastar::sharded<smf::rpc_server>& rpc, seastar::sstring directory) {
+    auto h = rpc
+               .map_reduce(
+                 smf::unique_histogram_adder(),
+                 &smf::rpc_server::copy_histogram)
+               .get0();
+    const seastar::sstring histogram_dir = fmt::format(
+      "{}/redpanda.latencies.hgrm", directory);
+    smf::histogram_seastar_utils::write(histogram_dir, std::move(h)).get();
+    rpc.stop().get();
 }
 
 bool hydrate_cfg(redpanda_cfg& c, std::string filename) {
@@ -118,7 +158,7 @@ seastar::future<> check_environment(const redpanda_cfg& c) {
 
 void register_service(
   smf::rpc_server& s,
-  seastar::distributed<write_ahead_log>* log,
+  seastar::sharded<write_ahead_log>* log,
   const redpanda_cfg* c) {
     using lz4_c_t = smf::lz4_compression_filter;
     using lz4_d_t = smf::lz4_decompression_filter;
