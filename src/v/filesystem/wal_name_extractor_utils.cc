0432d890f5f167f64abc4477e62db261bf56b528
Alexander Gallego <alex@vectorized.io>
2019-07-03 22:56:42 -0700

    filesystem: Replace re2 regex with c++11 std::regex
    Fix build as re2::re2 segfaults in debug mode.
    
    Signed-off-by: Alexander Gallego <alex@vectorized.io>
    Message-Id: <20190704055642.9584-1-alex@vectorized.io>

--- src/v/filesystem/wal_name_extractor_utils.cc
+++ src/v/filesystem/wal_name_extractor_utils.cc
@@ -2,43 +2,59 @@
 
 #include <seastar/core/sstring.hh>
 
-#include <re2/re2.h>
+#include <regex>
 
-static const re2::RE2
-  kEpochExtractor("([[:ascii:]]+[^\\d]+)?(\\d+)\\.(\\d+)\\.wal$");
-static const re2::RE2 kPartitionExtractor("([[:ascii:]]+[^\\d]+)?(\\d+)$");
+static constexpr const char* epoch_extractor_pattern
+  = "([\\x00-\\x7F]+[^\\d]+)?(\\d+)\\.(\\d+)\\.wal$";
+static constexpr const char* partition_extractor_pattern
+  = "([\\x00-\\x7F]+[^\\d]+)?(\\d+)$";
 
 std::pair<int64_t, int64_t>
 wal_name_extractor_utils::wal_segment_extract_epoch_term(
   const seastar::sstring& filename) {
     int64_t epoch = -1;
     int64_t term = -1;
-    re2::RE2::FullMatch(
-      filename.c_str(), kEpochExtractor, (void*)nullptr, &epoch, &term);
+    const std::regex re(epoch_extractor_pattern);
+    std::cmatch base_match;
+
+    if (std::regex_match(filename.c_str(), base_match, re)) {
+        // The first sub_match is the whole string; the next
+        // sub_match is the first parenthesized expression.
+        if (base_match.size() == 4) {
+            epoch = std::stol(base_match[2].str());
+            term = std::stol(base_match[3].str());
+        }
+    }
     return {epoch, term};
 }
 bool wal_name_extractor_utils::is_wal_segment(
   const seastar::sstring& filename) {
-    return re2::RE2::FullMatch(
-      filename.c_str(), kEpochExtractor, (void*)nullptr, (void*)nullptr);
+    auto [e, t] = wal_name_extractor_utils::wal_segment_extract_epoch_term(
+      filename);
+    return e >= 0 && t >= 0;
 }
 bool wal_name_extractor_utils::is_valid_ns_topic_name(const char* filename) {
     // cannot include '.' in the regex to simplify life
-    //
-    static const re2::RE2 kWalName("^[[:ascii:]]+$");
-    return re2::RE2::FullMatch(filename, kWalName);
+    const std::regex re("^[\\x00-\\x7F]+$");
+    return std::regex_match(filename, re);
 }
 
 bool wal_name_extractor_utils::is_valid_partition_name(
   const seastar::sstring& filename) {
-    return re2::RE2::FullMatch(
-      filename.c_str(), kPartitionExtractor, (void*)nullptr, (void*)nullptr);
+    return wal_name_extractor_utils::wal_partition_dir_extract(filename) >= 0;
 }
 
 int32_t wal_name_extractor_utils::wal_partition_dir_extract(
   const seastar::sstring& filename) {
-    int32_t retval = 0;
-    re2::RE2::FullMatch(
-      filename.c_str(), kPartitionExtractor, (void*)nullptr, &retval);
+    int32_t retval = -1;
+    const std::regex re(partition_extractor_pattern);
+    std::cmatch base_match;
+    if (std::regex_match(filename.c_str(), base_match, re)) {
+        // The first sub_match is the whole string; the next
+        // sub_match is the first parenthesized expression.
+        if (base_match.size() == 3) {
+            retval = std::stol(base_match[2].str());
+        }
+    }
     return retval;
 }
