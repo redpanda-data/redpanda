d22549f259abbf01c5255bc9b52958b291116ed7
Michal Maslanka <michal@vectorized.io>
2019-07-05 08:28:09 +0200

    rpk: looking for interface by IP address
    Added function to get network interfaces by an IP address. It is
    required to automatically determine NIC used by redpanda to accept
    connections.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/go/pkg/net/interfaces.go
@@ -0,0 +1,35 @@
+package net
+
+import (
+	"net"
+	"strings"
+	"vectorized/utils"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func GetInterfacesByIp(ipAddress string) ([]string, error) {
+	log.Debugf("Looking for interface with '%s' address", ipAddress)
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	nics := make(map[string]bool)
+	for _, iface := range ifaces {
+		addr, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, address := range addr {
+			log.Debugf("Checking '%s' address '%s'", iface.Name, address)
+			if ipAddress == "0.0.0.0" ||
+				strings.HasPrefix(address.String(), ipAddress) {
+				if (iface.Flags & net.FlagLoopback) == 0 {
+					nics[iface.Name] = true
+				}
+			}
+		}
+	}
+
+	return utils.GetKeys(nics), nil
+}
