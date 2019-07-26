48fbb3acdbb155b07ff27190f1469e13654a6b2a
Michal Maslanka <michal@vectorized.io>
2019-07-26 08:59:00 +0200

    conf: updated configuration file with kafka port
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/go/pkg/redpanda/config.go
+++ src/go/pkg/redpanda/config.go
@@ -8,11 +8,12 @@ import (
 )
 
 type Config struct {
-	Directory   string
-	Port        int
-	Ip          string
-	Id          int
-	SeedServers []*SeedServer `yaml:"seed_servers"`
+	Directory          string
+	Port               int
+	KafkaTransportPort int `yaml:"kafka_transport_port"`
+	Ip                 string
+	Id                 int
+	SeedServers        []*SeedServer `yaml:"seed_servers"`
 }
 
 type SeedServer struct {
@@ -47,6 +48,7 @@ func CheckConfig(config *Config) bool {
 		config.Port == 0 ||
 		config.Ip == "" ||
 		config.Id < 0 ||
+		config.KafkaTransportPort == 0 ||
 		len(config.SeedServers) == 0 {
 		return false
 	}
