03d798dd3b8fee1f4b643f366632af290bd9cd19
Michal Maslanka <michal@vectorized.io>
2019-07-31 12:16:50 +0200

    rpk/checkers: simplified getting information about system memory
    Using golang's unix package in order to simplify getting information
    regarding system memory
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/go/pkg/system/memory.go
+++ src/go/pkg/system/memory.go
@@ -1,23 +1,18 @@
 package system
 
 import (
-	"bufio"
-	"io"
-	"os"
-	"strconv"
-	"strings"
-
 	"github.com/docker/go-units"
 	"github.com/spf13/afero"
+	"golang.org/x/sys/unix"
 )
 
 type MemInfo struct {
-	MemTotal     int64
-	MemFree      int64
-	MemAvailable int64
+	MemTotal uint64
+	MemFree  uint64
 }
 
 func GetTransparentHugePagesActive(fs afero.Fs) (bool, error) {
+
 	options, err := ReadRuntineOptions(fs,
 		"/sys/kernel/mm/transparent_hugepage/enabled")
 
@@ -33,45 +28,21 @@ func GetTransparentHugePagesActive(fs afero.Fs) (bool, error) {
 }
 
 func GetMemTotalMB() (int, error) {
-	mInfo, err := readMemInfo()
+	mInfo, err := getMemInfo()
 	if err != nil {
 		return 0, err
 	}
 	return int(mInfo.MemTotal / units.MiB), nil
 }
 
-func readMemInfo() (*MemInfo, error) {
-	file, err := os.Open("/proc/meminfo")
+func getMemInfo() (*MemInfo, error) {
+	var si unix.Sysinfo_t
+	err := unix.Sysinfo(&si)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
-	return parseMemInfo(file)
-}
-
-func parseMemInfo(reader io.Reader) (*MemInfo, error) {
-	scanner := bufio.NewScanner(reader)
-	var result = &MemInfo{}
-	for scanner.Scan() {
-		parts := strings.Fields(scanner.Text())
-		if len(parts) < 3 || parts[2] != "kB" {
-			continue
-		}
-
-		size, err := strconv.Atoi(parts[1])
-		if err != nil {
-			continue
-		}
-		bytes := int64(size) * units.KiB
-		switch parts[0] {
-		case "MemTotal:":
-			result.MemTotal = bytes
-		case "MemFree:":
-			result.MemFree = bytes
-		case "MemAvailable:":
-			result.MemAvailable = bytes
-		}
-	}
-
-	return result, nil
+	return &MemInfo{
+		MemTotal: si.Totalram * uint64(si.Unit),
+		MemFree:  si.Freeram * uint64(si.Unit),
+	}, nil
 }
