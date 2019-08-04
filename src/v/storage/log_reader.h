b8a7356e013e0aa87adcf44c0ad0283066997170
Duarte Nunes <duarte@vectorized.io>
2019-08-03 22:07:52 -0300

    storage: Add log segment reader
    The log segment reader uses the parser to read record batch off disk.
    It uses a consumer which skips to the first offset while we don't
    implement an offset map.
    
    It also contains logic to determine when a read is done, or example
    when we reach the request max bytes.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

---
+++ src/v/storage/log_reader.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include "model/record_batch_reader.h"
+#include "storage/log_segment.h"
+#include "storage/offset_tracker.h"
+#include "storage/parser.h"
+#include "utils/fragmented_temporary_buffer.h"
+
+#include <seastar/core/io_queue.hh>
+#include <seastar/util/optimized_optional.hh>
+
+namespace storage {
+
+struct log_reader_config {
+    model::offset start_offset;
+    size_t max_bytes;
+    size_t min_bytes;
+    io_priority_class prio;
+};
+
+class log_segment_reader;
+
+class skipping_consumer : public batch_consumer {
+public:
+    explicit skipping_consumer(log_segment_reader& reader) noexcept
+      : _reader(reader) {
+    }
+
+    void set_timeout(model::timeout_clock::time_point timeout) {
+        _timeout = timeout;
+    }
+
+    virtual skip consume_batch_start(
+      model::record_batch_header, size_t num_records) override;
+
+    virtual skip consume_record_key(
+      size_t size_bytes,
+      int32_t timestamp_delta,
+      int32_t offset_delta,
+      fragmented_temporary_buffer&& key) override;
+
+    virtual void consume_record_value(fragmented_temporary_buffer&&) override;
+
+    virtual void
+    consume_compressed_records(fragmented_temporary_buffer&&) override;
+
+    virtual stop_iteration consume_batch_end() override;
+
+private:
+    log_segment_reader& _reader;
+    model::offset _start_offset;
+    model::record_batch_header _header;
+    size_t _record_size_bytes;
+    int32_t _record_timestamp_delta;
+    int32_t _record_offset_delta;
+    fragmented_temporary_buffer _record_key;
+    size_t _num_records;
+    model::record_batch::records_type _records;
+    model::timeout_clock::time_point _timeout;
+};
+
+class log_segment_reader : public model::record_batch_reader::impl {
+    static constexpr size_t max_buffer_size = 8 * 1024;
+    using span = model::record_batch_reader::impl::span;
+
+public:
+    log_segment_reader(
+      log_segment_ptr seg,
+      offset_tracker& tracker,
+      log_reader_config config) noexcept;
+
+    size_t bytes_read() const {
+        return _bytes_read;
+    }
+
+protected:
+    virtual future<span>
+      do_load_slice(model::timeout_clock::time_point) override;
+
+private:
+    future<> initialize();
+
+    bool is_initialized() const;
+
+    bool is_buffer_full() const;
+
+    /// reset_state() allows further reads to happen on a reader
+    /// that previously reached the end of the stream. Useful to
+    /// implement cached readers that can continue a read where
+    /// it left off.
+    void reset_state();
+
+    friend class log_reader;
+
+private:
+    log_segment_ptr _seg;
+    offset_tracker& _tracker;
+    log_reader_config _config;
+    size_t _bytes_read = 0;
+    skipping_consumer _consumer;
+    input_stream<char> _input;
+    continuous_batch_parser_opt _parser;
+    std::vector<model::record_batch> _buffer;
+    size_t _buffer_size = 0;
+    bool _over_committed_offset = false;
+
+    friend class skipping_consumer;
+};
+
+} // namespace storage
