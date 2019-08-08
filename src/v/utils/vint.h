1fde97449e4d85864d7181d48e8bb10de6b8d8b7
Duarte Nunes <duarte@vectorized.io>
2019-08-08 20:46:49 -0300

    utils/vint: Simply implementation by providing just one size
    We support signed variable length integer serialization and
    deserialization for both 4 and 8 byte values. However, this is
    unecessarily complex, since the application can always narrow if
    needed. Remove all the template bloat.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>
    Message-Id: <20190808234649.19446-1-duarte@vectorized.io>

--- src/v/utils/vint.h
+++ src/v/utils/vint.h
@@ -5,41 +5,38 @@
 #include <cstdint>
 #include <type_traits>
 
-using vint_size_type = bytes::size_type;
-
-static constexpr size_t max_vint_length = 9;
-
-namespace vint_internal {
-
-template<typename ValueType>
-class vint_base final {
-    static_assert(std::is_signed_v<ValueType>);
-    using UValueType = std::make_unsigned_t<ValueType>;
-    static constexpr uint8_t more_bytes = 128;
-
+class vint final {
 public:
-    using value_type = ValueType;
+    using value_type = int64_t;
+    using size_type = bytes::size_type;
+    static constexpr size_t max_length = sizeof(value_type) + 1;
 
     struct result {
-        ValueType value;
-        vint_size_type bytes_read;
+        value_type value;
+        size_type bytes_read;
     };
 
-    static constexpr UValueType encode_zigzag(ValueType n) noexcept {
+private:
+    static constexpr uint8_t more_bytes = 128;
+
+    using uvalue_type = uint64_t;
+
+    static constexpr uvalue_type encode_zigzag(value_type n) noexcept {
         // The right shift has to be arithmetic and not logical.
-        return (static_cast<UValueType>(n) << 1)
-               ^ static_cast<UValueType>(
-                 n >> std::numeric_limits<ValueType>::digits);
+        return (static_cast<uvalue_type>(n) << 1)
+               ^ static_cast<uvalue_type>(
+                 n >> std::numeric_limits<value_type>::digits);
     }
 
-    static constexpr ValueType decode_zigzag(UValueType n) noexcept {
-        return static_cast<std::make_signed_t<ValueType>>((n >> 1) ^ -(n & 1));
+    static constexpr value_type decode_zigzag(uvalue_type n) noexcept {
+        return static_cast<value_type>((n >> 1) ^ -(n & 1));
     }
 
-    static vint_size_type
-    serialize(ValueType value, bytes::iterator out) noexcept {
+public:
+    static size_type
+    serialize(value_type value, bytes::iterator out) noexcept {
         auto encode = encode_zigzag(value);
-        vint_size_type size = 1;
+        size_type size = 1;
         while (encode >= more_bytes) {
             *out++ = static_cast<int8_t>(encode | more_bytes);
             encode >>= 7;
@@ -51,25 +48,24 @@ public:
 
     template<typename Range>
     static result deserialize(Range&& r) noexcept {
-        UValueType result = 0;
+        uvalue_type result = 0;
         uint8_t shift = 0;
-        vint_size_type bytes_read = 0;
+        size_type bytes_read = 0;
         for (auto src = r.begin(); src != r.end(); ++src) {
             bytes_read++;
             uint64_t byte = *src;
-            if (byte & more_bytes) {
+            if (has_more_bytes(byte)) {
                 result |= (byte & (more_bytes - 1)) << shift;
             } else {
                 result |= byte << shift;
                 break;
             }
-            shift = std::min(std::numeric_limits<ValueType>::digits, shift + 7);
+            shift = std::min(std::numeric_limits<value_type>::digits, shift + 7);
         }
         return {decode_zigzag(result), bytes_read};
     }
+
+    static bool has_more_bytes(int8_t byte) {
+        return byte & more_bytes;
+    }
 };
-
-} // namespace internal
-
-using vint = vint_internal::vint_base<int32_t>;
-using vlong = vint_internal::vint_base<int64_t>;
