1ba89891ad4dec1fc34e3a6d4971d9ed7982e198
Michal Maslanka <michal@vectorized.io>
2019-08-14 13:38:08 +0200

    rpk/checkers: implemented network related checkers
    Network related checkers implementation. All of the checkers are defined
    in net_checkers.go file.
    The following checkers were implemented:
    - NIC IRQ affinity
    - NIC IRQ banned in balance service
    - NIC RPS Set
    - NIC RFS Set
    - NIC XPS Set
    - RFS table size
    - Listen backlog size
    - Syn() backlog size
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/go/pkg/tuners/network/const.go
+++ src/go/pkg/tuners/network/const.go
@@ -1,5 +1,11 @@
 package network
 
 const (
-	maxInt = int(^uint(0) >> 1)
+	rfsTableSizeProperty = "net.core.rps_sock_flow_entries"
+	listenBacklogFile    = "/proc/sys/net/core/somaxconn"
+	synBacklogFile       = "/proc/sys/net/ipv4/tcp_max_syn_backlog"
+	rfsTableSize         = 32768
+	synBacklogSize       = 4096
+	listenBacklogSize    = 4096
+	maxInt               = int(^uint(0) >> 1)
 )
---
+++ src/go/pkg/tuners/network/nics.go
@@ -0,0 +1,173 @@
+package network
+
+import (
+	"fmt"
+	"vectorized/pkg/tuners/irq"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func getDefaultMode(
+	nic Nic, cpuMask string, cpuMasks irq.CpuMasks,
+) (irq.Mode, error) {
+
+	if nic.IsHwInterface() {
+		rxQueuesCount, err := nic.GetRxQueueCount()
+		if err != nil {
+			return "", err
+		}
+		log.Debugf("Calculating default mode for '%s'", nic.Name())
+		numOfCores, err := cpuMasks.GetNumberOfCores(cpuMask)
+		if err != nil {
+			return "", err
+		}
+		numOfPUs, err := cpuMasks.GetNumberOfPUs(cpuMask)
+		if err != nil {
+			return "", err
+		}
+		log.Debugf("Considering '%d' cores and '%d' PUs", numOfCores, numOfPUs)
+
+		if numOfPUs <= 4 || rxQueuesCount == int(numOfPUs) {
+			return irq.Mq, nil
+		} else if numOfCores <= 4 {
+			return irq.Sq, nil
+		} else {
+			return irq.SqSplit, nil
+		}
+	}
+
+	if nic.IsBondIface() {
+		defaultMode := irq.Mq
+		slaves, err := nic.Slaves()
+		if err != nil {
+			return "", err
+		}
+		for _, slave := range slaves {
+			slaveDefaultMode, err := getDefaultMode(slave, cpuMask, cpuMasks)
+			if err != nil {
+				return "", err
+			}
+			if slaveDefaultMode == irq.Sq {
+				defaultMode = irq.Sq
+			} else if slaveDefaultMode == irq.SqSplit && defaultMode == irq.Mq {
+				defaultMode = irq.SqSplit
+			}
+		}
+		return defaultMode, nil
+	}
+	return "", fmt.Errorf("Virutal device %s is not supported", nic.Name())
+}
+
+func getRpsCPUMask(
+	nic Nic, mode irq.Mode, cpuMask string, cpuMasks irq.CpuMasks,
+) (string, error) {
+	effectiveCPUMask, err := cpuMasks.BaseCpuMask(cpuMask)
+	if err != nil {
+		return "", err
+	}
+	var effectiveMode = mode
+	if mode == irq.Default {
+		effectiveMode, err = getDefaultMode(nic, effectiveCPUMask, cpuMasks)
+		if err != nil {
+			return "", err
+		}
+	}
+	computationsCPUMask, err := cpuMasks.CpuMaskForComputations(
+		effectiveMode, effectiveCPUMask)
+	if err != nil {
+		return "", err
+	}
+	return computationsCPUMask, nil
+}
+
+func getHwInterfaceIRQsDistribution(
+	nic Nic, mode irq.Mode, cpuMask string, cpuMasks irq.CpuMasks,
+) (map[int]string, error) {
+	effectiveCPUMask, err := cpuMasks.BaseCpuMask(cpuMask)
+	if err != nil {
+		return nil, err
+	}
+	var effectiveMode = mode
+	if mode == irq.Default {
+		effectiveMode, err = getDefaultMode(nic, effectiveCPUMask, cpuMasks)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	maxRxQueues, err := nic.GetMaxRxQueueCount()
+	if err != nil {
+		return nil, err
+	}
+	allIRQs, err := nic.GetIRQs()
+	if err != nil {
+		return nil, err
+	}
+
+	irqCPUMask, err := cpuMasks.CpuMaskForIRQs(effectiveMode, effectiveCPUMask)
+	if err != nil {
+		return nil, err
+	}
+
+	if maxRxQueues >= len(allIRQs) {
+		log.Debugf("Calculating distribution '%s' IRQs", nic.Name())
+		IRQsDistribution, err := cpuMasks.GetIRQsDistributionMasks(
+			allIRQs, irqCPUMask)
+		if err != nil {
+			return nil, err
+		}
+		return IRQsDistribution, nil
+	}
+
+	rxQueues, err := nic.GetRxQueueCount()
+	if err != nil {
+		return nil, err
+	}
+	log.Debugf("Number of Rx queues for '%s' = '%d'", nic.Name(), rxQueues)
+	log.Infof("Distributing '%s' IRQs handling Rx queues", nic.Name())
+	IRQsDistribution, err := cpuMasks.GetIRQsDistributionMasks(
+		allIRQs[0:rxQueues], irqCPUMask)
+	if err != nil {
+		return nil, err
+	}
+	log.Infof("Distributing rest of '%s' IRQs", nic.Name())
+	restIRQsDistribution, err := cpuMasks.GetIRQsDistributionMasks(
+		allIRQs[rxQueues:], irqCPUMask)
+	if err != nil {
+		return nil, err
+	}
+	for irq, mask := range restIRQsDistribution {
+		IRQsDistribution[irq] = mask
+	}
+	return IRQsDistribution, nil
+}
+
+func collectIRQs(nic Nic) ([]int, error) {
+	var IRQs []int
+	if nic.IsHwInterface() {
+		nicIRQs, err := nic.GetIRQs()
+		if err != nil {
+			return nil, err
+		}
+		IRQs = append(IRQs, nicIRQs...)
+	}
+	if nic.IsBondIface() {
+		slaves, err := nic.Slaves()
+		if err != nil {
+			return nil, err
+		}
+		for _, slave := range slaves {
+			slaveIRQs, err := collectIRQs(slave)
+			if err != nil {
+				return nil, err
+			}
+			IRQs = append(IRQs, slaveIRQs...)
+
+		}
+	}
+	return IRQs, nil
+}
+
+func oneRPSQueueLimit(limits []string) int {
+	return rfsTableSize / len(limits)
+}
