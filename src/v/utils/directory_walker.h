702851e316a7027715d359fff1f78a2a0317aa0e
Duarte Nunes <duarte@vectorized.io>
2019-08-19 11:18:51 -0300

    utils: Cleanup directory_walker
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>
    Message-Id: <20190819141851.20889-2-duarte@vectorized.io>

--- src/v/utils/directory_walker.h
+++ src/v/utils/directory_walker.h
@@ -3,10 +3,10 @@
 #include "seastarx.h"
 
 #include <seastar/core/file.hh>
-#include <seastar/core/reactor.hh>
-#include <seastar/core/shared_ptr.hh>
+#include <seastar/core/seastar.hh>
 #include <seastar/core/sstring.hh>
 
+#include <memory>
 #include <utility>
 
 /// \brief map over all entries of a directory
@@ -15,32 +15,18 @@
 /// return directory_walker::walk(ns, [ns, this](directory_entry de) {
 ///   return visit_topic(ns, std::move(de));
 /// });
-///
 struct directory_walker {
-    using func_t = std::function<future<>(directory_entry)>;
+    using walker_type = std::function<future<>(directory_entry)>;
 
-    template<typename Func>
-    static future<> walk(sstring dirname, Func f) {
+    static future<> walk(sstring dirname, walker_type walker_func) {
         return open_directory(dirname).then(
-          [f = std::move(f)](auto d) mutable {
-              auto w = make_lw_shared<directory_walker>(
-                std::move(d), std::move(f));
-              return w->done().finally([w] {});
+          [walker_func = std::move(walker_func)](file f) mutable {
+              auto s = std::make_unique<subscription<directory_entry>>(
+                f.list_directory(std::move(walker_func)));
+              return s->done().finally(
+                [f = std::move(f), s = std::move(s)]() mutable {
+                    return f.close().finally([f = std::move(f)] {});
+                });
           });
     }
-
-    explicit directory_walker(file d, func_t f)
-      : directory(make_lw_shared<file>(std::move(d)))
-      , listing(directory->list_directory(std::move(f))) {
-    }
-
-    ~directory_walker() = default;
-
-    future<> done() {
-        auto l = directory;
-        return listing.done().then([l] { return l->close(); }).finally([l] {});
-    }
-
-    lw_shared_ptr<file> directory;
-    subscription<directory_entry> listing;
 };
