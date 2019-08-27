6558d6fd57e0d62c7f7ab0772a520bf5e1b37cf9
Duarte Nunes <duarte@vectorized.io>
2019-08-26 21:11:31 -0300

    storage: Add utility function to build path
    Similar to the unix command mkdir -p, create a directory by ensuring
    all missing directories in the path are created as well.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

---
+++ src/v/storage/directories.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "seastarx.h"
+#include "syschecks/syschecks.h"
+
+#include <seastar/core/future.hh>
+#include <seastar/core/seastar.hh>
+#include <seastar/core/sstring.hh>
+
+namespace storage::directories {
+
+static future<> initialize_directory(sstring dir) {
+    return recursive_touch_directory(dir)
+      .handle_exception([this, dir](std::exception_ptr ep) {
+          stlog.error(
+            "Directory `{}` cannot be initialized. Failed with {}", dir, ep);
+          return make_exception_future<>(std::move(ep));
+      })
+      .then([dir] {
+          stlog.info("Checking `{}` for supported filesystems", dir);
+          return syschecks::disk(dir);
+      });
+}
+
+} // namespace storage::directories
