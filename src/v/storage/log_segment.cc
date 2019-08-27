fd8143b611b79d2f9415d75aac697ecb72b4855f
Duarte Nunes <duarte@vectorized.io>
2019-08-26 21:33:25 -0300

    storage: Add container for segments
    The log_set class is a container for log_segments, which contains an
    ordered list of segments by their base offsets.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

--- src/v/storage/log_segment.cc
+++ src/v/storage/log_segment.cc
@@ -34,4 +34,40 @@ log_segment_appender log_segment::data_appender(const io_priority_class& pc) {
     options.io_priority_class = pc;
     return log_segment_appender(_data_file, std::move(options));
 }
+
+struct base_offset_ordering {
+    bool
+    operator()(const log_segment_ptr& seg1, const log_segment_ptr& seg2) const {
+        return seg1->base_offset() <= seg2->base_offset();
+    }
+    bool operator()(const log_segment_ptr& seg, model::offset value) const {
+        return value <= seg->base_offset();
+    }
+};
+
+log_set::log_set(std::vector<log_segment_ptr> segs) noexcept(
+  log_set::is_nothrow::value)
+  : _segments(std::move(segs)) {
+    std::sort(_segments.begin(), _segments.end(), base_offset_ordering{});
+}
+
+log_set::generation_advancer::generation_advancer(log_set& log_set) noexcept
+  : _log_set(log_set) {
+}
+
+log_set::generation_advancer::~generation_advancer() {
+    ++_log_set._generation;
+}
+
+void log_set::add(log_segment_ptr seg) {
+    generation_advancer ea(*this);
+    _segments.push_back(std::move(seg));
+}
+
+void log_set::pop_last() {
+    generation_advancer ea(*this);
+    _segments.pop_back();
+}
+
+
 } // namespace storage
