68513c9991a0e4f46061b84e033b7235b5cf870a
Duarte Nunes <duarte@vectorized.io>
2019-08-26 21:33:25 -0300

    storage: Add container for segments
    The log_set class is a container for log_segments, which contains an
    ordered list of segments by their base offsets.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

--- src/v/storage/log_segment.h
+++ src/v/storage/log_segment.h
@@ -10,6 +10,10 @@
 #include <seastar/core/iostream.hh>
 #include <seastar/util/log.hh>
 
+#include <optional>
+#include <type_traits>
+#include <vector>
+
 namespace storage {
 
 class log_segment {
@@ -76,5 +80,82 @@ private:
 };
 
 using log_segment_ptr = lw_shared_ptr<log_segment>;
+
+/*
+ * A container for log segments. Usage:
+ *
+ * log_set l;
+ * l.add(some_log_segment);
+ * ...
+ * l.add(another_log_segment);
+ * ...
+ * for (auto seg : l) {
+ *   // Do something with the segment
+ * }
+ */
+class log_set {
+public:
+    using const_iterator_type = std::vector<log_segment_ptr>::const_iterator;
+    using iterator_type = std::vector<log_segment_ptr>::iterator;
+
+    using iter_gen_type = uint64_t;
+
+    static constexpr iter_gen_type invalid_iter_gen = 0;
+
+    using is_nothrow
+      = std::is_nothrow_move_constructible<std::vector<log_segment_ptr>>;
+
+    explicit log_set(std::vector<log_segment_ptr>) noexcept(is_nothrow::value);
+
+    size_t size() {
+        return _segments.size();
+    }
+
+    /// New segments must be monotonically increasing in base offset
+    void add(log_segment_ptr);
+
+    void pop_last();
+
+    log_segment_ptr last() const {
+        return _segments.back();
+    }
+
+    const_iterator_type begin() const {
+        return _segments.begin();
+    }
+
+    const_iterator_type end() const {
+        return _segments.end();
+    }
+
+    iterator_type begin() {
+        return _segments.begin();
+    }
+
+    iterator_type end() {
+        return _segments.end();
+    }
+
+    // On generation changes, the iterators are invalidated.
+    iter_gen_type iter_gen() const {
+        return _generation;
+    }
+
+private:
+    class generation_advancer {
+    public:
+        explicit generation_advancer(log_set&) noexcept;
+        ~generation_advancer();
+
+    private:
+        log_set& _log_set;
+    };
+
+    friend class generation_advancer;
+
+private:
+    std::vector<log_segment_ptr> _segments;
+    iter_gen_type _generation = 1;
+};
 
 } // namespace storage
