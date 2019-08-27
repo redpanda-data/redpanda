ccae545d6a77d49680dc37ee82ba03d779becf43
Duarte Nunes <duarte@vectorized.io>
2019-08-26 22:24:02 -0300

    storage: Add log writting
    This patch adds the log_writer class, which is reponsible for
    appending to a log's active segment. The log and the writer are a
    cohesive unit: if the maximum size of the current log segment is
    reached, we roll the current log segment.
    
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

---
+++ src/v/storage/log_writer.cc
@@ -0,0 +1,100 @@
+#include "storage/log_writer.h"
+
+#include "storage/log.h"
+#include "utils/vint.h"
+
+#include <seastar/core/byteorder.hh>
+#include <seastar/core/unaligned.hh>
+
+#include <type_traits>
+
+namespace storage {
+
+default_log_writer::default_log_writer(log& log) noexcept
+  : _log(log) {
+}
+
+template<typename T>
+typename std::enable_if_t<std::is_integral<T>::value, seastar::future<>>
+write(log_segment_appender& out, T i) {
+    auto* nr = reinterpret_cast<const unaligned<T>*>(&i);
+    i = cpu_to_be(*nr);
+    auto p = reinterpret_cast<const char*>(&i);
+    return out.append(p, sizeof(T));
+}
+
+future<> write_vint(log_segment_appender& out, vint::value_type v) {
+    std::array<bytes::value_type, vint::max_length> encoding_buffer;
+    const auto size = vint::serialize(v, encoding_buffer.begin());
+    return out.append(
+      reinterpret_cast<const char*>(encoding_buffer.data()), size);
+}
+
+future<>
+write(log_segment_appender& out, const fragmented_temporary_buffer& buf) {
+    return out.append(buf);
+}
+
+future<> write(log_segment_appender& out, const model::record& record) {
+    return write_vint(out, record.size_bytes())
+      .then([&] {
+          // Note that we don't append the unused Kafka attributes, but we
+          // do take them into account when calculating the batch checksum.
+          return write_vint(out, record.timestamp_delta());
+      })
+      .then([&] { return write_vint(out, record.offset_delta()); })
+      .then([&] { return write_vint(out, record.key().size_bytes()); })
+      .then([&] { return write(out, record.key()); })
+      .then([&] { return write(out, record.packed_value_and_headers()); });
+}
+
+future<stop_iteration> default_log_writer::
+operator()(model::record_batch&& batch) {
+    return do_with(std::move(batch), [this](model::record_batch& batch) {
+        auto& appender = _log.appender();
+        return write_vint(appender, batch.size_bytes())
+          .then([&appender, &batch] {
+              return write(appender, batch.base_offset().value());
+          })
+          .then([&appender, &batch] { return write(appender, batch.crc()); })
+          .then([&appender, &batch] {
+              return write(appender, batch.attributes().value());
+          })
+          .then([&appender, &batch] {
+              return write(appender, batch.last_offset_delta());
+          })
+          .then([&appender, &batch] {
+              return write(appender, batch.first_timestamp().value());
+          })
+          .then([&appender, &batch] {
+              return write(appender, batch.max_timestamp().value());
+          })
+          .then([&appender, &batch] {
+              // Note that we don't append the unused Kafka fields, but we do
+              // take them into account when calculating the batch checksum.
+              return write(appender, static_cast<int32_t>(batch.size()));
+          })
+          .then([&appender, &batch] {
+              if (batch.compressed()) {
+                  return write(
+                    appender, batch.get_compressed_records().records());
+              }
+              return do_for_each(
+                batch, [&appender](const model::record& record) {
+                    return write(appender, record);
+                });
+          })
+          .then([this, &batch] {
+              _last_offset = batch.last_offset();
+              return _log.maybe_roll(_last_offset).then([] {
+                  return stop_iteration::no;
+              });
+          });
+    });
+}
+
+model::offset default_log_writer::end_of_stream() {
+    return _last_offset;
+}
+
+} // namespace storage
---
+++ src/v/storage/log_writer.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "model/fundamental.h"
+#include "model/record.h"
+#include "seastarx.h"
+
+#include <seastar/core/future.hh>
+
+namespace storage {
+
+class log;
+
+// Log writer interface, which represents a
+// consumer for record_batch_readers. It accepts
+// a concrete implementation, which knows how to serialize
+// data at a particular version.
+class log_writer {
+public:
+    class impl {
+    public:
+        virtual ~impl() {
+        }
+
+        virtual future<stop_iteration> operator()(model::record_batch&&) = 0;
+        virtual model::offset end_of_stream() = 0;
+    };
+
+    log_writer(std::unique_ptr<impl> impl) noexcept
+      : _impl(std::move(impl)) {
+    }
+
+    log_writer(log_writer&& o) = default;
+    log_writer& operator=(log_writer&& o) = default;
+
+    future<stop_iteration> operator()(model::record_batch&& batch) {
+        return _impl->operator()(std::move(batch));
+    }
+
+    model::offset end_of_stream() {
+        return _impl->end_of_stream();
+    }
+
+private:
+    std::unique_ptr<impl> _impl;
+};
+
+// Serializer for the current batch version.
+class default_log_writer : public log_writer::impl {
+public:
+    default_log_writer(log&) noexcept;
+
+    virtual future<stop_iteration> operator()(model::record_batch&&) override;
+
+    virtual model::offset end_of_stream() override;
+
+private:
+    log& _log;
+    model::offset _last_offset;
+};
+
+} // namespace storage
