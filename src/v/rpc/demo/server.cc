7121217c332950c343e308259a2c98deed67230b
Michal Maslanka <michal@vectorized.io>
2019-09-03 07:58:36 +0200

    rpc/demo: tls support in rpc demo app
    Added TLS support in RPC demo application. Both client and server can
    now use TLS to create secured connections.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/rpc/demo/server.cc
+++ src/v/rpc/demo/server.cc
@@ -6,6 +6,7 @@
 
 #include <seastar/core/app-template.hh>
 #include <seastar/core/distributed.hh>
+#include <seastar/core/thread.hh>
 
 #include <string>
 
@@ -17,6 +18,12 @@ void cli_opts(boost::program_options::options_description_easy_init o) {
       po::value<std::string>()->default_value("127.0.0.1"),
       "ip to connect to");
     o("port", po::value<uint16_t>()->default_value(20776), "port for service");
+    o("key",
+      po::value<std::string>()->default_value(""),
+      "key for TLS seccured connection");
+    o("cert",
+      po::value<std::string>()->default_value(""),
+      "cert for TLS seccured connection");
 }
 
 int main(int args, char** argv, char** env) {
@@ -28,12 +35,23 @@ int main(int args, char** argv, char** env) {
     return app.run_deprecated(args, argv, [&] {
         seastar::engine().at_exit([&serv] { return serv.stop(); });
         auto& cfg = app.configuration();
+
+    return async([&] {
         rpc::server_configuration scfg;
         scfg.addrs.push_back(socket_address(
           ipv4_addr(cfg["ip"].as<std::string>(), cfg["port"].as<uint16_t>())));
         scfg.max_service_memory_per_core = memory::stats().total_memory()
                                            * .9 /*90%*/;
-        return serv.start(scfg)
+        auto key = cfg["key"].as<std::string>();
+        auto cert = cfg["cert"].as<std::string>();
+        if (key != "" && cert != "") {
+            auto builder = tls::credentials_builder();
+            builder.set_dh_level(tls::dh_params::level::MEDIUM);
+            builder.set_x509_key_file(cert, key, tls::x509_crt_format::PEM)
+                .get();
+            scfg.credentials = std::move(builder);
+        }
+        serv.start(scfg)
           .then([&serv] {
               lgr.info("registering service");
               return serv.invoke_on_all(
@@ -42,6 +60,7 @@ int main(int args, char** argv, char** env) {
           .then([&serv] {
               lgr.info("Invoking rpc start on all cores");
               return serv.invoke_on_all(&rpc::server::start);
-          });
+          }).get();
+      });
     });
 }
