41f9a5c1071ccf9529635e29ef32bf4498e4ff86
Noah Watkins <noah@vectorized.io>
2019-09-05 14:30:09 -0700

    kafka: add find coordinator request message type
    Signed-off-by: Noah Watkins <noah@vectorized.io>

---
+++ src/v/redpanda/kafka/requests/find_coordinator_request.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "redpanda/kafka/requests/headers.h"
+#include "redpanda/kafka/requests/request_context.h"
+#include "redpanda/kafka/requests/response.h"
+#include "seastarx.h"
+
+#include <seastar/core/future.hh>
+
+namespace kafka::requests {
+
+class find_coordinator_request final {
+public:
+    static constexpr api_key key = api_key(10);
+    static constexpr api_version min_supported = api_version(0);
+    static constexpr api_version max_supported = api_version(2);
+
+    static future<response_ptr> process(request_context&, smp_service_group);
+};
+
+} // namespace kafka::requests
