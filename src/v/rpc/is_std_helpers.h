acc3ae6e443b6c37cfd3665349e6035f3db503c6
Alexander Gallego <alex@vectorized.io>
2019-09-07 00:59:19 -0700

    rpc: is_std_vector -> is_std_helpers
    In order to support multiple stl containers, we want to bundle these
    helpers in a small little util file that helps us with the static
    refelction for the serialization/deserialization of the RPC layer.

--- src/v/rpc/is_std_helpers.h
+++ src/v/rpc/is_std_helpers.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <type_traits>
 #include <vector>
 
@@ -10,4 +11,12 @@ template<typename... Args>
 struct is_std_vector<std::vector<Args...>> : std::true_type {};
 template<typename T>
 inline constexpr bool is_std_vector_v = is_std_vector<T>::value;
+
+template<typename T>
+struct is_std_optional : std::false_type {};
+template<typename... Args>
+struct is_std_optional<std::optional<Args...>> : std::true_type {};
+template<typename T>
+inline constexpr bool is_std_optional_v = is_std_optional<T>::value;
+
 } // namespace rpc
