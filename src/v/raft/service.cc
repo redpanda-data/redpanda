c8b6766fdbcbcda654051125753d21545a7e1c0f
Alexander Gallego <alex@vectorized.io>
2019-09-07 01:07:21 -0700

    raft: add service definition for RPC handling

---
+++ src/v/raft/service.cc
@@ -0,0 +1,24 @@
+#include "raft/service.h"
+
+namespace raft {
+service::service(scheduling_group& sc, smp_service_group& ssg)
+  : raftgen_service(sc, ssg) {
+}
+
+future<vote_reply> service::vote(vote_request, rpc::streaming_context&) {
+    throw std::runtime_error("unimplemented method");
+}
+
+future<append_entries_reply>
+service::append_entries(append_entries_request, rpc::streaming_context&) {
+    throw std::runtime_error("unimplemented method");
+}
+
+future<configuration_reply>
+service::configuration_update(configuration_request, rpc::streaming_context&) {
+    throw std::runtime_error("unimplemented method");
+}
+
+service::~service() {
+}
+} // namespace raft
