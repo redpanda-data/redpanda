4f9f00d3a818f32af09502b9225824ffe31dee01
Alexander Gallego <alex@vectorized.io>
2019-09-09 17:59:17 -0700

    build: remove gtest dependency altogether

--- src/v/hashing/xx_tests.cc
+++ src/v/hashing/xx_tests.cc
@@ -1,18 +1,15 @@
+#define BOOST_TEST_MODULE xxhash
 #include "hashing/xx.h"
 
-#include <gtest/gtest.h>
+#include <boost/test/unit_test.hpp>
 
 #include <utility>
 
-TEST(xx, incremental_same_as_array) {
+BOOST_AUTO_TEST_CASE(incremental_same_as_array) {
     incremental_xxhash64 inc;
     inc.update(1);
     inc.update(2);
     inc.update(42);
-    ASSERT_EQ(inc.digest(), xxhash_64(std::array{1, 2, 42}));
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    std::array<int, 3> arr = {1, 2, 42};
+    BOOST_CHECK_EQUAL(inc.digest(), xxhash_64(arr));
 }
