b78de88b3d1e595bd43a1a40ff2aedae9e507488
Alexander Gallego <alex@vectorized.io>
2019-09-09 17:54:35 -0700

    utils: split human.h into implementation and header file

---
+++ src/v/utils/human.cc
@@ -0,0 +1,30 @@
+#include "utils/human.h"
+
+#include "seastarx.h"
+
+#include <seastar/core/print.hh>
+
+std::ostream& operator<<(std::ostream& o, const ::human::latency& l) {
+    static const char* units[] = {"μs", "ms", "secs"};
+    static constexpr double step = 1000;
+    auto x = l.value;
+    for (size_t i = 0; i < 3; ++i) {
+        if (x <= step) {
+            return fmt_print(o, "{:03.3f}{}", x, units[i]);
+        }
+        x /= step;
+    }
+    return o << x << "uknown_units";
+}
+std::ostream& operator<<(std::ostream& o, const ::human::bytes& l) {
+    static const char* units[] = {"bytes", "MiB", "GiB", "TiB", "PiB"};
+    static constexpr double step = 1024;
+    auto x = l.value;
+    for (size_t i = 0; i < 5; ++i) {
+        if (x <= step) {
+            return fmt_print(o, "{:03.3f}{}", x, units[i]);
+        }
+        x /= step;
+    }
+    return o << x << "uknown_units";
+}
