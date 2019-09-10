e5584822b10bd8ad017a9f6e736456d1b1fede20
Alexander Gallego <alex@vectorized.io>
2019-09-09 17:59:17 -0700

    build: remove gtest dependency altogether

--- src/v/random/random_test.cc
+++ src/v/random/random_test.cc
@@ -1,21 +1,17 @@
+#define BOOST_TEST_MODULE xxhash
 #include "random/fast_prng.h"
 
-#include <gtest/gtest.h>
+#include <boost/test/unit_test.hpp>
 
 #include <set>
 #include <utility>
 
-TEST(fast_prng, basic_gen_100_unique_rands) {
+BOOST_AUTO_TEST_CASE(fast_prng_basic_gen_100_unique_rands) {
     fast_prng rng;
     std::set<uint32_t> test;
     for (auto i = 0; i < 100; ++i) {
         uint32_t x = rng();
-        ASSERT_FALSE(test.count(x));
+        BOOST_CHECK(!test.count(x));
         test.insert(x);
     }
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
