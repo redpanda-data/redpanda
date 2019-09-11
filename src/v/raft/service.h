b4d4a5af0d3ebc3a705525ffaa2aea69e658b5f0
Alexander Gallego <alex@vectorized.io>
2019-09-11 09:06:21 -0700

    raft: pass through the new consensus interface to the service tier
    The service tier is the translation service between network
    and the actual consensus module. Dispatch all calls in correct scheduling
    groups & service groups for the submodule

--- src/v/raft/service.h
+++ src/v/raft/service.h
@@ -1,12 +1,20 @@
 #pragma once
 
+#include "raft/consensus.h"
+#include "raft/node_local_controller.h"
 #include "raft/raftgen_service.h"
+
+#include <seastar/core/sharded.hh>
 
 namespace raft {
 class service final : public raftgen_service {
 public:
     using failure_probes = raftgen_service::failure_probes;
-    service(scheduling_group& sc, smp_service_group& ssg);
+    service(
+      scheduling_group sc,
+      smp_service_group ssg,
+      sharded<consensus>&,
+      sharded<node_local_controller>&);
     ~service();
 
     [[gnu::always_inline]] future<vote_reply>
@@ -42,5 +50,7 @@ private:
     do_configuration_update(configuration_request, rpc::streaming_context&);
 
     failure_probes _probe;
+    sharded<consensus>& _consensus;
+    sharded<node_local_controller>& _nlc;
 };
 } // namespace raft
