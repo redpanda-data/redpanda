4386cd731c1450431ed93dd6e7bb742bbbc66704
Alexander Gallego <alex@vectorized.io>
2019-09-11 09:01:33 -0700

    raft: add reconnect_client.h with exponential backoff
    Expose only one method which will either always return a
    disconnected_client_exception() or a valid raftgen_service::client

---
+++ src/v/raft/reconnect_client.cc
@@ -0,0 +1,56 @@
+#include "raft/reconnect_client.h"
+
+#include "raft/logger.h"
+
+#include <chrono>
+#include <functional>
+
+namespace raft {
+namespace ch = std::chrono; // NOLINT
+
+static inline bool
+has_backoff_expired(rpc::clock_type::time_point stamp, int32_t backoff) {
+    auto now = rpc::clock_type::now();
+    if (now < stamp) {
+        return false;
+    }
+    auto const secs = ch::duration_cast<ch::seconds>(now - stamp).count();
+    return secs < backoff;
+}
+uint32_t static inline next_backoff(uint32_t current_backoff) {
+    return std::max<uint32_t>(300, std::max<uint32_t>(1, current_backoff) << 1);
+}
+
+future<> reconnect_client::stop() {
+    _backoff_secs = std::numeric_limits<uint32_t>::max();
+    return _dispatch_gate.close().then([this] { return _client.stop(); });
+}
+
+future<> reconnect_client::reconnect() {
+    if (!has_backoff_expired(_stamp, _backoff_secs)) {
+        return make_exception_future<>(disconnected_client_exception());
+    }
+    _stamp = rpc::clock_type::now();
+    return with_gate(_dispatch_gate, [this] {
+        return with_semaphore(_connected_sem, 1, [this] {
+            if (_client.is_valid()) {
+                return make_ready_future<>();
+            }
+            return _client.connect().then_wrapped([this](future<> f) {
+                try {
+                    f.get();
+                    _backoff_secs = 0;
+                    return make_ready_future<>();
+                } catch (...) {
+                    _backoff_secs = next_backoff(_backoff_secs);
+                    raftlog().debug(
+                      "Error reconnecting: {}", std::current_exception());
+                    // keep the exception interface consistent
+                    return make_exception_future<>(
+                      disconnected_client_exception());
+                }
+            });
+        });
+    });
+}
+} // namespace raft
