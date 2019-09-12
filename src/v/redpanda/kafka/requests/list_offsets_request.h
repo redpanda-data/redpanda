3d6e79a7352302a34119feeae95168540b3d9813
Noah Watkins <noah@vectorized.io>
2019-09-12 13:49:55 -0700

    kafka: add list offsets request
    Signed-off-by: Noah Watkins <noah@vectorized.io>

---
+++ src/v/redpanda/kafka/requests/list_offsets_request.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "redpanda/kafka/requests/headers.h"
+#include "redpanda/kafka/requests/request_context.h"
+#include "redpanda/kafka/requests/response.h"
+#include "seastarx.h"
+
+#include <seastar/core/future.hh>
+
+namespace kafka::requests {
+
+class list_offsets_request final {
+public:
+    static constexpr const char* name = "list_offsets";
+    static constexpr api_key key = api_key(2);
+    static constexpr api_version min_supported = api_version(1);
+    static constexpr api_version max_supported = api_version(1);
+
+    static future<response_ptr> process(request_context&&, smp_service_group);
+};
+
+} // namespace kafka::requests
