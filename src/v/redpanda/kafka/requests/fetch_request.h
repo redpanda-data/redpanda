fbbc0aaf6fdd59d99638f02ea43cb5a3435471e1
Noah Watkins <noah@vectorized.io>
2019-09-12 13:50:24 -0700

    kafka: add fetch request
    this writes random batches using the random batch reader interface. the
    batches are fake now and created and consumed from memory.
    
    this patch enables the simple kafka-python program to run indefinitely.
    
    ```python
    from kafka import KafkaConsumer
    consumer = KafkaConsumer('my_favorite_topic',
    bootstrap_servers='127.0.0.1:9092')
    for msg in consumer:
        print(msg)
    ```
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

---
+++ src/v/redpanda/kafka/requests/fetch_request.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "redpanda/kafka/requests/headers.h"
+#include "redpanda/kafka/requests/request_context.h"
+#include "redpanda/kafka/requests/response.h"
+#include "seastarx.h"
+
+#include <seastar/core/future.hh>
+
+namespace kafka::requests {
+
+class fetch_request final {
+public:
+    static constexpr const char* name = "fetch";
+    static constexpr api_key key = api_key(1);
+    static constexpr api_version min_supported = api_version(4);
+    static constexpr api_version max_supported = api_version(4);
+
+    static future<response_ptr> process(request_context&&, smp_service_group);
+};
+
+} // namespace kafka::requests
