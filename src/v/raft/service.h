0b6f5d60cf0ba98d53214e8c1b417d1fac265696
Alexander Gallego <alex@vectorized.io>
2019-09-18 14:19:52 -0700

    raft/service: Use c++ concepts to break concrete type dependency
    Instead of depending on concrete types for finding a raft::consensus from
    a raft::group_id, allow the type to be wired
    at redpanda/main.cc.
    
    This allows us to break a circular dependency between the cluster/*
    submodule and the raft/* submodule

--- src/v/raft/service.h
+++ src/v/raft/service.h
@@ -1,21 +1,40 @@
 #pragma once
 
 #include "raft/consensus.h"
-#include "raft/node_local_controller.h"
+#include "raft/group_shard_table.h"
 #include "raft/raftgen_service.h"
 
 #include <seastar/core/sharded.hh>
 
 namespace raft {
+// clang-format off
+CONCEPT(template<typename Manager> concept bool RaftGroupManager() {
+    return requires(Manager m, group_id g) {
+       { m.consensus_for(std::move(g)) } -> consensus&;
+    };
+})
+// clang-format on
+
+template<typename Manager>
+CONCEPT(requires RaftGroupManager<Manager>())
 class service final : public raftgen_service {
 public:
     using failure_probes = raftgen_service::failure_probes;
     service(
       scheduling_group sc,
       smp_service_group ssg,
-      sharded<consensus>&,
-      sharded<node_local_controller>&);
-    ~service();
+      sharded<Manager>& mngr,
+      sharded<group_shard_table>& tbl)
+      : raftgen_service(sc, ssg)
+      , _group_manager(mngr)
+      , _shard_table(tbl) {
+        finjector::shard_local_badger().register_probe(
+          failure_probes::name(), &_probe);
+    }
+    ~service() {
+        finjector::shard_local_badger().deregister_probe(
+          failure_probes::name());
+    }
 
     [[gnu::always_inline]] future<vote_reply>
     vote(vote_request r, rpc::streaming_context& ctx) {
@@ -32,25 +51,37 @@ public:
           });
     }
 
-    [[gnu::always_inline]] future<configuration_reply>
-    configuration_update(configuration_request r, rpc::streaming_context& ctx) {
-        return _probe.configuration_update().then(
-          [this, r = std::move(r), &ctx]() mutable {
-              return do_configuration_update(std::move(r), ctx);
+private:
+    future<vote_reply> do_vote(vote_request r, rpc::streaming_context&) {
+        auto shard = _shard_table.local().shard_for(group_id(r.group));
+        return with_scheduling_group(
+          get_scheduling_group(), [this, shard, r = std::move(r)]() mutable {
+              return _group_manager.invoke_on(
+                shard,
+                get_smp_service_group(),
+                [this, r = std::move(r)](Manager& m) mutable {
+                    return m.consensus_for(group_id(r.group))
+                      .vote(std::move(r));
+                });
+          });
+    }
+    future<append_entries_reply>
+    do_append_entries(append_entries_request r, rpc::streaming_context&) {
+        auto shard = _shard_table.local().shard_for(group_id(r.meta.group));
+        return with_scheduling_group(
+          get_scheduling_group(), [this, shard, r = std::move(r)]() mutable {
+              return _group_manager.invoke_on(
+                shard,
+                get_smp_service_group(),
+                [this, r = std::move(r)](Manager& m) mutable {
+                    return m.consensus_for(group_id(r.meta.group))
+                      .append_entries(std::move(r));
+                });
           });
     }
 
-private:
-    future<vote_reply> do_vote(vote_request, rpc::streaming_context&);
-
-    future<append_entries_reply>
-    do_append_entries(append_entries_request, rpc::streaming_context&);
-
-    future<configuration_reply>
-    do_configuration_update(configuration_request, rpc::streaming_context&);
-
     failure_probes _probe;
-    sharded<consensus>& _consensus;
-    sharded<node_local_controller>& _nlc;
+    sharded<Manager>& _group_manager;
+    sharded<group_shard_table>& _shard_table;
 };
 } // namespace raft
