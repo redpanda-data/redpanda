7fce1a3c980b71cfb8cbaedb1af4f54f84d0d625
Duarte Nunes <duarte@vectorized.io>
2019-09-25 21:33:55 -0300

    kafka/requests: Add produce API
    Signed-off-by: Duarte Nunes <duarte@vectorized.io>

--- src/v/redpanda/kafka/requests/produce_request.h
+++ src/v/redpanda/kafka/requests/produce_request.h
@@ -13,8 +13,9 @@ class produce_request final {
 public:
     static constexpr const char* name = "produce";
     static constexpr api_key key = api_key(0);
-    static constexpr api_version min_supported = api_version(0);
-    static constexpr api_version max_supported = api_version(3);
+    // We don't support the obsolete Kafka record format
+    static constexpr api_version min_supported = api_version(3);
+    static constexpr api_version max_supported = api_version(7);
 
     static future<response_ptr>
     process(request_context&&, seastar::smp_service_group);
