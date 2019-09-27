bcd2cbecc06d78332ed3e4e8e04c72aafc22dd51
Alexander Gallego <alex@vectorized.io>
2019-09-27 12:17:09 -0700

    storage/log_manager: add usage-comment for sanitize_files option

--- src/v/storage/log_manager.h
+++ src/v/storage/log_manager.h
@@ -19,6 +19,7 @@ static constexpr size_t default_read_buffer_size = 128 * 1024;
 struct log_config {
     sstring base_dir;
     size_t max_segment_size;
+    // used for testing: keeps a backtrace of operations for debugging
     using sanitize_files = bool_class<struct sanitize_files_tag>;
     sanitize_files should_sanitize;
 };
