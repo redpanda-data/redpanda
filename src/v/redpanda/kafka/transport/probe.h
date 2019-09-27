9ea47ff4e61b657bebbefd9531f534b2b9b94ae8
Michal Maslanka <michal@vectorized.io>
2019-09-27 15:11:59 +0200

    kafka/probe.h: change types of some counter to limit memory usage
    Using uint32_t to count some of the events in order
    not to waste the memory and optimize CPU cache usage.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/redpanda/kafka/transport/probe.h
+++ src/v/redpanda/kafka/transport/probe.h
@@ -60,11 +60,11 @@ public:
     }
 
 private:
-    uint64_t _connects = 0;
-    uint64_t _connections = 0;
-    uint64_t _requests_blocked_memory = 0;
     uint64_t _requests_served = 0;
-    uint64_t _requests_serving = 0;
+    uint32_t _requests_serving = 0;
+    uint32_t _requests_blocked_memory = 0;
+    uint64_t _connects = 0;
+    uint32_t _connections = 0;
 };
 
 } // namespace kafka::transport
