f624347301f478d467ad1d37a43a0ff018d71e9a
Michal Maslanka <michal@vectorized.io>
2019-10-08 08:30:56 +0200

    rpc/tests: updated rpc test generated source
    Updated the `rpcgen.h` file with the code generated by latest version of
    `rpcgen.py`.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/rpc/test/rpcgen.h
+++ src/v/rpc/test/rpcgen.h
@@ -24,8 +24,8 @@ class team_movistar_service : public rpc::service {
 public:
     class client;
 
-    team_movistar_service(scheduling_group& sc, smp_service_group& ssg)
-       : _sc(std::ref(sc)), _ssg(std::ref(ssg)) {}
+    team_movistar_service(scheduling_group sc, smp_service_group ssg)
+       : _sc(sc), _ssg(ssg) {}
 
     team_movistar_service(team_movistar_service&& o) noexcept
       : _sc(std::move(o._sc)), _ssg(std::move(o._ssg)), _methods(std::move(o._methods)) {}
@@ -41,11 +41,11 @@ public:
     virtual ~team_movistar_service() noexcept = default;
 
     scheduling_group& get_scheduling_group() override {
-       return _sc.get();
+       return _sc;
     }
 
     smp_service_group& get_smp_service_group() override {
-       return _ssg.get();
+       return _ssg;
     }
 
     rpc::method* method_from_id(uint32_t idx) final {
@@ -60,12 +60,12 @@ public:
     raw_canyon(input_stream<char>& in, rpc::streaming_context& ctx) {
       auto fapply = execution_helper<ultimate_cf_slx, nairo_quintana>();
       return fapply.exec(in, ctx, 3887553648, [this](
-          ultimate_cf_slx t, rpc::streaming_context& ctx) -> future<nairo_quintana> {
+          ultimate_cf_slx&& t, rpc::streaming_context& ctx) -> future<nairo_quintana> {
           return canyon(std::move(t), ctx);
       });
     }
     virtual future<nairo_quintana>
-    canyon(ultimate_cf_slx, rpc::streaming_context&) {
+    canyon(ultimate_cf_slx&&, rpc::streaming_context&) {
        throw std::runtime_error("unimplemented method");
     }
     /// \brief san_francisco -> mount_tamalpais
@@ -73,17 +73,17 @@ public:
     raw_ibis_hakka(input_stream<char>& in, rpc::streaming_context& ctx) {
       auto fapply = execution_helper<san_francisco, mount_tamalpais>();
       return fapply.exec(in, ctx, 3924709550, [this](
-          san_francisco t, rpc::streaming_context& ctx) -> future<mount_tamalpais> {
+          san_francisco&& t, rpc::streaming_context& ctx) -> future<mount_tamalpais> {
           return ibis_hakka(std::move(t), ctx);
       });
     }
     virtual future<mount_tamalpais>
-    ibis_hakka(san_francisco, rpc::streaming_context&) {
+    ibis_hakka(san_francisco&&, rpc::streaming_context&) {
        throw std::runtime_error("unimplemented method");
     }
 private:
-    std::reference_wrapper<scheduling_group> _sc;
-    std::reference_wrapper<smp_service_group> _ssg;
+    scheduling_group _sc;
+    smp_service_group _ssg;
     std::array<rpc::method, 2> _methods{{
       rpc::method([this] (input_stream<char>& in, rpc::streaming_context& ctx) {
          return raw_canyon(in, ctx);
@@ -96,16 +96,16 @@ private:
 class team_movistar_service::client : public rpc::client {
 public:
     client(rpc::client_configuration c)
-      : rpc::client(std::move(c)) {
+      : rpc::client(std::move(c), "team_movistar") {
     }
     virtual inline future<rpc::client_context<nairo_quintana>>
-    canyon(ultimate_cf_slx r) {
+    canyon(ultimate_cf_slx&& r) {
        return send_typed<ultimate_cf_slx, nairo_quintana>(std::move(r), 3887553648);
     }
     virtual inline future<rpc::client_context<mount_tamalpais>>
-    ibis_hakka(san_francisco r) {
+    ibis_hakka(san_francisco&& r) {
        return send_typed<san_francisco, mount_tamalpais>(std::move(r), 3924709550);
     }
 };
 
-}
+} // namespace
