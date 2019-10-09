d47ab240c961dcca0f2769df2898aee04b1131d3
Alexander Gallego <alex@vectorized.io>
2019-10-09 16:45:19 -0700

    ci:build: fix CI, use /tmp instead of /dev/shm on CI machines

--- tools/cmake_test.py
+++ tools/cmake_test.py
@@ -14,15 +14,20 @@ logger = logging.getLogger('rp')
 
 import log
 import shell
+import fs
 
 
 class TestRunner():
-    def __init__(self, binary, repeat, base_directory, copy_files, *args):
+    def __init__(self, binary, repeat, copy_files, *args):
         self.binary = binary
         self.repeat = repeat
-        self.base_directory = base_directory
         self.copy_files = copy_files
         self.test_args = "" if len(args) == 0 else " ".join(map(str, args))
+        self.root = "/dev/shm/vectorized_io"
+        # our ci does not have shared mem on the dockerfile mounted
+        if "CI" in os.environ:
+            self.root = "/tmp/vectorized_io"
+        fs.mkdir_p(self.root)
 
     def _gen_alphanum(self, x=16):
         return ''.join(
@@ -30,9 +35,8 @@ class TestRunner():
             for _ in range(x))
 
     def _gen_testdir(self):
-        return tempfile.mkdtemp(
-            suffix=self._gen_alphanum(),
-            prefix="%s.test." % self.base_directory)
+        return tempfile.mkdtemp(suffix=self._gen_alphanum(),
+                                prefix="%s/test." % self.root)
 
     def _env(self):
         e = os.environ.copy()
@@ -45,12 +49,12 @@ class TestRunner():
             env = self._env()
             logger.info("Test dir: %s" % env["TEST_DIR"])
             if "TEST_DIR" in env:
-                atexit.register(lambda : shutil.rmtree(env["TEST_DIR"]))
+                atexit.register(lambda: shutil.rmtree(env["TEST_DIR"]))
             for f in self.copy_files:
                 logger.debug("Copying input file: %s" % f)
-                (src, dst) = f.split("=") if "=" in f else (f,f)
-                shutil.copy(src, "%s/%s" % (env["TEST_DIR"],
-                                            os.path.basename(dst)))
+                (src, dst) = f.split("=") if "=" in f else (f, f)
+                shutil.copy(src,
+                            "%s/%s" % (env["TEST_DIR"], os.path.basename(dst)))
             for i in range(self.repeat):
                 cmd = "cd %s && %s %s" % (env["TEST_DIR"], self.binary,
                                           self.test_args)
@@ -71,20 +75,14 @@ def main():
             type=str,
             default='DEBUG',
             help='info,debug, type log levels. i.e: --log=debug')
-        parser.add_argument(
-            '--base_directory',
-            type=str,
-            help='usually ${CMAKE_CURRENT_BINARY_DIR}')
-        parser.add_argument(
-            '--repeat',
-            type=int,
-            default=1,
-            help='how many times to repeat test')
-        parser.add_argument(
-            '--copy_file',
-            type=str,
-            action="append",
-            help='copy file to test execution directory')
+        parser.add_argument('--repeat',
+                            type=int,
+                            default=1,
+                            help='how many times to repeat test')
+        parser.add_argument('--copy_file',
+                            type=str,
+                            action="append",
+                            help='copy file to test execution directory')
         return parser
 
     parser = generate_options()
@@ -93,17 +91,14 @@ def main():
     if not options.binary:
         parser.print_help()
         exit(1)
-    if not options.base_directory:
-        parser.print_help()
-        exit(1)
     if not options.copy_file:
         options.copy_file = []
 
     log.set_logger_for_main(getattr(logging, options.log.upper()))
     logger.info("%s *args=%s" % (options, program_options))
 
-    runner = TestRunner(options.binary, options.repeat, options.base_directory,
-                        options.copy_file, *program_options)
+    runner = TestRunner(options.binary, options.repeat, options.copy_file,
+                        *program_options)
     runner.run()
     return 0
 
