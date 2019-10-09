510c04ae861ec3734187a9b161ed8f4d1bd15b72
Alexander Gallego <alex@vectorized.io>
2019-10-09 12:25:43 -0700

    Revert "Add from field check in verify_git_cfg.sh"
    This reverts commit d4f1b25c20f5c88d020188b819b81f07800075f8.
    
    verify_git_cfg.sh should enforce correct formatting of from

--- tools/verify_git_cfg.sh
+++ tools/verify_git_cfg.sh
@@ -18,12 +18,7 @@ if [[ "$(git config user.email)" != *"@vectorized.io" ]]; then
 fi
 
 if [[ "$(git config sendemail.smtpuser)" != *"@vectorized.io" ]]; then
-    error "Please set sendemail with 'git config sendemail.smtpuser <user>@vectorized.io'"
-    verified=false
-fi
-
-if [[ "$(git config sendemail.from)" != "" ]]; then
-    error "Please unset sendemail.from with 'git config --unset sendemail.from'"
+    error "Please sendemail with 'git config sendemail.smtpuser <user>@vectorized.io'"
     verified=false
 fi
 
