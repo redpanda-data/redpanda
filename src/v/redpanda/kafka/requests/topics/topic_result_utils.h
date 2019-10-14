1e6a1b80a3c5d1ad594a27df90f24f1e3d9368a2
Michal Maslanka <michal@vectorized.io>
2019-10-14 12:08:28 +0200

    kafka/requests: added topic_result_utils
    Introduced utility function to encode the Kafka Admin APIs topic
    results as bytes_ostream using response_writer. This utility function is
    intended to be used by Kafka Admin API's request handlers.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/redpanda/kafka/requests/topics/topic_result_utils.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "model/fundamental.h"
+#include "redpanda/kafka/requests/topics/types.h"
+#include "redpanda/kafka/requests/response.h"
+#include "seastarx.h"
+
+#include <seastar/util/bool_class.hh>
+namespace kafka::requests {
+
+using include_message = bool_class<struct include_message_tag>;
+
+kafka::requests::response_ptr encode_topic_results(
+  const std::vector<topic_result>& errors,
+  int32_t throttle_time_ms,
+  include_message inc_msg = include_message::yes) {
+    auto resp = std::make_unique<kafka::requests::response>();
+    // throttle time
+    if (throttle_time_ms >= 0) {
+        resp->writer().write(throttle_time_ms);
+    }
+    // errors
+    resp->writer().write_array(
+      errors, [inc_msg](const topic_result& r, response_writer& wr) {
+          wr.write(r.topic);
+          wr.write(r.error_code);
+          if (inc_msg) {
+              wr.write(r.err_msg);
+          }
+      });
+    return std::move(resp);
+}
+} // namespace kafka::requests
