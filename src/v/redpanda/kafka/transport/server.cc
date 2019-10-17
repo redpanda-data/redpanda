redpanda-data/redpanda

commit 4283b455e5b0a737d6234c0d3487a0b010a54193

kafka/request: Pass request_context by r reference
Signed-off-by: Duarte Nunes <duarte@vectorized.io>

src/v/redpanda/kafka/transport/server.cc
@@ -241,13 +241,12 @@ future<> kafka_server::connection::process_request() {
                                  header = std::move(header),
                                  units = std::move(units),
                                  &delay](fragbuf buf) mutable {
-                              auto ctx
-                                = std::make_unique<requests::request_context>(
-                                  _server._metadata_cache,
-                                  std::move(header),
-                                  std::move(buf),
-                                  delay.duration);
-                              _server._probe.serving_request(*ctx);
+                              auto ctx = requests::request_context(
+                                _server._metadata_cache,
+                                std::move(header),
+                                std::move(buf),
+                                delay.duration);
+                              _server._probe.serving_request(ctx);
                               do_process(std::move(ctx), std::move(units));
                           });
                     });
@@ -257,16 +256,13 @@ future<> kafka_server::connection::process_request() {
 }
 
 void kafka_server::connection::do_process(
-  std::unique_ptr<requests::request_context>&& ctx, semaphore_units<>&& units) {
-    auto correlation = ctx->header().correlation_id;
-    auto f = requests::process_request(*ctx, _server._smp_group);
+  requests::request_context&& ctx, semaphore_units<>&& units) {
+    auto correlation = ctx.header().correlation_id;
+    auto f = requests::process_request(std::move(ctx), _server._smp_group);
     auto ready = std::move(_ready_to_respond);
     _ready_to_respond = f.then_wrapped(
-      [this,
-       ctx = std::move(ctx),
-       units = std::move(units),
-       ready = std::move(ready),
-       correlation](future<requests::response_ptr>&& f) mutable {
+      [this, units = std::move(units), ready = std::move(ready), correlation](
+        future<requests::response_ptr>&& f) mutable {
           try {
               auto r = f.get0();
               return ready.then(

src/v/redpanda/kafka/requests/api_versions_request.h
@@ -17,8 +17,7 @@ public:
     static constexpr api_version min_supported = v0;
     static constexpr api_version max_supported = api_version(2);
 
-    static future<response_ptr>
-    process(request_context&, smp_service_group);
+    static future<response_ptr> process(request_context&&, smp_service_group);
 };
 
 } // namespace kafka::requests

src/v/redpanda/kafka/requests/find_coordinator_request.cc
@@ -12,7 +12,7 @@
 namespace kafka::requests {
 
 future<response_ptr>
-find_coordinator_request::process(request_context& ctx, smp_service_group g) {
+find_coordinator_request::process(request_context&& ctx, smp_service_group g) {
     if (
       ctx.header().version < min_supported
       || ctx.header().version > max_supported) {

src/v/redpanda/kafka/requests/find_coordinator_request.h
@@ -15,7 +15,7 @@ public:
     static constexpr api_version min_supported = api_version(0);
     static constexpr api_version max_supported = api_version(2);
 
-    static future<response_ptr> process(request_context&, smp_service_group);
+    static future<response_ptr> process(request_context&&, smp_service_group);
 };
 
 } // namespace kafka::requests

src/v/redpanda/kafka/requests/list_groups_request.cc
@@ -12,7 +12,7 @@
 namespace kafka::requests {
 
 future<response_ptr>
-list_groups_request::process(request_context& ctx, smp_service_group g) {
+list_groups_request::process(request_context&& ctx, smp_service_group g) {
     if (
       ctx.header().version < min_supported
       || ctx.header().version > max_supported) {

src/v/redpanda/kafka/requests/list_groups_request.h
@@ -15,7 +15,7 @@ public:
     static constexpr api_version min_supported = api_version(0);
     static constexpr api_version max_supported = api_version(2);
 
-    static future<response_ptr> process(request_context&, smp_service_group);
+    static future<response_ptr> process(request_context&&, smp_service_group);
 };
 
 } // namespace kafka::requests

src/v/redpanda/kafka/requests/metadata_request.cc
@@ -1,8 +1,9 @@
+#include "redpanda/kafka/requests/metadata_request.h"
+
 #include "cluster/metadata_cache.h"
 #include "model/metadata.h"
 #include "redpanda/kafka/errors/errors.h"
 #include "redpanda/kafka/requests/headers.h"
-#include "redpanda/kafka/requests/metadata_request.h"
 
 #include <seastar/core/thread.hh>
 
@@ -16,12 +17,13 @@ namespace kafka::requests {
 //  unknown_topic_or_partition
 //  topic_authorization_failed
 //  invalid_replication_factor
+//  leader_not_available
 // Possible partition-level error codes:
 //  leader_not_available
 //  listener_not_found
 //  replica_not_available
 future<response_ptr>
-metadata_request::process(request_context& ctx, smp_service_group g) {
+metadata_request::process(request_context&& ctx, smp_service_group g) {
     if (
       ctx.header().version < min_supported
       || ctx.header().version > max_supported) {
@@ -29,7 +31,7 @@ metadata_request::process(request_context& ctx, smp_service_group g) {
           std::runtime_error(fmt::format(
             "Unsupported version {} for metadata API", ctx.header().version)));
     }
-    return async([&ctx] {
+    return async([ctx = std::move(ctx)]() mutable {
         auto topics = ctx.reader().read_array([](request_reader& r) {
             return model::topic_view(r.read_string_view());
         });
@@ -57,7 +59,7 @@ metadata_request::process(request_context& ctx, smp_service_group g) {
               rw.write(b.id());
               rw.write(std::string_view(b.host()));
               rw.write(b.port());
-              if (ctx.header().version > api_version(0)) {
+              if (ctx.header().version >= api_version(1)) {
                   rw.write(b.rack());
               }
           });
@@ -78,11 +80,12 @@ metadata_request::process(request_context& ctx, smp_service_group g) {
               auto topic_metadata
                 = ctx.metadata_cache().get_topic_metadata(t).get0();
               if (!topic_metadata) {
+                  // FIXME: Tell the controller to create the topic
                   // until we have real metadata management fake the creation of
                   // the topic in the request to allow the stub to function with
                   // testing clients.
                   // rw.write(errors::error_code::unknown_topic_or_partition);
-                  rw.write(errors::error_code::none);
+                  rw.write(errors::error_code::leader_not_available);
                   topic_metadata.emplace(model::topic_metadata{t});
               } else {
                   rw.write(errors::error_code::none);
@@ -92,7 +95,8 @@ metadata_request::process(request_context& ctx, smp_service_group g) {
               // topic doesn't have at least one partition, so make sure there
               // is at least one.
               if (topic_metadata->partitions.empty()) {
-                  topic_metadata->partitions.emplace_back(model::partition_id(0));
+                  topic_metadata->partitions.emplace_back(
+                    model::partition_id(0));
               }
               rw.write(t.name());
               if (ctx.header().version >= api_version(1)) {

src/v/redpanda/kafka/requests/metadata_request.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#include "seastarx.h"
-
 #include "redpanda/kafka/requests/headers.h"
 #include "redpanda/kafka/requests/request_context.h"
 #include "redpanda/kafka/requests/response.h"
+#include "seastarx.h"
 
 #include <seastar/core/future.hh>
 
@@ -16,8 +15,7 @@ public:
     static constexpr api_version min_supported = api_version(0);
     static constexpr api_version max_supported = api_version(7);
 
-    static future<response_ptr>
-    process(request_context&, smp_service_group);
+    static future<response_ptr> process(request_context&&, smp_service_group);
 };
 
 } // namespace kafka::requests

commit 3d5dbbd71c19cd05e6f7560bd6cbc5b06103e60f

kafka: fix exception handling
The exceptional future throws the exception when we attempt to fut.get()
in the try block which means fut.get_exception() is no longer valid.
report the current exception instead.

-    throw std::runtime_error(
-      fmt::format("Unsupported API {}", ctx.header().key));
+    return seastar::make_exception_future<response_ptr>(
+      std::runtime_error(fmt::format("Unsupported API {}", ctx.header().key)));

Fixes: 4283b455e5

Signed-off-by: Noah Watkins <noah@vectorized.io>

src/v/redpanda/kafka/transport/server.cc
@@ -277,7 +277,8 @@ void kafka_server::connection::do_process(
                 .then([this] { _server._probe.request_served(); });
           } catch (...) {
               _server._probe.request_processing_error();
-              klog.debug("Failed to process request: {}", f.get_exception());
+              klog.debug(
+                "Failed to process request: {}", std::current_exception());
               return std::move(ready);
           }
       });