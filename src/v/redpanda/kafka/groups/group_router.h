7230a1ed95ed00626b19369695df821a1e608014
Noah Watkins <noah@vectorized.io>
2019-10-17 13:57:07 -0700

    kafka: add join group api request
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/redpanda/kafka/groups/group_router.h
+++ src/v/redpanda/kafka/groups/group_router.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "redpanda/kafka/requests/join_group_request.h"
 #include "seastarx.h"
 #include "redpanda/kafka/groups/types.h"
 #include "utils/concepts-enabled.h"
@@ -11,6 +12,17 @@ namespace kafka::groups {
 
 // clang-format off
 CONCEPT(
+template <typename T>
+concept GroupManager =
+requires(
+  T m,
+  const requests::request_context& ctx,
+  requests::join_group_request&& join_request) {
+
+    { m.join_group(ctx, std::move(join_request)) } ->
+        future<requests::join_group_response>;
+};
+
 template<typename T>
 concept GroupShardMapper = requires(T m, const kafka::group_id& group_id) {
     { m.shard_for(group_id) } -> seastar::shard_id;
@@ -20,7 +32,7 @@ concept GroupShardMapper = requires(T m, const kafka::group_id& group_id) {
 
 /// \brief Forward group operations to the owning core.
 template<typename GroupMgr, typename Shards>
-CONCEPT(requires GroupShardMapper<Shards>)
+CONCEPT(requires GroupManager<GroupMgr>&& GroupShardMapper<Shards>)
 class group_router final {
 public:
     /// \brief Create an instance of the group router.
@@ -38,6 +50,21 @@ public:
       , _ssg(smp_group)
       , _group_manager(group_manager)
       , _shards(shards.local()) {
+    }
+
+    future<requests::join_group_response> join_group(
+      const requests::request_context& ctx,
+      requests::join_group_request&& request) {
+        auto shard = _shards.shard_for(request.group_id);
+        return with_scheduling_group(
+          _sg, [this, shard, &ctx, request = std::move(request)]() mutable {
+              return _group_manager.invoke_on(
+                shard,
+                _ssg,
+                [&ctx, request = std::move(request)](GroupMgr& m) mutable {
+                    return m.join_group(ctx, std::move(request));
+                });
+          });
     }
 
 private:
