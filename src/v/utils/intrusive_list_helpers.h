4822ae1c87910e82d727fb406e7d4a13a10ebfa2
Alexander Gallego <alex@vectorized.io>
2019-10-23 15:14:57 -0700

    utils: add intrusive_list_helpers.h for boost::intrusive::list::*
    boost::intrusive::list* is difficult to use correctly and safely.
    Introduce type aliases for correct usage of intrusive lists
    with documentation on how to best use them

---
+++ src/v/utils/intrusive_list_helpers.h
@@ -0,0 +1,99 @@
+#pragma once
+
+/*
+ * This file contains convenience aliases that make boost::intrusive::list
+ * easier to use.
+ */
+
+#include <boost/intrusive/list.hpp>
+
+/**
+ * An auto-unlink intrusive list hook.
+ */
+using intrusive_list_hook = boost::intrusive::list_member_hook<
+  boost::intrusive::link_mode<boost::intrusive::auto_unlink>>;
+
+/**
+ * An intrusive list.
+ *
+ * An intrusive_list always uses an auto-unlink hook.
+ * Beware that intrusive_list::size() is an O(n) operation, since it has to walk
+ * the entire list.
+ *
+ * Example usage:
+ *
+ *   class Foo {
+ *     // Note that the listHook member variable needs to be visible
+ *     // to the code that defines the intrusive_list instantiation.
+ *     // The list hook can be made public, or you can make the other class a
+ *     // friend.
+ *     intrusive_list_hook listHook;
+ *   };
+ *
+ *   using FooList = intrusive_list<Foo, &Foo::listHook>;
+ *
+ *   Foo *foo = new Foo();
+ *   FooList myList;
+ *   myList.push_back(*foo);
+ *
+ * Note that each intrusive_list_hook can only be part of a single list at any
+ * given time.  If you need the same object to be stored in two lists at once,
+ * you need to use two different intrusive_list_hook member variables.
+ *
+ * The elements stored in the list must contain an intrusive_list_hook member
+ * variable.
+ */
+template<typename T, intrusive_list_hook T::*PtrToMember>
+using intrusive_list = boost::intrusive::list<
+  T,
+  boost::intrusive::member_hook<T, intrusive_list_hook, PtrToMember>,
+  boost::intrusive::constant_time_size<false>>;
+
+/**
+ * A safe-link intrusive list hook.
+ */
+using safe_intrusive_link_hook = boost::intrusive::list_member_hook<
+  boost::intrusive::link_mode<boost::intrusive::safe_link>>;
+
+/**
+ * An intrusive list with const-time size() method.
+ *
+ * A counted_intrusive_list always uses a safe-link hook.
+ * counted_intrusive_list::size() is an O(1) operation. Users of this type
+ * of lists need to remove a member from a list by calling one of the
+ * methods on the list (e.g., erase(), pop_front(), etc.), rather than
+ * calling unlink on the member's list hook. Given references to a
+ * list and a member, a constant-time removal operation can be
+ * accomplished by list.erase(list.iterator_to(member)). Also, when a
+ * member is destroyed, it is NOT automatically removed from the list.
+ *
+ * Example usage:
+ *
+ *   class Foo {
+ *     // Note that the listHook member variable needs to be visible
+ *     // to the code that defines the counted_intrusive_list instantiation.
+ *     // The list hook can be made public, or you can make the other class a
+ *     // friend.
+ *     safe_intrusive_link_hook listHook;
+ *   };
+ *
+ *   using FooList = counted_intrusive_list<Foo, &Foo::listHook> FooList;
+ *
+ *   Foo *foo = new Foo();
+ *   FooList myList;
+ *   myList.push_back(*foo);
+ *   myList.pop_front();
+ *
+ * Note that each safe_intrusive_link_hook can only be part of a single list at
+ * any given time.  If you need the same object to be stored in two lists at
+ * once, you need to use two different safe_intrusive_link_hook member
+ * variables.
+ *
+ * The elements stored in the list must contain an safe_intrusive_link_hook
+ * member variable.
+ */
+template<typename T, safe_intrusive_link_hook T::*PtrToMember>
+using counted_intrusive_list = boost::intrusive::list<
+  T,
+  boost::intrusive::member_hook<T, safe_intrusive_link_hook, PtrToMember>,
+  boost::intrusive::constant_time_size<true>>;
