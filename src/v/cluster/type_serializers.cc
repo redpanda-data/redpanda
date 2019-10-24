332fdfcc2e009c63260a3983e12e0e1940f654e8
Michal Maslanka <michal@vectorized.io>
2019-10-24 14:32:30 +0200

    cluster/types: added topic_configuration serialization
    Made `cluster::topic_configuration` RPC serializable.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/cluster/type_serializers.cc
+++ src/v/cluster/type_serializers.cc
@@ -2,6 +2,20 @@
 #include "rpc/models.h"
 #include "rpc/serialize.h"
 
+namespace rpc {
+template<>
+void serialize(bytes_ostream& out, cluster::topic_configuration&& t) {
+    rpc::serialize(
+      out,
+      sstring(t.topic),
+      t.partition_count,
+      t.replication_factor,
+      t.compression,
+      t.compaction,
+      t.retention_bytes,
+      t.retention);
+}
+
 template<>
 future<cluster::partition_assignment> deserialize(source& in) {
     struct _simple {
