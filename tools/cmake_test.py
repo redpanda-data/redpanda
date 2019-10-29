537cf49797274606293899db3d2f1601be13b945
Alexander Gallego <alex@vectorized.io>
2019-10-29 03:10:59 -0700

    build:ci: enable overprovisioning for tests on CI machines

--- tools/cmake_test.py
+++ tools/cmake_test.py
@@ -22,12 +22,17 @@ class TestRunner():
         self.binary = binary
         self.repeat = repeat
         self.copy_files = copy_files
-        self.test_args = "" if len(args) == 0 else " ".join(map(str, args))
         self.root = "/dev/shm/vectorized_io"
-        # our ci does not have shared mem on the dockerfile mounted
-        if "CI" in os.environ:
-            self.root = "/tmp/vectorized_io"
         fs.mkdir_p(self.root)
+
+        if "rpunit" in binary:
+            self.test_args = " ".join([
+                "--" if len(args) == 0 else " ".join(map(str, args)),
+                "--overprovisioned", "--unsafe-bypass-fsync 1",
+                "--blocked-reactor-notify-ms 2000000"
+            ])
+        else:
+            self.test_args = "" if len(args) == 0 else " ".join(map(str, args))
 
     def _gen_alphanum(self, x=16):
         return ''.join(random.choice(string.ascii_letters) for _ in range(x))
@@ -40,6 +45,11 @@ class TestRunner():
         test_dir = self._gen_testdir()
         env = os.environ.copy()
         env["TEST_DIR"] = test_dir
+        env["BOOST_TEST_LOG_LEVEL"] = "test_suite"
+        env["BOOST_TEST_COLOR_OUTPUT"] = "0"
+        env["BOOST_TEST_CATCH_SYSTEM_ERRORS"] = "no"
+        env["BOOST_TEST_REPORT_LEVEL"] = "no"
+        env["BOOST_LOGGER"] = "HRF,test_suite"
         logger.info("Test dir: %s" % test_dir)
         for f in self.copy_files:
             logger.debug("Copying input file: %s" % f)
@@ -48,8 +58,10 @@ class TestRunner():
 
         cmd = Template(
             "(cd $test_dir && $binary $args; e=$$?; "
-            "rm -rf $test_dir; echo \"Test Exit code $$e\"; exit $$e)").substitute(
-                test_dir=test_dir, binary=self.binary, args=self.test_args)
+            "rm -rf $test_dir; echo \"Test Exit code $$e\"; exit $$e)"
+        ).substitute(test_dir=test_dir,
+                     binary=self.binary,
+                     args=self.test_args)
         logger.info(cmd)
         os.execle("/bin/bash", "/bin/bash", "-c", cmd, env)
 
