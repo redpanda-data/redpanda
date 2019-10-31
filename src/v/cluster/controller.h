ab30a887358634e00088c32d0ab24babf168cf31
Michal Maslanka <michal@vectorized.io>
2019-10-31 12:06:59 +0100

    cluster/types: made partition_assignment to hold all replicas
    Changed the partition assignment type to hold all NTP replicas instead
    of representing just one of them. The following changes were made:
    
    - Changed partition assignment recovery in cluster::controller
    - Changed partition_allocator to return single partition_assignment per
      partition
    - Changed the partition_allocator_tests so that the assignments are
      counted using the replicas vector
    - Changed simple_batch_builder test as partition assignment type was
      used to create test batches.
    - Changed type serializers
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/cluster/controller.h
+++ src/v/cluster/controller.h
@@ -61,6 +61,7 @@ private:
     future<> recover_batch(model::record_batch);
     future<> recover_record(model::record);
     future<> recover_assignment(partition_assignment);
+    future<> recover_replica(model::ntp, raft::group_id, broker_shard);
     future<> dispatch_record_recovery(log_record_key, fragbuf&&);
     raft::entry create_topic_cfg_entry(const topic_configuration&);
     void end_of_stream();
--- src/v/cluster/type_serializers.cc
+++ src/v/cluster/type_serializers.cc
@@ -20,17 +20,16 @@ void serialize(bytes_ostream& out, cluster::topic_configuration&& t) {
 template<>
 future<cluster::partition_assignment> deserialize(source& in) {
     struct _simple {
-        uint32_t shard;
         raft::group_id group;
         model::ntp ntp;
     };
     return deserialize<_simple>(in).then([&in](_simple s) {
-        return deserialize<model::broker>(in).then(
-          [s = std::move(s)](model::broker b) {
-              return cluster::partition_assignment{.shard = s.shard,
-                                                   .group = s.group,
-                                                   .ntp = s.ntp,
-                                                   .broker = std::move(b)};
+        return deserialize<std::vector<cluster::broker_shard>>(in).then(
+          [s = std::move(s)](std::vector<cluster::broker_shard> replicas) {
+              return cluster::partition_assignment{
+                .group = s.group,
+                .ntp = std::move(s.ntp),
+                .replicas = std::move(replicas)};
           });
     });
 }
