a4d2162f0ca007a8d6a289d51f33fcca8d558466
Alexander Gallego <alex@vectorized.io>
2019-10-31 22:31:33 -0700

    raft/bootstrap: add config bootstrap parsing util
    raft/configuration_bootstrap_state: contains all the state of
    what it means to have successfully parsed the set of segments
    with early exit for parsing the current & prev entry commits & terms

---
+++ src/v/raft/configuration_bootstrap_state.cc
@@ -0,0 +1,70 @@
+#include "raft/configuration_bootstrap_state.h"
+
+#include "rpc/deserialize.h"
+
+namespace raft {
+void configuration_bootstrap_state::process_configuration_in_thread(
+  model::record_batch b) {
+    _config_batches_seen++;
+    if (__builtin_expect(b.type() != configuration_batch_type, false)) {
+        throw std::runtime_error(fmt::format(
+          "Logic error. Asked a configuration tracker to process an unknown "
+          "record_batch_type: {}",
+          b.type()));
+    }
+    if (__builtin_expect(b.compressed(), false)) {
+        throw std::runtime_error(
+          "Compressed configuration records are unsupported");
+    }
+    if (_log_config_offset_tracker < b.last_offset()) {
+        _log_config_offset_tracker = b.last_offset();
+        for (model::record& rec : b) {
+            _config = std::move(rpc::deserialize<group_configuration>(
+                                  rec.release_packed_value_and_headers())
+                                  .get0());
+        }
+    }
+}
+void configuration_bootstrap_state::process_data_offsets_in_thread(
+  model::record_batch b) {
+    _data_batches_seen++;
+    if (__builtin_expect(b.type() != data_batch_type, false)) {
+        throw std::runtime_error(fmt::format(
+          "Logic error. Asked a data tracker to process an unknown "
+          "record_batch_type: {}",
+          b.type()));
+    }
+    // happy path
+    if (b.last_offset() > _commit_index) {
+        _prev_log_index = _commit_index;
+        _prev_log_term = _term;
+        _commit_index = b.last_offset();
+        _commit_index_base_batch_offset = b.base_offset();
+        return;
+    }
+    // we need to test how to find prev term
+    if (
+      b.base_offset() < _commit_index_base_batch_offset
+      && b.last_offset() >= _commit_index_base_batch_offset) {
+        _prev_log_index = _commit_index;
+        _prev_log_term = _term;
+        return;
+    }
+    // ignore the rest of the records, only interested in the last 2
+}
+
+void configuration_bootstrap_state::process_batch_in_thread(
+  model::record_batch b) {
+    switch (b.type()) {
+    case configuration_batch_type:
+        process_configuration_in_thread(std::move(b));
+        break;
+    case data_batch_type:
+        process_data_offsets_in_thread(std::move(b));
+        break;
+    default:
+        throw std::runtime_error(fmt::format(
+          "Cannot process model::record_batch_type : {}", b.type()));
+    }
+}
+} // namespace raft
---
+++ src/v/raft/configuration_bootstrap_state.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "raft/types.h"
+
+namespace raft {
+
+class configuration_bootstrap_state {
+public:
+    configuration_bootstrap_state() = default;
+    configuration_bootstrap_state(configuration_bootstrap_state&&) noexcept
+      = default;
+    configuration_bootstrap_state&
+    operator=(configuration_bootstrap_state&&) noexcept
+      = default;
+
+    void process_batch_in_thread(model::record_batch);
+    void process_configuration_in_thread(model::record_batch);
+    void process_data_offsets_in_thread(model::record_batch);
+
+    model::offset commit_index() const {
+        return _commit_index;
+    }
+    model::term_id term() const {
+        return _term;
+    }
+    model::offset prev_log_index() const {
+        return _prev_log_index;
+    }
+    model::term_id prev_log_term() const {
+        return _prev_log_term;
+    }
+    const raft::group_configuration& config() const {
+        return _config;
+    }
+    raft::group_configuration&& release_config() {
+        return std::move(_config);
+    }
+    bool is_finished() const {
+        if (_end_of_log) {
+            return true;
+        }
+        // we must have seen at least 2 records
+        return _prev_log_index != _commit_index
+               && _prev_log_index != model::offset{0}
+               // we *must* have seen one configuration
+               && _log_config_offset_tracker != model::offset{0};
+    }
+    void set_term(model::term_id t) {
+        _term = t;
+    }
+    void set_end_of_log() {
+        _end_of_log = true;
+    }
+    uint32_t data_batches_seen() const {
+        return _data_batches_seen;
+    }
+    uint32_t config_batches_seen() const {
+        return _data_batches_seen;
+    }
+
+private:
+    uint32_t _data_batches_seen{0};
+    uint32_t _config_batches_seen{0};
+    bool _end_of_log{false};
+
+    model::offset _commit_index{0};
+    model::term_id _term{0};
+    model::offset _prev_log_index{0};
+    model::term_id _prev_log_term{0};
+    raft::group_configuration _config;
+
+    // we need to keep track of what we have processed in case we re-reprocess a
+    // multiple segments
+    model::offset _log_config_offset_tracker{0};
+    model::offset _commit_index_base_batch_offset{0};
+};
+} // namespace raft
