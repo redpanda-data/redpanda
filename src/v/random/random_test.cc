4eb5be8d71b112b5a594fd16e0295c600bdb0065
Alexander Gallego <alex@vectorized.io>
2019-11-05 13:13:51 -0800

    random: fix alphanum generator to not include nulls in gen strings

--- src/v/random/random_test.cc
+++ src/v/random/random_test.cc
@@ -1,5 +1,6 @@
-#define BOOST_TEST_MODULE xxhash
+#define BOOST_TEST_MODULE random
 #include "random/fast_prng.h"
+#include "random/generators.h"
 
 #include <boost/test/unit_test.hpp>
 
@@ -13,5 +14,14 @@ BOOST_AUTO_TEST_CASE(fast_prng_basic_gen_100_unique_rands) {
         uint32_t x = rng();
         BOOST_CHECK(!test.count(x));
         test.insert(x);
+    }
+}
+BOOST_AUTO_TEST_CASE(alphanum_generator) {
+    for (auto i = 0; i < 100; ++i) {
+        auto s = random_generators::gen_alphanum_string(i);
+        // ensure no \0 in size
+        for (auto j = 0; j < i; ++j) {
+            BOOST_REQUIRE('\0' != s[j]);
+        }
     }
 }
