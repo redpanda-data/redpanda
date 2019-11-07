da587b3a04689cd59fe9624f32232ef9cf6fd596
Michal Maslanka <michal@vectorized.io>
2019-11-07 13:58:41 +0100

    cluster/tests: added complex recovery tests
    Added test, testing controller recovery with randomly generated 100
    topics each having up to 20 partitions.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/cluster/tests/controller_test_fixture.h
+++ src/v/cluster/tests/controller_test_fixture.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "cluster/controller.h"
 #include "cluster/tests/utils.h"
+#include "fmt/format.h"
 #include "model/record.h"
 #include "random/generators.h"
 #include "storage/directories.h"
@@ -10,6 +11,8 @@ using lrk = cluster::log_record_key;
 
 class controller_tests_fixture {
 public:
+    static constexpr int complex_topic_count{100};
+    int complex_partitions_count{0};
     controller_tests_fixture()
       : _base_dir("test_dir_" + random_generators::gen_alphanum_string(4)) {
         _cli_cache.start().get0();
@@ -101,26 +104,26 @@ public:
         std::vector<model::record_batch> ret;
 
         // topic with partition replicas on other broker
-        auto b1 = std::move(cluster::simple_batch_builder(
-                              cluster::controller::controller_record_batch_type,
-                              off)
-                              .add_kv(
-                                lrk{lrk::type::topic_configuration},
-                                cluster::topic_configuration(
-                                  _test_ns, model::topic("topic_2"), 2, 3))
-                              // partition 0
-                              .add_kv(
-                                lrk{lrk::type::partition_assignment},
-                                create_test_assignment(
-                                  "topic_2",
-                                  0,                        // partition_id
-                                  {{2, 0}, {3, 0}, {4, 0}}, // shards_assignment
-                                  5))                       // group_id
-                              // partition 1
-                              .add_kv(
-                                lrk{lrk::type::partition_assignment},
-                                create_test_assignment(
-                                  "topic_2", 1, {{2, 0}, {3, 0}, {4, 0}}, 6)))
+        auto b1 = std::move(
+                    cluster::simple_batch_builder(
+                      cluster::controller::controller_record_batch_type, off)
+                      .add_kv(
+                        lrk{lrk::type::topic_configuration},
+                        cluster::topic_configuration(
+                          _test_ns, model::topic("topic_2"), 2, 3))
+                      // partition 0
+                      .add_kv(
+                        lrk{lrk::type::partition_assignment},
+                        create_test_assignment(
+                          "topic_2",
+                          0,                        // partition_id
+                          {{2, 0}, {3, 0}, {4, 0}}, // shards_assignment
+                          5))                       // group_id
+                      // partition 1
+                      .add_kv(
+                        lrk{lrk::type::partition_assignment},
+                        create_test_assignment(
+                          "topic_2", 1, {{2, 0}, {3, 0}, {4, 0}}, 6)))
                     .build();
         ret.push_back(std::move(b1));
 
@@ -136,9 +139,54 @@ public:
         return ret;
     }
 
+    std::vector<model::record_batch> make_complex_topics() {
+        std::vector<model::record_batch> ret;
+
+        auto max_partitions = 20;
+        auto max_rf = 5;
+        std::array<model::node_id, 5> brokers{model::node_id(0),
+                                              model::node_id(1),
+                                              model::node_id(2),
+                                              model::node_id(3),
+                                              model::node_id(4)};
+        model::offset offset{0};
+        for (int i = 0; i < complex_topic_count; i++) {
+            auto partitions = random_generators::get_int(max_partitions);
+            auto rf = random_generators::get_int(max_rf);
+            auto tp = fmt::format("topic_{}", i);
+            cluster::simple_batch_builder builder(
+              cluster::controller::controller_record_batch_type, offset);
+            builder.add_kv(
+              lrk{lrk::type::topic_configuration},
+              cluster::topic_configuration(
+                _test_ns, model::topic(tp), partitions, rf));
+            offset++;
+
+            for (int p = 0; p < partitions; p++) {
+                std::vector<std::pair<uint32_t, uint32_t>> replicas;
+                replicas.reserve(rf);
+                for (int r = 0; r < rf; r++) {
+                    replicas.push_back({brokers[r](), 0});
+                }
+                builder.add_kv(
+                  lrk{lrk::type::partition_assignment},
+                  create_test_assignment(
+                    tp,
+                    p,                              // partition_id
+                    std::move(replicas),            // shards_assignment
+                    complex_partitions_count + 1)); // group_id
+                offset++;
+                complex_partitions_count++;
+            }
+            ret.push_back(std::move(builder).build());
+        }
+        return ret;
+    }
+
 private:
     static constexpr size_t _max_segment_size = 100'000;
     static constexpr model::node_id _current_node{1};
+
     model::ns _test_ns{"test_ns"};
     sstring _base_dir;
     sharded<raft::client_cache> _cli_cache;
