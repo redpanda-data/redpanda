b22dde33fa9db16dcb87f92c343b0b536223bf9b
Michal Maslanka <michal@vectorized.io>
2019-11-15 16:30:38 +0100

    cluster/recovery-test: using only current node as replica
    The controller recovery test triggered an error when partition-allocation
    happened. On fast machines (like build node) the test failed because
    raft leader election triggered allocator recovery. The allocator was
    missing brokers that were generated while setint up the test so the test failed.
    This issue did not occurred on slower nodes as the leadership changed
    notification was not triggered.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>
    Message-Id: <20191115153038.75777-1-michal@vectorized.io>

--- src/v/cluster/tests/controller_test_fixture.h
+++ src/v/cluster/tests/controller_test_fixture.h
@@ -11,7 +11,7 @@ using lrk = cluster::log_record_key;
 
 class controller_tests_fixture {
 public:
-    static constexpr int complex_topic_count{100};
+    static constexpr int complex_topic_count{10};
     int complex_partitions_count{0};
     controller_tests_fixture()
       : _base_dir("test_dir_" + random_generators::gen_alphanum_string(4)) {
@@ -23,7 +23,7 @@ public:
         _pm
           .start(
             _current_node,
-            10s,
+            1s,
             _base_dir,
             _max_segment_size,
             storage::log_append_config::fsync::no,
@@ -71,29 +71,27 @@ public:
         std::vector<model::record_batch> ret;
 
         // topic with partition replicas on current broker
-        auto b1
-          = std::move(
-              cluster::simple_batch_builder(
-                cluster::controller::controller_record_batch_type,
-                model::offset(0))
-                .add_kv(
-                  lrk{lrk::type::topic_configuration},
-                  cluster::topic_configuration(
-                    _test_ns, model::topic("topic_1"), 2, 3))
-                // partition 0
-                .add_kv(
-                  lrk{lrk::type::partition_assignment},
-                  create_test_assignment(
-                    "topic_1",
-                    0,                                      // partition_id
-                    {{_current_node(), 0}, {2, 0}, {3, 0}}, // shards_assignment
-                    2))                                     // group_id
-                // partition 1
-                .add_kv(
-                  lrk{lrk::type::partition_assignment},
-                  create_test_assignment(
-                    "topic_1", 1, {{_current_node(), 0}, {2, 0}, {3, 0}}, 3)))
-              .build();
+        auto b1 = std::move(cluster::simple_batch_builder(
+                              cluster::controller::controller_record_batch_type,
+                              model::offset(0))
+                              .add_kv(
+                                lrk{lrk::type::topic_configuration},
+                                cluster::topic_configuration(
+                                  _test_ns, model::topic("topic_1"), 2, 1))
+                              // partition 0
+                              .add_kv(
+                                lrk{lrk::type::partition_assignment},
+                                create_test_assignment(
+                                  "topic_1",
+                                  0,                      // partition_id
+                                  {{_current_node(), 0}}, // shards_assignment
+                                  2))                     // group_id
+                              // partition 1
+                              .add_kv(
+                                lrk{lrk::type::partition_assignment},
+                                create_test_assignment(
+                                  "topic_1", 1, {{_current_node(), 0}}, 3)))
+                    .build();
         ret.push_back(std::move(b1));
 
         return ret;
@@ -143,7 +141,6 @@ public:
         std::vector<model::record_batch> ret;
 
         auto max_partitions = 20;
-        auto max_rf = 5;
         std::array<model::node_id, 5> brokers{model::node_id(0),
                                               model::node_id(1),
                                               model::node_id(2),
@@ -152,22 +149,18 @@ public:
         model::offset offset{0};
         for (int i = 0; i < complex_topic_count; i++) {
             auto partitions = random_generators::get_int(max_partitions);
-            auto rf = random_generators::get_int(max_rf);
             auto tp = fmt::format("topic_{}", i);
             cluster::simple_batch_builder builder(
               cluster::controller::controller_record_batch_type, offset);
             builder.add_kv(
               lrk{lrk::type::topic_configuration},
               cluster::topic_configuration(
-                _test_ns, model::topic(tp), partitions, rf));
+                _test_ns, model::topic(tp), partitions, 1));
             offset++;
 
             for (int p = 0; p < partitions; p++) {
                 std::vector<std::pair<uint32_t, uint32_t>> replicas;
-                replicas.reserve(rf);
-                for (int r = 0; r < rf; r++) {
-                    replicas.push_back({brokers[r](), 0});
-                }
+                replicas.push_back({_current_node, 0});
                 builder.add_kv(
                   lrk{lrk::type::partition_assignment},
                   create_test_assignment(
@@ -194,3 +187,16 @@ private:
     sharded<cluster::shard_table> st;
     sharded<cluster::partition_manager> _pm;
 };
+// Waits for controller to become a leader it poll every 200ms
+void wait_for_leadership(cluster::controller& cntrl) {
+    with_timeout(
+      model::timeout_clock::now() + std::chrono::seconds(30), repeat([&cntrl] {
+          if (cntrl.is_leader()) {
+              return make_ready_future<stop_iteration>(stop_iteration::yes);
+          }
+          return sleep(std::chrono::milliseconds(200)).then([] {
+              return stop_iteration::no;
+          });
+      }))
+      .get();
+}
