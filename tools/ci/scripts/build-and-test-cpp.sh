71f2b0b0e507db417cfeffb1852c9d843de8913d
Alexander Gallego <alex@vectorized.io>
2019-11-21 15:08:41 -0800

    CI: initialize git repo w/ every build type

--- tools/ci/scripts/build-and-test-cpp.sh
+++ tools/ci/scripts/build-and-test-cpp.sh
@@ -5,6 +5,13 @@ if [ "$COMPILER" == "clang" ]; then
   export CLANG_FLAG="--clang=/usr/local/bin/clang"
 fi
 
+# we create a repo to avoid having to COPY .git/ which would
+# invalidate the cache every time we rebuild the image
+git init .
+git config user.name "${COMPILER}:${BUILD_TYPE}"
+git config user.email "${COMPILER}-${BUILD_TYPE}@vectorized.io"
+git commit --allow-empty -m "${COMPILER}:${BUILD_TYPE}"
+
 tools/build.py $CLANG_FLAG \
   --log=debug \
   --fmt=false \
