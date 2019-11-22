103448339c61c9a3daca39507ce62f20ba61c89d
Michal Maslanka <michal@vectorized.io>
2019-11-22 09:29:05 +0100

    r/types: group_configuration api tests
    Added unit tests for group_configuration API
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/raft/tests/group_configuration_tests.cc
@@ -0,0 +1,35 @@
+#define BOOST_TEST_MODULE raft
+#include "raft/types.h"
+
+#include <boost/test/unit_test.hpp>
+
+model::broker create_broker(int32_t id) {
+    return model::broker(model::node_id{id}, "localhost", 9002, std::nullopt);
+}
+
+BOOST_AUTO_TEST_CASE(should_return_true_as_it_contains_learner) {
+    raft::group_configuration test_grp = {.leader_id = model::node_id(10),
+                                          .nodes = {},
+                                          .learners = {create_broker(1)}};
+
+    auto contains = test_grp.contains_machine(model::node_id(1));
+    BOOST_REQUIRE_EQUAL(contains, true);
+}
+
+BOOST_AUTO_TEST_CASE(should_return_true_as_it_contains_voter) {
+    raft::group_configuration test_grp = {.leader_id = model::node_id(1),
+                                          .nodes = {create_broker(1)},
+                                          .learners = {}};
+
+    auto contains = test_grp.contains_machine(model::node_id(1));
+    BOOST_REQUIRE_EQUAL(contains, true);
+}
+
+BOOST_AUTO_TEST_CASE(should_return_false_as_it_does_not_contain_machine) {
+    raft::group_configuration test_grp = {.leader_id = model::node_id(1),
+                                          .nodes = {create_broker(3)},
+                                          .learners = {create_broker(4)}};
+
+    auto contains = test_grp.contains_machine(model::node_id(1));
+    BOOST_REQUIRE_EQUAL(contains, false);
+}
