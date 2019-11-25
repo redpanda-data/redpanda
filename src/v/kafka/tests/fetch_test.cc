bbeca941ece1df9aee079b1e5236d3b1ec33bbe7
Noah Watkins <noah@vectorized.io>
2019-11-25 15:01:06 -0800

    k: add fetch request partition iterator
    the fetch request handler needs to iterate over the partition read
    requests in order. the on-wire representation is a multi-level data
    structure. in order to avoid creating a new data representation that
    would result in data copying or constructing a separate index over the
    data, this adds a multi-level iterator over the raw request data.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

---
+++ src/v/kafka/tests/fetch_test.cc
@@ -0,0 +1,128 @@
+#include "kafka/requests/fetch_request.h"
+#include "redpanda/tests/fixture.h"
+#include "resource_mgmt/io_priority.h"
+
+SEASTAR_THREAD_TEST_CASE(partition_iterator) {
+    /*
+     * extract topic partitions from the request
+     */
+    auto transform = [](const kafka::fetch_request& request) {
+        std::vector<model::topic_partition> res;
+        std::transform(
+          request.cbegin(),
+          request.cend(),
+          std::back_inserter(res),
+          [&res](const kafka::fetch_request::const_iterator::value_type& v) {
+              if (v.new_topic) {
+                  if (!res.empty()) {
+                      BOOST_TEST(res.back().topic != v.topic->name);
+                  }
+              } else {
+                  BOOST_TEST(!res.empty());
+                  BOOST_TEST(res.back().topic == v.topic->name);
+              }
+              return model::topic_partition{
+                .topic = v.topic->name,
+                .partition = v.partition->id,
+              };
+          });
+        return res;
+    };
+
+    {
+        // no topics -> empty
+        kafka::fetch_request req;
+        auto parts = transform(req);
+        BOOST_TEST(parts.empty());
+    }
+
+    {
+        // 1 topic, no partitions -> empty
+        kafka::fetch_request req;
+        req.topics.push_back({.name = model::topic("t0")});
+        auto parts = transform(req);
+        BOOST_TEST(parts.empty());
+    }
+
+    {
+        // 2 topics, no partitions -> empty
+        kafka::fetch_request req;
+        req.topics.push_back({.name = model::topic("t0")});
+        req.topics.push_back({.name = model::topic("t1")});
+        auto parts = transform(req);
+        BOOST_TEST(parts.empty());
+    }
+
+    {
+        // 1 topic, 1 partition
+        kafka::fetch_request req;
+        req.topics.push_back({
+          .name = model::topic("t0"),
+          .partitions = {{.id = model::partition_id(100)}},
+        });
+        auto parts = transform(req);
+        BOOST_TEST(parts.size() == 1);
+        BOOST_TEST(parts[0].topic == model::topic("t0"));
+        BOOST_TEST(parts[0].partition == 100);
+    }
+
+    {
+        // 1 topic, 2 partitions
+        kafka::fetch_request req;
+        req.topics.push_back(
+          {.name = model::topic("t0"),
+           .partitions = {{.id = model::partition_id(100)},
+                          {.id = model::partition_id(101)}}});
+        auto parts = transform(req);
+        BOOST_TEST(parts.size() == 2);
+        BOOST_TEST(parts[0].topic == model::topic("t0"));
+        BOOST_TEST(parts[0].partition == 100);
+        BOOST_TEST(parts[1].topic == model::topic("t0"));
+        BOOST_TEST(parts[1].partition == 101);
+    }
+
+    {
+        // 2 topics, 2/1 partition
+        kafka::fetch_request req;
+        req.topics.push_back(
+          {.name = model::topic("t0"),
+           .partitions = {{.id = model::partition_id(100)},
+                          {.id = model::partition_id(101)}}});
+        req.topics.push_back(
+          {.name = model::topic("t1"),
+           .partitions = {{.id = model::partition_id(102)}}});
+        auto parts = transform(req);
+        BOOST_TEST(parts.size() == 3);
+        BOOST_TEST(parts[0].topic == model::topic("t0"));
+        BOOST_TEST(parts[0].partition == 100);
+        BOOST_TEST(parts[1].topic == model::topic("t0"));
+        BOOST_TEST(parts[1].partition == 101);
+        BOOST_TEST(parts[2].topic == model::topic("t1"));
+        BOOST_TEST(parts[2].partition == 102);
+    }
+
+    {
+        // 4 topics, 2/{}/{}/2 partition
+        kafka::fetch_request req;
+        req.topics.push_back(
+          {.name = model::topic("t0"),
+           .partitions = {{.id = model::partition_id(100)},
+                          {.id = model::partition_id(101)}}});
+        req.topics.push_back({.name = model::topic("t1")});
+        req.topics.push_back({.name = model::topic("t2")});
+        req.topics.push_back(
+          {.name = model::topic("t3"),
+           .partitions = {{.id = model::partition_id(102)},
+                          {.id = model::partition_id(103)}}});
+        auto parts = transform(req);
+        BOOST_TEST(parts.size() == 4);
+        BOOST_TEST(parts[0].topic == model::topic("t0"));
+        BOOST_TEST(parts[0].partition == 100);
+        BOOST_TEST(parts[1].topic == model::topic("t0"));
+        BOOST_TEST(parts[1].partition == 101);
+        BOOST_TEST(parts[2].topic == model::topic("t3"));
+        BOOST_TEST(parts[2].partition == 102);
+        BOOST_TEST(parts[3].topic == model::topic("t3"));
+        BOOST_TEST(parts[3].partition == 103);
+    }
+}
