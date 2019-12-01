2057338990de234e90ab0d86057266c626cb897c
Alexander Gallego <alex@vectorized.io>
2019-11-30 20:50:33 -0800

    .dir-localas.el: add lsp-server settings

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,12 @@
 ;; We use clang-format for the whole project
 ;; see misc/fmt.py  - but this sets up the basic
 ;; environment for c++
-((c++-mode (helm-make-build-dir . "build/debug")))
+(
+ (nil . ((lsp-file-watch-ignored . ("/\\.git$"
+                                    ;; temporaries
+                                    "/\\.ccls-cache$"
+                                    "/build$"
+                                    ))
+         ))
+ (c++-mode (helm-make-build-dir . "build/debug"))
+ )
