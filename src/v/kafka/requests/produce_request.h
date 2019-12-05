3b440aaf59f9921e5b21eaae6e7e6e4a9b3d6f4f
Noah Watkins <noah@vectorized.io>
2019-12-04 19:57:29 -0800

    k: add produce request/response types
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/kafka/requests/produce_request.h
+++ src/v/kafka/requests/produce_request.h
@@ -12,12 +12,53 @@ class produce_api final {
 public:
     static constexpr const char* name = "produce";
     static constexpr api_key key = api_key(0);
-    // We don't support the obsolete Kafka record format
     static constexpr api_version min_supported = api_version(3);
     static constexpr api_version max_supported = api_version(7);
 
     static future<response_ptr>
     process(request_context&&, seastar::smp_service_group);
+};
+
+struct produce_request final {
+    struct topic_data {
+        model::partition_id id;
+        std::optional<iobuf> data;
+    };
+
+    struct topic {
+        model::topic name;
+        std::vector<topic_data> data;
+    };
+
+    std::optional<sstring> transactional_id;
+    int16_t acks;
+    std::chrono::milliseconds timeout;
+    std::vector<topic> topics;
+
+    void encode(const request_context& ctx, response_writer& writer);
+    void decode(request_context& ctx);
+};
+
+std::ostream& operator<<(std::ostream&, const produce_request&);
+
+struct produce_response final {
+    struct partition {
+        model::partition_id id;
+        error_code error;
+        model::offset base_offset;
+        model::timestamp log_append_time;
+        model::offset log_start_offset; // >= v5
+    };
+
+    struct topic {
+        model::topic name;
+        std::vector<partition> partitions;
+    };
+
+    std::vector<topic> topics;
+    std::chrono::milliseconds throttle;
+
+    void encode(const request_context& ctx, response& resp);
 };
 
 } // namespace kafka
