39597c2dbc551f51a1bd6bc86f031740373cfda5
David Castillo <david@vectorized.io>
2019-12-06 10:42:12 -0500

    vtools: Add offboard command in the client command group

--- tools/vtools/client/commands.py
+++ tools/vtools/client/commands.py
@@ -22,3 +22,19 @@ def onboard(access_token, client_name, client_id):
     master_token = pc.create_master_token(token, client_id)
     click.echo(master_token)
     read_token = pc.create_read_token(token, master_token['id'], client_id)
+
+
+@client.command()
+@click.option(
+    '--access-token',
+    help="Your Packagecloud access token. https://packagecloud.io/api_token")
+@click.option('--token-id',
+              help="The integer ID of the master token to revoke")
+def offboard(access_token, token_id):
+    token = pc.create_session_token(access_token)
+    info = pc.show_master_token(token, token_id)
+    click.echo(
+        "You're about to revoke this token and its associated resources:")
+    click.echo(info)
+    if click.confirm('Are you sure you want to continue?'):
+        pc.revoke_master_token(token, token_id)
