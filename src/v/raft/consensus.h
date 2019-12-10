b6fe250b264ab7c1de65d5ffd06b97b11837df9e
Alexander Gallego <alex@vectorized.io>
2019-12-10 13:31:51 -0800

    cosmetic: remove empty tab chacters

--- src/v/raft/consensus.h
+++ src/v/raft/consensus.h
@@ -96,9 +96,9 @@ private:
       protocol_metadata sender, std::vector<storage::log::append_result>);
     future<append_entries_reply>
       commit_entries(std::vector<entry>, append_entries_reply);
-    
+
     future<> do_replicate(std::vector<raft::entry>&&);
-    
+
     future<std::vector<storage::log::append_result>>
     disk_append(std::vector<entry>&&);
 
