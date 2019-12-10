65992d80da54a6ae6018c079545f3e48fbb7fa20
Ivo Jimenez <ivo@vectorized.io>
2019-12-10 19:36:35 +0000

    devprod/gcb: adds git clone step to gcb pipeline
    The gcb github app does not send the .git/ folder when it triggers
    builds from github. This commit adds a clone step to the pipeline in
    order to create the .git/ folder for the current commit.
    Message-Id: <20191210193635.2112-1-ivo@vectorized.io>

--- tools/ci/scripts/build-and-test-cpp.sh
+++ tools/ci/scripts/build-and-test-cpp.sh
@@ -8,13 +8,6 @@ fi
 # print CPU on CI machines to track illegal instructions
 command -v lscpu >/dev/null && lscpu
 
-# we create a repo to avoid having to COPY .git/ which would
-# invalidate the cache every time we rebuild the image
-git init .
-git config user.name "${COMPILER}:${BUILD_TYPE}"
-git config user.email "${COMPILER}-${BUILD_TYPE}@vectorized.io"
-git commit --allow-empty -m "${COMPILER}:${BUILD_TYPE}"
-
 tools/build.py $CLANG_FLAG \
   --log=debug \
   --fmt=false \
