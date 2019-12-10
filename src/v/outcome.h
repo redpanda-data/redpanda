5313dd96cfca34d2beaf2f68d847df13a3fa4917
Alexander Gallego <alex@vectorized.io>
2019-12-10 10:29:56 -0800

    outcome.h: add outcome util shortcuts with std::* converstions
    By default, boost::outcome uses the boost conversions. Since
    we are using std::exception_ptr and std::error_code with c++17,
    we want to use the standard headers instead of the boost ones,
    so re-define the default template args and add unit tests
    with standard types
    
    Signed-off-by: Alexander Gallego <alex@vectorized.io>
    Message-Id: <20191210182956.27746-1-alex@vectorized.io>

---
+++ src/v/outcome.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <system_error> // bring in std::error_code et al
+
+// use the standard ones instead
+#include <boost/outcome/std_outcome.hpp>
+#include <boost/outcome/std_result.hpp>
+
+// include utils
+#include <boost/outcome/outcome.hpp>
+#include <boost/outcome/try.hpp>
+
+namespace outcome = boost::outcome_v2;
+
+template<
+  class R,
+  class S = std::error_code,
+  class NoValuePolicy = outcome::policy::default_policy<R, S, void>>
+using result = outcome::basic_result<R, S, NoValuePolicy>;
+
+template<class R, class S = std::error_code>
+using unchecked = outcome::std_result<R, S, outcome::policy::all_narrow>;
+
+template<class R, class S = std::error_code>
+using checked
+  = outcome::result<R, S, outcome::policy::throw_bad_result_access<S, void>>;
+
+#define outcome_try(...) BOOST_OUTCOME_TRY(__VA_ARGS__)
