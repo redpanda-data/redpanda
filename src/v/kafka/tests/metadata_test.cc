ee7db75dd298e4b9002b9de643eb789a2f29fe15
Noah Watkins <noah@vectorized.io>
2019-12-10 10:43:05 -0800

    k/test: add metadata request test
    this uses the expected failures variant of the tests. this is useful
    because it allows us to write specific use case tests that aren't
    currently supported and which have associated asana tickets. once those
    tickets have been completed the tests will actually report failure and
    the expected annotations can be removed. this allows a nice decoupling
    of concerns--writing tests and fixing problems/adding features.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/kafka/tests/metadata_test.cc
+++ src/v/kafka/tests/metadata_test.cc
@@ -2,6 +2,12 @@
 #include "redpanda/tests/fixture.h"
 #include "test_utils/fixture.h"
 
+#if 0
+/*
+ * Disabled tests based on librdkafka that rely on an alien thread pool. There
+ * is a bug that has started popping up with that after a recent seastar update.
+ * that will need to be tracked down.
+ */
 #include <cppkafka/cppkafka.h>
 #include <v/native_thread_pool.h>
 
@@ -24,4 +30,55 @@ FIXTURE_TEST(get_metadadata, redpanda_thread_fixture) {
     thread_pool.start().get();
     get_metadata(thread_pool).get();
     thread_pool.stop().get();
+}
+#endif
+
+static kafka::metadata_request all_topics() {
+    kafka::metadata_request req;
+    // FIXME: req.topics = std::nullopt
+    // https://app.asana.com/0/1149841353291489/1153248907521409
+    return req;
+}
+
+// https://github.com/apache/kafka/blob/8968cdd/core/src/test/scala/unit/kafka/server/MetadataRequestTest.scala#L52
+FIXTURE_TEST(cluster_id_with_req_v1, redpanda_thread_fixture) {
+    auto req = all_topics();
+
+    auto client = make_kafka_client().get0();
+    client.connect().get();
+    auto resp = client.metadata(req, kafka::api_version(1)).get0();
+    client.stop().then([&client] { client.shutdown(); }).get();
+
+    BOOST_REQUIRE(resp.cluster_id == std::nullopt);
+}
+
+// https://github.com/apache/kafka/blob/8968cdd/core/src/test/scala/unit/kafka/server/MetadataRequestTest.scala#L59
+// https://app.asana.com/0/1149841353291489/1153248907521420
+FIXTURE_TEST_EXPECTED_FAILURES(
+  cluster_id_is_valid, redpanda_thread_fixture, 1) {
+    auto req = all_topics();
+
+    auto client = make_kafka_client().get0();
+    client.connect().get();
+    auto resp = client.metadata(req, kafka::api_version(2)).get0();
+    client.stop().then([&client] { client.shutdown(); }).get();
+
+    BOOST_TEST((resp.cluster_id && !resp.cluster_id->empty()));
+}
+
+// https://github.com/apache/kafka/blob/8968cdd/core/src/test/scala/unit/kafka/server/MetadataRequestTest.scala#L87
+// https://app.asana.com/0/1149841353291489/1153248907521428
+FIXTURE_TEST_EXPECTED_FAILURES(rack, redpanda_thread_fixture, 1) {
+    auto req = all_topics();
+
+    auto client = make_kafka_client().get0();
+    client.connect().get();
+    auto resp = client.metadata(req, kafka::api_version(1)).get0();
+    client.stop().then([&client] { client.shutdown(); }).get();
+
+    // expected rack name is configured in fixture setup
+    BOOST_REQUIRE(!resp.brokers.empty());
+    BOOST_REQUIRE(resp.brokers.size() == 1);
+    BOOST_TEST(
+      (resp.brokers[0].rack && resp.brokers[0].rack.value() == rack_name));
 }
