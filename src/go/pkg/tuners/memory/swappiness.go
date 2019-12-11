5f09b44dd57a98548fc04aaac39a435e78eb5357
David Castillo <david@vectorized.io>
2019-12-10 21:05:12 -0500

    rpk/tuners: Add swappiness tuner

--- src/go/pkg/tuners/memory/swappiness.go
+++ src/go/pkg/tuners/memory/swappiness.go
@@ -1,10 +1,15 @@
 package memory
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"vectorized/pkg/checkers"
+	"vectorized/pkg/tuners"
+	"vectorized/pkg/tuners/executors"
+	"vectorized/pkg/tuners/executors/commands"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
 )
 
@@ -25,5 +30,29 @@ func NewSwappinessChecker(fs afero.Fs) checkers.Checker {
 			}
 			return strconv.Atoi(strings.TrimSpace(string(content)))
 		},
+	)
+}
+
+func NewSwappinessTuner(
+	fs afero.Fs, executor executors.Executor,
+) tuners.Tunable {
+	return tuners.NewCheckedTunable(
+		NewSwappinessChecker(fs),
+		func() tuners.TuneResult {
+
+			log.Debugf("Setting swappiness to %d", Swappiness)
+			err := executor.Execute(
+				commands.NewWriteFileCmd(
+					fs, File, fmt.Sprint(Swappiness)))
+			if err != nil {
+				log.Errorf("got an error while writing %d to %s: %v", Swappiness, File, err)
+				return tuners.NewTuneError(err)
+			}
+			return tuners.NewTuneResult(false)
+		},
+		func() (bool, string) {
+			return true, ""
+		},
+		executor.IsLazy(),
 	)
 }
--- src/go/pkg/tuners/memory/swappiness_test.go
+++ src/go/pkg/tuners/memory/swappiness_test.go
@@ -2,7 +2,9 @@ package memory_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
+	"vectorized/pkg/tuners/executors"
 	"vectorized/pkg/tuners/memory"
 	"vectorized/pkg/utils"
 
@@ -68,6 +70,86 @@ func TestChecker(t *testing.T) {
 					"expected checker to return %t, but got %t",
 					tt.expectOk,
 					res.IsOk,
+				)
+			}
+		})
+	}
+}
+
+func TestTuner(t *testing.T) {
+	tests := []struct {
+		name      string
+		fs        afero.Fs
+		before    func(fs afero.Fs) error
+		expectErr bool
+	}{
+		{
+			name: "It should leave the same value if it was correct",
+			fs:   afero.NewMemMapFs(),
+			before: func(fs afero.Fs) error {
+				_, err := utils.WriteBytes(
+					fs,
+					[]byte(fmt.Sprint(memory.Swappiness)),
+					memory.File,
+				)
+				return err
+			},
+		},
+		{
+			name: "It should change the value if it was different",
+			fs:   afero.NewMemMapFs(),
+			before: func(fs afero.Fs) error {
+				_, err := utils.WriteBytes(
+					fs,
+					[]byte("120"),
+					memory.File,
+				)
+				return err
+			},
+		},
+		{
+			name:      "It should fail if the file doesn't exist",
+			fs:        afero.NewMemMapFs(),
+			expectErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.before != nil {
+				err := tt.before(tt.fs)
+				if err != nil {
+					t.Errorf(
+						"got an error setting up the test: %v",
+						err,
+					)
+				}
+			}
+			tuner := memory.NewSwappinessTuner(tt.fs, executors.NewDirectExecutor())
+			res := tuner.Tune()
+			if res.GetError() != nil {
+				if !tt.expectErr {
+					t.Errorf(
+						"got an unexpected error: %v",
+						res.GetError(),
+					)
+				}
+				return
+			}
+			lines, err := utils.ReadFileLines(tt.fs, memory.File)
+			if err != nil {
+				t.Errorf(
+					"got an error while reading back the file: %v",
+					err,
+				)
+			}
+			if len(lines) != 1 {
+				t.Errorf("expected 1 line, got %d", len(lines))
+				return
+			}
+			if lines[0] != fmt.Sprint(memory.Swappiness) {
+				t.Errorf(
+					"expected the file contents to be '1', but got '%s'",
+					strings.Join(lines, "\n"),
 				)
 			}
 		})
