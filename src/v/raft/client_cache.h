1a0c6bc8d13d76c05ff5b15290466967c1dfb8a3
Michal Maslanka <michal@vectorized.io>
2019-12-13 19:02:26 +0100

    rcp/client: sharing transport for different services
    Added ability to create RPC client that will share the same transport
    for different service clients. The client will support variable number
    of services. API of services will be accessible as the client public
    API.
    
    Example:
       rpc::client<srvc_a_client_protocol, srvc_b_client_protocol> cli;
       cli.method_from_a();
       cli.method_from_b();
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/raft/client_cache.h
+++ src/v/raft/client_cache.h
@@ -12,8 +12,9 @@
 namespace raft {
 class client_cache final {
 public:
-    using client_type = lw_shared_ptr<reconnect_client>;
-    using underlying = std::unordered_map<model::node_id, client_type>;
+    using client_type = reconnect_client;
+    using client_ptr = lw_shared_ptr<client_type>;
+    using underlying = std::unordered_map<model::node_id, client_ptr>;
     using iterator = typename underlying::iterator;
 
     static inline shard_id shard_for(const model::node_id&);
@@ -22,7 +23,7 @@ public:
     bool contains(model::node_id n) const {
         return _cache.find(n) != _cache.end();
     }
-    client_type get(model::node_id n) const { return _cache.find(n)->second; }
+    client_ptr get(model::node_id n) const { return _cache.find(n)->second; }
 
     /// \brief needs to be a future, because mutations may come from different
     /// fibers and they need to be synchronized
