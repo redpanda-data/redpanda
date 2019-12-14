56b69225b355b2a72479e83b3c31a5051c0fc827
Alexander Gallego <alex@vectorized.io>
2019-12-14 10:35:15 -0800

    raft/tests: separate test fixtures for record management
    extract a simple_record_fixture that produces valid raft
    record batches from the configuration bootstrap test

--- src/v/raft/tests/bootstrap_configuration_test.cc
+++ src/v/raft/tests/bootstrap_configuration_test.cc
@@ -9,14 +9,15 @@
 #include "storage/record_batch_builder.h"
 #include "test_utils/randoms.h"
 // testing
+#include "raft/tests/simple_record_fixture.h"
 #include "test_utils/fixture.h"
 
 #include <seastar/core/print.hh>
 #include <seastar/util/log.hh>
 
 using namespace raft; // NOLINT
-struct bootstrap_fixture {
-    static constexpr int active_nodes = 3;
+struct bootstrap_fixture : raft::simple_record_fixture {
+    using raft::simple_record_fixture::active_nodes;
     bootstrap_fixture()
       : _mngr(storage::log_config{
         .base_dir = ".",
@@ -35,64 +36,10 @@ struct bootstrap_fixture {
         _log->flush().get();
         return res;
     }
-    template<typename Func>
-    model::record_batch_reader reader_gen(std::size_t n, Func&& f) {
-        std::vector<model::record_batch> batches;
-        batches.reserve(n);
-        while (n-- > 0) {
-            batches.push_back(f());
-        }
-        return model::make_memory_record_batch_reader(std::move(batches));
-    }
-    model::record_batch_reader configs(std::size_t n) {
-        return reader_gen(n, [this] { return config_batch(); });
-    }
-    model::record_batch_reader datas(std::size_t n) {
-        return reader_gen(n, [this] { return data_batch(); });
-    }
-    model::record_batch data_batch() {
-        storage::record_batch_builder bldr(raft::data_batch_type, _base_offset);
-        bldr.add_raw_kv(rand_iobuf(), rand_iobuf());
-        ++_base_offset;
-        return std::move(bldr).build();
-    }
-    model::record_batch config_batch() {
-        storage::record_batch_builder bldr(
-          raft::configuration_batch_type, _base_offset);
-        bldr.add_raw_kv(rand_iobuf(), rpc::serialize(rand_config()));
-        ++_base_offset;
-        return std::move(bldr).build();
-    }
-    iobuf rand_iobuf() const {
-        iobuf b;
-        auto data = random_generators::gen_alphanum_string(100);
-        b.append(data.data(), data.size());
-        return b;
-    }
-    raft::group_configuration rand_config() const {
-        std::vector<model::broker> nodes;
-        std::vector<model::broker> learners;
 
-        for (auto i = 0; i < active_nodes; ++i) {
-            nodes.push_back(tests::random_broker(i, i));
-            learners.push_back(tests::random_broker(
-              active_nodes + 1, active_nodes * active_nodes));
-        }
-        return raft::group_configuration{
-          .leader_id = model::node_id(
-            random_generators::get_int(0, active_nodes)),
-          .nodes = std::move(nodes),
-          .learners = std::move(learners)};
-    }
     ~bootstrap_fixture() { _mngr.stop().get(); }
-    model::offset _base_offset{0};
     storage::log_ptr _log;
     storage::log_manager _mngr;
-    model::ntp _ntp{
-      model::ns("bootstrap_test_" + random_generators::gen_alphanum_string(8)),
-      model::topic_partition{
-        model::topic(random_generators::gen_alphanum_string(6)),
-        model::partition_id(random_generators::get_int(0, 24))}};
 };
 
 FIXTURE_TEST(serde_config, bootstrap_fixture) {
