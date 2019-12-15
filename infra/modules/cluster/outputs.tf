5548769e9376e2cb78f241a8978a488e2fde339e
David Castillo <david@vectorized.io>
2019-12-15 17:11:33 -0500

    infra/cluster: Add SSH command as module output

--- infra/modules/cluster/outputs.tf
+++ infra/modules/cluster/outputs.tf
@@ -1,3 +1,7 @@
 output "ip" {
   value = aws_instance.root_node.public_ip
 }
+
+output "ssh_cmd" {
+  value = "ssh -F ${abspath(dirname("."))}/ssh_config ${var.distro_ssh_user[var.distro]}@${aws_instance.root_node.public_ip}"
+}
