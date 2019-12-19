38c8421007d88dee94955820f59d3c9909400d2d
Ivo Jimenez <ivo@vectorized.io>
2019-12-19 02:46:23 -0500

    vtools: enables build, install and test commands

--- tools/vtools/main.py
+++ tools/vtools/main.py
@@ -1,16 +1,20 @@
 import click
 
-
 from .pkg import commands as pkg
 from .git import commands as git
 from .client import commands as client
 from .ssh import commands as ssh
 from .infra import commands as infra
+from .install import commands as install
+from .build import commands as build
+from .test import commands as test
+
 
 @click.group()
 def main():
-    #entry point
+    # entrypoint
     pass
+
 
 # add commands here
 main.add_command(pkg.print_deps)
@@ -18,3 +22,6 @@ main.add_command(git.verify_git)
 main.add_command(client.client)
 main.add_command(ssh.ssh)
 main.add_command(infra.infra)
+main.add_command(install.install)
+main.add_command(build.build)
+main.add_command(test.test)
