c73760722dd466a26c2a6ccc25498878632f4994
Alexander Gallego <alex@vectorized.io>
2020-01-09 11:28:46 -0800

    .dir-locals: quote paths and dir-local vars

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,9 @@
 
 ((nil
   (lsp-file-watch-ignored "/\\.git$" "/\\.ccls-cache$" "/build$"))
- (c-or-c++-mode
-  (helm-make-build-dir . build/debug/gcc)
-  (clang-format-executable . build/llvm/llvm-bin/bin/clang-format)))
+ ;; cannot be c-or-c++-mode; it must be c++ mode for M-x compile
+ ;; directories must be quoted, even if M-x add-dir-local-variable
+ ;; adds them without quotes
+ (c++-mode
+  (helm-make-build-dir . "build/debug/clang")
+  (clang-format-executable . "build/llvm/llvm-bin/bin/clang-format")))
