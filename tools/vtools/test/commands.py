b142bdb0b065ea4db08449067806d791452b814b
Ivo Jimenez <ivo@vectorized.io>
2020-01-13 20:33:08 +0000

    vtools/pkg: move 'print-deps' command to 'test' group

--- tools/vtools/test/commands.py
+++ tools/vtools/test/commands.py
@@ -60,3 +60,13 @@ def cpp(build_type, conf, clang, args):
         f'ctest '
         f' {"-V" if os.environ.get("CI") else ""} '
         + args)
+
+
+@test.command(short_help='print runtime dependencies of a binary')
+@click.option("--binary", required=True, help="path to binary")
+def print_deps(binary):
+    """prints ldd output"""
+    binpath = pathlib.Path(binary)
+    deps = lddwrap.list_dependencies(path=binpath)
+    for dep in deps:
+        click.echo("Found dep: %s" % dep)
