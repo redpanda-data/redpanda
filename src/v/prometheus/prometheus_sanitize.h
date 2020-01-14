a97d0490fe8565971282799c1ecd57f6179db61d
Kostas Kyrimis <kostas@vectorized.io>
2020-01-14 15:28:16 +0000

    const-ref qualified the function and enabled RVO
    Message-Id: <20200114152816.7008-3-kostas@vectorized.io>

--- src/v/prometheus/prometheus_sanitize.h
+++ src/v/prometheus/prometheus_sanitize.h
@@ -6,10 +6,11 @@
 #include <algorithm>
 
 struct prometheus_sanitize {
-    static ss::sstring metrics_name(ss::sstring n) {
+    static ss::sstring metrics_name(const ss::sstring& n) {
+        auto copy = n;
         constexpr char value  = '_';
-        std::replace_if(n.begin(), n.end(), 
+        std::replace_if(copy.begin(), copy.end(), 
                         [](auto c){ return !std::isalnum(c); }, value);
-        return n;
+        return copy;
     }
 };
