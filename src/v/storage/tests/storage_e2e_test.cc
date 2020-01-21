3fca63f52db2a3fa6879fb41cf99bd7dca0de3b3
Michal Maslanka <michal@vectorized.io>
2020-01-21 11:07:31 +0100

    s/storage_e2e_tests: moved rolling term test
    Moved rolling term test to e2e tests suite.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/storage/tests/storage_e2e_test.cc
+++ src/v/storage/tests/storage_e2e_test.cc
@@ -140,3 +140,32 @@ FIXTURE_TEST(test_reading_range_from_a_log, storage_test_fixture) {
         BOOST_REQUIRE_EQUAL(range.back().crc(), batches[7].crc());
     }
 };
+
+FIXTURE_TEST(test_rolling_term, storage_test_fixture) {
+    for (auto type : {storage::log_manager::storage_type::memory}) {
+        storage::log_manager mgr = make_log_manager();
+        auto deferred = ss::defer([&mgr]() mutable { mgr.stop().get0(); });
+        auto ntp = make_ntp("default", "test", 0);
+        auto log = mgr.manage(ntp, type).get0();
+        std::vector<model::record_batch_header> headers;
+        model::offset current_offset = model::offset{0};
+        for (auto i = 0; i < 5; i++) {
+            auto part = append_random_batches(log, 1, model::term_id(i));
+            for (auto h : part) {
+                current_offset += h.last_offset_delta + 1;
+            }
+            log.flush().get();
+            BOOST_REQUIRE_EQUAL(
+              model::term_id(i),
+              log.get_term(current_offset - model::offset(1)).value());
+            std::move(part.begin(), part.end(), std::back_inserter(headers));
+        }
+
+        auto read_batches = read_and_validate_all_batches(log);
+        BOOST_REQUIRE_EQUAL(
+          log.max_offset(), read_batches.back().last_offset());
+        BOOST_REQUIRE_EQUAL(
+          log.committed_offset(), read_batches.back().last_offset());
+    }
+};
+
