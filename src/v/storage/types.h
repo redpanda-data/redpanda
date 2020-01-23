f9f0fa06feb0f467faae2cfc58d5e92fe1801417
Alexander Gallego <alex@vectorized.io>
2020-01-22 18:43:56 -0800

    s/types.h: add output stream to reader and writer configs

--- src/v/storage/types.h
+++ src/v/storage/types.h
@@ -2,6 +2,7 @@
 
 #include "model/fundamental.h"
 #include "model/limits.h"
+#include "model/record.h"
 #include "model/timeout_clock.h"
 
 #include <seastar/core/file.hh>
@@ -23,6 +24,7 @@ struct append_result {
     log_clock::time_point append_time;
     model::offset base_offset;
     model::offset last_offset;
+    size_t byte_size;
 };
 /**
  * Log reader configuration.
@@ -44,5 +46,23 @@ struct log_reader_config {
     model::offset max_offset
       = model::model_limits<model::offset>::max(); // inclusive
 };
+
+inline std::ostream& operator<<(std::ostream& o, const log_reader_config& cfg) {
+    return o << "{start_offset:" << cfg.start_offset
+             << ", max_offset:" << cfg.max_offset
+             << ", min_bytes:" << cfg.min_bytes
+             << ", max_bytes:" << cfg.max_bytes
+             << ", type_filter_size:" << cfg.type_filter.size() << "}";
+}
+
+inline std::ostream& operator<<(std::ostream& o, const append_result& a) {
+    return o << "{append_time:"
+             << std::chrono::duration_cast<std::chrono::milliseconds>(
+                  a.append_time.time_since_epoch())
+                  .count()
+             << ", base_offset:" << a.base_offset
+             << ", last_offset:" << a.last_offset
+             << ", byte_size:" << a.byte_size << "}";
+}
 
 } // namespace storage
