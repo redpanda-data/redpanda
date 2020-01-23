9aaf6ae26365418e5027e990a8b92da05d98fc7d
Alexander Gallego <alex@vectorized.io>
2020-01-22 18:18:19 -0800

    constants.h: add comments to constants for record header

--- src/v/storage/constants.h
+++ src/v/storage/constants.h
@@ -4,13 +4,14 @@
 
 namespace storage {
 
+// Total: 39 bytes
 constexpr uint32_t packed_header_size
-  = sizeof(model::record_batch_header::base_offset)
-    + sizeof(model::record_batch_type::type)
-    + sizeof(model::record_batch_header::crc)
-    + sizeof(model::record_batch_attributes::type)
-    + sizeof(model::record_batch_header::last_offset_delta)
-    + sizeof(model::record_batch_header::first_timestamp)
-    + sizeof(model::record_batch_header::max_timestamp)
-    + sizeof(int32_t); // num_records
+  = sizeof(model::record_batch_header::base_offset)         // 8
+    + sizeof(model::record_batch_type::type)                // 1
+    + sizeof(model::record_batch_header::crc)               // 4
+    + sizeof(model::record_batch_attributes::type)          // 2
+    + sizeof(model::record_batch_header::last_offset_delta) // 4
+    + sizeof(model::record_batch_header::first_timestamp)   // 8
+    + sizeof(model::record_batch_header::max_timestamp)     // 8
+    + /*num of records*/ sizeof(int32_t);                   // 4
 } // namespace storage
