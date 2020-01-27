b81dcb5b7271178f2e2a45723a032c96cf61caa7
Michal Maslanka <michal@vectorized.io>
2020-01-27 08:09:24 +0100

    test_utils/logs: fixed log being deleted before writing ends
    Extended log lifespan in order to prevent it being deleted while reading
    batches.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/test_utils/logs.h
+++ src/v/test_utils/logs.h
@@ -26,12 +26,15 @@ static ss::future<> persist_log_file(
                 storage::log_append_config cfg{
                   storage::log_append_config::fsync::yes,
                   ss::default_priority_class(),
-                  model::no_timeout
-                  };
+                  model::no_timeout};
                 auto reader = model::make_memory_record_batch_reader(
                   std::move(b));
-                return std::move(reader).consume(
-                  log.make_appender(cfg), cfg.timeout);
+                return std::move(reader)
+                  .consume(log.make_appender(cfg), cfg.timeout)
+                  .then([log](storage::append_result) mutable {
+                      return log.flush();
+                  })
+                  .finally([log] {});
             })
             .finally([&mgr] { return mgr.stop(); })
             .discard_result();
