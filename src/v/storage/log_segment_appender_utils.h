5386cc4de547687dbd59e4028cc75a6a3abafe8b
Alexander Gallego <alex@vectorized.io>
2020-01-26 23:22:10 -0800

    storage/segment-utils: use iobuf utils for serializing disk header

--- src/v/storage/log_segment_appender_utils.h
+++ src/v/storage/log_segment_appender_utils.h
@@ -1,7 +1,12 @@
 #pragma once
+#include "bytes/iobuf.h"
 #include "model/record.h"
 #include "storage/log_segment_appender.h"
+
 namespace storage {
+
+iobuf disk_header_to_iobuf(
+  const model::record_batch_header&, uint32_t record_count);
 
 ss::future<> write(log_segment_appender& out, const model::record& record);
 
