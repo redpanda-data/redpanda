7a41fbd466b86926e0de7730b1e87c455a504c0c
Michal Maslanka <michal@vectorized.io>
2020-01-28 21:34:05 +0100

    r/consensus_utils: added term assigning reader
    Before batches are getting replicated they have to have current raft
    term assigned. Added a reader that wraps the calls to source reader
    with specified term assignment.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/raft/tests/term_assigning_reader_test.cc
@@ -0,0 +1,43 @@
+#include "model/fundamental.h"
+#include "model/record_batch_reader.h"
+#include "model/timeout_clock.h"
+#include "raft/consensus_utils.h"
+#include "storage/tests/random_batch.h"
+
+#include <seastar/testing/thread_test_case.hh>
+
+#include <boost/test/tools/old/interface.hpp>
+
+SEASTAR_THREAD_TEST_CASE(test_assigning_batch_term) {
+    auto batches = storage::test::make_random_batches(model::offset(0), 10);
+    auto term = model::term_id(11);
+    auto src_reader = model::make_memory_record_batch_reader(
+      std::move(batches));
+    auto assigning_reader = raft::details::term_assigning_reader(
+      std::move(src_reader), term);
+    auto batches_with_term = assigning_reader
+                               .consume(
+                                 raft::details::memory_batch_consumer(),
+                                 model::no_timeout)
+                               .get0();
+
+    BOOST_REQUIRE_EQUAL(batches_with_term.size(), 10);
+    for (auto& b : batches_with_term) {
+        BOOST_REQUIRE_EQUAL(b.term(), term);
+    }
+};
+
+SEASTAR_THREAD_TEST_CASE(test_assigning_batch_term_release) {
+    auto batches = storage::test::make_random_batches(model::offset(0), 10);
+    auto term = model::term_id(11);
+    auto src_reader = model::make_memory_record_batch_reader(
+      std::move(batches));
+    auto assigning_reader = raft::details::term_assigning_reader(
+      std::move(src_reader), term);
+    auto batches_with_term = assigning_reader.release_buffered_batches();
+
+    BOOST_REQUIRE_EQUAL(batches_with_term.size(), 10);
+    for (auto& b : batches_with_term) {
+        BOOST_REQUIRE_EQUAL(b.term(), term);
+    }
+};
