d667972b04f1f2ccf2e9ebfab47bcf4930ae04f0
Alexander Gallego <alex@vectorized.io>
2020-01-28 19:23:41 -0800

    likely: fix inline functions for macros to not change behavior
    Add godbolt to prove assembly differences in inline function
    and macros. Add comments on c++20 likely tag and recommendation
    from the abseil library

--- src/v/likely.h
+++ src/v/likely.h
@@ -1,5 +1,15 @@
 #pragma once
 
-static inline bool likely(bool cond) { return __builtin_expect(cond, true); }
-
-static inline bool unlikely(bool cond) { return __builtin_expect(cond, false); }
+// https://godbolt.org/z/iRot_i
+// they must be macros or behavior is altered
+// Replace with c++20 [[likely]] attr
+// https://en.cppreference.com/w/cpp/language/attributes/likely
+//
+// From Abseil:
+// Recommendation: Modern CPUs dynamically predict branch execution paths,
+// typically with accuracy greater than 97%. As a result, annotating every
+// branch in a codebase is likely counterproductive; however, annotating
+// specific branches that are both hot and consistently mispredicted is likely
+// to yield performance improvements.
+#define likely(cond) __builtin_expect(cond, true)
+#define unlikely(cond) __builtin_expect(cond, false)
