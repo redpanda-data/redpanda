1f797803a65f3e45c341841b28246daa2a78c72e
David Castillo <david@vectorized.io>
2020-01-29 14:48:51 -0500

    rpk/api: Add function to send environment data

--- src/go/pkg/api/api.go
+++ src/go/pkg/api/api.go
@@ -60,12 +60,54 @@ func SendMetrics(p MetricsPayload, conf config.Config) error {
 	return SendMetricsToUrl(b, defaultUrl)
 }
 
+func SendEnvironment(
+	env EnvironmentPayload,
+	conf config.Config,
+) error {
+	return SendEnvironmentToUrl(
+		env,
+		conf,
+		fmt.Sprintf("%s%s", defaultUrl, "/env"),
+	)
+}
+
 func SendMetricsToUrl(b metricsBody, url string) error {
 	bs, err := json.Marshal(b)
 	if err != nil {
 		return err
 	}
 	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(bs))
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("metrics request failed. Status: %d", res.StatusCode)
+	}
+	return nil
+}
+
+func SendEnvironmentToUrl(
+	env EnvironmentPayload,
+	conf config.Config,
+	url string,
+) error {
+	body := environmentBody{env, conf}
+	bs, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest(
+		http.MethodPost,
+		url,
+		bytes.NewBuffer(bs),
+	)
 	if err != nil {
 		return err
 	}
