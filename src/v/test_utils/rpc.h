7d763c23915e01bfb293aea822989bf26db11c9c
Alexander Gallego <alex@vectorized.io>
2020-01-31 14:34:20 -0800

    rpc/test_utils: add async_serialize_roundtrip helper func
    Provide both synd and async to test the new async_adl interface
    type dispatching inside the rpc/service.h and transport.h

--- src/v/test_utils/rpc.h
+++ src/v/test_utils/rpc.h
@@ -1,9 +1,25 @@
 #pragma once
 #include "reflection/adl.h"
+#include "reflection/async_adl.h"
+
+#include <memory>
 
 template<typename T>
 T serialize_roundtrip_rpc(T&& t) {
     iobuf io = reflection::to_iobuf(std::forward<T>(t));
     iobuf_parser parser(std::move(io));
     return reflection::adl<T>{}.from(parser);
+}
+template<typename T>
+ss::future<T> async_serialize_roundtrip_rpc(T&& t) {
+    auto b = std::make_unique<iobuf>();
+    auto raw = b.get();
+    return reflection::async_adl<T>{}
+      .to(*raw, std::move(t))
+      .then([b = std::move(b)]() mutable {
+          auto p = std::make_unique<iobuf_parser>(std::move(*b));
+          auto raw = p.get();
+          return reflection::async_adl<T>{}.from(*raw).finally(
+            [p = std::move(p)] {});
+      });
 }
