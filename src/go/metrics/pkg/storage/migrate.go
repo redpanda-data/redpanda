ba766377489e680dfba2b44b7bbf76428d0fbfd1
David Castillo <david@vectorized.io>
2020-02-05 15:43:53 -0500

    metrics: Add the metrics repository

---
+++ src/go/metrics/pkg/storage/migrate.go
@@ -0,0 +1,29 @@
+package storage
+
+import (
+	"database/sql"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func Migrate(db *sql.DB, dbName, migrationsUrl string) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		migrationsUrl,
+		dbName,
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
+}
