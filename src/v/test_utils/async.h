a3cdf43f32e0bd64839a1d843cda2b676d771a44
Noah Watkins <noah@vectorized.io>
2020-02-09 09:22:51 -0800

    utils: de-threadify cooperative spin wait
    let's us use nested futures for the predicate.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/test_utils/async.h
+++ src/v/test_utils/async.h
@@ -6,6 +6,7 @@
 #include <seastar/core/future-util.hh>
 #include <seastar/core/lowres_clock.hh>
 #include <seastar/core/reactor.hh>
+#include <seastar/core/sleep.hh>
 
 #include <chrono>
 
@@ -13,24 +14,26 @@ namespace tests {
 
 // clang-format off
 template<typename Rep, typename Period, typename Predicate>
-CONCEPT(requires requires(Predicate&& p) {
-        { p() } -> bool;
-})
+CONCEPT(requires ss::ApplyReturns<Predicate, bool> ||
+        ss::ApplyReturns<Predicate, ss::future<bool>>)
 // clang-format on
 /// Used to wait for Prediacate to become true
-void cooperative_spin_wait_with_timeout(
+ss::future<> cooperative_spin_wait_with_timeout(
   std::chrono::duration<Rep, Period> timeout, Predicate p) {
-    ss::with_timeout(
+    using futurator = ss::futurize<std::result_of_t<Predicate()>>;
+    return ss::with_timeout(
       model::timeout_clock::now() + timeout,
       ss::repeat([p = std::forward<Predicate>(p)]() {
-          if (p()) {
-              return ss::make_ready_future<ss::stop_iteration>(
-                ss::stop_iteration::yes);
-          }
-          return ss::sleep(std::chrono::milliseconds(10)).then([] {
-              return ss::stop_iteration::no;
+          auto f = futurator::apply(p);
+          return f.then([](bool stop) {
+              if (stop) {
+                  return ss::make_ready_future<ss::stop_iteration>(
+                    ss::stop_iteration::yes);
+              }
+              return ss::sleep(std::chrono::milliseconds(10)).then([] {
+                  return ss::stop_iteration::no;
+              });
           });
-      }))
-      .get();
+      }));
 }
 }; // namespace tests
