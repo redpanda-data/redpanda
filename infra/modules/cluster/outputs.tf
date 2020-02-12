8c078bf325bda0d6f4d636a77430eb9394176f40
David Castillo <david@vectorized.io>
2020-02-12 17:59:45 -0500

    infra/cluster: Add private IPs output

--- infra/modules/cluster/outputs.tf
+++ infra/modules/cluster/outputs.tf
@@ -2,6 +2,10 @@ output "ip" {
   value = aws_eip.elastic_ip.*.public_ip
 }
 
+output "private_ips" {
+  value = aws_instance.node.*.private_ip
+}
+
 output "ssh_cmd" {
   value = formatlist("ssh -F ${local.ssh_config_file} ${var.distro_ssh_user[var.distro]}@%s", aws_eip.elastic_ip.*.public_ip)
 }
