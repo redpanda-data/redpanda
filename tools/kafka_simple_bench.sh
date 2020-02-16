b5027b9756b1a5dbd392fd3fe5ebf2f268895265
Alexander Gallego <alex@vectorized.io>
2020-02-16 15:32:26 -0800

    tools/kafka_simple_bench.sh: simple test using the Kafka Java API
    Tested with kafka 2.3.1 binary release from apache. Simply
    invoke the test from inside the binary download directory and
    done.

---
+++ tools/kafka_simple_bench.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+set -x
+
+topic="sanfrancisco"
+replication=1
+partitions=6
+servers="localhost:9092"
+record_count=100000
+record_size=1024
+throughput=1024
+acks=1
+
+bin/kafka-topics.sh --bootstrap-server ${servers} \
+  --create --topic sanfrancisco --partitions ${partitions} \
+  --replication-factor ${replication}
+
+bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance \
+  --record-size ${record_size} \
+  --topic ${topic} \
+  --num-records ${record_count} \
+  --throughput ${throughput} \
+  --producer-props "acks=${acks}" \
+  bootstrap.servers=${servers} \
+  buffer.memory=$((1024 * 1024))
