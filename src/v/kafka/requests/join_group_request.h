4a8ade2a2ee14567fa5b994f690d4ae1d5007673
Noah Watkins <noah@vectorized.io>
2020-02-18 13:20:18 -0800

    kafka: check for join group errors before dispatching
    This patch adds a static membership (unsupported feature) check before
    incurring the cost of dispatching the request to the target core.
    
    Since the last time group membership has been worked on there has been
    development within kafka in other areas, and this patch also refactors a
    bit to align with patterns for handling requests that emerged with
    consume/produce.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/kafka/requests/join_group_request.h
+++ src/v/kafka/requests/join_group_request.h
@@ -29,6 +29,13 @@ struct join_group_request final {
     kafka::protocol_type protocol_type;
     std::vector<member_protocol> protocols;
 
+    join_group_request() = default;
+    explicit join_group_request(request_context& ctx) { decode(ctx); }
+    join_group_request(const join_group_request&) = delete;
+    join_group_request& operator=(const join_group_request&) = delete;
+    join_group_request(join_group_request&&) = default;
+    join_group_request& operator=(join_group_request&&) = delete;
+
     // extra context from request header
     api_version version;
     std::optional<ss::sstring> client_id;
@@ -38,6 +45,11 @@ struct join_group_request final {
 };
 
 std::ostream& operator<<(std::ostream&, const join_group_request&);
+
+static inline const kafka::member_id no_member("");
+static inline const kafka::member_id no_leader("");
+static constexpr kafka::generation_id no_generation(-1);
+static inline const kafka::protocol_name no_protocol("");
 
 struct join_group_response final {
     struct member_config {
@@ -53,6 +65,14 @@ struct join_group_response final {
     kafka::member_id leader_id;
     kafka::member_id member_id;
     std::vector<member_config> members;
+
+    join_group_response(kafka::error_code error)
+      : throttle_time(0)
+      , error(error)
+      , generation_id(no_generation)
+      , protocol_name(no_protocol)
+      , leader_id(no_leader)
+      , member_id(no_member) {}
 
     join_group_response(
       kafka::error_code error,
@@ -71,11 +91,6 @@ struct join_group_response final {
 
     void encode(const request_context& ctx, response& resp);
 };
-
-static inline const kafka::member_id no_member("");
-static inline const kafka::member_id no_leader("");
-static constexpr kafka::generation_id no_generation(-1);
-static inline const kafka::protocol_name no_protocol("");
 
 static inline join_group_response
 _make_join_error(kafka::member_id member_id, error_code error) {
