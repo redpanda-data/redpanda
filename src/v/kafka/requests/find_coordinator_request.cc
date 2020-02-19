b514d5a6d92535da87c44acb627c977f66639971
Noah Watkins <noah@vectorized.io>
2020-02-19 07:36:10 -0800

    kafka: add req/resp types for find coordinator api
    adds proper request and response structures for the kafka find
    coordinator api.
    
    this patch also stubs out what needs to be done once the create topics
    rpc api is in place.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/kafka/requests/find_coordinator_request.cc
+++ src/v/kafka/requests/find_coordinator_request.cc
@@ -11,29 +11,90 @@
 
 namespace kafka {
 
+void find_coordinator_request::encode(
+  response_writer& writer, api_version version) {
+    writer.write(key);
+    if (version >= api_version(1)) {
+        writer.write(int8_t(key_type));
+    }
+}
+
+void find_coordinator_request::decode(request_context& ctx) {
+    const auto version = ctx.header().version;
+
+    key = ctx.reader().read_string();
+    if (version >= api_version(1)) {
+        key_type = coordinator_type(ctx.reader().read_int8());
+    }
+}
+
+void find_coordinator_response::encode(
+  const request_context& ctx, response& resp) {
+    auto& writer = resp.writer();
+    const auto version = ctx.header().version;
+
+    if (version >= api_version(1)) {
+        writer.write(int32_t(throttle.count()));
+    }
+    writer.write(error);
+    if (version >= api_version(1)) {
+        writer.write(error_message);
+    }
+    writer.write(int32_t(node()));
+    writer.write(host);
+    writer.write(port);
+}
+
+void find_coordinator_response::decode(iobuf buf, api_version version) {
+    request_reader reader(std::move(buf));
+
+    if (version >= api_version(1)) {
+        throttle = std::chrono::milliseconds(reader.read_int32());
+    }
+    error = error_code(reader.read_int16());
+    if (version >= api_version(1)) {
+        error_message = reader.read_nullable_string();
+    }
+    node = model::node_id(reader.read_int32());
+    host = reader.read_string();
+    port = reader.read_int32();
+}
+
 ss::future<response_ptr>
 find_coordinator_api::process(request_context&& ctx, ss::smp_service_group g) {
-    // request
-    auto key = ctx.reader().read_string();
-    std::optional<int8_t> key_type;
-    if (ctx.header().version >= api_version(1)) {
-        key_type = ctx.reader().read_int8();
+    find_coordinator_request request(ctx);
+
+    if (request.key_type != coordinator_type::group) {
+        return ctx.respond(
+          find_coordinator_response(error_code::unsupported_version));
     }
-    // response
-    auto resp = std::make_unique<response>();
-    if (ctx.header().version >= api_version(1)) {
-        resp->writer().write(int32_t(0));
-    }
-    resp->writer().write(error_code::none);
-    if (ctx.header().version >= api_version(1)) {
-        std::optional<std::string_view> error_message;
-        resp->writer().write(error_message);
-    }
-    // TODO: return connection for raft group 0 leader
-    resp->writer().write(int32_t(0));
-    resp->writer().write(ss::sstring("localhost"));
-    resp->writer().write(int32_t(9092));
-    return ss::make_ready_future<response_ptr>(std::move(resp));
+
+    /*
+     * TODO 1: create the group membership internal topic if it doesn't exist
+     *
+     * if there is a problem creating the topic
+     * return ctx.respond(
+     *   find_coordinator_response(error_code::coordinator_not_available));
+     */
+
+    /*
+     * TODO 2: hash the group id to the target partition
+     *
+     * The set of partition will be available either from the metadata cache or
+     * from the response to the create topics rpc handled by the controller.
+     */
+
+    /*
+     * TODO 3: return the leader for the target partition
+     *
+     * Assuming that we do not wait for leadership election for each partition
+     * after creating the topic return any host and let the client use the
+     * normal retry protocol to wait for a leader.  When the leader can't be
+     * determined, can return error_code::coordinator_not_available.
+     */
+
+    return ctx.respond(
+      find_coordinator_response(model::node_id(0), "localhost", 9092));
 }
 
 } // namespace kafka
