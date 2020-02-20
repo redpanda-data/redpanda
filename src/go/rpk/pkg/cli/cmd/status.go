ef04b610793500a6a0c3a7bc0cabf62a66307acc
David Castillo <david@vectorized.io>
2020-02-20 10:42:04 -0500

    rpk/metrics: rename Free* to Free*MB

--- src/go/rpk/pkg/cli/cmd/status.go
+++ src/go/rpk/pkg/cli/cmd/status.go
@@ -82,8 +82,8 @@ func executeStatus(
 		}
 		payload := api.MetricsPayload{
 			uint64(metrics.CpuPercentage),
-			metrics.FreeMemory,
-			metrics.FreeSpace,
+			metrics.FreeMemoryMB,
+			metrics.FreeSpaceMB,
 		}
 		return api.SendMetrics(payload, *conf)
 	}
@@ -94,7 +94,7 @@ func printMetrics(p *system.Metrics) {
 	t := ui.NewRpkTable(log.StandardLogger().Out)
 	t.SetHeader([]string{"Name", "Value"})
 	t.Append([]string{"CPU%", fmt.Sprint(p.CpuPercentage)})
-	t.Append([]string{"Free Memory (MB)", fmt.Sprint(p.FreeMemory)})
-	t.Append([]string{"Free Space  (MB)", fmt.Sprint(p.FreeSpace)})
+	t.Append([]string{"Free Memory (MB)", fmt.Sprint(p.FreeMemoryMB)})
+	t.Append([]string{"Free Space  (MB)", fmt.Sprint(p.FreeSpaceMB)})
 	t.Render()
 }
