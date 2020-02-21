fdd630820d992a09336312edd2018b39ca1267b2
Michal Maslanka <michal@vectorized.io>
2020-02-21 08:35:11 +0100

    r/rpc_client_protocol: implemented RPC based client protocol
    The RPC based implementation provides a protocol that is based on
    rpc::client_cache and uses RPC transport to communicate with other
    nodes. It provides exactly the same features as are currently used
    across the consensus module.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/raft/rpc_client_protocol.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "model/metadata.h"
+#include "outcome_future_utils.h"
+#include "raft/consensus.h"
+#include "raft/consensus_client_protocol.h"
+#include "raft/errc.h"
+#include "raft/raftgen_service.h"
+#include "rpc/connection_cache.h"
+#include "rpc/transport.h"
+
+#include <system_error>
+
+namespace raft {
+
+/// Raft client protocol implementation underlied by RPC connections cache
+class rpc_client_protocol final : public consensus_client_protocol::impl {
+public:
+    explicit rpc_client_protocol(ss::sharded<rpc::connection_cache>& cache)
+      : _connection_cache(cache) {}
+
+    ss::future<result<vote_reply>>
+    vote(model::node_id, vote_request&&, clock_type::time_point) final;
+
+    ss::future<result<append_entries_reply>> append_entries(
+      model::node_id, append_entries_request&&, clock_type::time_point) final;
+
+    ss::future<result<heartbeat_reply>> heartbeat(
+      model::node_id, heartbeat_request&&, clock_type::time_point) final;
+
+private:
+    ss::sharded<rpc::connection_cache>& _connection_cache;
+};
+
+} // namespace raft
