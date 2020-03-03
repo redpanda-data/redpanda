726f5b28931505dee665ab353a26fb9fe2a1b9ec
Michal Maslanka <michal@vectorized.io>
2020-03-03 13:29:50 +0100

    cluster: implemented metadata_dissemination_service
    This service handles leadership updates on remote nodes that doesn't
    have instances of raft group that current node have. Instance of raft
    group triggers leadership notification and by that mean updates leadership in
    metadata cache.
    
    The service caches all leadership updates and sends them as
    batch per node, every configurable period of time (by default every 3 seconds).
    This service is also responsible for querying one of the cluster nodes for current
    leadership metadata when node has started.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/cluster/tests/metadata_dissemination_utils_test.cc
@@ -0,0 +1,69 @@
+#include "model/fundamental.h"
+
+#include <boost/test/tools/old/interface.hpp>
+
+#include <vector>
+#define BOOST_TEST_MODULE cluster
+#include "cluster/metadata_dissemination_utils.h"
+#include "model/metadata.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test_calculation_non_overlapping_nodes) {
+    std::vector<model::node_id> cluster_nodes{model::node_id{0},
+                                              model::node_id{1},
+                                              model::node_id{2},
+                                              model::node_id{3},
+                                              model::node_id{4}};
+    std::vector<model::node_id> single_node{model::node_id{3}};
+    std::vector<model::node_id> three_nodes{
+      model::node_id{0}, model::node_id{3}, model::node_id{4}};
+
+    auto non_overlapping_1 = cluster::calculate_non_overlapping_nodes(
+      single_node, cluster_nodes);
+    std::vector<model::node_id> expected_1{model::node_id{0},
+                                           model::node_id{1},
+                                           model::node_id{2},
+                                           model::node_id{4}};
+
+    BOOST_REQUIRE_EQUAL(non_overlapping_1, expected_1);
+
+    auto non_overlapping_2 = cluster::calculate_non_overlapping_nodes(
+      three_nodes, cluster_nodes);
+    std::vector<model::node_id> expected_2{
+      model::node_id{1},
+      model::node_id{2},
+    };
+
+    BOOST_REQUIRE_EQUAL(non_overlapping_2, expected_2);
+
+    auto non_overlapping_3 = cluster::calculate_non_overlapping_nodes(
+      cluster_nodes, cluster_nodes);
+
+    BOOST_REQUIRE_EQUAL(non_overlapping_3.size(), 0);
+};
+
+BOOST_AUTO_TEST_CASE(test_get_partition_members) {
+    model::topic_metadata tp_md(model::topic("test_tp"));
+
+    auto p0 = model::partition_metadata(model::partition_id(0));
+    p0.replicas = {
+      model::broker_shard{model::node_id{0}, 1},
+      model::broker_shard{model::node_id{1}, 1},
+      model::broker_shard{model::node_id{2}, 1},
+    };
+    auto p1 = model::partition_metadata(model::partition_id(1));
+    p1.replicas = {
+      model::broker_shard{model::node_id{3}, 1},
+      model::broker_shard{model::node_id{4}, 1},
+      model::broker_shard{model::node_id{5}, 1},
+    };
+    tp_md.partitions = {p0, p1};
+
+    auto members = cluster::get_partition_members(
+      model::partition_id{1}, tp_md);
+    std::vector<model::node_id> expected = {
+      model::node_id{3}, model::node_id{4}, model::node_id{5}};
+
+    BOOST_REQUIRE_EQUAL(members, expected);
+};
