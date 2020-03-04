589d13d9163589d6fba0220160f7b1db2e4c6562
Noah Watkins <noah@vectorized.io>
2020-03-03 16:11:06 -0800

    kafka: add base offset-commit api implementation
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/kafka/groups/group_manager.h
+++ src/v/kafka/groups/group_manager.h
@@ -5,6 +5,7 @@
 #include "kafka/requests/heartbeat_request.h"
 #include "kafka/requests/join_group_request.h"
 #include "kafka/requests/leave_group_request.h"
+#include "kafka/requests/offset_commit_request.h"
 #include "kafka/requests/sync_group_request.h"
 #include "seastarx.h"
 
@@ -53,6 +54,10 @@ public:
 
     /// \brief Handle a LeaveGroup request
     ss::future<leave_group_response> leave_group(leave_group_request&& request);
+
+    /// \brief Handle a OffsetCommit request
+    ss::future<offset_commit_response>
+    offset_commit(offset_commit_request&& request);
 
 public:
     static error_code validate_group_status(group_id group, api_key api);
