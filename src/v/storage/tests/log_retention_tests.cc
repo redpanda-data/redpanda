863eab327b922fbd5421c2c433a46c11b2bf3815
Alexander Gallego <alex@vectorized.io>
2020-03-08 14:54:02 -0700

    log_retention_tets: add size based retention tests

--- src/v/storage/tests/log_retention_tests.cc
+++ src/v/storage/tests/log_retention_tests.cc
@@ -2,17 +2,20 @@
 // fixture
 #include "test_utils/fixture.h"
 
+#include <optional>
+
 struct gc_fixture {
     storage::disk_log_builder builder;
 };
 
 FIXTURE_TEST(empty_log_garbage_collect, gc_fixture) {
     builder | storage::start()
-      | storage::garbage_collect(model::timestamp::now(), std::nullopt)
+      | storage::garbage_collect(
+        model::timestamp::now(), std::make_optional<size_t>(1024))
       | storage::stop();
 }
 
-FIXTURE_TEST(garbage_collect_all_segments, gc_fixture) {
+FIXTURE_TEST(retention_test_time, gc_fixture) {
     builder | storage::start() | storage::add_segment(0)
       | storage::add_random_batch(0, 100, storage::compression::yes)
       | storage::add_random_batch(100, 2, storage::compression::yes)
@@ -20,13 +23,63 @@ FIXTURE_TEST(garbage_collect_all_segments, gc_fixture) {
       | storage::add_random_batch(102, 2, storage::compression::yes)
       | storage::add_segment(104) | storage::add_random_batches(104, 3);
 
-    // should *not* collect
+    BOOST_TEST_MESSAGE(
+      "Should not collect segments with timestamp older than 1");
     BOOST_CHECK_EQUAL(builder.get_log().segment_count(), 3);
     builder | storage::garbage_collect(model::timestamp(1), std::nullopt);
     BOOST_CHECK_EQUAL(builder.get_log().segment_count(), 3);
 
+    BOOST_TEST_MESSAGE("Should not collect segments because size is infinity");
+    builder
+      | storage::garbage_collect(
+        model::timestamp(1),
+        std::make_optional<size_t>(std::numeric_limits<size_t>::max()));
+    BOOST_CHECK_EQUAL(builder.get_log().segment_count(), 3);
+
+    BOOST_TEST_MESSAGE("Should collect all segments && records since they were "
+                       "created before now");
     builder | storage::garbage_collect(model::timestamp::now(), std::nullopt)
       | storage::stop();
 
     BOOST_CHECK_EQUAL(builder.get_log().segment_count(), 0);
+}
+
+FIXTURE_TEST(retention_test_size, gc_fixture) {
+    builder | storage::start() | storage::add_segment(0)
+      | storage::add_random_batch(0, 100, storage::compression::yes)
+      | storage::add_random_batch(100, 2, storage::compression::yes)
+      | storage::add_segment(102)
+      | storage::add_random_batch(102, 2, storage::compression::yes)
+      | storage::add_segment(104) | storage::add_random_batches(104, 3);
+
+    BOOST_TEST_MESSAGE("Should not collect segments because size equal to "
+                       "current partition size");
+    builder
+      | storage::garbage_collect(
+        model::timestamp(1),
+        std::make_optional(
+          builder.get_disk_log_impl().get_probe().partition_size()));
+    BOOST_CHECK_EQUAL(builder.get_log().segment_count(), 3);
+
+    BOOST_TEST_MESSAGE(
+      "Should collect all segments && records since they exceed 0 byte");
+    builder
+      | storage::garbage_collect(model::timestamp(1), std::optional<size_t>(0))
+      | storage::stop();
+
+    BOOST_CHECK_EQUAL(builder.get_log().segment_count(), 0);
+    BOOST_CHECK_EQUAL(
+      builder.get_disk_log_impl().get_probe().partition_size(), 0);
+}
+
+FIXTURE_TEST(retention_test_after_truncation, gc_fixture) {
+    BOOST_TEST_MESSAGE("Should be safe to garbage collect after truncation");
+    builder | storage::start() | storage::add_segment(0)
+      | storage::add_random_batch(0, 100, storage::compression::yes)
+      | storage::truncate_log(model::offset(0))
+      | storage::garbage_collect(model::timestamp::now(), std::nullopt)
+      | storage::stop();
+    BOOST_CHECK_EQUAL(builder.get_log().segment_count(), 0);
+    BOOST_CHECK_EQUAL(
+      builder.get_disk_log_impl().get_probe().partition_size(), 0);
 }
