3e1dc2bfb4f41ec76a530c5f7d3a6f8ab01b61a5
Noah Watkins <noah@vectorized.io>
2020-03-10 16:14:50 -0700

    kafka: bump supported join group request version
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/kafka/requests/join_group_request.h
+++ src/v/kafka/requests/join_group_request.h
@@ -14,7 +14,7 @@ struct join_group_api final {
     static constexpr const char* name = "join group";
     static constexpr api_key key = api_key(11);
     static constexpr api_version min_supported = api_version(0);
-    static constexpr api_version max_supported = api_version(3);
+    static constexpr api_version max_supported = api_version(4);
 
     static ss::future<response_ptr>
     process(request_context&&, ss::smp_service_group);
