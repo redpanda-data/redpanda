537c8ece2a39e7271cdbbe81b531738e6a9b17ea
Alexander Gallego <alex@vectorized.io>
2020-03-13 14:37:42 -0700

    tools/kafka_simple_bench.sh: add switch for consume,produce,create

--- tools/kafka_simple_bench.sh
+++ tools/kafka_simple_bench.sh
@@ -10,15 +10,27 @@ record_size=1024
 throughput=-1
 acks=1
 
-bin/kafka-topics.sh --bootstrap-server ${servers} \
-  --create --topic sanfrancisco --partitions ${partitions} \
-  --replication-factor ${replication}
+if [[ $1 == "create" ]]; then
+  bin/kafka-topics.sh --bootstrap-server ${servers} \
+    --create --topic sanfrancisco --partitions ${partitions} \
+    --replication-factor ${replication}
+fi
 
-bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance \
-  --record-size ${record_size} \
-  --topic ${topic} \
-  --num-records ${record_count} \
-  --throughput ${throughput} \
-  --producer-props "acks=${acks}" \
-  bootstrap.servers=${servers} \
-  buffer.memory=$((1024 * 1024))
+if [[ $1 == "produce" ]]; then
+  bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance \
+    --record-size ${record_size} \
+    --topic ${topic} \
+    --num-records ${record_count} \
+    --throughput ${throughput} \
+    --producer-props "acks=${acks}" \
+    bootstrap.servers=${servers} \
+    buffer.memory=$((1024 * 1024))
+fi
+
+if [[ $1 == "consume" ]]; then
+  bin/kafka-consumer-perf-test.sh --broker-list=${servers} \
+    --fetch-size=$((1024 * 1024)) \
+    --messages=${record_count} \
+    --topic=${topic} \
+    --threads 1
+fi
