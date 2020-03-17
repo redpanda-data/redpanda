54c50c813b225457dafe5289cf0da8cb2b6d3699
Ivo Jimenez <ivo@vectorized.io>
2020-03-17 06:55:20 +0000

    ducktape/redpanda: uses rpk to start redpanda on test nodes
    Adds the option of reading extra configs from a user-provided
    configuration file. The path of the file is read from a RP_EXTRA_CONF
    environment variable.
    
    This also fixes the configuration for the redpanda instance so that
    proper seed servers are specified

--- tests/rp/ducktape/services/redpanda.py
+++ tests/rp/ducktape/services/redpanda.py
@@ -1,3 +1,4 @@
+import os
 import time
 import yaml
 
@@ -5,33 +6,43 @@ from ducktape.services.service import Service
 
 
 class RedpandaService(Service):
-    CONFIG = '/etc/redpanda/redpanda.yml'
-
     def __init__(self, context, num_nodes=3):
         super(RedpandaService, self).__init__(context, num_nodes=num_nodes)
 
+        self.extra_config = {}
+        extra_config_file = os.environ.get('RP_EXTRA_CONF', None)
+        if extra_config_file:
+            if not os.path.exists(extra_config_file):
+                raise Exception("{} doesn't exist".format(extra_config_file))
+            with open(extra_config_file, 'r') as f:
+                self.extra_config = yaml.load(f)
+
     def start_node(self, node):
-        rp_config = self.get_config(node)
+        rpk_config = self.get_config(node)
+        self.logger.debug('Using config: {}'.format(rpk_config))
 
-        self.logger.debug('Config file contents:')
-        self.logger.debug(rp_config)
+        node.account.create_file('/etc/redpanda/redpanda.yaml',
+                                 yaml.dump(rpk_config))
 
-        node.account.create_file(RedpandaService.CONFIG, yaml.dump(rp_config))
-
-        cmd = (
-            'nohup redpanda --redpanda-cfg {} `</dev/null` > nohup.out 2>&1 &'
-        ).format(RedpandaService.CONFIG)
+        rpk_flags = '--check=false --redpanda-cfg=/etc/redpanda/redpanda.yaml'
+        cmd = ('nohup rpk start --tune {} `</dev/null` > '
+               '/var/lib/redpanda/stdouterr 2>&1 &').format(rpk_flags)
 
         node.account.ssh(cmd)
 
         time.sleep(2)
 
-        self.pid = node.account.ssh_output('pgrep redpanda')
+        self.pid = node.account.ssh_output('cat /var/run/redpanda.pid')
 
         if not self.pid:
             raise Exception('Unable to obtain PID of redpanda process.')
 
-        # TODO: ensure that service was started (check redpanda process)
+        # check that process is running
+        ret = node.account.ssh('kill -0 {}'.format(self.pid), allow_fail=True)
+
+        if ret != 0:
+            out = node.account.ssh_output('cat /var/lib/redpanda/stdouterr')
+            raise Exception('Redpanda failed to start: {}.'.format(out))
 
     def get_config(self, node):
         node_idx = self.idx(node)
@@ -39,19 +50,17 @@ class RedpandaService(Service):
         rpc_port = 33145
 
         cfg = {
+            'pid_file': '/var/run/redpanda.pid',
             'redpanda': {
-                'data_directory':
-                "/var/lib/redpanda",
-                'node_id':
-                node_idx,
-                'raft_heartbeat_interval':
-                2000,
+                'data_directory': "/var/lib/redpanda",
+                'node_id': node_idx,
+                'raft_heartbeat_interval': 2000,
                 'rpc_server': {
                     'address': "0.0.0.0",
                     'port': rpc_port,
                 },
                 'advertised_rpc_api': {
-                    'address': str(node.account.hostname),
+                    'address': str(node.account.ssh_hostname),
                     'port': rpc_port,
                 },
                 'kafka_api': {
@@ -59,19 +68,35 @@ class RedpandaService(Service):
                     'port': kafka_port,
                 },
                 'advertised_kafka_api': {
-                    'address': str(node.account.hostname),
+                    'address': str(node.account.ssh_hostname),
                     'port': kafka_port,
                 },
-                # Raft configuration
-                'seed_servers': [{
-                    'host': {
-                        'address': str(self.get_node(1).account.hostname),
-                        'port': rpc_port,
-                    },
-                    'node_id': 1
-                }]
+            },
+            'rpk': {
+                'coredump_dir': '/var/lib/redpanda/coredump',
+                'additional_start_flags': [
+                    "--default-log-level=trace",
+                ]
             }
         }
+
+        # list of seed servers for all but the first (root) node
+        if self.get_node(1).account.hostname != node.account.hostname:
+            seed_servers = []
+
+            for n in self.nodes:
+                seed_servers.append({
+                    'host': {
+                        'address': str(n.account.hostname),
+                        'port': rpc_port,
+                    },
+                    'node_id': self.idx(n)
+                })
+
+            cfg['redpanda'].update({'seed_servers': seed_servers})
+
+        # extra config
+        cfg.update(self.extra_config)
 
         return cfg
 
