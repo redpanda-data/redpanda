9fffa6260a4fb407143a0e7be1c2c3c281a571ea
Alexander Gallego <alex@vectorized.io>
2020-03-18 16:52:53 -0700

    build: fix gcc build. kafka::request_context.h
    using all primitive fields as pointers, which basically just copies
    the addresses should be marked as noexcept by gcc. This works
    well in clang, but warns in gcc9. Punting to hunt this bug for later.
    
    Created asana tix.

--- src/v/kafka/requests/request_context.h
+++ src/v/kafka/requests/request_context.h
@@ -65,32 +65,52 @@ public:
       cluster::shard_table& shard_table,
       ss::sharded<cluster::partition_manager>& partition_manager,
       ss::sharded<coordinator_ntp_mapper>& coordinator_mapper) noexcept
-      : _metadata_cache(metadata_cache)
-      , _cntrl_dispatcher(cntrl_dispatcher)
+      : _metadata_cache(&metadata_cache)
+      , _cntrl_dispatcher(&cntrl_dispatcher)
       , _header(std::move(header))
       , _reader(std::move(request))
       , _throttle_delay(throttle_delay)
-      , _group_router(group_router)
-      , _shard_table(shard_table)
-      , _partition_manager(partition_manager)
-      , _coordinator_mapper(coordinator_mapper) {}
-
-    request_context(request_context&&) noexcept = default;
+      , _group_router(&group_router)
+      , _shard_table(&shard_table)
+      , _partition_manager(&partition_manager)
+      , _coordinator_mapper(&coordinator_mapper) {
+        // XXX: don't forget to extend the move ctor
+    }
+    ~request_context() noexcept = default;
+    request_context(request_context&& o) noexcept
+      : _metadata_cache(o._metadata_cache)
+      , _cntrl_dispatcher(o._cntrl_dispatcher)
+      , _header(std::move(o._header))
+      , _reader(std::move(o._reader))
+      , _throttle_delay(o._throttle_delay)
+      , _group_router(o._group_router)
+      , _shard_table(o._shard_table)
+      , _partition_manager(o._partition_manager)
+      , _coordinator_mapper(o._coordinator_mapper) {}
+    request_context& operator=(request_context&& o) noexcept {
+        if (this != &o) {
+            this->~request_context();
+            new (this) request_context(std::move(o));
+        }
+        return *this;
+    }
+    request_context(const request_context&) = delete;
+    request_context& operator=(const request_context&) = delete;
 
     const request_header& header() const { return _header; }
 
     request_reader& reader() { return _reader; }
 
     const cluster::metadata_cache& metadata_cache() const {
-        return _metadata_cache.local();
+        return _metadata_cache->local();
     }
 
     cluster::metadata_cache& metadata_cache() {
-        return _metadata_cache.local();
+        return _metadata_cache->local();
     }
 
     controller_dispatcher& cntrl_dispatcher() const {
-        return _cntrl_dispatcher;
+        return *_cntrl_dispatcher;
     }
 
     int32_t throttle_delay_ms() const {
@@ -99,12 +119,12 @@ public:
           .count();
     }
 
-    kafka::group_router_type& groups() { return _group_router; }
+    kafka::group_router_type& groups() { return *_group_router; }
 
-    cluster::shard_table& shards() { return _shard_table; }
+    cluster::shard_table& shards() { return *_shard_table; }
 
     ss::sharded<cluster::partition_manager>& partition_manager() {
-        return _partition_manager;
+        return *_partition_manager;
     }
 
     // clang-format off
@@ -121,19 +141,19 @@ public:
     }
 
     ss::sharded<kafka::coordinator_ntp_mapper>& coordinator_mapper() {
-        return _coordinator_mapper;
+        return *_coordinator_mapper;
     }
 
 private:
-    ss::sharded<cluster::metadata_cache>& _metadata_cache;
-    controller_dispatcher& _cntrl_dispatcher;
+    ss::sharded<cluster::metadata_cache>* _metadata_cache;
+    controller_dispatcher* _cntrl_dispatcher;
     request_header _header;
     request_reader _reader;
     ss::lowres_clock::duration _throttle_delay;
-    kafka::group_router_type& _group_router;
-    cluster::shard_table& _shard_table;
-    ss::sharded<cluster::partition_manager>& _partition_manager;
-    ss::sharded<kafka::coordinator_ntp_mapper>& _coordinator_mapper;
+    kafka::group_router_type* _group_router;
+    cluster::shard_table* _shard_table;
+    ss::sharded<cluster::partition_manager>* _partition_manager;
+    ss::sharded<kafka::coordinator_ntp_mapper>* _coordinator_mapper;
 };
 
 // Executes the API call identified by the specified request_context.
