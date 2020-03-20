758f2d96c4665be0d8119a27cce2e2d8b441da61
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-03-20 03:22:43 +0000

    terraform/cluster: fixes for ssh config of nodes
    Fixes sshd configuration to allow root to login remotely. In addition,
    the known SSH key used in ducktape tests is added to
    ~/.ssh/authorized_keys for the root user. This key is used by the
    ducktape test driver to connect to all the nodes in the cluster. Also
    changes the config to us-west-2.

--- infra/modules/cluster/cluster.tf
+++ infra/modules/cluster/cluster.tf
@@ -18,7 +18,7 @@ locals {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-1"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "node" {
@@ -34,11 +34,14 @@ resource "aws_instance" "node" {
     private_key = file(var.private_key_path)
   }
 
-  # allow ssh as root
+  # allow ssh as root and add known key
   provisioner "remote-exec" {
     inline = [
       "sudo cp /home/${var.distro_ssh_user[var.distro]}/.ssh/* /root/.ssh/",
+      "sudo sed -i 's/PermitRootLogin.*//' /etc/ssh/sshd_config",
       "sudo bash -c 'echo PermitRootLogin yes >> /etc/ssh/sshd_config'",
+      "sudo systemctl restart sshd",
+      "sudo bash -c 'echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjTM1NUIW/5VeG3bmwl1bJ351iQKLydfjchmgL8YVJcuF01P18APhX2R5NuGaIb31ilE5QSkIm2EMNdKXSXGd/8cz27A84+pBVzrLUXyRd3ia9aK3RQX398TW0zYAUaps93+oLlr1ycENmJfyxhHhi7aOPg2kG+hfwpraNBxP+UmdY1bocU9MZ6v48ER2VYYGi1FvGj2vDa+bCSPKITn12nsvAsuAlrBEpBnVGrwJ73CXITWOnIEDtOgs6OsZVzWYy8YP0lHGDn5UDS0qHWKKhXw+cdFHYs43Qs/cf+9F3A7ftmEXuZajsiMMFtCXR+K3/qYEmelRH7YJ7g+Nqhilx ivo@box >> /root/.ssh/authorized_keys'"
     ]
   }
 }
