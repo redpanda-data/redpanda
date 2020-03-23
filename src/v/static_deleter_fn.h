21b818db9e596440dfead5a3ca4665c880bbda54
Alexander Gallego <alex@vectorized.io>
2020-03-22 21:46:35 -0700

    compression: add zstd streaming compressor for iobufs.
    This change includes unit tests and benchmarks and a working
    implementation of the streaming zstd API.
    
    First part of implementing [ch65] (adding compression to RPC)

---
+++ src/v/static_deleter_fn.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstddef>
+
+// These functions for the gifts from the C language, to us.
+// wrappers around C-api free_*
+
+template<typename T, void (*f)(T*)>
+struct static_deleter_fn {
+    void operator()(T* t) const { f(t); }
+};
+template<typename T, size_t (*f)(T*)>
+struct static_sized_deleter_fn {
+    void operator()(T* t) const { (void)f(t); }
+};
+
+namespace internal {
+static inline void static_deleter_noop(void*) {}
+} // namespace internal
+template<typename T>
+using static_deleter_noop
+  = static_deleter_fn<T, &internal::static_deleter_noop>;
