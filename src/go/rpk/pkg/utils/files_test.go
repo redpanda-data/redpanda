52409dd684ada92d3ed9f4701fe1d9e9018c7a89
David Castillo <david@vectorized.io>
2020-04-03 17:36:06 -0500

    rpk/utils/files: Use testify, simplify test code

--- src/go/rpk/pkg/utils/files_test.go
+++ src/go/rpk/pkg/utils/files_test.go
@@ -1,11 +1,11 @@
 package utils_test
 
 import (
-	"bytes"
 	"testing"
 	"vectorized/pkg/utils"
 
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/require"
 )
 
 func TestWriteBytes(t *testing.T) {
@@ -15,26 +15,12 @@ func TestWriteBytes(t *testing.T) {
 	filepath := "/tmp/testwritebytes.yaml"
 
 	n, err := utils.WriteBytes(fs, bs, filepath)
-
-	if n != len(bs) {
-		t.Errorf("the number of bytes read doesn't match the number of bytes written")
-	}
-	if err != nil {
-		t.Errorf("utils.WriteBytes failed with error %v", err)
-	}
+	require.Equal(t, len(bs), n, "the number of bytes read doesn't match the number of bytes written")
+	require.NoError(t, err)
 	buf := make([]byte, len(bs))
 	file, err := fs.Open(filepath)
-	if err != nil {
-		t.Errorf("got an error while trying to open %v: %v", filepath, err)
-	}
-	if _, err := file.Read(buf); err != nil {
-		t.Errorf("got an error while reading %v: %v", filepath, err)
-	}
-	if res := bytes.Compare(bs, buf); res != 0 {
-		t.Errorf(
-			"the contents read don't match the contents written: Got %v, wanted %v",
-			buf,
-			bs,
-		)
-	}
+	require.NoError(t, err)
+	_, err = file.Read(buf)
+	require.NoError(t, err)
+	require.Exactly(t, bs, buf)
 }
