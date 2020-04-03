69cb2cf8e492043c3d831cfd97ac55773a80c5ba
David Castillo <david@vectorized.io>
2020-04-03 13:55:24 -0500

    rpk/config: Use testify, simplify test code

--- src/go/rpk/pkg/config/config_test.go
+++ src/go/rpk/pkg/config/config_test.go
@@ -2,69 +2,44 @@ package config
 
 import (
 	"path/filepath"
-	"reflect"
-	"strings"
 	"testing"
 	"vectorized/pkg/utils"
 	vyaml "vectorized/pkg/yaml"
 
 	"github.com/spf13/afero"
 	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
 )
 
 func getValidConfig() *Config {
-	return &Config{
-		PidFile:    "/var/lib/redpanda/pid",
-		ConfigFile: "/etc/redpanda/redpanda.yaml",
-		Redpanda: &RedpandaConfig{
-			Directory: "/var/lib/redpanda/data",
-			RPCServer: SocketAddress{
-				Port:    33145,
-				Address: "127.0.0.1",
-			},
-			Id: 1,
-			KafkaApi: SocketAddress{
-				Port:    9092,
-				Address: "127.0.0.1",
-			},
-			AdminApi: SocketAddress{
-				Port:    9644,
-				Address: "127.0.0.1",
-			},
-			SeedServers: []*SeedServer{
-				&SeedServer{
-					Host: SocketAddress{
-						Port:    33145,
-						Address: "127.0.0.1",
-					},
-					Id: 1,
-				},
-				&SeedServer{
-					Host: SocketAddress{
-						Port:    33146,
-						Address: "127.0.0.1",
-					},
-					Id: 2,
-				},
-			},
+	conf := DefaultConfig()
+	conf.Redpanda.SeedServers = []*SeedServer{
+		&SeedServer{
+			SocketAddress{"127.0.0.1", 33145},
+			1,
 		},
-		Rpk: &RpkConfig{
-			EnableUsageStats:    true,
-			TuneNetwork:         true,
-			TuneDiskScheduler:   true,
-			TuneNomerges:        true,
-			TuneDiskIrq:         true,
-			TuneCpu:             true,
-			TuneAioEvents:       true,
-			TuneClocksource:     true,
-			TuneSwappiness:      true,
-			EnableMemoryLocking: true,
-			TuneCoredump:        true,
-			CoredumpDir:         "/var/lib/redpanda/coredumps",
-			WellKnownIo:         "vendor:vm:storage",
+		&SeedServer{
+			SocketAddress{"127.0.0.1", 33146},
+			2,
 		},
 	}
+	conf.Rpk = &RpkConfig{
+		EnableUsageStats:    true,
+		TuneNetwork:         true,
+		TuneDiskScheduler:   true,
+		TuneNomerges:        true,
+		TuneDiskIrq:         true,
+		TuneCpu:             true,
+		TuneAioEvents:       true,
+		TuneClocksource:     true,
+		TuneSwappiness:      true,
+		EnableMemoryLocking: true,
+		TuneCoredump:        true,
+		CoredumpDir:         "/var/lib/redpanda/coredumps",
+		WellKnownIo:         "vendor:vm:storage",
+	}
+	return &conf
 }
 
 func TestSet(t *testing.T) {
@@ -185,36 +160,21 @@ func TestSet(t *testing.T) {
 			fs := afero.NewMemMapFs()
 			conf := DefaultConfig()
 			err := WriteConfig(fs, &conf, conf.ConfigFile)
-			if err != nil {
-				t.Error(err.Error())
-			}
+			require.NoError(t, err)
 			err = Set(fs, tt.key, tt.value, tt.format, conf.ConfigFile)
 			if tt.expectErr {
-				if err == nil {
-					t.Fatal("expected an error, got nil")
-				}
+				require.Error(t, err)
 				return
 			}
-			if err != nil {
-				t.Fatalf("got an unexpected error: %v", err.Error())
-			}
+			require.NoError(t, err)
 			v := viper.New()
 			v.SetFs(fs)
 			v.SetConfigType("yaml")
 			v.SetConfigFile(conf.ConfigFile)
 			err = v.ReadInConfig()
-			if err != nil {
-				t.Fatal(err)
-			}
+			require.NoError(t, err)
 			val := v.Get(tt.key)
-			if !reflect.DeepEqual(val, tt.expected) {
-				t.Fatalf(
-					"expected: \n'%+v'\n but got: \n'%+v'\n for key '%s'",
-					tt.expected,
-					val,
-					tt.key,
-				)
-			}
+			require.Exactly(t, tt.expected, val)
 		})
 	}
 }
@@ -247,20 +207,14 @@ func TestDefaultConfig(t *testing.T) {
 			CoredumpDir:         "/var/lib/redpanda/coredump",
 		},
 	}
-	if !reflect.DeepEqual(defaultConfig, expected) {
-		t.Fatalf("got:\n%v+\nexpected\n%v+", defaultConfig, expected)
-	}
+	require.Exactly(t, expected, defaultConfig)
 }
 
 func TestReadConfigFromPath(t *testing.T) {
 	const baseDir string = "/etc/redpanda"
-	type args struct {
-		fs   afero.Fs
-		path string
-	}
 	tests := []struct {
 		name    string
-		args    args
+		path    string
 		before  func(afero.Fs, string) error
 		want    func() *Config
 		wantErr bool
@@ -278,10 +232,7 @@ func TestReadConfigFromPath(t *testing.T) {
 				_, err = utils.WriteBytes(fs, bs, path)
 				return err
 			},
-			args: args{
-				fs:   afero.NewMemMapFs(),
-				path: baseDir + "/redpanda.yaml",
-			},
+			path:    baseDir + "/redpanda.yaml",
 			want:    getValidConfig,
 			wantErr: false,
 		},
@@ -289,18 +240,17 @@ func TestReadConfigFromPath(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.before(tt.args.fs, tt.args.path); err != nil {
-				t.Fatalf("got an error while setting up %v: %v", tt.name, err)
-			}
-			got, err := ReadConfigFromPath(tt.args.fs, tt.args.path)
+			fs := afero.NewMemMapFs()
+			err := tt.before(fs, tt.path)
+			require.NoError(t, err)
+			got, err := ReadConfigFromPath(fs, tt.path)
 			want := tt.want()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("ReadConfigFromPath() error = %v, wantErr %v", err, tt.wantErr)
+			if tt.wantErr {
+				require.Error(t, err)
 				return
 			}
-			if !reflect.DeepEqual(got, want) {
-				t.Errorf("ReadConfigFromPath() = %v, want %v", *got, *want)
-			}
+			require.NoError(t, err)
+			require.Exactly(t, want, got)
 		})
 	}
 }
@@ -308,42 +258,35 @@ func TestReadConfigFromPath(t *testing.T) {
 func TestWriteConfig(t *testing.T) {
 	const path string = "/redpanda.yaml"
 	type args struct {
-		existingConf string
-		conf         func() *Config
-		fs           afero.Fs
-		path         string
 	}
 	tests := []struct {
-		name     string
-		args     args
-		wantErr  bool
-		expected string
+		name         string
+		existingConf string
+		conf         func() *Config
+		wantErr      bool
+		expected     string
 	}{
 		{
 			name: "shall write a valid config file without an rpk config object",
-			args: args{
-				fs:   afero.NewMemMapFs(),
-				path: path,
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Rpk = nil
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Rpk = nil
+				return c
 			},
 			wantErr: false,
 			expected: `config_file: /etc/redpanda/redpanda.yaml
 pid_file: /var/lib/redpanda/pid
 redpanda:
   admin:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 9644
   data_directory: /var/lib/redpanda/data
   kafka_api:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 9092
-  node_id: 1
+  node_id: 0
   rpc_server:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 33145
   seed_servers:
   - host:
@@ -358,49 +301,37 @@ redpanda:
 		},
 		{
 			name: "shall fail with an invalid config",
-			args: args{
-				fs:   afero.NewMemMapFs(),
-				path: path,
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.Directory = ""
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.Directory = ""
+				return c
 			},
 			wantErr: true,
 		},
 		{
 			name: "shall fail if there's no redpanda config",
-			args: args{
-				fs:   afero.NewMemMapFs(),
-				path: path,
-				conf: func() *Config {
-					return &Config{}
-				},
+			conf: func() *Config {
+				return &Config{}
 			},
 			wantErr: true,
 		},
 		{
-			name: "shall write a valid config file with an rpk config object",
-			args: args{
-				fs:   afero.NewMemMapFs(),
-				path: path,
-				conf: getValidConfig,
-			},
+			name:    "shall write a valid config file with an rpk config object",
+			conf:    getValidConfig,
 			wantErr: false,
 			expected: `config_file: /etc/redpanda/redpanda.yaml
 pid_file: /var/lib/redpanda/pid
 redpanda:
   admin:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 9644
   data_directory: /var/lib/redpanda/data
   kafka_api:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 9092
-  node_id: 1
+  node_id: 0
   rpc_server:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 33145
   seed_servers:
   - host:
@@ -429,22 +360,21 @@ rpk:
 		},
 		{
 			name: "shall leave unrecognized fields untouched",
-			args: args{
-				existingConf: `config_file: /etc/redpanda/redpanda.yaml
+			existingConf: `config_file: /etc/redpanda/redpanda.yaml
 pid_file: /var/lib/redpanda/pid
 redpanda:
   admin:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 9644
   admin_api_doc_dir: /etc/redpanda/doc
   data_directory: /var/lib/redpanda/data
   default_window_sec: 100
   kafka_api:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 9092
-  node_id: 1
+  node_id: 0
   rpc_server:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 33145
   seed_servers:
   - host:
@@ -459,26 +389,23 @@ redpanda:
 unrecognized_top_field:
   child: true
 `,
-				fs:   afero.NewMemMapFs(),
-				path: path,
-				conf: getValidConfig,
-			},
+			conf:    getValidConfig,
 			wantErr: false,
 			expected: `config_file: /etc/redpanda/redpanda.yaml
 pid_file: /var/lib/redpanda/pid
 redpanda:
   admin:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 9644
   admin_api_doc_dir: /etc/redpanda/doc
   data_directory: /var/lib/redpanda/data
   default_window_sec: 100
   kafka_api:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 9092
-  node_id: 1
+  node_id: 0
   rpc_server:
-    address: 127.0.0.1
+    address: 0.0.0.0
     port: 33145
   seed_servers:
   - host:
@@ -511,48 +438,31 @@ unrecognized_top_field:
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.args.existingConf != "" {
-				_, err := utils.WriteBytes(tt.args.fs, []byte(tt.args.existingConf), tt.args.path)
-				if err != nil {
-					t.Fatal(err.Error())
-				}
+			fs := afero.NewMemMapFs()
+			if tt.existingConf != "" {
+				_, err := utils.WriteBytes(fs, []byte(tt.existingConf), path)
+				require.NoError(t, err)
 			} else {
 				// Write the config file so that WriteConfig backs it up
 				// when the new one is written.
-				err := vyaml.Persist(tt.args.fs, tt.args.conf(), tt.args.path)
-				if err != nil {
-					t.Fatal(err.Error())
-				}
+				err := vyaml.Persist(fs, tt.conf(), path)
+				require.NoError(t, err)
 			}
-			err := WriteConfig(tt.args.fs, tt.args.conf(), tt.args.path)
+			err := WriteConfig(fs, tt.conf(), path)
 			if tt.wantErr {
-				if err == nil {
-					t.Fatal("expected an error, got nil")
-				}
+				require.Error(t, err)
 				return
-			} else if err != nil {
-				t.Fatalf("got an unexpected error: %v", err)
 			}
+			require.NoError(t, err)
 
-			contentBytes, err := afero.ReadFile(tt.args.fs, path)
-			if err != nil {
-				t.Errorf("got an error while reading %v: %v", tt.args.path, err)
-			}
+			contentBytes, err := afero.ReadFile(fs, path)
+			require.NoError(t, err)
 			content := string(contentBytes)
-			if content != tt.expected {
-				t.Errorf("Expected:\n'%s'\n Got:\n'%s'\n content differs",
-					strings.ReplaceAll(tt.expected, " ", "·"),
-					strings.ReplaceAll(content, " ", "·"),
-				)
-			}
-			backup, err := findBackup(tt.args.fs, filepath.Dir(tt.args.path))
-			if err != nil {
-				t.Fatal(err)
-			}
-			_, err = tt.args.fs.Stat(backup)
-			if err != nil {
-				t.Errorf("got an error while stat'ing %v: %v", backup, err)
-			}
+			require.Equal(t, tt.expected, content)
+			backup, err := findBackup(fs, filepath.Dir(path))
+			require.NoError(t, err)
+			_, err = fs.Stat(backup)
+			require.NoError(t, err)
 		})
 	}
 }
@@ -592,159 +502,122 @@ func TestInitConfig(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			fs := afero.NewMemMapFs()
 			if tt.setup != nil {
-				if err := tt.setup(fs); err != nil {
-					t.Fatalf(
-						"got an error while running setup: %v",
-						err,
-					)
-				}
+				err := tt.setup(fs)
+				require.NoError(t, err)
 			}
 			_, err := ReadOrGenerate(fs, tt.configFile)
-			if err != nil {
-				if !tt.expectError {
-					t.Fatalf("got an unexpected error: %v", err)
-				} else {
-					return
-				}
-			} else {
-				if tt.expectError {
-					t.Fatalf("expected an error, but got nil")
-				}
+			if tt.expectError {
+				require.Error(t, err)
+				return
 			}
+			require.NoError(t, err)
 			_, err = ReadConfigFromPath(fs, tt.configFile)
-			if err != nil {
-				t.Fatalf("got an error reading the config: %v", err)
-			}
+			require.NoError(t, err)
 		})
 	}
 }
 
 func TestCheckConfig(t *testing.T) {
-	type args struct {
-		conf func() *Config
-	}
 	tests := []struct {
 		name     string
-		args     args
+		conf     func() *Config
 		expected []string
 	}{
 		{
-			name: "shall return no errors when config is valid",
-			args: args{
-				conf: getValidConfig,
-			},
+			name:     "shall return no errors when config is valid",
+			conf:     getValidConfig,
 			expected: []string{},
 		},
 		{
 			name: "shall return an error when config file does not contain data directory setting",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.Directory = ""
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.Directory = ""
+				return c
 			},
 			expected: []string{"redpanda.data_directory can't be empty"},
 		},
 		{
 			name: "shall return an error when id of server is negative",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.Id = -100
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.Id = -100
+				return c
 			},
 			expected: []string{"redpanda.node_id can't be a negative integer"},
 		},
 		{
 			name: "shall return an error when the RPC server port is 0",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.RPCServer.Port = 0
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.RPCServer.Port = 0
+				return c
 			},
 			expected: []string{"redpanda.rpc_server.port can't be 0"},
 		},
 		{
 			name: "shall return an error when the RPC server address is empty",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.RPCServer.Address = ""
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.RPCServer.Address = ""
+				return c
 			},
 			expected: []string{"redpanda.rpc_server.address can't be empty"},
 		},
 		{
 			name: "shall return an error when the Kafka API port is 0",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.KafkaApi.Port = 0
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.KafkaApi.Port = 0
+				return c
 			},
 			expected: []string{"redpanda.kafka_api.port can't be 0"},
 		},
 		{
 			name: "shall return an error when the Kafka API address is empty",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.KafkaApi.Address = ""
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.KafkaApi.Address = ""
+				return c
 			},
 			expected: []string{"redpanda.kafka_api.address can't be empty"},
 		},
 		{
 			name: "shall return an error when one of the seed servers' address is empty",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.SeedServers[0].Host.Address = ""
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.SeedServers[0].Host.Address = ""
+				return c
 			},
 			expected: []string{"redpanda.seed_servers.0.host.address can't be empty"},
 		},
 		{
 			name: "shall return an error when one of the seed servers' port is 0",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Redpanda.SeedServers[1].Host.Port = 0
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Redpanda.SeedServers[1].Host.Port = 0
+				return c
 			},
 			expected: []string{"redpanda.seed_servers.1.host.port can't be 0"},
 		},
 		{
 			name: "shall return no errors when tune_coredump is set to false," +
 				"regardless of coredump_dir's value",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Rpk.TuneCoredump = false
-					c.Rpk.CoredumpDir = ""
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Rpk.TuneCoredump = false
+				c.Rpk.CoredumpDir = ""
+				return c
 			},
 			expected: []string{},
 		},
 		{
 			name: "shall return an error when tune_coredump is set to true," +
 				"but coredump_dir is empty",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Rpk.CoredumpDir = ""
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Rpk.CoredumpDir = ""
+				return c
 			},
 			expected: []string{"if rpk.tune_coredump is set to true," +
 				"rpk.coredump_dir can't be empty"},
@@ -752,31 +625,22 @@ func TestCheckConfig(t *testing.T) {
 		{
 			name: "shall return no error if setup is empty," +
 				"but coredump_dir is empty",
-			args: args{
-				conf: func() *Config {
-					c := getValidConfig()
-					c.Rpk.WellKnownIo = ""
-					return c
-				},
+			conf: func() *Config {
+				c := getValidConfig()
+				c.Rpk.WellKnownIo = ""
+				return c
 			},
 			expected: []string{},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, got := CheckConfig(tt.args.conf())
-			if len(got) != len(tt.expected) {
-				t.Fatalf("got a different amount of errors than expected: got: %v expected: %v", got, tt.expected)
+			_, got := CheckConfig(tt.conf())
+			errMsgs := []string{}
+			for _, err := range got {
+				errMsgs = append(errMsgs, err.Error())
 			}
-			for _, errMsg := range tt.expected {
-				present := false
-				for _, err := range got {
-					present = present || errMsg == err.Error()
-				}
-				if !present {
-					t.Errorf("expected error msg \"%v\" wasn't among the result error set %v", errMsg, got)
-				}
-			}
+			require.Exactly(t, tt.expected, errMsgs)
 		})
 	}
 }
@@ -788,27 +652,15 @@ func TestWriteAndGenerateNodeUuid(t *testing.T) {
 	conf := getValidConfig()
 	conf.ConfigFile = path
 	bs, err := yaml.Marshal(conf)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err = fs.MkdirAll(baseDir, 0755); err != nil {
-		t.Fatal(err)
-	}
-	if _, err = utils.WriteBytes(fs, bs, path); err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
+	err = fs.MkdirAll(baseDir, 0755)
+	require.NoError(t, err)
+	_, err = utils.WriteBytes(fs, bs, path)
+	require.NoError(t, err)
 	conf, err = GenerateAndWriteNodeUuid(fs, conf)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if conf.NodeUuid == "" {
-		t.Fatal("the NodeUuid field is empty")
-	}
+	require.NoError(t, err)
+	require.NotEqual(t, "", conf.NodeUuid)
 	readConf, err := ReadConfigFromPath(fs, path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(conf, readConf) {
-		t.Fatalf("got\n'%v'\nexpected\n'%v'", readConf, conf)
-	}
+	require.NoError(t, err)
+	require.Exactly(t, conf, readConf)
 }
