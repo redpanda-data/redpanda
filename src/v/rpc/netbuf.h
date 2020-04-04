1f0b09357747c8997fd716f01b3b2182f4db8f11
Alexander Gallego <alex@vectorized.io>
2020-04-04 16:16:39 -0700

    rpc: add zstd compression support in RPC

--- src/v/rpc/netbuf.h
+++ src/v/rpc/netbuf.h
@@ -2,6 +2,7 @@
 
 #include "bytes/iobuf.h"
 #include "rpc/types.h"
+#include "vassert.h"
 
 #include <seastar/core/scattered_message.hh>
 
@@ -15,12 +16,27 @@ public:
     void set_correlation_id(uint32_t);
     void set_compression(rpc::compression_type c);
     void set_service_method_id(uint32_t);
-
-    iobuf& buffer() { return _out; }
+    void set_min_compression_bytes(size_t);
+    iobuf& buffer();
 
 private:
+    size_t _min_compression_bytes{1024};
     header _hdr;
     iobuf _out;
 };
+
+inline iobuf& netbuf::buffer() { return _out; }
+inline void netbuf::set_compression(rpc::compression_type c) {
+    vassert(
+      c >= compression_type::min && c <= compression_type::max,
+      "invalid compression type: {}",
+      int(c));
+    _hdr.compression = c;
+}
+inline void netbuf::set_correlation_id(uint32_t x) { _hdr.correlation_id = x; }
+inline void netbuf::set_service_method_id(uint32_t x) { _hdr.meta = x; }
+inline void netbuf::set_min_compression_bytes(size_t min) {
+    _min_compression_bytes = min;
+}
 
 } // namespace rpc
