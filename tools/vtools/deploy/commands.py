624df691f0961c7a71b682f967df9517304472c8
David Castillo <david@vectorized.io>
2020-04-07 13:17:09 -0500

    vtools/deploy: Add support for multiple providers

--- tools/vtools/deploy/commands.py
+++ tools/vtools/deploy/commands.py
@@ -1,7 +1,11 @@
 import click
 import os
 
+from absl import logging
+
 from ..vlib import config
+from ..vlib import git
+from ..vlib import rotate_ssh_keys as keys
 from ..vlib import shell
 from ..vlib import terraform as tf
 
@@ -17,6 +21,9 @@ def deploy():
                     'file is searched recursively starting from the current '
                     'working directory.'),
               default=None)
+@click.option('--provider',
+              default='aws',
+              type=click.Choice(['aws', 'gcp'], case_sensitive=False))
 @click.option('--destroy',
               default=False,
               help='Tear down the deployed resources')
@@ -29,13 +36,22 @@ def deploy():
               type=click.Choice(['debug', 'info', 'warning', 'error', 'fatal'],
                                 case_sensitive=False))
 @click.argument('tfvars', nargs=-1)
-def cluster(conf, destroy, ssh_key, log,
-            tfvars):
+def cluster(conf, provider, destroy, ssh_key, log, tfvars):
+    logging.set_verbosity(log)
     vconfig = config.VConfig(conf)
     if destroy:
-        tf.destroy(vconfig, ssh_key, log)
+        tf.destroy(vconfig, provider, 'cluster')
         return
-    tf.apply(vconfig, ssh_key, log, tfvars)
+
+    git.verify(vconfig.src_dir)
+    user_email = git.get_email(vconfig.src_dir)
+    user = user_email.replace('@vectorized.io', '')
+
+    abs_path = os.path.abspath(os.path.expanduser(ssh_key))
+    key_path, pub_key_path = keys.generate_key(abs_path)
+
+    tfvars += (f'public_key_path={pub_key_path}',)
+    tf.apply(vconfig, provider, 'cluster', tfvars)
 
 
 @deploy.command(short_help='Run ansible against a cluster.')
@@ -44,25 +60,33 @@ def cluster(conf, destroy, ssh_key, log,
                     'file is searched recursively starting from the current '
                     'working directory.'),
               default=None)
-@click.option('--ssh-key',
-              help='The path where of the SSH to use (the key will be' +
-              'generated if it doesn\'t exist)',
-              default='~/.ssh/infra-key')
 @click.option('--playbook',
               help='Ansible playbook to run',
               required=True,
               multiple=True)
+@click.option('--ssh-key',
+              help='The path where of the SSH to use (the key will be' +
+              'generated if it doesn\'t exist)',
+              default='~/.ssh/infra-key')
+@click.option('--provider',
+              default='aws',
+              type=click.Choice(['aws', 'gcp'], case_sensitive=False))
+@click.option('--log',
+              default='info',
+              type=click.Choice(['debug', 'info', 'warning', 'error', 'fatal'],
+                                case_sensitive=False))
 @click.option('--var',
               help='Ansible variable in FOO=BAR format',
               multiple=True,
               required=False)
-def ansible(conf, playbook, ssh_key, var):
+def ansible(conf, playbook, ssh_key, provider, log, var):
     """Runs a playbook against a cluster deployed with 'vtools deploy cluster'
     """
+    logging.set_verbosity(log)
     vconfig = config.VConfig(conf)
 
-    tf_out = tf._get_tf_outputs(vconfig, 'cluster')
-
+    tf_out = tf.get_tf_outputs(vconfig, provider, 'cluster')
+    ssh_user = tf_out['ssh_user']['value']
     os.makedirs(vconfig.ansible_dir, exist_ok=True)
 
     # write hosts.ini
@@ -70,14 +94,14 @@ def ansible(conf, playbook, ssh_key, var):
     with open(invfile, 'w') as f:
         zipped = zip(tf_out['ip']['value'], tf_out['private_ips']['value'])
         for i, (ip, pip) in enumerate(zipped):
-            f.write(f'{ip} ansible_user=root ansible_become=True '
+            f.write(f'{ip} ansible_user={ssh_user} ansible_become=True '
                     f'private_ip={pip} id={i+1}\n')
 
     # create extra vars flags
     evar = f'-e {" -e ".join(var)}' if var else ''
 
     # run given playbooks
-    cmd = (f'ansible-playbook --private-key {ssh_key} '
-           f'{evar} -i {invfile} -v {" ".join(playbook)}')
+    cmd = (f'ansible-playbook --private-key {ssh_key}'
+           f' {evar} -i {invfile} -v {" ".join(playbook)}')
 
     shell.run_subprocess(cmd, env=vconfig.environ)
