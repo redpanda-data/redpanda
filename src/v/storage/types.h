ef6af53ab9d2b697ab5d7f5c8abc2f9421e07248
Alexander Gallego <alex@vectorized.io>
2020-04-07 07:49:40 -0700

    storage:types.h de-inline output operators; make friends

--- src/v/storage/types.h
+++ src/v/storage/types.h
@@ -26,6 +26,8 @@ struct append_result {
     model::offset last_offset;
     size_t byte_size;
     model::term_id last_term;
+
+    friend std::ostream& operator<<(std::ostream& o, const append_result&);
 };
 struct timequery_config {
     timequery_config(
@@ -36,6 +38,8 @@ struct timequery_config {
     model::timestamp time;
     model::offset max_offset;
     ss::io_priority_class prio;
+
+    friend std::ostream& operator<<(std::ostream& o, const timequery_config&);
 };
 struct timequery_result {
     timequery_result(model::offset o, model::timestamp t) noexcept
@@ -43,6 +47,8 @@ struct timequery_result {
       , time(t) {}
     model::offset offset;
     model::timestamp time;
+
+    friend std::ostream& operator<<(std::ostream& o, const timequery_result&);
 };
 
 /**
@@ -104,34 +110,8 @@ struct log_reader_config {
         prio,
         std::nullopt,
         std::nullopt) {}
+
+    friend std::ostream& operator<<(std::ostream& o, const log_reader_config&);
 };
-
-inline std::ostream& operator<<(std::ostream& o, const log_reader_config& cfg) {
-    o << "{start_offset:" << cfg.start_offset
-      << ", max_offset:" << cfg.max_offset << ", min_bytes:" << cfg.min_bytes
-      << ", max_bytes:" << cfg.max_bytes << ", type_filter:";
-    if (cfg.type_filter) {
-        o << *cfg.type_filter;
-    } else {
-        o << "nullopt";
-    }
-    o << ", first_timestamp:";
-    if (cfg.first_timestamp) {
-        o << *cfg.first_timestamp;
-    } else {
-        o << "nullopt";
-    }
-    return o << "}";
-}
-
-inline std::ostream& operator<<(std::ostream& o, const append_result& a) {
-    return o << "{append_time:"
-             << std::chrono::duration_cast<std::chrono::milliseconds>(
-                  a.append_time.time_since_epoch())
-                  .count()
-             << ", base_offset:" << a.base_offset
-             << ", last_offset:" << a.last_offset
-             << ", byte_size:" << a.byte_size << "}";
-}
 
 } // namespace storage
