c2d33cd2462397fe17cb0dab263604e6210500c9
Alexander Gallego <alex@vectorized.io>
2020-04-10 15:21:14 -0700

    vtools: reduce logging noise. elevate some info to debug

--- tools/vtools/vlib/kv.py
+++ tools/vtools/vlib/kv.py
@@ -9,6 +9,6 @@ from playhouse.kv import KeyValue
 def vectorized_kv(root):
     root = os.path.abspath(root)
     db_file = f'{root}/.vtools.db'
-    logging.info(f"key=value db in {db_file}")
+    logging.debug(f"key=value db in {db_file}")
     db = SqliteDatabase(db_file)
     return KeyValue(ordered=True, database=db, table_name="vectorized")
