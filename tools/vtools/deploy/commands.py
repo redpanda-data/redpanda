7c99ff4fe4b053516539ca15992f2be6194b8a1f
David Castillo <david@vectorized.io>
2020-04-10 19:35:22 -0500

    vtools/deploy: Add subcommand to make it easier to ssh into vms

--- tools/vtools/deploy/commands.py
+++ tools/vtools/deploy/commands.py
@@ -7,6 +7,7 @@ from ..vlib import config
 from ..vlib import git
 from ..vlib import rotate_ssh_keys as keys
 from ..vlib import shell
+from ..vlib import ssh as vssh
 from ..vlib import terraform as tf
 
 
@@ -125,3 +126,51 @@ def ansible(conf, playbook, ssh_key, provider, module, log, var):
            f' {evar} -i {invfile} -v {" ".join(playbook)}')
 
     shell.run_subprocess(cmd, env=vconfig.environ)
+
+
+@deploy.command(short_help='SSH into the deployed VMs')
+@click.option('--conf',
+              help=('Path to configuration file. If not given, a .vtools.yml '
+                    'file is searched recursively starting from the current '
+                    'working directory.'),
+              default=None)
+@click.option('--provider',
+              required=False,
+              type=click.Choice(['aws', 'gcp'], case_sensitive=False))
+@click.option('--module',
+              default='cluster',
+              type=click.Choice(['cluster'], case_sensitive=False))
+def ssh(conf, provider, module):
+    vconfig = config.VConfig(conf)
+    tf_out = tf.get_tf_outputs(vconfig, provider, module)
+
+    if not tf_out:
+        logging.fatal(
+            f'No deployment found for module {module} in ptovider {provider}')
+
+    public_key = tf_out['public_key_path']['value']
+    key = public_key.replace('.pub', '')
+    chosen_ip = ''
+
+    ssh_user = tf_out['ssh_user']['value']
+    ip = tf_out['ip']['value']
+    if isinstance(ip, list):
+        ip = sorted(ip)
+        length = len(ip)
+        if length == 1:
+            chosen_ip = ip[0]
+        else:
+            for i in range(0, length):
+                click.echo(f'{i}: {ip[i]}')
+            val = click.prompt(
+                f'Please select a host from the list above (0 - {length - 1})',
+                type=int)
+            chosen_ip = ip[val]
+    else:
+        chosen_ip = ip
+    vssh.remove_known_host(chosen_ip)
+    vssh.add_known_host(chosen_ip)
+    logging.info(f'/usr/bin/ssh -i "{key}" {ssh_user}@{chosen_ip}')
+    os.execve('/usr/bin/ssh',
+              ['/usr/bin/ssh', '-i', key, f'{ssh_user}@{chosen_ip}'],
+              os.environ)
