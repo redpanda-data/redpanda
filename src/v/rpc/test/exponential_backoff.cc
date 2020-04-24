e23f0b1ad44504ffcb31bc5214fe95c1520dd764
Michal Maslanka <michal@vectorized.io>
2020-04-24 13:35:41 +0200

    rpc/backoff_policy: implemented backoff policy
    Implemented virtualized backoff_policy type the type will be used to
    implement circuit breaker pattern in rpc::reconnect_transport.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/rpc/test/exponential_backoff.cc
+++ src/v/rpc/test/exponential_backoff.cc
@@ -1,12 +1,43 @@
+#include <seastar/core/lowres_clock.hh>
+
+#include <boost/test/tools/old/interface.hpp>
+
+#include <chrono>
 #define BOOST_TEST_MODULE bytes
-#include "rpc/reconnect_transport.h"
+#include "rpc/backoff_policy.h"
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(test_exponential_backoff) {
-    uint32_t bo = 0;
-    for (auto i = 0; i < 32; ++i) {
-        bo = rpc::reconnect_transport::next_backoff(bo);
-        BOOST_CHECK(bo >= 1 && bo <= 300);
-    }
+BOOST_AUTO_TEST_CASE(exponential_backoff_policy_test) {
+    using namespace std::chrono_literals;
+    rpc::backoff_policy p
+      = rpc::make_exponential_backoff_policy<ss::lowres_clock>(1s, 120s);
+
+    BOOST_CHECK(p.current_backoff_duration() == 0s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 1s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 2s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 4s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 8s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 16s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 32s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 64s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 120s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 120s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 120s);
+    p.reset();
+    BOOST_CHECK(p.current_backoff_duration() == 0s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 1s);
+    p.next_backoff();
+    BOOST_CHECK(p.current_backoff_duration() == 2s);
 }
