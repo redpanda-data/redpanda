b5f477cb1cae5146f6ef50c7d0c35faa9f3df944
Alexander Gallego <alex@vectorized.io>
2020-04-24 18:08:10 -0700

    gcc:fix build: add workaround for std::dequeue not noexcept ctor
    https://gcc.gnu.org/legacy-ml/gcc-help/2017-08/msg00078.html
    in gcc making it noexcept would break ABI

--- src/v/storage/segment_set.h
+++ src/v/storage/segment_set.h
@@ -2,6 +2,8 @@
 
 #include "storage/segment.h"
 
+#include <seastar/core/circular_buffer.hh>
+
 #include <deque>
 
 namespace storage {
@@ -22,11 +24,14 @@ public:
     // type _must_ offer stable segment addresses
     // for readers and writers taking refs.
     using type = ss::lw_shared_ptr<segment>;
-    using underlying_t = std::deque<type>;
+
+    // NOTE: gcc has an ABI problem and cannot make std::deque noexcept
+    // so we use a circular instead of a dequeue that *is* noexcept ctor
+    // and allow us to truly have an empty container at ctor time
+    // We loose reverse-iterators tho
+    using underlying_t = ss::circular_buffer<type>;
     using const_iterator = underlying_t::const_iterator;
     using iterator = underlying_t::iterator;
-    using reverse_iterator = underlying_t::reverse_iterator;
-    using const_reverse_iterator = underlying_t::const_reverse_iterator;
 
     explicit segment_set(underlying_t);
     ~segment_set() noexcept = default;
@@ -63,11 +68,6 @@ public:
     iterator end() { return _handles.end(); }
     const_iterator begin() const { return _handles.begin(); }
     const_iterator end() const { return _handles.end(); }
-    // reverse
-    reverse_iterator rbegin() { return _handles.rbegin(); }
-    reverse_iterator rend() { return _handles.rend(); }
-    const_reverse_iterator rbegin() const { return _handles.rbegin(); }
-    const_reverse_iterator rend() const { return _handles.rend(); }
 
 private:
     underlying_t _handles;
