3a0a15d759675bb29d42d0ef10ce71f687bae952
Noah Watkins <noah@vectorized.io>
2020-04-28 09:50:20 -0700

    r/consensus: remove dead code
    this was left over from the lockless heartbeats work.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/raft/consensus.h
+++ src/v/raft/consensus.h
@@ -100,10 +100,6 @@ public:
 
     model::offset start_offset() const { return _log.offsets().start_offset; }
 
-    ss::future<ss::semaphore_units<>> op_lock_unit() {
-        return ss::get_units(_op_sem, 1);
-    }
-
 private:
     friend replicate_entries_stm;
     friend vote_stm;
