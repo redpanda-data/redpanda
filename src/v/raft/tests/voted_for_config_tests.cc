adcf67decfc3f86091bbcd1d655d6ae1e9084d1f
Michal Maslanka <michal@vectorized.io>
2020-05-08 13:31:59 +0200

    r/consensus_utils: using state_crc_file to persist voted_for file
    Switched over to state_crc_file utility to persist raft voted for state.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/raft/tests/voted_for_config_tests.cc
+++ src/v/raft/tests/voted_for_config_tests.cc
@@ -8,6 +8,18 @@
 #include <seastar/core/thread.hh>
 #include <seastar/testing/thread_test_case.hh>
 
+SEASTAR_THREAD_TEST_CASE(test_reading_legacy_voted_for_config) {
+    raft::consensus::voted_for_configuration cfg;
+    cfg.voted_for = model::node_id(42);
+    cfg.term = model::term_id(77);
+
+    raft::details::legacy_persist_voted_for("./test.yml", cfg).get();
+    auto read = raft::details::read_voted_for("./test.yml").get0().value();
+
+    BOOST_REQUIRE_EQUAL(cfg.voted_for, read.voted_for);
+    BOOST_REQUIRE_EQUAL(cfg.term, read.term);
+};
+
 SEASTAR_THREAD_TEST_CASE(write_and_read_voted_for_config) {
     raft::consensus::voted_for_configuration cfg;
     cfg.voted_for = model::node_id(42);
@@ -15,7 +27,8 @@ SEASTAR_THREAD_TEST_CASE(write_and_read_voted_for_config) {
     std::cout << "persisting?" << std::endl;
     raft::details::persist_voted_for("./test.yml", cfg).get();
     std::cout << "reading?" << std::endl;
-    auto const cfg_dup = raft::details::read_voted_for("./test.yml").get0();
+    auto const cfg_dup
+      = raft::details::read_voted_for("./test.yml").get0().value();
     std::cout << "cfg.voted_for " << cfg.voted_for << ", cfg.term " << cfg.term
               << ", dup.voted_for" << cfg_dup.voted_for << ", dup.term "
               << cfg_dup.term << std::endl;
