c91974d5cc9c17fa4253b48f7b7a92d942275bf8
David Castillo <david@vectorized.io>
2020-05-12 22:00:51 -0500

    rpk/generate: Add command to generate the Prometheus targets config

--- src/go/rpk/pkg/cli/cmd/generate.go
+++ src/go/rpk/pkg/cli/cmd/generate.go
@@ -3,14 +3,16 @@ package cmd
 import (
 	"vectorized/pkg/cli/cmd/generate"
 
+	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 )
 
-func NewGenerateCommand() *cobra.Command {
+func NewGenerateCommand(fs afero.Fs) *cobra.Command {
 	command := &cobra.Command{
 		Use:   "generate [template]",
 		Short: "Generate a configuration template for related services.",
 	}
 	command.AddCommand(generate.NewGrafanaDashboardCmd())
+	command.AddCommand(generate.NewPrometheusConfigCmd(fs))
 	return command
 }
