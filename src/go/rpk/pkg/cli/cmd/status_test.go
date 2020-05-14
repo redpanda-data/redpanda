8a54b05f0989a204750399a6dded3645a3d5fe8a
David Castillo <david@vectorized.io>
2020-05-14 11:54:27 -0500

    rpk/status: Compress sequential partition ids into ranges

--- src/go/rpk/pkg/cli/cmd/status_test.go
+++ src/go/rpk/pkg/cli/cmd/status_test.go
@@ -1,9 +1,8 @@
-package cmd_test
+package cmd
 
 import (
 	"bytes"
 	"testing"
-	"vectorized/pkg/cli/cmd"
 	"vectorized/pkg/config"
 
 	"github.com/sirupsen/logrus"
@@ -110,7 +109,7 @@ func TestStatus(t *testing.T) {
 				require.NoError(t, err)
 			}
 			var out bytes.Buffer
-			cmd := cmd.NewStatusCommand(fs)
+			cmd := NewStatusCommand(fs)
 			cmd.SetArgs(tt.args)
 			logrus.SetOutput(&out)
 			err := cmd.Execute()
@@ -122,6 +121,51 @@ func TestStatus(t *testing.T) {
 			if tt.expectedOut != "" {
 				require.Contains(t, out.String(), tt.expectedOut)
 			}
+		})
+	}
+}
+
+func TestCompress(t *testing.T) {
+	tests := []struct {
+		name     string
+		ints     []int
+		expected []string
+	}{
+		{
+			name:     "test 1",
+			ints:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			expected: []string{"1-20"},
+		},
+		{
+			name:     "test 2",
+			ints:     []int{0, 2, 3, 4, 5, 7, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			expected: []string{"0", "2-5", "7", "9", "10", "12-20"},
+		},
+		{
+			name:     "test 3",
+			ints:     []int{0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20},
+			expected: []string{"0", "2", "4", "6", "8", "10", "12", "14", "16", "18", "20"},
+		},
+		{
+			name:     "test 4",
+			ints:     []int{},
+			expected: []string{},
+		},
+		{
+			name:     "test 4",
+			ints:     []int{1},
+			expected: []string{"1"},
+		},
+		{
+			name:     "test 4",
+			ints:     []int{1, 2},
+			expected: []string{"1", "2"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(st *testing.T) {
+			require.Equal(st, tt.expected, compress(tt.ints))
 		})
 	}
 }
