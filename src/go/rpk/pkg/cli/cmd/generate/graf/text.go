b98130c93be5fcc682fa6f167abeaebd8408491b
David Castillo <david@vectorized.io>
2020-05-15 10:21:37 -0500

    rpk/graf: Add support for text panels

---
+++ src/go/rpk/pkg/cli/cmd/generate/graf/text.go
@@ -0,0 +1,38 @@
+package graf
+
+import "encoding/json"
+
+type TextPanel struct {
+	BasePanel
+	Content string `json:"content"`
+	Mode    string `json:"mode"`
+}
+
+func (*TextPanel) Type() string {
+	return "text"
+}
+
+func (p *TextPanel) MarshalJSON() ([]byte, error) {
+	type PanelAlias TextPanel
+	typedPanel := struct {
+		Type string `json:"type"`
+		PanelAlias
+	}{
+		p.Type(),
+		(PanelAlias)(*p),
+	}
+	return json.Marshal(typedPanel)
+}
+
+func NewTextPanel(content, mode string) *TextPanel {
+	return &TextPanel{
+		BasePanel: BasePanel{
+			ID:       nextID(),
+			Editable: true,
+			GridPos:  GridPos{W: 8, H: 8},
+			Span:     1,
+		},
+		Content: content,
+		Mode:    mode,
+	}
+}
