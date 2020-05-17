166a541f76198075d88f5428fdd869c78feccef4
Noah Watkins <noah@vectorized.io>
2020-05-17 10:25:33 -0700

    raft: add offset monitor utility
    Similar to notification latch, this utility allows monitoring to changes
    to a particular offset. The primary difference is that this supports
    multiple waiters at the same offset, and a combination of abort sources
    and timeouts.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

---
+++ src/v/raft/offset_monitor.cc
@@ -0,0 +1,77 @@
+#include "raft/offset_monitor.h"
+
+#include "vassert.h"
+
+#include <seastar/core/future-util.hh>
+
+namespace raft {
+
+void offset_monitor::stop() {
+    for (auto& waiter : _waiters) {
+        waiter.second->done.set_exception(wait_aborted());
+    }
+    _waiters.clear();
+}
+
+ss::future<> offset_monitor::wait(
+  model::offset offset,
+  model::timeout_clock::time_point timeout,
+  std::optional<std::reference_wrapper<ss::abort_source>> as) {
+    auto w = std::make_unique<waiter>(this, timeout, as);
+    auto f = w->done.get_future();
+    if (!f.available()) {
+        // the future may already be available, for example if an abort had
+        // already be requested. in that case, skip adding as a waiter.
+        _waiters.emplace(offset, std::move(w));
+    }
+    return f;
+}
+
+void offset_monitor::notify(model::offset offset) {
+    while (true) {
+        auto it = _waiters.begin();
+        if (it == _waiters.end() || offset < it->first) {
+            return;
+        }
+        it->second->done.set_value();
+        // when the waiter is destroyed here by erase, then if they are active,
+        // the timer is cancelled and the abort source subscription is removed.
+        _waiters.erase(it);
+    }
+}
+
+offset_monitor::waiter::waiter(
+  offset_monitor* mon,
+  model::timeout_clock::time_point timeout,
+  std::optional<std::reference_wrapper<ss::abort_source>> as)
+  : mon(mon) {
+    if (as) {
+        auto opt_sub = as->get().subscribe([this] { handle_abort(); });
+        if (opt_sub) {
+            sub = std::move(*opt_sub);
+        } else {
+            done.set_exception(wait_aborted());
+            return;
+        }
+    }
+    if (timeout != model::no_timeout) {
+        timer.set_callback([this] { handle_abort(); });
+        timer.arm(timeout);
+    }
+}
+
+void offset_monitor::waiter::handle_abort() {
+    done.set_exception(wait_aborted());
+    auto it = std::find_if(
+      mon->_waiters.begin(),
+      mon->_waiters.end(),
+      [this](const waiters_type::value_type& w) {
+          return w.second.get() == this;
+      });
+    vassert(it != mon->_waiters.end(), "waiter not found");
+    // when the waiter is destroyed here by erase, then if they are active,
+    // the timer is cancelled and the abort source subscription is removed.
+    mon->_waiters.erase(it); // *this is no longer valid after erase
+}
+
+} // namespace raft
