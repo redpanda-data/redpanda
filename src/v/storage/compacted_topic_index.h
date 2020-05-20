5d329a347e20dcedd8adce6a1b328dcfa5ae363e
Alexander Gallego <alex@vectorized.io>
2020-05-19 22:31:32 -0700

    storage:compacted_topic_index: add roundtrip file test

--- src/v/storage/compacted_topic_index.h
+++ src/v/storage/compacted_topic_index.h
@@ -16,30 +16,28 @@ namespace storage {
 /// vint     | []byte | vint
 /// vint     | []byte | vint
 /// ...
-/// footer
+/// footer - in little endian
 class compacted_topic_index {
 public:
-    static constexpr size_t footer_size = 16;
+    static constexpr size_t footer_size = 13;
     struct footer {
-        int8_t version{0};
-        int8_t unused_padding_1{0};
-        int16_t unused_padding_2{0};
         uint32_t size{0};
-        uint32_t crc{0};
         uint32_t keys{0};
+        uint32_t crc{0}; // crc32
+        // version *must* be the last value
+        int8_t version{0};
     };
-    static_assert(sizeof(footer) == footer_size, "needed for lazy reader");
 
     struct impl {
         virtual ~impl() noexcept = default;
-        virtual ss::future<> write_key(const bytes&, model::offset) = 0;
+        virtual ss::future<> write_key(bytes_view, model::offset) = 0;
         virtual ss::future<> close() = 0;
     };
 
     explicit compacted_topic_index(std::unique_ptr<impl> i)
       : _impl(std::move(i)) {}
 
-    ss::future<> write_key(const bytes&, model::offset);
+    ss::future<> write_key(bytes_view, model::offset);
     ss::future<> close();
     std::unique_ptr<impl> release() &&;
 
@@ -53,7 +51,7 @@ compacted_topic_index::release() && {
 }
 
 inline ss::future<>
-compacted_topic_index::write_key(const bytes& b, model::offset o) {
+compacted_topic_index::write_key(bytes_view b, model::offset o) {
     return _impl->write_key(b, o);
 }
 
