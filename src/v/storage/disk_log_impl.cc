cc418a74267ef3a405dcd71c25a74e26838f482e
Alexander Gallego <alex@vectorized.io>
2020-05-20 15:45:53 -0700

    storage:log_manager: use the full ntp_config when creating segments
    Pass the full storage::ntp_config object when asking the log manager
    to create segments on the log's behalf. This is a setup patch for
    the compaction topic segment creation

--- src/v/storage/disk_log_impl.cc
+++ src/v/storage/disk_log_impl.cc
@@ -190,7 +190,7 @@ ss::future<> disk_log_impl::new_segment(
   model::offset o, model::term_id t, ss::io_priority_class pc) {
     vassert(
       o() >= 0 && t() >= 0, "offset:{} and term:{} must be initialized", o, t);
-    return _manager.make_log_segment(config().ntp, o, t, pc)
+    return _manager.make_log_segment(config(), o, t, pc)
       .then([this](ss::lw_shared_ptr<segment> handles) mutable {
           return remove_empty_segments().then(
             [this, h = std::move(handles)]() mutable {
