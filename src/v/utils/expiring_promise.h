8a4691e0d4be69eb27857dcb2e97229db729ced2
Michal Maslanka <michal@vectorized.io>
2020-05-25 19:57:19 +0200

    utils/expiring_promise: added abort source to expiring promise
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/utils/expiring_promise.h
+++ src/v/utils/expiring_promise.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "seastarx.h"
 
+#include <seastar/core/abort_source.hh>
 #include <seastar/core/future.hh>
 #include <seastar/core/lowres_clock.hh>
 #include <seastar/core/timer.hh>
@@ -16,7 +17,24 @@ class expiring_promise {
 public:
     template<typename ErrorFactory>
     ss::future<T> get_future_with_timeout(
-      typename Clock::time_point timeout, ErrorFactory&& err_factory) {
+      typename Clock::time_point timeout,
+      ErrorFactory&& err_factory,
+      std::optional<std::reference_wrapper<ss::abort_source>> as
+      = std::nullopt) {
+        // handle abort source
+        if (as) {
+            auto opt_sub = as.value().get().subscribe([this] {
+                if (_timer.cancel()) {
+                    _promise.set_exception(ss::abort_requested_exception{});
+                }
+            });
+            if (opt_sub) {
+                _sub = std::move(*opt_sub);
+            } else {
+                _promise.set_exception(ss::abort_requested_exception{});
+                return _promise.get_future();
+            }
+        }
         _timer.set_callback(
           [this, ef = std::forward<ErrorFactory>(err_factory)] {
               using err_t = std::result_of_t<ErrorFactory()>;
@@ -26,6 +44,7 @@ public:
               } else {
                   _promise.set_exception(ef());
               }
+              unlink_abort_source();
           });
         auto f = _promise.get_future();
         _timer.arm(timeout);
@@ -35,22 +54,31 @@ public:
     void set_value(T val) {
         if (_timer.cancel()) {
             _promise.set_value(std::move(val));
+            unlink_abort_source();
         }
     }
 
     void set_exception(std::exception_ptr&& ex) {
         if (_timer.cancel()) {
             _promise.set_exception(ex);
+            unlink_abort_source();
         }
     }
 
     void set_exception(const std::exception_ptr& ex) {
         if (_timer.cancel()) {
             _promise.set_exception(ex);
+            unlink_abort_source();
         }
     }
 
 private:
+    void unlink_abort_source() {
+        if (_sub.is_linked()) {
+            _sub.unlink();
+        }
+    }
     ss::promise<T> _promise;
     ss::timer<Clock> _timer;
+    ss::abort_source::subscription _sub;
 };
