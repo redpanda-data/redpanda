83638d5f57d2ac09e2dceb9a738fbec896214dd5
Noah Watkins <noah@vectorized.io>
2020-06-04 14:41:05 -0700

    cluster: add reserved kvstore namespace
    This namespace is redpanda:kvstore:core-id and is used for key-value
    segment storage.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/cluster/namespace.h
+++ src/v/cluster/namespace.h
@@ -1,10 +1,23 @@
 #pragma once
 #include "model/fundamental.h"
+#include "seastarx.h"
+
+#include <seastar/core/smp.hh>
 
 namespace cluster {
 
+inline const model::ns redpanda_ns("redpanda");
+
 inline const model::ntp controller_ntp(
-  model::ns("redpanda"), model::topic("controller"), model::partition_id(0));
+  redpanda_ns, model::topic("controller"), model::partition_id(0));
+
+/*
+ * The kvstore is organized as an ntp with a partition per core.
+ */
+inline const model::topic kvstore_topic("kvstore");
+inline model::ntp kvstore_ntp(ss::shard_id shard) {
+    return model::ntp(redpanda_ns, kvstore_topic, model::partition_id(shard));
+}
 
 inline const model::ns kafka_namespace("kafka");
 
