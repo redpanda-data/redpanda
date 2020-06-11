e60ebe70e2c2d8478fe37e1f0bbfdecc82dbe061
Alexander Gallego <alex@vectorized.io>
2020-06-11 14:10:38 -0700

    storage: add tests for skipping full segment reads

--- src/v/storage/tests/disk_log_builder_test.cc
+++ src/v/storage/tests/disk_log_builder_test.cc
@@ -1,3 +1,4 @@
+#include "model/record.h"
 #include "seastarx.h"
 #include "storage/tests/disk_log_builder_fixture.h"
 #include "storage/tests/utils/random_batch.h"
@@ -5,6 +6,8 @@
 
 #include <seastar/core/file.hh>
 #include <seastar/core/reactor.hh>
+
+#include <optional>
 
 FIXTURE_TEST(kitchen_sink, log_builder_fixture) {
     using namespace storage; // NOLINT
@@ -70,4 +73,46 @@ FIXTURE_TEST(test_valid_segment_name_with_zeroes_data, log_builder_fixture) {
     BOOST_TEST(stats.seg_count == 0);
     BOOST_TEST(stats.batch_count == 0);
     BOOST_TEST(stats.record_count >= 0);
+}
+
+FIXTURE_TEST(iterator_invalidation, log_builder_fixture) {
+    using namespace storage; // NOLINT
+    constexpr const model::record_batch_type configuration
+      = model::record_batch_type(2);
+    constexpr const model::record_batch_type data = model::record_batch_type(1);
+
+    b | start() | add_segment(0)
+      | add_random_batch(0, 1, maybe_compress_batches::yes, configuration)
+      | add_segment(1)
+      | add_random_batch(1, 1, maybe_compress_batches::yes, data)
+      | add_segment(2)
+      | add_random_batch(2, 1, maybe_compress_batches::yes, configuration)
+      | add_segment(3)
+      | add_random_batch(3, 1, maybe_compress_batches::yes, data);
+
+    auto data_batches = b.consume(log_reader_config(
+                                    model::offset(0),
+                                    model::model_limits<model::offset>::max(),
+                                    0,
+                                    std::numeric_limits<size_t>::max(),
+                                    ss::default_priority_class(),
+                                    data,
+                                    std::nullopt,
+                                    std::nullopt))
+                          .get0();
+    auto config_batches = b.consume(log_reader_config(
+                                      model::offset(0),
+                                      model::model_limits<model::offset>::max(),
+                                      0,
+                                      std::numeric_limits<size_t>::max(),
+                                      ss::default_priority_class(),
+                                      configuration,
+                                      std::nullopt,
+                                      std::nullopt))
+                            .get0();
+    auto all_batches = b.consume().get0();
+    b | stop();
+    BOOST_REQUIRE_EQUAL(data_batches.size(), 2);
+    BOOST_REQUIRE_EQUAL(config_batches.size(), 2);
+    BOOST_REQUIRE_EQUAL(all_batches.size(), 4);
 }
