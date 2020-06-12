5770898118514203a3fd49c9581c055666b7e7d6
Michal Maslanka <michal@vectorized.io>
2020-06-12 15:41:01 +0200

    rpc/simple_protocol: handle payload parsing exception
    RPC server have to distinguish between exceptions thrown by the service
    handler and the one caused by the corrupted payload. Data corruption on
    the wire may lead to the situation where connection is not longer
    usable as every next read is not aligned to the header/payload format.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/rpc/simple_protocol.cc
+++ src/v/rpc/simple_protocol.cc
@@ -100,6 +100,14 @@ simple_protocol::dispatch_method_once(header h, server::resources rs) {
               try {
                   reply_buf = fut.get0();
                   reply_buf.set_status(rpc::status::success);
+              } catch (const rpc_internal_body_parsing_exception& e) {
+                  // We have to distinguish between exceptions thrown by the
+                  // service handler and the one caused by the corrupted
+                  // payload. Data corruption on the wire may lead to the
+                  // situation where connection is not longer usable and so it
+                  // have to be terminated.
+                  ctx->pr.set_exception(e);
+                  return ss::now();
               } catch (const ss::timed_out_error& e) {
                   reply_buf.set_status(rpc::status::request_timeout);
               } catch (...) {
