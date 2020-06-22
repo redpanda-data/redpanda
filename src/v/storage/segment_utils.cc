431b79f5e99c12d4ac7c6ba1023b63f3c59c99be
Alexander Gallego <alex@vectorized.io>
2020-06-22 15:31:37 -0700

    storage:segment_utils: bugfix ss::open_flags manipulation
    while creating segments we do not need to check if the file
    has been created, we can simply pass ` x | ss::open_flags::create`
    to the open call.
    
    In case of `x | ss::open_flags::excluxive` checking if the handle
    is already open is a race condition and in fact we should throw.

--- src/v/storage/segment_utils.cc
+++ src/v/storage/segment_utils.cc
@@ -10,6 +10,7 @@
 #include "vassert.h"
 #include "vlog.h"
 
+#include <seastar/core/file-types.hh>
 #include <seastar/core/future.hh>
 #include <seastar/core/reactor.hh>
 #include <seastar/core/seastar.hh>
@@ -22,22 +23,20 @@
 namespace storage::internal {
 using namespace storage; // NOLINT
 
+static inline ss::file wrap_handle(ss::file f, debug_sanitize_files debug) {
+    if (debug) {
+        return ss::file(ss::make_shared(file_io_sanitizer(std::move(f))));
+    }
+    return f;
+}
 static inline ss::future<ss::file> make_handle(
   const std::filesystem::path& path,
   ss::open_flags flags,
   ss::file_open_options opt,
   debug_sanitize_files debug) {
-    return ss::file_exists(path.string())
-      .then([opt, path, debug, flags](bool exists) {
-          const auto opf = exists ? flags : flags | ss::open_flags::create;
-          return ss::open_file_dma(path.string(), opf, opt)
-            .then([debug](ss::file writer) {
-                if (debug) {
-                    return ss::file(
-                      ss::make_shared(file_io_sanitizer(std::move(writer))));
-                }
-                return writer;
-            });
+    return ss::open_file_dma(path.string(), flags, opt)
+      .then([debug](ss::file writer) {
+          return wrap_handle(std::move(writer), debug);
       });
 }
 
@@ -53,13 +52,17 @@ static inline ss::file_open_options writer_opts() {
 /// make file handle with default opts
 ss::future<ss::file> make_writer_handle(
   const std::filesystem::path& path, storage::debug_sanitize_files debug) {
-    return make_handle(path, ss::open_flags::rw, writer_opts(), debug);
+    return make_handle(
+      path, ss::open_flags::rw | ss::open_flags::create, writer_opts(), debug);
 }
 /// make file handle with default opts
 ss::future<ss::file> make_reader_handle(
   const std::filesystem::path& path, storage::debug_sanitize_files debug) {
     return make_handle(
-      path, ss::open_flags::ro, ss::file_open_options{}, debug);
+      path,
+      ss::open_flags::ro | ss::open_flags::create,
+      ss::file_open_options{},
+      debug);
 }
 
 ss::future<compacted_index_writer> make_compacted_index_writer(
