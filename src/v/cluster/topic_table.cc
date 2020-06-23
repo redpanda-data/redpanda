42b645786a12208207eb462620751ee1de396331
Michal Maslanka <michal@vectorized.io>
2020-06-23 13:59:58 +0200

    c/topic_table: added topic_table class
    In refactored controller we leverage the optimistic locking concurrency
    for topic/partitions related operations. The topic table represent all topics
    configuration and partition assignments. The topic table is copied on each
    core to minimize cross core communication  when requesting topic information.
    The topic_table provides an API for Kafka requests and delta API for controller
    backend. The delta API allows backend to wait for changes in topics state.
    Topics table is updated directly from controller_stm. The state is always
    updated before any actions related with topic creation or deletion are executed.
    Topic table is also responsible for confirming or removing pending
    partition allocations.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/cluster/topic_table.cc
@@ -0,0 +1,160 @@
+
+#include "cluster/topic_table.h"
+
+#include "cluster/types.h"
+
+namespace cluster {
+
+template<typename Func>
+std::vector<std::result_of_t<Func(topic_configuration_assignment)>>
+topic_table::transform_topics(Func&& f) const {
+    std::vector<std::result_of_t<Func(topic_configuration_assignment)>> ret;
+    ret.reserve(_topics.size());
+    std::transform(
+      std::cbegin(_topics),
+      std::cend(_topics),
+      std::back_inserter(ret),
+      [f = std::forward<Func>(f)](
+        const std::pair<model::topic_namespace, topic_configuration_assignment>&
+          p) { return f(p.second); });
+    return ret;
+}
+
+ss::future<std::error_code> topic_table::apply(create_topic_cmd cmd) {
+    if (_topics.contains(cmd.key)) {
+        // topic already exists
+        return ss::make_ready_future<std::error_code>(
+          errc::topic_already_exists);
+    }
+    // calculate delta
+    delta d;
+    d.topics.additions.push_back(cmd.value.cfg);
+    for (auto& pas : cmd.value.assignments) {
+        d.partitions.additions.emplace_back(cmd.value.cfg.tp_ns, pas);
+    }
+    _pending_deltas.push_back(std::move(d));
+
+    _topics.insert({cmd.key, std::move(cmd.value)});
+    notify_waiters();
+    return ss::make_ready_future<std::error_code>(errc::success);
+}
+
+ss::future<> topic_table::stop() {
+    for (auto& w : _waiters) {
+        w->promise.set_exception(ss::abort_requested_exception());
+    }
+    return ss::now();
+}
+
+ss::future<std::error_code> topic_table::apply(delete_topic_cmd cmd) {
+    if (auto tp = _topics.find(cmd.value); tp != _topics.end()) {
+        delta d;
+        d.topics.deletions.push_back(tp->second.cfg);
+        for (auto& p : tp->second.assignments) {
+            d.partitions.deletions.emplace_back(tp->first, p);
+        }
+        _pending_deltas.push_back(std::move(d));
+        _topics.erase(tp);
+        notify_waiters();
+        return ss::make_ready_future<std::error_code>(errc::success);
+    }
+    return ss::make_ready_future<std::error_code>(errc::topic_not_exists);
+}
+
+void topic_table::notify_waiters() {
+    if (_waiters.empty()) {
+        return;
+    }
+    std::vector<delta> changes;
+    changes.swap(_pending_deltas);
+    std::vector<std::unique_ptr<waiter>> active_waiters;
+    active_waiters.swap(_waiters);
+    for (auto& w : active_waiters) {
+        w->promise.set_value(changes);
+    }
+}
+
+ss::future<std::vector<topic_table::delta>>
+topic_table::wait_for_changes(ss::abort_source& as) {
+    using ret_t = std::vector<topic_table::delta>;
+    if (!_pending_deltas.empty()) {
+        ret_t ret;
+        ret.swap(_pending_deltas);
+        return ss::make_ready_future<ret_t>(std::move(ret));
+    }
+    auto w = std::make_unique<waiter>(_waiter_id++);
+    auto opt_sub = as.subscribe([this, &pr = w->promise, id = w->id]() {
+        pr.set_exception(ss::abort_requested_exception{});
+        auto it = std::find_if(
+          _waiters.begin(), _waiters.end(), [id](std::unique_ptr<waiter>& ptr) {
+              return ptr->id == id;
+          });
+        if (it != _waiters.end()) {
+            _waiters.erase(it);
+        }
+    });
+
+    if (unlikely(!opt_sub)) {
+        return ss::make_exception_future<ret_t>(
+          ss::abort_requested_exception{});
+    } else {
+        w->sub = std::move(*opt_sub);
+    }
+
+    auto f = w->promise.get_future();
+    _waiters.push_back(std::move(w));
+    return f;
+}
+
+bool topic_table::delta::empty() const {
+    return partitions.empty() && topics.empty();
+}
+
+std::vector<model::topic_namespace> topic_table::all_topics() const {
+    return transform_topics(
+      [](const topic_configuration_assignment& td) { return td.cfg.tp_ns; });
+}
+
+std::optional<model::topic_metadata>
+topic_table::get_topic_metadata(model::topic_namespace_view tp) const {
+    if (auto it = _topics.find(tp); it != _topics.end()) {
+        return it->second.get_metadata();
+    }
+    return {};
+}
+
+std::optional<topic_configuration>
+topic_table::get_topic_cfg(model::topic_namespace_view tp) const {
+    if (auto it = _topics.find(tp); it != _topics.end()) {
+        return it->second.cfg;
+    }
+    return {};
+}
+
+std::optional<model::timestamp_type>
+topic_table::get_topic_timestamp_type(model::topic_namespace_view tp) const {
+    if (auto it = _topics.find(tp); it != _topics.end()) {
+        return it->second.cfg.timestamp_type;
+    }
+    return {};
+}
+
+std::vector<model::topic_metadata> topic_table::all_topics_metadata() const {
+    return transform_topics([](const topic_configuration_assignment& td) {
+        return td.get_metadata();
+    });
+}
+
+bool topic_table::contains(
+  model::topic_namespace_view topic, model::partition_id pid) const {
+    if (auto it = _topics.find(topic); it != _topics.end()) {
+        const auto& partitions = it->second.assignments;
+        return std::any_of(
+          partitions.cbegin(),
+          partitions.cend(),
+          [&pid](const partition_assignment& pas) { return pas.id == pid; });
+    }
+    return false;
+}
+
+} // namespace cluster
---
+++ src/v/cluster/topic_table.h
@@ -0,0 +1,118 @@
+#pragma once
+
+#include "cluster/commands.h"
+#include "cluster/partition_allocator.h"
+#include "cluster/types.h"
+#include "utils/expiring_promise.h"
+
+#include <absl/container/flat_hash_map.h>
+
+namespace cluster {
+
+/// Topic table represent all topics configuration and partition assignments.
+/// The topic table is copied on each core to minimize cross core communication
+/// when requesting topic information. The topics table provides an API for
+/// Kafka requests and delta API for controller backend. The delta API allows
+/// backend to wait for changes in topics table. Topics table is update directly
+/// from controller_stm. The table is always updated before any actions related
+/// with topic creation or deletion are executed. Topic table is also
+/// responsible for commiting or removing pending allocations
+///
+class topic_table {
+public:
+    // delta propagated to backend
+    struct delta {
+        using partition
+          = std::pair<model::topic_namespace, partition_assignment>;
+
+        patch<partition> partitions;
+        patch<topic_configuration> topics;
+
+        bool empty() const;
+    };
+
+    bool is_batch_applicable(const model::record_batch& b) const {
+        return b.header().type == topic_batch_type;
+    }
+
+    // list of commands that this table is able to apply, the list is used to
+    // automatically deserialize batch into command
+    static constexpr auto accepted_commands
+      = make_commands_list<create_topic_cmd, delete_topic_cmd>{};
+
+    /// State machine applies
+    ss::future<std::error_code> apply(create_topic_cmd);
+    ss::future<std::error_code> apply(delete_topic_cmd);
+
+    ss::future<> stop();
+
+    /// Delta API
+
+    ss::future<std::vector<delta>> wait_for_changes(ss::abort_source&);
+
+    bool has_pending_changes() const { return !_pending_deltas.empty(); }
+
+    /// Query API
+
+    /// Returns list of all topics that exists in the cluster.
+    std::vector<model::topic_namespace> all_topics() const;
+
+    ///\brief Returns metadata of single topic.
+    ///
+    /// If topic does not exists it returns an empty optional
+    std::optional<model::topic_metadata>
+      get_topic_metadata(model::topic_namespace_view) const;
+
+    ///\brief Returns configuration of single topic.
+    ///
+    /// If topic does not exists it returns an empty optional
+    std::optional<topic_configuration>
+      get_topic_cfg(model::topic_namespace_view) const;
+
+    ///\brief Returns topics timestamp type
+    ///
+    /// If topic does not exists it returns an empty optional
+    std::optional<model::timestamp_type>
+      get_topic_timestamp_type(model::topic_namespace_view) const;
+
+    /// Returns metadata of all topics.
+    std::vector<model::topic_metadata> all_topics_metadata() const;
+
+    /// Checks if it has given partition
+    bool contains(model::topic_namespace_view, model::partition_id) const;
+
+    /// Returns partition leader
+    std::optional<model::node_id> get_leader(const model::ntp&) const;
+
+    /// Updates partition leader and notify waiters if needed
+    void update_partition_leader(
+      const model::ntp&, model::term_id, std::optional<model::node_id>);
+
+private:
+    struct waiter {
+        explicit waiter(uint64_t id)
+          : id(id) {}
+        ss::promise<std::vector<delta>> promise;
+        ss::abort_source::subscription sub;
+        uint64_t id;
+    };
+    void deallocate_topic_partitions(const std::vector<partition_assignment>&);
+
+    void notify_waiters();
+
+    template<typename Func>
+    std::vector<std::result_of_t<Func(topic_configuration_assignment)>>
+    transform_topics(Func&&) const;
+
+    absl::flat_hash_map<
+      model::topic_namespace,
+      topic_configuration_assignment,
+      model::topic_namespace_hash,
+      model::topic_namespace_eq>
+      _topics;
+
+    std::vector<delta> _pending_deltas;
+    std::vector<std::unique_ptr<waiter>> _waiters;
+    uint64_t _waiter_id{0};
+};
+} // namespace cluster
