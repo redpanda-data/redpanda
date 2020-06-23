8312d04eb73cd376efaafbbb764dd2e3669ea457
Michal Maslanka <michal@vectorized.io>
2020-06-23 16:11:31 +0200

    c/controller_backend: implemented controller backend
    The controller backend is a class responsible for applying changes to
    redpanda cluster. Currently controller_backend is responsible only for
    applying topic related changes. The backend waits for the deltas coming
    from state. The deltas are then reconciled in reconciliation loop. When
    applying changes comming from delta failed the houskeeping timer will
    trigger retires and changes will eventually be applied.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/cluster/controller_backend.cc
@@ -0,0 +1,220 @@
+
+#include "cluster/controller_backend.h"
+
+#include "cluster/cluster_utils.h"
+#include "cluster/logger.h"
+#include "cluster/members_table.h"
+#include "cluster/shard_table.h"
+#include "cluster/topic_table.h"
+#include "cluster/types.h"
+#include "config/configuration.h"
+#include "model/fundamental.h"
+#include "model/metadata.h"
+#include "outcome.h"
+#include "raft/types.h"
+
+#include <seastar/core/abort_source.hh>
+#include <seastar/core/future-util.hh>
+#include <seastar/core/future.hh>
+#include <seastar/core/gate.hh>
+#include <seastar/core/semaphore.hh>
+#include <seastar/core/sharded.hh>
+#include <seastar/core/smp.hh>
+
+/// Class that contains the controller state, for now we will have single
+/// controller backend
+
+/// on every core, sharded
+namespace cluster {
+
+controller_backend::controller_backend(
+  ss::sharded<topic_table>& tp_state,
+  ss::sharded<shard_table>& st,
+  ss::sharded<partition_manager>& pm,
+  ss::sharded<members_table>& members,
+  ss::sharded<ss::abort_source>& as)
+  : _topics(tp_state)
+  , _shard_table(st)
+  , _partition_manager(pm)
+  , _members_table(members)
+  , _self(model::node_id(config::shard_local_cfg().node_id))
+  , _data_directory(config::shard_local_cfg().data_directory().as_sstring())
+  , _as(as) {}
+
+ss::future<> controller_backend::stop() {
+    _housekeeping_timer.cancel();
+    return _gate.close();
+}
+
+ss::future<> controller_backend::start() {
+    start_topics_reconciliation_loop();
+    _housekeeping_timer.set_callback([this] { housekeeping(); });
+    _housekeeping_timer.arm_periodic(std::chrono::seconds(1));
+    return ss::now();
+}
+
+void controller_backend::start_topics_reconciliation_loop() {
+    (void)ss::with_gate(_gate, [this] {
+        return ss::do_until(
+          [this] { return _as.local().abort_requested(); },
+          [this] {
+              return _topics.local()
+                .wait_for_changes(_as.local())
+                .then([this](std::vector<topic_table::delta> deltas) {
+                    for (auto& d : deltas) {
+                        _topic_deltas.emplace_back(std::move(d));
+                    }
+                    return reconcile_topics();
+                })
+                .handle_exception_type([](
+                                         const ss::abort_requested_exception&) {
+                    vlog(clusterlog.info, "Waiting for topic changes aborted");
+                });
+          });
+    });
+}
+
+void controller_backend::housekeeping() {
+    if (!_topic_deltas.empty() && _topics_sem.available_units() > 0) {
+        (void)ss::with_gate(_gate, [this] { return reconcile_topics(); });
+    }
+}
+
+ss::future<> controller_backend::reconcile_topics() {
+    using meta_t = task_meta<topic_table::delta>;
+    return ss::with_semaphore(_topics_sem, 1, [this] {
+        return ss::parallel_for_each(
+                 std::begin(_topic_deltas),
+                 std::end(_topic_deltas),
+                 [this](meta_t& task) {
+                     return do_reconcile_topic(task).then(
+                       [&task]() { task.finished = true; });
+                 })
+          .then([this] {
+              // remove finished tasks
+              auto it = std::stable_partition(
+                std::begin(_topic_deltas),
+                std::end(_topic_deltas),
+                [](meta_t& task) { return task.finished; });
+              _topic_deltas.erase(it, _topic_deltas.end());
+          });
+    });
+}
+
+bool has_local_replicas(
+  model::node_id self, const std::vector<model::broker_shard>& replicas) {
+    return std::find_if(
+             std::cbegin(replicas),
+             std::cend(replicas),
+             [self](const model::broker_shard& bs) {
+                 return bs.node_id == self && bs.shard == ss::this_shard_id();
+             })
+           != replicas.cend();
+}
+
+std::vector<model::broker> create_brokers_set(
+  const std::vector<model::broker_shard>& replicas,
+  cluster::members_table& members) {
+    std::vector<model::broker> brokers;
+    brokers.reserve(replicas.size());
+    std::transform(
+      std::cbegin(replicas),
+      std::cend(replicas),
+      std::back_inserter(brokers),
+      [&members](const model::broker_shard& bs) {
+          auto br = members.get_broker(bs.node_id);
+          if (!br) {
+              throw std::logic_error(
+                fmt::format("Replica node {} is not available", bs.node_id));
+          }
+          return *br->get();
+      });
+    return brokers;
+}
+
+ss::future<>
+controller_backend::do_reconcile_topic(task_meta<topic_table::delta>& task) {
+    // new partitions
+    auto f = ss::parallel_for_each(
+      task.delta.partitions.additions.begin(),
+      task.delta.partitions.additions.end(),
+      [this](topic_table::delta::partition& p) {
+          // only create partitions for this backend
+          // partitions created on current shard at this node
+          if (!has_local_replicas(_self, p.second.replicas)) {
+              return ss::now();
+          }
+          return create_partition(
+            model::ntp(p.first.ns, p.first.tp, p.second.id),
+            p.second.group,
+            create_brokers_set(p.second.replicas, _members_table.local()));
+      });
+
+    // delete partitions
+    f = f.then([this, &task] {
+        return ss::parallel_for_each(
+          std::cbegin(task.delta.partitions.deletions),
+          std::cend(task.delta.partitions.deletions),
+          [this](const topic_table::delta::partition& p) {
+              return delete_partition(
+                model::ntp(p.first.ns, p.first.tp, p.second.id));
+          });
+    });
+
+    return f.then([&task] { task.finished = true; });
+}
+
+ss::future<> controller_backend::add_to_shard_table(
+  model::ntp ntp, raft::group_id raft_group, uint32_t shard) {
+    // update shard_table: broadcast
+    return _shard_table.invoke_on_all(
+      [ntp = std::move(ntp), raft_group, shard](shard_table& s) mutable {
+          s.insert(std::move(ntp), shard);
+          s.insert(raft_group, shard);
+      });
+}
+
+ss::future<> controller_backend::create_partition(
+  model::ntp ntp, raft::group_id group_id, std::vector<model::broker> members) {
+    auto cfg = _topics.local().get_topic_cfg(model::topic_namespace_view(ntp));
+
+    if (!cfg) {
+        // partition was already removed, do nothing
+        return ss::now();
+    }
+
+    auto f = ss::now();
+    // handle partially created topic
+    if (likely(_partition_manager.local().get(ntp).get() == nullptr)) {
+        f = _partition_manager.local()
+              .manage(
+                cfg->make_ntp_config(_data_directory, ntp.tp.partition),
+                group_id,
+                std::move(members))
+              .discard_result();
+    }
+
+    return f.then([this, ntp = std::move(ntp), group_id]() mutable {
+        // we create only partitions that belongs to current shard
+        return add_to_shard_table(
+          std::move(ntp), group_id, ss::this_shard_id());
+    });
+}
+
+ss::future<> controller_backend::delete_partition(model::ntp ntp) {
+    auto part = _partition_manager.local().get(ntp);
+    if (unlikely(part.get() == nullptr)) {
+        return ss::now();
+    }
+    auto group_id = part->group();
+
+    return _shard_table
+      .invoke_on_all(
+        [ntp, group_id](shard_table& st) mutable { st.erase(ntp, group_id); })
+      .then([this, ntp = std::move(ntp)] {
+          // remove partition
+          return _partition_manager.local().remove(ntp);
+      });
+}
+
+} // namespace cluster
---
+++ src/v/cluster/controller_backend.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "cluster/members_table.h"
+#include "cluster/partition_manager.h"
+#include "cluster/shard_table.h"
+#include "cluster/topic_table.h"
+#include "model/fundamental.h"
+#include "outcome.h"
+
+#include <seastar/core/gate.hh>
+#include <seastar/core/sharded.hh>
+
+namespace cluster {
+
+/// on every core, sharded
+
+class controller_backend {
+public:
+    controller_backend(
+      ss::sharded<cluster::topic_table>&,
+      ss::sharded<shard_table>&,
+      ss::sharded<partition_manager>&,
+      ss::sharded<members_table>&,
+      ss::sharded<seastar::abort_source>&);
+
+    ss::future<> stop();
+    ss::future<> start();
+
+private:
+    template<typename T>
+    struct task_meta {
+        explicit task_meta(T t)
+          : delta(std::move(t)) {}
+
+        bool finished = false;
+        T delta;
+    };
+
+    // Topics
+    void start_topics_reconciliation_loop();
+    ss::future<> reconcile_topics();
+    ss::future<> do_reconcile_topic(task_meta<topic_table::delta>&);
+    ss::future<>
+      create_partition(model::ntp, raft::group_id, std::vector<model::broker>);
+    ss::future<> add_to_shard_table(model::ntp, raft::group_id, ss::shard_id);
+    ss::future<> delete_partition(model::ntp);
+    void housekeeping();
+
+    ss::sharded<topic_table>& _topics;
+    ss::sharded<shard_table>& _shard_table;
+    ss::sharded<partition_manager>& _partition_manager;
+    ss::sharded<members_table>& _members_table;
+    model::node_id _self;
+    ss::sstring _data_directory;
+    ss::sharded<ss::abort_source>& _as;
+    std::vector<task_meta<topic_table::delta>> _topic_deltas;
+    ss::timer<> _housekeeping_timer;
+    ss::semaphore _topics_sem{1};
+    ss::gate _gate;
+};
+} // namespace cluster
