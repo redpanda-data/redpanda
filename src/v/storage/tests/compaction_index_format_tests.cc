beb96c6918ce4634ced080eed24b947a3edcc89e
Alexander Gallego <alex@vectorized.io>
2020-06-23 22:45:30 -0700

    storage:compaction: add test for index_filtered_copy
    The final step for writing a compacted index after producing the
    bitmaps is to write the filtered bitmaps natural indexes into the
    final file. Add a test to ensure behavior of copy+writer.

--- src/v/storage/tests/compaction_index_format_tests.cc
+++ src/v/storage/tests/compaction_index_format_tests.cc
@@ -6,6 +6,7 @@
 #include "storage/compacted_index_reader.h"
 #include "storage/compacted_index_writer.h"
 #include "storage/compaction_reducers.h"
+#include "storage/segment_utils.h"
 #include "test_utils/fixture.h"
 #include "units.h"
 
@@ -361,4 +362,73 @@ FIXTURE_TEST(key_reducer_max_mem, compacted_topic_fixture) {
     BOOST_REQUIRE_EQUAL(small_mem_bitmap.cardinality(), 100);
     BOOST_REQUIRE(exact_mem_bitmap.contains(98));
     BOOST_REQUIRE(exact_mem_bitmap.contains(99));
+}
+FIXTURE_TEST(index_filtered_copy_tests, compacted_topic_fixture) {
+    iobuf index_data;
+    auto idx = storage::make_file_backed_compacted_index(
+      "dummy name",
+      ss::file(ss::make_shared(iobuf_file(index_data))),
+      ss::default_priority_class(),
+      // FORCE eviction with every key basically
+      1_KiB);
+
+    const auto key1 = random_generators::get_bytes(1_KiB);
+    const auto key2 = random_generators::get_bytes(1_KiB);
+    for (auto i = 0; i < 100; ++i) {
+        bytes_view put_key;
+        if (i % 2) {
+            put_key = key1;
+        } else {
+            put_key = key2;
+        }
+        idx.index(put_key, model::offset(i), 0).get();
+    }
+    idx.close().get();
+    info("{}", idx);
+
+    auto rdr = storage::make_file_backed_compacted_reader(
+      "dummy name",
+      ss::file(ss::make_shared(iobuf_file(index_data))),
+      ss::default_priority_class(),
+      32_KiB);
+
+    auto bitmap = storage::internal::index_of_index_of_entries(rdr).get0();
+    {
+        auto vec = compaction_index_reader_to_memory(rdr).get0();
+        BOOST_REQUIRE_EQUAL(vec.size(), 100);
+    }
+    info("key bitmap: {}", bitmap.toString());
+    BOOST_REQUIRE_EQUAL(bitmap.cardinality(), 2);
+    BOOST_REQUIRE(bitmap.contains(98));
+    BOOST_REQUIRE(bitmap.contains(99));
+
+    // the main test
+    iobuf final_data;
+    auto final_idx = storage::make_file_backed_compacted_index(
+      "dummy name - final idx",
+      ss::file(ss::make_shared(iobuf_file(final_data))),
+      ss::default_priority_class(),
+      // FORCE eviction with every key basically
+      1_KiB);
+
+    rdr.reset();
+    rdr.load_footer().get();
+    rdr
+      .consume(
+        storage::internal::index_filtered_copy_reducer(
+          std::move(bitmap), final_idx),
+        model::no_timeout)
+      .get();
+    final_idx.close().get();
+    {
+        auto final_rdr = storage::make_file_backed_compacted_reader(
+          "dummy name - final ",
+          ss::file(ss::make_shared(iobuf_file(final_data))),
+          ss::default_priority_class(),
+          32_KiB);
+        auto vec = compaction_index_reader_to_memory(final_rdr).get0();
+        BOOST_REQUIRE_EQUAL(vec.size(), 2);
+        BOOST_REQUIRE_EQUAL(vec[0].offset, model::offset(98));
+        BOOST_REQUIRE_EQUAL(vec[1].offset, model::offset(99));
+    }
 }
