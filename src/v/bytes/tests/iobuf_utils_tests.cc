fec0dc73beec7aec563b5e056db402010424ecdf
Alexander Gallego <alex@vectorized.io>
2020-07-01 17:51:27 -0700

    iobuf: add operator== and operator hash unit tests for bytes

--- src/v/bytes/tests/iobuf_utils_tests.cc
+++ src/v/bytes/tests/iobuf_utils_tests.cc
@@ -27,3 +27,21 @@ SEASTAR_THREAD_TEST_CASE(test_reading_some_bytes) {
     auto read_buf = read_iobuf_exactly(is, 16).get0();
     BOOST_REQUIRE_EQUAL(read_buf.size_bytes(), 16);
 };
+
+SEASTAR_THREAD_TEST_CASE(test_bytes_conversion) {
+    static constexpr std::string_view key = "magic_key";
+    iobuf buf;
+    buf.append(key.data(), key.size());
+    // convert to bytes
+    bytes bytes_buf = iobuf_to_bytes(buf);
+
+    // convert bytes back
+    iobuf converted_back = bytes_to_iobuf(bytes_buf);
+    bytes roundtrip_buf = iobuf_to_bytes(converted_back);
+    BOOST_REQUIRE_EQUAL(buf, converted_back);
+    BOOST_REQUIRE_EQUAL(bytes_type_eq{}(bytes_buf, buf), true);
+    BOOST_REQUIRE_EQUAL(
+      absl::Hash<bytes>{}(roundtrip_buf), absl::Hash<bytes>{}(bytes_buf));
+    BOOST_REQUIRE_EQUAL(
+      absl::Hash<iobuf>{}(buf), absl::Hash<iobuf>{}(converted_back));
+}
