7622552ffb5c02f1fb28754b1d73d0174a54a31a
Alexander Gallego <alex@vectorized.io>
2020-07-07 08:36:20 -0700

    compression: tests: use hand crafted hard inputs to handle

--- src/v/compression/tests/zstd_tests.cc
+++ src/v/compression/tests/zstd_tests.cc
@@ -8,6 +8,21 @@
 
 #include <seastar/testing/thread_test_case.hh>
 
+static inline constexpr std::array<size_t, 12> sizes{{
+  0,
+  8,
+  16,
+  32,
+  64,
+  512,
+  1_KiB,
+  2_KiB,
+  4_KiB,
+  6_KiB,
+  8_KiB,
+  10_KiB,
+}};
+
 static inline iobuf gen(const size_t data_size) {
     const auto data = random_generators::gen_alphanum_string(512);
     iobuf ret;
@@ -18,9 +33,20 @@ static inline iobuf gen(const size_t data_size) {
     return ret;
 }
 
+template<typename CompressFunc, typename DecompressFunc>
+inline void
+roundtrip_compression(CompressFunc&& comp_fn, DecompressFunc&& decomp_fn) {
+    for (size_t i : sizes) {
+        iobuf buf = gen(i);
+        auto cbuf = comp_fn(buf.share(0, i));
+        auto dbuf = decomp_fn(cbuf);
+        BOOST_CHECK_EQUAL(dbuf, buf);
+    }
+}
+
 SEASTAR_THREAD_TEST_CASE(stream_zstd_test) {
     compression::stream_zstd fn;
-    for (size_t i = 1024; i < 1024 + 100; ++i) {
+    for (size_t i : sizes) {
         iobuf buf = gen(i);
         auto cbuf = fn.compress(buf.share(0, i));
         auto dbuf = fn.uncompress(std::move(cbuf));
