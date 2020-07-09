9a267e29ed3e3676552b506c955ba4c928aa93a1
Andres Aristizabal <andres@vectorized.io>
2020-07-09 16:30:14 -0500

    Add CoprocessorsReposity and it's interface
    The CoprocessorsReposity is responsable for manage the coprocessor
    functions that we'll apply to record batch. CoprocessorsRepository
    has method for interact with coprocessor functions like add, remove
    and findByGlobalId.
    
    ---
    since v8:
    	- change name scripts to coprocessor on CoprocessorRepository
    since v6:
    	- Change interface name CoprocessorLoaded -> CoprocessorHandle
    since v5:
    	- Change CoprocessorManager -> CoprocessorRepository
    	- delete methods in CoprocessorRepository related with folders/Dir
    	- delete CoprocessorManager interface
    	- delete Coprocessor suffixes
    since v2:
    	- Change name from MaterializedScript to CoprocessorManager
    
    since v1:
    	- Change supervisor implementation, spliting watcher and
    	coprocessor function state in two files. defailt new flow:
            https://drive.google.com/file/d/17rmXDFtTVhzHa5oFZjXFX_HdEJq2WaiO/view?usp=sharing

---
+++ src/js/modules/domain/CoprocessorManager.ts
@@ -0,0 +1,13 @@
+import {Coprocessor} from "../public/Coprocessor";
+
+/**
+ * CoprocessorHandle interface represents a coprocessor loaded into memory
+ * @coprocessor: is a Coprocessor implementation instance
+ * @filename: the path to the Javascript file where the coprocessor is defined
+ * @checksum: is the the file's content checksum
+ */
+export interface CoprocessorHandle {
+  coprocessor: Coprocessor,
+  filename: string,
+  checksum: string
+}
