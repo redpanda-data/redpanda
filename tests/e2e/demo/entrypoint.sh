210275c7b84558874d8657fa925475690ff69e77
Alexander Gallego <alex@vectorized.io>
2020-07-23 13:48:58 -0700

    tests:e2e:demo: use 2 producers+sync+1K records per put
    This allows us to make 2 round trips to the servers

--- tests/e2e/demo/entrypoint.sh
+++ tests/e2e/demo/entrypoint.sh
@@ -6,7 +6,7 @@ export KAFKA_PATH=/opt/kafka
 export SERVERS=${BAMBOO_BROKERS}
 export TOPIC="${BAMBOO_NAMESPACE}-demo-topic"
 export REPLICATION_FACTOR=${BAMBOO_BROKER_COUNT}
-export RECORD_COUNT=100
+export RECORD_COUNT=1000
 export BACKGROUND_PRODUCE=0
-export PRODUCER_COUNT=1
+export PRODUCER_COUNT=2
 /opt/v/tools/demo_script.sh
