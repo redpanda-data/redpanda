fe7290c1a1b452da67529db663af5c6cf3e6135e
Denis Rystsov <denis@vectorized.io>
2020-08-13 13:59:41 -0700

    Adds logging of commands and latency to Gobekli
    Logging of get/cas commands helps to see the commands which preceded
    the consistency violation and understand why it happened.
    
    Latency logging gives us an opportunity to use consistensy testing
    also as an additional way to get performance measurements.

--- src/consistency-testing/gobekli/gobekli/workloads/symmetrical_mrsw.py
+++ src/consistency-testing/gobekli/gobekli/workloads/symmetrical_mrsw.py
@@ -5,10 +5,14 @@ from gobekli.kvapi import RequestCanceled, RequestTimedout
 from gobekli.consensus import Violation
 from gobekli.workloads.common import (Stat, StatDumper, ReaderClient,
                                       LinearizabilityHashmapChecker)
+from gobekli.logging import (log_write_started, log_write_ended,
+                             log_write_timeouted, log_write_failed,
+                             log_violation, log_latency)
 
 
 class WriterClient:
-    def __init__(self, stat, checker, name, node, key):
+    def __init__(self, started_at, stat, checker, name, node, key):
+        self.started_at = started_at
         self.stat = stat
         self.node = node
         self.name = name
@@ -23,17 +27,28 @@ class WriterClient:
         self.is_active = False
 
     async def start(self):
+        loop = asyncio.get_running_loop()
         while self.is_active and self.checker.is_valid:
             prev = self.last_write_id
             curr_write_id = str(uuid.uuid1())
             curr_version = self.last_version + 1
+            op_started = None
             try:
+                log_write_started(self.node.name, self.pid, curr_write_id,
+                                  self.key, prev, curr_version,
+                                  f"42:{curr_version}")
                 self.checker.read_started(self.pid, self.key)
                 self.checker.cas_started(curr_write_id, self.key, prev,
                                          curr_version, f"42:{curr_version}")
+                op_started = loop.time()
                 data = await self.node.cas_aio(self.key, prev,
                                                f"42:{curr_version}",
                                                curr_write_id)
+                op_ended = loop.time()
+                log_latency("o", op_started - self.started_at,
+                            op_ended - op_started)
+                log_write_ended(self.node.name, self.pid, self.key,
+                                data.write_id, data.value)
                 if data.write_id == curr_write_id:
                     self.checker.cas_ended(curr_write_id, self.key)
                 else:
@@ -48,10 +63,18 @@ class WriterClient:
                 self.stat.inc(self.name + ":ok")
             except RequestTimedout:
                 self.stat.inc(self.name + ":out")
+                op_ended = loop.time()
+                log_latency("t", op_started - self.started_at,
+                            op_ended - op_started)
+                log_write_timeouted(self.node.name, self.pid, self.key)
                 self.checker.read_canceled(self.pid, self.key)
                 self.checker.cas_timeouted(curr_write_id, self.key)
             except RequestCanceled:
                 self.stat.inc(self.name + ":err")
+                op_ended = loop.time()
+                log_latency("e", op_started - self.started_at,
+                            op_ended - op_started)
+                log_write_failed(self.node.name, self.pid, self.key)
                 self.checker.read_canceled(self.pid, self.key)
                 try:
                     self.checker.cas_canceled(curr_write_id, self.key)
@@ -59,7 +82,7 @@ class WriterClient:
                     print(f"violation: {e.message}")
                     break
             except Violation:
-                print(f"violation: {e.message}")
+                log_violation(self.pid, e.message)
                 break
 
 
@@ -90,17 +113,21 @@ async def start_mrsw_workload_aio(kv_nodes, numOfKeys, numOfReaders, timeout):
     dumper = StatDumper(stat, dims)
     clients = []
 
+    loop = asyncio.get_running_loop()
+    started_at = loop.time()
+
     for key in keys:
         for kv in kv_nodes:
-            clients.append(WriterClient(stat, checker, kv.name, kv, key))
+            clients.append(
+                WriterClient(started_at, stat, checker, kv.name, kv, key))
             for _ in range(0, numOfReaders):
-                clients.append(ReaderClient(stat, checker, kv.name, kv, key))
+                clients.append(
+                    ReaderClient(started_at, stat, checker, kv.name, kv, key))
     tasks = []
     for client in clients:
         tasks.append(asyncio.create_task(client.start()))
     tasks.append(asyncio.create_task(dumper.start()))
 
-    loop = asyncio.get_running_loop()
     end_time = loop.time() + timeout
     while checker.is_valid:
         if (loop.time() + 2) >= end_time:
