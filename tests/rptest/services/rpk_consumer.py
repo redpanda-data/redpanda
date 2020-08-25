020de846b9d484db7f216417a79c7c3dd9bfe472
David Castillo <david@vectorized.io>
2020-08-24 23:34:27 -0500

    tests/ducky: Add rpk consumer service

---
+++ tests/rptest/services/rpk_consumer.py
@@ -0,0 +1,61 @@
+import json
+from ducktape.services.background_thread import BackgroundThreadService
+
+
+class RpkConsumer(BackgroundThreadService):
+    def __init__(self,
+                 context,
+                 redpanda,
+                 topic,
+                 partitions=[],
+                 offset='oldest',
+                 group='',
+                 commit=False):
+        super(RpkConsumer, self).__init__(context, num_nodes=1)
+        self._redpanda = redpanda
+        self._topic = topic
+        self._partitions = partitions
+        self._offset = offset
+        self._group = group
+        self._commit = commit
+        self.done = False
+        self.messages = []
+        self.error = None
+        self.offset = dict()
+
+    def _worker(self, idx, node):
+        try:
+            self.messages = self._consume(node)
+        except Exception as e:
+            self.error = e
+            raise
+        finally:
+            self.done = True
+
+    def stop_node(self, node):
+        node.account.kill_process('rpk', clean_shutdown=False)
+
+    def _consume(self, node):
+        cmd = '%s api consume --offset %s --pretty-print=false --brokers %s %s' % (
+            self._redpanda.find_binary('rpk'),
+            self._offset,
+            self._redpanda.brokers(),
+            self._topic,
+        )
+
+        if self._group:
+            cmd += ' -g %s' % self._group
+
+        if self._partitions:
+            cmd += ' -p %s' % ','.join([str(p) for p in self._partitions])
+
+        if self._commit:
+            cmd += ' --commit'
+
+        for line in node.account.ssh_capture(cmd):
+            l = line.rstrip()
+            self.logger.debug(l)
+            if not l:
+                continue
+            msg = json.loads(l)
+            self.messages.append(msg)
