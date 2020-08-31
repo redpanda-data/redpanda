412a6edc1503b878bcb7ec0d5bbb0018a886c573
Alexander Gallego <alex@vectorized.io>
2020-08-30 22:08:07 -0700

    storage:parser_utils: add batch compression utils + docs
    Add batch compression utils to pair up with the batch decompression
    utils.

--- src/v/storage/parser_utils.h
+++ src/v/storage/parser_utils.h
@@ -14,9 +14,22 @@ model::record parse_one_record_from_buffer(iobuf_parser& parser);
 model::record
 parse_one_record_from_buffer_using_kafka_format(iobuf_parser& parser);
 
+/// \brief appends the record @r to buffer @a
 void append_record_using_kafka_format(iobuf& a, const model::record& r);
 
+/// \brief batch decompression
 ss::future<model::record_batch> decompress_batch(model::record_batch&&);
+/// \brief batch decompression
 ss::future<model::record_batch> decompress_batch(const model::record_batch&);
+
+/// \brief batch compression
+ss::future<model::record_batch>
+compress_batch(model::compression, model::record_batch&&);
+/// \brief batch compression
+ss::future<model::record_batch>
+compress_batch(model::compression, const model::record_batch&);
+
+/// \brief resets the size, header crc and payload crc
+void reset_size_checksum_metadata(model::record_batch&);
 
 } // namespace storage::internal
