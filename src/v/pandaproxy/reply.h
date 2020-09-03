9b9ba6589429897417ff418274b4253a0af7a083
Ben Pope <ben@vectorized.io>
2020-09-03 18:57:22 +0100

    pandaproxy: Handle parse errors

--- src/v/pandaproxy/reply.h
+++ src/v/pandaproxy/reply.h
@@ -22,11 +22,26 @@ inline std::unique_ptr<ss::httpd::reply> reply_unavailable() {
     return rep;
 }
 
+inline std::unique_ptr<ss::httpd::reply> unprocessable_entity(ss::sstring msg) {
+    pandaproxy::json::error_body body{
+      .error_code = ss::httpd::reply::status_type(422),
+      .message = std::move(msg)};
+
+    auto rep = std::make_unique<ss::httpd::reply>();
+    rep->set_status(body.error_code);
+    auto b = json::rjson_serialize(body);
+    rep->write_body("json", b);
+
+    return rep;
+}
+
 inline std::unique_ptr<ss::httpd::reply> exception_reply(std::exception_ptr e) {
     try {
         std::rethrow_exception(e);
     } catch (const ss::gate_closed_exception& e) {
         return reply_unavailable();
+    } catch (const pandaproxy::json::parse_error& e) {
+        return unprocessable_entity(e.what());
     } catch (...) {
         vlog(plog.error, "{}", std::current_exception());
         throw;
