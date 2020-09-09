3b1e3a44f60156fb6738eda59c3ff3ad72cceb31
Rob Blafford <rob@vectorized.io>
2020-09-09 16:55:45 -0400

    coproc/tests: copro/service unit tests + coproc harness
    - Unit tests for coproc/service tests the registration/deregistration API
    
    - Includes a fixture for the soon to be mocked supervisor service which is a
    stripped down c++ version of the js engine.
    
    - To use simply initialize the harness with the expected 'universe' of storage::logs
    
    - Use the push and poll methods to verify the results

---
+++ src/v/coproc/tests/coproc_fixture_base.cc
@@ -0,0 +1,203 @@
+#include "coproc_fixture_base.h"
+
+#include "coproc/errc.h"
+#include "coproc/logger.h"
+#include "coproc/tests/utils.h"
+#include "model/record_batch_reader.h"
+#include "model/timeout_clock.h"
+#include "model/timestamp.h"
+#include "vassert.h"
+#include "vlog.h"
+
+#include <seastar/core/circular_buffer.hh>
+
+#include <chrono>
+
+using namespace std::literals;
+
+void coproc_fixture_base::startup(log_layout_map&& data) {
+    _data = to_ntps(std::move(data));
+    _api.start(default_kvstorecfg(), default_logcfg()).get0();
+    _api.invoke_on_all(&storage::api::start).get();
+    _router
+      .start(
+        ss::socket_address(ss::net::inet_address("127.0.0.1"), 43189),
+        std::ref(_api))
+      .get();
+    expand().get();
+    if (_start_router) {
+        // All tests must involve a router or to even compile. A coproc::service
+        // takes a router as its main argument. However in some cases its not
+        // desired that the router start, as it will create side effects or
+        // even throw due to it attempting to connect to a processor engine on
+        // call to start
+        _router.invoke_on_all(&coproc::router::start).get();
+    }
+}
+
+coproc_fixture_base::~coproc_fixture_base() {
+    _router.stop().get();
+    _api.stop().get();
+    _data.clear();
+}
+
+// Keep calling 'fn' until function returns true
+// OR if 'timeout' was specified, exit when timeout is reached
+template<typename Func>
+ss::future<> poll_until(
+  Func&& fn, model::timeout_clock::time_point timeout = model::no_timeout) {
+    return ss::do_with(
+      model::timeout_clock::now(),
+      std::forward<Func>(fn),
+      false,
+      [timeout](auto& now, auto& fn, auto& exit) {
+          return ss::do_until(
+                   [&exit, &now, timeout] { return exit || (now > timeout); },
+                   [&exit, &now, &fn] {
+                       return fn().then([&exit, &now](bool r) {
+                           exit = r;
+                           now = model::timeout_clock::now();
+                       });
+                   })
+            .then([]() { return ss::sleep(200ms); });
+      });
+}
+
+ss::future<coproc_fixture_base::opt_reader_data_t> coproc_fixture_base::drain(
+  const model::ntp& ntp,
+  std::size_t limit,
+  model::timeout_clock::time_point timeout) {
+    if (!coproc::is_materialized_topic(ntp.tp.topic)) {
+        return ss::make_ready_future<opt_reader_data_t>(std::nullopt);
+    }
+    const auto c = hash_scheme(ntp.tp.topic);
+    vlog(
+      coproc::coproclog.info,
+      "searching for ntp {} on shard id {} ...with value for limit: {}",
+      ntp,
+      c,
+      limit);
+    return _api.invoke_on(c, [this, ntp, limit, timeout](storage::api& api) {
+        return poll_until(
+                 [&api, ntp]() {
+                     return ss::make_ready_future<bool>(
+                       api.log_mgr().get(ntp).has_value());
+                 },
+                 model::timeout_clock::now() + 2s)
+          .then([this, &api, ntp, limit, timeout] {
+              auto log = api.log_mgr().get(ntp);
+              if (!log.has_value()) {
+                  return ss::make_ready_future<opt_reader_data_t>(std::nullopt);
+              }
+              return do_drain(std::move(*log), limit, timeout)
+                .then([ntp](auto rval) mutable {
+                    return opt_reader_data_t(std::move(rval));
+                });
+          });
+    });
+}
+
+ss::future<model::record_batch_reader::data_t> coproc_fixture_base::do_drain(
+  storage::log&& log,
+  std::size_t limit,
+  model::timeout_clock::time_point timeout) {
+    return ss::do_with(
+      timeout,
+      std::move(log),
+      static_cast<std::size_t>(0),
+      model::offset(0),
+      model::record_batch_reader::data_t(),
+      [limit](auto& timeout, auto& log, auto& acc, auto& offset, auto& b) {
+          // Loop until the expected number of records is read
+          // OR a non-configurable timeout has been reached
+          return poll_until(
+                   [&b, &log, &acc, &offset, &timeout, limit] {
+                       return log.make_reader(log_rdr_cfg(model::offset(acc)))
+                         .then([&timeout](model::record_batch_reader reader) {
+                             return model::consume_reader_to_memory(
+                               std::move(reader), timeout);
+                         })
+                         .then([&acc, &b, &offset](
+                                 model::record_batch_reader::data_t data) {
+                             offset += data.size();
+                             for (auto&& r : data) {
+                                 acc += r.record_count();
+                                 b.push_back(std::move(r));
+                             }
+                         })
+                         .then([&acc, limit] { return acc >= limit; });
+                   },
+                   timeout)
+            .then([&b, &acc] { return std::move(b); });
+      });
+}
+
+ss::future<> coproc_fixture_base::push(
+  const model::ntp& ntp, ss::circular_buffer<model::record_batch>&& data) {
+    vlog(
+      coproc::coproclog.info,
+      "Pushing {} record batches to ntp: {}",
+      data.size(),
+      ntp);
+    return _api.invoke_on(
+      hash_scheme(ntp.tp.topic),
+      [data = std::move(data), ntp](storage::api& api) mutable {
+          auto log = api.log_mgr().get(ntp);
+          // TODO(rob): Future enhancement, push can make a log if one doesn't
+          // exist, for now no test needs or desires this
+          vassert(log.has_value(), "Log doesn't exist when it is expected to");
+          auto rbr = model::make_memory_record_batch_reader(std::move(data));
+          return std::move(rbr)
+            .for_each_ref(log->make_appender(log_app_cfg()), model::no_timeout)
+            .then([log](auto) mutable { return log->flush(); });
+      });
+}
+
+ss::future<> coproc_fixture_base::expand_copros(active_copros&& copros) {
+    return ss::do_with(std::move(copros), [this](auto& copros) {
+        return _router.invoke_on_all([&copros](coproc::router& r) {
+            return ss::do_for_each(
+              copros.begin(), copros.end(), [&r](const auto& data) {
+                  for (const auto& topic : data.topics) {
+                      if (hash_scheme(topic.first) != ss::this_shard_id()) {
+                          continue;
+                      }
+                      vlog(
+                        coproc::coproclog.info,
+                        "Adding source with id: {} and topic: {}",
+                        data.id,
+                        topic.first);
+                      const auto v = r.add_source(
+                        data.id, make_ts(topic.first), topic.second);
+                      if (v != coproc::errc::success) {
+                          vassert(false, "r.add_source() failed, {}", v);
+                      }
+                  }
+              });
+        });
+    });
+}
+
+ss::future<> coproc_fixture_base::expand() {
+    return _api.invoke_on_all([this](storage::api& api) {
+        return ss::do_for_each(
+          _data.cbegin(), _data.cend(), [this, &api](const auto& ntp) {
+              return maybe_create_log(api, ntp);
+          });
+    });
+}
+
+ss::future<> coproc_fixture_base::maybe_create_log(
+  storage::api& api, const model::ntp& ntp) {
+    if (hash_scheme(ntp.tp.topic) != ss::this_shard_id()) {
+        return ss::make_ready_future<>();
+    }
+    vlog(
+      coproc::coproclog.info,
+      "Inserting ntp {} onto shard id {}",
+      ntp,
+      ss::this_shard_id());
+    return api.log_mgr()
+      .manage(storage::ntp_config(ntp, _cfg_dir))
+      .then([](const storage::log&) {});
+}
---
+++ src/v/coproc/tests/management_service_test.cc
@@ -0,0 +1,208 @@
+#include "coproc/management.h"
+#include "coproc/router.h"
+#include "coproc/service.h"
+#include "coproc/tests/coproc_test_fixture.h"
+#include "coproc/tests/utils.h"
+#include "coproc/types.h"
+#include "test_utils/fixture.h"
+
+#include <seastar/util/defer.hh>
+
+ss::future<result<rpc::client_context<coproc::enable_copros_reply>>>
+coproc_register_topics(
+  rpc::client<coproc::management_client_protocol>& client,
+  std::vector<coproc::enable_copros_request::data>&& data) {
+    coproc::enable_copros_request req{.inputs = std::move(data)};
+    return client.enable_copros(
+      std::move(req), rpc::client_opts(rpc::no_timeout));
+}
+
+ss::future<result<rpc::client_context<coproc::disable_copros_reply>>>
+coproc_deregister_topics(
+  rpc::client<coproc::management_client_protocol>& client,
+  std::vector<uint32_t>&& sids) {
+    std::vector<coproc::script_id> script_ids;
+    script_ids.reserve(sids.size());
+    std::transform(
+      sids.begin(),
+      sids.end(),
+      std::back_inserter(script_ids),
+      [](uint32_t id) { return coproc::script_id(id); });
+    coproc::disable_copros_request req{.ids = std::move(script_ids)};
+    return client.disable_copros(
+      std::move(req), rpc::client_opts(rpc::no_timeout));
+}
+
+class management_service_fixture
+  : public coproc_test_fixture<rpc_sharded_service_tag> {
+public:
+    management_service_fixture()
+      : coproc_test_fixture<rpc_sharded_service_tag>(43118, false) {
+        register_service<coproc::service>(std::ref(get_router()));
+    }
+
+    // This function ensures that the internals of the coproc::active_mappings
+    // caches are laid out how they are expected to be i.e. the right ntps
+    // existing on the correct cores determined by the _shard_table
+    ss::future<int> coproc_validate(absl::flat_hash_set<model::topic> topics) {
+        return get_router().map_reduce0(
+          [this, topics = std::move(topics)](const coproc::router& r) {
+              return std::count_if(
+                get_data().cbegin(),
+                get_data().cend(),
+                [&topics, &r](const model::ntp& ntp) {
+                    return topics.find(ntp.tp.topic) != topics.end()
+                             ? r.ntp_exists(ntp)
+                             : false;
+                });
+          },
+          0, // 3. Aggregate sum
+          std::plus<>());
+    }
+};
+
+// This test fixture tests the edge cases, i.e. situations that should fail
+FIXTURE_TEST(test_coproc_invalid_topics, management_service_fixture) {
+    startup(
+      {{make_ts("foo"), 5}, {make_ts("bar"), 3}, {make_ts("baz"), 18}}, {});
+    auto client = rpc::client<coproc::management_client_protocol>(
+      client_config());
+    client.connect().get();
+    auto dclient = ss::defer([&client] { client.stop().get(); });
+
+    // This string is more then 249 chars, should be an error to attempt to
+    // register
+    static const ss::sstring too_long_topic(
+      "abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_"
+      "abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_"
+      "abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_"
+      "abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_abcedgtpr_"
+      "abcedgtpr_abcedgtpr_");
+
+    const auto resp
+      = coproc_register_topics(
+          client,
+          {make_enable_req(
+            1234,
+            {{".", l}, {"..", l}, {"foo", l}, {"", l}, {too_long_topic, l}})})
+          .get0()
+          .value()
+          .data;
+    BOOST_CHECK_EQUAL(resp.acks.size(), 1);
+    const auto& first_acks = resp.acks[0].second;
+    BOOST_CHECK_EQUAL(first_acks.size(), 5);
+    BOOST_CHECK_EQUAL(resp.acks[0].first, coproc::script_id(1234));
+    BOOST_CHECK(first_acks[0] == erc::invalid_topic);
+    BOOST_CHECK(first_acks[1] == erc::invalid_topic);
+    BOOST_CHECK(first_acks[2] == erc::success);
+    BOOST_CHECK(first_acks[3] == erc::invalid_topic);
+    BOOST_CHECK(first_acks[4] == erc::invalid_topic);
+    BOOST_CHECK_EQUAL(coproc_validate(to_topic_set({"foo"})).get0(), 5);
+}
+
+FIXTURE_TEST(test_coproc_reg_materialized_topic, management_service_fixture) {
+    startup(
+      {{make_ts("foo"), 6}, {make_ts("bar"), 3}, {make_ts("baz"), 18}}, {});
+    auto client = rpc::client<coproc::management_client_protocol>(
+      client_config());
+    client.connect().get();
+    auto dclient = ss::defer([&client] { client.stop().get(); });
+
+    const auto resp = coproc_register_topics(
+                        client,
+                        {make_enable_req(2313, {{"foo", l}, {"foo.$bar$", l}})})
+                        .get0()
+                        .value()
+                        .data;
+    BOOST_CHECK_EQUAL(resp.acks.size(), 1);
+    const auto& acks = resp.acks[0].second;
+    BOOST_CHECK_EQUAL(coproc::script_id(2313), resp.acks[0].first);
+    BOOST_CHECK_EQUAL(acks.size(), 2);
+    BOOST_CHECK(acks[0] == erc::success);
+    BOOST_CHECK(acks[1] == erc::materialized_topic);
+    BOOST_CHECK_EQUAL(coproc_validate(to_topic_set({"foo"})).get0(), 6);
+    BOOST_CHECK_EQUAL(coproc_validate(to_topic_set({"foo.$bar$"})).get0(), 0);
+}
+
+FIXTURE_TEST(test_coproc_script_id_already_exists, management_service_fixture) {
+    const uint32_t script_id = 55431;
+    log_layout_map storage_layout = {
+      {make_ts("foo"), 5}, {make_ts("bar"), 3}, {make_ts("baz"), 18}};
+    active_copros router_layout = {make_enable_req(script_id, {{"foo", l}})};
+    startup(std::move(storage_layout), std::move(router_layout));
+    auto client = rpc::client<coproc::management_client_protocol>(
+      client_config());
+    client.connect().get();
+    auto dclient = ss::defer([&client] { client.stop().get(); });
+
+    const auto resp = coproc_register_topics(
+                        client,
+                        {make_enable_req(3289, {{"foo", l}, {"bar", e}}),
+                         {make_enable_req(script_id, {{"nogo", l}})}})
+                        .get0()
+                        .value()
+                        .data;
+    BOOST_CHECK_EQUAL(coproc_validate(to_topic_set({"foo", "bar"})).get0(), 5);
+    BOOST_CHECK_EQUAL(resp.acks.size(), 2);
+    const auto& acks_1 = resp.acks[0].second;
+    const auto& acks_2 = resp.acks[1].second;
+    BOOST_CHECK_EQUAL(acks_1.size(), 2);
+    BOOST_CHECK_EQUAL(acks_2.size(), 1);
+    BOOST_CHECK_EQUAL(resp.acks[0].first, coproc::script_id(3289));
+    BOOST_CHECK_EQUAL(resp.acks[1].first, coproc::script_id(script_id));
+    BOOST_CHECK(acks_1[0] == erc::success);
+    BOOST_CHECK(acks_1[1] == erc::invalid_ingestion_policy);
+    BOOST_CHECK(acks_2[0] == erc::script_id_already_exists);
+    BOOST_CHECK_EQUAL(coproc_validate(to_topic_set({"foo", "bar"})).get0(), 5);
+}
+
+FIXTURE_TEST(test_coproc_topics, management_service_fixture) {
+    startup(
+      {{make_ts("foo"), 8}, {make_ts("bar"), 2}, {make_ts("baz"), 18}}, {});
+    auto client = rpc::client<coproc::management_client_protocol>(
+      client_config());
+    client.connect().get();
+    auto dclient = ss::defer([&client] { client.stop().get(); });
+
+    // 1. Attempt to register foo, bar and baz
+    const auto resp = coproc_register_topics(
+                        client,
+                        {make_enable_req(
+                           1523, {{"foo", l}, {"bar", l}, {"baz", l}}),
+                         make_enable_req(123, {{"foo", l}})})
+                        .get0()
+                        .value()
+                        .data;
+    // 2. ensure server replied with success for all topics
+    BOOST_CHECK_EQUAL(resp.acks.size(), 2);
+    const auto& first_acks = resp.acks[0].second;
+    const auto& second_acks = resp.acks[1].second;
+    BOOST_CHECK_EQUAL(resp.acks[0].first, coproc::script_id(1523));
+    BOOST_CHECK_EQUAL(resp.acks[1].first, coproc::script_id(123));
+    BOOST_CHECK_EQUAL(first_acks.size(), 3);
+    BOOST_CHECK(first_acks[0] == erc::success);
+    BOOST_CHECK(first_acks[1] == erc::success);
+    BOOST_CHECK(first_acks[2] == erc::success);
+    BOOST_CHECK_EQUAL(second_acks.size(), 1);
+    BOOST_CHECK(second_acks[0] == erc::success);
+
+    // 3. Verify they actually exist in the 'source_topics' struct
+    BOOST_CHECK_EQUAL(
+      coproc_validate(to_topic_set({"foo", "bar", "baz"})).get0(), 28);
+
+    // 4-6. Attempt to deregister some
+    const auto disable_acks
+      = coproc_deregister_topics(client, {1523}).get0().value().data;
+    BOOST_CHECK_EQUAL(disable_acks.acks.size(), 1);
+    BOOST_CHECK(disable_acks.acks[0] == drc::success);
+    BOOST_CHECK_EQUAL(
+      coproc_validate(to_topic_set({"foo", "bar", "baz"})).get0(), 8);
+    BOOST_CHECK_EQUAL(coproc_validate(to_topic_set({"foo"})).get0(), 8);
+
+    const auto disable_acks2
+      = coproc_deregister_topics(client, {123}).get0().value().data;
+    BOOST_CHECK_EQUAL(disable_acks2.acks.size(), 1);
+    BOOST_CHECK(disable_acks2.acks[0] == drc::success);
+    BOOST_CHECK_EQUAL(
+      coproc_validate(to_topic_set({"foo", "bar", "baz"})).get0(), 0);
+}
