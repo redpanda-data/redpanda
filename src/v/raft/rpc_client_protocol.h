1a394d392909d4b4e49d78ad97cbe98ff4d3ac02
Noah Watkins <noah@vectorized.io>
2020-09-13 14:20:00 -0700

    tidy: remove static from function impl in header
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/raft/rpc_client_protocol.h
+++ src/v/raft/rpc_client_protocol.h
@@ -35,7 +35,7 @@ private:
     ss::sharded<rpc::connection_cache>& _connection_cache;
 };
 
-static inline consensus_client_protocol
+inline consensus_client_protocol
 make_rpc_client_protocol(ss::sharded<rpc::connection_cache>& clients) {
     return raft::make_consensus_client_protocol<raft::rpc_client_protocol>(
       clients);
