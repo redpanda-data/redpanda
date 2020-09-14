246070045c2bfb6ae869a1c0b3e59eaf360c833c
Andres Aristizabal <andres@vectorized.io>
2020-09-14 09:37:05 -0500

    coproc: change rpcgen path on generate-entries.sh and add rpc generator command
    ---
    since 3:
      - add commands for generate enableDisableClass

--- src/js/generate-entries.sh
+++ src/js/generate-entries.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 root=$(git rev-parse --show-toplevel)
-cd "$root"/tools/ts-generator &&
-  python rpcgen_js.py \
+cd "$root"/tools/ts-generator/types &&
+  python types_gen_js.py \
     --entities-define-file "$root"/src/js/modules/domain/generatedRpc/entitiesDefinition.json \
     --output-file "$root"/src/js/modules/domain/generatedRpc/generatedClasses.ts
+
+cd "$root"/tools/ts-generator/types &&
+  python types_gen_js.py \
+    --entities-define-file "$root"/src/js/modules/domain/generatedRpc/enableDisableCoproc.json \
+    --output-file "$root"/src/js/modules/domain/generatedRpc/enableDisableCoprocClasses.ts
+
+cd "$root"/tools/ts-generator/rpc &&
+  python rpc_gen_js.py \
+    --server-define-file "$root"/src/idl/coproc_idl.json \
+    --output-file "$root"/src/js/modules/rpc/serverAndClients/server.ts
