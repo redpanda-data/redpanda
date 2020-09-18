4aa516b097a53042787763fe85210757ad3fc25e
Michal Maslanka <michal@vectorized.io>
2020-09-18 11:13:42 +0200

    c/controller_backend: finish backend task based on result
    Added std::error_code result to controller backend operations. Thanks to
    this the backend can decide whether it can mark task as finished.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/cluster/controller_backend.h
+++ src/v/cluster/controller_backend.h
@@ -17,6 +17,7 @@ namespace cluster {
 
 class controller_backend {
 public:
+    using results_t = std::vector<std::error_code>;
     controller_backend(
       ss::sharded<cluster::topic_table>&,
       ss::sharded<shard_table>&,
@@ -42,10 +43,10 @@ private:
     void start_topics_reconciliation_loop();
     ss::future<> reconcile_topics();
     ss::future<> do_reconcile_topic(task_meta<topic_table::delta>&);
-    ss::future<>
+    ss::future<std::error_code>
       create_partition(model::ntp, raft::group_id, std::vector<model::broker>);
     ss::future<> add_to_shard_table(model::ntp, raft::group_id, ss::shard_id);
-    ss::future<> delete_partition(model::ntp);
+    ss::future<std::error_code> delete_partition(model::ntp);
     void housekeeping();
 
     ss::sharded<topic_table>& _topics;
