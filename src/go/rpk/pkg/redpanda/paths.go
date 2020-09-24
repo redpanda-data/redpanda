a50d9c00411c21f1803a3ee083952ffaed92a047
David Castillo <david@vectorized.io>
2020-09-24 14:44:19 -0500

    rpk/redpanda: Remove func to find config from old location

--- src/go/rpk/pkg/redpanda/paths.go
+++ src/go/rpk/pkg/redpanda/paths.go
@@ -1,7 +1,6 @@
 package redpanda
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -18,35 +17,8 @@ var redpandaInstallDirContent = []string{
 	"libexec/redpanda",
 }
 
-type pathProvider func() ([]string, error)
-
-var configPathProviders = []pathProvider{
-	currentDirectory,
-	sysConfDirectory,
-	currentDirectoryParents,
-}
-
 func GetIOConfigPath(configFileDirectory string) string {
 	return filepath.Join(configFileDirectory, "io-config.yaml")
-}
-
-func FindConfig(fs afero.Fs) (string, error) {
-	log.Debugf("Looking for redpanda config file")
-
-	for _, provider := range configPathProviders {
-		paths, err := provider()
-		if err != nil {
-			return "", err
-		}
-		for _, path := range paths {
-			candidate := filepath.Join(path, "redpanda.yaml")
-			log.Debugf("Looking for redpanda config file in '%s'", path)
-			if exists, _ := afero.Exists(fs, candidate); exists {
-				return candidate, nil
-			}
-		}
-	}
-	return "", errors.New("Redpanda config not found")
 }
 
 func FindInstallDir(fs afero.Fs) (string, error) {
@@ -66,36 +38,4 @@ func FindInstallDir(fs afero.Fs) (string, error) {
 	}
 	log.Debugf("Redpanda is installed in '%s'", installDirCandidate)
 	return installDirCandidate, nil
-}
-
-func sysConfDirectory() ([]string, error) {
-	return []string{"/etc/redpanda"}, nil
-}
-
-func getCurrentDirectory() (string, error) {
-	path, err := os.Getwd()
-	if err != nil {
-		return "", err
-	}
-	return path, nil
-}
-
-func currentDirectory() ([]string, error) {
-	currentDir, err := getCurrentDirectory()
-	if err != nil {
-		return nil, err
-	}
-	return []string{currentDir}, nil
-}
-
-func currentDirectoryParents() ([]string, error) {
-	currentDir, err := getCurrentDirectory()
-	if err != nil {
-		return nil, err
-	}
-	var result []string
-	for dir := filepath.Dir(currentDir); dir != string(filepath.Separator); dir = filepath.Dir(dir) {
-		result = append(result, dir)
-	}
-	return result, nil
 }
