7582efd988e12c05f0c08fd6dd008f68f1273e9e
Rob Blafford <rob@vectorized.io>
2020-09-30 18:10:02 -0500

    reflection: Template specialization for async_adl::std::vector<T>
    - Included are unit tests that serialize / deserialize a std::vector<model::ntp>

--- src/v/reflection/async_adl.h
+++ src/v/reflection/async_adl.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "reflection/adl.h"
+
+#include <boost/range/irange.hpp>
 namespace reflection {
 template<typename T>
 struct async_adl {
@@ -11,6 +13,38 @@ struct async_adl {
     }
     ss::future<type> from(iobuf_parser& p) {
         return ss::make_ready_future<type>(reflection::adl<type>{}.from(p));
+    }
+};
+
+template<typename T>
+struct async_adl<std::vector<T>> {
+    using value_type = std::remove_reference_t<std::decay_t<T>>;
+
+    ss::future<> to(iobuf& out, std::vector<value_type> t) {
+        reflection::serialize<int32_t>(out, t.size());
+        return ss::do_with(std::move(t), [&out](auto& t) {
+            return ss::do_for_each(t, [&out](value_type& element) {
+                return async_adl<value_type>{}.to(out, std::move(element));
+            });
+        });
+    }
+
+    ss::future<std::vector<value_type>> from(iobuf_parser& in) {
+        const auto size = adl<int32_t>{}.from(in);
+        std::vector<value_type> result;
+        result.reserve(size);
+        return ss::do_with(std::move(result), [size, &in](auto& result) {
+            const auto r = boost::irange<size_t>(0, size);
+            return ss::do_for_each(
+                     r,
+                     [&result, &in](size_t) {
+                         return async_adl<value_type>{}.from(in).then(
+                           [&result](auto d) {
+                               result.push_back(std::move(d));
+                           });
+                     })
+              .then([&result] { return std::move(result); });
+        });
     }
 };
 } // namespace reflection
---
+++ src/v/reflection/test/async_adl_test.cc
@@ -0,0 +1,50 @@
+#include "bytes/iobuf.h"
+#include "bytes/iobuf_parser.h"
+#include "model/adl_serde.h"
+#include "model/fundamental.h"
+#include "random/generators.h"
+#include "reflection/async_adl.h"
+
+#include <seastar/testing/thread_test_case.hh>
+
+#include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test.hpp>
+
+namespace rand_gen = random_generators;
+
+model::ntp make_random_ntp() {
+    auto ns = rand_gen::gen_alphanum_string(5);
+    auto topic = rand_gen::gen_alphanum_string(10);
+    auto partition_id = rand_gen::get_int(0, 20);
+    return model::ntp(ns, topic, partition_id);
+}
+
+std::vector<model::ntp> make_random_collection() {
+    const auto size = rand_gen::get_int(0, 5);
+    std::vector<model::ntp> ntps;
+    ntps.reserve(size);
+    for (auto i = 0; i < size; ++i) {
+        ntps.emplace_back(make_random_ntp());
+    }
+    return ntps;
+}
+
+SEASTAR_THREAD_TEST_CASE(test_async_adl_collection) {
+    using ntp_vec = std::vector<model::ntp>;
+    // Serialize
+    iobuf out;
+    auto original_vec = make_random_collection();
+    reflection::async_adl<ntp_vec>{}.to(out, original_vec).get();
+    const auto originals_hash = std::hash<iobuf>{}(out);
+
+    // Deserialize
+    iobuf_parser in(std::move(out));
+    auto result = reflection::async_adl<ntp_vec>{}.from(in).get0();
+    BOOST_REQUIRE_EQUAL(original_vec, result);
+
+    // Reserialize
+    iobuf second_out;
+    reflection::async_adl<ntp_vec>{}.to(second_out, std::move(result)).get();
+    const auto seconds_hash = std::hash<iobuf>{}(second_out);
+    BOOST_REQUIRE_EQUAL(originals_hash, seconds_hash);
+}
