402b6e720ddd487197c93e71ae8791b35d48927b
Rob Blafford <rob@vectorized.io>
2020-10-01 10:24:39 -0500

    coproc: Update coproc types
    - Modify the registration API to be script_id aware
    
    - Add types that descirbe the request/reply protocol between the coprocessor
    engine and redpanda/coproc
    
    - The service will have one endpoint that takes a process_batch_request
      and responds with a process_batch_reply
    
    - The model::record_batch_reader will represent the batch of records
    
    - coproc: Rename generated coproc::registration to coproc::management

--- src/v/coproc/types.cc
+++ src/v/coproc/types.cc
@@ -2,6 +2,8 @@
 
 #include "model/validation.h"
 
+#include <boost/range/irange.hpp>
+
 namespace coproc {
 
 std::optional<materialized_topic>
@@ -42,4 +44,85 @@ make_materialized_topic(const model::topic& topic) {
     return materialized_topic{.src = src_tv, .dest = dest_tv};
 }
 
+std::ostream& operator<<(
+  std::ostream& os, enable_copros_request::data::topic_mode topic_mode) {
+    os << "<Topic: " << topic_mode.first << " mode: " << (int)topic_mode.second
+       << ">";
+    return os;
+}
+
 } // namespace coproc
+
+namespace reflection {
+
+ss::future<> async_adl<coproc::process_batch_request>::to(
+  iobuf& out, coproc::process_batch_request&& r) {
+    return async_adl<std::vector<coproc::process_batch_request::data>>{}.to(
+      out, std::move(r.reqs));
+}
+
+ss::future<coproc::process_batch_request>
+async_adl<coproc::process_batch_request>::from(iobuf_parser& in) {
+    return async_adl<std::vector<coproc::process_batch_request::data>>{}
+      .from(in)
+      .then([](std::vector<coproc::process_batch_request::data> reqs) mutable {
+          return coproc::process_batch_request{.reqs = std::move(reqs)};
+      });
+}
+
+ss::future<> async_adl<coproc::process_batch_request::data>::to(
+  iobuf& out, coproc::process_batch_request::data&& r) {
+    reflection::serialize<std::vector<coproc::script_id>>(
+      out, std::move(r.ids));
+    reflection::serialize<model::ntp>(out, std::move(r.ntp));
+    return async_adl<model::record_batch_reader>{}.to(out, std::move(r.reader));
+}
+
+ss::future<coproc::process_batch_request::data>
+async_adl<coproc::process_batch_request::data>::from(iobuf_parser& in) {
+    auto ids = adl<std::vector<coproc::script_id>>{}.from(in);
+    auto ntp = adl<model::ntp>{}.from(in);
+    return async_adl<model::record_batch_reader>{}.from(in).then(
+      [ids = std::move(ids),
+       ntp = std::move(ntp)](model::record_batch_reader rbr) mutable {
+          return coproc::process_batch_request::data{
+            .ids = std::move(ids),
+            .ntp = std::move(ntp),
+            .reader = std::move(rbr)};
+      });
+}
+
+ss::future<> async_adl<coproc::process_batch_reply::data>::to(
+  iobuf& out, coproc::process_batch_reply::data&& r) {
+    reflection::serialize<coproc::script_id>(out, std::move(r.id));
+    reflection::serialize<model::ntp>(out, std::move(r.ntp));
+    return async_adl<model::record_batch_reader>{}.to(out, std::move(r.reader));
+}
+
+ss::future<coproc::process_batch_reply::data>
+async_adl<coproc::process_batch_reply::data>::from(iobuf_parser& in) {
+    auto id = adl<coproc::script_id>{}.from(in);
+    auto ntp = adl<model::ntp>{}.from(in);
+    return async_adl<model::record_batch_reader>{}.from(in).then(
+      [id, ntp = std::move(ntp)](model::record_batch_reader rbr) mutable {
+          return coproc::process_batch_reply::data{
+            .id = id, .ntp = std::move(ntp), .reader = std::move(rbr)};
+      });
+}
+
+ss::future<> async_adl<coproc::process_batch_reply>::to(
+  iobuf& out, coproc::process_batch_reply&& r) {
+    return async_adl<std::vector<coproc::process_batch_reply::data>>{}.to(
+      out, std::move(r.resps));
+}
+
+ss::future<coproc::process_batch_reply>
+async_adl<coproc::process_batch_reply>::from(iobuf_parser& in) {
+    return async_adl<std::vector<coproc::process_batch_reply::data>>{}
+      .from(in)
+      .then([](std::vector<coproc::process_batch_reply::data> resps) mutable {
+          return coproc::process_batch_reply{.resps = std::move(resps)};
+      });
+}
+
+} // namespace reflection
