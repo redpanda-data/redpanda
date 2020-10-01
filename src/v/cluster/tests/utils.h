289a5cb083287a8881cd47d828db98b18b502bec
Michal Maslanka <michal@vectorized.io>
2020-10-01 15:48:54 +0200

    c/tests: added partition movement tests
    Implemented fuzzing tests for partitions movement.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/cluster/tests/utils.h
+++ src/v/cluster/tests/utils.h
@@ -10,7 +10,11 @@
  */
 
 #pragma once
+#include "cluster/topic_table.h"
 #include "cluster/types.h"
+#include "test_utils/async.h"
+
+#include <chrono>
 
 static const model::ns test_ns = model::ns("test-namespace");
 
@@ -33,4 +37,40 @@ static cluster::partition_assignment create_test_assignment(
           };
       });
     return p_as;
+}
+
+using batches_t = ss::circular_buffer<model::record_batch>;
+using batches_ptr_t = ss::lw_shared_ptr<batches_t>;
+using foreign_batches_t = ss::foreign_ptr<batches_ptr_t>;
+
+static void wait_for_metadata(
+  cluster::topic_table& topic_table,
+  const std::vector<cluster::topic_result>& results,
+  std::chrono::milliseconds tout = std::chrono::seconds(5)) {
+    tests::cooperative_spin_wait_with_timeout(tout, [&results, &topic_table] {
+        return std::all_of(
+          results.begin(),
+          results.end(),
+          [&topic_table](const cluster::topic_result& r) {
+              return topic_table.get_topic_metadata(r.tp_ns);
+          });
+    }).get0();
+}
+
+inline bool
+are_batches_the_same(const foreign_batches_t& a, const foreign_batches_t& b) {
+    if (a->size() != b->size()) {
+        return false;
+    }
+
+    auto p = std::mismatch(
+      a->begin(),
+      a->end(),
+      b->begin(),
+      b->end(),
+      [](
+        const model::record_batch& a_batch,
+        const model::record_batch& b_batch) { return a_batch == b_batch; });
+
+    return p.first == a->end() && p.second == b->end();
 }
