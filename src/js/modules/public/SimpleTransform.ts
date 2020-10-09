0f66bf3e6ec450927f544bffc7faa408d58834a3
Andres Aristizabal <andres@vectorized.io>
2020-10-08 20:58:21 -0500

    coproc: add wrapper for coprocessor class
    ---
    since v2:
     - fix typos
     - improve error message

---
+++ src/js/modules/public/SimpleTransform.ts
@@ -0,0 +1,34 @@
+import { RecordBatch, Coprocessor, PolicyError } from "./Coprocessor";
+
+export class SimpleTransform implements Coprocessor {
+  subscribe(inputTopics: Coprocessor["inputTopics"]): void {
+    this.inputTopics = inputTopics;
+  }
+
+  errorHandler(policyError: PolicyError): void {
+    switch (policyError) {
+      case PolicyError.Deregister:
+        this.policyError = policyError;
+        break;
+      case PolicyError.SkipOnFailure:
+        this.policyError = policyError;
+        break;
+      default:
+        throw Error(
+          `unexpected policy ${policyError}, valid: ${Object.keys(PolicyError)}`
+        );
+    }
+  }
+
+  processRecord(applyFunction: Coprocessor["apply"]): void {
+    this.apply = applyFunction;
+  }
+
+  globalId: number;
+  inputTopics: string[];
+  policyError: PolicyError;
+
+  apply(record: RecordBatch): RecordBatch {
+    throw Error("processRecord isn't implemented yet");
+  }
+}
