4b33a2422303884f674e91de028b0a190a0909c5
Noah Watkins <noah@vectorized.io>
2020-10-08 22:00:34 -0700

    model: remove unused code
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/model/record_utils.h
+++ src/v/model/record_utils.h
@@ -11,13 +11,9 @@ class record;
 
 void crc_record_batch_header(crc32&, const record_batch_header&);
 
-void crc_record_batch(crc32&, const record_batch&);
-
 /// \brief int32_t because that's what kafka uses
 int32_t crc_record_batch(const record_batch& b);
 int32_t crc_record_batch(const record_batch_header&, const iobuf&);
-
-int32_t recompute_record_batch_size(const record_batch& b);
 
 /// \brief uint32_t because that's what crc32c uses
 /// it is *only* record_batch_header.header_crc;
