80f3e36c6368ea781c3ec258db63df684065290f
Ben Pope <ben@vectorized.io>
2020-10-11 18:14:45 +0100

    pp/client: Add retry_with_mitigation

---
+++ src/v/pandaproxy/client/retry_with_mitigation.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "utils/concepts-enabled.h"
+#include "utils/retry.h"
+
+namespace pandaproxy::client {
+
+/// \brief Perform an action with retry on failure.
+///
+/// If the action returns an error, it is retried with a backoff.
+/// There is an attempt to mitigate the error after the backoff and prior
+/// to the retry.
+template<
+  typename Func,
+  typename ErrFunc,
+  typename Futurator = ss::futurize<std::invoke_result_t<Func>>>
+CONCEPT(requires requires(Func f) {
+    Func g = f;
+    seastar::futurize_invoke(f);
+})
+auto retry_with_mitigation(
+  int32_t retries,
+  std::chrono::milliseconds retry_base_backoff,
+  Func func,
+  ErrFunc errFunc) {
+    using namespace std::chrono_literals;
+    return ss::do_with(
+      std::move(func),
+      std::move(errFunc),
+      std::exception_ptr(),
+      [retries, retry_base_backoff](
+        Func& func, ErrFunc& errFunc, std::exception_ptr& eptr) {
+          return retry_with_backoff(
+            retries,
+            [&func, &errFunc, &eptr]() {
+                auto fut = ss::now();
+                if (eptr) {
+                    auto fut = errFunc(eptr).handle_exception(
+                      [](const std::exception_ptr&) {
+                          // ignore failed mitigation
+                      });
+                }
+                return fut.then(func).handle_exception(
+                  [&eptr](std::exception_ptr ex) mutable {
+                      eptr = ex;
+                      return Futurator::make_exception_future(eptr);
+                  });
+            },
+            retry_base_backoff);
+      });
+}
+
+} // namespace pandaproxy::client
