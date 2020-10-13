93f405e254f7e14d2e1d58f596b2e61c2c09fc3f
Andres Aristizabal <andres@vectorized.io>
2020-10-13 10:17:10 -0500

    fmt: format js files with js_fmt

--- tools/ci/gcb_hooks/slack/index.js
+++ tools/ci/gcb_hooks/slack/index.js
@@ -1,4 +1,4 @@
-const {IncomingWebhook} = require('@slack/webhook');
+const { IncomingWebhook } = require("@slack/webhook");
 const url = process.env.SLACK_WEBHOOK_URL;
 
 const webhook = new IncomingWebhook(url);
@@ -8,7 +8,7 @@ module.exports.subscribeSlack = (pubSubEvent, context) => {
   const build = eventToBuild(pubSubEvent.data);
 
   // Skip if manually triggered
-  if (!build.hasOwnProperty('buildTriggerId')) {
+  if (!build.hasOwnProperty("buildTriggerId")) {
     return;
   }
 
@@ -16,7 +16,7 @@ module.exports.subscribeSlack = (pubSubEvent, context) => {
   // Add additional statuses to list if you'd like:
   // QUEUED, WORKING, SUCCESS, FAILURE,
   // INTERNAL_ERROR, TIMEOUT, CANCELLED
-  const status = [ 'SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT' ];
+  const status = ["SUCCESS", "FAILURE", "INTERNAL_ERROR", "TIMEOUT"];
   if (status.indexOf(build.status) === -1) {
     return;
   }
@@ -27,15 +27,16 @@ module.exports.subscribeSlack = (pubSubEvent, context) => {
 };
 
 // eventToBuild transforms pubsub event message to a build object.
-const eventToBuild =
-    (data) => { return JSON.parse(Buffer.from(data, 'base64').toString()); };
+const eventToBuild = (data) => {
+  return JSON.parse(Buffer.from(data, "base64").toString());
+};
 
 // createSlackMessage creates a message from a build object.
 const createSlackMessage = (build) => {
   console.log(JSON.stringify(build));
   let repo, branch, sha;
   if (build.source && build.source.repoSource) {
-    ({repo, branch} = build.source.repoSource);
+    ({ repo, branch } = build.source.repoSource);
     sha = build.sourceProvenance.resolvedRepoSource.commitSha.substring(0, 8);
   } else if (build.substitutions) {
     repo = build.substitutions.REPO_NAME;
@@ -46,14 +47,15 @@ const createSlackMessage = (build) => {
   const cc = build.substitutions._COMPILER;
   // TODO: Add "thread_ts": sha,
   const message = {
-    'blocks' : [ {
-      'type' : 'section',
-      'text' : {
-        'type' : 'mrkdwn',
-        'text' : `${build.status} <${build.logUrl} | ${repo}/${branch}@${sha}-${
-            cc}-${type}>`
-      }
-    } ]
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `${build.status} <${build.logUrl} | ${repo}/${branch}@${sha}-${cc}-${type}>`,
+        },
+      },
+    ],
   };
 
   return message;
