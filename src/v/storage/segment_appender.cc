5551a439b30167e17af84856f0dfbb6888b8e963
Noah Watkins <noah@vectorized.io>
2020-10-14 10:58:51 -0700

    storage: appender options is trivially copyable
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/storage/segment_appender.cc
+++ src/v/storage/segment_appender.cc
@@ -20,7 +20,7 @@ size_missmatch_error(const char* ctx, size_t expected, size_t got) {
 }
 segment_appender::segment_appender(ss::file f, options opts)
   : _out(std::move(f))
-  , _opts(std::move(opts))
+  , _opts(opts)
   , _concurrent_flushes(_opts.number_of_chunks) {
     const auto align = _out.disk_write_dma_alignment();
     for (size_t i = 0; i < _opts.number_of_chunks; ++i) {
