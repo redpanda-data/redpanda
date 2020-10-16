720e3423efdab6564630914c9fae66ea4a423258
Dimitris Bachtis <dimitris@vectorized.io>
2020-10-16 14:29:31 +0300

    tests/rptest: Fix errors in python3
    `sys.maxint` is not present in python3 https://docs.python.org/3.1/whatsnew/3.0.html#integers
    `xrange` was renamed to `range`

--- tests/rptest/services/kaf_producer.py
+++ tests/rptest/services/kaf_producer.py
@@ -3,14 +3,14 @@ from ducktape.services.background_thread import BackgroundThreadService
 
 
 class KafProducer(BackgroundThreadService):
-    def __init__(self, context, redpanda, topic, num_records=sys.maxint):
+    def __init__(self, context, redpanda, topic, num_records=sys.maxsize):
         super(KafProducer, self).__init__(context, num_nodes=1)
         self._redpanda = redpanda
         self._topic = topic
         self._num_records = num_records
 
     def _worker(self, idx, node):
-        for i in xrange(self._num_records):
+        for i in range(self._num_records):
             cmd = "echo record-%08d | kaf produce -b %s --key key-%08d %s" % (
                 i, self._redpanda.brokers(), i, self._topic)
             for line in node.account.ssh_capture(cmd, timeout_sec=10):
