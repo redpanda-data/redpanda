3428c1c923fccd7f4a4b940da33c9a91fa11d211
David Castillo <david@vectorized.io>
2020-10-19 18:00:54 -0500

    rpk/tuners/kernel_ver: Add OS-specific files for Linux & Mac

--- src/go/rpk/pkg/tuners/kernel_version.go
+++ src/go/rpk/pkg/tuners/kernel_version.go
@@ -1,9 +1,6 @@
 package tuners
 
-import (
-	"fmt"
-	"syscall"
-)
+import "fmt"
 
 const (
 	ExpectedKernelVersion string = "4.19"
@@ -64,25 +61,4 @@ func (c kernelVersionChecker) Check() *CheckResult {
 
 	res.IsOk = true
 	return res
-}
-
-func UtsnameStr(in []int8) string {
-	i, out := 0, make([]byte, 0, len(in))
-	for ; i < len(in); i++ {
-		if in[i] == 0 {
-			break
-		}
-		out = append(out, byte(in[i]))
-	}
-	return string(out)
-}
-
-func GetKernelVersion() (string, error) {
-	var uname syscall.Utsname
-	err := syscall.Uname(&uname)
-	if err != nil {
-		return "", err
-	}
-
-	return UtsnameStr(uname.Release[:]), nil
 }
