33a405097b890ca4408a3b7cd7c3cb9c1db298a7
David Castillo <david@vectorized.io>
2020-10-20 16:43:04 -0500

    rpk/container: Add command to stop a cluster

---
+++ src/go/rpk/pkg/cli/cmd/container/stop.go
@@ -0,0 +1,66 @@
+package container
+
+import (
+	"context"
+	"sync"
+	"time"
+	"vectorized/pkg/cli/cmd/container/common"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+)
+
+func Stop(fs afero.Fs) *cobra.Command {
+	command := &cobra.Command{
+		Use:   "stop",
+		Short: "Stop an existing local container cluster",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			c, err := common.NewDockerClient()
+			if err != nil {
+				return err
+			}
+			defer c.Close()
+			return common.WrapIfConnErr(stopCluster(fs, c))
+		},
+	}
+	return command
+}
+
+func stopCluster(fs afero.Fs, c common.Client) error {
+	nodeIDs, err := common.GetExistingNodes(fs)
+	if err != nil {
+		return err
+	}
+	if len(nodeIDs) == 0 {
+		log.Info(
+			`No cluster available.
+You may start a new cluster with 'rpk container start'`,
+		)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(len(nodeIDs))
+	for _, nodeID := range nodeIDs {
+		go func(id uint) {
+			log.Debugf("Stopping node %d", id)
+			ctx := context.Background()
+			// Redpanda sometimes takes a while to stop, so 20
+			// seconds is a safe estimate
+			timeout := 20 * time.Second
+			err = c.ContainerStop(
+				ctx,
+				common.Name(id),
+				&timeout,
+			)
+			if err != nil {
+				log.Errorf("Couldn't stop node %d", id)
+				log.Debug(err)
+			}
+			wg.Done()
+		}(nodeID)
+	}
+	wg.Wait()
+	log.Info("Stopped current cluster.")
+	return nil
+}
