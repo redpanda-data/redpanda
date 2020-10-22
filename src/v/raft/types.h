43dfd0bca6ecc6fc30f332f75c10367248dd36ab
Denis Rystsov <denis@vectorized.io>
2020-10-21 17:33:03 -0700

    Renames hschlag_timestamp to hbeat_timestamp

--- src/v/raft/types.h
+++ src/v/raft/types.h
@@ -67,7 +67,7 @@ struct follower_index_metadata {
     model::offset next_index;
     // timestamp of last append_entries_rpc call
     clock_type::time_point last_append_timestamp;
-    clock_type::time_point last_hschlag_timestamp;
+    clock_type::time_point last_hbeat_timestamp;
     uint64_t failed_appends{0};
     // The pair of sequences used to track append entries requests sent and
     // received by the follower. Every time append entries request is created
