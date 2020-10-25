d496a069d810301728ca5d7fc0795296ca5a41e7
David Castillo <david@vectorized.io>
2020-10-25 10:06:46 -0500

    rpk/start: Update redpanda flags hydration
    Update the flags hydration logic to prioritize values passed through
    rpk flags, and to also take into consideration the current config
    when the flags weren't set.

--- src/go/rpk/pkg/cli/cmd/start_test.go
+++ src/go/rpk/pkg/cli/cmd/start_test.go
@@ -9,18 +9,18 @@ import (
 func TestMergeFlags(t *testing.T) {
 	tests := []struct {
 		name      string
-		current   map[string]string
+		current   map[string]interface{}
 		overrides []string
 		expected  map[string]string
 	}{
 		{
 			name:      "it should override the existent values",
-			current:   map[string]string{"a": "true", "b": "2", "c": "127.0.0.1"},
+			current:   map[string]interface{}{"a": "true", "b": "2", "c": "127.0.0.1"},
 			overrides: []string{"--a false", "b 42"},
 			expected:  map[string]string{"a": "false", "b": "42", "c": "127.0.0.1"},
 		}, {
 			name:    "it should override the existent values (2)",
-			current: map[string]string{"lock-memory": "true", "cpumask": "0-1", "logger-log-level": "'exception=debug'"},
+			current: map[string]interface{}{"lock-memory": "true", "cpumask": "0-1", "logger-log-level": "'exception=debug'"},
 			overrides: []string{"--overprovisioned", "--unsafe-bypass-fsync 1",
 				"--default-log-level=trace", "--logger-log-level='exception=debug'",
 				"--fail-on-abandoned-failed-futures"},
@@ -35,12 +35,12 @@ func TestMergeFlags(t *testing.T) {
 			},
 		}, {
 			name:      "it should create values not present in the current flags",
-			current:   map[string]string{},
+			current:   map[string]interface{}{},
 			overrides: []string{"b 42", "c 127.0.0.1"},
 			expected:  map[string]string{"b": "42", "c": "127.0.0.1"},
 		}, {
 			name:      "it shouldn't change the current flags if no overrides are given",
-			current:   map[string]string{"b": "42", "c": "127.0.0.1"},
+			current:   map[string]interface{}{"b": "42", "c": "127.0.0.1"},
 			overrides: []string{},
 			expected:  map[string]string{"b": "42", "c": "127.0.0.1"},
 		},
@@ -59,5 +59,4 @@ func TestMergeFlags(t *testing.T) {
 			}
 		})
 	}
-
 }
