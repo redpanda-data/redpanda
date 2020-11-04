b4f885e011541e7f75c1a25bb82017d4f742d40e
Alexander Gallego <alex@vectorized.io>
2020-11-03 22:03:24 -0800

    build.sh: explicitly specify Debug as default build type

--- build.sh
+++ build.sh
@@ -4,5 +4,13 @@ root=$(git rev-parse --show-toplevel)
 if [[ -z ${CC} ]]; then export CC=/usr/bin/gcc; fi
 if [[ -z ${CXX} ]]; then export CXX=/usr/bin/g++; fi
 if [[ ! -z ${CCACHE_DIR} && -e /dev/shm ]]; then export CCACHE_DIR=/dev/shm/redpanda; fi
-cmake -B$root/build -H$root -GNinja -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
+
+# Change Debug to `Release` for release builds
+cmake -DCMAKE_BUILD_TYPE=Debug \
+      -B$root/build \
+      -H$root \
+      -GNinja \
+      -DCMAKE_C_COMPILER=$CC \
+      -DCMAKE_CXX_COMPILER=$CXX
+
 (cd $root/build && ninja && ctest -R _rpunit)
