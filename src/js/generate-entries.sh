60cebe93dd865a235dcf0e6419e5ce84ad75b22f
Alexander Gallego <alex@vectorized.io>
2020-11-14 15:47:40 -0800

    vtools: fix generate-entries to only work w/ python3

--- src/js/generate-entries.sh
+++ src/js/generate-entries.sh
@@ -9,21 +9,21 @@
 
 root=$(git rev-parse --show-toplevel)
 cd "$root"/tools/ts-generator/types &&
-  python types_gen_js.py \
+  python3 types_gen_js.py \
     --entities-define-file "$root"/src/js/modules/domain/generatedRpc/entitiesDefinition.json \
     --output-file "$root"/src/js/modules/domain/generatedRpc/generatedClasses.ts
 
 cd "$root"/tools/ts-generator/types &&
-  python types_gen_js.py \
+  python3 types_gen_js.py \
     --entities-define-file "$root"/src/js/modules/domain/generatedRpc/enableDisableCoproc.json \
     --output-file "$root"/src/js/modules/domain/generatedRpc/enableDisableCoprocClasses.ts
 
 cd "$root"/tools/ts-generator/rpc &&
-  python rpc_gen_js.py \
+  python3 rpc_gen_js.py \
     --server-define-file "$root"/src/idl/coproc_idl.json \
     --output-file "$root"/src/js/modules/rpc/serverAndClients/server.ts
 
 cd "$root"/tools/ts-generator/rpc &&
-  python rpc_gen_js.py \
+  python3 rpc_gen_js.py \
     --server-define-file "$root"/src/idl/process_batch_coproc.json \
     --output-file "$root"/src/js/modules/rpc/serverAndClients/processBatch.ts
