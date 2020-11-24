b34b7aebf25c7bbc34428475139a579234566323
Michal Maslanka <michal@vectorized.io>
2020-11-24 14:32:51 +0100

    k/fetch_session_cache: introduced fetch session cache
    Implemented fetch session cache.
    
    Fetch session cache is a core local cache holding incremental fetch
    sessions. Each core local cache instance assigns session ids that are
    unique for the node (non overlapping ranges of ids are assigned to each core).
    The cache evicts not used sessions after configurable period of inactivity.
    Fetch session cache will stop adding new sessions after its max
    memory usage is reached.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/kafka/tests/fetch_session_test.cc
+++ src/v/kafka/tests/fetch_session_test.cc
@@ -9,6 +9,7 @@
  * by the Apache License, Version 2.0
  */
 #include "kafka/fetch_session.h"
+#include "kafka/fetch_session_cache.h"
 #include "kafka/requests/fetch_request.h"
 #include "kafka/types.h"
 #include "model/fundamental.h"
@@ -23,6 +24,7 @@
 
 #include <tuple>
 
+using namespace std::chrono_literals; // NOLINT
 struct fixture {
     static kafka::fetch_partition make_fetch_partition(
       model::topic topic, model::partition_id p_id, model::offset offset) {
@@ -33,13 +35,30 @@ struct fixture {
           .fetch_offset = offset,
           .high_watermark = offset};
     }
+
+    static kafka::fetch_request::topic
+    make_fetch_request_topic(model::topic tp, int partitions_count) {
+        kafka::fetch_request::topic fetch_topic{
+          .name = std::move(tp),
+          .partitions = {},
+        };
+
+        for (int i = 0; i < partitions_count; ++i) {
+            fetch_topic.partitions.push_back(kafka::fetch_request::partition{
+              .id = model::partition_id(i),
+              .fetch_offset = model::offset(i * 10),
+              .partition_max_bytes = 100_KiB,
+            });
+        }
+        return fetch_topic;
+    }
 };
 
 FIXTURE_TEST(test_next_epoch, fixture) {
     BOOST_REQUIRE_EQUAL(
       kafka::next_epoch(kafka::initial_fetch_session_epoch),
       kafka::fetch_session_epoch(1));
-    
+
     BOOST_REQUIRE_EQUAL(
       kafka::next_epoch(kafka::fetch_session_epoch(25)),
       kafka::fetch_session_epoch(26));
@@ -105,4 +124,95 @@ FIXTURE_TEST(test_fetch_session_basic_operations, fixture) {
     BOOST_REQUIRE(session.partitions().find(key) == session.partitions().end());
 
     BOOST_REQUIRE_LT(session.mem_usage(), mem_usage_before);
+}
+
+FIXTURE_TEST(test_session_operations, fixture) {
+    kafka::fetch_session_cache cache(120s);
+    kafka::fetch_request req;
+    req.session_epoch = kafka::initial_fetch_session_epoch;
+    req.session_id = kafka::invalid_fetch_session_id;
+    req.topics = {make_fetch_request_topic(model::topic("test"), 3)};
+    {
+        BOOST_TEST_MESSAGE("create new session");
+        auto ctx = cache.maybe_get_session(req);
+
+        BOOST_REQUIRE_EQUAL(ctx.error(), kafka::error_code::none);
+        BOOST_REQUIRE_EQUAL(ctx.has_error(), false);
+        // first fetch has to be full fetch
+        BOOST_REQUIRE_EQUAL(ctx.is_full_fetch(), true);
+        BOOST_REQUIRE_EQUAL(ctx.is_sessionless(), false);
+        BOOST_REQUIRE_NE(ctx.session().get(), nullptr);
+        auto rng = boost::make_iterator_range(
+          ctx.session()->partitions().cbegin_insertion_order(),
+          ctx.session()->partitions().cend_insertion_order());
+        auto i = 0;
+        BOOST_REQUIRE_EQUAL(ctx.session()->partitions().size(), 3);
+        for (const auto& fp : rng) {
+            BOOST_REQUIRE_EQUAL(fp.topic, req.topics[0].name);
+            BOOST_REQUIRE_EQUAL(fp.partition, req.topics[0].partitions[i].id);
+            BOOST_REQUIRE_EQUAL(
+              fp.fetch_offset, req.topics[0].partitions[i].fetch_offset);
+            BOOST_REQUIRE_EQUAL(
+              fp.max_bytes, req.topics[0].partitions[i].partition_max_bytes);
+            i++;
+        }
+
+        req.session_id = ctx.session()->id();
+        req.session_epoch = ctx.session()->epoch();
+    }
+
+    BOOST_TEST_MESSAGE("test updating session");
+    {
+        req.topics[0].partitions.erase(
+          std::next(req.topics[0].partitions.begin()));
+        // add 2 partitons from new topic, forget one from the first topic
+        req.topics.push_back(
+          make_fetch_request_topic(model::topic("test-new"), 2));
+        req.forgotten_topics.push_back(kafka::fetch_request::forgotten_topic{
+          .name = model::topic("test"), .partitions = {1}});
+
+        auto ctx = cache.maybe_get_session(req);
+
+        BOOST_REQUIRE_EQUAL(ctx.error(), kafka::error_code::none);
+        BOOST_REQUIRE_EQUAL(ctx.has_error(), false);
+        // this is an incremental fetch
+        BOOST_REQUIRE_EQUAL(ctx.is_full_fetch(), false);
+        BOOST_REQUIRE_EQUAL(ctx.is_sessionless(), false);
+        BOOST_REQUIRE_NE(ctx.session().get(), nullptr);
+
+        BOOST_REQUIRE_EQUAL(ctx.session()->partitions().size(), 4);
+        auto rng = boost::make_iterator_range(
+          ctx.session()->partitions().cbegin_insertion_order(),
+          ctx.session()->partitions().cend_insertion_order());
+
+        auto i = 0;
+        // check that insertion order is preserved
+        for (const auto& fp : rng) {
+            auto t_idx = i < 2 ? 0 : 1;
+            auto p_idx = i < 2 ? i : i - 2;
+
+            BOOST_REQUIRE_EQUAL(fp.topic, req.topics[t_idx].name);
+            BOOST_REQUIRE_EQUAL(
+              fp.partition, req.topics[t_idx].partitions[p_idx].id);
+            BOOST_REQUIRE_EQUAL(
+              fp.fetch_offset,
+              req.topics[t_idx].partitions[p_idx].fetch_offset);
+            BOOST_REQUIRE_EQUAL(
+              fp.max_bytes,
+              req.topics[t_idx].partitions[p_idx].partition_max_bytes);
+            i++;
+        }
+    }
+    BOOST_TEST_MESSAGE("removing session");
+    {
+        req.session_epoch = kafka::final_fetch_session_epoch;
+        req.topics = {};
+
+        auto ctx = cache.maybe_get_session(req);
+
+        BOOST_REQUIRE_EQUAL(ctx.is_sessionless(), true);
+        BOOST_REQUIRE_EQUAL(ctx.has_error(), false);
+        BOOST_REQUIRE(ctx.session().get() == nullptr);
+        BOOST_REQUIRE(cache.size() == 0);
+    }
 }
