8021156afa9669755c49337b19c4ac4c3c365426
Noah Watkins <noah@vectorized.io>
2020-12-02 10:26:45 -0800

    hashing: add sha computation utility
    Adds wrapper around gnutls hashing and adds types configured for sha256
    and sha512 variants.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/hashing/secure.h
+++ src/v/hashing/secure.h
@@ -25,6 +25,17 @@ private:
     const char* _msg;
 };
 
+class hash_exception final : public std::exception {
+public:
+    explicit hash_exception(const char* msg)
+      : _msg(msg) {}
+
+    const char* what() const noexcept final { return _msg; }
+
+private:
+    const char* _msg;
+};
+
 namespace internal {
 
 template<gnutls_mac_algorithm_t Algo, size_t DigestSize>
@@ -85,7 +96,58 @@ private:
     gnutls_hmac_hd_t _handle;
 };
 
+template<gnutls_digest_algorithm_t Algo, size_t DigestSize>
+class hash {
+    static_assert(DigestSize > 0, "digest cannot be zero length");
+
+public:
+    // silence clang-tidy about _handle being uninitialized
+    // NOLINTNEXTLINE(hicpp-member-init, cppcoreguidelines-pro-type-member-init)
+    hash() {
+        int ret = gnutls_hash_init(&_handle, Algo);
+        if (unlikely(ret)) {
+            throw hash_exception(gnutls_strerror(ret));
+        }
+
+        ret = gnutls_hash_get_len(Algo);
+        if (unlikely(ret != DigestSize)) {
+            throw hash_exception("invalid digest length");
+        }
+    }
+
+    hash(const hash&) = delete;
+    hash& operator=(const hash&) = delete;
+    hash(hash&&) = delete;
+    hash& operator=(hash&&) = delete;
+
+    ~hash() noexcept { gnutls_hash_deinit(_handle, nullptr); }
+
+    void update(const bytes& data) { update(data.data(), data.size()); }
+
+    /**
+     * Return the current output and reset.
+     */
+    std::array<char, DigestSize> reset() {
+        std::array<char, DigestSize> digest;
+        gnutls_hash_output(_handle, digest.data());
+        return digest;
+    }
+
+private:
+    void update(const void* data, size_t size) {
+        int ret = gnutls_hash(_handle, data, size);
+        if (unlikely(ret)) {
+            throw hash_exception(gnutls_strerror(ret));
+        }
+    }
+
+    gnutls_hash_hd_t _handle;
+};
+
 } // namespace internal
 
 using hmac_sha256 = internal::hmac<GNUTLS_MAC_SHA256, 32>; // NOLINT
 using hmac_sha512 = internal::hmac<GNUTLS_MAC_SHA512, 64>; // NOLINT
+
+using hash_sha256 = internal::hash<GNUTLS_DIG_SHA256, 32>; // NOLINT
+using hash_sha512 = internal::hash<GNUTLS_DIG_SHA512, 64>; // NOLINT
