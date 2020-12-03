3500a2d67a993e82454aaacc952595bed1de3660
Rob Blafford <rob@vectorized.io>
2020-12-03 14:44:56 -0500

    js: Create helpful log statements with winston

--- src/js/modules/rpc/service.ts
+++ src/js/modules/rpc/service.ts
@@ -13,13 +13,17 @@ import { safeLoadAll } from "js-yaml";
 import { join, resolve } from "path";
 import * as fs from "fs";
 import { promisify } from "util";
+import { newLogger } from "../utilities/Logging";
+
+const logger = newLogger("service");
 
 // read yaml config file
 const readConfigFile = (confPath: string): Promise<Record<string, any>> => {
   try {
+    logger.info(`Reading from config file: ${confPath}`);
     return fs.promises.readFile(confPath).then((file) => safeLoadAll(file)[0]);
   } catch (e) {
-    return Promise.reject(new Error(`Error reading config file: ${e}`));
+    return Promise.reject(new Error(`Error reading config file: ${e.message}`));
   }
 };
 
@@ -33,7 +37,7 @@ const validateOrCreateScaffolding = (directoryPath: string): Promise<void> => {
     const path = join(directoryPath, folder);
     return exists(path).then((exist) => {
       if (!exist) {
-        console.log(path);
+        logger.info(`Creating part of scaffolding ${path}`);
         return fs.promises.mkdir(path, { recursive: true });
       }
     });
@@ -41,22 +45,30 @@ const validateOrCreateScaffolding = (directoryPath: string): Promise<void> => {
   return Promise.all(validations).then(() => null);
 };
 
-// read config file path argument
-const configPathArg = process.argv.splice(2)[0];
-const defaultConfigPath = "/var/lib/redpanda/conf/redpanda.yaml";
-const defaultCoprocessorPath = "/var/lib/redpanda/coprocessor";
-// resolve config path or assign default value
-const configPath = configPathArg ? resolve(configPathArg) : defaultConfigPath;
+function main() {
+  // read config file path argument
+  logger.info("Starting redpanda wasm service...");
+  const configPathArg = process.argv.splice(2)[0];
+  const defaultConfigPath = "/var/lib/redpanda/conf/redpanda.yaml";
+  const defaultCoprocessorPath = "/var/lib/redpanda/coprocessor";
+  // resolve config path or assign default value
+  const configPath = configPathArg ? resolve(configPathArg) : defaultConfigPath;
+  logger.info(`Using redpanda configuration path: ${configPath}`);
 
-readConfigFile(configPath).then((config) => {
-  const port = config?.redpanda?.coproc_supervisor_server || 43189;
-  const path = config?.coproc_engine?.path || defaultCoprocessorPath;
-  validateOrCreateScaffolding(path).then(() => {
-    const service = new ProcessBatchServer(
-      join(path, "active"),
-      join(path, "inactive"),
-      join(path, "submit")
-    );
-    service.listen(port);
+  readConfigFile(configPath).then((config) => {
+    const port = config?.redpanda?.coproc_supervisor_server || 43189;
+    const path = config?.coproc_engine?.path || defaultCoprocessorPath;
+    logger.info(`Using root scaffolding path: ${path}`);
+    validateOrCreateScaffolding(path).then(() => {
+      const service = new ProcessBatchServer(
+        join(path, "active"),
+        join(path, "inactive"),
+        join(path, "submit")
+      );
+      logger.info(`Starting redpanda wasm service on port: ${port}`);
+      service.listen(port);
+    });
   });
-});
+}
+
+main();
