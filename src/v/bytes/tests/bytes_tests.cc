edb97f477d94ed6d9bc4899c7e2c11e4a96b9914
Noah Watkins <noah@vectorized.io>
2020-12-16 15:51:03 -0800

    bytes: add test for bytes xor utilities
    Signed-off-by: Noah Watkins <noah@vectorized.io>

---
+++ src/v/bytes/tests/bytes_tests.cc
@@ -0,0 +1,31 @@
+// Copyright 2020 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#include "bytes/bytes.h"
+
+#include <seastar/testing/thread_test_case.hh>
+
+#include <boost/test/unit_test.hpp>
+
+SEASTAR_THREAD_TEST_CASE(test_xor) {
+    const std::array<char, 4> data = {33, 17, 27, 103};
+    const std::array<char, 4> data2 = {13, 3, 125, 73};
+
+    const auto data3 = data ^ data2;
+    const std::array<char, 4> computed_xor = {44, 18, 102, 46};
+    BOOST_REQUIRE(data3 == computed_xor);
+
+    const auto data4
+      = bytes(reinterpret_cast<const uint8_t*>(data.data()), data.size())
+        ^ bytes(reinterpret_cast<const uint8_t*>(data2.data()), data2.size());
+
+    BOOST_REQUIRE_EQUAL(
+      data4,
+      bytes(reinterpret_cast<const uint8_t*>(data3.data()), data3.size()));
+}
