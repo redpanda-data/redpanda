69e633a55fa11e1c14bb025fc7caf921bde64248
Camilo Aguilar <camilo.aguilar@gmail.com>
2020-12-19 16:57:35 -0500

    rpk/build: Prevent globbing and word splitting

--- src/go/rpk/build.sh
+++ src/go/rpk/build.sh
@@ -14,11 +14,11 @@ img_tag=${version:-latest}
 
 out_dir="$(go env GOOS)-$(go env GOARCH)"
 
-mkdir -p ${out_dir}
+mkdir -p "${out_dir}"
 
 ver_pkg='vectorized/pkg/cli/cmd/version'
 cont_pkg='vectorized/pkg/cli/cmd/container/common'
 
 go build \
   -ldflags "-X ${ver_pkg}.version=${version} -X ${ver_pkg}.rev=${rev} -X ${cont_pkg}.tag=${img_tag}" \
-  -o ${out_dir} ./...
+  -o "${out_dir}" ./...
