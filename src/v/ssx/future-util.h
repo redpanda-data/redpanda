3ccd3b860bc16d2425166c808ef124c1b9651121
Rob Blafford <rob@vectorized.io>
2021-01-05 15:34:29 -0500

    ssx: async_flat_transform method
    - Useful when your mapper produces a vector of items and instead of recieving a
    vector<vector<item>> you get a single vector with all items included.

--- src/v/ssx/future-util.h
+++ src/v/ssx/future-util.h
@@ -99,13 +99,88 @@ inline auto async_transform(Rng&& rng, Func&& func) {
     return async_transform(rng.begin(), rng.end(), std::forward<Func>(func));
 }
 
+/// \brief Run tasks synchronously in order and wait for completion only
+/// invoking futures one after a previous has completed (range version).
+/// Difference is that the results are expected to be a series of collections
+/// for which are then transformed into one single collection.
+///
+/// Given a range [\c begin, \c end) of objects, run \c func on each \c *i in
+/// the range, and return a \c future<> containing a \c std::vector<> of
+/// std::vector<> of values. These values are then unflattened into a single
+/// vector as the functions return value. In case any of the given tasks fails,
+/// one of the exceptions is returned by this function as a failed future.
+///
+/// \param begin an \c InputIterator designating the beginning of the range
+/// \param end an \c InputIterator designating the end of the range
+/// \param func Function to invoke with each element in the range (will be
+/// futurized if it doesn't return a \c future<> and expected to return a
+/// std::vector<> of values)
+/// \return a \c future<> containing a \c std::vector<>
+/// of the results of the function invocations that resolves when all the
+/// function invocations complete.  If one or more return an exception, the
+/// return value contains one of the exceptions.
+// clang-format off
+template<typename Iterator, typename Func>
+CONCEPT(requires requires(Func f, Iterator i) {
+    *i++;
+    { i != i } ->std::convertible_to<bool>;
+    seastar::futurize_invoke(f, *i).get0().begin();
+    seastar::futurize_invoke(f, *i).get0().end();
+})
+// clang-format on
+inline auto async_flat_transform(Iterator begin, Iterator end, Func&& func) {
+    using result_type = decltype(seastar::futurize_invoke(func, *begin).get0());
+    using value_type = typename result_type::value_type;
+    std::vector<value_type> res;
+    res.reserve(std::distance(begin, end));
+    return seastar::do_with(
+      std::move(res),
+      std::move(begin),
+      std::move(end),
+      [func{std::forward<Func>(func)}](
+        std::vector<value_type>& res, Iterator& begin, Iterator& end) mutable {
+          return seastar::do_for_each(
+                   begin,
+                   end,
+                   [&res, func{std::forward<Func>(func)}](auto&& val) mutable {
+                       return seastar::futurize_invoke(
+                                func, std::forward<decltype(val)>(val))
+                         .then([&res](std::vector<value_type> r) {
+                             return seastar::do_with(
+                               std::move(r),
+                               [&res](std::vector<value_type>& r) {
+                                   return seastar::do_for_each(
+                                     r, [&res](value_type& element) {
+                                         res.push_back(std::move(element));
+                                     });
+                               });
+                         });
+                   })
+            .then([&res] { return std::move(res); });
+      });
+}
+
+// clang-format off
+template<typename Rng, typename Func>
+CONCEPT(requires requires(Func f, Rng r) {
+    r.begin();
+    r.end();
+    { r.begin() != r.begin() } -> std::convertible_to<bool>;
+    seastar::futurize_invoke(f, *r.begin()).get0();
+})
+// clang-format on
+inline auto async_flat_transform(Rng& rng, Func&& func) {
+    return async_flat_transform(
+      rng.begin(), rng.end(), std::forward<Func>(func));
+}
+
 /// \brief Run tasks in parallel and wait for completion, capturing possible
 /// errors (iterator version).
 ///
 /// Given a range [\c begin, \c end) of objects, run \c func on each \c *i in
-/// the range, and return a \c future<> containing a \c std::vector<> of values.
-/// In case any of the given tasks fails, one of the exceptions is returned by
-/// this function as a failed future.
+/// the range, and return a \c future<> containing a \c std::vector<> of
+/// values. In case any of the given tasks fails, one of the exceptions is
+/// returned by this function as a failed future.
 ///
 /// \param begin an \c InputIterator designating the beginning of the range
 /// \param end an \c InputIterator designating the end of the range
