7f98022deebbbe24e604bb322e4fec181c2e1b58
Rob Blafford <rob@vectorized.io>
2021-01-05 14:40:57 -0500

    coproc: pacemaker - new service for managing the coproc framework
    - The pacemaker manages registrations and deregistrations of all coproc scripts.
    
    - It holds references to currently active scripts and to actively registered
    ntps.
    
    - A mutex is used to protect these data structures from concurrent accesses.

---
+++ src/v/coproc/pacemaker.cc
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#include "coproc/pacemaker.h"
+
+#include "cluster/namespace.h"
+#include "coproc/logger.h"
+#include "coproc/types.h"
+#include "model/validation.h"
+#include "rpc/reconnect_transport.h"
+#include "rpc/types.h"
+#include "ssx/future-util.h"
+
+#include <absl/container/flat_hash_set.h>
+
+#include <algorithm>
+
+namespace coproc {
+
+rpc::transport_configuration
+wasm_transport_cfg(const ss::socket_address& addr) {
+    return rpc::transport_configuration{
+      .server_addr = addr,
+      .max_queued_bytes = static_cast<uint32_t>(
+        config::shard_local_cfg().coproc_max_inflight_bytes.value()),
+      .credentials = nullptr};
+}
+
+rpc::backoff_policy wasm_transport_backoff() {
+    using namespace std::chrono_literals;
+    return rpc::make_exponential_backoff_policy<rpc::clock_type>(1s, 10s);
+}
+
+pacemaker::pacemaker(ss::socket_address addr, ss::sharded<storage::api>& api)
+  : _shared_res(
+    rpc::reconnect_transport(
+      wasm_transport_cfg(addr), wasm_transport_backoff()),
+    api.local()) {}
+
+ss::future<> pacemaker::stop() {
+    std::vector<script_id> ids;
+    ids.reserve(_scripts.size());
+    for (const auto& [id, _] : _scripts) {
+        ids.push_back(id);
+    }
+    return ss::do_with(
+             std::move(ids),
+             [this](const std::vector<script_id>& ids) {
+                 return ssx::async_all_of(
+                   ids.begin(), ids.end(), [this](script_id id) {
+                       return remove_source(id).then(
+                         xform::equal_to(errc::success));
+                   });
+             })
+      .then([this](bool success) {
+          if (!success) {
+              vlog(
+                coproclog.error,
+                "Failed to gracefully shutdown all copro fibers");
+          }
+          vlog(coproclog.info, "Closing connection to coproc wasm engine");
+          return _shared_res.transport.stop();
+      });
+}
+
+std::vector<errc> pacemaker::add_source(
+  script_id id, std::vector<topic_namespace_policy> topics) {
+    ntp_context_cache ctxs;
+    std::vector<errc> acks;
+    vassert(
+      _scripts.find(id) == _scripts.end(),
+      "add_source() detects already existing script_id: {}",
+      id);
+    do_add_source(id, ctxs, acks, topics);
+    if (ctxs.empty()) {
+        /// Failure occurred or no matching topics/ntps found
+        /// Reasons are returned in the 'acks' structure
+        return acks;
+    }
+    const auto& [itr, success] = _scripts.emplace(
+      std::piecewise_construct,
+      std::forward_as_tuple(id),
+      std::forward_as_tuple(id, _shared_res, std::move(ctxs)));
+    vassert(success, "Double coproc insert detected");
+    (void)itr->second.start();
+    return acks;
+}
+
+errc check_topic_policy(const model::topic& topic, topic_ingestion_policy tip) {
+    if (tip != topic_ingestion_policy::latest) {
+        return errc::invalid_ingestion_policy;
+    }
+    if (model::is_materialized_topic(topic)) {
+        return errc::materialized_topic;
+    }
+    if (model::validate_kafka_topic_name(topic).value() != 0) {
+        return errc::invalid_topic;
+    }
+    return errc::success;
+}
+
+void pacemaker::do_add_source(
+  script_id id,
+  ntp_context_cache& ctxs,
+  std::vector<errc>& acks,
+  const std::vector<topic_namespace_policy>& topics) {
+    for (const topic_namespace_policy& tnp : topics) {
+        const errc r = check_topic_policy(tnp.tn.tp, tnp.policy);
+        if (r != errc::success) {
+            acks.push_back(r);
+            continue;
+        }
+        auto logs = _shared_res.api.log_mgr().get(tnp.tn);
+        if (logs.empty()) {
+            acks.push_back(errc::topic_does_not_exist);
+            continue;
+        }
+        for (auto& [ntp, log] : logs) {
+            auto found = _ntps.find(ntp);
+            ss::lw_shared_ptr<ntp_context> ntp_ctx;
+            if (found != _ntps.end()) {
+                ntp_ctx = found->second;
+            } else {
+                ntp_ctx = ss::make_lw_shared<ntp_context>(log);
+                _ntps.emplace(ntp, ntp_ctx);
+            }
+            vassert(
+              ntp_ctx->offsets.emplace(id, ntp_context::offset_pair()).second,
+              "Script id expected to not exist: {}, id");
+            ctxs.emplace(ntp, ntp_ctx);
+        }
+        acks.push_back(errc::success);
+    }
+}
+
+ss::future<errc> pacemaker::remove_source(script_id id) {
+    auto found = _scripts.find(id);
+    if (found == _scripts.end()) {
+        return ss::make_ready_future<errc>(errc::script_id_does_not_exist);
+    }
+    script_context& ctx = found->second;
+    return ctx.shutdown()
+      .then([this] {
+          /// shutdown explicity clears out strong references to ntp
+          /// contexts. It is known to remove them from the pacemakers cache
+          /// when the use_count() == 1, as there are then known to be no
+          /// more subscribing scripts for the ntp
+          absl::erase_if(_ntps, [](const ntp_context_cache::value_type& p) {
+              return p.second.use_count() == 1;
+          });
+      })
+      .then([this, found] {
+          /// Finally, remove the script_context itself
+          _scripts.erase(found);
+          return errc::success;
+      });
+}
+
+bool pacemaker::local_script_id_exists(script_id id) {
+    return _scripts.find(id) != _scripts.end();
+}
+
+bool pacemaker::ntp_is_registered(model::ntp ntp) {
+    auto found = _ntps.find(ntp);
+    return found != _ntps.end() && found->second;
+}
+
+} // namespace coproc
---
+++ src/v/coproc/pacemaker.h
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "coproc/ntp_context.h"
+#include "coproc/script_context.h"
+#include "coproc/types.h"
+#include "rpc/reconnect_transport.h"
+#include "storage/api.h"
+
+#include <seastar/core/sharded.hh>
+#include <seastar/net/inet_address.hh>
+#include <seastar/net/socket_defs.hh>
+
+#include <map>
+
+namespace coproc {
+/**
+ * Sharded service that manages the registration of scripts to registered
+ * topics. Each script is its own 'fiber', containing its own event loop which
+ * reads data from its interested topics, sends to the wasm engine, and
+ * processes the response (almost always a write to a materialized log)
+ */
+class pacemaker {
+public:
+    /**
+     * class constructor
+     * @param address of coprocessor engine
+     * @param reference to the storage layer
+     */
+    pacemaker(ss::socket_address, ss::sharded<storage::api>&);
+
+    /**
+     * Gracefully stops and deregisters all coproc scripts
+     */
+    ss::future<> stop();
+
+    /**
+     * Registers a new coproc script into the system
+     *
+     * @param script_id You must verify that this is unique by querying all
+     shards. A shard only contains an entry for a script id if there is at least
+     one partition of a topic for that shard.
+     * @param topics vector of pairs of topics and their registration policies
+     * @returns a vector of error codes per input topic corresponding to the
+     * order of the topic vector passed to this method
+     */
+    std::vector<errc>
+      add_source(script_id, std::vector<topic_namespace_policy>);
+
+    /**
+     * Removes a script_id from the pacemaker. Shuts down relevent fibers and
+     * deregisters ntps that no longer have any registered interested scripts
+     *
+     * @param script_id The identifer of the script desired to be shutdown
+     */
+    ss::future<errc> remove_source(script_id);
+
+    /**
+     * @returns true if a matching script id exists on 'this' shard
+     */
+    bool local_script_id_exists(script_id);
+
+    /**
+     * @returns true if a matching ntp exists on 'this' shard
+     */
+    bool ntp_is_registered(model::ntp);
+
+private:
+    void do_add_source(
+      script_id,
+      ntp_context_cache&,
+      std::vector<errc>& acks,
+      const std::vector<topic_namespace_policy>&);
+
+private:
+    /// Data to be referenced by script_contexts on the current shard
+    shared_script_resources _shared_res;
+
+    /// Main datastructure containing all active script_contexts
+    std::map<script_id, script_context> _scripts;
+
+    /// Referencable cache of active ntps
+    ntp_context_cache _ntps;
+};
+
+} // namespace coproc
