2ce66f6f73b67be14fa6c9f385a1af4bdbb9d226
Ivo Jimenez <ivo@vectorized.io>
2021-01-05 23:49:39 -0700

    rptest: create python package
    Create python package so that it can be installed inside test node
    container images. Specific version of dependencies are pinned.

---
+++ tests/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='rptest',
+    version='0',
+    provides=['rptest'],
+    author='vectorized',
+    author_email='hi@vectorized.io',
+    description='vectorized internal testing',
+    packages=find_packages(),
+    setup_requires=['setuptools'],
+    package_data={'': ['*.md']},
+    include_package_data=True,
+    install_requires=[
+        'ducktape==0.8.0', 'prometheus-client==0.9.0', 'pyyaml==5.3.1'
+    ],
+    scripts=[],
+)
