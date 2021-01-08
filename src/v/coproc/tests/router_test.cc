cd9e367f78999d42b6ae59da713d312b24e7010b
Rob Blafford <rob@vectorized.io>
2021-01-07 20:05:02 -0500

    coproc/tests: Add missing or fix inconsistent BSL license headers

--- src/v/coproc/tests/router_test.cc
+++ src/v/coproc/tests/router_test.cc
@@ -1,10 +1,12 @@
-// Copyright 2020 Vectorized, Inc.
-//
-// Licensed as a Redpanda Enterprise file under the Redpanda Community
-// License (the "License"); you may not use this file except in compliance with
-// the License. You may obtain a copy of the License at
-//
-// https://github.com/vectorizedio/redpanda/blob/master/licenses/rcl.md
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/vectorizedio/redpanda/blob/master/licenses/rcl.md
+ */
 
 #include "coproc/tests/utils/coprocessor.h"
 #include "coproc/tests/utils/router_test_fixture.h"
