5db6e788d337dd68cf0a612a611225d4b06a8816
Rob Blafford <rob@vectorized.io>
2021-01-07 19:59:29 -0500

    coproc/tests: Removing unused methods

--- src/v/coproc/tests/utils/utils.cc
+++ src/v/coproc/tests/utils/utils.cc
@@ -9,74 +9,17 @@
 #include "coproc/tests/utils/utils.h"
 
 #include "model/record_batch_reader.h"
-#include "storage/log.h"
-#include "storage/log_appender.h"
 
-template<typename itr_start, typename itr_end, typename func>
-std::size_t sum_records(const itr_start begin, const itr_end end, func f) {
-    return std::accumulate(
-      begin,
-      end,
-      static_cast<std::size_t>(0),
-      [&f](std::size_t acc, const auto& rb) { return acc + f(rb); });
-}
-
-std::size_t sum_records(const std::vector<model::record_batch>& batches) {
-    return sum_records(
-      batches.cbegin(), batches.cend(), [](const model::record_batch& rb) {
-          return rb.record_count();
-      });
-}
+#include <algorithm>
 
 std::size_t sum_records(const model::record_batch_reader::data_t& batches) {
-    return sum_records(
-      batches.cbegin(), batches.cend(), [](const model::record_batch& rb) {
-          return rb.record_count();
-      });
-}
-
-std::size_t
-sum_records(const std::vector<model::record_batch_reader::data_t>& batches) {
-    return sum_records(
+    return std::accumulate(
       batches.cbegin(),
       batches.cend(),
-      [](const model::record_batch_reader::data_t& data) {
-          return sum_records(data);
+      static_cast<std::size_t>(0),
+      [](std::size_t acc, const model::record_batch& rb) {
+          return acc + rb.record_count();
       });
-}
-
-// to_topic_set
-absl::flat_hash_set<model::topic>
-to_topic_set(std::vector<ss::sstring>&& topics) {
-    absl::flat_hash_set<model::topic> topic_set;
-    std::transform(
-      topics.begin(),
-      topics.end(),
-      std::inserter(topic_set, topic_set.begin()),
-      [](const ss::sstring& t) { return model::topic(t); });
-    return topic_set;
-}
-
-// to_topic_vector
-std::vector<model::topic> to_topic_vector(std::vector<ss::sstring>&& topics) {
-    std::vector<model::topic> topic_vec;
-    std::transform(
-      topics.begin(),
-      topics.end(),
-      std::back_inserter(topic_vec),
-      [](const ss::sstring& t) { return model::topic(t); });
-    return topic_vec;
-}
-
-absl::flat_hash_set<model::ntp>
-to_ntps(absl::flat_hash_map<model::topic_namespace, std::size_t>&& mp) {
-    absl::flat_hash_set<model::ntp> r;
-    for (auto&& e : mp) {
-        for (std::size_t n = 0; n < e.second; ++n) {
-            r.emplace(e.first.ns, e.first.tp, model::partition_id(n));
-        }
-    }
-    return r;
 }
 
 coproc::enable_copros_request::data make_enable_req(
--- src/v/coproc/tests/utils/utils.h
+++ src/v/coproc/tests/utils/utils.h
@@ -14,17 +14,12 @@
 #include "model/fundamental.h"
 #include "model/metadata.h"
 #include "model/namespace.h"
+#include "model/record_batch_reader.h"
 #include "model/record_batch_types.h"
 #include "raft/types.h"
-#include "storage/log.h"
-#include "storage/record_batch_builder.h"
 #include "storage/types.h"
 
 #include <seastar/core/future.hh>
-#include <seastar/core/sharded.hh>
-
-#include <absl/container/flat_hash_map.h>
-#include <absl/container/flat_hash_set.h>
 
 inline static model::topic_namespace make_ts(ss::sstring&& topic) {
     return model::topic_namespace(model::kafka_namespace, model::topic(topic));
@@ -32,13 +27,6 @@ inline static model::topic_namespace make_ts(ss::sstring&& topic) {
 
 inline static model::topic_namespace make_ts(const model::topic& topic) {
     return model::topic_namespace(model::kafka_namespace, topic);
-}
-
-inline static storage::log_append_config log_app_cfg() {
-    return storage::log_append_config{
-      .should_fsync = storage::log_append_config::fsync::no,
-      .io_priority = ss::default_priority_class(),
-      .timeout = model::no_timeout};
 }
 
 inline static storage::log_reader_config
@@ -66,23 +54,8 @@ inline static storage::log_reader_config log_rdr_cfg(const size_t min_bytes) {
       std::nullopt);
 }
 
-/// \brief returns the number of records across all batches of batches
-std::size_t sum_records(const std::vector<model::record_batch_reader::data_t>&);
-
 /// \brief returns the number of records across all batches
 std::size_t sum_records(const model::record_batch_reader::data_t&);
-std::size_t sum_records(const std::vector<model::record_batch>&);
-
-/// \brief to_topic_set convert a vector of strings into a set of topics
-absl::flat_hash_set<model::topic> to_topic_set(std::vector<ss::sstring>&&);
-
-/// \brief to_topic_vector convert a vector of strings into a vector of
-/// topics
-std::vector<model::topic> to_topic_vector(std::vector<ss::sstring>&&);
-
-/// \brief convert a topic + size map to a set of ntps
-absl::flat_hash_set<model::ntp>
-to_ntps(absl::flat_hash_map<model::topic_namespace, std::size_t>&&);
 
 /// \brief short way to construct an enable_copros_request::data
 coproc::enable_copros_request::data make_enable_req(
