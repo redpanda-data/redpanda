4a76683defdf83968763707593f80ff929ee0975
Evgeny Lazin <evgeny@vectorized.io>
2021-01-19 16:34:35 +0300

    s3: Add missing license headers

--- src/v/s3/error.h
+++ src/v/s3/error.h
@@ -5,6 +5,15 @@
 
 #include <seastar/core/sstring.hh>
 
+// Copyright 2021 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
 #include <exception>
 #include <system_error>
 
