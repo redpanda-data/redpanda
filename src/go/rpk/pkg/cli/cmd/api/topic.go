689b167ac96f61a0141b8096686130d3fba17a1d
David Castillo <david@vectorized.io>
2021-01-21 15:29:52 -0500

    rpk/cli: Add 'rpk topic create', depr. 'rpk api topic create'

--- src/go/rpk/pkg/cli/cmd/api/topic.go
+++ src/go/rpk/pkg/cli/cmd/api/topic.go
@@ -21,6 +21,7 @@ import (
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/cli/cmd/common"
+	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/cli/cmd/topic"
 	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/cli/ui"
 	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/kafka"
 )
@@ -45,102 +46,10 @@ func NewTopicCommand(
 }
 
 func createTopic(admin func() (sarama.ClusterAdmin, error)) *cobra.Command {
-	var (
-		partitions	int32
-		replicas	int16
-		compact		bool
-		config		[]string
+	return common.Deprecated(
+		topic.NewCreateCommand(admin),
+		"rpk topic create",
 	)
-	cmd := &cobra.Command{
-		Use:	"create <topic name>",
-		Short:	"Create a topic",
-		Args:	common.ExactArgs(1, "topic's name is missing."),
-		// We don't want Cobra printing CLI usage help if the error isn't about CLI usage.
-		SilenceUsage:	true,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			configEntries, err := parseKVs(config)
-			if err != nil {
-				return err
-			}
-			if _, ok := configEntries["cleanup.policy"]; !ok {
-				cleanupPolicy := "delete"
-				if compact {
-					cleanupPolicy = "compact"
-				}
-				configEntries["cleanup.policy"] = &cleanupPolicy
-			}
-			adm, err := admin()
-			if err != nil {
-				log.Error("Couldn't initialize API admin")
-				return err
-			}
-			defer adm.Close()
-			topicName := args[0]
-			topicDetail := &sarama.TopicDetail{
-				NumPartitions:	partitions,
-				ConfigEntries:	configEntries,
-			}
-			if replicas > 0 {
-				topicDetail.ReplicationFactor = replicas
-			}
-			err = adm.CreateTopic(
-				topicName,
-				topicDetail,
-				false,
-			)
-			if err != nil {
-				return err
-			}
-			configList := []string{}
-			for k, v := range configEntries {
-				configList = append(
-					configList,
-					fmt.Sprintf("'%s':'%s'", k, *v),
-				)
-			}
-			sort.Strings(configList)
-			log.Infof(
-				"Created topic '%s'. Partitions: %d,"+
-					" replicas: %d, configuration:\n%s",
-				topicName,
-				partitions,
-				replicas,
-				strings.Join(configList, "\n"),
-			)
-			return nil
-		},
-	}
-	cmd.Flags().StringArrayVarP(
-		&config,
-		"config",
-		"c",
-		[]string{},
-		"Config entries in the format <key>:<value>. May be used multiple times"+
-			" to add more entries.",
-	)
-	cmd.Flags().Int32VarP(
-		&partitions,
-		"partitions",
-		"p",
-		int32(1),
-		"Number of partitions",
-	)
-	cmd.Flags().Int16VarP(
-		&replicas,
-		"replicas",
-		"r",
-		int16(-1),
-		"Replication factor. If it's negative or is left unspecified,"+
-			" it will use the cluster's default topic replication"+
-			" factor.",
-	)
-	cmd.Flags().BoolVar(
-		&compact,
-		"compact",
-		false,
-		"Enable topic compaction",
-	)
-	return cmd
 }
 
 func deleteTopic(admin func() (sarama.ClusterAdmin, error)) *cobra.Command {
---
+++ src/go/rpk/pkg/cli/cmd/topic.go
@@ -0,0 +1,66 @@
+// Copyright 2021 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package cmd
+
+import (
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/cli/cmd/common"
+	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/cli/cmd/topic"
+	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/config"
+)
+
+func NewTopicCommand(fs afero.Fs, mgr config.Manager) *cobra.Command {
+	var (
+		brokers		[]string
+		configFile	string
+	)
+	command := &cobra.Command{
+		Use:	"topic",
+		Short:	"Create, delete, produce to and consume from Redpanda topics.",
+	}
+
+	command.PersistentFlags().StringSliceVar(
+		&brokers,
+		"brokers",
+		[]string{},
+		"Comma-separated list of broker ip:port pairs",
+	)
+	command.PersistentFlags().StringVar(
+		&configFile,
+		"config",
+		"",
+		"Redpanda config file, if not set the file will be searched for"+
+			" in the default locations",
+	)
+
+	// The ideal way to pass common (global flags') values would be to
+	// declare PersistentPreRun hooks on each command root (such as rpk
+	// api), validating them there and them passing them down to its
+	// subcommands. However, Cobra only executes the last hook defined in
+	// the command chain. Since NewTopicCommand requires a PersistentPreRun
+	// hook to initialize the sarama Client and Admin, it overrides whatever
+	// PersistentPreRun hook was declared in a parent command.
+	// An alternative would be to declare a global var to hold the global
+	// flag's value, but this would require flattening out the package
+	// hierarchy to avoid import cycles (parent command imports child
+	// command's package, child cmd import parent cmd's package to access
+	// the flag's value), but this leads to entangled code.
+	// As a cleaner workaround, the flags are provided through a
+	// closure with references to the required values (the config file
+	// path, the list of brokers passed through --brokers).
+	configClosure := common.FindConfigFile(mgr, &configFile)
+	brokersClosure := common.DeduceBrokers(fs, configClosure, &brokers)
+	adminClosure := common.CreateAdmin(fs, brokersClosure, configClosure)
+
+	command.AddCommand(topic.NewCreateCommand(adminClosure))
+
+	return command
+}
