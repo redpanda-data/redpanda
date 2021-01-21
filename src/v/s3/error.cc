d4c370d177393e078851e40ec3b657af724488d1
Evgeny Lazin <evgeny@vectorized.io>
2021-01-21 21:55:26 +0300

    s3: change license headers

--- src/v/s3/error.cc
+++ src/v/s3/error.cc
@@ -1,11 +1,12 @@
-// Copyright 2021 Vectorized, Inc.
-//
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.md
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/vectorizedio/redpanda/blob/master/licenses/rcl.md
+ */
 
 #include "s3/error.h"
 
