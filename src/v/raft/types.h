7e6c761670de1122cb364ce3736bd0d3b1a62c15
Michal Maslanka <michal@vectorized.io>
2021-01-27 09:48:04 +0100

    r/tests: added last to metadata_key enum
    Added last field to enable iteration over `metadata_key` enum.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/raft/types.h
+++ src/v/raft/types.h
@@ -437,6 +437,7 @@ enum class metadata_key : int8_t {
     config_latest_known_offset = 2,
     last_applied_offset = 3,
     unique_local_id = 4,
+    last
 };
 
 // priority used to implement semi-deterministic leader election
