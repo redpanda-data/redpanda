0d4bfde82d1068937f932256a486770a8bb76481
Ben Pope <ben@vectorized.io>
2021-01-28 13:10:29 +0000

    k/protocol: Introduce an exception hierarchy
    Signed-off-by: Ben Pope <ben@vectorized.io>

---
+++ src/v/kafka/protocol/exceptions.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+#include "kafka/protocol/errors.h"
+
+#include <fmt/core.h>
+
+#include <exception>
+
+namespace kafka {
+
+struct exception_base : std::exception {
+    explicit exception_base(error_code err, std::string msg)
+      : std::exception{}
+      , error{err}
+      , msg{std::move(msg)} {}
+    const char* what() const noexcept final { return msg.c_str(); }
+    error_code error;
+    std::string msg;
+};
+
+struct exception final : exception_base {
+    explicit exception(error_code err, std::string msg)
+      : exception_base{err, std::move(msg)} {}
+};
+
+} // namespace kafka
