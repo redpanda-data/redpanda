44d3fd85be012ec87d5cacab14ac5f3f554c15c4
Michal Maslanka <michal@vectorized.io>
2021-01-29 15:04:20 +0100

    rpc/dns: implemented simple and safe resolve_dns method
    The dns resolver included in seastar caused redpanda to crash in K8S
    installations. The issue is related with concurrent access to file
    descriptors and incorrect error handling. In order to quickly fix the
    issue, implemented simple class providing mutually exclusive DNS
    lookup. This eliminates the concurrency problems and segmentation
    fault.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/rpc/dns.cc
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#include "rpc/dns.h"
+
+#include "utils/mutex.h"
+#include "utils/unresolved_address.h"
+
+#include <seastar/core/coroutine.hh>
+#include <seastar/net/dns.hh>
+
+namespace rpc {
+ss::future<ss::socket_address> resolve_dns(unresolved_address address) {
+    static thread_local ss::net::dns_resolver resolver;
+    static thread_local mutex m;
+    // lock
+    auto units = co_await m.get_units();
+    // resolve
+    auto i_a = co_await resolver.resolve_name(address.host(), {});
+
+    co_return ss::socket_address(i_a, address.port());
+};
+} // namespace rpc
---
+++ src/v/rpc/dns.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+#include "utils/unresolved_address.h"
+namespace rpc {
+/**
+ * Resolves addresses using seastar DNS resolver. It uses mutex to workaround
+ * seastar bug causing segmentation fault when udp channel is being accessed by
+ * different fibers.
+ */
+ss::future<ss::socket_address> resolve_dns(unresolved_address);
+} // namespace rpc
