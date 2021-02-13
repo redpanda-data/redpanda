b77a601a70fe720dc0718b28f9c0a70f5fc35503
Dan McHugh <hi@danmchugh.co.uk>
2021-02-12 18:34:40 -0600

    Dashboard: connect to redpanda prometheus data - Replace test data with real stream data - Fix for relative asset paths in package.json - Added Timewindow selector

--- src/v/dashboard/src/hooks/useInterval.ts
+++ src/v/dashboard/src/hooks/useInterval.ts
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from "react";
 
-// Default interval from promethus snapshots - check prometheus output + choose names
-const initialInterval = 2500;
+// Time Converters
+const intervalFromTimeWindow = (timeWindow: number, limit: number) =>
+  timeWindow / limit;
+const timeWindowFromInterval = (interval: number, limit: number) =>
+  interval * limit;
+
+// Initial Values - all time is in Milliseconds (ms)
+const initialCount: number = 0;
+const initialLimit: number = 500;
+const initialTimeWindow: number = 1000 * 60 * 15; // 15 minutes
+const initialInterval: number = intervalFromTimeWindow(
+  initialTimeWindow,
+  initialLimit
+);
+
+// User set defaults
+const defaultInterval = localStorage.getItem("defaultInterval");
 
 const useInterval = () => {
-  const defaultInterval = localStorage.getItem("defaultInterval");
+  const [count, setCount] = useState(initialCount);
+  const [limit, setLimit] = useState(initialLimit);
+
   const [interval, setInterval] = useState(
     defaultInterval ? parseInt(JSON.parse(defaultInterval)) : initialInterval
   );
-  const [count, setCount] = useState(0);
+
+  const [timeWindow, setTimeWindow] = useState(
+    timeWindowFromInterval(interval, limit)
+  );
 
   const updateInterval = (data: number) => {
     setInterval(data);
@@ -16,7 +36,15 @@ const useInterval = () => {
   };
 
   const changeInterval = (event: any) => {
-    updateInterval(parseInt(event?.target?.value));
+    const interval = parseInt(event?.target?.value);
+    setTimeWindow(timeWindowFromInterval(interval, limit));
+    updateInterval(interval);
+  };
+
+  const changeTimeWindow = (event: any) => {
+    const timeWindow = parseInt(event?.target?.value);
+    setTimeWindow(timeWindow);
+    updateInterval(intervalFromTimeWindow(timeWindow, limit));
   };
 
   const resetInterval = () => {
@@ -26,15 +54,22 @@ const useInterval = () => {
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), interval);
     return () => clearTimeout(timer);
-  }, [count, setCount]);
+  }, [interval, count, setCount]);
 
   return {
+    limit,
+    setLimit,
+
     count,
     setCount,
+
     interval,
     setInterval,
     changeInterval,
     resetInterval,
+
+    timeWindow,
+    changeTimeWindow,
   };
 };
 
--- src/v/dashboard/src/hooks/useSnapshots.ts
+++ src/v/dashboard/src/hooks/useSnapshots.ts
@@ -1,28 +1,19 @@
 import React, { useState, useEffect } from "react";
 
-// README
-// When ready to use live data find NOTE 0, NOTE 1, NOTE 2 below.
-
-// Test data
-import testSnapshot from "../prometheus-test-snapshot";
-
 // Libs
 // @ts-ignore
 import parsePrometheusTextFormat from "parse-prometheus-text-format";
 
 // Utils
 import { Metric, Snapshot } from "../types";
-import fetchPrometheusSnapshot from "../utils/fetchPrometheusSnapshot";
 import getRandomInt from "utils/getRandomInt";
 
 const useSnapshots = (
+  limit: number,
   count: number,
   setCount: (count: number) => void,
-  useTestData: boolean = true // NOTE 0 - change this to false
+  useTestData: boolean = false // NOTE 0 - change this to false
 ) => {
-  // Limit the amount of snapshots that the browser retains in memory
-  const limit = 500;
-
   // Snapshot data
   const [snapshots, setSnapshots] = useState<any[]>([]);
 
@@ -45,22 +36,20 @@ const useSnapshots = (
   });
 
   const updateSnapshots = () => {
-    // NOTE 1 - Enable this
-    // let parsedSnapshot = undefined
-    // fetchSnapshotTest()
-    //   .then(response => response.text())
-    //   .then(snapshots => { parsedSnapshot = snapshots })
-    //   .catch(error => console.log(error))
+    fetch(`/metrics`)
+      .then((response) => response.text())
+      .then((data) => {
+        // Parse the data
+        const parsedSnapshot = parsePrometheusTextFormat(data);
+        // Append new snapshot + add timestamp
+        const existingSnapshots =
+          snapshots.length < limit
+            ? snapshots
+            : snapshots.filter((item, i) => i !== 0);
 
-    // NOTE 2 - Disable this
-    const parsedSnapshot = parsePrometheusTextFormat(testSnapshot);
-
-    // Append new snapshot + add timestamp
-    const existingSnapshots =
-      snapshots.length < limit
-        ? snapshots
-        : snapshots.filter((item, i) => i !== 0);
-    setSnapshots([...existingSnapshots, addTimestamp(parsedSnapshot)]);
+        setSnapshots([...existingSnapshots, addTimestamp(parsedSnapshot)]);
+      })
+      .catch((error) => console.log(error));
   };
 
   // Start snapshot fetching
