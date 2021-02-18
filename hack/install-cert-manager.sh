dbe5852f6b97aca13e9a8fb0bc4ca24d60f7017f
Alena Varkockova <varkockova.a@gmail.com>
2021-02-18 13:42:57 -0500

    (split) operator: Enable webhooks in config and test

---
+++ hack/install-cert-manager.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+VERSION="0.1.0"
+
+# TODO: support more OS/architectures as they are needed
+if [ "$(uname)" == 'Darwin' ]; then
+  curl -L https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${VERSION}"/cert-manager-verifier_"${VERSION}"_Darwin_x86_64.tar.gz | tar -xvf -
+else
+  curl -L https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${VERSION}"/cert-manager-verifier_"${VERSION}"_Linux_x86_64.tar.gz | tar -xzvf -
+fi
+
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
+./cm-verifier
---
+++ hack/wait-for-webhook-ready.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# This script tries to create sample cluster and waits until it succeeds
+# this way we are able to wait until webhook is ready to serve our content
+
+if ! command -v kubectl &>/dev/null; then
+  echo "required kubectl command NOT found"
+  exit 1
+fi
+
+MAX=50
+CURRENT=0
+
+until $(kubectl create -f ./config/samples/redpanda_v1alpha1_cluster.yaml >/dev/null 2>&1); do
+  CURRENT=$((CURRENT + 1))
+  printf '.'
+  sleep 5
+
+  if [ "$CURRENT" -gt "$MAX" ]; then
+    echo "FAILED: Webhook not ready, giving up."
+    exit 1
+  fi
+done
+
+echo "Webhook is ready!"
+
+kubectl delete -f ./config/samples/redpanda_v1alpha1_cluster.yaml
