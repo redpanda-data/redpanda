5ff166bb12d7a4f3ea43f5921955e0ec6014b620
Ben Pope <ben@vectorized.io>
2021-03-18 20:08:20 +0000

    utils: Use fewer test binaries
    * Improve build times
    * Renamed the tests to be more descriptive
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/utils/tests/named_type_tests.cc
+++ src/v/utils/tests/named_type_tests.cc
@@ -7,8 +7,6 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
-#define BOOST_TEST_MODULE utils
-
 #include "seastarx.h"
 #include "utils/named_type.h"
 
