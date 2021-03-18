f35d67768ad25362d3300dcc72dcf94fdeb1314e
Noah Watkins <noah@vectorized.io>
2021-03-18 12:10:02 -0700

    kafka: convert leave group handler to coroutines
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/kafka/server/handlers/leave_group.cc
+++ src/v/kafka/server/handlers/leave_group.cc
@@ -16,6 +16,7 @@
 #include "utils/remote.h"
 #include "utils/to_string.h"
 
+#include <seastar/core/coroutine.hh>
 #include <seastar/core/print.hh>
 
 namespace kafka {
@@ -27,17 +28,10 @@ void leave_group_response::encode(const request_context& ctx, response& resp) {
 template<>
 ss::future<response_ptr> leave_group_handler::handle(
   request_context ctx, [[maybe_unused]] ss::smp_service_group g) {
-    return ss::do_with(
-      remote(std::move(ctx)), [](remote<request_context>& remote_ctx) {
-          auto& ctx = remote_ctx.get();
-          leave_group_request request;
-          request.decode(ctx.reader(), ctx.header().version);
-          return ctx.groups()
-            .leave_group(std::move(request))
-            .then([&ctx](leave_group_response&& reply) {
-                return ctx.respond(std::move(reply));
-            });
-      });
+    leave_group_request request;
+    request.decode(ctx.reader(), ctx.header().version);
+    auto resp = co_await ctx.groups().leave_group(std::move(request));
+    co_return co_await ctx.respond(std::move(resp));
 }
 
 } // namespace kafka
