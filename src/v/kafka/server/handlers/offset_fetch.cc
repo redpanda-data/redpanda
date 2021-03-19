565512337f3b9dd7d21d768f922fd59f3c739e72
Noah Watkins <noah@vectorized.io>
2021-03-18 18:35:58 -0700

    kafka: convert offset fetch handler to coroutines
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/kafka/server/handlers/offset_fetch.cc
+++ src/v/kafka/server/handlers/offset_fetch.cc
@@ -16,6 +16,7 @@
 #include "kafka/server/response.h"
 #include "model/metadata.h"
 
+#include <seastar/core/coroutine.hh>
 #include <seastar/core/do_with.hh>
 #include <seastar/core/smp.hh>
 #include <seastar/util/log.hh>
@@ -40,35 +41,15 @@ void offset_fetch_response::encode(const request_context& ctx, response& resp) {
     data.encode(resp.writer(), ctx.header().version);
 }
 
-struct offset_fetch_ctx {
-    request_context rctx;
-    offset_fetch_request request;
-    ss::smp_service_group ssg;
-
-    offset_fetch_ctx(
-      request_context&& rctx,
-      offset_fetch_request&& request,
-      ss::smp_service_group ssg)
-      : rctx(std::move(rctx))
-      , request(std::move(request))
-      , ssg(ssg) {}
-};
-
 template<>
 ss::future<response_ptr>
-offset_fetch_handler::handle(request_context ctx, ss::smp_service_group ssg) {
+offset_fetch_handler::handle(request_context ctx, ss::smp_service_group) {
     offset_fetch_request request;
     request.decode(ctx.reader(), ctx.header().version);
     klog.trace("Handling request {}", request);
-    return ss::do_with(
-      offset_fetch_ctx(std::move(ctx), std::move(request), ssg),
-      [](offset_fetch_ctx& octx) {
-          return octx.rctx.groups()
-            .offset_fetch(std::move(octx.request))
-            .then([&octx](offset_fetch_response resp) {
-                return octx.rctx.respond(std::move(resp));
-            });
-      });
+
+    auto resp = co_await ctx.groups().offset_fetch(std::move(request));
+    co_return co_await ctx.respond(std::move(resp));
 }
 
 } // namespace kafka
