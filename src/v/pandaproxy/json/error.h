54b70ad15a38d401741ad43f4a3e68b6f7d72ec2
Ben Pope <ben@vectorized.io>
2021-03-19 02:52:34 +0000

    pp/json: Introduce error_code
    The set of error_codes reported in the json body
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

---
+++ src/v/pandaproxy/json/error.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <system_error>
+
+namespace pandaproxy::json {
+
+enum class error_code : uint16_t {
+    // 0 is success
+    invalid_json = 1,
+};
+
+std::error_code make_error_code(error_code);
+
+} // namespace pandaproxy::json
+
+namespace std {
+
+template<>
+struct is_error_code_enum<pandaproxy::json::error_code> : true_type {};
+
+} // namespace std
