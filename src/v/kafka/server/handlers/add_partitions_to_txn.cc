74bece10f3b9ec8f87e9242c13ee6600b275fa88
Denis Rystsov <denis@vectorized.io>
2021-03-20 00:56:50 -0700

    kafka: connect add_partitions_to_txn with tx logic

--- src/v/kafka/server/handlers/add_partitions_to_txn.cc
+++ src/v/kafka/server/handlers/add_partitions_to_txn.cc
@@ -10,6 +10,7 @@
 #include "kafka/server/handlers/add_partitions_to_txn.h"
 
 #include "cluster/topics_frontend.h"
+#include "cluster/tx_gateway_frontend.h"
 #include "kafka/server/group_manager.h"
 #include "kafka/server/group_router.h"
 #include "kafka/server/logger.h"
@@ -28,8 +29,47 @@ ss::future<response_ptr> add_partitions_to_txn_handler::handle(
     return ss::do_with(std::move(ctx), [](request_context& ctx) {
         add_partitions_to_txn_request request;
         request.decode(ctx.reader(), ctx.header().version);
-        return ss::make_exception_future<response_ptr>(std::runtime_error(
-          fmt::format("Unsupported API {}", ctx.header().key)));
+
+        cluster::add_paritions_tx_request tx_request{
+          .transactional_id = request.data.transactional_id,
+          .producer_id = request.data.producer_id,
+          .producer_epoch = request.data.producer_epoch};
+        tx_request.topics.reserve(request.data.topics.size());
+        for (auto topic : request.data.topics) {
+            cluster::add_paritions_tx_request::topic tx_topic{
+              .name = topic.name, .partitions = topic.partitions};
+            tx_request.topics.push_back(tx_topic);
+        }
+
+        return ctx.tx_gateway_frontend()
+          .add_partition_to_tx(
+            tx_request, config::shard_local_cfg().create_topic_timeout_ms())
+          .then([&ctx](cluster::add_paritions_tx_reply tx_response) {
+              add_partitions_to_txn_response_data data;
+              for (auto tx_topic : tx_response.results) {
+                  add_partitions_to_txn_topic_result topic{
+                    .name = tx_topic.name};
+                  for (auto tx_partition : tx_topic.results) {
+                      add_partitions_to_txn_partition_result partition{
+                        .partition_index = tx_partition.partition_index};
+                      switch (tx_partition.error_code) {
+                      case cluster::tx_errc::none:
+                          partition.error_code = error_code::none;
+                          break;
+                      default:
+                          partition.error_code
+                            = error_code::unknown_server_error;
+                          break;
+                      }
+                      topic.results.push_back(std::move(partition));
+                  }
+                  data.results.push_back(std::move(topic));
+              }
+
+              add_partitions_to_txn_response response;
+              response.data = data;
+              return ctx.respond(std::move(response));
+          });
     });
 }
 
