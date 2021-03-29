e98ec51dce085fe040328153b689f5278accc90f
Ben Pope <ben@vectorized.io>
2021-03-30 00:09:13 +0100

    pp/parsing: Introduce http status to error_code
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/parsing/error.h
+++ src/v/pandaproxy/parsing/error.h
@@ -20,6 +20,8 @@ enum class error_code : uint16_t {
     // ok = 0
     empty_param = 100,
     invalid_param = 101,
+    not_acceptable = 406,
+    unsupported_media_type = 415,
 };
 
 std::error_code make_error_code(error_code);
