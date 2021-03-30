d587c4d59aecfb28e94c3fdfdf898e67f072eb9b
Noah Watkins <noah@vectorized.io>
2021-03-29 18:26:23 -0700

    redpanda: add admin api for creating user
    Allows for posting username and scram credentials to /v1/security/users
    to create new users.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/redpanda/application.h
+++ src/v/redpanda/application.h
@@ -92,6 +92,7 @@ private:
 
     void admin_register_raft_routes(ss::http_server& server);
     void admin_register_kafka_routes(ss::http_server& server);
+    void admin_register_security_routes(ss::http_server& server);
 
     bool coproc_enabled() {
         const auto& cfg = config::shard_local_cfg();
