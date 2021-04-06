adf8f9a8965169fd05b3f81d005d59bf8723f8d3
Dimitris Skourtis <skourtis@vectorized.io>
2021-04-06 16:30:13 -0700

    operator/apis: kafka api becomes a list
    this is an intermediate change. only works with a single item, as before

--- src/go/k8s/apis/redpanda/v1alpha1/cluster_webhook.go
+++ src/go/k8s/apis/redpanda/v1alpha1/cluster_webhook.go
@@ -195,18 +195,22 @@ func (r *Cluster) ValidateDelete() error {
 func (r *Cluster) checkCollidingPorts() field.ErrorList {
 	var allErrs field.ErrorList
 
-	if r.Spec.Configuration.AdminAPI.Port == r.Spec.Configuration.KafkaAPI.Port {
-		allErrs = append(allErrs,
-			field.Invalid(field.NewPath("spec").Child("configuration", "admin", "port"),
-				r.Spec.Configuration.AdminAPI.Port,
-				"admin port collide with Spec.Configuration.KafkaAPI.Port"))
+	for _, kafka := range r.Spec.Configuration.KafkaAPI {
+		if r.Spec.Configuration.AdminAPI.Port == kafka.Port {
+			allErrs = append(allErrs,
+				field.Invalid(field.NewPath("spec").Child("configuration", "admin", "port"),
+					r.Spec.Configuration.AdminAPI.Port,
+					"admin port collide with Spec.Configuration.KafkaAPI Port"))
+		}
 	}
 
-	if r.Spec.Configuration.RPCServer.Port == r.Spec.Configuration.KafkaAPI.Port {
-		allErrs = append(allErrs,
-			field.Invalid(field.NewPath("spec").Child("configuration", "rpcServer", "port"),
-				r.Spec.Configuration.RPCServer.Port,
-				"rpc port collide with Spec.Configuration.KafkaAPI.Port"))
+	for _, kafka := range r.Spec.Configuration.KafkaAPI {
+		if r.Spec.Configuration.RPCServer.Port == kafka.Port {
+			allErrs = append(allErrs,
+				field.Invalid(field.NewPath("spec").Child("configuration", "rpcServer", "port"),
+					r.Spec.Configuration.RPCServer.Port,
+					"rpc port collide with Spec.Configuration.KafkaAPI Port"))
+		}
 	}
 
 	if r.Spec.Configuration.AdminAPI.Port == r.Spec.Configuration.RPCServer.Port {
@@ -216,18 +220,22 @@ func (r *Cluster) checkCollidingPorts() field.ErrorList {
 				"admin port collide with Spec.Configuration.RPCServer.Port"))
 	}
 
-	if r.Spec.ExternalConnectivity.Enabled && r.Spec.Configuration.KafkaAPI.Port+1 == r.Spec.Configuration.RPCServer.Port {
-		allErrs = append(allErrs,
-			field.Invalid(field.NewPath("spec").Child("configuration", "rpcServer", "port"),
-				r.Spec.Configuration.RPCServer.Port,
-				"rpc port collide with external Kafka API that is not visible in the Cluster CR"))
+	for _, kafka := range r.Spec.Configuration.KafkaAPI {
+		if r.Spec.ExternalConnectivity.Enabled && kafka.Port+1 == r.Spec.Configuration.RPCServer.Port {
+			allErrs = append(allErrs,
+				field.Invalid(field.NewPath("spec").Child("configuration", "rpcServer", "port"),
+					r.Spec.Configuration.RPCServer.Port,
+					"rpc port collide with external Kafka API that is not visible in the Cluster CR"))
+		}
 	}
 
-	if r.Spec.ExternalConnectivity.Enabled && r.Spec.Configuration.KafkaAPI.Port+1 == r.Spec.Configuration.AdminAPI.Port {
-		allErrs = append(allErrs,
-			field.Invalid(field.NewPath("spec").Child("configuration", "admin", "port"),
-				r.Spec.Configuration.AdminAPI.Port,
-				"admin port collide with external Kafka API that is not visible in the Cluster CR"))
+	for _, kafka := range r.Spec.Configuration.KafkaAPI {
+		if r.Spec.ExternalConnectivity.Enabled && kafka.Port+1 == r.Spec.Configuration.AdminAPI.Port {
+			allErrs = append(allErrs,
+				field.Invalid(field.NewPath("spec").Child("configuration", "admin", "port"),
+					r.Spec.Configuration.AdminAPI.Port,
+					"admin port collide with external Kafka API that is not visible in the Cluster CR"))
+		}
 	}
 
 	if r.Spec.ExternalConnectivity.Enabled && r.Spec.Configuration.AdminAPI.Port+1 == r.Spec.Configuration.RPCServer.Port {
@@ -237,11 +245,13 @@ func (r *Cluster) checkCollidingPorts() field.ErrorList {
 				"rpc port collides with external Admin API port that is not visible in the Cluster CR"))
 	}
 
-	if r.Spec.ExternalConnectivity.Enabled && r.Spec.Configuration.AdminAPI.Port+1 == r.Spec.Configuration.KafkaAPI.Port {
-		allErrs = append(allErrs,
-			field.Invalid(field.NewPath("spec").Child("configuration", "kafka", "port"),
-				r.Spec.Configuration.KafkaAPI.Port,
-				"kafka port collides with external Admin API port that is not visible in the Cluster CR"))
+	for _, kafka := range r.Spec.Configuration.KafkaAPI {
+		if r.Spec.ExternalConnectivity.Enabled && r.Spec.Configuration.AdminAPI.Port+1 == kafka.Port+1 {
+			allErrs = append(allErrs,
+				field.Invalid(field.NewPath("spec").Child("configuration", "kafka", "port"),
+					kafka.Port,
+					"kafka port collides with external Admin API port that is not visible in the Cluster CR"))
+		}
 	}
 
 	return allErrs
--- src/go/k8s/controllers/redpanda/cluster_controller.go
+++ src/go/k8s/controllers/redpanda/cluster_controller.go
@@ -90,13 +90,18 @@ func (r *ClusterReconciler) Reconcile(
 
 	headlessPorts := []resources.NamedServicePort{
 		{Name: resources.AdminPortName, Port: redpandaCluster.Spec.Configuration.AdminAPI.Port},
-		{Name: resources.KafkaPortName, Port: redpandaCluster.Spec.Configuration.KafkaAPI.Port},
 	}
 	nodeports := []resources.NamedServicePort{
 		{Name: resources.AdminPortName, Port: redpandaCluster.Spec.Configuration.AdminAPI.Port + 1},
 		{Name: resources.KafkaPortName, Port: redpandaCluster.Spec.Configuration.KafkaAPI.Port + 1},
 	}
-	headlessSvc := resources.NewHeadlessService(r.Client, &redpandaCluster, r.Scheme, headlessPorts, log)
+	for _, listener := range redpandaCluster.Spec.Configuration.KafkaAPI {
+		if redpandaCluster.Spec.ExternalConnectivity.Enabled {
+			nodeports = append(nodeports, resources.NamedServicePort{Name: listener.Name, Port: listener.Port + 1})
+		}
+		headlessports = append(headlessports, resources.NamedServicePort{Name: listener.Name, Port: listener.Port})
+	}
+	headlessSvc := resources.NewHeadlessService(r.Client, &redpandaCluster, r.Scheme, headlessports, log)
 	nodeportSvc := resources.NewNodePortService(r.Client, &redpandaCluster, r.Scheme, nodeports, log)
 
 	pki := certmanager.NewPki(r.Client, &redpandaCluster, headlessSvc.HeadlessServiceFQDN(), r.Scheme, log)
@@ -249,8 +254,16 @@ func (r *ClusterReconciler) createExternalNodesList(
 		return []string{}, []string{}, fmt.Errorf("failed to retrieve node port service %s: %w", nodePortName, err)
 	}
 
-	if len(nodePortSvc.Spec.Ports) != 2 || nodePortSvc.Spec.Ports[0].NodePort == 0 || nodePortSvc.Spec.Ports[1].NodePort == 0 {
+	numPorts := len(pandaCluster.Spec.Configuration.KafkaAPI) + 1 // Admin has a port too.
+
+	if len(nodePortSvc.Spec.Ports) != numPorts {
 		return []string{}, []string{}, fmt.Errorf("node port service %s: %w", nodePortName, errNodePortMissing)
+	}
+
+	for _, port := range nodePortSvc.Spec.Ports {
+		if port.NodePort == 0 {
+			return []string{}, []string{}, fmt.Errorf("node port service %s, port %s is 0: %w", nodePortName, port.Name, errNodePortMissing)
+		}
 	}
 
 	var node corev1.Node
@@ -263,7 +276,7 @@ func (r *ClusterReconciler) createExternalNodesList(
 				fmt.Sprintf("%s.%s:%d",
 					pods[i].Name[prefixLen:],
 					pandaCluster.Spec.ExternalConnectivity.Subdomain,
-					getNodePort(&nodePortSvc, resources.KafkaPortName),
+					getNodePort(&nodePortSvc, nodePortSvc.Spec.Ports[1].Name),
 				))
 			observedNodesExternalAdmin = append(observedNodesExternalAdmin,
 				fmt.Sprintf("%s.%s:%d",
@@ -279,7 +292,7 @@ func (r *ClusterReconciler) createExternalNodesList(
 			observedNodesExternal = append(observedNodesExternal,
 				fmt.Sprintf("%s:%d",
 					getExternalIP(&node),
-					getNodePort(&nodePortSvc, resources.KafkaPortName),
+					getNodePort(&nodePortSvc, nodePortSvc.Spec.Ports[1].Name),
 				))
 			observedNodesExternalAdmin = append(observedNodesExternalAdmin,
 				fmt.Sprintf("%s:%d",
