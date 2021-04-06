22ee98a0a34efe03424bfc1f4c2ac38872f5afe6
Rob Blafford <rob@vectorized.io>
2021-04-06 19:26:39 -0400

    rptest: Wasm full-node failure recovery tests

--- tests/rptest/tests/wasm_identity_test.py
+++ tests/rptest/tests/wasm_identity_test.py
@@ -20,8 +20,14 @@ class WasmIdentityTest(WasmTest):
                         replication_factor=3,
                         cleanup_policy=TopicSpec.CLEANUP_DELETE), )
 
-    def __init__(self, test_context, num_records=1024, record_size=1024):
-        super(WasmIdentityTest, self).__init__(test_context, extra_rp_conf={})
+    def __init__(self,
+                 test_context,
+                 extra_rp_conf=None,
+                 num_records=1024,
+                 record_size=1024):
+        super(WasmIdentityTest, self).__init__(test_context,
+                                               extra_rp_conf=extra_rp_conf
+                                               or {})
         self._num_records = num_records
         self._record_size = record_size
         assert len(self.topics) >= 1
