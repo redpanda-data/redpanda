d8cc310e6207b4fb22bb9d0f59dff917db43d4c4
Ben Pope <ben@vectorized.io>
2021-04-07 22:52:12 +0100

    kafka/client: Introduce partitioners
    A partitioner takes a record and assigns it a partition.
    A partitioner may fail to assign a partition, so it returns an optional.
    
    Several strategies are included:
    
    * Identity - return the partition id already in the record
    * Murmur2 - assign a partition id dervied from the murmer2 hash of the key
    * roundrobin - Use round-robin to assign a partition id
    * default_fallback - Use all three, in order. Cannot fail.
    
    The default partitioner in java client uses a different strategy, with
    a sticky partitioner where round-robin is. The sticky partitioner
    moves to the next partition when the batch is full. This strategy should
    be implemented later.
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

---
+++ src/v/kafka/client/partitioners.cc
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#include "kafka/client/partitioners.h"
+
+#include "hashing/murmur.h"
+
+#include <functional>
+#include <optional>
+#include <tuple>
+#include <type_traits>
+
+namespace kafka::client {
+
+namespace detail {
+
+class identity_partitioner final : public partitioner_impl {
+public:
+    std::optional<model::partition_id>
+    operator()(const record_essence& rec, size_t) override {
+        return rec.partition_id;
+    }
+};
+
+class murmur2_key_partitioner final : public partitioner_impl {
+public:
+    std::optional<model::partition_id>
+    operator()(const record_essence& rec, size_t partition_count) override {
+        if (!rec.key || rec.key->empty()) {
+            return std::nullopt;
+        }
+        iobuf_const_parser p(*rec.key);
+        auto key = p.read_bytes(p.bytes_left());
+        auto hash = murmur2(key.data(), key.size());
+        return model::partition_id(hash % partition_count);
+    }
+};
+
+class roundrobin_partitioner final : public partitioner_impl {
+public:
+    explicit roundrobin_partitioner(model::partition_id initial)
+      : partitioner_impl{}
+      , _next(initial) {}
+
+    std::optional<model::partition_id>
+    operator()(const record_essence&, size_t partition_count) override {
+        return model::partition_id(_next++ % partition_count);
+    }
+
+private:
+    model::partition_id _next;
+};
+
+// Try each partitioner in the list until one succeeds.
+template<typename... Impls>
+class composed_partitioner final : public partitioner_impl {
+public:
+    explicit composed_partitioner(Impls&&... impls)
+      : _impls{std::forward<Impls>(impls)...} {}
+
+    std::optional<model::partition_id>
+    operator()(const record_essence& rec, size_t partition_count) override {
+        return std::apply(
+          [&rec, partition_count, p_id{std::optional<model::partition_id>{}}](
+            auto&&... partitioner) mutable {
+              return (
+                (p_id = p_id.has_value() ? p_id
+                                         : partitioner(rec, partition_count)),
+                ...);
+          },
+          _impls);
+    }
+
+private:
+    std::tuple<Impls...> _impls;
+};
+
+} // namespace detail
+
+partitioner identity_partitioner() {
+    return partitioner{std::make_unique<detail::identity_partitioner>()};
+}
+
+partitioner murmur2_key_partitioner() {
+    return partitioner{std::make_unique<detail::murmur2_key_partitioner>()};
+}
+
+partitioner roundrobin_partitioner(model::partition_id initial) {
+    return partitioner{
+      std::make_unique<detail::roundrobin_partitioner>(initial)};
+}
+
+partitioner default_partitioner(model::partition_id initial) {
+    return partitioner{std::make_unique<detail::composed_partitioner<
+      detail::identity_partitioner,
+      detail::murmur2_key_partitioner,
+      detail::roundrobin_partitioner>>(
+      detail::identity_partitioner{},
+      detail::murmur2_key_partitioner{},
+      detail::roundrobin_partitioner{initial})};
+}
+
+} // namespace kafka::client
---
+++ src/v/kafka/client/partitioners.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "kafka/client/types.h"
+#include "model/fundamental.h"
+
+namespace kafka::client {
+
+class partitioner_impl {
+public:
+    partitioner_impl() = default;
+    partitioner_impl(const partitioner_impl&) = delete;
+    partitioner_impl(partitioner_impl&&) = default;
+    partitioner_impl& operator=(const partitioner_impl&) = delete;
+    partitioner_impl& operator=(partitioner_impl&&) = delete;
+    virtual ~partitioner_impl() = default;
+
+    virtual std::optional<model::partition_id>
+    operator()(const record_essence&, size_t partition_count) = 0;
+};
+
+class partitioner {
+public:
+    partitioner() = default;
+    explicit partitioner(std::unique_ptr<partitioner_impl> impl)
+      : _impl(std::move(impl)) {}
+
+    explicit operator bool() const { return bool(_impl); }
+
+    std::optional<model::partition_id>
+    operator()(const record_essence& rec, size_t partition_count) {
+        return (*_impl)(rec, partition_count);
+    }
+
+private:
+    std::unique_ptr<partitioner_impl> _impl;
+};
+
+/// \brief Returns the partition_id in the record
+partitioner identity_partitioner();
+
+/// \brief Returns the murmur2 hash of the key,
+/// or nullopt if there is no key or the key is empty
+partitioner murmur2_key_partitioner();
+
+/// \brief Returns the partition_id in round-robin fashion, starting from
+/// \ref initial
+partitioner roundrobin_partitioner(model::partition_id initial);
+
+/// \brief Returns the partition_id if one exists in the record, or,
+/// returns the murmer2 hash of the key if there is one, or,
+/// returns partition_id based on round-robin.
+partitioner default_partitioner(model::partition_id initial);
+
+} // namespace kafka::client
