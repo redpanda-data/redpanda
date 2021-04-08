f682fd142a11f8fac7d2bc516338005cdeeb602f
Alena Varkockova <varkockova.a@gmail.com>
2021-04-08 16:28:15 +0200

    operator/api: get rid of tls field under configuration

--- src/go/k8s/pkg/resources/statefulset.go
+++ src/go/k8s/pkg/resources/statefulset.go
@@ -484,7 +484,7 @@ func (r *StatefulSetResource) secretVolumeMounts() []corev1.VolumeMount {
 			MountPath: tlsDirCA,
 		})
 	}
-	if r.pandaCluster.Spec.Configuration.TLS.AdminAPI.Enabled {
+	if r.pandaCluster.Spec.Configuration.AdminAPI.TLS.Enabled {
 		mounts = append(mounts, corev1.VolumeMount{
 			Name:      "tlsadmincert",
 			MountPath: tlsAdminDir,
@@ -538,7 +538,7 @@ func (r *StatefulSetResource) secretVolumes() []corev1.Volume {
 	}
 
 	// When Admin TLS is enabled, Redpanda needs a keypair certificate.
-	if r.pandaCluster.Spec.Configuration.TLS.AdminAPI.Enabled {
+	if r.pandaCluster.Spec.Configuration.AdminAPI.TLS.Enabled {
 		vols = append(vols, corev1.Volume{
 			Name: "tlsadmincert",
 			VolumeSource: corev1.VolumeSource{
