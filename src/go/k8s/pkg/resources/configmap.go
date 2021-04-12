2aed30e79d4dd331e80c6f84dc34b5a9ae482227
Dimitris Skourtis <skourtis@vectorized.io>
2021-04-11 17:24:54 -0700

    operator/admin: create internal, external admin API listeners

--- src/go/k8s/pkg/resources/configmap.go
+++ src/go/k8s/pkg/resources/configmap.go
@@ -40,6 +40,9 @@ const (
 
 	oneMB          = 1024 * 1024
 	logSegmentSize = 512 * oneMB
+
+	internal = "Internal"
+	external = "External"
 )
 
 var errKeyDoesNotExistInSecretData = errors.New("cannot find key in secret data")
@@ -164,6 +167,18 @@ func (r *ConfigMapResource) createConfiguration(
 	}
 
 	cr.AdminApi[0].Port = clusterCRPortOrRPKDefault(c.AdminAPI.Port, cr.AdminApi[0].Port)
+	cr.AdminApi[0].Name = internal
+	if r.pandaCluster.Spec.ExternalConnectivity.Enabled {
+		externalAdminAPI := config.NamedSocketAddress{
+			SocketAddress: config.SocketAddress{
+				Address: cr.AdminApi[0].Address,
+				Port:    cr.AdminApi[0].Port + 1,
+			},
+			Name: external,
+		}
+		cr.AdminApi = append(cr.AdminApi, externalAdminAPI)
+	}
+
 	cr.DeveloperMode = c.DeveloperMode
 	cr.Directory = dataDirectory
 	if r.pandaCluster.Spec.Configuration.TLS.KafkaAPI.Enabled {
@@ -188,7 +203,12 @@ func (r *ConfigMapResource) createConfiguration(
 		}
 	}
 	if r.pandaCluster.Spec.Configuration.TLS.AdminAPI.Enabled {
+		name := internal
+		if r.pandaCluster.Spec.ExternalConnectivity.Enabled {
+			name = external
+		}
 		adminTLS := config.ServerTLS{
+			Name:              name,
 			KeyFile:           fmt.Sprintf("%s/%s", tlsAdminDir, corev1.TLSPrivateKeyKey),
 			CertFile:          fmt.Sprintf("%s/%s", tlsAdminDir, corev1.TLSCertKey),
 			Enabled:           true,
