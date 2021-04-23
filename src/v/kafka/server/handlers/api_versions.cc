82c771293b42aae04a79a17b9356af9168213a23
Denis Rystsov <denis@vectorized.io>
2021-04-23 16:44:22 -0700

    kafka: advertise tx rpcs via metadata (api_versions)
    Enables init_producer_id_handler & txn_offset_commit_handler API to be
    advertised via metadata

--- src/v/kafka/server/handlers/api_versions.cc
+++ src/v/kafka/server/handlers/api_versions.cc
@@ -53,7 +53,9 @@ using request_types = make_request_types<
   describe_acls_handler,
   describe_log_dirs_handler,
   create_acls_handler,
-  delete_acls_handler>;
+  delete_acls_handler,
+  init_producer_id_handler,
+  txn_offset_commit_handler>;
 
 template<typename RequestType>
 static auto make_api() {
