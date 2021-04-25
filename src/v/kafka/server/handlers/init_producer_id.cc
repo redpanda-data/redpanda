6efa522cae137c028d0da8fea1ce760e131732cb
Denis Rystsov <denis@vectorized.io>
2021-04-25 14:57:18 -0700

    kafka: connect init_producer_id with tx logic

--- src/v/kafka/server/handlers/init_producer_id.cc
+++ src/v/kafka/server/handlers/init_producer_id.cc
@@ -11,6 +11,7 @@
 
 #include "cluster/id_allocator_frontend.h"
 #include "cluster/topics_frontend.h"
+#include "cluster/tx_gateway_frontend.h"
 #include "config/configuration.h"
 #include "kafka/server/group_manager.h"
 #include "kafka/server/group_router.h"
@@ -40,9 +41,34 @@ ss::future<response_ptr> init_producer_id_handler::handle(
                   = error_code::transactional_id_authorization_failed;
                 return ctx.respond(reply);
             }
-        } else if (!ctx.authorized(
-                     security::acl_operation::idempotent_write,
-                     security::default_cluster_name)) {
+
+            return ctx.tx_gateway_frontend()
+              .init_tm_tx(
+                request.data.transactional_id.value(),
+                config::shard_local_cfg().create_topic_timeout_ms())
+              .then([&ctx](cluster::init_tm_tx_reply r) {
+                  init_producer_id_response reply;
+
+                  if (r.ec == cluster::tx_errc::none) {
+                      reply.data.producer_id = kafka::producer_id(r.pid.id);
+                      reply.data.producer_epoch = r.pid.epoch;
+                      vlog(
+                        klog.trace,
+                        "allocated pid {} with epoch {} via tx_gateway",
+                        reply.data.producer_id,
+                        reply.data.producer_epoch);
+                  } else {
+                      vlog(klog.warn, "failed to allocate pid, ec: {}", r.ec);
+                      reply.data.error_code = error_code::broker_not_available;
+                  }
+
+                  return ctx.respond(std::move(reply));
+              });
+        }
+
+        if (!ctx.authorized(
+              security::acl_operation::idempotent_write,
+              security::default_cluster_name)) {
             init_producer_id_response reply;
             reply.data.error_code = error_code::cluster_authorization_failed;
             return ctx.respond(reply);
@@ -62,7 +88,7 @@ ss::future<response_ptr> init_producer_id_handler::handle(
                     reply.data.producer_id,
                     reply.data.producer_epoch);
               } else {
-                  vlog(klog.warn, "failed to allocate pid");
+                  vlog(klog.warn, "failed to allocate pid, ec: {}", r.ec);
                   reply.data.error_code = error_code::broker_not_available;
               }
 
