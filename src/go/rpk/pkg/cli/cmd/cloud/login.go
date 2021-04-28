1a7c89537a57995ec9bae0d1f60740ea80632136
Alena Varkockova <varkockova.a@gmail.com>
2021-04-28 16:06:18 +0200

    rpk/cloud: implement login flow with config

--- src/go/rpk/pkg/cli/cmd/cloud/login.go
+++ src/go/rpk/pkg/cli/cmd/cloud/login.go
@@ -16,6 +16,7 @@ import (
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/vcloud"
+	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/vcloud/config"
 )
 
 func NewLoginCommand(fs afero.Fs) *cobra.Command {
@@ -24,14 +25,47 @@ func NewLoginCommand(fs afero.Fs) *cobra.Command {
 		Short: "Login to vectorized cloud",
 		Long:  `Authorize rpk to access vectorized cloud with your user credentials.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			auth0Client := vcloud.NewDefaultAuth0Client()
-			token, err := vcloud.Login(auth0Client)
-			if err != nil {
-				return fmt.Errorf("login failed, please retry.\n%w", err)
+			// try to look for existing token in config first
+			configRW := config.NewVCloudConfigReaderWriter(fs)
+			token, err := configRW.ReadToken()
+			if err != nil && err != config.ErrConfigFileDoesNotExist {
+				return fmt.Errorf("error reading token from config file: %w", err)
 			}
-			log.Info("Success!")
-			log.Info(token.Token)
-			return nil
+			auth0Client := vcloud.NewDefaultAuth0Client()
+			if err == config.ErrConfigFileDoesNotExist {
+				// no config file, have to log in
+				return login(auth0Client, configRW)
+			}
+
+			// validate token from config
+			valid, err := auth0Client.VerifyToken(token)
+			if err != nil {
+				return fmt.Errorf("error when validating token from config. %w", err)
+			}
+			if valid {
+				log.Info("Already logged in!")
+				return nil
+			}
+			// token not valid, need to log in
+			return login(auth0Client, configRW)
 		},
 	}
+}
+
+func login(
+	authClient vcloud.AuthClient, configRW config.ConfigReaderWriter,
+) error {
+	// token not valid or first time logging in
+	newToken, err := vcloud.Login(authClient)
+	if err != nil {
+		return fmt.Errorf("login failed, please retry.\n%w", err)
+	}
+
+	// write token to config
+	err = configRW.WriteToken(newToken.Token)
+	if err != nil {
+		return fmt.Errorf("error writing token into the config file. %w", err)
+	}
+	log.Info("Success!")
+	return nil
 }
