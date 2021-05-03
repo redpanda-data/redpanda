33818627357a507cbbf538c9bab5848efa6c633a
Alena Varkockova <varkockova.a@gmail.com>
2021-05-03 10:09:11 +0200

    rpk/cloud: duplicate ui table view
    This is to keep vcloud independent of rpk. This just duplicates the
    current rpk table.

---
+++ src/go/rpk/pkg/vcloud/ui/tables.go
@@ -0,0 +1,28 @@
+// Copyright 2020 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package ui
+
+import (
+	"io"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func NewVcloudTable(writer io.Writer) *tablewriter.Table {
+	table := tablewriter.NewWriter(writer)
+	table.SetBorder(false)
+	table.SetColumnSeparator("")
+	table.SetHeaderLine(false)
+	table.SetColWidth(80)
+	table.SetAutoWrapText(true)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	return table
+}
