4bab4722079a06077bbe37563fd065091420b528
Ben Pope <ben@vectorized.io>
2021-05-07 18:11:16 +0100

    pandaproxy: Improve pandaproxy_client defaults
    Fix an issue where Redpanda startup is prevented if
    pandaproxy_client.broker_tls can't be inferred.
    
    The TLS config inference in #1333 had some bad assumptions:
    * For each kafka_api, there is a corresponding kafka_api_tls
    * The ordering is likely to be the same
    
    Now there is a search by name. No match is ok. Empty name is ok.
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/redpanda/application.cc
+++ src/v/redpanda/application.cc
@@ -243,14 +243,14 @@ void application::hydrate_config(const po::variables_map& cfg) {
               std::vector<unresolved_address>{kafka_api[0].address});
             const auto& kafka_api_tls
               = config::shard_local_cfg().kafka_api_tls.value();
-            if (!kafka_api_tls.empty()) {
-                vassert(
-                  kafka_api[0].name == kafka_api_tls[0].name,
-                  "pandaproxy_client.broker_tls could not be inferred from "
-                  "kafka_api_tls: {}",
-                  kafka_api_tls);
-                _proxy_client_config->broker_tls.set_value(
-                  kafka_api_tls[0].config);
+            auto tls_it = std::find_if(
+              kafka_api_tls.begin(),
+              kafka_api_tls.end(),
+              [&kafka_api](const config::endpoint_tls_config& tls) {
+                  return tls.name == kafka_api[0].name;
+              });
+            if (tls_it != kafka_api_tls.end()) {
+                _proxy_client_config->broker_tls.set_value(tls_it->config);
             }
         }
         _proxy_config->for_each(config_printer("pandaproxy"));
