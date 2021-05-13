b7acbf73d2c9e17cb91e8b782e5c4b0a984ff380
Michal Maslanka <michal@vectorized.io>
2021-05-13 10:15:54 +0200

    s/backlog_controller: introduced backlog_controller
    Backlog controller is PID controller implementation to manage amount of
    seastar CPU and IO shares. This contoller works in an negative feedback loop
    using a backlog size as process value and amount of shares as a control
    value.
    ```
                       ┌────────────────────┐            ┌──────────────┐
      set backlog size │                    │ set shares │              │
          ───────────┬─┤ backlog_controller ├────────────┤ some process │
                   - │ │                    │            │              │
                     │ └────────────────────┘            └──────┬───────┘
                     │                                          │
                     │                                          │
                     └──────────────────────────────────────────┘
                                    current backlog size
    
    Backlog controller uses PID controller design to calculate the output shares.
    
                    ┌────────────────────────┐
                    │                        │
                  P │          error         ├─────┐
                    │                        │     │
                    └────────────────────────┘     │
                                                   │
                    ┌────────────────────────┐     │
                    │                        │    ┌▼┐      ┌─────┐ shares
                  I │ k_i*(aggregated_error) ├───►│+├─────►│ k_p ├──────►
                    │                        │    └▲┘      └─────┘
                    └────────────────────────┘     │
                                                   │
                    ┌────────────────────────┐     │
                    │                        │     │
                  D │  k_d*(err - prev_err)  ├─────┘
                    │                        │
                    └────────────────────────┘
    ```
    Both the integral and derivative actions of the controller can be disabled
    by setting corresponding coefficient to 0.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/storage/backlog_controller.h
@@ -0,0 +1,130 @@
+// Copyright 2020 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#pragma once
+#include "seastarx.h"
+
+#include <seastar/core/gate.hh>
+#include <seastar/core/io_priority_class.hh>
+#include <seastar/core/metrics_registration.hh>
+#include <seastar/core/timer.hh>
+#include <seastar/util/log.hh>
+
+namespace storage {
+struct backlog_controller_config {
+    backlog_controller_config(
+      double proportional_coeff,
+      double integral_coeff,
+      double derivative_coeff,
+      int64_t normalization_factor,
+      int64_t setpoint,
+      int initial_shares,
+      std::chrono::milliseconds sampling_interval,
+      ss::scheduling_group sg,
+      ss::io_priority_class iopc,
+      int min_shares,
+      int max_shares);
+
+    double proportional_coeff;
+    double integral_coeff;
+    double derivative_coeff;
+    int64_t normalization_factor;
+    int64_t setpoint;
+    int initial_shares;
+    std::chrono::milliseconds sampling_interval;
+    ss::scheduling_group scheduling_group;
+    ss::io_priority_class io_priority;
+    int min_shares;
+    int max_shares;
+};
+/**
+ * Backlog controller is PID controller implementation to manage amount of
+ * seastar CPU and IO shares. This contoller works in an negative feedback loop
+ * using a backlog size as process value and amount of shares as a control
+ * value.
+ *
+ *                    ┌────────────────────┐            ┌──────────────┐
+ *   set backlog size │                    │ set shares │              │
+ *       ───────────┬─┤ backlog_controller ├────────────┤ some process │
+ *                - │ │                    │            │              │
+ *                  │ └────────────────────┘            └──────┬───────┘
+ *                  │                                          │
+ *                  │                                          │
+ *                  └──────────────────────────────────────────┘
+ *                                 current backlog size
+ *
+ * Backlog controller uses PID controller design to calculate the output shares.
+ *
+ *
+ *                 ┌────────────────────────┐
+ *                 │                        │
+ *               P │          error         ├─────┐
+ *                 │                        │     │
+ *                 └────────────────────────┘     │
+ *                                                │
+ *                 ┌────────────────────────┐     │
+ *                 │                        │    ┌▼┐      ┌─────┐ shares
+ *               I │ k_i*(aggregated_error) ├───►│+├─────►│ k_p ├──────►
+ *                 │                        │    └▲┘      └─────┘
+ *                 └────────────────────────┘     │
+ *                                                │
+ *                 ┌────────────────────────┐     │
+ *                 │                        │     │
+ *               D │  k_d*(err - prev_err)  ├─────┘
+ *                 │                        │
+ *                 └────────────────────────┘
+ *
+ * Both the integral and derivative actions of the controller can be disabled
+ * by setting corresponding coefficient to 0.
+ */
+class backlog_controller {
+public:
+    struct sampler {
+        virtual ss::future<int64_t> sample_backlog() = 0;
+        virtual ~sampler() noexcept = default;
+    };
+
+    backlog_controller(
+      std::unique_ptr<sampler>, ss::logger&, backlog_controller_config);
+
+    void update_setpoint(int64_t);
+    ss::future<> start();
+    ss::future<> stop();
+
+    void setup_metrics(const ss::sstring&);
+
+private:
+    ss::future<> set();
+    ss::future<> update();
+
+    void accumulate_error(int64_t);
+
+    std::unique_ptr<sampler> _sampler;
+    ss::logger& _log;
+    double _proportional_coeff; // controller 'gain' -  proportional coefficient
+    double _integral_coeff;     // integral part coefficient
+    double _derivative_coeff;   // derivative part coefficient
+    int64_t _norm;
+    std::chrono::milliseconds _sampling_interval;
+    ss::timer<> _sampling_timer;
+    // controlled resources
+    ss::scheduling_group _scheduling_group;
+    ss::io_priority_class _io_priority;
+    // state
+    int64_t _current_backlog{0};
+    int64_t _prev_error{0};
+    int64_t _error_integral{0};
+    int64_t _setpoint;
+    int _current_shares;
+    int _min_shares;
+    int _max_shares;
+    ss::gate _gate;
+    ss::metrics::metric_groups _metrics;
+};
+} // namespace storage
---
+++ src/v/storage/tests/backlog_controller_test.cc
@@ -0,0 +1,98 @@
+// Copyright 2020 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#include "seastarx.h"
+#include "storage/backlog_controller.h"
+#include "test_utils/async.h"
+#include "test_utils/fixture.h"
+
+#include <seastar/core/coroutine.hh>
+#include <seastar/core/io_priority_class.hh>
+#include <seastar/core/metrics_api.hh>
+#include <seastar/core/metrics_registration.hh>
+#include <seastar/core/reactor.hh>
+#include <seastar/core/scheduling.hh>
+#include <seastar/core/sstring.hh>
+#include <seastar/util/log.hh>
+
+#include <cstdint>
+
+using namespace std::chrono_literals; // NOLINT
+static ss::logger ctrl_logger{"test-controller"};
+
+struct simple_backlog_sampler : storage::backlog_controller::sampler {
+    explicit simple_backlog_sampler(int64_t& b)
+      : current_backlog(b){};
+    ss::future<int64_t> sample_backlog() final { co_return current_backlog; }
+
+    int64_t& current_backlog;
+};
+
+struct backlog_controller_fixture {
+    const std::map<ss::sstring, ss::sstring> sch_group_label = {
+      {"group", "sch_control_gr"}, {"shard", "0"}};
+    backlog_controller_fixture()
+      : iopc(ss::engine().register_one_priority_class("io_control_gr", 100)) {
+        sg = ss::create_scheduling_group("sch_control_gr", 100).get();
+        /**
+         * Controller settings:
+         *
+         * proportional coefficient kp = -1.0 - negative reverse proportional to
+         * backlog integral coefficient ki = 0.4 derivative coefficient kd = 0.2
+         * initial setpoint sp = 20
+         * initial shares = 100
+         * min shares = 10
+         * max shares = 800
+         */
+        auto cfg = storage::backlog_controller_config(
+          -1.0, 0.4, 0.2, 1, 20, 100, 10ms, sg, iopc, 10, 800);
+
+        ctrl = std::make_unique<storage::backlog_controller>(
+          std::make_unique<simple_backlog_sampler>(backlog), ctrl_logger, cfg);
+    }
+
+    ss::io_priority_class iopc;
+    ss::scheduling_group sg;
+    std::unique_ptr<storage::backlog_controller> ctrl;
+    int64_t backlog{0};
+
+    int get_current_cpu_shares() const {
+        auto metrics = ss::metrics::impl::get_value_map();
+        auto it = std::find_if(
+          metrics.begin(),
+          metrics.end(),
+          [](const ss::metrics::impl::value_map::value_type& p) {
+              return p.first == "scheduler_shares";
+          });
+
+        auto m = it->second.find(sch_group_label);
+        return m->second->get_function()().i();
+    }
+};
+
+FIXTURE_TEST(test_feedback_loop, backlog_controller_fixture) {
+    ctrl->start().get0();
+    /**
+     * current backlog is equal to 0, setpoint is set to 20, error = 20.0
+     * since error hasn't changed and want some more backlog we will down
+     * schedule the group until min limit is reached
+     */
+    tests::cooperative_spin_wait_with_timeout(1500ms, [this] {
+        return get_current_cpu_shares() == 10;
+    }).get();
+
+    /**
+     * huge change in backlog size should cause immediate reaction of controller
+     */
+    backlog = 200;
+
+    tests::cooperative_spin_wait_with_timeout(1500ms, [this] {
+        return get_current_cpu_shares() == 800;
+    }).get();
+}
