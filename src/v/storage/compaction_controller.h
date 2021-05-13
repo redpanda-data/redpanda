b9a9e59c3acbc3c7071980824d0f43be23ed0fbb
Michal Maslanka <michal@vectorized.io>
2021-05-13 10:17:50 +0200

    s/compaction_controller: introduced compaction_controller
    Introduced `storage::compaction_controller` a backend controller
    sampling compaction backlog and controlling compaction IO and CPU
    priority shares.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/storage/compaction_controller.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "storage/backlog_controller.h"
+#include "storage/fwd.h"
+
+#include <seastar/core/sharded.hh>
+
+namespace storage {
+
+struct compaction_backlog_sampler : public backlog_controller::sampler {
+    explicit compaction_backlog_sampler(ss::sharded<api>& api)
+      : _api(api) {}
+
+    ss::future<int64_t> sample_backlog() final;
+
+private:
+    ss::sharded<api>& _api;
+};
+/**
+ * PID controller to controll compaction scheduling and IO shares
+ */
+class compaction_controller {
+public:
+    compaction_controller(ss::sharded<api>&, backlog_controller_config);
+
+    ss::future<> start() { return _ctrl.start(); }
+    ss::future<> stop() { return _ctrl.stop(); }
+
+private:
+    backlog_controller _ctrl;
+};
+
+} // namespace storage
