f59e39c97d50700134b2c7bfeeb7dc4cb624f239
Michal Maslanka <michal@vectorized.io>
2021-05-17 09:20:46 +0200

    k/fetch: introduced fetch_plan_executor
    Introduced definition of `kafka::fetch_plan_executor` class. The
    `fetch_plan_executor` is responsible for execution of partition reads
    that are part of the fetch plan. Based on the read results fetch
    executor should fill responses being part of fetch operation context.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/kafka/server/handlers/fetch/fetch_plan_executor.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+#include "kafka/server/handlers/fetch.h"
+
+namespace kafka {
+/**
+ * Executes fetch plan, it uses op_context to fill fetch response placeholders
+ */
+class fetch_plan_executor final {
+public:
+    struct impl {
+        virtual ss::future<> execute_plan(op_context&, fetch_plan) = 0;
+        virtual ~impl() noexcept = default;
+    };
+
+    explicit fetch_plan_executor(std::unique_ptr<impl> impl)
+      : _impl(std::move(impl)) {}
+
+    ss::future<> execute_plan(op_context& octx, fetch_plan plan) {
+        return _impl->execute_plan(octx, std::move(plan));
+    }
+
+private:
+    std::unique_ptr<impl> _impl;
+};
+
+template<typename T, typename... Args>
+fetch_plan_executor make_fetch_plan_executor(Args&&... args) {
+    return fetch_plan_executor(
+      std::make_unique<T>(std::forward<Args>(args)...));
+}
+
+} // namespace kafka
