059fa600a3dc58a34eaf017c8d683bf03d44ff16
Ben Pope <ben@vectorized.io>
2021-05-20 23:01:53 +0100

    schema_registry: Wire-up get_subject_versions handler
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/schema_registry/service.cc
+++ src/v/pandaproxy/schema_registry/service.cc
@@ -30,6 +30,10 @@ server::routes_t get_schema_registry_routes() {
       ss::httpd::schema_registry_json::get_schemas_types, get_schemas_types});
 
     routes.routes.emplace_back(server::route_t{
+      ss::httpd::schema_registry_json::get_subject_versions,
+      get_subject_versions});
+
+    routes.routes.emplace_back(server::route_t{
       ss::httpd::schema_registry_json::post_subject_versions,
       post_subject_versions});
 
