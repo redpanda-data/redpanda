69ae183c62d882f27e4b95db8a9b7e7184a7d3ff
Michal Maslanka <michal@vectorized.io>
2021-05-21 13:55:16 +0200

    s/segment_reader: disabled dynamic adjustments
    Disabled dynamic prefetching adjustments based on the
    `input_file_stream` history.
    
    Seastar file input stream dynamic adjustment mechanism is intended to
    optimize memory usage by limiting number of issued read ahead requests.
    In redpanda reader use case we recycle readers and dynamic adjustment
    mechanism slows down consumers. Read ahead adjustments are not used in
    Scylla DB hot path.
    
    Removing history based adjustment increase fetch performance by up to
    ~20%.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/storage/segment_reader.cc
+++ src/v/storage/segment_reader.cc
@@ -40,7 +40,6 @@ segment_reader::data_stream(size_t pos, const ss::io_priority_class& pc) {
     options.buffer_size = _buffer_size;
     options.io_priority_class = pc;
     options.read_ahead = 10;
-    options.dynamic_adjustments = _history;
     return make_file_input_stream(
       _data_file, pos, _file_size - pos, std::move(options));
 }
