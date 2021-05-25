26b3f453c069b3e0e3c1d0f8540f069608d770f4
Ben Pope <ben@vectorized.io>
2021-05-25 14:16:14 +0100

    schema_registry/storage: Introduce JSON serde for schema_key
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/schema_registry/storage.h
+++ src/v/pandaproxy/schema_registry/storage.h
@@ -11,8 +11,11 @@
 
 #pragma once
 
+#include "pandaproxy/json/rjson_parse.h"
+#include "pandaproxy/json/rjson_util.h"
 #include "pandaproxy/schema_registry/error.h"
 #include "pandaproxy/schema_registry/types.h"
+#include "utils/string_switch.h"
 
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/writer.h>
@@ -21,5 +24,124 @@ namespace pandaproxy::schema_registry {
 
 using topic_key_magic = named_type<int32_t, struct topic_key_magic_tag>;
 using topic_key_type = named_type<ss::sstring, struct topic_key_type_tag>;
+
+struct schema_key {
+    topic_key_type keytype{"SCHEMA"};
+    subject sub;
+    schema_version version;
+    topic_key_magic magic{1};
+};
+
+inline void rjson_serialize(
+  rapidjson::Writer<rapidjson::StringBuffer>& w,
+  const schema_registry::schema_key& key) {
+    w.StartObject();
+    w.Key("keytype");
+    ::json::rjson_serialize(w, key.keytype);
+    w.Key("subject");
+    ::json::rjson_serialize(w, key.sub());
+    w.Key("version");
+    ::json::rjson_serialize(w, key.version);
+    w.Key("magic");
+    ::json::rjson_serialize(w, key.magic);
+    w.EndObject();
+}
+
+template<typename Encoding = rapidjson::UTF8<>>
+class schema_key_handler : public json::base_handler<Encoding> {
+    enum class state {
+        empty = 0,
+        object,
+        keytype,
+        subject,
+        version,
+        magic,
+    };
+    state _state = state::empty;
+
+public:
+    using Ch = typename json::base_handler<Encoding>::Ch;
+    using rjson_parse_result = schema_key;
+    rjson_parse_result result;
+
+    schema_key_handler()
+      : json::base_handler<Encoding>{json::serialization_format::none} {}
+
+    bool Key(const Ch* str, rapidjson::SizeType len, bool) {
+        auto sv = std::string_view{str, len};
+        switch (_state) {
+        case state::object: {
+            std::optional<state> s{string_switch<std::optional<state>>(sv)
+                                     .match("keytype", state::keytype)
+                                     .match("subject", state::subject)
+                                     .match("version", state::version)
+                                     .match("magic", state::magic)
+                                     .default_match(std::nullopt)};
+            if (s.has_value()) {
+                _state = *s;
+            }
+            return s.has_value();
+        }
+        case state::empty:
+        case state::keytype:
+        case state::subject:
+        case state::version:
+        case state::magic:
+            return false;
+        }
+        return false;
+    }
+
+    bool Uint(int i) {
+        switch (_state) {
+        case state::version: {
+            result.version = schema_version{i};
+            _state = state::object;
+            return true;
+        }
+        case state::magic: {
+            result.magic = topic_key_magic{i};
+            _state = state::object;
+            return true;
+        }
+        case state::empty:
+        case state::object:
+        case state::keytype:
+        case state::subject:
+            return false;
+        }
+        return false;
+    }
+
+    bool String(const Ch* str, rapidjson::SizeType len, bool) {
+        auto sv = std::string_view{str, len};
+        switch (_state) {
+        case state::keytype: {
+            result.keytype = topic_key_type{ss::sstring{sv}};
+            _state = state::object;
+            return true;
+        }
+        case state::subject: {
+            result.sub = subject{ss::sstring{sv}};
+            _state = state::object;
+            return true;
+        }
+        case state::empty:
+        case state::object:
+        case state::version:
+        case state::magic:
+            return false;
+        }
+        return false;
+    }
+
+    bool StartObject() {
+        return std::exchange(_state, state::object) == state::empty;
+    }
+
+    bool EndObject(rapidjson::SizeType) {
+        return std::exchange(_state, state::empty) == state::object;
+    }
+};
 
 } // namespace pandaproxy::schema_registry
