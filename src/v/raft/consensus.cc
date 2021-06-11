76839badd3642924eb8fd282ef33b1c739aa93d4
Michal Maslanka <michal@vectorized.io>
2021-06-11 10:25:46 +0200

    r/consensus: removed usage of log committed offset from raft
    Do not using log committed offset in raft protocol implementation will
    allow us to simplify offset management in storage layer. This way the
    only source of information about which offsets were safely stored on
    disk will be raft protocol that manages underlying log flushes.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>
    (cherry picked from commit 8f3d2d0c0108057172399f12aa873abced73046d)

--- src/v/raft/consensus.cc
+++ src/v/raft/consensus.cc
@@ -379,7 +379,7 @@ void consensus::maybe_promote_to_voter(vnode id) {
         }
 
         // do not promote to voter, learner is not up to date
-        if (it->second.match_index < _log.offsets().committed_offset) {
+        if (it->second.match_index < _flushed_offset) {
             return ss::now();
         }
 
@@ -1483,7 +1483,7 @@ consensus::do_append_entries(append_entries_request&& r) {
     reply.group = r.meta.group;
     reply.term = _term;
     reply.last_dirty_log_index = lstats.dirty_offset;
-    reply.last_committed_log_index = lstats.committed_offset;
+    reply.last_committed_log_index = _flushed_offset;
     reply.result = append_entries_reply::status::failure;
     _probe.append_request();
 
@@ -2007,14 +2007,13 @@ ss::future<result<replicate_result>> consensus::dispatch_replicate(
 
 append_entries_reply consensus::make_append_entries_reply(
   vnode target_node, storage::append_result disk_results) {
-    auto lstats = _log.offsets();
     append_entries_reply reply;
     reply.node_id = _self;
     reply.target_node_id = target_node;
     reply.group = _group;
     reply.term = _term;
     reply.last_dirty_log_index = disk_results.last_offset;
-    reply.last_committed_log_index = lstats.committed_offset;
+    reply.last_committed_log_index = _flushed_offset;
     reply.result = append_entries_reply::status::success;
     return reply;
 }
@@ -2252,18 +2251,17 @@ consensus::do_maybe_update_leader_commit_idx(ss::semaphore_units<> u) {
     // If there exists an N such that N > commitIndex, a majority
     // of matchIndex[i] ≥ N, and log[N].term == currentTerm:
     // set commitIndex = N (§5.3, §5.4).
-    auto majority_match = config().quorum_match(
-      [this, committed_offset = lstats.committed_offset](vnode id) {
-          // current node - we just return commited offset
-          if (id == _self) {
-              return committed_offset;
-          }
-          if (auto it = _fstats.find(id); it != _fstats.end()) {
-              return it->second.match_committed_index();
-          }
+    auto majority_match = config().quorum_match([this](vnode id) {
+        // current node - we just return commited offset
+        if (id == _self) {
+            return _flushed_offset;
+        }
+        if (auto it = _fstats.find(id); it != _fstats.end()) {
+            return it->second.match_committed_index();
+        }
 
-          return model::offset{};
-      });
+        return model::offset{};
+    });
     /**
      * we have to make sure that we do not advance committed_index beyond the
      * point which is readable in log. Since we are not waiting for flush to
@@ -2275,7 +2273,8 @@ consensus::do_maybe_update_leader_commit_idx(ss::semaphore_units<> u) {
      * batcher aren't readable since some of the writes are still in flight in
      * segment appender.
      */
-    majority_match = std::min(majority_match, lstats.committed_offset);
+    majority_match = std::min(majority_match, _flushed_offset);
+
     if (
       majority_match > _commit_index
       && _log.get_term(majority_match) == _term) {
@@ -2306,14 +2305,12 @@ consensus::do_maybe_update_leader_commit_idx(ss::semaphore_units<> u) {
 }
 ss::future<>
 consensus::maybe_update_follower_commit_idx(model::offset request_commit_idx) {
-    auto lstats = _log.offsets();
     // Raft paper:
     //
     // If leaderCommit > commitIndex, set commitIndex =
     // min(leaderCommit, index of last new entry)
     if (request_commit_idx > _commit_index) {
-        auto new_commit_idx = std::min(
-          request_commit_idx, lstats.committed_offset);
+        auto new_commit_idx = std::min(request_commit_idx, _flushed_offset);
         if (new_commit_idx != _commit_index) {
             _commit_index = new_commit_idx;
             vlog(
