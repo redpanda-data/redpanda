679e37d2175d2d46bc8c67ff94e7969a9ffd493a
Ben Pope <ben@vectorized.io>
2021-06-17 18:37:14 +0100

    utils/human: Fix missing KiB
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/utils/human.cc
+++ src/v/utils/human.cc
@@ -30,10 +30,10 @@ std::ostream& operator<<(std::ostream& o, const ::human::latency& l) {
     return o << x << "uknown_units";
 }
 std::ostream& operator<<(std::ostream& o, const ::human::bytes& l) {
-    static const char* units[] = {"bytes", "MiB", "GiB", "TiB", "PiB"};
+    static const char* units[] = {"bytes", "KiB", "MiB", "GiB", "TiB", "PiB"};
     static constexpr double step = 1024;
     auto x = l.value;
-    for (size_t i = 0; i < 5; ++i) {
+    for (size_t i = 0; i < 6; ++i) {
         if (x <= step) {
             return ss::fmt_print(o, "{:03.3f}{}", x, units[i]);
         }
---
+++ src/v/utils/tests/human_test.cc
@@ -0,0 +1,32 @@
+// Copyright 2021 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#include "utils/human.h"
+
+#include <boost/test/unit_test.hpp>
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+BOOST_AUTO_TEST_CASE(human_bytes) {
+    BOOST_CHECK_EQUAL(fmt::format("{}", human::bytes(-1)), "-1.000bytes");
+    BOOST_CHECK_EQUAL(fmt::format("{}", human::bytes(0)), "0.000bytes");
+    BOOST_CHECK_EQUAL(fmt::format("{}", human::bytes(1)), "1.000bytes");
+    BOOST_CHECK_EQUAL(fmt::format("{}", human::bytes(1024)), "1024.000bytes");
+    BOOST_CHECK_EQUAL(fmt::format("{}", human::bytes(1025)), "1.001KiB");
+    BOOST_CHECK_EQUAL(
+      fmt::format("{}", human::bytes(1UL << 20U)), "1024.000KiB");
+    BOOST_CHECK_EQUAL(
+      fmt::format("{}", human::bytes(1UL << 30U)), "1024.000MiB");
+    BOOST_CHECK_EQUAL(
+      fmt::format("{}", human::bytes(1UL << 40U)), "1024.000GiB");
+    BOOST_CHECK_EQUAL(
+      fmt::format("{}", human::bytes(1UL << 50U)), "1024.000TiB");
+    BOOST_CHECK_EQUAL(
+      fmt::format("{}", human::bytes(1UL << 60U)), "1024.000PiB");
+}
