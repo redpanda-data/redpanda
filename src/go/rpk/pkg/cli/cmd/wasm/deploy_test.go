e114e76bd7b0742f54a6a2003eda29ed1224f40e
Rob Blafford <rob@vectorized.io>
2021-06-17 11:51:47 -0400

    rpk: Add missing license info to files

--- src/go/rpk/pkg/cli/cmd/wasm/deploy_test.go
+++ src/go/rpk/pkg/cli/cmd/wasm/deploy_test.go
@@ -1,3 +1,12 @@
+// Copyright 2021 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
 package wasm
 
 import (
