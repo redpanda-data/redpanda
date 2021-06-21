f39dee57f0fa27474718a3da32f4b459b8c6355c
Ben Pope <ben@vectorized.io>
2021-06-21 16:53:25 +0100

    schema_registry: Wire-up delete_subject handler
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/schema_registry/service.cc
+++ src/v/pandaproxy/schema_registry/service.cc
@@ -87,6 +87,10 @@ server::routes_t get_schema_registry_routes(ss::gate& gate, one_shot& es) {
       wrap(gate, es, get_subject_versions_version)});
 
     routes.routes.emplace_back(server::route_t{
+      ss::httpd::schema_registry_json::delete_subject,
+      wrap(gate, es, delete_subject)});
+
+    routes.routes.emplace_back(server::route_t{
       ss::httpd::schema_registry_json::compatibility_subject_version,
       wrap(gate, es, compatibility_subject_version)});
 
