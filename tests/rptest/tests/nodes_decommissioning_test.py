f67de91d9470d0e8338b1660c3814bfeb7a10ec2
Michal Maslanka <michal@vectorized.io>
2021-06-24 12:41:18 +0200

    ducky: added basic node decommissioning test
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ tests/rptest/tests/nodes_decommissioning_test.py
@@ -0,0 +1,106 @@
+# Copyright 2020 Vectorized, Inc.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.md
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0
+
+import random
+import time
+
+from ducktape.mark.resource import cluster
+from ducktape.mark import ignore
+from ducktape.utils.util import wait_until
+from rptest.clients.kafka_cat import KafkaCat
+import requests
+
+from rptest.clients.types import TopicSpec
+from rptest.tests.end_to_end import EndToEndTest
+from rptest.services.admin import Admin
+from rptest.services.honey_badger import HoneyBadger
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+
+
+class NodesDecommissioningTest(EndToEndTest):
+    """
+    Basic nodes decommissioning test.
+    """
+    @cluster(num_nodes=6)
+    def test_decommissioning_working_node(self):
+
+        self.start_redpanda(num_nodes=4)
+        topics = []
+        for partition_count in range(1, 5):
+            for replication_factor in (3, 3):
+                name = f"topic{len(topics)}"
+                spec = TopicSpec(name=name,
+                                 partition_count=partition_count,
+                                 replication_factor=replication_factor)
+                topics.append(spec)
+
+        for spec in topics:
+            self.redpanda.create_topic(spec)
+            self.topic = spec.name
+
+        self.start_producer(1)
+        self.start_consumer(1)
+        self.await_startup()
+        admin = Admin(self.redpanda)
+
+        brokers = admin.get_brokers()
+        to_decommission = random.choice(brokers)
+        self.logger.info(f"decommissioning node: {to_decommission}", )
+        admin.decommission_broker(to_decommission['node_id'])
+
+        def node_removed():
+            brokers = admin.get_brokers()
+            for b in brokers:
+                if b['node_id'] == to_decommission['node_id']:
+                    return False
+            return True
+
+        wait_until(node_removed, timeout_sec=30, backoff_sec=1)
+
+        self.run_validation(enable_idempotence=False, consumer_timeout_sec=45)
+
+    @cluster(num_nodes=6)
+    def test_decommissioning_crashed_node(self):
+
+        self.start_redpanda(num_nodes=4)
+        topics = []
+        for partition_count in range(1, 5):
+            for replication_factor in (3, 3):
+                name = f"topic{len(topics)}"
+                spec = TopicSpec(name=name,
+                                 partition_count=partition_count,
+                                 replication_factor=replication_factor)
+                topics.append(spec)
+
+        for spec in topics:
+            self.redpanda.create_topic(spec)
+            self.topic = spec.name
+
+        self.start_producer(1)
+        self.start_consumer(1)
+        self.await_startup()
+        admin = Admin(self.redpanda)
+
+        to_decommission = self.redpanda.nodes[1]
+        node_id = 2
+        self.redpanda.stop_node(node=to_decommission)
+        self.logger.info(f"decommissioning node: {node_id}", )
+        admin.decommission_broker(id=node_id)
+
+        def node_removed():
+            brokers = admin.get_brokers()
+            for b in brokers:
+                if b['node_id'] == node_id:
+                    return False
+            return True
+
+        wait_until(node_removed, timeout_sec=30, backoff_sec=1)
+
+        self.run_validation(enable_idempotence=False, consumer_timeout_sec=45)
