d90ed407a7d32bb521b09bd497e24f88a48fdd7e
Ben Pope <ben@vectorized.io>
2021-06-24 11:13:47 +0100

    schema_registry/ducktape: Smoketest delete_subject_version endpoint
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- tests/rptest/tests/schema_registry_test.py
+++ tests/rptest/tests/schema_registry_test.py
@@ -138,6 +138,15 @@ class SchemaRegistryTest(RedpandaTest):
             f"{self._base_uri()}/subjects/{subject}{'?permanent=true' if permanent else ''}",
             headers=headers)
 
+    def _delete_subject_version(self,
+                                subject,
+                                version,
+                                permanent=False,
+                                headers=HTTP_GET_HEADERS):
+        return requests.delete(
+            f"{self._base_uri()}/subjects/{subject}/versions/{version}{'?permanent=true' if permanent else ''}",
+            headers=headers)
+
     def _post_compatibility_subject_version(self,
                                             subject,
                                             version,
@@ -426,3 +435,69 @@ class SchemaRegistryTest(RedpandaTest):
                                           permanent=True)
         self.logger.debug(result_raw)
         assert result_raw.status_code == requests.codes.ok
+
+    @cluster(num_nodes=3)
+    def test_delete_subject_version(self):
+        """
+        Verify delete subject version
+        """
+
+        topic = create_topic_names(1)[0]
+
+        self.logger.debug(f"Register a schema against a subject")
+        schema_1_data = json.dumps({"schema": schema1_def})
+        schema_2_data = json.dumps({"schema": schema2_def})
+        schema_3_data = json.dumps({"schema": schema3_def})
+
+        self.logger.debug("Posting schema 1 as a subject key")
+        result_raw = self._post_subjects_subject_versions(
+            subject=f"{topic}-key", data=schema_1_data)
+        self.logger.debug(result_raw)
+        assert result_raw.status_code == requests.codes.ok
+
+        self.logger.debug("Set subject config - NONE")
+        result_raw = self._set_config_subject(subject=f"{topic}-key",
+                                              data=json.dumps(
+                                                  {"compatibility": "NONE"}))
+        assert result_raw.status_code == requests.codes.ok
+
+        self.logger.debug("Posting schema 2 as a subject key")
+        result_raw = self._post_subjects_subject_versions(
+            subject=f"{topic}-key", data=schema_2_data)
+        self.logger.debug(result_raw)
+        assert result_raw.status_code == requests.codes.ok
+
+        self.logger.debug("Posting schema 3 as a subject key")
+        result_raw = self._post_subjects_subject_versions(
+            subject=f"{topic}-key", data=schema_3_data)
+        self.logger.debug(result_raw)
+        assert result_raw.status_code == requests.codes.ok
+
+        self.logger.debug("Permanently delete version 2")
+        result_raw = self._delete_subject_version(subject=f"{topic}-key",
+                                                  version=2,
+                                                  permanent=True)
+        self.logger.debug(result_raw)
+        assert result_raw.status_code == requests.codes.not_found
+
+        self.logger.debug("Soft delete version 2")
+        result_raw = self._delete_subject_version(
+            subject=f"{topic}-key",
+            version=2,
+        )
+        self.logger.debug(result_raw)
+        assert result_raw.status_code == requests.codes.ok
+
+        self.logger.debug("Get versions")
+        result_raw = self._get_subjects_subject_versions(
+            subject=f"{topic}-key")
+        self.logger.debug(result_raw)
+        assert result_raw.status_code == requests.codes.ok
+        assert result_raw.json() == [1, 3]
+
+        self.logger.debug("Get versions - include deleted")
+        result_raw = self._get_subjects_subject_versions(
+            subject=f"{topic}-key", deleted=True)
+        self.logger.debug(result_raw)
+        assert result_raw.status_code == requests.codes.ok
+        assert result_raw.json() == [1, 2, 3]
