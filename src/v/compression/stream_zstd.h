69044d70b873941488217fcf07d3eec69ef04385
Noah Watkins <noahwatkins@gmail.com>
2021-07-08 19:41:32 -0700

    storage: use a fixed workspace allocation for zstd
    Generally speaking when using stream decompression a window will be
    required whose size is determiend by parameters of the compression
    process. It looks like the default comrpession settings in something
    like librdkafka result in a window size need of 2 MB. This gets larger
    the more extreme paramters are used during compression.
    
    In this PR we allocate a fixed size workspace at early bootup when
    fragmentation isn't an issue and then re-use this for the lifetime of
    the process.
    
    This ticket is tracking future improvements:
    
      https://github.com/vectorizedio/redpanda/issues/1802
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/compression/stream_zstd.h
+++ src/v/compression/stream_zstd.h
@@ -23,27 +23,23 @@ public:
       ZSTD_CCtx,
       // wrap ZSTD C API
       static_sized_deleter_fn<ZSTD_CCtx, &ZSTD_freeCCtx>>;
-    using zstd_decompress_ctx = std::unique_ptr<
-      ZSTD_DCtx,
-      // wrap ZSTD C API
-      static_sized_deleter_fn<ZSTD_DCtx, &ZSTD_freeDCtx>>;
 
     iobuf compress(const iobuf& b) { return do_compress(b); }
     iobuf uncompress(const iobuf& b) { return do_uncompress(b); }
     iobuf compress(iobuf&& b) { return do_compress(b); }
     iobuf uncompress(iobuf&& b) { return do_uncompress(b); }
 
+    static void init_workspace(size_t);
+
 private:
     iobuf do_compress(const iobuf&);
     iobuf do_uncompress(const iobuf&);
 
     void reset_compressor();
-    void reset_decompressor();
     zstd_compress_ctx& compressor();
-    zstd_decompress_ctx& decompressor();
+    ZSTD_DCtx* decompressor();
 
     zstd_compress_ctx _compress{nullptr};
-    zstd_decompress_ctx _decompress{nullptr};
 };
 
 } // namespace compression
