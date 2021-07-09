aa8b020936ea19cde3beddf4f3544efa211cd198
Pankaj Patil <pankaj.patil2099@hotmail.com>
2021-07-09 22:05:17 +0530

    changed kv map to absl node map from flat hash

--- src/v/storage/kvstore.h
+++ src/v/storage/kvstore.h
@@ -25,7 +25,7 @@
 #include <seastar/core/semaphore.hh>
 #include <seastar/core/timer.hh>
 
-#include <absl/container/flat_hash_map.h>
+#include <absl/container/node_hash_map.h>
 
 namespace storage {
 
@@ -144,7 +144,7 @@ private:
     ss::semaphore _sem{0};
     ss::lw_shared_ptr<segment> _segment;
     model::offset _next_offset;
-    absl::flat_hash_map<bytes, iobuf, bytes_type_hash, bytes_type_eq> _db;
+    absl::node_hash_map<bytes, iobuf, bytes_type_hash, bytes_type_eq> _db;
 
     ss::future<> put(key_space ks, bytes key, std::optional<iobuf> value);
     void apply_op(bytes key, std::optional<iobuf> value);
