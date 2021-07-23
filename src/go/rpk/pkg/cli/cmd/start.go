a4d37d6f6fb7da58d242063ce36af4fb033d83fb
David Castillo <david@vectorized.io>
2021-07-23 09:27:04 -0500

    rpk: Fix darwin builds
    The use of unix.Fallocate in create_sized_file.go unveiled a set
    of files that were being built for Mac even though they shouldn't
    be. Changes made to fix this:
    
    - Add the "+build linux" build directive in the linux-only files.
    - Create an "empty" file (src/go/rpk/pkg/cli/cmd/redpanda/package.go)
      to prevent darwin builds from failing, since all the other files in the
      "cmd/redpanda" package are excluded.
    - Make `rpk mode` only available on Linux.

--- src/go/rpk/pkg/cli/cmd/start.go
+++ src/go/rpk/pkg/cli/cmd/start.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Vectorized, Inc.
+// Copyright 2021 Vectorized, Inc.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.md
@@ -6,6 +6,8 @@
 // As of the Change Date specified in that file, in accordance with
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
+
+// +build linux
 
 package cmd
 
