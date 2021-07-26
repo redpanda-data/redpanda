e282be8cde5bbb78b030b306cf025030d303901c
Rafal Korepta <rafal@vectorized.io>
2021-07-26 19:31:14 +0200

    operator: Generate custom resource definition

--- src/go/k8s/apis/redpanda/v1alpha1/cluster_webhook_test.go
+++ src/go/k8s/apis/redpanda/v1alpha1/cluster_webhook_test.go
@@ -16,7 +16,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/vectorizedio/redpanda/src/go/k8s/apis/redpanda/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
