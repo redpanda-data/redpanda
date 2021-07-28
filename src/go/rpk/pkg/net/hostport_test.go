67fbdc17e7be71e3e55f0eba38df68506527f441
Travis Bischel <travis.bischel+github@gmail.com>
2021-07-28 16:10:24 -0600

    net: add ParseHostMaybeScheme
    Parsing hostnames that optionally have a scheme and optionally have a
    port is tricky. This new function parses everything properly and has
    strict validation on the well-formed-ness of schemes, hosts, and ports.

---
+++ src/go/rpk/pkg/net/hostport_test.go
@@ -0,0 +1,165 @@
+package net
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseHostMaybeScheme(t *testing.T) {
+	for _, test := range []struct {
+		name  string
+		input string
+
+		expScheme string
+		expHost   string
+		expErr    bool
+	}{
+		{
+			name:    "valid just host with path slash",
+			input:   "foo.com/",
+			expHost: "foo.com",
+		},
+
+		{
+			name:    "valid hostport with path slash",
+			input:   "foo.com:8080/",
+			expHost: "foo.com:8080",
+		},
+
+		{
+			name:    "valid hostport no path slash",
+			input:   "foo.com:8080",
+			expHost: "foo.com:8080",
+		},
+
+		{
+			name:      "valid scheme://host",
+			input:     "scheme://foo.com",
+			expScheme: "scheme",
+			expHost:   "foo.com",
+		},
+
+		{
+			name:      "valid scheme://host:port",
+			input:     "scheme://foo.com:0",
+			expScheme: "scheme",
+			expHost:   "foo.com:0",
+		},
+
+		{
+			name:   "invalid missing slashes in scheme:host:port",
+			input:  "scheme:foo.com:0",
+			expErr: true,
+		},
+
+		{
+			name:   "invalid missing numbers in port",
+			input:  "scheme://foo.com:",
+			expErr: true,
+		},
+
+		{
+			name:   "invalid scheme",
+			input:  "scheme_bar://foo.com",
+			expErr: true,
+		},
+
+		{
+			name:   "invalid port",
+			input:  "scheme://foo.com:9d",
+			expErr: true,
+		},
+
+		{
+			name:   "completely invalid",
+			input:  "(",
+			expErr: true,
+		},
+
+		{
+			name:   "empty is invalid",
+			input:  "",
+			expErr: true,
+		},
+
+		// TestIsDomain and TestIsIP below offer more enhanced host
+		// validation.
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			scheme, host, err := ParseHostMaybeScheme(test.input)
+
+			gotErr := err != nil
+			if gotErr != test.expErr {
+				t.Errorf("input %q: got err? %v, exp err? %v",
+					test.input, gotErr, test.expErr)
+				return
+			}
+			if test.expErr {
+				return
+			}
+
+			if scheme != test.expScheme {
+				t.Errorf("input %q: got scheme %q != exp %q",
+					test.input, scheme, test.expScheme)
+			}
+			if host != test.expHost {
+				t.Errorf("input %q: got host %q != exp %q",
+					test.input, host, test.expHost)
+			}
+		})
+	}
+}
+
+func TestIsDomain(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		input    string
+		expValid bool
+	}{
+		{"valid", "foo.com.", true},
+		{"underscores and dashes can be anywhere in middle of label", "a.0-_-0.foo.com", true},
+		{"labels can be just numbers", "0.com", true},
+
+		{"tld requires alphabet", "foo.a0", false},
+		{"cannot just be tld", "foo", false},
+		{"invalid duplicate dots", "foo..com", false},
+		{"label cannot start with dash", "a.-bar.foo.com", false},
+		{"label cannot end with dash", "a.bar-.foo.com", false},
+
+		{"labels can be up to 63 characters", strings.Repeat("a", 63) + ".com", true},
+		{"labels cannot be more than 63 characters", strings.Repeat("a", 64) + ".com", false},
+
+		{"the entire domain can be exactly 255 characters", strings.Repeat("a.", 125) + "abcde", true},        // 250 + 5
+		{"the entire domain cannot be more than 255 characters", strings.Repeat("a.", 125) + "abcdef", false}, // 250 + 6
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			got := isDomain(test.input)
+			if got != test.expValid {
+				t.Errorf("input %q: got valid? %v, expected? %v",
+					test.input, got, test.expValid)
+			}
+		})
+	}
+}
+
+func TestIsIP(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		input    string
+		expValid bool
+	}{
+		{"valid 4", "0.0.0.0", true},
+		{"invalid 4", "0.0.0", false},
+		{"valid 6", "[::0]", true},
+		{"valid 6 missing braces is invalid", "::0", false},
+		{"valid 4 cannot be in braces", "[0.0.0.0]", false},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			got := isIP(test.input)
+			if got != test.expValid {
+				t.Errorf("input %q: got valid? %v, expected? %v",
+					test.input, got, test.expValid)
+			}
+		})
+	}
+}
