24c5b43a7bcf9c7a8cb9e9d5445df19a98fa40bc
Ben Pope <ben@vectorized.io>
2021-08-05 16:28:48 +0100

    schema_registry/avro: Add tests for union
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/schema_registry/test/compatibility_avro.cc
+++ src/v/pandaproxy/schema_registry/test/compatibility_avro.cc
@@ -43,6 +43,12 @@ BOOST_AUTO_TEST_CASE(test_avro_enum) {
     // BOOST_REQUIRE(check_compatible(enum3, enum2_def));
 }
 
+BOOST_AUTO_TEST_CASE(test_avro_union) {
+    BOOST_REQUIRE(check_compatible(union2, union0));
+
+    BOOST_REQUIRE(!check_compatible(union1, union0));
+}
+
 BOOST_AUTO_TEST_CASE(test_avro_basic_backwards_compat) {
     // Backward compatibility: A new schema is backward compatible if it can be
     // used to read the data written in the previous schema.
