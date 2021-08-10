911b3c4b1930495b1c1182a8e65a0978b2556759
John Spray <jcs@vectorized.io>
2021-08-10 18:28:40 +0100

    tools: do addr2lines conversion on unit test failure output
    The downside of this is that we're going to output all
    stderr output in one go at the end, rather than it being
    roughly interleaved with stdout output.
    
    Signed-off-by: John Spray <jcs@vectorized.io>

--- tools/cmake_test.py
+++ tools/cmake_test.py
@@ -16,6 +16,7 @@ import tempfile
 import random
 import string
 import shutil
+import subprocess
 from string import Template
 
 sys.path.append(os.path.dirname(__file__))
@@ -64,6 +65,51 @@ class TestRunner():
         return tempfile.mkdtemp(suffix=self._gen_alphanum(),
                                 prefix="%s/test." % self.root)
 
+    def _addr2lines(self, backtrace):
+        if not backtrace:
+            # Maybe we saw a 'Backtrace:' line with no following
+            # address lines, and got an empty list.  Ignore it.
+            return
+
+        # Find our build directory by searching back from binary
+        path_parts = self.binary.split("/")
+        try:
+            vbuild = "/".join(path_parts[0:path_parts.index("vbuild") + 3])
+        except (ValueError, IndexError):
+            sys.stderr.write(
+                f"Could not find vbuild in binary path {self.binary}")
+            return
+        else:
+            addr2line_script = os.path.join(
+                vbuild,
+                "v_deps_build/seastar-prefix/src/seastar/scripts/seastar-addr2line"
+            )
+
+        ran = subprocess.run([addr2line_script, "-e", self.binary],
+                             input="\n".join(backtrace),
+                             encoding='utf-8',
+                             capture_output=True)
+        ran.check_returncode()
+
+        sys.stderr.write("Captured a Seastar backtrace:\n")
+        sys.stderr.write(ran.stderr)
+        sys.stderr.write(ran.stdout)
+
+    def _capture_backtraces(self, stderr):
+        backtrace = None
+        for line in stderr.split("\n"):
+            if backtrace is None and line.startswith("Backtrace:"):
+                backtrace = []
+            elif backtrace is not None:
+                if line.startswith("  "):
+                    backtrace.append(line)
+                else:
+                    self._addr2lines(backtrace)
+                    backtrace = None
+
+        if backtrace:
+            self._addr2lines(backtrace)
+
     def run(self):
         test_dir = self._gen_testdir()
         env = os.environ.copy()
@@ -88,16 +134,32 @@ class TestRunner():
             shutil.copy(src, "%s/%s" % (test_dir, os.path.basename(dst)))
 
         cmd = Template(
-            "(cd $test_dir; $prepare_command; $binary $args; $post_command; e=$$?; "
+            "(cd $test_dir; $prepare_command; $binary $args; $post_command e=$$?; "
             "rm -rf $test_dir; echo \"Test Exit code $$e\"; exit $$e)"
         ).substitute(test_dir=test_dir,
                      prepare_command=" && ".join(self.prepare_command)
                      or "true",
-                     post_command=" && ".join(self.post_command) or "true",
+                     post_command=" && ".join(self.post_command) +
+                     ";" if self.post_command else "",
                      binary=self.binary,
                      args=self.test_args)
         logger.info(cmd)
-        os.execle("/bin/bash", "/bin/bash", "-exc", cmd, env)
+        # os.execle("/bin/bash", "/bin/bash", "-exc", cmd, env)
+
+        # We only capture stderr becasuse that's where backtraces go
+        ran = subprocess.run(cmd,
+                             shell=True,
+                             stderr=subprocess.PIPE,
+                             encoding='utf-8')
+        sys.stderr.write(ran.stderr)
+
+        if ran.returncode != 0:
+            # If there was at least one test failure, process the output
+            # to extract Seastar backtraces, and run them through
+            # seastar-addr2line to produce human readable output.
+            self._capture_backtraces(ran.stderr)
+
+        sys.exit(ran.returncode)
 
 
 def main():
