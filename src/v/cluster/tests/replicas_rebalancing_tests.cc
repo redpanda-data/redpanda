d6c589c85ce025578bd05a91b8bba7d703e78310
Michal Maslanka <michal@vectorized.io>
2021-08-23 11:23:54 +0200

    c/tests: checking if all operations finished in rebalancing test
    Added check if all node operations have finished in nodes rebalancing
    test. This way we will return an error if partition operations are not
    finished.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>
    (cherry picked from commit 183d4df21114651242f17afe960c1b1ef1b6b7ee)

--- src/v/cluster/tests/replicas_rebalancing_tests.cc
+++ src/v/cluster/tests/replicas_rebalancing_tests.cc
@@ -1,8 +1,10 @@
 
+#include "cluster/controller_api.h"
 #include "cluster/metadata_cache.h"
 #include "cluster/tests/rebalancing_tests_fixture.h"
 #include "model/metadata.h"
 #include "model/namespace.h"
+#include "ssx/future-util.h"
 #include "test_utils/async.h"
 
 #include <absl/container/node_hash_map.h>
@@ -54,6 +56,42 @@ void wait_for_even_replicas_distribution(
       .get();
 }
 
+void wait_for_all_partition_moves_to_finish(
+  cluster::metadata_cache& cache, cluster::controller_api& api) {
+    tests::cooperative_spin_wait_with_timeout(60s, [&cache, &api] {
+        auto topics = cache.all_topics();
+        return ss::do_with(
+          cache.all_topics(),
+          [&api](std::vector<model::topic_namespace>& topics) {
+              return ssx::parallel_transform(
+                       topics.begin(),
+                       topics.end(),
+                       [&api](const model::topic_namespace& tp) {
+                           return api.get_reconciliation_state(tp).then(
+                             [](auto res) {
+                                 if (res.has_error()) {
+                                     return false;
+                                 }
+                                 auto states = std::move(res.value());
+                                 return std::all_of(
+                                   states.begin(),
+                                   states.end(),
+                                   [](const cluster::ntp_reconciliation_state&
+                                        st) {
+                                       return st.status()
+                                              == cluster::
+                                                reconciliation_status::done;
+                                   });
+                             });
+                       })
+                .then([](std::vector<bool> results) {
+                    return std::all_of(
+                      results.begin(), results.end(), [](bool r) { return r; });
+                });
+          });
+    }).get();
+}
+
 FIXTURE_TEST(test_adding_single_node_to_cluster, rebalancing_tests_fixture) {
     start_cluster(3);
     // in total we have 24 partition replicas
@@ -65,6 +103,9 @@ FIXTURE_TEST(test_adding_single_node_to_cluster, rebalancing_tests_fixture) {
 
     // add node
     add_node(10);
+    wait_for_all_partition_moves_to_finish(
+      apps.begin()->second->metadata_cache.local(),
+      apps.begin()->second->controller->get_api().local());
 
     // wait until all the nodes will host between 4 and 8 replicas
     wait_for_even_replicas_distribution(
@@ -80,13 +121,15 @@ FIXTURE_TEST(test_adding_multiple_nodes, rebalancing_tests_fixture) {
     populate_all_topics_with_data();
     // add node
     add_node(2);
-
     // wait until all nodes will host between 16 and 20 partitons
     wait_for_even_replicas_distribution(
       14, 22, model::node_id(2), apps.begin()->second->metadata_cache.local());
 
     // add second node
     add_node(3);
+    wait_for_all_partition_moves_to_finish(
+      apps.begin()->second->metadata_cache.local(),
+      apps.begin()->second->controller->get_api().local());
     // wait until all nodes will host between 10 and 14 partitons
     wait_for_even_replicas_distribution(
       8, 16, model::node_id(3), apps.begin()->second->metadata_cache.local());
