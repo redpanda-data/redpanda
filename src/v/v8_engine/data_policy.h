93f8a28f80b61765d9911e752fa3f05531ce6910
Vadim <vadim@vectorized.io>
2021-08-26 13:26:57 +0300

    v8_engine: Add data-policy property
    It contains function name and script name for v8_runtime.
    Will be used in fetch request for data transform

---
+++ src/v/v8_engine/data_policy.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/vectorizedio/redpanda/blob/master/licenses/rcl.md
+ */
+
+#pragma once
+
+#include "json/json.h"
+#include "reflection/adl.h"
+#include "seastarx.h"
+#include "vlog.h"
+
+#include <seastar/core/sstring.hh>
+
+#include <exception>
+#include <string_view>
+
+namespace v8_engine {
+
+// Datapolicy property for v8_engine. In first version it contains only
+// function_name and script_name, in the future it will contain ACLs, geo,
+// e.t.c.
+class data_policy {
+public:
+    static constexpr int8_t version{1};
+
+    data_policy(ss::sstring fn, ss::sstring sn) noexcept
+      : _function_name(std::move(fn))
+      , _script_name(std::move(sn)) {}
+
+    const ss::sstring& function_name() const { return _function_name; }
+    const ss::sstring& script_name() const { return _script_name; }
+
+private:
+    ss::sstring _function_name;
+    ss::sstring _script_name;
+
+    friend std::ostream&
+    operator<<(std::ostream& os, const data_policy& datapolicy);
+};
+
+} // namespace v8_engine
+
+namespace reflection {
+
+template<>
+struct adl<v8_engine::data_policy> {
+    void to(iobuf& out, v8_engine::data_policy&& dp);
+    v8_engine::data_policy from(iobuf_parser& in);
+};
+
+} // namespace reflection
