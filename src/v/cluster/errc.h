d62fd885c096cf5579dd436545be6400fc3a512d
Vadim <vadim@vectorized.io>
2021-08-27 18:16:25 +0300

    cluster: Add data_policy_manager for data_policy_cmd from log
    In the future it will deliver updates to v8 scripts_dispatcher
    It handles cmds from replicated log

--- src/v/cluster/errc.h
+++ src/v/cluster/errc.h
@@ -51,6 +51,8 @@ enum class errc : int16_t {
     partition_configuration_in_joint_mode,
     partition_configuration_leader_config_not_committed,
     partition_configuration_differs,
+    data_policy_already_exists,
+    data_policy_not_exists,
 
 };
 struct errc_category final : public std::error_category {
@@ -138,6 +140,10 @@ struct errc_category final : public std::error_category {
             return "Partition configuration wasn't committed on the leader";
         case errc::partition_configuration_differs:
             return "Current and requested partition configuration differs";
+        case errc::data_policy_already_exists:
+            return "Data-policy already exists";
+        case errc::data_policy_not_exists:
+            return "Data-policy does not exist";
         }
         return "cluster::errc::unknown";
     }
