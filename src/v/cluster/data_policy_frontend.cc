3d066930fe53a26fbf6466bc2215230886b98983
Vadim <vadim@vectorized.io>
2021-08-27 13:37:20 +0300

    cluster: add data_policy_frontend
    Fronted can create 2 commands (create, delete):
    - create is used for create or change data-policy
    - delete is used for delete data-policy

---
+++ src/v/cluster/data_policy_frontend.cc
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#include "cluster/data_policy_frontend.h"
+
+#include "cluster/cluster_utils.h"
+
+#include <optional>
+
+namespace cluster {
+
+data_policy_frontend::data_policy_frontend(
+  ss::sharded<controller_stm>& stm, ss::sharded<ss::abort_source>& as) noexcept
+  : _stm(stm)
+  , _as(as) {}
+
+ss::future<std::error_code> data_policy_frontend::create_data_policy(
+  model::topic_namespace topic,
+  v8_engine::data_policy dp,
+  model::timeout_clock::time_point tout) {
+    create_data_policy_cmd_data cmd_data{.dp = std::move(dp)};
+    create_data_policy_cmd cmd(std::move(topic), std::move(cmd_data));
+    return replicate_and_wait(_stm, _as, std::move(cmd), tout);
+}
+
+ss::future<std::error_code> data_policy_frontend::clear_data_policy(
+  model::topic_namespace topic, model::timeout_clock::time_point tout) {
+    delete_data_policy_cmd cmd(std::move(topic), std::nullopt);
+    return replicate_and_wait(_stm, _as, std::move(cmd), tout);
+}
+
+} // namespace cluster
---
+++ src/v/cluster/data_policy_frontend.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "cluster/controller_stm.h"
+#include "cluster/fwd.h"
+#include "model/timeout_clock.h"
+#include "v8_engine/data_policy.h"
+
+#include <seastar/core/abort_source.hh>
+#include <seastar/core/sharded.hh>
+
+namespace cluster {
+
+class data_policy_frontend final {
+public:
+    data_policy_frontend(
+      ss::sharded<controller_stm>&, ss::sharded<ss::abort_source>&) noexcept;
+
+    ss::future<std::error_code> create_data_policy(
+      model::topic_namespace,
+      v8_engine::data_policy,
+      model::timeout_clock::time_point);
+
+    ss::future<std::error_code> clear_data_policy(
+      model::topic_namespace, model::timeout_clock::time_point);
+
+private:
+    ss::sharded<controller_stm>& _stm;
+    ss::sharded<ss::abort_source>& _as;
+};
+
+} // namespace cluster
