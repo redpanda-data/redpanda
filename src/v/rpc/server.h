5be92e2ec3cafc473625cfd3fbf1a3ec2147e911
Michal Maslanka <michal@vectorized.io>
2021-09-03 14:42:36 +0200

    rpc/server: enabled two phase shutdown of rpc server
    Added possibility to shutdown RPC server in two phases. This way
    shutting down RPC server (at least it first phase) is not dependent from
    all services involved in request processing. This approach allow us to
    decouple RPC shutdown sequence from other services shutdown.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/rpc/server.h
+++ src/v/rpc/server.h
@@ -76,6 +76,21 @@ public:
         _proto = std::move(proto);
     }
     void start();
+
+    /**
+     * The RPC server can be shutdown in two phases. First phase initiated with
+     * `shutdown_input` prevents server from accepting new requests and
+     * connections. In second phases `wait_for_shutdown` caller waits for all
+     * pending requests to finish. This interface is convinient as it allows
+     * stopping the server without waiting for downstream services to stop
+     * requests processing
+     */
+    void shutdown_input();
+    ss::future<> wait_for_shutdown();
+    /**
+     * Stop function is a nop when `shutdown_input` was previously called. Left
+     * here for convenience when dealing with `seastar::sharded` type
+     */
     ss::future<> stop();
 
     const server_configuration cfg; // NOLINT
