e63b6e55f84fff6eb78814a26bb723a02cae44ff
Simon <simon@vectorized.io>
2021-09-08 19:59:20 +0200

    operator: add APIListener interface to incentivize code reuse
    (cherry picked from commit 2b159b72301fb50901f843e5d7a31c93f4d9e0d3)

---
+++ src/go/k8s/pkg/resources/certmanager/type_helpers.go
@@ -0,0 +1,80 @@
+package certmanager
+
+import redpandav1alpha1 "github.com/vectorizedio/redpanda/src/go/k8s/apis/redpanda/v1alpha1"
+
+// Helper functions and types for Listeners
+
+// APIListener is a generic API Listener
+type APIListener interface {
+	// GetPort returns API port
+	GetPort() int
+	// GetTLS returns API TLSConfig
+	GetTLS() *redpandav1alpha1.TLSConfig
+	// GetExternal returns API's ExternalConnectivityConfig
+	GetExternal() *redpandav1alpha1.ExternalConnectivityConfig
+}
+
+func kafkaAPIListeners(r *redpandav1alpha1.Cluster) []APIListener {
+	listeners := []APIListener{}
+	for _, el := range r.Spec.Configuration.KafkaAPI {
+		listeners = append(listeners, el)
+	}
+	return listeners
+}
+
+func adminAPIListeners(r *redpandav1alpha1.Cluster) []APIListener {
+	listeners := []APIListener{}
+	for _, el := range r.Spec.Configuration.AdminAPI {
+		listeners = append(listeners, el)
+	}
+	return listeners
+}
+
+func schemaRegistryAPIListeners(r *redpandav1alpha1.Cluster) []APIListener {
+	if r.Spec.Configuration.SchemaRegistry == nil {
+		return []APIListener{}
+	}
+
+	return []APIListener{*r.Spec.Configuration.SchemaRegistry}
+}
+
+// PandaProxyAPIListeners returns all PandaProxyAPI listeners
+func pandaProxyAPIListeners(r *redpandav1alpha1.Cluster) []APIListener {
+	listeners := []APIListener{}
+	for _, el := range r.Spec.Configuration.PandaproxyAPI {
+		listeners = append(listeners, el)
+	}
+	return listeners
+}
+
+func getExternalTLSListener(listeners []APIListener) APIListener {
+	tlsListener := getTLSListener(listeners)
+	if tlsListener == nil {
+		return nil
+	}
+	if ext := tlsListener.GetExternal(); ext != nil && ext.Enabled {
+		return tlsListener
+	}
+	return nil
+}
+
+func getInternalTLSListener(listeners []APIListener) APIListener {
+	tlsListener := getTLSListener(listeners)
+	if tlsListener == nil {
+		return nil
+	}
+	if ext := tlsListener.GetExternal(); ext == nil || !ext.Enabled {
+		return tlsListener
+	}
+	return nil
+}
+
+func getTLSListener(listeners []APIListener) APIListener {
+	for _, el := range listeners {
+		tls := el.GetTLS()
+		if tls != nil && tls.Enabled {
+			return el
+		}
+	}
+	return nil
+}
