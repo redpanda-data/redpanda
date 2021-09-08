15924c0225d661fba3b9efaedcd946df908aa7b6
Simon <simon@vectorized.io>
2021-09-08 20:00:23 +0200

    operator: refactor apis cert generation
    (cherry picked from commit 6a84570d5622a8d4baade519e4ef3931be8aeadc)

--- src/go/k8s/pkg/resources/certmanager/kafka_api.go
+++ src/go/k8s/pkg/resources/certmanager/kafka_api.go
@@ -9,16 +9,7 @@
 
 package certmanager
 
-import (
-	"context"
-
-	cmmetav1 "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
-	"github.com/vectorizedio/redpanda/src/go/k8s/apis/redpanda/v1alpha1"
-	"github.com/vectorizedio/redpanda/src/go/k8s/pkg/resources"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
-)
+import "k8s.io/apimachinery/pkg/types"
 
 const (
 	kafkaAPI = "kafka"
@@ -53,97 +44,4 @@ func (r *PkiReconciler) NodeCert() types.NamespacedName {
 		}
 	}
 	return types.NamespacedName{Name: r.pandaCluster.Name + "-" + RedpandaNodeCert, Namespace: r.pandaCluster.Namespace}
-}
-
-func (r *PkiReconciler) prepareKafkaAPI(
-	ctx context.Context,
-	issuerRef *cmmetav1.ObjectReference,
-	tlsListener *v1alpha1.KafkaAPITLS,
-	keystoreSecret *types.NamespacedName,
-) ([]resources.Resource, error) {
-	toApply := []resources.Resource{}
-
-	externalIssuerRef := tlsListener.IssuerRef
-	nodeSecretRef := tlsListener.NodeSecretRef
-
-	if nodeSecretRef == nil {
-		certName := NewCertName(r.pandaCluster.Name, RedpandaNodeCert)
-		certsKey := types.NamespacedName{Name: string(certName), Namespace: r.pandaCluster.Namespace}
-		nodeIssuerRef := issuerRef
-		if externalIssuerRef != nil {
-			// if external issuer is provided, we will use it to generate node certificates
-			nodeIssuerRef = externalIssuerRef
-		}
-
-		externalListener := r.pandaCluster.ExternalListener()
-		internalListener := r.pandaCluster.InternalListener()
-		dnsNames := []string{}
-		if internalListener != nil && internalListener.TLS.Enabled {
-			dnsNames = append(dnsNames, r.internalFQDN)
-		}
-		if externalListener != nil && externalListener.TLS.Enabled && externalListener.External.Subdomain != "" {
-			dnsNames = append(dnsNames, externalListener.External.Subdomain)
-		}
-		redpandaCert := NewNodeCertificate(r.Client, r.scheme, r.pandaCluster, certsKey, nodeIssuerRef, dnsNames, EmptyCommonName, keystoreSecret, r.logger)
-
-		toApply = append(toApply, redpandaCert)
-	}
-
-	if nodeSecretRef != nil && nodeSecretRef.Namespace != r.pandaCluster.Namespace {
-		if err := r.copyNodeSecretToLocalNamespace(ctx, nodeSecretRef); err != nil {
-			return nil, err
-		}
-	}
-
-	if tlsListener.RequireClientAuth {
-		// Certificate for external clients to call the Kafka API on any broker in this Redpanda cluster
-		userClientCn := NewCommonName(r.pandaCluster.Name, UserClientCert)
-		userClientKey := types.NamespacedName{Name: string(userClientCn), Namespace: r.pandaCluster.Namespace}
-		externalClientCert := NewCertificate(r.Client, r.scheme, r.pandaCluster, userClientKey, issuerRef, userClientCn, false, keystoreSecret, r.logger)
-
-		// Certificate for operator to call the Kafka API on any broker in this Redpanda cluster
-		operatorClientCn := NewCommonName(r.pandaCluster.Name, OperatorClientCert)
-		operatorClientKey := types.NamespacedName{Name: string(operatorClientCn), Namespace: r.pandaCluster.Namespace}
-		internalClientCert := NewCertificate(r.Client, r.scheme, r.pandaCluster, operatorClientKey, issuerRef, operatorClientCn, false, keystoreSecret, r.logger)
-
-		// Certificate for admin to call the Kafka API on any broker in this Redpanda cluster
-		adminClientCn := NewCommonName(r.pandaCluster.Name, AdminClientCert)
-		adminClientKey := types.NamespacedName{Name: string(adminClientCn), Namespace: r.pandaCluster.Namespace}
-		adminClientCert := NewCertificate(r.Client, r.scheme, r.pandaCluster, adminClientKey, issuerRef, adminClientCn, false, keystoreSecret, r.logger)
-
-		toApply = append(toApply, externalClientCert, internalClientCert, adminClientCert)
-	}
-
-	return toApply, nil
-}
-
-// Creates copy of secret in Redpanda cluster's namespace
-func (r *PkiReconciler) copyNodeSecretToLocalNamespace(
-	ctx context.Context, secretRef *corev1.ObjectReference,
-) error {
-	var secret corev1.Secret
-	err := r.Get(ctx, types.NamespacedName{Name: secretRef.Name, Namespace: secretRef.Namespace}, &secret)
-	if err != nil {
-		return err
-	}
-
-	tlsKey := secret.Data[corev1.TLSPrivateKeyKey]
-	tlsCrt := secret.Data[corev1.TLSCertKey]
-	caCrt := secret.Data[cmmetav1.TLSCAKey]
-
-	caSecret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      r.NodeCert().Name,
-			Namespace: r.NodeCert().Namespace,
-			Labels:    secret.Labels,
-		},
-		Type: secret.Type,
-		Data: map[string][]byte{
-			cmmetav1.TLSCAKey:       caCrt,
-			corev1.TLSCertKey:       tlsCrt,
-			corev1.TLSPrivateKeyKey: tlsKey,
-		},
-	}
-	_, err = resources.CreateIfNotExists(ctx, r, caSecret, r.logger)
-	return err
 }
--- src/go/k8s/pkg/resources/certmanager/pki.go
+++ src/go/k8s/pkg/resources/certmanager/pki.go
@@ -12,17 +12,23 @@ package certmanager
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	cmmetav1 "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
 	redpandav1alpha1 "github.com/vectorizedio/redpanda/src/go/k8s/apis/redpanda/v1alpha1"
 	"github.com/vectorizedio/redpanda/src/go/k8s/pkg/resources"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ resources.Reconciler = &PkiReconciler{}
+var (
+	_             resources.Reconciler = &PkiReconciler{}
+	errNoDNSNames                      = fmt.Errorf("failed to generate node TLS certificate")
+)
 
 // RootCert cert name
 const RootCert = "rootcert"
@@ -56,7 +62,6 @@ func (r *PkiReconciler) prepareRoot(
 	prefix string,
 ) ([]resources.Resource, *cmmetav1.ObjectReference) {
 	toApply := []resources.Resource{}
-
 	selfSignedIssuer := NewIssuer(r.Client,
 		r.scheme,
 		r.pandaCluster,
@@ -91,39 +96,42 @@ func (r *PkiReconciler) prepareRoot(
 // Ensure will manage PKI for redpanda.vectorized.io custom resource
 func (r *PkiReconciler) Ensure(ctx context.Context) error {
 	toApply := []resources.Resource{}
-	tlsListener := r.pandaCluster.KafkaTLSListener()
 
 	keystoreKey := types.NamespacedName{Name: keystoreName(r.pandaCluster.Name), Namespace: r.pandaCluster.Namespace}
 	keystoreSecret := NewKeystoreSecretResource(r.Client, r.scheme, r.pandaCluster, keystoreKey, r.logger)
 
 	toApply = append(toApply, keystoreSecret)
 
-	if tlsListener != nil {
-		toApplyRootKafka, kafkaIssuerRef := r.prepareRoot(kafkaAPI)
-		toApplyKafka, err := r.prepareKafkaAPI(ctx, kafkaIssuerRef, &tlsListener.TLS, &keystoreKey)
+	if kafkaListeners := kafkaAPIListeners(r.pandaCluster); len(kafkaListeners) > 0 {
+		toApplyAPI, err := r.prepareAPI(ctx, kafkaAPI, RedpandaNodeCert, []string{OperatorClientCert, UserClientCert, AdminClientCert}, kafkaListeners, &keystoreKey)
 		if err != nil {
 			return err
 		}
-		toApply = append(toApply, toApplyRootKafka...)
-		toApply = append(toApply, toApplyKafka...)
+		toApply = append(toApply, toApplyAPI...)
 	}
 
-	if r.pandaCluster.AdminAPITLS() != nil {
-		toApplyRootAdmin, adminIssuerRef := r.prepareRoot(adminAPI)
-		toApply = append(toApply, toApplyRootAdmin...)
-		toApply = append(toApply, r.prepareAdminAPI(adminIssuerRef, &keystoreKey)...)
+	if adminListeners := adminAPIListeners(r.pandaCluster); len(adminListeners) > 0 {
+		toApplyAPI, err := r.prepareAPI(ctx, adminAPI, adminAPINodeCert, []string{adminAPIClientCert}, adminListeners, &keystoreKey)
+		if err != nil {
+			return err
+		}
+		toApply = append(toApply, toApplyAPI...)
 	}
 
-	if r.pandaCluster.PandaproxyAPITLS() != nil {
-		toApplyRootPandaproxy, pandaproxyIssuerRef := r.prepareRoot(pandaproxyAPI)
-		toApply = append(toApply, toApplyRootPandaproxy...)
-		toApply = append(toApply, r.preparePandaproxyAPI(pandaproxyIssuerRef, &keystoreKey)...)
+	if pandaProxyListeners := pandaProxyAPIListeners(r.pandaCluster); len(pandaProxyListeners) > 0 {
+		toApplyAPI, err := r.prepareAPI(ctx, pandaproxyAPI, pandaproxyAPINodeCert, []string{pandaproxyAPIClientCert}, pandaProxyListeners, &keystoreKey)
+		if err != nil {
+			return err
+		}
+		toApply = append(toApply, toApplyAPI...)
 	}
 
-	if r.pandaCluster.Spec.Configuration.SchemaRegistry != nil && r.pandaCluster.Spec.Configuration.SchemaRegistry.TLS != nil {
-		toApplyRootPandaproxy, schemaRegistryIssuerRef := r.prepareRoot(schemaRegistryAPI)
-		toApply = append(toApply, toApplyRootPandaproxy...)
-		toApply = append(toApply, r.prepareSchemaRegistryAPI(schemaRegistryIssuerRef, &keystoreKey)...)
+	if schemaRegistryListeners := schemaRegistryAPIListeners(r.pandaCluster); len(schemaRegistryListeners) > 0 {
+		toApplyAPI, err := r.prepareAPI(ctx, schemaRegistryAPI, schemaRegistryAPINodeCert, []string{schemaRegistryAPIClientCert}, schemaRegistryListeners, &keystoreKey)
+		if err != nil {
+			return err
+		}
+		toApply = append(toApply, toApplyAPI...)
 	}
 
 	for _, res := range toApply {
@@ -138,4 +146,105 @@ func (r *PkiReconciler) Ensure(ctx context.Context) error {
 
 func (r *PkiReconciler) issuerNamespacedName(name string) types.NamespacedName {
 	return types.NamespacedName{Name: r.pandaCluster.Name + "-" + name, Namespace: r.pandaCluster.Namespace}
+}
+
+func (r *PkiReconciler) prepareAPI(
+	ctx context.Context,
+	rootCertSuffix string,
+	nodeCertSuffix string,
+	clientCerts []string,
+	listeners []APIListener,
+	keystoreSecret *types.NamespacedName,
+) ([]resources.Resource, error) {
+	var (
+		tlsListener         = getTLSListener(listeners)
+		toApply             = []resources.Resource{}
+		externalTLSListener = getExternalTLSListener(listeners)
+		internalTLSListener = getInternalTLSListener(listeners)
+		// Issuer for the nodes
+		nodeIssuerRef *cmmetav1.ObjectReference
+	)
+
+	if tlsListener == nil || tlsListener.GetTLS() == nil || !tlsListener.GetTLS().Enabled {
+		return []resources.Resource{}, nil
+	}
+
+	toApplyRoot, rootIssuerRef := r.prepareRoot(rootCertSuffix)
+	toApply = append(toApply, toApplyRoot...)
+	nodeIssuerRef = rootIssuerRef
+
+	if tlsListener.GetTLS().IssuerRef != nil {
+		// if external issuer is provided, we will use it to generate node certificates
+		nodeIssuerRef = tlsListener.GetTLS().IssuerRef
+	}
+
+	nodeSecretRef := tlsListener.GetTLS().NodeSecretRef
+	if nodeSecretRef == nil || nodeSecretRef.Name == "" {
+		certName := NewCertName(r.pandaCluster.Name, nodeCertSuffix)
+		certsKey := types.NamespacedName{Name: string(certName), Namespace: r.pandaCluster.Namespace}
+		dnsNames := []string{}
+
+		if internalTLSListener != nil {
+			dnsNames = append(dnsNames, r.clusterFQDN, r.internalFQDN)
+		}
+		// TODO(#2256): Add support for external listener + TLS certs for IPs
+		if externalTLSListener != nil && externalTLSListener.GetExternal().Subdomain != "" {
+			dnsNames = append(dnsNames, externalTLSListener.GetExternal().Subdomain)
+		}
+
+		if len(dnsNames) == 0 {
+			return nil, fmt.Errorf("failed to generate node TLS certificate %s. If external is enabled, please add a subdomain: %w", certName, errNoDNSNames)
+		}
+
+		nodeCert := NewNodeCertificate(r.Client, r.scheme, r.pandaCluster, certsKey, nodeIssuerRef, dnsNames, EmptyCommonName, keystoreSecret, r.logger)
+		toApply = append(toApply, nodeCert)
+	}
+
+	if nodeSecretRef != nil && nodeSecretRef.Name != "" && nodeSecretRef.Namespace != r.pandaCluster.Namespace {
+		if err := r.copyNodeSecretToLocalNamespace(ctx, nodeSecretRef); err != nil {
+			return nil, fmt.Errorf("copy node secret for %s cert group to namespace %s: %w", rootCertSuffix, nodeSecretRef.Namespace, err)
+		}
+	}
+
+	if tlsListener.GetTLS().RequireClientAuth {
+		for _, clientCertName := range clientCerts {
+			clientCn := NewCommonName(r.pandaCluster.Name, clientCertName)
+			clientKey := types.NamespacedName{Name: string(clientCn), Namespace: r.pandaCluster.Namespace}
+			clientCert := NewCertificate(r.Client, r.scheme, r.pandaCluster, clientKey, rootIssuerRef, clientCn, false, keystoreSecret, r.logger)
+			toApply = append(toApply, clientCert)
+		}
+	}
+
+	return toApply, nil
+}
+
+// Creates copy of secret in Redpanda cluster's namespace
+func (r *PkiReconciler) copyNodeSecretToLocalNamespace(
+	ctx context.Context, secretRef *corev1.ObjectReference,
+) error {
+	var secret corev1.Secret
+	err := r.Get(ctx, types.NamespacedName{Name: secretRef.Name, Namespace: secretRef.Namespace}, &secret)
+	if err != nil {
+		return err
+	}
+
+	tlsKey := secret.Data[corev1.TLSPrivateKeyKey]
+	tlsCrt := secret.Data[corev1.TLSCertKey]
+	caCrt := secret.Data[cmmetav1.TLSCAKey]
+
+	caSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secret.Name,
+			Namespace: r.pandaCluster.Namespace,
+			Labels:    secret.Labels,
+		},
+		Type: secret.Type,
+		Data: map[string][]byte{
+			cmmetav1.TLSCAKey:       caCrt,
+			corev1.TLSCertKey:       tlsCrt,
+			corev1.TLSPrivateKeyKey: tlsKey,
+		},
+	}
+	_, err = resources.CreateIfNotExists(ctx, r, caSecret, r.logger)
+	return err
 }
