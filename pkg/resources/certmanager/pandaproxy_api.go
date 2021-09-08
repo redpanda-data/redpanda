ea4b2150c17b25aa84cef8cb56157d872f23c85a
Simon <simon@vectorized.io>
2021-09-08 20:00:23 +0200

    (split) operator: refactor apis cert generation

--- pkg/resources/certmanager/pandaproxy_api.go
+++ pkg/resources/certmanager/pandaproxy_api.go
@@ -8,14 +8,9 @@
 // by the Apache License, Version 2.0
 
 // Package certmanager contains resources for TLS certificate handling using cert-manager
-// nolint:dupl // this is similar to the admin pki
 package certmanager
 
-import (
-	cmmeta "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
-	"github.com/vectorizedio/redpanda/src/go/k8s/pkg/resources"
-	"k8s.io/apimachinery/pkg/types"
-)
+import "k8s.io/apimachinery/pkg/types"
 
 const (
 	pandaproxyAPI           = "proxy"
@@ -31,34 +26,4 @@ func (r *PkiReconciler) PandaproxyAPINodeCert() types.NamespacedName {
 // PandaproxyAPIClientCert returns the namespaced name for the Pandaproxy API certificate used by clients
 func (r *PkiReconciler) PandaproxyAPIClientCert() types.NamespacedName {
 	return types.NamespacedName{Name: r.pandaCluster.Name + "-" + pandaproxyAPIClientCert, Namespace: r.pandaCluster.Namespace}
-}
-
-func (r *PkiReconciler) preparePandaproxyAPI(
-	issuerRef *cmmeta.ObjectReference, keystoreSecret *types.NamespacedName,
-) []resources.Resource {
-	toApply := []resources.Resource{}
-
-	// Redpanda cluster certificate for Pandaproxy API - to be provided to each broker
-	cn := NewCommonName(r.pandaCluster.Name, pandaproxyAPINodeCert)
-	certsKey := types.NamespacedName{Name: string(cn), Namespace: r.pandaCluster.Namespace}
-
-	dnsName := []string{r.internalFQDN}
-	externalListener := r.pandaCluster.PandaproxyAPIExternal()
-	if externalListener != nil && externalListener.External.Subdomain != "" {
-		dnsName = append(dnsName, externalListener.External.Subdomain)
-	}
-
-	nodeCert := NewNodeCertificate(r.Client, r.scheme, r.pandaCluster, certsKey, issuerRef, dnsName, cn, keystoreSecret, r.logger)
-	toApply = append(toApply, nodeCert)
-
-	if r.pandaCluster.PandaproxyAPITLS() != nil && r.pandaCluster.PandaproxyAPITLS().TLS.RequireClientAuth {
-		// Certificate for calling the Pandaproxy API on any broker
-		cn := NewCommonName(r.pandaCluster.Name, pandaproxyAPIClientCert)
-		clientCertsKey := types.NamespacedName{Name: string(cn), Namespace: r.pandaCluster.Namespace}
-		proxyClientCert := NewCertificate(r.Client, r.scheme, r.pandaCluster, clientCertsKey, issuerRef, cn, false, keystoreSecret, r.logger)
-
-		toApply = append(toApply, proxyClientCert)
-	}
-
-	return toApply
 }
