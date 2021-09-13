95540ff923fe04008ece80dda0ca51c399fa7b6d
John Spray <jcs@vectorized.io>
2021-09-13 15:57:29 +0100

    tests/partition_movement_test: fix not covering replicas=1
    These tuples are clearly meant to vary the replica count,
    but were only testing replicas=3 twice.
    
    Signed-off-by: John Spray <jcs@vectorized.io>

--- tests/rptest/tests/partition_movement_test.py
+++ tests/rptest/tests/partition_movement_test.py
@@ -219,7 +219,7 @@ class PartitionMovementTest(EndToEndTest):
 
         topics = []
         for partition_count in range(1, 5):
-            for replication_factor in (3, 3):
+            for replication_factor in (1, 3):
                 name = f"topic{len(topics)}"
                 spec = TopicSpec(name=name,
                                  partition_count=partition_count,
@@ -241,7 +241,7 @@ class PartitionMovementTest(EndToEndTest):
 
         topics = []
         for partition_count in range(1, 5):
-            for replication_factor in (3, 3):
+            for replication_factor in (1, 3):
                 name = f"topic{len(topics)}"
                 spec = TopicSpec(name=name,
                                  partition_count=partition_count,
