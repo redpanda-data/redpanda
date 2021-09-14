redpanda-data/redpanda

commit 4662dd4fe5722a1c89ed48a75a76962c4871d59f

franz-go, kmsg: bump to latest (1.0), fix breakage
1.0 is now stable, at least for the kgo package.

kmsg itself is a dedicated submodule.

src/go/rpk/pkg/cli/cmd/cluster/metadata.go
@@ -178,7 +178,9 @@ func printBrokers(controllerID int32, brokers []kmsg.MetadataResponseBroker) {
 
 func PrintTopics(topics []kmsg.MetadataResponseTopic, internal, detailed bool) {
 	sort.Slice(topics, func(i, j int) bool {
-		return topics[i].Topic < topics[j].Topic
+		// Topics is only non-nil if we requested with topic IDs,
+		// which we are not doing.
+		return *topics[i].Topic < *topics[j].Topic
 	})
 
 	if !detailed {
@@ -212,7 +214,7 @@ func PrintTopics(topics []kmsg.MetadataResponseTopic, internal, detailed bool) {
 		}
 
 		// "foo, 20 partitions, 3 replicas"
-		fmt.Fprintf(buf, "%s", topic.Topic)
+		fmt.Fprintf(buf, "%s", *topic.Topic)
 		if topic.IsInternal {
 			fmt.Fprint(buf, " (internal)")
 		}

src/go/rpk/go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/docker/go-connections v0.4.0
 	github.com/docker/go-units v0.4.0
 	github.com/fatih/color v1.7.0
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/uuid v1.1.1
 	github.com/hashicorp/go-multierror v1.1.0
@@ -46,8 +47,8 @@ require (
 	github.com/spf13/viper v1.7.0
 	github.com/stretchr/testify v1.7.0
 	github.com/tklauser/go-sysconf v0.1.0
-	github.com/twmb/franz-go v0.10.2
-	github.com/twmb/franz-go/pkg/kmsg v0.0.0-20210823212011-0d01f7456b4d
+	github.com/twmb/franz-go v1.0.0
+	github.com/twmb/franz-go/pkg/kmsg v0.0.0-20210913185252-b87af2dffd6a
 	github.com/twmb/tlscfg v1.2.0
 	github.com/xdg/scram v1.0.3
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5

src/go/rpk/go.sum
@@ -137,7 +137,6 @@ github.com/golang/protobuf v1.4.2/go.mod h1:oDoupMAO8OvCJWAcko0GGGIgR6R6ocIYbsSw
 github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db/go.mod h1:/XxbfmMg8lxefKM7IXC3fBNl/7bRcc72aCRzEWrmP2Q=
 github.com/golang/snappy v0.0.1/go.mod h1:/XxbfmMg8lxefKM7IXC3fBNl/7bRcc72aCRzEWrmP2Q=
 github.com/golang/snappy v0.0.2/go.mod h1:/XxbfmMg8lxefKM7IXC3fBNl/7bRcc72aCRzEWrmP2Q=
-github.com/golang/snappy v0.0.3/go.mod h1:/XxbfmMg8lxefKM7IXC3fBNl/7bRcc72aCRzEWrmP2Q=
 github.com/golang/snappy v0.0.4 h1:yAGX7huGHXlcLOEtBnF4w7FQwA26wojNCwOYAEhLjQM=
 github.com/golang/snappy v0.0.4/go.mod h1:/XxbfmMg8lxefKM7IXC3fBNl/7bRcc72aCRzEWrmP2Q=
 github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c/go.mod h1:lNA+9X1NB3Zf8V7Ke586lFgjr2dZNuvo3lPJSGZ5JPQ=
@@ -231,8 +230,8 @@ github.com/kisielk/errcheck v1.1.0/go.mod h1:EZBBE59ingxPouuu3KfxchcWSUPOHkagtvW
 github.com/kisielk/gotool v1.0.0/go.mod h1:XhKaO+MFFWcvkIS/tQcRk01m1F5IRFswLeQ+oQHNcck=
 github.com/klauspost/compress v1.10.10/go.mod h1:aoV0uJVorq1K+umq18yTdKaF57EivdYsUV+/s2qKfXs=
 github.com/klauspost/compress v1.11.7/go.mod h1:aoV0uJVorq1K+umq18yTdKaF57EivdYsUV+/s2qKfXs=
-github.com/klauspost/compress v1.13.4 h1:0zhec2I8zGnjWcKyLl6i3gPqKANCCn5e9xmviEEeX6s=
-github.com/klauspost/compress v1.13.4/go.mod h1:8dP1Hq4DHOhN9w426knH3Rhby4rFm6D8eO+e+Dq5Gzg=
+github.com/klauspost/compress v1.13.5 h1:9O69jUPDcsT9fEm74W92rZL9FQY7rCdaXVneq+yyzl4=
+github.com/klauspost/compress v1.13.5/go.mod h1:/3/Vjq9QcHkK5uEr5lBEmyoZ1iFhe47etQ6QUkpK6sk=
 github.com/konsorten/go-windows-terminal-sequences v1.0.1/go.mod h1:T0+1ngSBFLxvqU3pZ+m/2kptfBszLMUkC4ZK/EgS/cQ=
 github.com/konsorten/go-windows-terminal-sequences v1.0.2 h1:DB17ag19krx9CFsz4o3enTrPXyIXCl+2iCXH/aMAp9s=
 github.com/konsorten/go-windows-terminal-sequences v1.0.2/go.mod h1:T0+1ngSBFLxvqU3pZ+m/2kptfBszLMUkC4ZK/EgS/cQ=
@@ -394,10 +393,11 @@ github.com/tklauser/go-sysconf v0.1.0/go.mod h1:h54uFIrVIJBr8RXt3F5JJdxVkmFeallW
 github.com/tklauser/numcpus v0.1.0 h1:BQeAuOQKZtytv8qblsFbi9mhTfXQdFFkyX0vaV6B4tc=
 github.com/tklauser/numcpus v0.1.0/go.mod h1:i3up9VjARpkV00NkBbexuDd0RAVQz4rV5Gl8miYgd5k=
 github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5/go.mod h1:ncp9v5uamzpCO7NfCPTXjqaC+bZgJeR0sMTm6dMHP7U=
-github.com/twmb/franz-go v0.10.2 h1:xg6kFR6CmtSNRJAzUuu+S/l9ThKYbwPn6AEvbqD5hw8=
-github.com/twmb/franz-go v0.10.2/go.mod h1:MmlCzU1afJbPlAEKimYwYOv1ICIJcrIu7gaKMPt/xS0=
-github.com/twmb/franz-go/pkg/kmsg v0.0.0-20210823212011-0d01f7456b4d h1:8k2HiP1zjY6N+OvNrBR/621PafQ2h1qdiu+JwT+WAk0=
-github.com/twmb/franz-go/pkg/kmsg v0.0.0-20210823212011-0d01f7456b4d/go.mod h1:SxG/xJKhgPu25SamAq0rrucfp7lbzCpEXOC+vH/ELrY=
+github.com/twmb/franz-go v1.0.0 h1:JcsqEImDhr7H/eQx/V58fwzjmVi2FRwz6dyylmdJ0NU=
+github.com/twmb/franz-go v1.0.0/go.mod h1:cdFLk8d/5/ox88y38xgiDKP3Yo338OO0t5QbTEM2K6I=
+github.com/twmb/franz-go/pkg/kmsg v0.0.0-20210901051457-3c197a133ddd/go.mod h1:SxG/xJKhgPu25SamAq0rrucfp7lbzCpEXOC+vH/ELrY=
+github.com/twmb/franz-go/pkg/kmsg v0.0.0-20210913185252-b87af2dffd6a h1:swOnegqiMoZbIFdpWgQST5YKjVjVbacurGZNwOaOL0o=
+github.com/twmb/franz-go/pkg/kmsg v0.0.0-20210913185252-b87af2dffd6a/go.mod h1:SxG/xJKhgPu25SamAq0rrucfp7lbzCpEXOC+vH/ELrY=
 github.com/twmb/go-rbtree v1.0.0 h1:KxN7dXJ8XaZ4cvmHV1qqXTshxX3EBvX/toG5+UR49Mg=
 github.com/twmb/go-rbtree v1.0.0/go.mod h1:UlIAI8gu3KRPkXSobZnmJfVwCJgEhD/liWzT5ppzIyc=
 github.com/twmb/tlscfg v1.2.0 h1:WCzLHtmnVJ94+veAO4TLTB1ENx7TPYLkTl4Q6WFF4Vo=
@@ -473,8 +473,8 @@ golang.org/x/net v0.0.0-20200822124328-c89045814202/go.mod h1:/O7V0waA8r7cgGh81R
 golang.org/x/net v0.0.0-20201021035429-f5854403a974/go.mod h1:sp8m0HH+o8qH0wwXwYZr8TS3Oi6o0r6Gce1SSxlDquU=
 golang.org/x/net v0.0.0-20210222171744-9060382bd457/go.mod h1:m0MpNAwzfU5UDzcl9v0D8zg8gWTRqZa9RBIspLL5mdg=
 golang.org/x/net v0.0.0-20210226172049-e18ecbb05110/go.mod h1:m0MpNAwzfU5UDzcl9v0D8zg8gWTRqZa9RBIspLL5mdg=
-golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d h1:LO7XpTYMwTqxjLcGWPijK3vRXg1aWdlNOVOHRq45d7c=
-golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d/go.mod h1:9nx3DQGgdP8bBQD5qxJ1jj9UTztislL4KSBs9R2vV5Y=
+golang.org/x/net v0.0.0-20210825183410-e898025ed96a h1:bRuuGXV8wwSdGTB+CtJf+FjgO1APK1CoO39T4BN/XBw=
+golang.org/x/net v0.0.0-20210825183410-e898025ed96a/go.mod h1:9nx3DQGgdP8bBQD5qxJ1jj9UTztislL4KSBs9R2vV5Y=
 golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be/go.mod h1:N/0e6XlmueqKjAGxoOufVs8QHGRruUQn6yWY3a++T0U=
 golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421/go.mod h1:gOpvHmFTYa4IltrdGE7lF6nIHvwfUNPOp7c8zoXwtLw=
 golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45/go.mod h1:gOpvHmFTYa4IltrdGE7lF6nIHvwfUNPOp7c8zoXwtLw=

src/go/rpk/pkg/kafka/client_franz.go
@@ -36,7 +36,7 @@ func NewFranzClient(fs afero.Fs, cfg *config.Config) (*kgo.Client, error) {
 		kgo.RetryTimeout(5 * time.Second),
 
 		kgo.ProduceRequestTimeout(5 * time.Second),
-		kgo.RecordTimeout(8 * time.Second),
+		kgo.RecordDeliveryTimeout(8 * time.Second),
 	}
 
 	if k.SASL != nil {

commit 7b90ff2c20cc82d2afd479c5b3e29e2b7cf4eaa0

cluster metadata: dereference the topic pointer
I thought I caught all of the breakage in
4662dd4fe5722a1c89ed48a75a76962c4871d59f,
but derefing the topic in the response was missed in one location.

I relied on go vet, but in this instance, the function `Print` takes a
bunch of interfaces, so the pointer was just fine.

src/go/rpk/pkg/cli/cmd/cluster/metadata.go
@@ -196,7 +196,7 @@ func PrintTopics(topics []kmsg.MetadataResponseTopic, internal, detailed bool) {
 			if parts > 0 {
 				replicas = len(topic.Partitions[0].Replicas)
 			}
-			tw.Print(topic.Topic, parts, replicas)
+			tw.Print(*topic.Topic, parts, replicas)
 		}
 		return
 	}