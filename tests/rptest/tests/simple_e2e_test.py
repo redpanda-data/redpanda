4136986e13684d3d9b64ea59e0691c6cfc9ce516
Michal Maslanka <michal@vectorized.io>
2021-09-16 09:15:24 +0200

    ducky/e2e: added test checking e2e flow in face of log eviction
    Signed-off-by: Michal Maslanka <michal@vectorized.io>
    (cherry picked from commit f9c2866a6214ea6ea32788fa02c41e41e83c72c2)

---
+++ tests/rptest/tests/simple_e2e_test.py
@@ -0,0 +1,37 @@
+import random
+import time
+
+from ducktape.mark.resource import cluster
+from ducktape.utils.util import wait_until
+import requests
+
+from rptest.clients.types import TopicSpec
+from rptest.tests.end_to_end import EndToEndTest
+
+
+class SimpleEndToEndTest(EndToEndTest):
+    @cluster(num_nodes=9)
+    def test_correctness_while_evicitng_log(self):
+        '''
+        Validate that all the records will be delivered to consumers when there 
+        are multiple producers and log is evicted
+        '''
+        # use small segment size to enable log eviction
+        self.start_redpanda(num_nodes=3,
+                            extra_rp_conf={
+                                "log_segment_size": 1048576,
+                                "retention_bytes": 5242880,
+                                "default_topic_replications": 3,
+                            })
+
+        spec = TopicSpec(name="topic", partition_count=1, replication_factor=1)
+        self.redpanda.create_topic(spec)
+        self.topic = spec.name
+
+        self.start_producer(5, throughput=10000)
+        self.start_consumer(1)
+        self.await_startup()
+
+        self.run_validation(min_records=100000,
+                            producer_timeout_sec=120,
+                            consumer_timeout_sec=120)
