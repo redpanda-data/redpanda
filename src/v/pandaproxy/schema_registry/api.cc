9bbf4d4192c1632732e01ddea74d89117194b21c
Ben Pope <ben@vectorized.io>
2021-09-20 20:34:52 +0100

    schema_registry: Introduce api.h
    Manages schema registry object lifetimes.
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

---
+++ src/v/pandaproxy/schema_registry/api.cc
@@ -0,0 +1,66 @@
+// Copyright 2021 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#include "pandaproxy/schema_registry/api.h"
+
+#include "kafka/client/client.h"
+#include "kafka/client/configuration.h"
+#include "model/metadata.h"
+#include "pandaproxy/schema_registry/configuration.h"
+#include "pandaproxy/schema_registry/seq_writer.h"
+#include "pandaproxy/schema_registry/service.h"
+#include "pandaproxy/schema_registry/sharded_store.h"
+
+#include <seastar/core/coroutine.hh>
+
+#include <functional>
+#include <memory>
+
+namespace pandaproxy::schema_registry {
+api::api(
+  model::node_id node_id,
+  ss::smp_service_group sg,
+  size_t max_memory,
+  kafka::client::configuration& client_cfg,
+  configuration& cfg) noexcept
+  : _node_id{node_id}
+  , _sg{sg}
+  , _max_memory{max_memory}
+  , _client_cfg{client_cfg}
+  , _cfg{cfg} {}
+
+api::~api() noexcept = default;
+
+ss::future<> api::start() {
+    _store = std::make_unique<sharded_store>();
+    co_await _store->start(_sg);
+    co_await _client.start(config::to_yaml(_client_cfg));
+    co_await _sequencer.start(
+      _node_id, _sg, std::ref(_client), std::ref(*_store));
+    co_await _service.start(
+      config::to_yaml(_cfg),
+      _sg,
+      _max_memory,
+      std::ref(_client),
+      std::ref(*_store),
+      std::ref(_sequencer));
+
+    co_await _service.invoke_on_all(&service::start);
+}
+
+ss::future<> api::stop() {
+    // co_await _service.invoke_on_all(&service::stop);
+
+    co_await _service.stop();
+    co_await _sequencer.stop();
+    co_await _client.stop();
+    co_await _store->stop();
+}
+
+} // namespace pandaproxy::schema_registry
---
+++ src/v/pandaproxy/schema_registry/api.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "kafka/client/fwd.h"
+#include "model/metadata.h"
+#include "pandaproxy/schema_registry/fwd.h"
+#include "seastarx.h"
+
+#include <seastar/core/sharded.hh>
+
+namespace YAML {
+class Node;
+}
+
+namespace pandaproxy::schema_registry {
+
+class api {
+public:
+    api(
+      model::node_id node_id,
+      ss::smp_service_group sg,
+      size_t max_memory,
+      kafka::client::configuration& client_cfg,
+      configuration& cfg) noexcept;
+    ~api() noexcept;
+
+    ss::future<> start();
+    ss::future<> stop();
+
+private:
+    model::node_id _node_id;
+    ss::smp_service_group _sg;
+    size_t _max_memory;
+    kafka::client::configuration& _client_cfg;
+    configuration& _cfg;
+
+    ss::sharded<kafka::client::client> _client;
+    std::unique_ptr<pandaproxy::schema_registry::sharded_store> _store;
+    ss::sharded<pandaproxy::schema_registry::service> _service;
+    ss::sharded<pandaproxy::schema_registry::seq_writer> _sequencer;
+};
+
+} // namespace pandaproxy::schema_registry
+
+namespace google::protobuf {
+
+class FileDescriptor;
+
+}
