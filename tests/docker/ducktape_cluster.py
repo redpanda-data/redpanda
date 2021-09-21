acf7f94b8fc4d7033dd446ba75ec70e2dfe2bb70
Michal Maslanka <michal@vectorized.io>
2021-09-21 09:49:08 +0200

    ducky: parametrized output path in cluster file generating script
    When cluster file is generated we want it to be in build directory to
    prevent modifying source tree when running tests locally.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- tests/docker/ducktape_cluster.py
+++ tests/docker/ducktape_cluster.py
@@ -7,10 +7,10 @@ import os
 # of nodes (i.e. the --scale parameter to docker-compose)
 
 scale = int(sys.argv[1])
+out_file = sys.argv[2]
 template_file = os.path.join(os.path.dirname(__file__),
                              'ducktape_cluster.json.j2')
 template = jinja2.Template(open(template_file).read())
-out_file = os.path.join(os.path.dirname(__file__), 'ducktape_cluster.json')
 
 print(f"Writing ducktape config for {scale} nodes to {out_file}")
 open(out_file, 'w').write(template.render(scale=scale))
