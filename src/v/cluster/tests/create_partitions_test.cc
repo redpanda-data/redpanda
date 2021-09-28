cf9065b5b51111a3922dfdc37c731d23a3d596c0
Michal Maslanka <michal@vectorized.io>
2021-09-28 09:46:37 +0200

    cluster: fixed semantics of create partitions api
    Previous implementation of create partitions API assumed that requested
    number of partitions will be added to current topic partition count.
    This assumption is incorrect since requested number of partitions is the
    new total number of partitions for given topic. Adjusted implementation
    and tests to account for that change in semantics.
    
    Fixes: #2393
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/cluster/tests/create_partitions_test.cc
+++ src/v/cluster/tests/create_partitions_test.cc
@@ -15,13 +15,20 @@ model::topic_namespace make_tp_ns(const ss::sstring& tp) {
 
 FIXTURE_TEST(test_creating_partitions, rebalancing_tests_fixture) {
     start_cluster(3);
+    /**
+     * Test setup:
+     * topic test-1, 3 paritions, replication factor 1
+     * topic test-2, 4 paritions, replication factor 3
+     */
     create_topic(create_topic_cfg("test-1", 3, 1));
     create_topic(create_topic_cfg("test-2", 4, 3));
 
+    // topic test-1, increase partition count to 6
     cluster::create_partititions_configuration cfg_test_1(
-      make_tp_ns("test-1"), 3);
+      make_tp_ns("test-1"), 6);
+    // topic test-2, increase partition count to 9
     cluster::create_partititions_configuration cfg_test_2(
-      make_tp_ns("test-2"), 5);
+      make_tp_ns("test-2"), 9);
 
     auto res = (*get_leader_node_application())
                  ->controller->get_topics_frontend()
@@ -38,14 +45,38 @@ FIXTURE_TEST(test_creating_partitions, rebalancing_tests_fixture) {
     auto tp_2_md = topics.local().get_topic_metadata(make_tp_ns("test-2"));
 
     // total 6 partitions
-    BOOST_REQUIRE_EQUAL(tp_1_md->partitions.size(), 3 + 3);
+    BOOST_REQUIRE_EQUAL(tp_1_md->partitions.size(), 6);
     for (auto i = 0; i < 6; ++i) {
         BOOST_REQUIRE_EQUAL(tp_1_md->partitions[i].id, model::partition_id(i));
     }
 
     // total 9 partitions
-    BOOST_REQUIRE_EQUAL(tp_2_md->partitions.size(), 4 + 5);
+    BOOST_REQUIRE_EQUAL(tp_2_md->partitions.size(), 9);
     for (auto i = 0; i < 9; ++i) {
         BOOST_REQUIRE_EQUAL(tp_2_md->partitions[i].id, model::partition_id(i));
     }
+}
+
+FIXTURE_TEST(test_error_handling, rebalancing_tests_fixture) {
+    start_cluster(3);
+    /**
+     * Test setup:
+     * topic test-1, 3 paritions, replication factor 1
+     */
+    create_topic(create_topic_cfg("test-1", 3, 1));
+
+    // topic test-1, try decreasing partition count
+    cluster::create_partititions_configuration cfg_test_1(
+      make_tp_ns("test-1"), 2);
+
+    auto res = (*get_leader_node_application())
+                 ->controller->get_topics_frontend()
+                 .local()
+                 .create_partitions({cfg_test_1}, model::no_timeout)
+                 .get();
+
+    BOOST_REQUIRE_EQUAL(res.size(), 1);
+    BOOST_REQUIRE_EQUAL(
+      res[0].ec,
+      cluster::make_error_code(cluster::errc::topic_invalid_partitions));
 }
