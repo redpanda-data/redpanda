c66385ef10f6d53393a8eb24ba6ddcdf4014fa1c
Travis Bischel <travis.bischel+github@gmail.com>
2021-09-30 16:06:26 -0600

    rpk topic describe: deeply sort configs before printing
    This makes the output configs consistently sorted, which is nice.

--- src/go/rpk/pkg/cli/cmd/topic/describe.go
+++ src/go/rpk/pkg/cli/cmd/topic/describe.go
@@ -21,6 +21,7 @@ import (
 	"github.com/twmb/franz-go/pkg/kerr"
 	"github.com/twmb/franz-go/pkg/kgo"
 	"github.com/twmb/franz-go/pkg/kmsg"
+	"github.com/twmb/types"
 	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/config"
 	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/kafka"
 	"github.com/vectorizedio/redpanda/src/go/rpk/pkg/out"
@@ -132,6 +133,7 @@ partitions section. By default, the summary and configs sections are printed.
 
 				tw := out.NewTable("KEY", "VALUE", "SOURCE")
 				defer tw.Flush()
+				types.Sort(resp)
 				for _, config := range resp.Resources[0].Configs {
 					var val string
 					if config.IsSensitive {
