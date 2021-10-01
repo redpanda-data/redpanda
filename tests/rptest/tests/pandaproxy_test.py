1196eb2db4e505be88c2f394ef7bd5c97709821c
John Spray <jcs@vectorized.io>
2021-10-01 15:49:04 +0100

    tests: disable pandaproxy test_consumer_group_json_v2
    Pending investigation of:
    https://github.com/vectorizedio/redpanda/issues/2501
    
    Signed-off-by: John Spray <jcs@vectorized.io>

--- tests/rptest/tests/pandaproxy_test.py
+++ tests/rptest/tests/pandaproxy_test.py
@@ -13,6 +13,7 @@ import logging
 import uuid
 import requests
 from ducktape.mark.resource import cluster
+from ducktape.mark import ignore
 from ducktape.utils.util import wait_until
 
 from rptest.clients.types import TopicSpec
@@ -746,6 +747,7 @@ class PandaProxyTest(RedpandaTest):
         rc_res = c0.remove()
         assert rc_res.status_code == requests.codes.no_content
 
+    @ignore  # https://github.com/vectorizedio/redpanda/issues/2501
     @cluster(num_nodes=3)
     def test_consumer_group_json_v2(self):
         """
