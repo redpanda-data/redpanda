0448e1a7bce738b8f6fb4e13d993006b162d0224
Ben Pope <ben@vectorized.io>
2021-10-03 23:15:15 +0100

    schema_registry/handlers: Support getting references
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/schema_registry/requests/get_subject_versions_version.h
+++ src/v/pandaproxy/schema_registry/requests/get_subject_versions_version.h
@@ -28,10 +28,30 @@ inline void rjson_serialize(
     w.StartObject();
     w.Key("subject");
     ::json::rjson_serialize(w, res.schema.sub());
-    w.Key("id");
-    ::json::rjson_serialize(w, res.id);
     w.Key("version");
     ::json::rjson_serialize(w, res.version);
+    w.Key("id");
+    ::json::rjson_serialize(w, res.id);
+    auto type = res.schema.type();
+    if (type != schema_type::avro) {
+        w.Key("schemaType");
+        ::json::rjson_serialize(w, to_string_view(type));
+    }
+    if (!res.schema.refs().empty()) {
+        w.Key("references");
+        w.StartArray();
+        for (const auto& ref : res.schema.refs()) {
+            w.StartObject();
+            w.Key("name");
+            ::json::rjson_serialize(w, ref.name);
+            w.Key("subject");
+            ::json::rjson_serialize(w, ref.sub);
+            w.Key("version");
+            ::json::rjson_serialize(w, ref.version);
+            w.EndObject();
+        }
+        w.EndArray();
+    }
     w.Key("schema");
     ::json::rjson_serialize(w, res.schema.def().raw());
     w.EndObject();
---
+++ src/v/pandaproxy/schema_registry/requests/test/get_subject_versions_version.cc
@@ -0,0 +1,61 @@
+// Copyright 2021 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#include "pandaproxy/schema_registry/requests/get_subject_versions_version.h"
+
+#include "seastarx.h"
+
+#include <seastar/testing/thread_test_case.hh>
+
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+#include <type_traits>
+
+namespace ppj = pandaproxy::json;
+namespace pps = pandaproxy::schema_registry;
+
+SEASTAR_THREAD_TEST_CASE(test_post_subject_versions_version_response) {
+    const ss::sstring escaped_schema_def{
+      R"({\"type\":\"record\",\"name\":\"test\",\"fields\":[{\"type\":\"string\",\"name\":\"field1\"},{\"type\":\"com.acme.Referenced\",\"name\":\"int\"}]})"};
+    const pps::canonical_schema_definition schema_def{
+      R"({"type":"record","name":"test","fields":[{"type":"string","name":"field1"},{"type":"com.acme.Referenced","name":"int"}]})",
+      pps::schema_type::avro};
+    const pps::subject sub{"imported-ref"};
+
+    pps::post_subject_versions_version_response response{
+      .schema{
+        pps::subject{"imported-ref"},
+        schema_def,
+        {{{"com.acme.Referenced"},
+          pps::subject{"childSubject"},
+          pps::schema_version{1}}}},
+      .id{12},
+      .version{2}};
+
+    const ss::sstring expected{
+      R"(
+{
+  "subject": "imported-ref",
+  "version": 2,
+  "id": 12,
+  "references": [
+    {
+       "name": "com.acme.Referenced",
+       "subject":  "childSubject",
+       "version": 1
+    }
+  ],
+  "schema": ")"
+      + escaped_schema_def + R"("})"};
+
+    auto result{ppj::rjson_serialize(response)};
+
+    BOOST_REQUIRE_EQUAL(ppj::minify(expected), result);
+}
