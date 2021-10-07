graphcareful/redpanda-1

cluster: metadata_cache query materialized status

src/v/cluster/metadata_cache.h
@@ -59,6 +59,10 @@ public:
     /// Returns list of all topics that exists in the cluster.
     std::vector<model::topic_namespace> all_topics() const;
 
+    ///\brief Returns coprocessor status
+    std::optional<model::topic>
+      get_materialized_status(model::topic_namespace_view) const;
+
     ///\brief Returns metadata of single topic.
     ///
     /// If topic does not exists it returns an empty optional

redpanda-data/redpanda

src/v/cluster/metadata_cache.h
@@ -59,6 +59,10 @@ public:
     /// Returns list of all topics that exists in the cluster.
     std::vector<model::topic_namespace> all_topics() const;
 
+    ///\brief Returns coprocessor status
+    std::optional<model::topic>
+      get_source_topic(model::topic_namespace_view) const;
+
     ///\brief Returns metadata of single topic.
     ///
     /// If topic does not exists it returns an empty optional