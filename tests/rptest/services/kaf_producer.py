f02e14c8150a25bdef04e5fc198c554d34fb4cee
John Spray <jcs@vectorized.io>
2021-10-28 13:44:14 +0100

    tests/KafProducer: improve shutdown (again)
    Detaching the SSH session appears not to have
    reliably stopped the bash process running
    the for loop.  Grab the shell's PID at startup
    and kill it explicitly.
    
    Fixes: https://github.com/vectorizedio/redpanda/issues/2795
    
    Signed-off-by: John Spray <jcs@vectorized.io>

--- tests/rptest/services/kaf_producer.py
+++ tests/rptest/services/kaf_producer.py
@@ -22,17 +22,21 @@ class KafProducer(BackgroundThreadService):
         self._stopping = Event()
 
     def _worker(self, _idx, node):
-        cmd = f"for (( i=0; i < {self._num_records}; i++ )) ; do export KEY=key-$(printf %08d $i) ; export VALUE=record-$(printf %08d $i) ; echo $VALUE | kaf produce -b {self._redpanda.brokers()} --key $KEY {self._topic} ; done"
+        cmd = f"echo $$ ; for (( i=0; i < {self._num_records}; i++ )) ; do export KEY=key-$(printf %08d $i) ; export VALUE=record-$(printf %08d $i) ; echo $VALUE | kaf produce -b {self._redpanda.brokers()} --key $KEY {self._topic} ; done"
 
         self._stopping.clear()
+        self._pid = None
         try:
             out_iter = node.account.ssh_capture(cmd, timeout_sec=10)
             for line in out_iter:
-                if self._stopping.is_set():
-                    # Promptly close SSH channel to terminate the bash for loop
-                    del out_iter
-                    break
-                self.logger.debug(line.rstrip())
+                if self._pid is None:
+                    # Take first line as pid
+                    self._pid = line.strip()
+                    self._redpanda.logger.debug(
+                        f"Spawned remote shell {self._pid}")
+                    continue
+                else:
+                    self.logger.debug(line.rstrip())
         except RemoteCommandError:
             if self._stopping.is_set():
                 pass
@@ -42,6 +46,8 @@ class KafProducer(BackgroundThreadService):
     def stop_node(self, node):
         self._stopping.set()
         try:
+            if self._pid is not None:
+                node.account.signal(self._pid, 9, allow_fail=True)
             node.account.kill_process("kaf", clean_shutdown=False)
         except RemoteCommandError as e:
             if b"No such process" in e.msg:
