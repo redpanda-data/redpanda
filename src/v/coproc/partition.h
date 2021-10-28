3a26e361bb8f96a901a3d16289b944daa6eb481e
Robert Blafford <rob@vectorized.io>
2021-10-28 15:44:07 -0500

    coproc: New partition class
    - This class wraps a storage::log so that close() may be safely
    performed to the underlying log without worrying about in in progress
    writes or other concurrent operations to the log.
    
    - This works in the same way as cluster::partition. Many entities may
    have a handle to it, but when stop is called, existing entities will
    observe an exception upon further use.
    
    - Fixes #2733

---
+++ src/v/coproc/partition.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "cluster/partition.h"
+#include "storage/fwd.h"
+
+#include <seastar/core/gate.hh>
+
+namespace coproc {
+
+class partition {
+public:
+    partition(storage::log, ss::lw_shared_ptr<cluster::partition>) noexcept;
+
+    ss::future<> start() { return ss::now(); }
+    ss::future<> stop() { return _gate.close(); }
+
+    model::offset start_offset() const { return _log.offsets().start_offset; }
+    model::offset dirty_offset() const { return _log.offsets().dirty_offset; }
+
+    const model::ntp& source() const { return _source->ntp(); }
+    const model::ntp& ntp() const { return _log.config().ntp(); }
+    const storage::ntp_config& config() const { return _log.config(); }
+    bool is_leader() const { return _source->is_leader(); }
+    model::revision_id get_revision_id() const {
+        return _log.config().get_revision();
+    }
+
+    /// \brief Returns a reader that enters the \ref _gate when it performs
+    /// operations
+    ss::future<model::record_batch_reader>
+      make_reader(storage::log_reader_config);
+
+    /// \brief Returns an appender that ensures appends will not occur after
+    /// future returned from stop() resolves
+    storage::log_appender make_appender(storage::log_append_config);
+
+    /// \brief Invokes the timequery method on the \ref _log within the context
+    /// of the \ref gate
+    ss::future<std::optional<storage::timequery_result>>
+      timequery(storage::timequery_config);
+
+private:
+    ss::gate _gate;
+    storage::log _log;
+    ss::lw_shared_ptr<cluster::partition> _source;
+};
+
+} // namespace coproc
