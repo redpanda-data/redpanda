336dc2722ca9dfe6f394a681a3d03ef9e3b3e20c
Vadim Plakhtinskiy <vadim@vectorized.io>
2021-11-09 12:54:08 +0300

    v8_engine: add logger

---
+++ src/v/v8_engine/logger.cc
@@ -0,0 +1,13 @@
+// Copyright 2020 Vectorized, Inc.
+//
+// Licensed as a Redpanda Enterprise file under the Redpanda Community
+// License (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// https://github.com/vectorizedio/redpanda/blob/master/licenses/rcl.md
+
+#include "logger.h"
+
+namespace v8_engine {
+ss::logger log{"v8_engine"};
+} // namespace v8_engine
