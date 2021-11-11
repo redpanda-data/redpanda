81f7ed02435a1ca190d40a8fefb97726a4ffc918
Rob Blafford <rob@vectorized.io>
2021-11-11 12:50:28 -0500

    coproc: Wire up reconciliation backend
    (cherry picked from commit 3ed009ed61636b7f638e272cbfe6d546ea4a821e)

--- src/v/redpanda/application.cc
+++ src/v/redpanda/application.cc
@@ -785,6 +785,8 @@ void application::wire_up_redpanda_services() {
           coprocessing,
           config::node().coproc_supervisor_server(),
           std::ref(storage),
+          std::ref(controller->get_topics_state()),
+          std::ref(shard_table),
           std::ref(controller->get_topics_frontend()),
           std::ref(metadata_cache),
           std::ref(partition_manager));
