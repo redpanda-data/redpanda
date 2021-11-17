c0c177e61f8112b5923421787f519c7d64294937
Michal Maslanka <michal@vectorized.io>
2021-11-17 08:28:53 +0100

    c/tests: make timeout waiting for replicas rebalancing longer
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/cluster/tests/replicas_rebalancing_tests.cc
+++ src/v/cluster/tests/replicas_rebalancing_tests.cc
@@ -36,7 +36,7 @@ void wait_for_even_replicas_distribution(
   const cluster::metadata_cache& cache) {
     static ss::logger logger("test-log");
     tests::cooperative_spin_wait_with_timeout(
-      30s,
+      60s,
       [&cache, added_node, min_expected, max_expected] {
           auto per_node = calculate_replicas_per_node(cache);
           for (auto& [id, count] : per_node) {
