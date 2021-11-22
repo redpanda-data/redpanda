graphcareful/redpanda-1

coproc/tests: Utils for creating test batches

src/v/coproc/tests/utils/batch_utils.h
@@ -1,0 +1,22 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "model/record_batch_reader.h"
+
+/// \brief Returns the number of individual records in an entire batch readers
+/// result
+std::size_t record_sum(const model::record_batch_reader::data_t&);
+
+/// \brief Makes a batch with the exact number of records specified, across a
+/// random number of record_batches
+model::record_batch_reader make_random_batch(std::size_t n_records);

redpanda-data/redpanda

src/v/coproc/tests/utils/batch_utils.h
@@ -1,0 +1,22 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "model/record_batch_reader.h"
+
+/// \brief Returns the number of individual records in an entire batch readers
+/// result
+std::size_t num_records(const model::record_batch_reader::data_t&);
+
+/// \brief Makes a batch with the exact number of records specified, across a
+/// random number of record_batches
+model::record_batch_reader make_random_batch(std::size_t n_records);