graphcareful/redpanda-1

coproc/tests: Read until exact record count
- Due to the introduction of the kafka::client in the coproc/tests
harness, an unforceen side effect was introduced. Re-batching is
performed by the client and coproc can no longer assert the number of
record batches.

- Sometimes tests were passing when they shouldn't have because the
number of expected record batches was observed, but the number of
records was not.

- The only metrics coproc/tests should be using to assert if a test is
correct is to compare the exact number of records.

- To do this the consume() API was again modified to take a limit
parameter. This is when the consumption loop stops, when the limit
reaches the records_read local var, or if timeout has been reached.

- Call sites have been modified to use a new utility method that makes a
record batch reader with the exact records specified.

src/v/coproc/tests/failure_recovery_tests.cc
@@ -9,6 +9,7 @@
  */
 
 #include "coproc/tests/fixtures/coproc_test_fixture.h"
+#include "coproc/tests/utils/batch_utils.h"
 #include "coproc/tests/utils/coprocessor.h"
 #include "model/fundamental.h"
 #include "model/namespace.h"
@@ -44,10 +45,7 @@ FIXTURE_TEST(test_wasm_engine_restart, coproc_test_fixture) {
       [this](const std::vector<model::ntp>& ntps) -> ss::future<> {
         std::vector<ss::future<model::offset>> fs;
         for (const auto& ntp : ntps) {
-            fs.emplace_back(push(
-              ntp,
-              storage::test::make_random_memory_record_batch_reader(
-                model::offset(0), 10, 2, false)));
+            fs.emplace_back(push(ntp, make_random_batch(500)));
         }
         return ss::when_all_succeed(fs.begin(), fs.end()).discard_result();
     };
@@ -69,8 +67,10 @@ FIXTURE_TEST(test_wasm_engine_restart, coproc_test_fixture) {
     /// commit interval, the more likely this is.
     std::vector<ss::future<model::record_batch_reader::data_t>> fs;
     for (const auto& ntp : outputs) {
-        fs.emplace_back(consume(ntp));
+        fs.emplace_back(consume(ntp, (500 * 2)));
     }
-    auto data = ss::when_all_succeed(fs.begin(), fs.end()).get0();
-    BOOST_CHECK(!data[0].empty());
+    auto results = ss::when_all_succeed(fs.begin(), fs.end()).get0();
+    for (const auto& data : results) {
+        BOOST_CHECK_GE(record_sum(data), (500 * 2));
+    }
 }

src/v/coproc/tests/pacemaker_tests.cc
@@ -11,6 +11,7 @@
 #include "coproc/api.h"
 #include "coproc/pacemaker.h"
 #include "coproc/tests/fixtures/coproc_test_fixture.h"
+#include "coproc/tests/utils/batch_utils.h"
 #include "coproc/tests/utils/coprocessor.h"
 #include "coproc/types.h"
 #include "model/fundamental.h"
@@ -55,11 +56,7 @@ FIXTURE_TEST(test_coproc_router_no_results, coproc_test_fixture) {
     // Test -> Start pushing to registered topics and check that NO
     // materialized logs have been created
     model::ntp input_ntp(model::kafka_namespace, foo, model::partition_id(0));
-    push(
-      input_ntp,
-      storage::test::make_random_memory_record_batch_reader(
-        model::offset(0), 40, 1, false))
-      .get();
+    push(input_ntp, make_random_batch(40)).get();
 
     // Wait for any side-effects, ...expecting that none occur
     ss::sleep(1s).get();
@@ -69,13 +66,6 @@ FIXTURE_TEST(test_coproc_router_no_results, coproc_test_fixture) {
     /// .. but total should be exactly 11 due to the introducion of the
     /// coprocessor_internal_topic
     BOOST_REQUIRE_EQUAL(final_n_logs, 11);
-}
-
-model::record_batch_reader single_record_record_batch_reader() {
-    model::record_batch_reader::data_t batches;
-    batches.push_back(
-      storage::test::make_random_batch(model::offset(0), 1, false));
-    return model::make_memory_record_batch_reader(std::move(batches));
 }
 
 /// Tests an off-by-one error where producing recordbatches of size 1 on the
@@ -98,9 +88,9 @@ FIXTURE_TEST(test_coproc_router_off_by_one, coproc_test_fixture) {
       .get();
     auto fn =
       [this, input_ntp, output_ntp](model::offset start) -> ss::future<size_t> {
-        co_await push(input_ntp, single_record_record_batch_reader());
-        auto r = co_await consume(output_ntp, start, start + model::offset{1});
-        co_return r.size();
+        co_await push(input_ntp, make_random_batch(1));
+        auto r = co_await consume(output_ntp, 1, start);
+        co_return record_sum(r);
     };
     // Perform push/consume twice
     size_t wr = fn(model::offset(0)).get();
@@ -133,16 +123,12 @@ FIXTURE_TEST(test_coproc_router_double, coproc_test_fixture) {
       to_materialized_topic(foo, identity_coprocessor::identity_topic),
       model::partition_id(0));
 
-    push(
-      input_ntp,
-      storage::test::make_random_memory_record_batch_reader(
-        model::offset(0), 50, 1, false))
-      .get();
-    auto read_batches = consume(output_ntp).get0();
+    push(input_ntp, make_random_batch(50)).get();
+    auto read_batches = consume(output_ntp, 100).get0();
 
     /// The identity coprocessor should not have modified the number of
     /// record batches from the source log onto the output log
-    BOOST_CHECK_EQUAL(read_batches.size(), 100);
+    BOOST_CHECK_EQUAL(record_sum(read_batches), 100);
 }
 
 FIXTURE_TEST(test_copro_auto_deregister_function, coproc_test_fixture) {
@@ -162,8 +148,7 @@ FIXTURE_TEST(test_copro_auto_deregister_function, coproc_test_fixture) {
     for (auto i = 0; i < 24; ++i) {
         fs.emplace_back(push(
           model::ntp(model::kafka_namespace, foo, model::partition_id(i)),
-          storage::test::make_random_memory_record_batch_reader(
-            model::offset(0), 10, 1, false)));
+          make_random_batch(100)));
     }
     ss::when_all_succeed(fs.begin(), fs.end()).get();
 

src/v/coproc/tests/topic_ingestion_policy_tests.cc
@@ -11,6 +11,7 @@
 #include "coproc/api.h"
 #include "coproc/pacemaker.h"
 #include "coproc/tests/fixtures/coproc_test_fixture.h"
+#include "coproc/tests/utils/batch_utils.h"
 #include "coproc/tests/utils/coprocessor.h"
 #include "model/namespace.h"
 #include "storage/tests/utils/random_batch.h"
@@ -23,17 +24,17 @@
 
 class tip_fixture : public coproc_test_fixture {
 public:
-    std::size_t run(coproc::topic_ingestion_policy tip, std::size_t n) {
+    std::size_t run(
+      coproc::topic_ingestion_policy tip,
+      std::size_t records,
+      std::size_t expect) {
         model::topic infoo("infoo");
         model::ntp infoo_ntp(
           model::kafka_namespace, infoo, model::partition_id(0));
         setup({{infoo, 1}}).get();
 
-        push(
-          infoo_ntp,
-          storage::test::make_random_memory_record_batch_reader(
-            model::offset(0), n, 1, false))
-          .get();
+        push(infoo_ntp, make_random_batch(records)).get();
+        consume(infoo_ntp, 400).get();
 
         enable_coprocessors(
           {{.id = 78,
@@ -42,42 +43,27 @@ public:
               .topics = {{infoo, tip}}}}})
           .get();
 
-        /// Wait for the coprocessor to startup before next batch
-        coproc::script_id id(78);
-        tests::cooperative_spin_wait_with_timeout(60s, [this, id]() {
-            return root_fixture()
-              ->app.coprocessing->get_pacemaker()
-              .map_reduce0(
-                [id](coproc::pacemaker& p) {
-                    return p.local_script_id_exists(id);
-                },
-                false,
-                std::logical_or<>());
-        }).get();
-
-        push(
-          infoo_ntp,
-          storage::test::make_random_memory_record_batch_reader(
-            model::offset{0}, n, 1, false))
-          .get();
+        push(infoo_ntp, make_random_batch(records)).get();
+        consume(infoo_ntp, records).get();
 
         model::ntp output_ntp(
           model::kafka_namespace,
           to_materialized_topic(infoo, identity_coprocessor::identity_topic),
           model::partition_id(0));
-        return consume(output_ntp).get0().size();
+
+        auto rbs = consume(output_ntp, expect).get0();
+        return record_sum(rbs);
     }
 };
 
-/// 'tip' stands for topic_ingestion_policy
 FIXTURE_TEST(test_copro_tip_latest, tip_fixture) {
-    auto result = run(tp_latest, 40);
-    BOOST_CHECK_EQUAL(result, 40);
+    auto results = run(tp_latest, 400, 400);
+    BOOST_CHECK_EQUAL(400, results);
 }
 
 FIXTURE_TEST(test_copro_tip_earliest, tip_fixture) {
-    auto result = run(tp_earliest, 40);
-    BOOST_CHECK_EQUAL(result, 80);
+    auto results = run(tp_earliest, 400, 800);
+    BOOST_CHECK_EQUAL(800, results);
 }
 
 FIXTURE_TEST(test_copro_tip_stored, coproc_test_fixture) {
@@ -96,25 +82,15 @@ FIXTURE_TEST(test_copro_tip_stored, coproc_test_fixture) {
           .topics = {{sttp, tp_stored}}}}})
       .get();
 
-    push(
-      sttp_ntp,
-      storage::test::make_random_memory_record_batch_reader(
-        model::offset{0}, 40, 1, false))
-      .get();
-
-    auto a_results = consume(output_ntp).get();
-    BOOST_CHECK(a_results.size() == 40);
+    push(sttp_ntp, make_random_batch(200)).get();
+    auto a_results = consume(output_ntp, 200).get();
+    BOOST_CHECK_EQUAL(record_sum(a_results), 200);
 
     ss::sleep(1s).get();
     info("Restarting....");
     restart().get();
 
-    push(
-      sttp_ntp,
-      storage::test::make_random_memory_record_batch_reader(
-        model::offset{0}, 40, 1, false))
-      .get();
-
-    auto results = consume(output_ntp).get();
-    BOOST_CHECK(results.size() >= 80);
+    push(sttp_ntp, make_random_batch(200)).get();
+    auto results = consume(output_ntp, 400).get();
+    BOOST_CHECK_GE(record_sum(results), 400);
 }

redpanda-data/redpanda

src/v/coproc/tests/failure_recovery_tests.cc
@@ -9,6 +9,7 @@
  */
 
 #include "coproc/tests/fixtures/coproc_test_fixture.h"
+#include "coproc/tests/utils/batch_utils.h"
 #include "coproc/tests/utils/coprocessor.h"
 #include "model/fundamental.h"
 #include "model/namespace.h"
@@ -44,10 +45,7 @@ FIXTURE_TEST(test_wasm_engine_restart, coproc_test_fixture) {
       [this](const std::vector<model::ntp>& ntps) -> ss::future<> {
         std::vector<ss::future<model::offset>> fs;
         for (const auto& ntp : ntps) {
-            fs.emplace_back(push(
-              ntp,
-              storage::test::make_random_memory_record_batch_reader(
-                model::offset(0), 10, 2, false)));
+            fs.emplace_back(push(ntp, make_random_batch(500)));
         }
         return ss::when_all_succeed(fs.begin(), fs.end()).discard_result();
     };
@@ -69,8 +67,10 @@ FIXTURE_TEST(test_wasm_engine_restart, coproc_test_fixture) {
     /// commit interval, the more likely this is.
     std::vector<ss::future<model::record_batch_reader::data_t>> fs;
     for (const auto& ntp : outputs) {
-        fs.emplace_back(consume(ntp));
+        fs.emplace_back(consume(ntp, (500 * 2)));
     }
-    auto data = ss::when_all_succeed(fs.begin(), fs.end()).get0();
-    BOOST_CHECK(!data[0].empty());
+    auto results = ss::when_all_succeed(fs.begin(), fs.end()).get0();
+    for (const auto& data : results) {
+        BOOST_CHECK_GE(num_records(data), (500 * 2));
+    }
 }

src/v/coproc/tests/pacemaker_tests.cc
@@ -11,6 +11,7 @@
 #include "coproc/api.h"
 #include "coproc/pacemaker.h"
 #include "coproc/tests/fixtures/coproc_test_fixture.h"
+#include "coproc/tests/utils/batch_utils.h"
 #include "coproc/tests/utils/coprocessor.h"
 #include "coproc/types.h"
 #include "model/fundamental.h"
@@ -55,11 +56,7 @@ FIXTURE_TEST(test_coproc_router_no_results, coproc_test_fixture) {
     // Test -> Start pushing to registered topics and check that NO
     // materialized logs have been created
     model::ntp input_ntp(model::kafka_namespace, foo, model::partition_id(0));
-    push(
-      input_ntp,
-      storage::test::make_random_memory_record_batch_reader(
-        model::offset(0), 40, 1, false))
-      .get();
+    push(input_ntp, make_random_batch(40)).get();
 
     // Wait for any side-effects, ...expecting that none occur
     ss::sleep(1s).get();
@@ -69,13 +66,6 @@ FIXTURE_TEST(test_coproc_router_no_results, coproc_test_fixture) {
     /// .. but total should be exactly 11 due to the introducion of the
     /// coprocessor_internal_topic
     BOOST_REQUIRE_EQUAL(final_n_logs, 11);
-}
-
-model::record_batch_reader single_record_record_batch_reader() {
-    model::record_batch_reader::data_t batches;
-    batches.push_back(
-      storage::test::make_random_batch(model::offset(0), 1, false));
-    return model::make_memory_record_batch_reader(std::move(batches));
 }
 
 /// Tests an off-by-one error where producing recordbatches of size 1 on the
@@ -98,9 +88,9 @@ FIXTURE_TEST(test_coproc_router_off_by_one, coproc_test_fixture) {
       .get();
     auto fn =
       [this, input_ntp, output_ntp](model::offset start) -> ss::future<size_t> {
-        co_await push(input_ntp, single_record_record_batch_reader());
-        auto r = co_await consume(output_ntp, start, start + model::offset{1});
-        co_return r.size();
+        co_await push(input_ntp, make_random_batch(1));
+        auto r = co_await consume(output_ntp, 1, start);
+        co_return num_records(r);
     };
     // Perform push/consume twice
     size_t wr = fn(model::offset(0)).get();
@@ -133,16 +123,12 @@ FIXTURE_TEST(test_coproc_router_double, coproc_test_fixture) {
       to_materialized_topic(foo, identity_coprocessor::identity_topic),
       model::partition_id(0));
 
-    push(
-      input_ntp,
-      storage::test::make_random_memory_record_batch_reader(
-        model::offset(0), 50, 1, false))
-      .get();
-    auto read_batches = consume(output_ntp).get0();
+    push(input_ntp, make_random_batch(50)).get();
+    auto read_batches = consume(output_ntp, 100).get0();
 
     /// The identity coprocessor should not have modified the number of
     /// record batches from the source log onto the output log
-    BOOST_CHECK_EQUAL(read_batches.size(), 100);
+    BOOST_CHECK_EQUAL(num_records(read_batches), 100);
 }
 
 FIXTURE_TEST(test_copro_auto_deregister_function, coproc_test_fixture) {
@@ -162,8 +148,7 @@ FIXTURE_TEST(test_copro_auto_deregister_function, coproc_test_fixture) {
     for (auto i = 0; i < 24; ++i) {
         fs.emplace_back(push(
           model::ntp(model::kafka_namespace, foo, model::partition_id(i)),
-          storage::test::make_random_memory_record_batch_reader(
-            model::offset(0), 10, 1, false)));
+          make_random_batch(100)));
     }
     ss::when_all_succeed(fs.begin(), fs.end()).get();
 

src/v/coproc/tests/topic_ingestion_policy_tests.cc
@@ -11,6 +11,7 @@
 #include "coproc/api.h"
 #include "coproc/pacemaker.h"
 #include "coproc/tests/fixtures/coproc_test_fixture.h"
+#include "coproc/tests/utils/batch_utils.h"
 #include "coproc/tests/utils/coprocessor.h"
 #include "model/namespace.h"
 #include "storage/tests/utils/random_batch.h"
@@ -23,17 +24,17 @@
 
 class tip_fixture : public coproc_test_fixture {
 public:
-    std::size_t run(coproc::topic_ingestion_policy tip, std::size_t n) {
+    std::size_t run(
+      coproc::topic_ingestion_policy tip,
+      std::size_t records,
+      std::size_t expect) {
         model::topic infoo("infoo");
         model::ntp infoo_ntp(
           model::kafka_namespace, infoo, model::partition_id(0));
         setup({{infoo, 1}}).get();
 
-        push(
-          infoo_ntp,
-          storage::test::make_random_memory_record_batch_reader(
-            model::offset(0), n, 1, false))
-          .get();
+        push(infoo_ntp, make_random_batch(records)).get();
+        consume(infoo_ntp, 400).get();
 
         enable_coprocessors(
           {{.id = 78,
@@ -42,42 +43,27 @@ public:
               .topics = {{infoo, tip}}}}})
           .get();
 
-        /// Wait for the coprocessor to startup before next batch
-        coproc::script_id id(78);
-        tests::cooperative_spin_wait_with_timeout(60s, [this, id]() {
-            return root_fixture()
-              ->app.coprocessing->get_pacemaker()
-              .map_reduce0(
-                [id](coproc::pacemaker& p) {
-                    return p.local_script_id_exists(id);
-                },
-                false,
-                std::logical_or<>());
-        }).get();
-
-        push(
-          infoo_ntp,
-          storage::test::make_random_memory_record_batch_reader(
-            model::offset{0}, n, 1, false))
-          .get();
+        push(infoo_ntp, make_random_batch(records)).get();
+        consume(infoo_ntp, records).get();
 
         model::ntp output_ntp(
           model::kafka_namespace,
           to_materialized_topic(infoo, identity_coprocessor::identity_topic),
           model::partition_id(0));
-        return consume(output_ntp).get0().size();
+
+        auto rbs = consume(output_ntp, expect).get0();
+        return num_records(rbs);
     }
 };
 
-/// 'tip' stands for topic_ingestion_policy
 FIXTURE_TEST(test_copro_tip_latest, tip_fixture) {
-    auto result = run(tp_latest, 40);
-    BOOST_CHECK_EQUAL(result, 40);
+    auto results = run(tp_latest, 400, 400);
+    BOOST_CHECK_EQUAL(400, results);
 }
 
 FIXTURE_TEST(test_copro_tip_earliest, tip_fixture) {
-    auto result = run(tp_earliest, 40);
-    BOOST_CHECK_EQUAL(result, 80);
+    auto results = run(tp_earliest, 400, 800);
+    BOOST_CHECK_EQUAL(800, results);
 }
 
 FIXTURE_TEST(test_copro_tip_stored, coproc_test_fixture) {
@@ -96,25 +82,15 @@ FIXTURE_TEST(test_copro_tip_stored, coproc_test_fixture) {
           .topics = {{sttp, tp_stored}}}}})
       .get();
 
-    push(
-      sttp_ntp,
-      storage::test::make_random_memory_record_batch_reader(
-        model::offset{0}, 40, 1, false))
-      .get();
-
-    auto a_results = consume(output_ntp).get();
-    BOOST_CHECK(a_results.size() == 40);
+    push(sttp_ntp, make_random_batch(200)).get();
+    auto a_results = consume(output_ntp, 200).get();
+    BOOST_CHECK_EQUAL(num_records(a_results), 200);
 
     ss::sleep(1s).get();
     info("Restarting....");
     restart().get();
 
-    push(
-      sttp_ntp,
-      storage::test::make_random_memory_record_batch_reader(
-        model::offset{0}, 40, 1, false))
-      .get();
-
-    auto results = consume(output_ntp).get();
-    BOOST_CHECK(results.size() >= 80);
+    push(sttp_ntp, make_random_batch(200)).get();
+    auto results = consume(output_ntp, 400).get();
+    BOOST_CHECK_GE(num_records(results), 400);
 }