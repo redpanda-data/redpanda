11e40632882dd7b887d4eb026b5f888e00d7f406
Michal Maslanka <michal@vectorized.io>
2021-11-25 15:40:28 +0100

    k/fetch: aggregate fetch bytes
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/kafka/server/handlers/fetch.cc
+++ src/v/kafka/server/handlers/fetch.cc
@@ -97,6 +97,7 @@ static ss::future<read_result> read_from_partition(
           kafka_batch_serializer(), deadline ? *deadline : model::no_timeout);
         data = std::make_unique<iobuf>(std::move(result.data));
         part.probe().add_records_fetched(result.record_count);
+        part.probe().add_bytes_fetched(data->size_bytes());
         if (result.record_count > 0) {
             // Reader should live at least until this point to hold on to the
             // segment locks so that prefix truncation doesn't happen.
