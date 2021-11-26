2224df195db42a0edd772356fde67faf64132e86
Alena Varkockova <varkockova.a@gmail.com>
2021-11-26 15:39:02 +0100

    (split) k8s: bump deps to newer controller-runtime and k8s 1.21

--- pkg/resources/configmap_test.go
+++ pkg/resources/configmap_test.go
@@ -12,7 +12,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/deprecated/scheme"
+	"k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
