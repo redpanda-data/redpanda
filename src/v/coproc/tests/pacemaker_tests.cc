107a8aea367b3a8d7c7192932634b39fd4c15d69
Rob Blafford <rob@vectorized.io>
2021-11-29 12:31:34 -0500

    coproc/tests: Unit test concurrent topic delete
    - This unit test removes a materialized topic while there is a
    coprocessor writing to the same topic.
    
    - The expected behavior is for the topic to be deleted and then
    recreated upon next write to this topic by any of the scripts fibers.
    
    - Since the unit test initializes the topic to be processed at the
    earliest offset, upon re-creation the topic should be fully re-processed
    and the test can assert for a pre-calculated expected amount.

--- src/v/coproc/tests/pacemaker_tests.cc
+++ src/v/coproc/tests/pacemaker_tests.cc
@@ -10,6 +10,7 @@
 
 #include "coproc/api.h"
 #include "coproc/pacemaker.h"
+#include "coproc/partition_manager.h"
 #include "coproc/tests/fixtures/coproc_test_fixture.h"
 #include "coproc/tests/utils/batch_utils.h"
 #include "coproc/tests/utils/coprocessor.h"
@@ -162,4 +163,86 @@ FIXTURE_TEST(test_copro_auto_deregister_function, coproc_test_fixture) {
         return root_fixture()->app.coprocessing->get_pacemaker().map_reduce0(
           std::move(map), true, std::logical_and<>());
     }).get();
+}
+
+/// Ensures that a deletion of a materialized topic doesn't conflict
+/// with an active coprocessor producing onto it. Furthermore asserts the topic
+/// exists at tests end and contains all expected records.
+FIXTURE_TEST(test_copro_delete_topic, coproc_test_fixture) {
+    model::topic_namespace tbd(model::kafka_namespace, model::topic("tbd"));
+    setup({{tbd.tp, 4}}).get();
+    auto id = coproc::script_id(59);
+    // Use the earliest policy so that when the topic is re-created processing
+    // begins at 0, so that the full amount of records can be asserted at tests
+    // end
+    enable_coprocessors(
+      {{.id = id(),
+        .data{
+          .tid = copro_typeid::identity_coprocessor,
+          .topics = {{tbd.tp, coproc::topic_ingestion_policy::earliest}}}}})
+      .get();
+
+    /// Find shard of one of the partitions
+    model::partition_id target_pid(0);
+    auto shard = root_fixture()->app.shard_table.local().shard_for(
+      model::ntp(tbd.ns, tbd.tp, target_pid));
+    BOOST_CHECK(shard.has_value());
+    info("On shard: {}", *shard);
+
+    /// Setup listeners to inject the deletion
+    model::ntp target(
+      tbd.ns,
+      to_materialized_topic(tbd.tp, identity_coprocessor::identity_topic),
+      target_pid);
+    auto f = root_fixture()
+               ->app.cp_partition_manager
+               .invoke_on(
+                 *shard,
+                 [this, target](coproc::partition_manager& pm) {
+                     ss::promise<> p;
+                     auto f = p.get_future();
+                     auto id = pm.register_manage_notification(
+                       target.ns,
+                       target.tp.topic,
+                       [this, p = std::move(p)](
+                         ss::lw_shared_ptr<coproc::partition>) mutable {
+                           p.set_value();
+                       });
+                     return f.then(
+                       [id, &pm] { pm.unregister_manage_notification(id); });
+                 })
+               .then([this, target] {
+                   model::topic_namespace tp{target.ns, target.tp.topic};
+                   info("Deleting topic: {} ", tp);
+                   return root_fixture()->delete_topic(tp).then(
+                     [this, tp] { info("Topic {} deleted", tp); });
+               });
+
+    // Push some data across input topic....
+    const std::size_t total_records = 10000;
+    std::vector<ss::future<>> fs;
+    fs.reserve(4);
+    for (auto i = 0; i < 4; ++i) {
+        info("Producing onto: {}-{}", tbd, i);
+        fs.emplace_back(produce(
+          model::ntp(tbd.ns, tbd.tp, model::partition_id(i)),
+          make_random_batch(total_records)));
+    }
+    ss::when_all_succeed(fs.begin(), fs.end()).get();
+    info("All produced onto: {}", tbd);
+    f.get();
+
+    /// Wait until all data is recieved on output topic, which should have been
+    /// recreated and fully processed from the earliest offset
+    for (auto i = 0; i < 4; ++i) {
+        auto results = consume(
+                         model::ntp(
+                           tbd.ns,
+                           to_materialized_topic(
+                             tbd.tp, identity_coprocessor::identity_topic),
+                           model::partition_id(i)),
+                         total_records)
+                         .get();
+        BOOST_CHECK_EQUAL(num_records(results), total_records);
+    }
 }
