d0bc1abdf7dd8b8f676e5deb3670b5e6762fa181
Alena Varkockova <varkockova.a@gmail.com>
2021-11-29 17:05:25 +0100

    k8s: add rbac for poddisruptionbudget

--- src/go/k8s/controllers/redpanda/cluster_controller.go
+++ src/go/k8s/controllers/redpanda/cluster_controller.go
@@ -62,6 +62,7 @@ type ClusterReconciler struct {
 //+kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch
 //+kubebuilder:rbac:groups=cert-manager.io,resources=issuers;certificates;clusterissuers,verbs=create;get;list;watch;patch;delete;update;
 //+kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=create;get;list;watch;patch;delete;update;
+//+kubebuilder:rbac:groups=policy,resources=poddisruptionbudgets,verbs=create;get;list;watch;patch;delete;update;
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
