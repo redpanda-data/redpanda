fc4c6edd1c165d6d7018e1bd41cee866f903810c
Vadim Plakhtinskiy <vadim@vectorized.io>
2021-12-02 14:33:39 +0200

    v8_engine: Delete compilation logic from script_dispathcer
    script_dispathcer should only store v8_scripts and provide
    data_base interface to put/remove/get/contains v8_scripts
    Is should be simple. Contains only mapping from topic
    to v8_runtimes with data_policy

--- src/v/v8_engine/script_dispatcher.h
+++ src/v/v8_engine/script_dispatcher.h
@@ -23,15 +23,17 @@
 
 namespace v8_engine {
 
-struct script_compile_params {
-    script_compile_params(ss::sstring function_name, iobuf code)
-      : function_name(std::move(function_name))
-      , code(std::move(code)) {}
+struct script_wrpapper {
+    ss::condition_variable cv;
+    ss::lw_shared_ptr<script> function{nullptr};
 
-    ss::sstring function_name;
-    iobuf code;
-    size_t max_heap_size_bytes{10000};
-    size_t run_timeout_ms{10000};
+    bool is_compiling() { return function.get() == nullptr; }
+
+    // TODO: add stop compilation (Is it possible?))
+    void destroy() {
+        function = nullptr;
+        cv.broadcast();
+    }
 };
 
 template<typename Executor>
@@ -40,161 +42,59 @@ public:
     explicit script_dispatcher(Executor& executor)
       : _executor(executor) {}
 
-    // Insert only put compilation params to map. It can not rewrite
-    // key, because we need to run remove for delete old v8_script and insert
-    // new
     bool insert(
       const model::topic_namespace& topic,
-      ss::sstring function_name,
-      iobuf code) {
-        return _scripts
-          .emplace(
-            topic,
-            script_compile_params(std::move(function_name), std::move(code)))
-          .second;
+      ss::lw_shared_ptr<script_wrpapper> new_script) {
+        return _scripts.emplace(topic, new_script).second;
     }
 
-    // Remove check do user run get and start to compile script. After that it
-    // delete all information about v8_script from hash_map
-    bool remove(const model::topic_namespace& topic) {
+    void remove(const model::topic_namespace& topic) {
         auto it = _scripts.find(topic);
         if (it == _scripts.end()) {
-            return false;
+            return;
         }
-
-        ss::visit(
-          it->second,
-          [](const script_compile_params&) {
-              // Do nothing, because nobody try to get this script. User only
-              // insert data_policy for topic and did not try t ouse it
-              return;
-          },
-          [](ss::lw_shared_ptr<script_wrpapper> script_ptr) {
-              // If user started to compile script we need to notify waters that
-              // we delete this data_policy, after that we can clear hash_table
-              if (script_ptr->is_compiling()) {
-                  script_ptr->destroy();
-              }
-              return;
-          });
-
+        it->second->destroy();
         _scripts.erase(it);
-
-        return true;
     }
 
-    // Get reqeust use lazy compilation for v8_script. When user run get first
-    // time for data_policy we will compile script and anotjer get request for
-    // cirrent topic will wait finish of compilation
+    bool contains(const model::topic_namespace& topic) {
+        return _scripts.contains(topic);
+    }
+
+    // Get reqeust use lazy compilation for v8_script.
     ss::future<ss::lw_shared_ptr<script>> get(model::topic_namespace topic) {
         auto it = _scripts.find(topic);
         if (it == _scripts.end()) {
             co_return nullptr;
         }
 
-        co_return co_await ss::visit(
-          it->second,
-          // If we have only compilation params we need to compile script
-          [topic, this](script_compile_params& params)
-            -> ss::future<ss::lw_shared_ptr<script>> {
-              auto topic_name = topic;
-              script_compile_params copy_params = std::move(params);
-              auto value = ss::make_lw_shared(script_wrpapper());
-
-              // Can skip result, because it contains compilation_params and
-              // nobody change this map since find.
-              _scripts.insert_or_assign(topic_name, value);
-
-              auto runtime = ss::make_lw_shared<v8_engine::script>(
-                copy_params.max_heap_size_bytes, copy_params.run_timeout_ms);
-
-              try {
-                  co_await runtime->init(
-                    copy_params.function_name,
-                    std::move(copy_params.code.copy()),
-                    _executor);
-              } catch (const v8_engine::script_exception& ex) {
-                  // TODO: add log
-
-                  // If script is exist we need to return compilation params
-                  // back, becuase use want to try recompile it
-                  if (!script_is_deleted(topic_name, value)) {
-                      _scripts.insert_or_assign(
-                        topic_name, std::move(copy_params));
-                      value->destroy();
-                  }
-
-                  // If script was deleted we do not do anything, because remove
-                  // method destroy it.
-                  co_return nullptr;
-              }
-
-              // Skip runtime is script was deleted
-              if (script_is_deleted(topic_name, value)) {
-                  co_return nullptr;
-              }
-              value->function = runtime;
-              value->cv.broadcast();
-              co_return runtime;
-          },
-          // If we see ptr to v8_script we need wait compilation or return
-          // result
-          [](ss::lw_shared_ptr<script_wrpapper> script_ptr)
-            -> ss::future<ss::lw_shared_ptr<script>> {
-              if (script_ptr->is_compiling()) {
-                  // If script is being compiled now we need to wait finish
-                  co_await script_ptr->cv.wait();
-                  // If user delete script, function will contain nullptr
-                  co_return script_ptr->function;
-              } else {
-                  // Return value (v8_script) for current topic
-                  co_return script_ptr->function;
-              }
-          });
-    }
-
-private:
-    struct script_wrpapper {
-        ss::condition_variable cv;
-        ss::lw_shared_ptr<script> function{nullptr};
-
-        bool is_compiling() { return function.get() == nullptr; }
-
-        // TODO: add stop compilation (Is it possible?))
-        void destroy() {
-            function = nullptr;
-            cv.broadcast();
+        if (it->second->is_compiling()) {
+            // If script is being compiled now we need to wait finish
+            co_await it->second->cv.wait();
         }
-    };
+
+        // If user delete script, function will contain nullptr
+        co_return it->second->function;
+    }
 
     // Compare pointers and check if script was deleted
     bool script_is_deleted(
       const model::topic_namespace& topic,
-      ss::lw_shared_ptr<script_wrpapper> script_ptr) {
+      ss::lw_shared_ptr<script_wrpapper> value) {
         auto it = _scripts.find(topic);
         if (it == _scripts.end()) {
             return true;
         }
 
-        return ss::visit(
-          it->second,
-          // User delete script and set new compilation params
-          [](const script_compile_params&) { return true; },
-          // User can compile new script and put it to hash table, we need to
-          // check pointers
-          [script_ptr](ss::lw_shared_ptr<script_wrpapper> new_script_ptr) {
-              return script_ptr.get() != new_script_ptr.get();
-          });
+        return value.get() != it->second.get();
     }
 
-    using scripts_value
-      = std::variant<script_compile_params, ss::lw_shared_ptr<script_wrpapper>>;
-
-    // Hash map from topic to scripts(v8 runtimes). It store compilation params
-    // or shared_ptr to script_wrpapper. When user set data_policy by using
-    // incremental topic config we check does code exist and put information
-    // about compilation to hash map.
-    absl::node_hash_map<model::topic_namespace, scripts_value> _scripts;
+private:
+    // Hash map from topic to scripts(v8 runtimes). It stores v8_script for
+    // fetch requests
+    absl::
+      node_hash_map<model::topic_namespace, ss::lw_shared_ptr<script_wrpapper>>
+        _scripts;
 
     Executor& _executor;
 };
