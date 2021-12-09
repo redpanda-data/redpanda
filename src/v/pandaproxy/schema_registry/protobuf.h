64c9effc95a1d9aaa6764aaa80f31e486cbe6a94
Ben Pope <ben@vectorized.io>
2021-12-09 18:06:21 +0000

    schema_registry/protobuf: Prefer to pass coroutine params by value
    https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#cp53-parameters-to-coroutines-should-not-be-passed-by-reference
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/schema_registry/protobuf.h
+++ src/v/pandaproxy/schema_registry/protobuf.h
@@ -16,11 +16,11 @@
 
 namespace pandaproxy::schema_registry {
 
-ss::future<protobuf_schema_definition> make_protobuf_schema_definition(
-  sharded_store& store, const canonical_schema& schema);
+ss::future<protobuf_schema_definition>
+make_protobuf_schema_definition(sharded_store& store, canonical_schema schema);
 
 ss::future<canonical_schema_definition>
-validate_protobuf_schema(sharded_store& store, const canonical_schema& schema);
+validate_protobuf_schema(sharded_store& store, canonical_schema schema);
 
 ss::future<canonical_schema>
 make_canonical_protobuf_schema(sharded_store& store, unparsed_schema schema);
