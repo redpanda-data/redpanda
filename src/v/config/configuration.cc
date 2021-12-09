0ad4f57c56ace0207a92f25e933cd9c2afd7024e
John Spray <jcs@vectorized.io>
2021-12-09 23:08:42 +0000

    config: mark cloud_storage_secret_key as secret
    Signed-off-by: John Spray <jcs@vectorized.io>

--- src/v/config/configuration.cc
+++ src/v/config/configuration.cc
@@ -736,7 +736,7 @@ configuration::configuration()
       *this,
       "cloud_storage_secret_key",
       "AWS secret key",
-      {.visibility = visibility::user},
+      {.visibility = visibility::user, .secret = is_secret::yes},
       std::nullopt)
   , cloud_storage_region(
       *this,
