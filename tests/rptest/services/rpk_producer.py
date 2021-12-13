6f630363d1363c486e9ec2a95748cdef2d4b11ff
John Spray <jcs@vectorized.io>
2021-12-13 20:15:04 +0000

    tests/rpk_producer: extend with auth, quiet, custom nodes
    These changes are to enable use during pre-test population
    of topics
    
    nodes: passing explicit nodes enables using this class without
    requiring that the test definition have a high enough
    num_nodes to make spare nodes available for a dedicated
    producer.
    
    quiet: in setup, we don't want to spam the log with a log line
    for every message.
    
    user/password: some tests enable sasl by default
    
    Signed-off-by: John Spray <jcs@vectorized.io>

--- tests/rptest/services/rpk_producer.py
+++ tests/rptest/services/rpk_producer.py
@@ -15,25 +15,57 @@ class RpkProducer(BackgroundThreadService):
                  topic,
                  msg_size,
                  msg_count,
-                 acks=None):
+                 acks=None,
+                 user=None,
+                 password=None,
+                 quiet=False,
+                 nodes=None):
+        """
+
+        :param context:
+        :param redpanda:
+        :param topic: string, topic name
+        :param msg_size: integer
+        :param msg_count: integer, bytes
+        :param acks: 0, 1 or -1
+        :param quiet: if true, no per-message logging will be emitted
+        :param nodes: list of Node, if set then no nodes will be auto-allocated
+        """
+        self.custom_nodes = nodes is not None
+
         super(RpkProducer, self).__init__(context, num_nodes=1)
+
+        if nodes:
+            assert not self.nodes
+            self.nodes = nodes
+
         self._redpanda = redpanda
         self._topic = topic
         self._msg_size = msg_size
         self._msg_count = msg_count
         self._acks = acks
+        self._user = user
+        self._password = password
         self._stopping = Event()
+        self._quiet = quiet
 
     def _worker(self, _idx, node):
         rpk_binary = self._redpanda.find_binary("rpk")
-        cmd = f"dd if=/dev/urandom bs={self._msg_size} count={self._msg_count} | {rpk_binary} topic --brokers {self._redpanda.brokers()} produce --compression none --key test {self._topic} -f '%V{{{self._msg_size}}}%v'"
+
+        auth_args = ""
+        if self._user:
+            # Auth enabled
+            auth_args = f"--user {self._user} --password {self._password} --sasl-mechanism SCRAM-SHA-256"
+
+        cmd = f"dd if=/dev/urandom bs={self._msg_size} count={self._msg_count} | {rpk_binary} topic {auth_args} --brokers {self._redpanda.brokers()} produce --compression none --key test {self._topic} -f '%V{{{self._msg_size}}}%v'"
         if self._acks is not None:
             cmd += f" --acks {self._acks}"
 
         self._stopping.clear()
         try:
             for line in node.account.ssh_capture(cmd, timeout_sec=10):
-                self.logger.debug(line.rstrip())
+                if not self._quiet:
+                    self.logger.debug(line.rstrip())
         except RemoteCommandError as e:
             if self._stopping.is_set():
                 pass
@@ -43,3 +75,15 @@ class RpkProducer(BackgroundThreadService):
     def stop_node(self, node):
         self._stopping.set()
         node.account.kill_process("rpk", clean_shutdown=False)
+
+    def allocate_nodes(self):
+        if self.custom_nodes:
+            return
+        else:
+            return super(RpkProducer, self).allocate_nodes()
+
+    def free_all(self):
+        if self.custom_nodes:
+            return
+        else:
+            return super(RpkProducer, self).free_all()
