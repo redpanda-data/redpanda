3bbd1c8bd71403b9ac1e5bd1715cb2e68f145579
Ben Pope <ben@vectorized.io>
2021-12-15 14:32:20 +0000

    redpanda/cluster: Reduce log level for No leadership balance improvements
    * Reduce log level to debug for No leadership balance improvements
    
    Fixes #3269
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/cluster/scheduling/leader_balancer.cc
+++ src/v/cluster/scheduling/leader_balancer.cc
@@ -263,7 +263,7 @@ ss::future<ss::stop_iteration> leader_balancer::balance() {
     auto transfer = strategy.find_movement(muted_groups());
     if (!transfer) {
         vlog(
-          clusterlog.info,
+          clusterlog.debug,
           "No leadership balance improvements found with total delta {}, "
           "number of muted groups {}",
           error,
