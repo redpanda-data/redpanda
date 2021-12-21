a708f5a24594b20942abcaac0cc5a679b430ee51
Noah Watkins <noah@vectorized.io>
2021-12-21 15:06:27 -0800

    rpc: silence clang-tidy performance warning
    simple_protocol.cc:65:51: warning: the parameter 'ctx' is copied for
    each invocation but only used as a const reference
    
    This can be done by making the parameter const&, but instead the value
    is moved into the final continuation to preserve the good practice of
    futurized functions taking values.
    
    Signed-off-by: Noah Watkins <noah@vectorized.io>

--- src/v/rpc/simple_protocol.cc
+++ src/v/rpc/simple_protocol.cc
@@ -81,7 +81,7 @@ send_reply(ss::lw_shared_ptr<server_context_impl> ctx, netbuf buf) {
         return ss::make_ready_future<>();
     }
     return ctx->res.conn->write(std::move(view))
-      .handle_exception([ctx](std::exception_ptr e) {
+      .handle_exception([ctx = std::move(ctx)](std::exception_ptr e) {
           vlog(rpclog.info, "Error dispatching method: {}", e);
           ctx->res.conn->shutdown_input();
       });
