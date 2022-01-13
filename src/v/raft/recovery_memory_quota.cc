f6fee3003f99f27352239f345afac14c9713f498
Michal Maslanka <michal@vectorized.io>
2022-01-13 08:23:28 +0100

    r/recovery: introduced recovery memory quota
    Introduced `raft::recovery_memory_quota` to control how much memory is
    consumed by raft recovery process. Per shard memory quota allow us to
    limit the memory usage independently from number of raft groups that
    exist on the shard.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ src/v/raft/recovery_memory_quota.cc
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#include "raft/recovery_memory_quota.h"
+
+#include "raft/logger.h"
+#include "resource_mgmt/memory_groups.h"
+#include "vlog.h"
+
+#include <seastar/core/memory.hh>
+
+namespace raft {
+
+recovery_memory_quota::recovery_memory_quota(
+  recovery_memory_quota::config_provider_fn config_provider)
+  : _cfg(config_provider())
+  , _current_max_recovery_mem(
+      _cfg.max_recovery_memory().value_or(memory_groups::recovery_max_memory()))
+  , _memory(_current_max_recovery_mem) {
+    _cfg.max_recovery_memory.watch([this] { on_max_memory_changed(); });
+}
+
+ss::future<ss::semaphore_units<>> recovery_memory_quota::acquire_read_memory() {
+    return ss::get_units(
+      _memory,
+      std::min(_current_max_recovery_mem, _cfg.default_read_buffer_size()));
+}
+
+void recovery_memory_quota::on_max_memory_changed() {
+    int64_t new_size = _cfg.max_recovery_memory().value_or(
+      memory_groups::recovery_max_memory());
+
+    vlog(raftlog.info, "max recovery memory changed to {} bytes", new_size);
+
+    int64_t diff = new_size - static_cast<int64_t>(_current_max_recovery_mem);
+    if (diff < 0) {
+        _memory.consume(-diff);
+    } else if (diff > 0) {
+        _memory.signal(diff);
+    }
+    _current_max_recovery_mem = new_size;
+}
+
+} // namespace raft
---
+++ src/v/raft/recovery_memory_quota.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#pragma once
+#include "config/property.h"
+#include "seastarx.h"
+
+#include <seastar/core/semaphore.hh>
+#include <seastar/util/noncopyable_function.hh>
+
+namespace raft {
+/**
+ * Thread local memory quota for raft recovery
+ */
+class recovery_memory_quota {
+public:
+    struct configuration {
+        config::binding<std::optional<size_t>> max_recovery_memory;
+        config::binding<size_t> default_read_buffer_size;
+    };
+    using config_provider_fn = ss::noncopyable_function<configuration()>;
+
+    explicit recovery_memory_quota(config_provider_fn);
+
+    ss::future<ss::semaphore_units<>> acquire_read_memory();
+
+private:
+    void on_max_memory_changed();
+
+    configuration _cfg;
+    size_t _current_max_recovery_mem;
+    ss::semaphore _memory;
+};
+
+} // namespace raft
