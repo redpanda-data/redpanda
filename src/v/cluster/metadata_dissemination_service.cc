redpanda-data/redpanda

commit c8f4f12ae88dafab7f26b1e99e4711b3fa39642f

c/dissemination: use health manager information to update leaders
When redpanda starts it query one of the other cluster members for
current partition leaders. This is used to initialize partition leaders
table which is then updated with incremental updates propagated by
metadata dissemination service. When initial metadata request will be
answered by the node which doesn't have current view of metadata it may
lead to situation in which node receiving incomplete information will
never update its metadata state (when leadership is stable and there are
no leadership update events propagate for partitions which state in
stale on the node).

In health monitor we periodically collect partition leaders information
from the whole cluster. Implemented using current health monitor cluster
health report snapshot to update metadata information for the node to be
able to eventually refresh its metadata with current state.

Currently we will have two leadership refresh paths i.e. metadata
dissemination - to achieve low latency metadata updates, and health
report based - to eventually update metadata state in face of failures.

Signed-off-by: Michal Maslanka <michal@vectorized.io>

src/v/cluster/metadata_dissemination_service.cc
@@ -10,6 +10,8 @@
 #include "cluster/metadata_dissemination_service.h"
 
 #include "cluster/cluster_utils.h"
+#include "cluster/health_monitor_frontend.h"
+#include "cluster/health_monitor_types.h"
 #include "cluster/logger.h"
 #include "cluster/members_table.h"
 #include "cluster/metadata_cache.h"
@@ -50,13 +52,15 @@ metadata_dissemination_service::metadata_dissemination_service(
   ss::sharded<partition_leaders_table>& leaders,
   ss::sharded<members_table>& members,
   ss::sharded<topic_table>& topics,
-  ss::sharded<rpc::connection_cache>& clients)
+  ss::sharded<rpc::connection_cache>& clients,
+  ss::sharded<health_monitor_frontend>& health_monitor)
   : _raft_manager(raft_manager)
   , _partition_manager(partition_manager)
   , _leaders(leaders)
   , _members_table(members)
   , _topics(topics)
   , _clients(clients)
+  , _health_monitor(health_monitor)
   , _self(make_self_broker(config::node()))
   , _dissemination_interval(
       config::shard_local_cfg().metadata_dissemination_interval_ms)
@@ -315,15 +319,44 @@ void metadata_dissemination_service::cleanup_finished_updates() {
 }
 
 ss::future<> metadata_dissemination_service::dispatch_disseminate_leadership() {
-    collect_pending_updates();
-    return ss::parallel_for_each(
-             _pending_updates.begin(),
-             _pending_updates.end(),
-             [this](broker_updates_t::value_type& br_update) {
-                 return dispatch_one_update(br_update.first, br_update.second);
-             })
+    /**
+     * Use currently available health report snapshot to update leadership
+     * information. If snapshot would contain stale data they will be ignored by
+     * term check in partition leaders table
+     */
+    return _health_monitor.local()
+      .get_current_cluster_health_snapshot(cluster_report_filter{})
+      .then([this](cluster_health_report report) {
+          return update_leaders_with_health_report(std::move(report));
+      })
+      .then([this] {
+          collect_pending_updates();
+          return ss::parallel_for_each(
+            _pending_updates.begin(),
+            _pending_updates.end(),
+            [this](broker_updates_t::value_type& br_update) {
+                return dispatch_one_update(br_update.first, br_update.second);
+            });
+      })
       .then([this] { cleanup_finished_updates(); })
       .finally([this] { _dispatch_timer.arm(_dissemination_interval); });
+}
+
+ss::future<> metadata_dissemination_service::update_leaders_with_health_report(
+  cluster_health_report report) {
+    for (auto& node_report : report.node_reports) {
+        co_await _leaders.invoke_on_all(
+          [node_report](partition_leaders_table& leaders) {
+              for (auto& tp : node_report.topics) {
+                  for (auto& p : tp.partitions) {
+                      leaders.update_partition_leader(
+                        model::ntp(tp.tp_ns.ns, tp.tp_ns.tp, p.id),
+                        p.term,
+                        p.leader_id);
+                  }
+              }
+          });
+    }
 }
 
 ss::future<> metadata_dissemination_service::dispatch_one_update(

src/v/cluster/metadata_dissemination_service.h
@@ -12,6 +12,7 @@
 #pragma once
 
 #include "cluster/fwd.h"
+#include "cluster/health_monitor_types.h"
 #include "cluster/metadata_dissemination_types.h"
 #include "config/tls_config.h"
 #include "model/fundamental.h"
@@ -77,7 +78,8 @@ public:
       ss::sharded<partition_leaders_table>&,
       ss::sharded<members_table>&,
       ss::sharded<topic_table>&,
-      ss::sharded<rpc::connection_cache>&);
+      ss::sharded<rpc::connection_cache>&,
+      ss::sharded<health_monitor_frontend>&);
 
     void disseminate_leadership(
       model::ntp, model::term_id, std::optional<model::node_id>);
@@ -127,12 +129,15 @@ private:
     ss::future<>
       update_metadata_with_retries(std::vector<net::unresolved_address>);
 
+    ss::future<> update_leaders_with_health_report(cluster_health_report);
+
     ss::sharded<raft::group_manager>& _raft_manager;
     ss::sharded<cluster::partition_manager>& _partition_manager;
     ss::sharded<partition_leaders_table>& _leaders;
     ss::sharded<members_table>& _members_table;
     ss::sharded<topic_table>& _topics;
     ss::sharded<rpc::connection_cache>& _clients;
+    ss::sharded<health_monitor_frontend>& _health_monitor;
     model::broker _self;
     std::chrono::milliseconds _dissemination_interval;
     config::tls_config _rpc_tls_config;

src/v/redpanda/application.cc
@@ -716,7 +716,8 @@ void application::wire_up_redpanda_services() {
       std::ref(controller->get_partition_leaders()),
       std::ref(controller->get_members_table()),
       std::ref(controller->get_topics_state()),
-      std::ref(_raft_connection_cache))
+      std::ref(_raft_connection_cache),
+      std::ref(controller->get_health_monitor()))
       .get();
 
     if (archival_storage_enabled()) {

commit 1335dff91dc8a2325b1992139254dedb4b3f8263

c/metadata_dissemination: fix applying updates from health
This regressed in c8f4f12ae88dafab7f26b1e99e4711b3fa39642f

Node health reports may disagree with one another about
leadership in a particular term, if some of them claim
that it's null (because they've seen the term in their
own log after restart, but not yet received an append_entries
from the leader).

To avoid a rogue node health report resetting the leadership
of a topic to null, ignore health report leadership
information if it claims a null leader.

Non-null claims are always believable, because of the term:
if they're out of date, then they were still correct for
the term they claim, and we ignore those out of date
terms in partition_leaders_table::update_partition_leader.

Fixes https://github.com/vectorizedio/redpanda/issues/3486

src/v/cluster/metadata_dissemination_service.cc
@@ -349,10 +349,18 @@ ss::future<> metadata_dissemination_service::update_leaders_with_health_report(
           [node_report](partition_leaders_table& leaders) {
               for (auto& tp : node_report.topics) {
                   for (auto& p : tp.partitions) {
-                      leaders.update_partition_leader(
-                        model::ntp(tp.tp_ns.ns, tp.tp_ns.tp, p.id),
-                        p.term,
-                        p.leader_id);
+                      // Nodes may report a null leader if they're out of
+                      // touch, even if the leader is actually still up.  Only
+                      // trust leadership updates from health reports if
+                      // they're non-null (non-null updates are safe to apply
+                      // in any order because update_partition leader will
+                      // ignore old terms)
+                      if (p.leader_id.has_value()) {
+                          leaders.update_partition_leader(
+                            model::ntp(tp.tp_ns.ns, tp.tp_ns.tp, p.id),
+                            p.term,
+                            p.leader_id);
+                      }
                   }
               }
           });