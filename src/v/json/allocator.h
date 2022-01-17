e4f7077db3d1d6b2aabd2662d79972c49a7b75b9
Elena Anyusheva <lena@vectorized.io>
2022-01-17 20:51:58 +0300

    json: add wrapper around rapid_json allocator
    Wrapper allocator throws bad_alloc if memory was requested but wasn't
    allocated.
    
    (cherry picked from commit 2a0443d40e65dcb194fd0d48dc1120d1d7d34d00)

---
+++ src/v/json/allocator.h
@@ -0,0 +1,47 @@
+// Copyright 2022 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#include "json/logger.h"
+#include "vlog.h"
+
+#include <fmt/format.h>
+#include <rapidjson/allocators.h>
+namespace json {
+class throwing_allocator {
+public:
+    void* Malloc(size_t size) {
+        void* res = _rp_allocator.Malloc(size);
+        if (!res && (0 != size)) {
+            vlog(json_log.error, "Could not allocate {} bytes", size);
+            throw std::bad_alloc{};
+        }
+        return res;
+    }
+
+    void* Realloc(void* originalPtr, size_t originalSize, size_t newSize) {
+        void* res = _rp_allocator.Realloc(originalPtr, originalSize, newSize);
+
+        if (!res && (0 != newSize)) {
+            vlog(
+              json_log.error,
+              "Could not reallocate memory: original size: {} bytes, new size: "
+              "{} bytes",
+              originalSize,
+              newSize);
+            throw std::bad_alloc{};
+        }
+        return res;
+    }
+
+    static void Free(void* ptr) { return rapidjson::CrtAllocator::Free(ptr); }
+
+private:
+    [[no_unique_address]] rapidjson::CrtAllocator _rp_allocator;
+};
+} // namespace json
