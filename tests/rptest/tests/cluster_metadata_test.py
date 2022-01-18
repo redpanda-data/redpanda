6b753603abeb24ecf712b6f5f215c25b4450caa9
John Spray <jcs@vectorized.io>
2022-01-18 19:35:05 +0000

    tests: use our @cluster for recently added tests
    These tests raced with the PR for adding log error
    scanning.

--- tests/rptest/tests/cluster_metadata_test.py
+++ tests/rptest/tests/cluster_metadata_test.py
@@ -8,7 +8,7 @@
 # by the Apache License, Version 2.0
 
 import random
-from ducktape.mark.resource import cluster
+from rptest.services.cluster import cluster
 from ducktape.utils.util import wait_until
 from ducktape.mark import matrix
 from rptest.clients.rpk import RpkTool
