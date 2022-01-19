bef62c6c950b3abe0213f6eea47b930139cf378f
Solonas Gousteris <solonas@vectorized.io>
2022-01-19 21:12:57 +0200

    dummy change

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0
 
+
 set -ex
 root="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 if [[ -z ${CC} ]]; then export CC=clang; fi
