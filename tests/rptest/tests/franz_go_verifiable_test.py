66de382f50cefb23a5b7ea23e87eb89246fd0c80
Vadim Plakhtinskiy <vadim@vectorized.io>
2022-01-19 13:42:35 +0300

    ducktape: Add si in franz-go test

--- tests/rptest/tests/franz_go_verifiable_test.py
+++ tests/rptest/tests/franz_go_verifiable_test.py
@@ -11,11 +11,10 @@ from ducktape.mark.resource import cluster
 from ducktape.utils.util import wait_until
 from ducktape.cluster.cluster_spec import ClusterSpec
 
-import time
-import random
-
+from rptest.clients.rpk import RpkTool
 from rptest.clients.types import TopicSpec
 from rptest.tests.redpanda_test import RedpandaTest
+from rptest.services.redpanda import SISettings
 from rptest.services.franz_go_verifiable_services import FranzGoVerifiableProducer, FranzGoVerifiableSeqConsumer, FranzGoVerifiableRandomConsumer
 
 
@@ -59,3 +58,82 @@ class FranzGoVerifiableTest(RedpandaTest):
         self._rand_consumer.start()
 
         self._producer.wait()
+
+
+class FranzGoVerifiableWithSiTest(RedpandaTest):
+    MSG_SIZE = 1000000
+    segment_size = 5 * 1000000
+
+    topics = (TopicSpec(partition_count=100, replication_factor=3), )
+
+    def __init__(self, ctx):
+        si_settings = SISettings(
+            log_segment_size=FranzGoVerifiableWithSiTest.segment_size,
+            cloud_storage_cache_size=5 *
+            FranzGoVerifiableWithSiTest.segment_size)
+
+        super(FranzGoVerifiableWithSiTest, self).__init__(
+            test_context=ctx,
+            num_brokers=3,
+            extra_rp_conf={
+                # Disable prometheus metrics, because we are doing lots
+                # of restarts with lots of partitions, and current high
+                # metric counts make that sometimes cause reactor stalls
+                # during shutdown on debug builds.
+                'disable_metrics': True,
+
+                # We will run relatively large number of partitions
+                # and want it to work with slow debug builds and
+                # on noisy developer workstations: relax the raft
+                # intervals
+                'election_timeout_ms': 5000,
+                'raft_heartbeat_interval_ms': 500,
+            },
+            si_settings=si_settings)
+
+        self._node_for_franz_go = ctx.cluster.alloc(
+            ClusterSpec.simple_linux(1))
+
+        self._producer = FranzGoVerifiableProducer(
+            ctx, self.redpanda, self.topic,
+            FranzGoVerifiableWithSiTest.MSG_SIZE, 20000,
+            self._node_for_franz_go)
+        self._seq_consumer = FranzGoVerifiableSeqConsumer(
+            ctx, self.redpanda, self.topic,
+            FranzGoVerifiableWithSiTest.MSG_SIZE, self._node_for_franz_go)
+        self._rand_consumer = FranzGoVerifiableRandomConsumer(
+            ctx, self.redpanda, self.topic,
+            FranzGoVerifiableWithSiTest.MSG_SIZE, 1000, 20,
+            self._node_for_franz_go)
+
+    # In the future producer will signal about json creation
+    def _create_json_file(self):
+        small_producer = FranzGoVerifiableProducer(
+            self.test_context, self.redpanda, self.topic,
+            FranzGoVerifiableWithSiTest.MSG_SIZE, 10000,
+            self._node_for_franz_go)
+        small_producer.start()
+        small_producer.wait()
+
+    @cluster(num_nodes=4)
+    def test_with_all_type_of_loads_and_si(self):
+
+        rpk = RpkTool(self.redpanda)
+        rpk.alter_topic_config(self.topic, 'redpanda.remote.write', 'true')
+        rpk.alter_topic_config(self.topic, 'redpanda.remote.read', 'true')
+        rpk.alter_topic_config(self.topic, 'retention.bytes',
+                               str(self.segment_size))
+
+        # Need create json file for consumer at first
+        self._create_json_file()
+
+        # Verify that we really enabled shadow indexing correctly, such
+        # that some objects were written
+        objects = list(self.redpanda.get_objects_from_si())
+        assert len(objects) > 0
+        for o in objects:
+            self.logger.info(f"S3 object: {o.Key}, {o.ContentLength}")
+
+        self._producer.start()
+        self._seq_consumer.start()
+        self._rand_consumer.start()
