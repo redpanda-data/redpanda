graphcareful/redpanda-1

kafka/s: Reference the copro partition_manager
- To handle copro requests v/kafka looked up the copro flag in the
metadata cache, and on success then obtained a raw log from storage.

- This is being replaced with a single looking with the
coproc::partition_manager.

- Although this simplifies the logic in make_partition_proxy, this is
necessitated to fix an underlying race between fetch and shutdown.
materialized_partition can not just wrap a storage::log without
gates/sharing semantics. coproc::partition + manager is the
comprehensive solution for this bug.

src/v/kafka/server/materialized_partition.h
@@ -10,30 +10,30 @@
  */
 #pragma once
 #include "cluster/partition_probe.h"
+#include "coproc/partition.h"
 #include "kafka/server/partition_proxy.h"
 #include "storage/log.h"
 
 namespace kafka {
 class materialized_partition final : public kafka::partition_proxy::impl {
 public:
-    materialized_partition(
-      storage::log log, ss::lw_shared_ptr<cluster::partition> p)
-      : _log(log)
-      , _probe(cluster::make_materialized_partition_probe())
+    explicit materialized_partition(
+      ss::lw_shared_ptr<coproc::partition> p) noexcept
+      : _probe(cluster::make_materialized_partition_probe())
       , _partition(p) {}
 
-    const model::ntp& ntp() const final { return _log.config().ntp(); }
+    const model::ntp& ntp() const final { return _partition->ntp(); }
     model::offset start_offset() const final {
-        model::offset start = _log.offsets().start_offset;
+        model::offset start = _partition->start_offset();
         return start < model::offset{0} ? model::offset{0} : start;
     }
 
     model::offset high_watermark() const final {
-        return raft::details::next_offset(_log.offsets().dirty_offset);
+        return raft::details::next_offset(_partition->last_offset());
     }
 
     model::offset last_stable_offset() const final {
-        return raft::details::next_offset(_log.offsets().dirty_offset);
+        return raft::details::next_offset(_partition->last_offset());
     }
 
     bool is_leader() const final { return _partition->is_leader(); }
@@ -41,13 +41,13 @@ public:
     ss::future<model::record_batch_reader> make_reader(
       storage::log_reader_config cfg,
       std::optional<model::timeout_clock::time_point>) final {
-        return _log.make_reader(cfg);
+        return _partition->make_reader(cfg);
     }
 
     ss::future<std::optional<storage::timequery_result>>
     timequery(model::timestamp ts, ss::io_priority_class io_pc) final {
-        storage::timequery_config cfg(ts, _log.offsets().dirty_offset, io_pc);
-        return _log.timequery(cfg);
+        storage::timequery_config cfg(ts, last_stable_offset(), io_pc);
+        return _partition->timequery(cfg);
     };
 
     ss::future<std::vector<cluster::rm_stm::tx_range>>
@@ -63,9 +63,8 @@ private:
         return o > model::offset(0) ? o : model::offset(0);
     }
 
-    storage::log _log;
     cluster::partition_probe _probe;
-    ss::lw_shared_ptr<cluster::partition> _partition;
+    ss::lw_shared_ptr<coproc::partition> _partition;
 };
 
 } // namespace kafka

src/v/kafka/server/partition_proxy.cc
@@ -12,6 +12,7 @@
 #include "partition_proxy.h"
 
 #include "cluster/partition_manager.h"
+#include "coproc/partition_manager.h"
 #include "kafka/server/materialized_partition.h"
 #include "kafka/server/replicated_partition.h"
 
@@ -19,31 +20,17 @@ namespace kafka {
 
 std::optional<partition_proxy> make_partition_proxy(
   const model::ntp& ntp,
-  cluster::metadata_cache& md_cache,
-  cluster::partition_manager& pm) {
-    auto log = pm.get(ntp);
-    if (log) {
-        return make_partition_proxy<replicated_partition>(log);
+  cluster::partition_manager& pm,
+  coproc::partition_manager& cpm) {
+    auto partition = pm.get(ntp);
+    if (partition) {
+        return make_partition_proxy<replicated_partition>(partition);
     }
-    auto mts = md_cache.get_source_topic(model::topic_namespace_view{ntp});
-    if (!mts) {
-        return std::nullopt;
+    auto cp_partition = cpm.get(ntp);
+    if (cp_partition) {
+        return make_partition_proxy<materialized_partition>(cp_partition);
     }
-    auto source_ntp = model::ntp(ntp.ns, *mts, ntp.tp.partition);
-    auto src_log = pm.get(source_ntp);
-    if (!src_log) {
-        /// Unlikley casse where controller_backend hasn't yet created the
-        /// partition for the source ntp.
-        return std::nullopt;
-    }
-    auto materialized_log = pm.log(ntp);
-    if (!materialized_log) {
-        /// Unlikley case where controller_backend hasn't yet created the
-        /// storage::log for the materialized ntp
-        return std::nullopt;
-    }
-    return make_partition_proxy<materialized_partition>(
-      *materialized_log, src_log);
+    return std::nullopt;
 }
 
 } // namespace kafka

redpanda-data/redpanda

(cherry picked from commit 0155c3000aa00a12cfd79ac299b3b7ac91d9065f)

src/v/kafka/server/materialized_partition.h
@@ -10,6 +10,7 @@
  */
 #pragma once
 #include "cluster/partition_probe.h"
+#include "coproc/partition.h"
 #include "kafka/server/partition_proxy.h"
 #include "raft/errc.h"
 #include "storage/log.h"
@@ -19,30 +20,29 @@
 namespace kafka {
 class materialized_partition final : public kafka::partition_proxy::impl {
 public:
-    materialized_partition(
-      storage::log log, ss::lw_shared_ptr<cluster::partition> p)
-      : _log(log)
-      , _probe(cluster::make_materialized_partition_probe())
+    explicit materialized_partition(
+      ss::lw_shared_ptr<coproc::partition> p) noexcept
+      : _probe(cluster::make_materialized_partition_probe())
       , _partition(p) {}
 
-    const model::ntp& ntp() const final { return _log.config().ntp(); }
+    const model::ntp& ntp() const final { return _partition->ntp(); }
     model::offset start_offset() const final {
-        model::offset start = _log.offsets().start_offset;
+        model::offset start = _partition->start_offset();
         return start < model::offset{0} ? model::offset{0} : start;
     }
 
     model::offset high_watermark() const final {
-        return raft::details::next_offset(_log.offsets().dirty_offset);
+        return raft::details::next_offset(_partition->dirty_offset());
     }
 
     model::offset last_stable_offset() const final {
-        return raft::details::next_offset(_log.offsets().dirty_offset);
+        return raft::details::next_offset(_partition->dirty_offset());
     }
 
     bool is_leader() const final { return _partition->is_leader(); }
 
     ss::future<std::error_code> linearizable_barrier() final {
-        return _partition->linearizable_barrier().then(
+        return _partition->source_partition()->linearizable_barrier().then(
           [](result<model::offset> r) {
               if (r) {
                   return raft::make_error_code(raft::errc::success);
@@ -54,7 +54,8 @@ public:
     ss::future<storage::translating_reader> make_reader(
       storage::log_reader_config cfg,
       std::optional<model::timeout_clock::time_point>) final {
-        co_return storage::translating_reader(co_await _log.make_reader(cfg));
+        co_return storage::translating_reader(
+          co_await _partition->make_reader(cfg));
     }
 
     ss::future<std::optional<storage::timequery_result>> timequery(
@@ -62,7 +63,7 @@ public:
       model::offset offset_limit,
       ss::io_priority_class io_pc) final {
         storage::timequery_config cfg(ts, offset_limit, io_pc);
-        return _log.timequery(cfg);
+        return _partition->timequery(cfg);
     };
 
     ss::future<std::vector<cluster::rm_stm::tx_range>> aborted_transactions(
@@ -80,9 +81,8 @@ private:
         return o > model::offset(0) ? o : model::offset(0);
     }
 
-    storage::log _log;
     cluster::partition_probe _probe;
-    ss::lw_shared_ptr<cluster::partition> _partition;
+    ss::lw_shared_ptr<coproc::partition> _partition;
 };
 
 } // namespace kafka

src/v/kafka/server/partition_proxy.cc
@@ -12,6 +12,7 @@
 #include "partition_proxy.h"
 
 #include "cluster/partition_manager.h"
+#include "coproc/partition_manager.h"
 #include "kafka/server/materialized_partition.h"
 #include "kafka/server/replicated_partition.h"
 
@@ -19,31 +20,17 @@ namespace kafka {
 
 std::optional<partition_proxy> make_partition_proxy(
   const model::ntp& ntp,
-  cluster::metadata_cache& md_cache,
-  cluster::partition_manager& pm) {
-    auto log = pm.get(ntp);
-    if (log) {
-        return make_partition_proxy<replicated_partition>(log);
+  cluster::partition_manager& cluster_pm,
+  coproc::partition_manager& coproc_pm) {
+    auto partition = cluster_pm.get(ntp);
+    if (partition) {
+        return make_partition_proxy<replicated_partition>(partition);
     }
-    auto mts = md_cache.get_source_topic(model::topic_namespace_view{ntp});
-    if (!mts) {
-        return std::nullopt;
+    auto cp_partition = coproc_pm.get(ntp);
+    if (cp_partition) {
+        return make_partition_proxy<materialized_partition>(cp_partition);
     }
-    auto source_ntp = model::ntp(ntp.ns, *mts, ntp.tp.partition);
-    auto src_log = pm.get(source_ntp);
-    if (!src_log) {
-        /// Unlikley casse where controller_backend hasn't yet created the
-        /// partition for the source ntp.
-        return std::nullopt;
-    }
-    auto materialized_log = pm.log(ntp);
-    if (!materialized_log) {
-        /// Unlikley case where controller_backend hasn't yet created the
-        /// storage::log for the materialized ntp
-        return std::nullopt;
-    }
-    return make_partition_proxy<materialized_partition>(
-      *materialized_log, src_log);
+    return std::nullopt;
 }
 
 } // namespace kafka