913f19d928f567dea442a98add5290f4bcd5329f
Ben Pope <ben@vectorized.io>
2022-01-24 17:38:00 +0000

    schema_registry/store: Copy value to prevent crash
    The previous commits address Core CPP guideline CP.51 and CP.53,
    but there is still a crash when producing schema in quick succession.
    
    Copy the schema to prevent UB.
    
    Fix #3559
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/schema_registry/sharded_store.cc
+++ src/v/pandaproxy/schema_registry/sharded_store.cc
@@ -102,8 +102,7 @@ sharded_store::make_valid_schema(canonical_schema schema) {
     case schema_type::avro:
         co_return make_avro_schema_definition(schema.def().raw()()).value();
     case schema_type::protobuf:
-        co_return co_await make_protobuf_schema_definition(
-          *this, std::move(schema));
+        co_return co_await make_protobuf_schema_definition(*this, schema);
     case schema_type::json:
         throw as_exception(invalid_schema_type(schema.type()));
     }
