a37f97b72bf626bce0c7fedbb7491031a894fadb
Michal Maslanka <michal@vectorized.io>
2022-02-03 14:17:32 +0100

    tools: added simple tool to map group id to underlying partition
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

---
+++ tools/group_id_mapper/mapper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+import jump
+import xxhash
+
+
+def main():
+    import argparse
+
+    def generate_options():
+        parser = argparse.ArgumentParser(
+            description='Redpanda Consumer Group Mapper')
+        parser.add_argument('group_id',
+                            type=str,
+                            help='Id of the group to map')
+        parser.add_argument('--partition_count',
+                            type=int,
+                            help='Number of consumer group topic partitions',
+                            required=True)
+
+        return parser
+
+    parser = generate_options()
+    options, _ = parser.parse_known_args()
+    xx = xxhash.xxh64(seed=0)
+
+    xx.update(options.group_id)
+
+    partition_id = jump.hash(xx.intdigest(), options.partition_count)
+    print(f"kafka_internal/group/{partition_id}")
+
+
+if __name__ == '__main__':
+    main()
