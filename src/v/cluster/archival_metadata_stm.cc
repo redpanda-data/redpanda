5caaa3b0611c86e4583148dd77f1ce7cff580b19
Evgeny Lazin <evgeny@vectorized.io>
2022-02-09 07:44:55 -0500

    archival: Fix shutdown deadlock
    The following situation triggers a deadlock during shutdown.
    - The node is being stopped and scheduler_serivce::stop is invoked.
    - The method calls ntp_archiver::stop for every archiver that it has.
    - If the archiver operation hangs the shutdown can't proceed and because
      the scheduler_service is stopped before most other services the node
      stays online.
    
    The ntp_archiver::stop can hang if after the manifest update we're
    trying to update the archival_meta_stm but all other replica nodes of
    the same ntp are already down. This can happen during cluster shutdown
    or during normral operation (if some nodes are not available for
    whatever reasons).
    
    The code inside the ntp_arichiver sets the timeout via the
    retry_chain_node that it passes to archival_meta_stm. But the problem is
    that the call to _raft->replicate inside the archival_meta_stm doesn't
    allow to set a timeout. So when all replicas are not responsive it will
    just wait for the quorum. When this happens during node shutdown we can
    expect that the raft group will be stopped and the 'replicate' call will
    throw an exception or return an error. But it doesn't happen because the
    archival subsytem is being shut down in the begining. And when it hangs
    it prevents the partition_manager and other components to be stopped. So
    the raft_group never gets stopped.
    
    This commit fixes that by using a ss::with_timeout wrapper. In case of
    timeout the exception will be thrown inside the upload loop and the
    whole operation will be retried some time later. The ntp_archiver itself
    won't be used before that moment.
    
    (cherry picked from commit 1e4abb2e7257e998574fb827ee5741e4f41d4e13)

--- src/v/cluster/archival_metadata_stm.cc
+++ src/v/cluster/archival_metadata_stm.cc
@@ -104,6 +104,8 @@ ss::future<std::error_code> archival_metadata_stm::do_add_segments(
         co_return errc::timeout;
     }
 
+    rc_node.check_abort();
+
     auto segments = segments_from_manifest(new_manifest.difference(_manifest));
     if (segments.empty()) {
         co_return errc::success;
@@ -119,10 +121,13 @@ ss::future<std::error_code> archival_metadata_stm::do_add_segments(
     }
 
     auto batch = std::move(b).build();
-    auto result = co_await _raft->replicate(
+    auto fut = _raft->replicate(
       _insync_term,
       model::make_memory_record_batch_reader(std::move(batch)),
       raft::replicate_options{raft::consistency_level::quorum_ack});
+
+    auto result = co_await ss::with_timeout(
+      rc_node.get_deadline(), std::move(fut));
 
     if (!result) {
         vlog(
