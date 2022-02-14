2001039dc875a878eb23db39e98db2ab9a5cfada
Michal Maslanka <michal@vectorized.io>
2022-02-14 10:16:31 +0100

    kafka: added leader epoch validation helper
    If client provided leader epoch is greater than current partition
    leader epoch broker returns `UNKNOWN_LEADER_EPOCH` error. When client
    provided leader epoch is smaller than partition leader epoch then broker
    returns `FENCED_LEADER_EPOCH` error.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>
    (cherry picked from commit b2c16b84794b5ee013a9f9b6be36471a7916b119)

---
+++ src/v/kafka/server/handlers/details/leader_epoch.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#pragma once
+
+#include "kafka/protocol/errors.h"
+#include "kafka/server/partition_proxy.h"
+#include "kafka/types.h"
+
+#include <compare>
+
+namespace kafka::details {
+
+inline kafka::error_code check_leader_epoch(
+  kafka::leader_epoch request_epoch, const kafka::partition_proxy& p) {
+    /**
+     * no leader epoch provided, skip validation
+     */
+    if (request_epoch < 0) {
+        return error_code::none;
+    }
+    auto const partition_epoch = p.leader_epoch();
+
+    if (request_epoch > partition_epoch) {
+        return error_code::unknown_leader_epoch;
+    } else if (request_epoch < partition_epoch) {
+        return error_code::fenced_leader_epoch;
+    } else {
+        return error_code::none;
+    }
+}
+
+} // namespace kafka::details
