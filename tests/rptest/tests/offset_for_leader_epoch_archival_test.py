35c012511ead4b0e5ef1279e0b8fb44f45e6e82b
Michal Maslanka <michal@vectorized.io>
2022-02-14 10:34:02 +0100

    tests: added offset for leader epoch api test
    Signed-off-by: Michal Maslanka <michal@vectorized.io>
    (cherry picked from commit 80477c320a1cfde1393af3742ec6bfa5d03e37a1)

---
+++ tests/rptest/tests/offset_for_leader_epoch_archival_test.py
@@ -0,0 +1,95 @@
+# Copyright 2021 Vectorized, Inc.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.md
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0
+
+from ducktape.mark.resource import cluster
+from ducktape.utils.util import wait_until
+
+from rptest.clients.kcl import KCL
+from rptest.clients.rpk import RpkTool
+from rptest.services.redpanda import RESTART_LOG_ALLOW_LIST, SISettings
+from rptest.tests.redpanda_test import RedpandaTest
+from rptest.clients.types import TopicSpec
+from rptest.clients.rpk import RpkTool
+from rptest.util import (
+    produce_until_segments,
+    wait_for_segments_removal,
+)
+
+
+class OffsetForLeaderEpochArchivalTest(RedpandaTest):
+    """
+    Check offset for leader epoch handling
+    """
+    segment_size = 1 * (2 << 20)
+
+    def _produce(self, topic, msg_cnt):
+        rpk = RpkTool(self.redpanda)
+        for i in range(0, msg_cnt):
+            rpk.produce(topic, f"k-{i}", f"v-{i}")
+
+    def __init__(self, test_context):
+        super(OffsetForLeaderEpochArchivalTest, self).__init__(
+            test_context=test_context,
+            extra_rp_conf={
+                'enable_leader_balancer': False,
+                "log_compaction_interval_ms": 1000
+            },
+            si_settings=SISettings(
+                log_segment_size=OffsetForLeaderEpochArchivalTest.segment_size,
+                cloud_storage_cache_size=5 *
+                OffsetForLeaderEpochArchivalTest.segment_size))
+
+    @cluster(num_nodes=3, log_allow_list=RESTART_LOG_ALLOW_LIST)
+    def test_querying_remote_partitions(self):
+        topic = TopicSpec(redpanda_remote_read=True,
+                          redpanda_remote_write=True)
+        epoch_offsets = {}
+        rpk = RpkTool(self.redpanda)
+        self.client().create_topic(topic)
+        rpk.alter_topic_config(topic.name, "redpanda.remote.read", 'true')
+        rpk.alter_topic_config(topic.name, "redpanda.remote.write", 'true')
+
+        def wait_for_topic():
+            wait_until(lambda: len(list(rpk.describe_topic(topic.name))) > 0,
+                       30,
+                       backoff_sec=2)
+
+        # restart whole cluster 6 times to trigger term rolls
+        for i in range(0, 6):
+            wait_for_topic()
+            produce_until_segments(
+                redpanda=self.redpanda,
+                topic=topic.name,
+                partition_idx=0,
+                count=2 * i,
+            )
+            res = list(rpk.describe_topic(topic.name))
+            epoch_offsets[res[0].leader_epoch] = res[0].high_watermark
+            self.redpanda.restart_nodes(self.redpanda.nodes)
+
+        self.logger.info(f"ledear epoch high watermarks: {epoch_offsets}")
+
+        wait_for_topic()
+
+        rpk.alter_topic_config(topic.name, TopicSpec.PROPERTY_RETENTION_BYTES,
+                               OffsetForLeaderEpochArchivalTest.segment_size)
+        wait_for_segments_removal(redpanda=self.redpanda,
+                                  topic=topic.name,
+                                  partition_idx=0,
+                                  count=7)
+        kcl = KCL(self.redpanda)
+
+        for epoch, offset in epoch_offsets.items():
+            self.logger.info(f"querying partition epoch {epoch} end offsets")
+            epoch_end_offset = kcl.offset_for_leader_epoch(
+                topics=topic.name, leader_epoch=epoch)[0].epoch_end_offset
+            self.logger.info(
+                f"epoch {epoch} end_offset: {epoch_end_offset}, expected offset: {offset}"
+            )
+            assert epoch_end_offset == offset
---
+++ tests/rptest/tests/offset_for_leader_epoch_test.py
@@ -0,0 +1,112 @@
+# Copyright 2021 Vectorized, Inc.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.md
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0
+
+import random
+from ducktape.mark.resource import cluster
+from ducktape.utils.util import wait_until
+
+from rptest.clients.kcl import KCL
+from rptest.clients.rpk import RpkTool
+from rptest.services.redpanda import RESTART_LOG_ALLOW_LIST
+from rptest.tests.redpanda_test import RedpandaTest
+from rptest.clients.types import TopicSpec
+from rptest.clients.rpk import RpkTool
+
+
+class OffsetForLeaderEpochTest(RedpandaTest):
+    """
+    Check offset for leader epoch handling
+    """
+    def _produce(self, topic, msg_cnt):
+        rpk = RpkTool(self.redpanda)
+        for i in range(0, msg_cnt):
+            rpk.produce(topic, f"k-{i}", f"v-{i}")
+
+    def __init__(self, test_context):
+        super(OffsetForLeaderEpochTest,
+              self).__init__(num_brokers=5,
+                             test_context=test_context,
+                             extra_rp_conf={
+                                 'enable_leader_balancer': False,
+                                 "log_compaction_interval_ms": 1000
+                             })
+
+    @cluster(num_nodes=5, log_allow_list=RESTART_LOG_ALLOW_LIST)
+    def test_offset_for_leader_epoch(self):
+        replication_factors = [1, 3, 5]
+        cleanup_policies = [
+            TopicSpec.CLEANUP_COMPACT, TopicSpec.CLEANUP_DELETE
+        ]
+        topics = []
+
+        for i in range(0, 10):
+            topics.append(
+                TopicSpec(
+                    partition_count=random.randint(1, 50),
+                    replication_factor=random.choice(replication_factors),
+                    cleanup_policy=random.choice(cleanup_policies)))
+
+        topic_names = [t.name for t in topics]
+        # create test topics
+        self.client().create_topic(topics)
+        kcl = KCL(self.redpanda)
+        for t in topics:
+            self._produce(t.name, 20)
+
+        def list_offsets_map():
+            offsets_map = {}
+            offsets = kcl.list_offsets(topic_names)
+            self.logger.info(f"offsets_list: {offsets}")
+            for p in offsets:
+                offsets_map[(p.topic, p.partition)] = int(p.end_offset)
+            self.logger.info(f"offsets_map: {offsets_map}")
+            return offsets_map
+
+        initial_offsets = list_offsets_map()
+
+        leader_epoch_offsets = kcl.offset_for_leader_epoch(topics=topic_names,
+                                                           leader_epoch=1)
+
+        for o in leader_epoch_offsets:
+            assert initial_offsets[(o.topic,
+                                    o.partition)] == o.epoch_end_offset
+
+        # restart all the nodes to force leader election
+        self.redpanda.restart_nodes(self.redpanda.nodes)
+        # produce more data
+        for t in topics:
+            self._produce(t.name, 20)
+
+        # check epoch end offsets for term 1
+        leader_epoch_offsets = kcl.offset_for_leader_epoch(topics=topic_names,
+                                                           leader_epoch=1)
+
+        for o in leader_epoch_offsets:
+            assert initial_offsets[(o.topic,
+                                    o.partition)] == o.epoch_end_offset
+
+        last_offsets = list_offsets_map()
+        rpk = RpkTool(self.redpanda)
+        for t in topics:
+            tp_desc = rpk.describe_topic(t.name)
+            for p in tp_desc:
+                for o in kcl.offset_for_leader_epoch(
+                        topics=f"{t.name}:{p.id}",
+                        leader_epoch=p.leader_epoch,
+                        current_leader_epoch=p.leader_epoch):
+                    assert last_offsets[(o.topic,
+                                         o.partition)] == o.epoch_end_offset
+
+        # test returning unknown leader epoch error, we use large leader epoch value
+
+        leader_epoch_offsets = kcl.offset_for_leader_epoch(
+            topics=topic_names, leader_epoch=1, current_leader_epoch=1000)
+
+        for o in leader_epoch_offsets:
+            assert o.error is not None and "UNKNOWN_LEADER_EPOCH" in o.error
