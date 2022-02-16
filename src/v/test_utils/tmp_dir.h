ac3725b214f904092888444a575457ff8b5c7587
Evgeny Lazin <evgeny@vectorized.io>
2022-02-16 11:39:09 -0500

    cloud_storage: Improve directory walker tests
    Use seastar::tmp_dir instead of hard coded paths since the hardcoded
    paths cause test failures when the tests are started locally. Also,
    make sure that the temporary dirs and files are deleted.

---
+++ src/v/test_utils/tmp_dir.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Vectorized, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "seastarx.h"
+#include "vlog.h"
+
+#include <seastar/util/log.hh>
+#include <seastar/util/tmp_file.hh>
+
+#include <exception>
+
+namespace details {
+inline ss::logger tmpdir_logger("tmpdir-log");
+}
+
+class temporary_dir {
+public:
+    explicit temporary_dir(const char* test_name)
+      : temporary_dir(std::filesystem::path("."), test_name) {}
+    temporary_dir(const std::filesystem::path& root, const char* test_name) {
+        auto path = root / fmt::format("{}-XXXX", test_name);
+        try {
+            vlog(
+              details::tmpdir_logger.info,
+              "Creating temporary directory {}",
+              path.native());
+            _dir = ss::make_tmp_dir(path.native()).get();
+        } catch (...) {
+            vlog(
+              details::tmpdir_logger.error,
+              "Can't create temporary directory at {}, Error: {}",
+              path,
+              std::current_exception());
+            throw;
+        }
+    }
+    temporary_dir(const temporary_dir&) = delete;
+    temporary_dir& operator=(const temporary_dir&) = delete;
+    temporary_dir(temporary_dir&&) = delete;
+    temporary_dir& operator=(temporary_dir&&) = delete;
+    ~temporary_dir() noexcept {
+        try {
+            vlog(details::tmpdir_logger.info, "About to call remove");
+            if (_dir.has_path()) {
+                _dir.remove().get0();
+            }
+        } catch (...) {
+            vlog(
+              details::tmpdir_logger.error,
+              "Can't remove temporary directory. Error: {}",
+              std::current_exception());
+        }
+    }
+    std::filesystem::path get_path() const { return _dir.get_path(); }
+    ss::future<> remove() { return _dir.remove(); }
+
+private:
+    ss::tmp_dir _dir;
+};
