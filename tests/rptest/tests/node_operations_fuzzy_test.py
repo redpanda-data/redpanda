28b9602bbe16f5a80affa3e5c6d8c08c5652cb0c
Michal Maslanka <michal@vectorized.io>
2022-02-22 08:05:10 +0100

    Revert "test/node_operations_fuzzy: disable flaky CI test"
    This reverts commit ea2aa0eae5272f0203f73ada4f7e94661e38dc58.

--- tests/rptest/tests/node_operations_fuzzy_test.py
+++ tests/rptest/tests/node_operations_fuzzy_test.py
@@ -117,7 +117,7 @@ class NodeOperationFuzzyTest(EndToEndTest):
     """
 
     @cluster(num_nodes=7, log_allow_list=CHAOS_LOG_ALLOW_LIST)
-    # @ignore see issue #3866: @parametrize(enable_failures=True)
+    @parametrize(enable_failures=True)
     @parametrize(enable_failures=False)
     def test_node_operations(self, enable_failures):
         # allocate 5 nodes for the cluster
