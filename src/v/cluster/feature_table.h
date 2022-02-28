24ace36207eb1cf1965746931769d54dcb418089
John Spray <jcs@redpanda.com>
2022-02-28 16:19:03 +0000

    cluster: refactor feature table and add consumer_offsets
    The new consumer_offsets feature has a requirement to
    perform a data migration when the feature becomes available.
    This is expressed as the feature_state::state::preparing state.
    
    To enable that, features now have a finer-grained state,
    rather than just being active or inactive.  Features
    have a feature_spec that defines how they advance
    through the states (i.e. whether they must wait
    for a migration or may proceed straight to activation).

--- src/v/cluster/feature_table.h
+++ src/v/cluster/feature_table.h
@@ -14,14 +14,168 @@
 #include "cluster/types.h"
 #include "utils/waiter_queue.h"
 
+#include <array>
 #include <string_view>
-
-class cluster_version;
 
 namespace cluster {
 
 enum class feature {
     central_config = 0x1,
+};
+
+/**
+ * The definition of a feature specifies rules for when it should
+ * be activated,
+ */
+struct feature_spec {
+    // Policy defining how the feature behaves when in 'available' state.
+    enum class available_policy {
+        // The feature proceeds to activate as soon as it is available
+        always,
+
+        // The feature only becomes available once all cluster nodes
+        // are recent enough *and* an administrator explicitly enables it.
+        explicit_only,
+    };
+
+    // Policy defining whether the feature passes through 'preparing'
+    // state on the way to 'active' state.
+    enum class prepare_policy {
+        // The feature is activated as soon as it becomes available.
+        always,
+
+        // The feature only becomes active once a migration step has
+        // completed and the feature manager has been notified of this.
+        requires_migration
+    };
+
+    constexpr feature_spec(
+      cluster_version require_version_,
+      std::string_view name_,
+      feature bits_,
+      available_policy apol,
+      prepare_policy ppol)
+      : bits(bits_)
+      , name(name_)
+      , require_version(require_version_)
+      , available_rule(apol)
+      , prepare_rule(ppol) {}
+
+    feature bits{0};
+    std::string_view name;
+    cluster_version require_version;
+
+    available_policy available_rule;
+    prepare_policy prepare_rule;
+};
+
+constexpr static std::array feature_schema{feature_spec{
+  cluster_version{1},
+  "central_config",
+  feature::central_config,
+  feature_spec::available_policy::always,
+  feature_spec::prepare_policy::always}};
+
+/**
+ * Feature states
+ * ==============
+ *
+ * Start as unavailable.  Become available once all nodes
+ * are recent enough.
+ *
+ * Once available, either advance straight to 'preparing'
+ * if available_policy is 'always', else wait for administrator
+ * to activate the feature.
+ *
+ * Once in preparing, either advance straight to 'active'
+ * if prepare_policy is 'always', else wait for notification
+ * that preparation is complete before proceeding.
+ *
+ * Features may be disabled at any time, but from unavailable/available
+ * states they go to disabled_clean, whereas from other states they
+ * go to disabled_dirty.  This tracks whether the feature may have
+ * written persistent structures.
+ *
+    ┌──────────────┐           ┌──────────────────┐
+    │              ├──────────►│                  │
+    │  unavailable │           │  disabled_clean  │
+    │              │◄──────────┤                  │
+    └───────┬──────┘           └───┬──────────────┘
+            │                      │            ▲
+            ▼                      │            │
+    ┌──────────────┐               │            │
+    │              │◄──────────────┘            │
+    │   available  │                            │
+    │              ├────────────────────────────┘
+    └───────┬──────┘
+            │
+            ▼
+    ┌──────────────┐            ┌─────────────────────┐
+    │              ├───────────►│                     │
+    │   preparing  │            │  disabled_preparing │
+    │              │◄───────────┤                     │
+    └───────┬──────┘            └─────────────────────┘
+            │
+            ▼
+    ┌──────────────┐            ┌─────────────────────┐
+    │              ├───────────►│                     │
+    │    active    │            │  disabled_active    │
+    │              │◄───────────┤                     │
+    └──────────────┘            └─────────────────────┘
+ *
+ */
+class feature_state {
+public:
+    enum class state {
+        // Unavailable means not all nodes in the cluster are recent
+        unavailable,
+
+        // Available means the feature is eligible for activation.  If the
+        // feature spec allows it, it may proceed autonomously to preparing
+        // or active.  Otherwise, it may have too wait for an administrator
+        // to permit it to activate.
+        available,
+
+        // Preparing means the feature is in the process of a data migration
+        // or other preparatory step.  It will proceed to active status
+        // autonomously.
+        preparing,
+
+        // Active means the feature is up and running and ready to use.  This
+        // is the normal state of most features through most of their lifetime.
+        active,
+
+        // Administratively disabled, but it was in 'active' or 'preparing'
+        // state at some point in the past.  Indicates that while the feature
+        // is disabled now, there may be data structures written to disk that
+        // depend on this feature to read back.
+        // The distinction between _active and _perparing variants is needed
+        // so that when an administrator re-activates the feature, we know
+        // what state to go back into.
+        disabled_active,
+        disabled_preparing,
+
+        // Administratively disabled, and never progressed past 'available'.
+        // Means that any data structures dependent on this feature were
+        // never written to disk, and no migrations for this feature were done.
+        disabled_clean,
+    };
+
+    const feature_spec& spec;
+
+    feature_state(const feature_spec& spec_)
+      : spec(spec_){};
+
+    // External inputs
+    void notify_version(cluster_version v);
+
+    // State transition hooks
+    void transition_available();
+    void transition_preparing();
+    void transition_active();
+
+private:
+    state _state{state::unavailable};
 };
 
 std::string_view to_string_view(feature);
@@ -58,11 +212,15 @@ public:
 
     static cluster_version get_latest_logical_version();
 
+    feature_table();
+
 private:
     // Only for use by our friends feature backend & manager
     void set_active_version(cluster_version);
 
     cluster_version _active_version{invalid_version};
+
+    std::vector<feature_state> _feature_state;
 
     // Bitmask only used at runtime: if we run out of bits for features
     // just use a bigger one.  Do not serialize this as a bitmask anywhere.
