605746de9cdba6b4057d7d6a5d70262e1333a35d
Rob Blafford <rob@vectorized.io>
2022-03-03 17:43:00 -0500

    rptest: Unignoring all wasm_identity* tests
    - Fixes: #2514

--- tests/rptest/tests/wasm_identity_test.py
+++ tests/rptest/tests/wasm_identity_test.py
@@ -8,7 +8,6 @@
 # by the Apache License, Version 2.0
 
 from rptest.services.cluster import cluster
-from ducktape.mark import ignore
 from rptest.clients.types import TopicSpec
 from rptest.wasm.topic import get_source_topic
 from rptest.wasm.topics_result_set import materialized_result_set_compare, group_fan_in_verifier
@@ -50,7 +49,6 @@ class WasmIdentityTest(WasmTest):
         """
         return {topic: self._num_records for topic in self.wasm_test_output()}
 
-    @ignore  # https://github.com/vectorizedio/redpanda/issues/2514
     @cluster(num_nodes=4, log_allow_list=WASM_LOG_ALLOW_LIST)
     def verify_materialized_topics_test(self):
         self.verify_results(materialized_result_set_compare)
@@ -158,7 +156,6 @@ class WasmMultiInputTopicIdentityTest(WasmIdentityTest):
                        script=WasmTemplateRepository.IDENTITY_TRANSFORM)
         ]
 
-    @ignore  # https://github.com/vectorizedio/redpanda/issues/2514
     @cluster(num_nodes=6, log_allow_list=WASM_LOG_ALLOW_LIST)
     def verify_materialized_topics_test(self):
         self.verify_results(materialized_result_set_compare)
@@ -219,7 +216,6 @@ class WasmAllInputsToAllOutputsIdentityTest(WasmIdentityTest):
                        script=WasmTemplateRepository.IDENTITY_TRANSFORM)
         ]
 
-    @ignore  # https://github.com/vectorizedio/redpanda/issues/2514
     @cluster(num_nodes=6, log_allow_list=WASM_LOG_ALLOW_LIST)
     def verify_materialized_topics_test(self):
         # Cannot compare topics to topics, can only verify # of records
