23b562e1302f871606da8c832de8da850d487082
John Spray <jcs@redpanda.com>
2022-03-04 14:00:29 +0000

    tests: extend CHAOS_LOG_ALLOW_LIST
    This is a rare message that can appear during NodeOperationFuzzyTest

--- tests/rptest/services/redpanda.py
+++ tests/rptest/services/redpanda.py
@@ -102,6 +102,9 @@ CHAOS_LOG_ALLOW_LIST = [
     re.compile(
         "rpc - .*Unable to parse received RPC request payload - std::out_of_range"
     ),
+    re.compile(
+        "cluster - .*exception while executing partition operation:.*std::exception (std::exception)"
+    ),
 ]
 
 
