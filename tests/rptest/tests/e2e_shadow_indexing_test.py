1c9538a09393069cd70496f5758f37a1495fdf52
NyaliaLui <nyalia@vectorized.io>
2022-03-08 15:33:47 -0500

    ducky: test SI for data loss
    Addresses Test8 in #3572

--- tests/rptest/tests/e2e_shadow_indexing_test.py
+++ tests/rptest/tests/e2e_shadow_indexing_test.py
@@ -435,3 +435,178 @@ class MoreE2EShadowIndexingTest(EndToEndTest):
         for ex in self._exceptions:
             if ex:
                 raise ex
+
+    def _listdir(self, path, node, only_dirs=False):
+        try:
+            ents = node.account.sftp_client.listdir(path)
+        except FileNotFoundError:
+            # The SI cache doesn't exist until something
+            # is fetched from the bucket. So the
+            # FileNotFoundError is possible.
+            return []
+
+        if not only_dirs:
+            return ents
+        paths = map(lambda fn: (fn, os.path.join(path, fn)), ents)
+
+        def safe_isdir(path):
+            try:
+                return node.account.isdir(path)
+            except FileNotFoundError:
+                # Things that no longer exist are also no longer directories
+                return False
+
+        return [p[0] for p in paths if safe_isdir(p[1])]
+
+    # Useful for debugging
+    def _walk_path(self, root):
+        for node in self.redpanda.nodes:
+            dirs = [root]
+
+            # Simple dir walk implementation.
+            while len(dirs) > 0:
+                path = dirs.pop()
+                contents = self._listdir(path, node)
+                self.logger.debug(
+                    f'node: {node.name}, path: {path}, contents: {contents}')
+
+                sub_dirs = self._listdir(path, node, only_dirs=True)
+                for sub_dir in sub_dirs:
+                    dirs.append(f'{path}/{sub_dir}')
+
+    def _find_segment_files_on_node(self, topic_path, node, segments):
+        # Get the partition directories
+        partition_dirs = self._listdir(topic_path, node, only_dirs=True)
+        self.logger.debug(
+            f'node: {node.name}, path: {topic_path}, partition dirs: {partition_dirs}'
+        )
+
+        for part_dir in partition_dirs:
+            part_path = f'{topic_path}/{part_dir}'
+            files = self._listdir(part_path, node)
+
+            self.logger.debug(
+                f'node: {node.name}, path: {part_path}, files: {files}')
+            for _file in files:
+                self.logger.debug(f'file: {_file}')
+                # The regex is from src/v/storage/fs_utils.h::parse_segment_filename
+                match = re.match(r'(^(\d+)-(\d+)-([\x00-\x7F]+).log$)', _file)
+                if match:
+                    segments.append(_file)
+
+    def _find_segment_files_on_all_nodes(self, segments):
+        topic_path = f'/var/lib/redpanda/data/kafka/{self.topic}'
+
+        for node in self.redpanda.nodes:
+            self._find_segment_files_on_node(topic_path, node, segments)
+
+    def _check_si_cache_before_fetch(self):
+        # SI cache should be empty on all nodes
+        for node in self.redpanda.nodes:
+            contents = self._listdir(
+                '/var/lib/redpanda/data/cloud_storage_cache', node)
+            assert contents == [], f'SI cache on {node.name} is not empty before fetch'
+
+    def _check_si_cache_after_fetch(self, data_dir_segments):
+        # Sample file path:
+        # /var/lib/redpanda/data/cloud_storage_cache/<some string>/kafka/<topic name>/<partition>/<segment file>
+        root = '/var/lib/redpanda/data/cloud_storage_cache'
+
+        segment_files = []
+
+        # Get the paths to the partition dirs within the
+        # SI cache
+        for node in self.redpanda.nodes:
+            all_parts_dirs = []
+            some_dirs = self._listdir(root, node, only_dirs=True)
+
+            for d in some_dirs:
+                path = f'{root}/{d}/kafka/{self.topic}'
+                parts_dirs = self._listdir(path, node, only_dirs=True)
+
+                # Merge the lists of partitions together.
+                all_parts_dirs += parts_dirs
+
+            for part_dir in all_parts_dirs:
+                path = f'{root}/{d}/kafka/{self.topic}/{part_dir}'
+                files = self._listdir(path, node)
+
+                # The data within the SI cache is a .log.N segment file
+                # where N is an positive integer
+                # Trim off the .N of the segment file
+                temp = [_file[:-2] for _file in files]
+
+                # Merge the list of files
+                segment_files += temp
+
+        for segment_file in segment_files:
+            match = re.match(r'(^(\d+)-(\d+)-([\x00-\x7F]+).log$)',
+                             segment_file)
+            assert match, f'Invalid segment filename {segment_file}'
+            assert segment_file in data_dir_segments, f'{segment_file} was not in data dir'
+
+    @cluster(num_nodes=5)
+    def test_data_loss(self):
+        # Node alloc is first because CI fails in the debug pipeline due to
+        # the warning message "Test requested X nodes, used only Y"
+        fgo_node = self.test_context.cluster.alloc(ClusterSpec.simple_linux(1))
+        self.logger.debug(f"Allocated verifier node {fgo_node[0].name}")
+
+        # 1k messages of size 2**15
+        # is ~32MB of data.
+        msg_size = 2**15
+        msg_count = 1000
+        producer = FranzGoVerifiableProducer(self.test_context, self.redpanda,
+                                             self.topic, msg_size, msg_count,
+                                             fgo_node)
+
+        consumer = RpkConsumer(self.test_context,
+                               self.redpanda,
+                               self.topic,
+                               num_msgs=msg_count)
+
+        self.logger.info(f"Environment: {os.environ}")
+        if os.environ.get('BUILD_TYPE', None) == 'debug':
+            self.logger.info(
+                "Skipping test in debug mode (requires release build)")
+            return
+
+        # Remote write/read and retention set at topic level
+        rpk = RpkTool(self.redpanda)
+        rpk.alter_topic_config(self.topic, 'redpanda.remote.write', 'true')
+        rpk.alter_topic_config(self.topic, 'redpanda.remote.read', 'true')
+        rpk.alter_topic_config(self.topic, 'retention.bytes',
+                               str(self.segment_size))
+        # Small retention.ms so we do not have to wait that long
+        # for segment removal
+        retention_ms = 120000
+        rpk.alter_topic_config(self.topic, 'retention.ms', str(retention_ms))
+
+        self._gen_manifests(msg_size, fgo_node)
+
+        # Produce some data
+        producer.start(clean=False)
+        producer.wait()
+
+        # Profile the data dir
+        self.logger.debug('Before retention')
+        data_dir_segments = []
+        self._find_segment_files_on_all_nodes(data_dir_segments)
+
+        # Wait for retention to kick in.
+        time.sleep((retention_ms / 1000))
+
+        self.logger.debug('Before fetch')
+        self._check_si_cache_before_fetch()
+
+        # Fetch the data. The data should go
+        # into the SI cache.
+        consumer.start()
+        consumer.wait()
+
+        assert len(consumer.messages) == msg_count
+
+        # The SI cache should have segments in it.
+        # Check if those segment files existed before retention
+        self.logger.debug('After fetch')
+        self._check_si_cache_after_fetch(data_dir_segments)
