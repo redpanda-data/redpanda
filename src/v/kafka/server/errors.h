740292c402288c96edb206b4b9aa9d527c39b53b
Michal Maslanka <michal@vectorized.io>
2022-03-16 09:35:46 +0100

    cluster/errc: added error indicating that feature is disabled
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/kafka/server/errors.h
+++ src/v/kafka/server/errors.h
@@ -71,6 +71,7 @@ constexpr error_code map_topic_error_code(cluster::errc code) {
     case cluster::errc::wating_for_partition_shutdown:
     case cluster::errc::error_collecting_health_report:
     case cluster::errc::leadership_changed:
+    case cluster::errc::feature_disabled:
         break;
     }
     return error_code::unknown_server_error;
