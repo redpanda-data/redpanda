aee282f647c8ea0cb31925c6dec398b40c66f9ba
Andrew Hsu <xuzuan@gmail.com>
2022-03-21 20:47:39 +0000

    replace vectorized with redpanda in 2020 copyright
    find . -type f -exec sed -i 's|Copyright 2020 Vectorized, Inc.|Copyright 2020 Redpanda Data, Inc.|g' {} \;

--- src/v/pandaproxy/schema_registry/test/consume_to_store.cc
+++ src/v/pandaproxy/schema_registry/test/consume_to_store.cc
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
-// Copyright 2020 Vectorized, Inc.
+// Copyright 2020 Redpanda Data, Inc.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.md
