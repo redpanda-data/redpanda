ff239e1fb30137fc0cee57a8fc61f3c603f2f53f
Andrew Hsu <xuzuan@gmail.com>
2022-03-21 17:03:46 -0500

    Update src/v/redpanda/main.cc
    Co-authored-by: Coral Waters <95656677+coral-waters@users.noreply.github.com>

--- src/v/redpanda/main.cc
+++ src/v/redpanda/main.cc
@@ -18,13 +18,13 @@ static constexpr std::string_view community_msg = R"banner(
 
 Welcome to the Redpanda community!
 
-Documentation: https://docs.redpanda.com - official docs site
-Github Discussion: https://github.com/redpanda-data/redpanda/discussions - longer, async, thoughtful discussions
-GitHub Issues: https://github.com/redpanda-data/redpanda/issues - reporting and tracking issues with the codebase
-Support: https://support.redpanda.com - share private information with the production support team
-Product Feedback: https://redpanda.com/feedback - let us know how we can improve your experience
-Slack: https://redpanda.com/slack - the main way the community interacts with one another in real time :)
-Twitter: https://twitter.com/redpandadata - come say hi!
+Documentation: https://docs.redpanda.com - Product documentation site
+GitHub Discussion: https://github.com/redpanda-data/redpanda/discussions - Longer, more involved discussions
+GitHub Issues: https://github.com/redpanda-data/redpanda/issues - Report and track issues with the codebase
+Support: https://support.redpanda.com - Contact the support team privately
+Product Feedback: https://redpanda.com/feedback - Let us know how we can improve your experience
+Slack: https://redpanda.com/slack - Chat about all things Redpanda. Join the conversation!
+Twitter: https://twitter.com/redpandadata - All the latest Redpanda news!
 
 )banner";
 
