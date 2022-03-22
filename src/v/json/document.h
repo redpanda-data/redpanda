5e2b766dffb588a5205b213c74bb1829fb1089a8
Ben Pope <ben@vectorized.io>
2022-03-22 11:07:33 +0000

    json: Add GenericDocument and Value
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/json/document.h
+++ src/v/json/document.h
@@ -9,14 +9,23 @@
 
 #pragma once
 
+#include "json/_include_first.h"
 #include "json/allocator.h"
+#include "json/encodings.h"
 
 #include <rapidjson/document.h>
-#include <rapidjson/encodings.h>
 
 namespace json {
-using Document = rapidjson::GenericDocument<
-  rapidjson::UTF8<>,
-  rapidjson::MemoryPoolAllocator<throwing_allocator>,
-  throwing_allocator>;
-}
+
+template<typename Encoding = json::UTF8<>>
+using GenericDocument = rapidjson::
+  GenericDocument<Encoding, MemoryPoolAllocator, throwing_allocator>;
+
+using Document = GenericDocument<>;
+
+template<typename Encoding = json::UTF8<>>
+using GenericValue = typename GenericDocument<Encoding>::ValueType;
+
+using Value = GenericValue<>;
+
+} // namespace json
