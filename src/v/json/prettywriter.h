5c91c0ddb92674eb48f45952be151f1b4b70694b
Ben Pope <ben@vectorized.io>
2022-03-22 11:10:08 +0000

    json: Introduce PrettyWriter
    Signed-off-by: Ben Pope <ben@vectorized.io>

---
+++ src/v/json/prettywriter.h
@@ -0,0 +1,32 @@
+// Copyright 2022 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#pragma once
+
+#include "json/_include_first.h"
+#include "json/allocator.h"
+#include "json/encodings.h"
+
+#include <rapidjson/prettywriter.h>
+
+namespace json {
+
+template<
+  typename OutputStream,
+  typename SourceEncoding = json::UTF8<>,
+  typename TargetEncoding = json::UTF8<>,
+  unsigned writeFlags = rapidjson::kWriteDefaultFlags>
+using PrettyWriter = rapidjson::PrettyWriter<
+  OutputStream,
+  SourceEncoding,
+  TargetEncoding,
+  throwing_allocator,
+  writeFlags>;
+
+} // namespace json
