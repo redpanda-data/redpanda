0613f81fb645b96e956f34edc49e446499dbe0e7
Ben Pope <ben@vectorized.io>
2022-03-22 11:10:20 +0000

    json: Introduce Reader
    Signed-off-by: Ben Pope <ben@vectorized.io>

---
+++ src/v/json/reader.h
@@ -0,0 +1,27 @@
+// Copyright 2022 Vectorized, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#pragma once
+
+#include "json/allocator.h"
+#include "json/encodings.h"
+
+#include <rapidjson/reader.h>
+
+namespace json {
+
+template<
+  typename SourceEncoding = json::UTF8<>,
+  typename TargetEncoding = json::UTF8<>>
+using GenericReader = rapidjson::
+  GenericReader<SourceEncoding, TargetEncoding, throwing_allocator>;
+
+using Reader = GenericReader<>;
+
+} // namespace json
