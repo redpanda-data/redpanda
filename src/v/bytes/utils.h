9ec477f1e4cf1b7de32567aec375235e9468b82d
Michal Maslanka <michal@vectorized.io>
2022-03-25 19:50:27 +0100

    iobuf: added is_zero utility function
    Added utility function indicating that `iobuf` is filled with zeros.
    
    Signed-off-by: Michal Maslanka <michal@vectorized.io>

--- src/v/bytes/utils.h
+++ src/v/bytes/utils.h
@@ -21,3 +21,24 @@ inline void crc_extend_iobuf(crc::crc32c& crc, const iobuf& buf) {
         return ss::stop_iteration::no;
     });
 }
+
+inline bool is_zero(const char* data, size_t size) {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    return data[0] == 0 && memcmp(data, data + 1, size - 1) == 0;
+}
+
+inline bool is_zero(const iobuf& buffer) {
+    if (buffer.empty()) {
+        return false;
+    }
+    bool ret = true;
+    iobuf::iterator_consumer in(buffer.cbegin(), buffer.cend());
+    in.consume(buffer.size_bytes(), [&ret](const char* src, size_t len) {
+        if (!is_zero(src, len)) {
+            ret = false;
+            return ss::stop_iteration::yes;
+        }
+        return ss::stop_iteration::no;
+    });
+    return ret;
+}
