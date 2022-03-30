afa66c8bd7f960514a21d02905ab80cd24918ee7
nicolaferraro <ni.ferraro@gmail.com>
2022-03-30 12:13:22 +0200

    (split) operator: add license headers where missing

--- pkg/resources/configuration/configuration.go
+++ pkg/resources/configuration/configuration.go
@@ -1,3 +1,12 @@
+// Copyright 2022 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
 // Package configuration provide tools for the operator to manage cluster configuration
 package configuration
 
