6ad820e0eeb40715c4ceb502198a6b3314ab3f81
nicolaferraro <ni.ferraro@gmail.com>
2022-03-31 12:59:11 +0200

    operator: externalize utils function for checking readiness

---
+++ src/go/k8s/pkg/utils/kubernetes.go
@@ -0,0 +1,23 @@
+// Copyright 2022 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package utils
+
+import corev1 "k8s.io/api/core/v1"
+
+// IsPodReady tells if a given pod is ready looking at its status.
+func IsPodReady(pod *corev1.Pod) bool {
+	for _, c := range pod.Status.Conditions {
+		if c.Type == corev1.PodReady && c.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+
+	return false
+}
