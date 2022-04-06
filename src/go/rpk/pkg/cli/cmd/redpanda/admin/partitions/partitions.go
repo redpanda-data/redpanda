e3b0a1f6d15eedd2a941de06f63f45c8a6ad8b20
Utkarsh <utkarsh.bhatnagar@lacework.net>
2022-04-06 15:11:27 -0700

    Topic Partition mapping works

--- src/go/rpk/pkg/cli/cmd/redpanda/admin/partitions/partitions.go
+++ src/go/rpk/pkg/cli/cmd/redpanda/admin/partitions/partitions.go
@@ -14,6 +14,7 @@ package partitions
 import (
 	"context"
 	"strconv"
+	"strings"
 
 	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/config"
 	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/kafka"
@@ -32,6 +33,7 @@ func NewCommand(fs afero.Fs) *cobra.Command {
 	}
 	cmd.AddCommand(
 		newListCommand(fs),
+		newMoveAllCommand(fs),
 	)
 	return cmd
 }
@@ -89,4 +91,110 @@ func newListCommand(fs afero.Fs) *cobra.Command {
 	cmd.Flags().BoolVarP(&leaderOnly, "leader-only", "l", false, "print the partitions on broker which are leaders")
 
 	return cmd
+}
+
+func newMoveAllCommand(fs afero.Fs) *cobra.Command {
+	var (
+		sourceBrokerId string
+		destinationBrokerIds string
+	)
+        cmd :=  &cobra.Command{
+                Use:     "move-all --source-broker-id [SOURCE BROKER ID] --destination-broker-ids [DESTINATION BROKER IDs]",
+                Aliases: []string{"mv-all"},
+                Short:   "Move all the partitions from a source broker to multiple destination brokers in a cluster.",
+                Args:    cobra.ExactArgs(0),
+                Run: func(cmd *cobra.Command, args []string) {
+
+			p := config.ParamsFromCommand(cmd)
+                        cfg, err := p.Load(fs)
+                        out.MaybeDie(err, "unable to load config: %v", err)
+
+                        adm, err := kafka.NewAdmin(fs, p, cfg)
+                        out.MaybeDie(err, "unable to initialize kafka client: %v", err)
+
+			var m kadm.Metadata
+                        m, err = adm.Metadata(context.Background())
+                        out.MaybeDie(err, "unable to request metadata: %v", err)
+
+                        // Check if Destination Broker list does not have Source Broker ID
+                        destinationBrokersMap := make(map[int]NodePartitionCount)
+                        for _, db := range strings.Split(destinationBrokerIds, ",") {
+                        	dbId, err := strconv.Atoi(db)
+                        	out.MaybeDie(err, "unable to parse destination broker id: %v", err)
+                        	leaderPartitions, replicaPartitions := getTopicPartitionMap(m, dbId)
+                        	destinationBrokersMap[dbId] = NodePartitionCount{len(leaderPartitions), len(replicaPartitions)}
+                        }
+
+                        tw := out.NewTable("DESTINATION", "LEADER_PARTITION_COUNT", "REPLICA_PARTITION_COUNT")
+                        defer tw.Flush()
+
+                        /*
+                        For debugging leader and replica count
+                        for dbId, db := range destinationBrokersMap {
+                        	tw.Print(dbId, db.leaderCount, db.replicaCount)	
+                        }
+                        */
+                        
+			brokerId, err := strconv.Atoi(sourceBrokerId)
+			leaderPartitions, replicaPartitions := getTopicPartitionMap(m, brokerId)
+
+                        for _, t := range leaderPartitions {				
+				// Transfer leadership to the replica on a broker, which has lower number of leaders
+				tw.Print(t.topic, t.partition, remove(t.replicas, int32(brokerId)));				
+				// Verify that no leadership is present on the current broker before proceeding
+			}
+
+			for _, t := range replicaPartitions {
+                                tw.Print(t.topic, t.partition, "NO")
+                                break
+                        }
+		},
+	}
+
+	cmd.Flags().StringVar(&sourceBrokerId, "source-broker-id", "", "Source Broker Id")
+	cmd.Flags().StringVar(&destinationBrokerIds, "destination-broker-ids", "", "Destination Broker Ids")
+
+	return cmd
+}
+
+type NodePartitionCount struct {
+	leaderCount int
+	replicaCount int
+}
+
+type TopicPartition struct {
+	topic string
+	partition int32
+	replicas []int32
+}
+
+func getTopicPartitionMap(m kadm.Metadata, brokerId int) ([]TopicPartition, []TopicPartition) {
+
+	var leaderPartitions []TopicPartition
+	var replicaPartitions []TopicPartition
+
+	for _, t := range m.Topics.Sorted() {
+	        for _, pt := range t.Partitions.Sorted() {
+                	for _, rs := range pt.Replicas {
+                        	if int(rs) == brokerId {
+                                        if int(pt.Leader) == brokerId {
+                                        	leaderPartitions = append(leaderPartitions, TopicPartition{t.Topic, pt.Partition, pt.Replicas})
+                                        } else {
+                                        	replicaPartitions = append(replicaPartitions, TopicPartition{t.Topic, pt.Partition, pt.Replicas})
+					}
+                                }
+                        }
+                }
+        }
+
+	return leaderPartitions,replicaPartitions
+}
+
+func remove(s []int32, r int32) []int32 {
+    for i, v := range s {
+        if v == r {
+            return append(s[:i], s[i+1:]...)
+        }
+    }
+    return s
 }
