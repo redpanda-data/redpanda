78726c0299ec1aaa4ff5cad4ab2f7c1802d59fe4
John Spray <jcs@redpanda.com>
2022-04-07 11:36:49 +0100

    test: add ducktape test for connection limit overrides

--- tests/rptest/tests/connection_limits_test.py
+++ tests/rptest/tests/connection_limits_test.py
@@ -7,7 +7,7 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0
 
-import re
+import socket
 
 from rptest.tests.redpanda_test import RedpandaTest
 from rptest.services.cluster import cluster
@@ -15,6 +15,7 @@ from rptest.services.rpk_producer import RpkProducer
 from rptest.services.rpk_consumer import RpkConsumer
 from rptest.clients.types import TopicSpec
 from rptest.services.metrics_check import MetricCheck
+from rptest.util import expect_exception
 
 REJECTED_METRIC = "vectorized_kafka_rpc_connections_rejected_total"
 
@@ -98,3 +99,73 @@ class ConnectionLimitsTest(RedpandaTest):
             m.evaluate([(REJECTED_METRIC, lambda a, b: b == a)])
             for m in metrics
         ])
+
+    @cluster(num_nodes=3)
+    def test_overrides(self):
+        """
+        Check that per-IP overrides are applied correctly.  More
+        detailed tests are in the unit test: this is a smoke test
+        that some overrides work end to end.
+        """
+        def setup_producers():
+            producer_a = RpkProducer(self.test_context,
+                                     self.redpanda,
+                                     self.topic,
+                                     msg_size=16384,
+                                     msg_count=1,
+                                     quiet=True,
+                                     produce_timeout=5)
+            producer_a_addr = socket.gethostbyname(producer_a.nodes[0].name)
+
+            producer_b = RpkProducer(self.test_context,
+                                     self.redpanda,
+                                     self.topic,
+                                     msg_size=16384,
+                                     msg_count=1,
+                                     quiet=True,
+                                     produce_timeout=5)
+            producer_b_addr = socket.gethostbyname(producer_b.nodes[0].name)
+
+            return producer_a, producer_a_addr, producer_b, producer_b_addr
+
+        producer_a, producer_a_addr, producer_b, producer_b_addr = setup_producers(
+        )
+
+        self.logger.info(
+            f"producer_a: {producer_a_addr}, producer_b: {producer_b_addr}")
+
+        self.redpanda.set_cluster_config(
+            {"kafka_connections_max_overrides": [
+                f"{producer_a_addr}:0",
+            ]})
+
+        self.logger.info(f"Trying producer_a, should be blocked")
+        with expect_exception(Exception,
+                              lambda e: 'timeout' in str(e).lower()):
+            producer_a.start()
+            producer_a.wait()
+
+        self.logger.info(f"Tearing down producer_a")
+        with expect_exception(Exception,
+                              lambda e: 'timeout' in str(e).lower()):
+            producer_a.stop()
+        producer_a.free()
+
+        self.logger.info(f"Trying producer_b, should be OK")
+        producer_b.start()
+        producer_b.wait()
+        producer_b.free()
+
+        self.redpanda.set_cluster_config(
+            {"kafka_connections_max_overrides": []})
+
+        producer_a, producer_a_addr, producer_b, producer_b_addr = setup_producers(
+        )
+
+        # Both producers should work now
+        producer_a.start()
+        producer_b.start()
+        producer_a.wait()
+        producer_b.wait()
+        producer_a.free()
+        producer_b.free()
