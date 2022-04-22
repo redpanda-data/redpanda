485f99a33dcceca1712c05a4eab0296e4c024e9b
John Spray <jcs@redpanda.com>
2022-04-22 17:32:11 +0100

    tests: convert EndToEndShadowIndexingTest to use SISettings

--- tests/rptest/tests/e2e_shadow_indexing_test.py
+++ tests/rptest/tests/e2e_shadow_indexing_test.py
@@ -8,26 +8,20 @@
 # by the Apache License, Version 2.0
 
 from rptest.services.cluster import cluster
+from rptest.services.redpanda import SISettings
 from rptest.clients.kafka_cli_tools import KafkaCliTools
 from rptest.clients.types import TopicSpec
 from rptest.services.redpanda import RedpandaService
 from rptest.util import Scale
-from rptest.archival.s3_client import S3Client
 from rptest.tests.end_to_end import EndToEndTest
 from rptest.util import (
     produce_until_segments,
     wait_for_segments_removal,
 )
 
-import uuid
-
 
 class EndToEndShadowIndexingTest(EndToEndTest):
     segment_size = 1048576  # 1 Mb
-    s3_host_name = "minio-s3"
-    s3_access_key = "panda-user"
-    s3_secret_key = "panda-secret"
-    s3_region = "panda-region"
     s3_topic_name = "panda-topic"
     topics = (TopicSpec(
         name=s3_topic_name,
@@ -39,43 +33,27 @@ class EndToEndShadowIndexingTest(EndToEndTest):
         super(EndToEndShadowIndexingTest,
               self).__init__(test_context=test_context)
 
-        self.s3_bucket_name = f"panda-bucket-{uuid.uuid1()}"
         self.topic = EndToEndShadowIndexingTest.s3_topic_name
-        self._extra_rp_conf = dict(
-            cloud_storage_enabled=True,
-            cloud_storage_enable_remote_read=True,
-            cloud_storage_enable_remote_write=True,
-            cloud_storage_access_key=EndToEndShadowIndexingTest.s3_access_key,
-            cloud_storage_secret_key=EndToEndShadowIndexingTest.s3_secret_key,
-            cloud_storage_region=EndToEndShadowIndexingTest.s3_region,
-            cloud_storage_bucket=self.s3_bucket_name,
-            cloud_storage_disable_tls=True,
-            cloud_storage_api_endpoint=EndToEndShadowIndexingTest.s3_host_name,
-            cloud_storage_api_endpoint_port=9000,
+
+        si_settings = SISettings(
             cloud_storage_reconciliation_interval_ms=500,
             cloud_storage_max_connections=5,
             log_segment_size=EndToEndShadowIndexingTest.segment_size,  # 1MB
         )
+        self.s3_bucket_name = si_settings.cloud_storage_bucket
+
+        si_settings.load_context(self.logger, test_context)
 
         self.scale = Scale(test_context)
         self.redpanda = RedpandaService(
             context=test_context,
             num_brokers=3,
-            extra_rp_conf=self._extra_rp_conf,
+            si_settings=si_settings,
         )
 
         self.kafka_tools = KafkaCliTools(self.redpanda)
-        self.s3_client = S3Client(
-            region=EndToEndShadowIndexingTest.s3_region,
-            access_key=EndToEndShadowIndexingTest.s3_access_key,
-            secret_key=EndToEndShadowIndexingTest.s3_secret_key,
-            endpoint=f"http://{EndToEndShadowIndexingTest.s3_host_name}:9000",
-            logger=self.logger,
-        )
 
     def setUp(self):
-        self.s3_client.empty_bucket(self.s3_bucket_name)
-        self.s3_client.create_bucket(self.s3_bucket_name)
         self.redpanda.start()
         for topic in EndToEndShadowIndexingTest.topics:
             self.kafka_tools.create_topic(topic)
