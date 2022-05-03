139a9a08e96108b9d7ec2f13640f44edef94920f
nicolaferraro <ni.ferraro@gmail.com>
2022-05-03 10:10:32 +0200

    rpk: support features endpoint in admin API client
    (cherry picked from commit 8313652c0eeec484b69592cee89ef2bdaa599b39)

---
+++ src/go/rpk/pkg/api/admin/api_features.go
@@ -0,0 +1,46 @@
+// Copyright 2021 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package admin
+
+import "net/http"
+
+// FeatureState enumerates the possible states of a feature
+type FeatureState string
+
+const (
+	FeatureStateActive      FeatureState = "active"
+	FeatureStatePreparing   FeatureState = "preparing"
+	FeatureStateAvailable   FeatureState = "available"
+	FeatureStateUnavailable FeatureState = "unavailable"
+	FeatureStateDisabled    FeatureState = "disabled"
+)
+
+// Feature contains information on the state of a feature
+type Feature struct {
+	Name      string       `json:"name"`
+	State     FeatureState `json:"state"`
+	WasActive bool         `json:"was_active"`
+}
+
+// FeaturesResponse contains information on the features available on a Redpanda cluster.
+type FeaturesResponse struct {
+	ClusterVersion int       `json:"cluster_version"`
+	Features       []Feature `json:"features"`
+}
+
+// GetFeatures returns information about the available features
+func (a *AdminAPI) GetFeatures() (FeaturesResponse, error) {
+	var features FeaturesResponse
+	return features, a.sendAny(
+		http.MethodGet,
+		"/v1/features",
+		nil,
+		&features)
+}
