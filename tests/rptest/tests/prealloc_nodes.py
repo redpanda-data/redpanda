8842a0de804ff50228d7da2e9cc5e17263cd47f7
John Spray <jcs@redpanda.com>
2022-05-03 21:35:26 +0100

    tests: make PreallocNodesTest allocate on demand
    This enables tests to do something else with their
    node allowance earlier in a test, free the nodes,
    and then use those nodes as the preallocated nodes.
    
    Also allows freeing the nodes and allocating them
    again later in a test, rather than having to hold
    them throughout the lifetime of the test.

--- tests/rptest/tests/prealloc_nodes.py
+++ tests/rptest/tests/prealloc_nodes.py
@@ -17,32 +17,57 @@ class PreallocNodesTest(RedpandaTest):
     """
     Extends RedpandaTest to preallocate ducktape
     nodes from the given test context.
+
+    Having some explicitly allocated nodes is useful for
+    re-using those nodes to run multiple services at once, or to
+    run services that need to be on the same host (e.g. the FranzGoVerifiable
+    producer/consumers that coordinate via a local file)
     """
     def __init__(self, test_context: TestContext, node_prealloc_count: int,
                  *args, **kwargs):
         super(PreallocNodesTest, self).__init__(test_context, *args, **kwargs)
         self.node_prealloc_count = node_prealloc_count
 
-        self.preallocated_nodes = test_context.cluster.alloc(
-            ClusterSpec.simple_linux(node_prealloc_count))
+        # Nodes are allocated later on first access
+        self._preallocated_nodes = []
 
-        for node in self.preallocated_nodes:
-            self.logger.debug(f'Allocated node {node.name}')
+    @property
+    def preallocated_nodes(self):
+        if not self._preallocated_nodes:
+            self._preallocated_nodes = self.test_context.cluster.alloc(
+                ClusterSpec.simple_linux(self.node_prealloc_count))
+
+            for node in self._preallocated_nodes:
+                self.logger.debug(f'Allocated node {node.name}')
+
+        return self._preallocated_nodes
 
     def free_nodes(self):
         # Free the normally allocated nodes (e.g. RedpandaService)
         super().free_nodes()
 
-        assert len(self.preallocated_nodes) == self.node_prealloc_count
+        self.free_preallocated_nodes()
 
-        # Some tests may open huge numbers of connections, which can interfere
-        # with subsequent tests' use of the node. Clear them down first.
-        # For example, those tests that use FranzGoVerifiableProducer.
-        for node in self.preallocated_nodes:
-            wait_until(lambda: self.redpanda.sockets_clear(node),
-                       timeout_sec=120,
-                       backoff_sec=10)
+    def free_preallocated_nodes(self):
+        """
+        If `preallocated_nodes` has been accessed, free the nodes that
+        were allocated there.  If `preallocated_nodes` is used again after
+        calling this, then some fresh nodes will be allocated: it is safe
+        to do this repeatedly.
+        """
+        if self._preallocated_nodes:
+            assert len(self.preallocated_nodes) == self.node_prealloc_count
 
-            # Free the hand-allocated nodes
-            self.logger.debug(f"Freeing node {node.name}")
-            self.test_context.cluster.free_single(node)
+            # Some tests may open huge numbers of connections, which can interfere
+            # with subsequent tests' use of the node. Clear them down first.
+            # For example, those tests that use FranzGoVerifiableProducer.
+            for node in self.preallocated_nodes:
+                wait_until(lambda: self.redpanda.sockets_clear(node),
+                           timeout_sec=120,
+                           backoff_sec=10)
+
+                # Free the hand-allocated nodes
+                self.logger.debug(f"Freeing node {node.name}")
+                self.test_context.cluster.free_single(node)
+
+            self._preallocated_nodes = []
