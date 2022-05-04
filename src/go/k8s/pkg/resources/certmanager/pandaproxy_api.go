313b609889638e919e0026810bde53b46d37a606
Alena Varkockova <varkockova.a@gmail.com>
2022-05-04 08:12:59 +0200

    k8s: remove unused cert names

--- src/go/k8s/pkg/resources/certmanager/pandaproxy_api.go
+++ src/go/k8s/pkg/resources/certmanager/pandaproxy_api.go
@@ -10,20 +10,8 @@
 // Package certmanager contains resources for TLS certificate handling using cert-manager
 package certmanager
 
-import "k8s.io/apimachinery/pkg/types"
-
 const (
 	pandaproxyAPI           = "proxy"
 	pandaproxyAPIClientCert = "proxy-api-client"
 	pandaproxyAPINodeCert   = "proxy-api-node"
 )
-
-// PandaproxyAPINodeCert returns the namespaced name for the Pandaproxy API certificate used by nodes
-func (r *PkiReconciler) PandaproxyAPINodeCert() types.NamespacedName {
-	return types.NamespacedName{Name: r.pandaCluster.Name + "-" + pandaproxyAPINodeCert, Namespace: r.pandaCluster.Namespace}
-}
-
-// PandaproxyAPIClientCert returns the namespaced name for the Pandaproxy API certificate used by clients
-func (r *PkiReconciler) PandaproxyAPIClientCert() types.NamespacedName {
-	return types.NamespacedName{Name: r.pandaCluster.Name + "-" + pandaproxyAPIClientCert, Namespace: r.pandaCluster.Namespace}
-}
--- src/go/k8s/pkg/resources/certmanager/schemaregistry_api.go
+++ src/go/k8s/pkg/resources/certmanager/schemaregistry_api.go
@@ -10,27 +10,8 @@
 // Package certmanager contains resources for TLS certificate handling using cert-manager
 package certmanager
 
-import "k8s.io/apimachinery/pkg/types"
-
 const (
 	schemaRegistryAPI           = "schema-registry"
 	schemaRegistryAPIClientCert = "schema-registry-client"
 	schemaRegistryAPINodeCert   = "schema-registry-node"
 )
-
-// SchemaRegistryAPINodeCert returns the namespaced name for the SchemaRegistry API certificate used by nodes
-func (r *PkiReconciler) SchemaRegistryAPINodeCert() types.NamespacedName {
-	schemaRegistryTLSListener := getTLSListener(schemaRegistryAPIListeners(r.pandaCluster))
-	if schemaRegistryTLSListener != nil && schemaRegistryTLSListener.GetTLS() != nil && schemaRegistryTLSListener.GetTLS().NodeSecretRef != nil {
-		return types.NamespacedName{
-			Name:      schemaRegistryTLSListener.GetTLS().NodeSecretRef.Name,
-			Namespace: r.pandaCluster.Namespace,
-		}
-	}
-	return types.NamespacedName{Name: r.pandaCluster.Name + "-" + schemaRegistryAPINodeCert, Namespace: r.pandaCluster.Namespace}
-}
-
-// SchemaRegistryAPIClientCert returns the namespaced name for the SchemaRegistry API certificate used by clients
-func (r *PkiReconciler) SchemaRegistryAPIClientCert() types.NamespacedName {
-	return types.NamespacedName{Name: r.pandaCluster.Name + "-" + schemaRegistryAPIClientCert, Namespace: r.pandaCluster.Namespace}
-}
