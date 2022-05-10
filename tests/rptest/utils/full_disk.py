58886816a32a2b6bf1600e208fa62e3d170553c8
Aaron Fabbri <fabbri@redpanda.com>
2022-05-10 14:29:25 -0700

    tests: Full disk write rejection integration tests
    Two new integration tests which validate that we block clients from
    producing when any node in the cluster has less than the minimum
    (configurable) amount of free disk space.
    
    - WriteRejectTest: Assert that a kafka-python client sees Broker Not
    Available when producing, after we trigger a degraded low disk space
    state. (This one would be cleaner with a raw kafka client.)
    
    - FullDiskTest run background a producer, and move in and out of degraded
    low disk space state. During each iteration, measure and validate
    expected producer rate using partition statistics. (This one is fun to
    watch execute.)
    
    - Also includes separate test utility utils/full_disk.py which has helper
    functions for setting and clearing a full disk test scenario.

---
+++ tests/rptest/utils/full_disk.py
@@ -0,0 +1,52 @@
+from logging import Logger
+import random
+
+from ducktape.utils.util import wait_until
+from rptest.services.redpanda import RedpandaService
+from rptest.services.admin import Admin
+
+
+class FullDiskHelper:
+    CONF_MIN_FREE_BYTES = "storage_min_free_bytes"
+
+    def __init__(self, logger: Logger, redpanda: RedpandaService):
+        self.logger = logger
+        self.redpanda = redpanda
+        self.admin = Admin(redpanda)
+
+    # TODO factor out similar code in cluster_config_test.py
+    def _wait_for_node_config_value(self, key: str, value: int) -> None:
+        _get = lambda k: self.admin.get_cluster_config()[k]
+
+        def match(key: str, val: int) -> bool:
+            v = _get(key)
+            self.logger.info(
+                f"config wait: want {type(value)} {value}, got {v}" +
+                f" {type(v)}")
+            return v == value
+
+        wait_until(lambda: match(key, value), timeout_sec=15)
+        self.logger.debug(f"Verified val {value} for {key}.")
+
+    def _set_low_space(self, degraded):
+        victim = random.choice(self.redpanda.nodes)
+        new_threshold: int = 0
+        if degraded:
+            new_threshold = 2**60  # arbitrary huge value
+        else:
+            new_threshold = 2**30  # smallest supported value
+        updates = {self.CONF_MIN_FREE_BYTES: new_threshold}
+        self.logger.debug(f" victim {victim.name}, " +
+                          f"new_thresh {new_threshold}")
+        self.redpanda.set_cluster_config(updates)
+
+        # self.admin.patch_cluster_config(upsert=updates, remove=[])
+        self.logger.debug(f"Confirming new config values..")
+        self._wait_for_node_config_value(self.CONF_MIN_FREE_BYTES,
+                                         new_threshold)
+
+    def trigger_low_space(self):
+        self._set_low_space(True)
+
+    def clear_low_space(self):
+        self._set_low_space(False)
