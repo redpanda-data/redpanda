e2fe728a8ea4c11e81ca1d74e324d9c3573c679e
Travis Downs <travis.downs@redpanda.com>
2022-05-11 13:44:09 -0700

    group_router: move impl to cc file
    Currently a large majority of the implementation of the
    router is in the heater. These are heavy functions and there is no
    reason for them to be in the header, move them into
    implementation for hygene and to reduce compile times.

--- src/v/kafka/server/group_router.h
+++ src/v/kafka/server/group_router.h
@@ -46,122 +46,6 @@ namespace kafka {
  * operation on the destination shard's group manager is invoked.
  */
 class group_router final {
-    template<typename Request, typename FwdFunc>
-    auto route(Request&& r, FwdFunc func) {
-        // get response type from FwdFunc it has return future<response>.
-        using return_type = std::invoke_result_t<
-          FwdFunc,
-          decltype(std::declval<group_manager>()),
-          Request&&>;
-        using resp_type = typename return_type::value_type;
-
-        auto m = shard_for(r.data.group_id);
-        if (!m || _disabled) {
-            return ss::make_ready_future<resp_type>(
-              resp_type(r, error_code::not_coordinator));
-        }
-        r.ntp = std::move(m->first);
-        return with_scheduling_group(
-          _sg, [this, func, shard = m->second, r = std::move(r)]() mutable {
-              return get_group_manager().invoke_on(
-                shard,
-                _ssg,
-                [func, r = std::move(r)](group_manager& mgr) mutable {
-                    return std::invoke(func, mgr, std::move(r));
-                });
-          });
-    }
-
-    template<typename Request, typename FwdFunc>
-    auto route_tx(Request&& r, FwdFunc func) {
-        // get response type from FwdFunc it has return future<response>.
-        using return_type = std::invoke_result_t<
-          FwdFunc,
-          decltype(std::declval<group_manager>()),
-          Request&&>;
-        using resp_type = typename return_type::value_type;
-
-        auto m = shard_for(r.group_id);
-        if (!m || _disabled) {
-            resp_type reply;
-            // route_tx routes internal intra cluster so it uses
-            // cluster::tx_errc instead of kafka::error_code
-            // because the latter is part of the kafka protocol
-            // we can't extend it
-            reply.ec = cluster::tx_errc::not_coordinator;
-            return ss::make_ready_future<resp_type>(reply);
-        }
-        r.ntp = std::move(m->first);
-        return with_scheduling_group(
-          _sg, [this, func, shard = m->second, r = std::move(r)]() mutable {
-              return get_group_manager().invoke_on(
-                shard,
-                _ssg,
-                [func, r = std::move(r)](group_manager& mgr) mutable {
-                    return std::invoke(func, mgr, std::move(r));
-                });
-          });
-    }
-    template<typename Request, typename FwdFunc>
-    auto route_stages(Request r, FwdFunc func) {
-        using return_type = std::invoke_result_t<
-          FwdFunc,
-          decltype(std::declval<group_manager>()),
-          Request&&>;
-        using resp_type = typename return_type::value_type;
-        auto m = shard_for(r.data.group_id);
-        if (!m || _disabled) {
-            return group::stages(resp_type(r, error_code::not_coordinator));
-        }
-        r.ntp = std::move(m->first);
-        auto dispatched = std::make_unique<ss::promise<>>();
-        auto dispatched_f = dispatched->get_future();
-        auto f = with_scheduling_group(
-          _sg,
-          [this,
-           shard = m->second,
-           r = std::move(r),
-           dispatched = std::move(dispatched),
-           func]() mutable {
-              return get_group_manager().invoke_on(
-                shard,
-                _ssg,
-                [r = std::move(r),
-                 dispatched = std::move(dispatched),
-                 source_shard = ss::this_shard_id(),
-                 func](group_manager& mgr) mutable {
-                    auto stages = std::invoke(func, mgr, std::move(r));
-                    /**
-                     * dispatched future is always ready before committed one,
-                     * we do not have to use gate in here
-                     */
-                    return stages.dispatched
-                      .then_wrapped([source_shard, d = std::move(dispatched)](
-                                      ss::future<> f) mutable {
-                          if (f.failed()) {
-                              (void)ss::smp::submit_to(
-                                source_shard,
-                                [d = std::move(d),
-                                 e = f.get_exception()]() mutable {
-                                    d->set_exception(e);
-                                    d.reset();
-                                });
-                              return;
-                          }
-                          (void)ss::smp::submit_to(
-                            source_shard, [d = std::move(d)]() mutable {
-                                d->set_value();
-                                d.reset();
-                            });
-                      })
-                      .then([f = std::move(stages.result)]() mutable {
-                          return std::move(f);
-                      });
-                });
-          });
-        return return_type(std::move(dispatched_f), std::move(f));
-    }
-
 public:
     group_router(
       ss::scheduling_group sched_group,
@@ -181,84 +65,38 @@ public:
       , _consumer_offsets_coordinators(consumer_offsets_coordinators)
       , _feature_table(feature_table) {}
 
-    auto join_group(join_group_request&& request) {
-        return route_stages(std::move(request), &group_manager::join_group);
-    }
+    group::join_group_stages join_group(join_group_request&& request);
 
-    auto sync_group(sync_group_request&& request) {
-        return route_stages(std::move(request), &group_manager::sync_group);
-    }
+    group::sync_group_stages sync_group(sync_group_request&& request);
 
-    auto heartbeat(heartbeat_request&& request) {
-        return route(std::move(request), &group_manager::heartbeat);
-    }
+    ss::future<heartbeat_response> heartbeat(heartbeat_request&& request);
 
-    auto leave_group(leave_group_request&& request) {
-        return route(std::move(request), &group_manager::leave_group);
-    }
+    ss::future<leave_group_response> leave_group(leave_group_request&& request);
 
-    group::offset_commit_stages offset_commit(offset_commit_request&& request) {
-        return route_stages(std::move(request), &group_manager::offset_commit);
-    }
+    ss::future<offset_fetch_response>
+    offset_fetch(offset_fetch_request&& request);
 
-    auto txn_offset_commit(txn_offset_commit_request&& request) {
-        return route(std::move(request), &group_manager::txn_offset_commit);
-    }
+    group::offset_commit_stages offset_commit(offset_commit_request&& request);
 
-    auto commit_tx(cluster::commit_group_tx_request request) {
-        return route_tx(std::move(request), &group_manager::commit_tx);
-    }
+    ss::future<txn_offset_commit_response>
+    txn_offset_commit(txn_offset_commit_request&& request);
 
-    auto begin_tx(cluster::begin_group_tx_request request) {
-        return route_tx(std::move(request), &group_manager::begin_tx);
-    }
+    ss::future<cluster::commit_group_tx_reply>
+    commit_tx(cluster::commit_group_tx_request request);
 
-    auto prepare_tx(cluster::prepare_group_tx_request request) {
-        return route_tx(std::move(request), &group_manager::prepare_tx);
-    }
+    ss::future<cluster::begin_group_tx_reply>
+    begin_tx(cluster::begin_group_tx_request request);
 
-    auto abort_tx(cluster::abort_group_tx_request request) {
-        return route_tx(std::move(request), &group_manager::abort_tx);
-    }
+    ss::future<cluster::prepare_group_tx_reply>
+    prepare_tx(cluster::prepare_group_tx_request request);
 
-    auto offset_fetch(offset_fetch_request&& request) {
-        return route(std::move(request), &group_manager::offset_fetch);
-    }
+    ss::future<cluster::abort_group_tx_reply>
+    abort_tx(cluster::abort_group_tx_request request);
 
     // return groups from across all shards, and if any core was still loading
-    ss::future<std::pair<error_code, std::vector<listed_group>>> list_groups() {
-        using type = std::pair<error_code, std::vector<listed_group>>;
-        return get_group_manager().map_reduce0(
-          [](group_manager& mgr) { return mgr.list_groups(); },
-          type{},
-          [](type a, type b) {
-              // reduce errors into `a` and retain the first
-              if (a.first == error_code::none) {
-                  a.first = b.first;
-              }
-              a.second.insert(a.second.end(), b.second.begin(), b.second.end());
-              return a;
-          });
-    }
+    ss::future<std::pair<error_code, std::vector<listed_group>>> list_groups();
 
-    ss::future<described_group> describe_group(kafka::group_id g) {
-        auto m = shard_for(g);
-        if (!m) {
-            return ss::make_ready_future<described_group>(
-              describe_groups_response::make_empty_described_group(
-                std::move(g), error_code::not_coordinator));
-        }
-        return with_scheduling_group(
-          _sg, [this, g = std::move(g), m = std::move(m)]() mutable {
-              return get_group_manager().invoke_on(
-                m->second,
-                _ssg,
-                [g = std::move(g),
-                 ntp = std::move(m->first)](group_manager& mgr) mutable {
-                    return mgr.describe_group(ntp, g);
-                });
-          });
-    }
+    ss::future<described_group> describe_group(kafka::group_id g);
 
     ss::future<std::vector<deletable_group_result>>
     delete_groups(std::vector<group_id> groups);
@@ -282,6 +120,15 @@ public:
     }
 
 private:
+    template<typename Request, typename FwdFunc>
+    auto route(Request&& r, FwdFunc func);
+
+    template<typename Request, typename FwdFunc>
+    auto route_tx(Request&& r, FwdFunc func);
+
+    template<typename Request, typename FwdFunc>
+    auto route_stages(Request r, FwdFunc func);
+
     using sharded_groups = absl::
       node_hash_map<ss::shard_id, std::vector<std::pair<model::ntp, group_id>>>;
 
