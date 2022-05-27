9b6924c8bc1044478e5b3882b533c8999c00fb7c
Rob Blafford <rob@vectorized.io>
2022-05-26 20:46:58 -0400

    kafka/s: Gen compile time list of flex versions
    - This master list of first flexible versions will be created on demand
    using information from the Request hander. If a handler supports
    T::min_flexible, its value will be used in the creation of a first_flex
    map, otherwise a dummy value (-1) will be put in its place to denote the
    request should never be parsed as flexible.

---
+++ src/v/kafka/server/flex_versions.cc
@@ -0,0 +1,79 @@
+// Copyright 2022 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#include "kafka/server/flex_versions.h"
+
+#include "kafka/server/handlers/handlers.h"
+namespace kafka {
+
+template<typename T>
+concept HasMinFlex = requires(T) {
+    {T::min_flexible};
+};
+
+/// The only request that is never flexible is sasl_handshake_request - 17
+/// Older versions of our schema may also contain values of 'none' that map
+/// to -1
+static constexpr api_version never_flexible = api_version(-1);
+
+/// Not every value from 0 -> max_api_key is a valid request, non-supported
+/// requests will map to a value of api_key(-2)
+static constexpr api_version invalid_api = api_version(-2);
+
+template<typename RequestType>
+static constexpr api_version get_min_flex() {
+    /// Necessary until all requests provide a value for `::min_flexible`
+    if constexpr (HasMinFlex<RequestType>) {
+        return RequestType::min_flexible;
+    } else {
+        return never_flexible;
+    }
+}
+
+template<typename... RequestTypes>
+static constexpr size_t max_api_key(type_list<RequestTypes...>) {
+    /// Black magic here is an overload of std::max() that takes an
+    /// std::initializer_list
+    return std::max({RequestTypes::api::key()...});
+}
+
+template<typename... RequestTypes>
+static constexpr auto
+get_flexible_request_min_versions_list(type_list<RequestTypes...> r) {
+    /// An std::array where the indicies map to api_keys and values at an index
+    /// map to the first flex version for a given api. If an api doesn't exist
+    /// at an index -2 or \ref invalid_api will be the value at the index.
+    std::array<api_version, max_api_key(r) + 1> versions;
+    for (auto& elem : versions) {
+        elem = api_version(-2);
+    }
+    ((versions[RequestTypes::api::key] = get_min_flex<RequestTypes>()), ...);
+    return versions;
+}
+
+static constexpr auto g_flex_mapping = get_flexible_request_min_versions_list(
+  request_types());
+
+bool flex_versions::is_flexible_request(api_key key, api_version version) {
+    /// If bounds checking is desired call is_api_in_schema(key) beforehand
+    const api_version first_flex_version = g_flex_mapping[key()];
+    return (version >= first_flex_version)
+           && (first_flex_version != never_flexible);
+}
+
+bool flex_versions::is_api_in_schema(api_key key) noexcept {
+    constexpr auto max_version = g_flex_mapping.max_size() - 1;
+    if (key() < 0 || static_cast<size_t>(key()) > max_version) {
+        return false;
+    }
+    const api_version first_flex_version = g_flex_mapping.at(key());
+    return first_flex_version != invalid_api;
+}
+
+} // namespace kafka
