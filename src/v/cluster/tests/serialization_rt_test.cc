7804cfebcabceccd5ffdba496dfecc7b0432db32
Noah Watkins <noah@redpanda.com>
2022-06-09 15:21:51 -0700

    cluster: add serde support to abort_tx_request
    Signed-off-by: Noah Watkins <noah@redpanda.com>

--- src/v/cluster/tests/serialization_rt_test.cc
+++ src/v/cluster/tests/serialization_rt_test.cc
@@ -988,6 +988,15 @@ SEASTAR_THREAD_TEST_CASE(serde_reflection_roundtrip) {
         cluster::commit_tx_reply data{random_tx_errc()};
         roundtrip_test(data);
     }
+    {
+        cluster::abort_tx_request data{
+          model::random_ntp(),
+          random_producer_identity(),
+          tests::random_named_int<model::tx_seq>(),
+          random_timeout_clock_duration()};
+
+        roundtrip_test(data);
+    }
 }
 
 SEASTAR_THREAD_TEST_CASE(cluster_property_kv_exchangable_with_pair) {
--- src/v/cluster/types.h
+++ src/v/cluster/types.h
@@ -397,11 +397,13 @@ struct commit_tx_reply : serde::envelope<commit_tx_reply, serde::version<0>> {
     auto serde_fields() { return std::tie(ec); }
 };
 
-struct abort_tx_request {
+struct abort_tx_request : serde::envelope<abort_tx_request, serde::version<0>> {
     model::ntp ntp;
     model::producer_identity pid;
     model::tx_seq tx_seq;
     model::timeout_clock::duration timeout;
+
+    abort_tx_request() noexcept = default;
 
     abort_tx_request(
       model::ntp ntp,
@@ -412,6 +414,8 @@ struct abort_tx_request {
       , pid(pid)
       , tx_seq(tx_seq)
       , timeout(timeout) {}
+
+    auto serde_fields() { return std::tie(ntp, pid, tx_seq, timeout); }
 };
 
 struct abort_tx_reply {
