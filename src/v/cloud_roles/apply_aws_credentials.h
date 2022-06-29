a931526aa33d9d57006e3cf1cdadc0a3d7d04ddb
Abhijat Malviya <abhijat@redpanda.com>
2022-06-29 20:04:12 +0530

    cloud_roles: adds aws credentials applier
    aws credentials applier contains a key pair and an optional token, which
    are applied to http requests.
    
    this applier contains an optional token which is present and used when
    IAM roles are active.
    
    this applier is also used for normal static credentials, in which case
    the token is absent.

---
+++ src/v/cloud_roles/apply_aws_credentials.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
+#pragma once
+
+#include "cloud_roles/apply_credentials.h"
+#include "s3/signature.h"
+
+namespace cloud_roles {
+
+class apply_aws_credentials final : public apply_credentials::impl {
+public:
+    explicit apply_aws_credentials(aws_credentials credentials);
+
+    std::error_code
+    add_auth(http::client::request_header& header) const override;
+
+    void reset_creds(credentials creds) override;
+    std::ostream& print(std::ostream& os) const override;
+
+private:
+    static std::string_view sha_for_verb(boost::beast::http::verb verb);
+
+private:
+    s3::signature_v4 _signature;
+    std::optional<s3_session_token> _session_token;
+};
+
+} // namespace cloud_roles
