5509f2dd51cf53cfc41670f492a677e455a5ec0c
John Spray <jcs@redpanda.com>
2022-07-05 12:05:14 +0100

    storage: don't save clean_segment on log close errors
    This is the case where I/O errors occur trying to
    close() out segments, and we risk a scenario
    where something goes wrong flushing the segments
    but we then succeed writing out a bogus clean_segment
    record.
    
    React to I/O errors by not marking the latest segment
    clean at all, even if it wasn't the one that had
    the error: the system is in a weird state and we
    should exercise an abundance of caution.

--- src/v/storage/disk_log_impl.cc
+++ src/v/storage/disk_log_impl.cc
@@ -128,15 +128,26 @@ ss::future<std::optional<ss::sstring>> disk_log_impl::close() {
     vlog(stlog.trace, "waiting for {} compaction to finish", config().ntp());
     co_await _compaction_gate.close();
     vlog(stlog.trace, "stopping {} readers cache", config().ntp());
-    co_await _readers_cache->stop().then([this] {
-        return ss::parallel_for_each(_segs, [](ss::lw_shared_ptr<segment>& h) {
-            return h->close().handle_exception([h](std::exception_ptr e) {
-                vlog(stlog.error, "Error closing segment:{} - {}", e, h);
-            });
-        });
+
+    // close() on the segments is not expected to fail, but it might
+    // encounter I/O errors (e.g. ENOSPC, EIO, out of file handles)
+    // when trying to flush.  If that happens, all bets are off and
+    // we will not mark our latest segment as clean (even if that
+    // particular segment didn't report an I/O error)
+    bool errors = false;
+
+    co_await _readers_cache->stop().then([this, &errors] {
+        return ss::parallel_for_each(
+          _segs, [&errors](ss::lw_shared_ptr<segment>& h) {
+              return h->close().handle_exception(
+                [&errors, h](std::exception_ptr e) {
+                    vlog(stlog.error, "Error closing segment:{} - {}", e, h);
+                    errors = true;
+                });
+          });
     });
 
-    if (_segs.size()) {
+    if (_segs.size() && !errors) {
         auto clean_seg = _segs.back()->filename();
         vlog(
           stlog.debug,
