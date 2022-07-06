redpanda-data/redpanda

commit 1163eccb2e8578d18b8a11c753979f2009858a8c

cluster: respect shard0 reservation in partition allocator limits check
This was a minor discrepancy when the shard0 reservation
was hardcoded to 2, but is much more important now that it
could be configured to larger values.

src/v/cluster/scheduling/partition_allocator.cc
@@ -195,8 +195,8 @@ std::error_code partition_allocator::check_cluster_limits(
 
     // Refuse to create a partition count that would violate the per-core
     // limit.
-    // TODO this calculation should also include partitions_reserve_shard0
-    const uint64_t core_limit = effective_cpu_count * _partitions_per_shard();
+    const uint64_t core_limit
+      = (effective_cpu_count * _partitions_per_shard() - all_brokers.size() * _partitions_reserve_shard0());
     if (proposed_total_partitions > core_limit) {
         vlog(
           clusterlog.warn,

commit 711dd7d4c87cfbdac4ed57d27e7b6250c8c44466

cluster: fix double-counting of reserved partition slots
This was a mistake while adding the configurable shard0
reservation size.  I didn't realize that our existent_partitions
counter already included those reservations.

This fixes a bug introduced in 1163eccb2e8578d18b8a11c753979f2009858a8c

src/v/cluster/scheduling/partition_allocator.cc
@@ -195,8 +195,7 @@ std::error_code partition_allocator::check_cluster_limits(
 
     // Refuse to create a partition count that would violate the per-core
     // limit.
-    const uint64_t core_limit
-      = (effective_cpu_count * _partitions_per_shard() - all_brokers.size() * _partitions_reserve_shard0());
+    const uint64_t core_limit = (effective_cpu_count * _partitions_per_shard());
     if (proposed_total_partitions > core_limit) {
         vlog(
           clusterlog.warn,