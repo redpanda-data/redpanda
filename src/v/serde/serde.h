85efae1c4a9c302bf7e1efd1780dbbe5c35feceb
Bharath Vissapragada <bharathv@redpanda.com>
2022-07-12 09:40:56 -0700

    serde: static assert when serializing time_points
    A compile time check to discourage users from serializing time_points.

--- src/v/serde/serde.h
+++ src/v/serde/serde.h
@@ -143,6 +143,10 @@ template<class T>
 concept is_chrono_duration
   = ::detail::is_specialization_of_v<T, std::chrono::duration>;
 
+template<class T>
+concept is_chrono_time_point
+  = ::detail::is_specialization_of_v<T, std::chrono::time_point>;
+
 template<typename T>
 inline constexpr auto const is_serde_compatible_v
   = is_envelope<T>
@@ -227,6 +231,10 @@ int64_t checked_duration_cast_to_nanoseconds(
 template<typename T>
 void write(iobuf& out, T t) {
     using Type = std::decay_t<T>;
+    static_assert(
+      !is_chrono_time_point<Type>,
+      "Time point serialization is risky and can have unintended "
+      "consequences. Check with Redpanda team before fixing this.");
     static_assert(are_bytes_and_string_different<Type>);
     static_assert(has_serde_write<Type> || is_serde_compatible_v<Type>);
 
@@ -502,6 +510,10 @@ header read_header(iobuf_parser& in, std::size_t const bytes_left_limit) {
 template<typename T>
 void read_nested(iobuf_parser& in, T& t, std::size_t const bytes_left_limit) {
     using Type = std::decay_t<T>;
+    static_assert(
+      !is_chrono_time_point<Type>,
+      "Time point serialization is risky and can have unintended "
+      "consequences. Check with Redpanda team before fixing this.");
     static_assert(are_bytes_and_string_different<Type>);
     static_assert(has_serde_read<T> || is_serde_compatible_v<Type>);
 
