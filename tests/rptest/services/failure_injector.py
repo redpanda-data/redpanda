599afce352bcad274870ff0d299fd15f2c141dcc
Michal Maslanka <michal@redpanda.com>
2022-07-26 17:29:30 +0200

    tests: add netem failures to failure injector
    Signed-off-by: Michal Maslanka <michal@redpanda.com>

--- tests/rptest/services/failure_injector.py
+++ tests/rptest/services/failure_injector.py
@@ -7,9 +7,12 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0
 
+import random
 import signal
 import threading
 from ducktape.utils.util import wait_until
+
+from rptest.services import tc_netem
 
 
 class FailureSpec:
@@ -18,8 +21,20 @@ class FailureSpec:
     FAILURE_TERMINATE = 1
     FAILURE_SUSPEND = 2
     FAILURE_ISOLATE = 3
+    # network failures
+    FAILURE_NETEM_RANDOM_DELAY = 4
+    FAILURE_NETEM_PACKET_LOSS = 5
+    FAILURE_NETEM_PACKET_CORRUPT = 6
+    FAILURE_NETEM_PACKET_DUPLICATE = 7
 
-    FAILURE_TYPES = [FAILURE_KILL, FAILURE_SUSPEND, FAILURE_TERMINATE]
+    FAILURE_TYPES = [
+        FAILURE_KILL, FAILURE_SUSPEND, FAILURE_TERMINATE,
+        FAILURE_NETEM_RANDOM_DELAY
+    ]
+    NETEM_FAILURE_TYPES = [
+        FAILURE_NETEM_RANDOM_DELAY, FAILURE_NETEM_PACKET_LOSS,
+        FAILURE_NETEM_PACKET_CORRUPT, FAILURE_NETEM_PACKET_DUPLICATE
+    ]
 
     def __init__(self, type, node, length=None):
         self.type = type
@@ -73,6 +88,14 @@ class FailureInjector:
             return self._terminate
         elif tp == FailureSpec.FAILURE_ISOLATE:
             return self._isolate
+        elif tp == FailureSpec.FAILURE_NETEM_RANDOM_DELAY:
+            return self._netem_delay
+        elif tp == FailureSpec.FAILURE_NETEM_PACKET_LOSS:
+            return self._netem_loss
+        elif tp == FailureSpec.FAILURE_NETEM_PACKET_CORRUPT:
+            return self._netem_corrupt
+        elif tp == FailureSpec.FAILURE_NETEM_PACKET_DUPLICATE:
+            return self._netem_duplicate
 
     def _stop_func(self, tp):
         if tp == FailureSpec.FAILURE_KILL or tp == FailureSpec.FAILURE_TERMINATE:
@@ -81,6 +104,8 @@ class FailureInjector:
             return self._continue
         elif tp == FailureSpec.FAILURE_ISOLATE:
             return self._heal
+        else:
+            return self._delete_netem
 
     def _kill(self, node):
         self.redpanda.logger.info(
@@ -109,6 +134,9 @@ class FailureInjector:
         cmd = "iptables -D INPUT -p tcp --destination-port 33145 -j DROP"
         node.account.ssh(cmd)
 
+    def _delete_netem(self, node):
+        tc_netem.tc_netem_delete(node)
+
     def _heal_all(self):
         self.redpanda.logger.info(f"healling all network failures")
         for n in self.redpanda.nodes:
@@ -117,6 +145,11 @@ class FailureInjector:
             n.account.ssh("iptables -P OUTPUT ACCEPT")
             n.account.ssh("iptables -F")
             n.account.ssh("iptables -X")
+            try:
+                self._delete_netem(n)
+            except:
+                # skip error as deleting netem may fail if there are no rules applied
+                pass
 
     def _suspend(self, node):
         self.redpanda.logger.info(
@@ -144,3 +177,29 @@ class FailureInjector:
             self.redpanda.logger.info(
                 f"starting redpanda on {node.account.hostname}")
             self.redpanda.start_redpanda(node)
+
+    def _netem(self, node, op):
+        self.redpanda.logger.info(
+            f"executing: '{op.get_command()}' on '{node.account.hostname}'")
+        tc_netem.tc_netem_add(node=node, option=op)
+
+    def _netem_delay(self, node):
+        op = tc_netem.NetemDelay(
+            random.randint(1000, 5000),
+            random.randint(100, 1000),
+            correlation_us=None,
+            distribution=tc_netem.NetemDelayDistribution.PARETO_NORMAL)
+
+        self._netem(node, op=op)
+
+    def _netem_loss(self, node):
+        op = tc_netem.NetemLoss(random.randint(1, 60))
+        self._netem(node, op)
+
+    def _netem_corrupt(self, node):
+        op = tc_netem.NetemCorrupt(random.randint(1, 60), correlation=None)
+        self._netem(node, op=op)
+
+    def _netem_duplicate(self, node):
+        op = tc_netem.NetemDuplicate(random.randint(1, 60), correlation=None)
+        self._netem(node, op=op)
