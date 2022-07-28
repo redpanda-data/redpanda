65820ac14d518971ef33c5301d559a8f7fd41689
Travis Downs <travis.downs@redpanda.com>
2022-07-27 19:38:38 -0700

    Remove some restrictions on frag_vector elem type
    fragmented_vector is a vector-like class which limits the maximum
    contiguous allocation required by breaking the elements up into chunks
    of a specific maximum size call the "fragment size".
    
    Currently, the vetor places one restrictions on the fragment size:
     - It must be a power of two
     and one on the element size:
     - It must evenly divide the fragment size.
    
    It seems to be that neither of these is necessary, and they severely
    limit the element types that may be used: effectively, only elements
    whose size is a power of 2 can meet these requirements.
    
    The first restriction may be in place to ensure the fragment size lines
    exactly with seastar span sizes, and the second may be so that there is
    no internal waste within a fragment, but with many object sizes this
    waste is very small.
    
    We simply remove these restictions for now.

--- src/v/utils/fragmented_vector.h
+++ src/v/utils/fragmented_vector.h
@@ -36,10 +36,6 @@
  */
 template<typename T, size_t fragment_size = 8192>
 class fragmented_vector {
-    static_assert(
-      (fragment_size & (fragment_size - 1)) == 0,
-      "fragment size must be a power of 2");
-    static_assert(fragment_size % sizeof(T) == 0);
     static constexpr size_t elems_per_frag = fragment_size / sizeof(T);
     static_assert(elems_per_frag >= 1);
 
