e39a8da914c4d32577d14cb04d2b9c52f6d23655
pvsune <pvsune@hotmail.com>
2022-07-30 02:16:35 +0800

    operator: Ensure Service resource.

--- src/go/k8s/controllers/redpanda/console_controller.go
+++ src/go/k8s/controllers/redpanda/console_controller.go
@@ -63,6 +63,7 @@ func (r *ConsoleReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	applyResources := []resources.Resource{
 		consolepkg.NewConfigMap(r.Client, r.Scheme, console, cluster, r.clusterDomain, r.AdminAPIClientFactory, log),
 		consolepkg.NewDeployment(r.Client, r.Scheme, console, cluster, log),
+		consolepkg.NewService(r.Client, r.Scheme, console, log),
 	}
 
 	for _, each := range applyResources {
