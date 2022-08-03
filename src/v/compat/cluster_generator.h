f9a8921700184a4ed2f555f3ef20606e9291f120
Rob Blafford <rob@vectorized.io>
2022-08-03 14:49:16 -0400

    compat: Gen random cluster::errc's

---
+++ src/v/compat/cluster_generator.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2022 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#pragma once
+
+#include "cluster/errc.h"
+#include "cluster/types.h"
+#include "compat/generator.h"
+#include "test_utils/randoms.h"
+
+namespace compat {
+
+template<>
+struct instance_generator<cluster::errc> {
+    static cluster::errc random() {
+        return random_generators::random_choice(
+          {cluster::errc::success,
+           cluster::errc::notification_wait_timeout,
+           cluster::errc::topic_invalid_partitions,
+           cluster::errc::topic_invalid_replication_factor,
+           cluster::errc::topic_invalid_config,
+           cluster::errc::not_leader_controller,
+           cluster::errc::topic_already_exists,
+           cluster::errc::replication_error,
+           cluster::errc::shutting_down,
+           cluster::errc::no_leader_controller,
+           cluster::errc::join_request_dispatch_error,
+           cluster::errc::seed_servers_exhausted,
+           cluster::errc::auto_create_topics_exception,
+           cluster::errc::timeout,
+           cluster::errc::topic_not_exists,
+           cluster::errc::invalid_topic_name,
+           cluster::errc::partition_not_exists,
+           cluster::errc::not_leader,
+           cluster::errc::partition_already_exists,
+           cluster::errc::waiting_for_recovery,
+           cluster::errc::update_in_progress,
+           cluster::errc::user_exists,
+           cluster::errc::user_does_not_exist,
+           cluster::errc::invalid_producer_epoch,
+           cluster::errc::sequence_out_of_order,
+           cluster::errc::generic_tx_error,
+           cluster::errc::node_does_not_exists,
+           cluster::errc::invalid_node_operation,
+           cluster::errc::invalid_configuration_update,
+           cluster::errc::topic_operation_error,
+           cluster::errc::no_eligible_allocation_nodes,
+           cluster::errc::allocation_error,
+           cluster::errc::partition_configuration_revision_not_updated,
+           cluster::errc::partition_configuration_in_joint_mode,
+           cluster::errc::partition_configuration_leader_config_not_committed,
+           cluster::errc::partition_configuration_differs,
+           cluster::errc::data_policy_already_exists,
+           cluster::errc::data_policy_not_exists,
+           cluster::errc::source_topic_not_exists,
+           cluster::errc::source_topic_still_in_use,
+           cluster::errc::waiting_for_partition_shutdown,
+           cluster::errc::error_collecting_health_report,
+           cluster::errc::leadership_changed,
+           cluster::errc::feature_disabled,
+           cluster::errc::invalid_request,
+           cluster::errc::no_update_in_progress,
+           cluster::errc::unknown_update_interruption_error});
+    }
+
+    static std::vector<cluster::errc> limits() { return {}; }
+};
+
+} // namespace compat
