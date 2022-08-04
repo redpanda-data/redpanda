ce557a1bc95076422e031d3751bce46dc1ab9595
Noah Watkins <noahwatkins@gmail.com>
2022-08-03 19:49:31 -0700

    compat: move compat generator macro
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/compat/cluster_compat.h
+++ src/v/compat/cluster_compat.h
@@ -16,34 +16,6 @@
 #include "compat/cluster_json.h"
 #include "compat/json.h"
 
-#define GEN_COMPAT_CHECK(Type, ToJson, FromJson)                               \
-    template<>                                                                 \
-    struct compat_check<Type> {                                                \
-        static constexpr std::string_view name = #Type;                        \
-                                                                               \
-        static std::vector<Type> create_test_cases() {                         \
-            return generate_instances<Type>();                                 \
-        }                                                                      \
-                                                                               \
-        static void to_json(Type obj, json::Writer<json::StringBuffer>& wr) {  \
-            ToJson;                                                            \
-        }                                                                      \
-                                                                               \
-        static Type from_json(json::Value& rd) {                               \
-            Type obj;                                                          \
-            FromJson;                                                          \
-            return obj;                                                        \
-        }                                                                      \
-                                                                               \
-        static std::vector<compat_binary> to_binary(Type obj) {                \
-            return compat_binary::serde_and_adl(obj);                          \
-        }                                                                      \
-                                                                               \
-        static bool check(Type obj, compat_binary test) {                      \
-            return verify_adl_or_serde(obj, std::move(test));                  \
-        }                                                                      \
-    };
-
 namespace compat {
 
 GEN_COMPAT_CHECK(
