37bcba2e693a046ad9777c31d278265e04af40d6
Abhijat Malviya <abhijat@redpanda.com>
2022-08-08 17:35:43 +0530

    cloud_storage: avoid possible shadowing of cache size by local variable

--- src/v/cloud_storage/cache_service.cc
+++ src/v/cloud_storage/cache_service.cc
@@ -158,8 +158,9 @@ ss::future<> cache::clean_up_at_start() {
 ss::future<> cache::clean_up_cache() {
     vassert(ss::this_shard_id() == 0, "Method can only be invoked on shard 0");
     gate_guard guard{_gate};
-    auto [_current_cache_size, candidates_for_deletion] = co_await _walker.walk(
+    auto [current_cache_size, candidates_for_deletion] = co_await _walker.walk(
       _cache_dir.native(), _access_time_tracker);
+    _current_cache_size = current_cache_size;
     probe.set_size(_current_cache_size);
     probe.set_num_files(candidates_for_deletion.size());
 
