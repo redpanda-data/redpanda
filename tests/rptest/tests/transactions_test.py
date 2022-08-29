717824e01b6cc4e11396a4c7ead7f6695343c958
Vadim Plakhtinskiy <vadim@redpanda.com>
2022-08-29 16:16:16 +0200

    ducktape: Test serialization during upgrade

--- tests/rptest/tests/transactions_test.py
+++ tests/rptest/tests/transactions_test.py
@@ -14,6 +14,9 @@ from rptest.clients.types import TopicSpec
 import time
 
 from rptest.tests.redpanda_test import RedpandaTest
+from rptest.services.redpanda import RedpandaService
+from rptest.clients.default import DefaultClient
+from rptest.services.redpanda import RESTART_LOG_ALLOW_LIST
 from rptest.services.rpk_consumer import RpkConsumer
 import confluent_kafka as ck
 from rptest.clients.kafka_cat import KafkaCat
@@ -249,3 +252,70 @@ class TransactionsTest(RedpandaTest):
             assert kafka_error.code() == ck.cimpl.KafkaError.FENCED_INSTANCE_ID
 
         producer.abort_transaction()
+
+
+class UpgradeTransactionTest(RedpandaTest):
+    @cluster(num_nodes=2, log_allow_list=RESTART_LOG_ALLOW_LIST)
+    def check_parsing_test(self):
+        self.redpanda = RedpandaService(
+            self.test_context,
+            1,
+            extra_rp_conf={
+                "enable_idempotence": True,
+                "enable_transactions": True,
+                "transaction_coordinator_replication": 1,
+                "id_allocator_replication": 1,
+                "enable_leader_balancer": False,
+                "enable_auto_rebalance_on_node_add": False,
+            },
+            environment={"__REDPANDA_LOGICAL_VERSION": 5})
+
+        self.redpanda.start()
+
+        spec = TopicSpec(partition_count=1, replication_factor=1)
+        self._client = DefaultClient(self.redpanda)
+        self.client().create_topic(spec)
+        topic_name = spec.name
+
+        producer = ck.Producer({
+            'bootstrap.servers': self.redpanda.brokers(),
+            'transactional.id': '123',
+            'transaction.timeout.ms': 10000,
+        })
+
+        producer.init_transactions()
+
+        def on_del(err, msg):
+            assert err == None
+
+        max_tx = 100
+        for i in range(max_tx):
+            producer.begin_transaction()
+            producer.produce(topic_name, str(i), str(i), 0, on_del)
+            producer.commit_transaction()
+
+        self.redpanda.set_environment({"__REDPANDA_LOGICAL_VERSION": 6})
+        for n in self.redpanda.nodes:
+            self.redpanda.restart_nodes(n, stop_timeout=60)
+
+        consumer = ck.Consumer({
+            'bootstrap.servers': self.redpanda.brokers(),
+            'group.id': "test",
+            'auto.offset.reset': 'earliest',
+        })
+
+        consumer.subscribe([topic_name])
+
+        num_consumed = 0
+        prev_rec = bytes("0", 'UTF-8')
+
+        while num_consumed != max_tx:
+            max_records = 10
+            timeout = 10
+            records = consumer.consume(max_records, timeout)
+
+            for record in records:
+                assert prev_rec == record.key()
+                prev_rec = bytes(str(int(prev_rec) + 1), 'UTF-8')
+
+            num_consumed += len(records)
