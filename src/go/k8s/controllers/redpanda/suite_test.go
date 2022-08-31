6c524e61c152e49b7363f5277d51b65b4ba75edf
pvsune <pvsune@hotmail.com>
2022-08-31 09:03:40 +0800

    operator: Sync Kafka client Secret.
    - Support syncing Kafka client Secret if Console is installed in different namespace
    - Refactor copying the synced Secret to its own function in Store

--- src/go/k8s/controllers/redpanda/suite_test.go
+++ src/go/k8s/controllers/redpanda/suite_test.go
@@ -109,7 +109,7 @@ var _ = BeforeSuite(func(done Done) {
 		}
 		return testAdminAPI, nil
 	}
-	testStore = consolepkg.NewStore(k8sManager.GetClient())
+	testStore = consolepkg.NewStore(k8sManager.GetClient(), k8sManager.GetScheme())
 	testKafkaAdmin = &mockKafkaAdmin{}
 	testKafkaAdminFactory = func(context.Context, client.Client, *redpandav1alpha1.Cluster) (consolepkg.KafkaAdminClient, error) {
 		return testKafkaAdmin, nil
