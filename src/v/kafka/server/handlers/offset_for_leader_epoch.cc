63f410ca0f719cb2dd504027f2b3cce77d30d02f
Michal Maslanka <michal@redpanda.com>
2022-09-14 14:26:36 +0200

    k/offset_for_leader_epoch: fix incorrectly returned error code
    When shard is not found for a partition but it is expected to exists
    based on the information from metadata cache we must return
    `not_leader_for_partition` error as we want client to retry
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>
    (cherry picked from commit cdf9f31bcc6d0cbef77733b929c67b94f89d59c2)

--- src/v/kafka/server/handlers/offset_for_leader_epoch.cc
+++ src/v/kafka/server/handlers/offset_for_leader_epoch.cc
@@ -154,7 +154,7 @@ get_offsets_for_leader_epochs(
             if (!shard) {
                 partition_response = response_t::make_epoch_end_offset(
                   request_partition.partition,
-                  error_code::unknown_topic_or_partition);
+                  error_code::not_leader_for_partition);
                 continue;
             }
 
