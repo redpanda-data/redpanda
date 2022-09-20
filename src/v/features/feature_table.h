f3ce4bbe7f2a144d6c796845a444247b3152d8b3
Ben Pope <ben@redpanda.com>
2022-09-20 15:17:31 +0100

    features: Introduce ephemeral_secrets feature
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/features/feature_table.h
+++ src/v/features/feature_table.h
@@ -45,6 +45,7 @@ enum class feature : std::uint64_t {
     cloud_retention = 0x800,
     node_id_assignment = 0x1000,
     replication_factor_change = 0x2000,
+    ephemeral_secrets = 0x4000,
 
     // Dummy features for testing only
     test_alpha = uint64_t(1) << 63,
@@ -179,6 +180,12 @@ constexpr static std::array feature_schema{
     cluster_version{7},
     "replication_factor_change",
     feature::replication_factor_change,
+    feature_spec::available_policy::always,
+    feature_spec::prepare_policy::always},
+  feature_spec{
+    cluster_version{7},
+    "ephemeral_secrets",
+    feature::ephemeral_secrets,
     feature_spec::available_policy::always,
     feature_spec::prepare_policy::always},
   feature_spec{
