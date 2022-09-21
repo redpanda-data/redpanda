c744241b6895db3d0d10de0c002f68f3d6e43036
Rob Blafford <rob@vectorized.io>
2022-09-21 11:18:54 -0400

    kafka/s: Make create_topics::handler a coroutine
    - Pure refactoring commit, subsequent commit will make edits to this
    file that would create a large diff when considering formatting

--- src/v/kafka/server/handlers/create_topics.cc
+++ src/v/kafka/server/handlers/create_topics.cc
@@ -115,132 +115,125 @@ ss::future<response_ptr> create_topics_handler::handle(
     request.decode(ctx.reader(), ctx.header().version);
     vlog(
       klog.debug, "Handling {} request: {}", create_topics_api::name, request);
-    return ss::do_with(
-      std::move(ctx),
-      [request = std::move(request)](request_context& ctx) mutable {
-          create_topics_response response;
-          auto begin = request.data.topics.begin();
-          // Duplicated topic names are not accepted
-          auto valid_range_end = validate_range_duplicates(
-            begin,
-            request.data.topics.end(),
-            std::back_inserter(response.data.topics));
 
-          const auto has_cluster_auth = ctx.authorized(
-            security::acl_operation::create, security::default_cluster_name);
+    create_topics_response response;
+    auto begin = request.data.topics.begin();
 
-          if (!has_cluster_auth) {
-              auto unauthorized_it = std::partition(
-                begin, valid_range_end, [&ctx](const creatable_topic& t) {
-                    return ctx.authorized(
-                      security::acl_operation::create, t.name);
-                });
-              std::transform(
-                unauthorized_it,
-                valid_range_end,
-                std::back_inserter(response.data.topics),
-                [](const creatable_topic& t) {
-                    return generate_error(
-                      t,
-                      error_code::topic_authorization_failed,
-                      "Unauthorized");
-                });
-              valid_range_end = unauthorized_it;
-          }
+    // Duplicated topic names are not accepted
+    auto valid_range_end = validate_range_duplicates(
+      begin,
+      request.data.topics.end(),
+      std::back_inserter(response.data.topics));
 
-          // fill in defaults if necessary
-          for (auto& r : boost::make_iterator_range(begin, valid_range_end)) {
-              // skip custom assigned topics
-              if (!r.assignments.empty()) {
-                  continue;
+    const auto has_cluster_auth = ctx.authorized(
+      security::acl_operation::create, security::default_cluster_name);
+
+    if (!has_cluster_auth) {
+        auto unauthorized_it = std::partition(
+          begin, valid_range_end, [&ctx](const creatable_topic& t) {
+              return ctx.authorized(security::acl_operation::create, t.name);
+          });
+        std::transform(
+          unauthorized_it,
+          valid_range_end,
+          std::back_inserter(response.data.topics),
+          [](const creatable_topic& t) {
+              return generate_error(
+                t, error_code::topic_authorization_failed, "Unauthorized");
+          });
+        valid_range_end = unauthorized_it;
+    }
+
+    // fill in defaults if necessary
+    for (auto& r : boost::make_iterator_range(begin, valid_range_end)) {
+        // skip custom assigned topics
+        if (!r.assignments.empty()) {
+            continue;
+        }
+        if (r.num_partitions == -1) {
+            r.num_partitions
+              = config::shard_local_cfg().default_topic_partitions();
+        }
+        if (r.replication_factor == -1) {
+            r.replication_factor
+              = config::shard_local_cfg().default_topic_replication();
+        }
+    }
+
+    // Validate with validators
+    valid_range_end = validate_requests_range(
+      begin,
+      valid_range_end,
+      std::back_inserter(response.data.topics),
+      validators{});
+
+    // Print log if not supported configuration options are present
+    for (auto& r : boost::make_iterator_range(begin, valid_range_end)) {
+        for (auto c : r.configs) {
+            if (!is_supported(c.name)) {
+                vlog(
+                  klog.info,
+                  "topic {} not supported configuration {}={} property "
+                  "will be ignored",
+                  r.name,
+                  c.name,
+                  c.value);
+            }
+        }
+    }
+
+    if (request.data.validate_only) {
+        // We do not actually create the topics, only validate the
+        // request
+        // Generate successes for topics that passed the
+        // validation.
+        std::transform(
+          begin,
+          valid_range_end,
+          std::back_inserter(response.data.topics),
+          [&ctx](const creatable_topic& t) {
+              auto result = generate_successfull_result(t);
+              if (ctx.header().version >= api_version(5)) {
+                  auto default_properties
+                    = ctx.metadata_cache().get_default_properties();
+                  result.configs = {properties_to_result_configs(
+                    from_cluster_type(default_properties))};
               }
-              if (r.num_partitions == -1) {
-                  r.num_partitions
-                    = config::shard_local_cfg().default_topic_partitions();
-              }
-              if (r.replication_factor == -1) {
-                  r.replication_factor
-                    = config::shard_local_cfg().default_topic_replication();
-              }
+              return result;
+          });
+        co_return co_await ctx.respond(std::move(response));
+    }
+    auto to_create = to_cluster_type(begin, valid_range_end);
+    /**
+     * We always override cleanup policy. i.e. topic cleanup policy will
+     * stay the same even if it was changed in defaults (broker
+     * configuration) and there was no override passed by client while
+     * creating a topic. The the same policy is applied in Kafka.
+     */
+    for (auto& tp : to_create) {
+        if (!tp.cfg.properties.cleanup_policy_bitflags.has_value()) {
+            tp.cfg.properties.cleanup_policy_bitflags
+              = ctx.metadata_cache().get_default_cleanup_policy_bitflags();
+        }
+    }
+    // Create the topics with controller on core 0
+    co_return co_await ctx.topics_frontend()
+      .create_topics(std::move(to_create), to_timeout(request.data.timeout_ms))
+      .then([&ctx, tout = to_timeout(request.data.timeout_ms)](
+              std::vector<cluster::topic_result> c_res) mutable {
+          return wait_for_topics(c_res, ctx.controller_api(), tout)
+            .then([c_res = std::move(c_res)]() mutable { return c_res; });
+      })
+      .then([&ctx,
+             response = std::move(response),
+             tout = to_timeout(request.data.timeout_ms)](
+              std::vector<cluster::topic_result> c_res) mutable {
+          // Append controller results to validation errors
+          append_cluster_results(c_res, response.data.topics);
+          if (ctx.header().version >= api_version(5)) {
+              append_topic_configs(ctx, response);
           }
-
-          // Validate with validators
-          valid_range_end = validate_requests_range(
-            begin,
-            valid_range_end,
-            std::back_inserter(response.data.topics),
-            validators{});
-
-          // Print log if not supported configuration options are present
-          for (auto& r : boost::make_iterator_range(begin, valid_range_end)) {
-              for (auto c : r.configs) {
-                  if (!is_supported(c.name)) {
-                      vlog(
-                        klog.info,
-                        "topic {} not supported configuration {}={} property "
-                        "will be ignored",
-                        r.name,
-                        c.name,
-                        c.value);
-                  }
-              }
-          }
-
-          if (request.data.validate_only) {
-              // We do not actually create the topics, only validate the
-              // request
-              // Generate successes for topics that passed the
-              // validation.
-              std::transform(
-                begin,
-                valid_range_end,
-                std::back_inserter(response.data.topics),
-                [&ctx](const creatable_topic& t) {
-                    auto result = generate_successfull_result(t);
-                    if (ctx.header().version >= api_version(5)) {
-                        auto default_properties
-                          = ctx.metadata_cache().get_default_properties();
-                        result.configs = {properties_to_result_configs(
-                          from_cluster_type(default_properties))};
-                    }
-                    return result;
-                });
-              return ctx.respond(std::move(response));
-          }
-          auto to_create = to_cluster_type(begin, valid_range_end);
-          /**
-           * We always override cleanup policy. i.e. topic cleanup policy will
-           * stay the same even if it was changed in defaults (broker
-           * configuration) and there was no override passed by client while
-           * creating a topic. The the same policy is applied in Kafka.
-           */
-          for (auto& tp : to_create) {
-              if (!tp.cfg.properties.cleanup_policy_bitflags.has_value()) {
-                  tp.cfg.properties.cleanup_policy_bitflags
-                    = ctx.metadata_cache()
-                        .get_default_cleanup_policy_bitflags();
-              }
-          }
-          // Create the topics with controller on core 0
-          return ctx.topics_frontend()
-            .create_topics(
-              std::move(to_create), to_timeout(request.data.timeout_ms))
-            .then([&ctx, tout = to_timeout(request.data.timeout_ms)](
-                    std::vector<cluster::topic_result> c_res) mutable {
-                return wait_for_topics(c_res, ctx.controller_api(), tout)
-                  .then([c_res = std::move(c_res)]() mutable { return c_res; });
-            })
-            .then([&ctx,
-                   response = std::move(response),
-                   tout = to_timeout(request.data.timeout_ms)](
-                    std::vector<cluster::topic_result> c_res) mutable {
-                // Append controller results to validation errors
-                append_cluster_results(c_res, response.data.topics);
-                if (ctx.header().version >= api_version(5)) {
-                    append_topic_configs(ctx, response);
-                }
-                return ctx.respond(response);
-            });
+          return ctx.respond(response);
       });
 }
 
