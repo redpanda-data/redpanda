db620664bb517fe80467c03e9318c6ad798ae899
Andrew Wong <awong@redpanda.com>
2022-09-27 19:11:42 -0700

    cluster: allow for serde-only controller commands
    Currently controller commands must support both ADL and serde
    serialization by virtue of the serialization being determined at runtime
    based on a feature flag. Moving forward, some types will never be ADL
    serialized, by virtue of having certain commands only being sent once
    all nodes are upgraded.
    
    This commit addresses this by adding a new type argument `serde_opts`
    that, when specified as `serde_only`, will statically avoid hitting ADL
    code serialization paths.
    
    I considered inferring serde-only status by checking whether the command
    types were ADL exempt. I opted instead to go the route of having users
    manually set this, as it seems more fail-safe, easier to reason about,
    and sets us up to extend this down the line with other serialization
    modes if need be.

--- src/v/cluster/cluster_utils.h
+++ src/v/cluster/cluster_utils.h
@@ -222,16 +222,21 @@ ss::future<std::error_code> replicate_and_wait(
        &as = as,
        timeout,
        use_serde_serialization](controller_stm& stm) mutable {
-          if (likely(use_serde_serialization)) {
-              auto b = serde_serialize_cmd(std::forward<Cmd>(cmd));
-              return stm.replicate_and_wait(
-                std::move(b), timeout, as.local(), term);
+          if constexpr (Cmd::serde_opts == serde_opts::adl_and_serde) {
+              if (unlikely(!use_serde_serialization)) {
+                  return serialize_cmd(std::forward<Cmd>(cmd))
+                    .then([&stm, timeout, term, &as](model::record_batch b) {
+                        return stm.replicate_and_wait(
+                          std::move(b), timeout, as.local(), term);
+                    });
+              }
           }
-          return serialize_cmd(std::forward<Cmd>(cmd))
-            .then([&stm, timeout, term, &as](model::record_batch b) {
-                return stm.replicate_and_wait(
-                  std::move(b), timeout, as.local(), term);
-            });
+          vassert(
+            use_serde_serialization,
+            "Requested to ADL serialize a serde-only type");
+          auto b = serde_serialize_cmd(std::forward<Cmd>(cmd));
+          return stm.replicate_and_wait(
+            std::move(b), timeout, as.local(), term);
       });
 }
 
