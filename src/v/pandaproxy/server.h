a86c7eb0483eb9eaf254d5c6fcb238150e32656d
Ben Pope <ben@redpanda.com>
2022-09-29 21:43:09 +0100

    pp: Add dispatch helpers to auth_ctx_server::request_t
    These are used to simplify teh handlers, and properly dispatch
    requests (and request factories) to the correct client.
    
    In particular, the situation without http basic auth can now
    dispatch consumer requests based on group_id, to restore prior
    behaviour of spreading out consumer across shards.
    
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/pandaproxy/server.h
+++ src/v/pandaproxy/server.h
@@ -15,6 +15,8 @@
 #include "config/rest_authn_endpoint.h"
 #include "kafka/client/client.h"
 #include "pandaproxy/json/types.h"
+#include "pandaproxy/kafka_client_cache.h"
+#include "pandaproxy/sharded_client_cache.h"
 #include "pandaproxy/types.h"
 #include "redpanda/request_auth.h"
 #include "seastarx.h"
@@ -22,6 +24,7 @@
 #include <seastar/core/abort_source.hh>
 #include <seastar/core/future.hh>
 #include <seastar/core/gate.hh>
+#include <seastar/core/shared_ptr.hh>
 #include <seastar/http/api_docs.hh>
 #include <seastar/http/handlers.hh>
 #include <seastar/http/httpd.hh>
@@ -32,8 +35,24 @@
 #include <seastar/util/noncopyable_function.hh>
 
 #include <memory>
+#include <type_traits>
 
 namespace pandaproxy {
+
+namespace impl {
+
+template<typename T>
+concept KafkaRequestType = std::same_as<T, typename T::api_type::request_type>;
+
+template<typename F>
+concept KafkaRequestFactory = KafkaRequestType<std::invoke_result_t<F>>;
+
+ss::shard_id inline consumer_shard(const kafka::group_id& g_id) {
+    auto hash = xxhash_64(g_id().data(), g_id().length());
+    return jump_consistent_hash(hash, ss::smp::count);
+}
+
+} // namespace impl
 
 /// \brief wrapper around ss::httpd.
 ///
@@ -160,6 +179,54 @@ public:
         };
         // The service
         service_t& service() const { return context().service; };
+
+        template<std::invocable<kafka_client_cache&> Func>
+        auto dispatch(Func&& func) {
+            return service().client_cache().invoke_on_cache(
+              user, std::forward<Func>(func));
+        }
+
+        template<std::invocable<kafka::client::client&> Func>
+        auto dispatch(Func&& func) {
+            switch (authn_method) {
+            case config::rest_authn_method::none: {
+                return std::forward<Func>(func)(service().client().local());
+            }
+            case config::rest_authn_method::http_basic: {
+                return dispatch([this, &func](kafka_client_cache& cache) {
+                    auto client = cache.fetch_or_insert(user, authn_method);
+                    return std::forward<Func>(func)(*client).finally(
+                      [client] {});
+                });
+            }
+            }
+        }
+
+        template<std::invocable<kafka::client::client&> Func>
+        auto dispatch(kafka::group_id const& group_id, Func&& func) {
+            switch (authn_method) {
+            case config::rest_authn_method::none: {
+                return service().client().invoke_on(
+                  impl::consumer_shard(group_id), [&func](auto& client) {
+                      return std::forward<Func>(func)(client);
+                  });
+            }
+            case config::rest_authn_method::http_basic: {
+                return dispatch([this, &func](kafka_client_cache& cache) {
+                    auto client = cache.fetch_or_insert(user, authn_method);
+                    return std::forward<Func>(func)(*client).finally(
+                      [client] {});
+                });
+            }
+            }
+        }
+
+        template<impl::KafkaRequestFactory Func>
+        auto dispatch(Func&& func) {
+            return dispatch([&func](kafka::client::client& client) {
+                return client.dispatch(std::forward<Func>(func));
+            });
+        }
 
         void authenticate() {
             authn_method = config::get_authn_method(
