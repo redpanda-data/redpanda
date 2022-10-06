c57cd9d97750009be446cd1b58446549469631ae
Alexey Biryukov <alexey@redpanda.com>
2022-10-06 00:37:54 -0400

    cluster: bootstrap command creates default user
    bootstrap_backend operates on credentail_storage for that.
    In security_frontend, maybe_create_bootstrap_user() is changed into
    get_bootstrap_user_creds_from_env(), and the actual bootstrap user
    creation is not done anymore.

--- src/v/cluster/controller.cc
+++ src/v/cluster/controller.cc
@@ -131,8 +131,10 @@ controller::start(std::vector<model::broker> initial_raft0_brokers) {
       .then([this] {
           return _feature_backend.start_single(std::ref(_feature_table));
       })
-      .then(
-        [this] { return _bootstrap_backend.start_single(std::ref(_storage)); })
+      .then([this] {
+          return _bootstrap_backend.start_single(
+            std::ref(_credentials), std::ref(_storage));
+      })
       .then([this] {
           return _config_frontend.start(
             std::ref(_stm),
@@ -460,6 +462,8 @@ ss::future<> controller::create_cluster() {
       "Cluster can only be created from controller_stm_shard");
     bootstrap_cluster_cmd_data cmd_data;
     cmd_data.uuid = model::cluster_uuid(uuid_t::create());
+    cmd_data.bootstrap_user_cred
+      = security_frontend::get_bootstrap_user_creds_from_env();
     vlog(clusterlog.info, "Creating cluster {}", cmd_data.uuid);
 
     for (simple_time_jitter<model::timeout_clock> retry_jitter(1s);;) {
@@ -528,13 +532,6 @@ controller::cluster_creation_hook(const bool local_node_is_seed_server) {
         while (!_raft0->is_leader()) {
             co_await ss::sleep(100ms);
         }
-
-        auto err
-          = co_await _security_frontend.local().maybe_create_bootstrap_user();
-        vassert(
-          err == errc::success,
-          "Controller write should always succeed in single replica state "
-          "during creation");
 
         co_return co_await create_cluster();
     }
