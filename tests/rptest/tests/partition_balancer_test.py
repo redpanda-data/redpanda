ab96f44baf5dabb41b6e9cc1b9bac25384624ed4
Denis Rystsov <denis@vectorized.io>
2022-10-06 08:07:04 -0700

    ducky: unmute fixed 5884 test

--- tests/rptest/tests/partition_balancer_test.py
+++ tests/rptest/tests/partition_balancer_test.py
@@ -432,7 +432,6 @@ class PartitionBalancerTest(EndToEndTest):
             ns.make_available()
             self.run_validation(consumer_timeout_sec=CONSUMER_TIMEOUT)
 
-    @ok_to_fail  # https://github.com/redpanda-data/redpanda/issues/5884
     @cluster(num_nodes=6, log_allow_list=CHAOS_LOG_ALLOW_LIST)
     def test_full_nodes(self):
         """
