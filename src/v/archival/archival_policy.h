3bfabe74501da0d45e4e5dd65d421b4a2da5f443
Abhijat Malviya <abhijat@redpanda.com>
2022-10-07 11:18:31 +0530

    archival: cleans up unused code
    Some of the code to search for compacted segments and gap traversal is
    now part of the segment collector, so this commit cleans up unused code
    from archival policy and related tests.

--- src/v/archival/archival_policy.h
+++ src/v/archival/archival_policy.h
@@ -48,9 +48,6 @@ public:
       std::optional<segment_time_limit> limit = std::nullopt,
       ss::io_priority_class io_priority = ss::default_priority_class());
 
-    using search_for_compacted_segments
-      = ss::bool_class<struct search_for_compacted_segments_tag>;
-
     /// \brief regurn next upload candidate
     ///
     /// \param begin_inclusive is an inclusive begining of the range
@@ -61,8 +58,7 @@ public:
       model::offset begin_inclusive,
       model::offset end_exclusive,
       storage::log,
-      const storage::offset_translator_state&,
-      search_for_compacted_segments = search_for_compacted_segments::no);
+      const storage::offset_translator_state&);
 
 private:
     /// Check if the upload have to be forced due to timeout
@@ -83,9 +79,6 @@ private:
       model::offset adjusted_lso,
       storage::log,
       const storage::offset_translator_state&);
-
-    lookup_result
-    find_compacted_segment(model::offset start_offset, storage::log log);
 
     model::ntp _ntp;
     std::optional<segment_time_limit> _upload_limit;
