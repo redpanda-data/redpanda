ff80fec4e5bc53d2be5e611907d9c44d63c699c9
Vadim Plakhtinskiy <vadim@redpanda.com>
2022-10-13 16:56:28 +0200

    cluster: introduce log_state inflight

--- src/v/cluster/rm_stm.h
+++ src/v/cluster/rm_stm.h
@@ -402,6 +402,8 @@ private:
         absl::flat_hash_map<model::producer_identity, seq_entry> seq_table;
 
         absl::flat_hash_map<model::producer_identity, model::tx_seq> tx_seqs;
+        // number of batches replicated withing a transaction
+        absl::flat_hash_map<model::producer_identity, int64_t> inflight;
     };
 
     struct mem_state {
