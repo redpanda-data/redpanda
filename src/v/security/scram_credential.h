2bfa68569112d2aab78a30967b3621786ce2fc40
Ben Pope <ben@redpanda.com>
2022-10-19 13:45:59 +0100

    security: Add principal to scram_credential
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/security/scram_credential.h
+++ src/v/security/scram_credential.h
@@ -11,6 +11,7 @@
 #pragma once
 #include "bytes/bytes.h"
 #include "reflection/adl.h"
+#include "security/acl.h"
 #include "serde/envelope.h"
 
 #include <iosfwd>
@@ -29,10 +30,23 @@ public:
       , _stored_key(std::move(stored_key))
       , _iterations(iterations) {}
 
+    scram_credential(
+      bytes salt,
+      bytes server_key,
+      bytes stored_key,
+      int iterations,
+      acl_principal principal) noexcept
+      : _salt(std::move(salt))
+      , _server_key(std::move(server_key))
+      , _stored_key(std::move(stored_key))
+      , _iterations(iterations)
+      , _principal(std::move(principal)) {}
+
     const bytes& salt() const { return _salt; }
     const bytes& server_key() const { return _server_key; }
     const bytes& stored_key() const { return _stored_key; }
     int iterations() const { return _iterations; }
+    const std::optional<acl_principal>& principal() { return _principal; }
 
     bool operator==(const scram_credential&) const = default;
 
@@ -47,6 +61,8 @@ private:
     bytes _server_key;
     bytes _stored_key;
     int _iterations{0};
+    // Principal is not serialized on disk, it is sent over internal rpc
+    std::optional<acl_principal> _principal;
 };
 
 } // namespace security
