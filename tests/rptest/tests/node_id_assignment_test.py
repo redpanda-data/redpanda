8503c28ed6e1f4b96b6123e444bcd61d3ebd9a13
Andrew Wong <awong@redpanda.com>
2022-10-20 08:17:48 -0700

    tests: cache node ID when starting up
    This updates the node_id() method in RedpandaService to use a cached
    value, optionally forcing a refresh that pings the admin endpoint for
    the node config.

--- tests/rptest/tests/node_id_assignment_test.py
+++ tests/rptest/tests/node_id_assignment_test.py
@@ -44,14 +44,14 @@ class NodeIdAssignment(RedpandaTest):
         """
         original_node_id_by_idx = {}
         for node in self.redpanda.nodes:
-            original_node_id = self.redpanda.node_id(node)
+            original_node_id = self.redpanda.node_id(node, force_refresh=True)
             original_node_id_by_idx[self.redpanda.idx(node)] = original_node_id
 
         self.redpanda.restart_nodes(self.redpanda.nodes,
                                     auto_assign_node_id=True)
         for node in self.redpanda.nodes:
             original_node_id = original_node_id_by_idx[self.redpanda.idx(node)]
-            new_node_id = self.redpanda.node_id(node)
+            new_node_id = self.redpanda.node_id(node, force_refresh=True)
             assert new_node_id == original_node_id, f"{new_node_id} vs {original_node_id}"
 
     @cluster(num_nodes=3)
@@ -71,7 +71,7 @@ class NodeIdAssignment(RedpandaTest):
 
         brokers = self.admin.get_brokers()
         assert 4 == len(brokers), f"Got {len(brokers)} brokers"
-        new_node_id = self.redpanda.node_id(clean_node)
+        new_node_id = self.redpanda.node_id(clean_node, force_refresh=True)
         assert original_node_id != new_node_id, f"Cleaned node came back with node ID {new_node_id}"
         self.check_node_ids_persist()
 
@@ -108,5 +108,5 @@ class NodeIdAssignmentUpgrade(RedpandaTest):
 
         brokers = self.admin.get_brokers()
         assert 4 == len(brokers), f"Got {len(brokers)} brokers"
-        new_node_id = self.redpanda.node_id(clean_node)
+        new_node_id = self.redpanda.node_id(clean_node, force_refresh=True)
         assert original_node_id != new_node_id, f"Cleaned node came back with node ID {new_node_id}"
