014fe4ec8cf34bcbe242e0b2a34ccca64568d71d
Noah Watkins <noahwatkins@gmail.com>
2022-10-26 15:43:34 -0700

    utils: move directory walker impl out of header
    The motivation is that we are in general paying a lot in compilation
    time for all the different things we keep in headers.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ src/v/utils/directory_walker.cc
@@ -0,0 +1,24 @@
+#include "directory_walker.h"
+
+ss::future<>
+directory_walker::walk(ss::sstring dirname, walker_type walker_func) {
+    return open_directory(std::move(dirname))
+      .then([walker_func = std::move(walker_func)](ss::file f) mutable {
+          auto s = f.list_directory(std::move(walker_func));
+          return s.done().finally([f = std::move(f)]() mutable {
+              return f.close().finally([f = std::move(f)] {});
+          });
+      });
+}
+
+ss::future<bool> directory_walker::empty(const std::filesystem::path& dir) {
+    return directory_walker::walk(
+             dir.string(),
+             [](const ss::directory_entry&) {
+                 return ss::make_exception_future<>(
+                   directory_walker::stop_walk());
+             })
+      .then([] { return true; })
+      .handle_exception_type(
+        [](const directory_walker::stop_walk&) { return false; });
+}
--- src/v/utils/directory_walker.h
+++ src/v/utils/directory_walker.h
@@ -36,25 +36,6 @@ struct directory_walker {
         }
     };
 
-    static ss::future<> walk(ss::sstring dirname, walker_type walker_func) {
-        return open_directory(std::move(dirname))
-          .then([walker_func = std::move(walker_func)](ss::file f) mutable {
-              auto s = f.list_directory(std::move(walker_func));
-              return s.done().finally([f = std::move(f)]() mutable {
-                  return f.close().finally([f = std::move(f)] {});
-              });
-          });
-    }
-
-    static ss::future<bool> empty(const std::filesystem::path& dir) {
-        return directory_walker::walk(
-                 dir.string(),
-                 [](const ss::directory_entry&) {
-                     return ss::make_exception_future<>(
-                       directory_walker::stop_walk());
-                 })
-          .then([] { return true; })
-          .handle_exception_type(
-            [](const directory_walker::stop_walk&) { return false; });
-    }
+    static ss::future<> walk(ss::sstring dirname, walker_type walker_func);
+    static ss::future<bool> empty(const std::filesystem::path& dir);
 };
