f2184dc87d8e20acf45dc937aa2dad5cc76eb393
John Spray <jcs@redpanda.com>
2022-10-28 18:09:19 +0100

    cluster: add a fixture test case for feature table snapshots
    This isn't a perfect home, but I do not want to create
    a separate fixture test binary in features/ because
    they're 1.5GB each!
    
    This test is validating:
    - that snapshots get written
    - that a modified snapshot's state is reflected in
      the feature table next time redpanda starts (i.e.
      they are being read)

--- src/v/cluster/tests/cluster_tests.cc
+++ src/v/cluster/tests/cluster_tests.cc
@@ -12,6 +12,7 @@
 #include "cluster/simple_batch_builder.h"
 #include "cluster/tests/cluster_test_fixture.h"
 #include "config/configuration.h"
+#include "features/feature_table_snapshot.h"
 #include "net/unresolved_address.h"
 #include "test_utils/fixture.h"
 
@@ -102,4 +103,64 @@ FIXTURE_TEST(
     // single broker
     BOOST_REQUIRE_EQUAL(brokers.size(), 1);
     BOOST_REQUIRE_EQUAL(brokers[0]->id(), id0);
+}
+
+FIXTURE_TEST(test_feature_table_snapshots, cluster_test_fixture) {
+    // Switch on the dummy test features for use in the test.  It is safe
+    // to leave this set for the remainder of process lifetime, as other
+    // tests should not notice or care that there is an extra feature flag.
+    setenv("__REDPANDA_TEST_FEATURES", "true", 1);
+
+    // Start up a node normally
+    const model::node_id id0{0};
+    auto app = create_node_application(
+      id0, configure_node_id::no, empty_seed_starts_cluster::no);
+    BOOST_REQUIRE_EQUAL(0, *config::node().node_id());
+    wait_for_controller_leadership(id0).get();
+    wait_for_all_members(3s).get();
+
+    // Peek at storage, see that it created a snapshot
+    auto snap_opt = app->storage.local().kvs().get(
+      storage::kvstore::key_space::controller,
+      features::feature_table_snapshot::kvstore_key());
+    BOOST_REQUIRE(snap_opt.has_value());
+
+    auto active_version = app->feature_table.local().get_active_version();
+
+    // Restart it
+    remove_node_application(id0);
+    app = create_node_application(
+      id0, configure_node_id::no, empty_seed_starts_cluster::no);
+
+    // Peek at its feature table to check that it is in the expected state,
+    // although this doesn't prove it was loaded from a snapshot.
+    BOOST_REQUIRE(
+      active_version == app->feature_table.local().get_active_version());
+    BOOST_REQUIRE(
+      app->feature_table.local().is_active(features::feature::test_alpha)
+      == false);
+
+    // Now inject a phony snapshot to check that the application really is
+    // loading from the snapshot and not just reconstituting via controller log.
+    features::feature_table_snapshot bogus_snapshot;
+    bogus_snapshot.applied_offset
+      = app->feature_table.local().get_applied_offset();
+    bogus_snapshot.version = app->feature_table.local().get_active_version();
+    bogus_snapshot.states = {features::feature_state_snapshot{
+      .name = "__test_alpha", .state = features::feature_state::state::active}};
+    app->storage.local()
+      .kvs()
+      .put(
+        storage::kvstore::key_space::controller,
+        features::feature_table_snapshot::kvstore_key(),
+        serde::to_iobuf(bogus_snapshot))
+      .get();
+
+    remove_node_application(id0);
+    app = create_node_application(
+      id0, configure_node_id::no, empty_seed_starts_cluster::no);
+
+    BOOST_REQUIRE(
+      app->feature_table.local().is_active(features::feature::test_alpha)
+      == true);
 }
