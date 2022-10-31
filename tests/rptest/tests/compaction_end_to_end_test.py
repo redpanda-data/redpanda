fa9d707bd6ed1c74fb3c4c18c777a55725ceab93
Michal Maslanka <michal@redpanda.com>
2022-10-31 11:57:43 +0100

    tests: extend consumer timeout in debug mode
    Compaction end to end tests relay on certain data volume being produced
    and consumed from redpanda. Debug mode experience a lot of overhead
    because of address sanitizer and non seastar memory allocator.
    
    Made consumer timeout longer for debug test to make sure they are not
    flaky.
    
    Fixes: #6745
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>

--- tests/rptest/tests/compaction_end_to_end_test.py
+++ tests/rptest/tests/compaction_end_to_end_test.py
@@ -148,7 +148,7 @@ class CompactionEndToEndTest(EndToEndTest):
 
         # enable consumer and validate consumed records
         self.start_consumer(num_nodes=1, verify_offsets=False)
-
+        consumer_timeout = 180 if self.debug_mode else 90
         self.run_validation(enable_compaction=True,
                             enable_transactions=transactions,
-                            consumer_timeout_sec=90)
+                            consumer_timeout_sec=consumer_timeout)
