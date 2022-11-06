08e36b7f5f8b28024edb99b41c44d5d466f4c8dc
Noah Watkins <noahwatkins@gmail.com>
2022-11-06 14:48:30 -0800

    cloud_storage: decoro partition recover manager lambda
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/cloud_storage/partition_recovery_manager.cc
+++ src/v/cloud_storage/partition_recovery_manager.cc
@@ -393,10 +393,8 @@ partition_downloader::download_log_with_capped_size(
     co_await ss::max_concurrent_for_each(
       staged_downloads,
       max_concurrency,
-      [this, _dlpart{&dlpart}, _dloffsets{&dloffsets}](
+      [this, &dlpart, &dloffsets](
         const segment_meta& s) -> ss::future<> {
-          auto& dloffsets{*_dloffsets};
-          auto& dlpart{*_dlpart};
           retry_chain_node fib(&_rtcnode);
           retry_chain_logger dllog(cst_log, fib);
           vlog(
@@ -409,10 +407,11 @@ partition_downloader::download_log_with_capped_size(
             s.size_bytes,
             dlpart.part_prefix,
             dlpart.dest_prefix);
-          auto offsets = co_await download_segment_file(s, dlpart);
+          return download_segment_file(s, dlpart).then([&dloffsets](auto offsets) {
           if (offsets) {
               dloffsets.push_back(offsets.value());
           }
+          });
       });
     update_downloaded_offsets(std::move(dloffsets), dlpart);
     if (dlpart.num_files == 0) {
@@ -488,10 +487,8 @@ partition_downloader::download_log_with_capped_time(
     co_await ss::max_concurrent_for_each(
       staged_downloads,
       max_concurrency,
-      [this, _dlpart{&dlpart}, _dloffsets{&dloffsets}](
+      [this, &dlpart, &dloffsets](
         const segment_meta& s) -> ss::future<> {
-          auto& dlpart{*_dlpart};
-          auto& dloffsets{*_dloffsets};
           retry_chain_node fib(&_rtcnode);
           retry_chain_logger dllog(cst_log, fib);
           vlog(
@@ -502,10 +499,11 @@ partition_downloader::download_log_with_capped_time(
             s.segment_term,
             dlpart.part_prefix,
             dlpart.dest_prefix);
-          auto offsets = co_await download_segment_file(s, dlpart);
+          return download_segment_file(s, dlpart).then([&dloffsets](auto offsets) {
           if (offsets) {
               dloffsets.push_back(offsets.value());
           }
+          });
       });
     update_downloaded_offsets(std::move(dloffsets), dlpart);
     if (dlpart.num_files == 0) {
