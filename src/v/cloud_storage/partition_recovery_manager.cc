41573da5e445c4bb77bb5aae4d7ec4fa8ddf27a4
Andrea Barbadoro <andrea@redpanda.com>
2022-11-09 11:30:37 +0100

    cloud_storage/partition_recovery_manager: download remote segments skipping over configuration-only segments
    When retention.bytes is set, download_log_with_capped_size is used to not exceed this size limit.
    Now the function counts toward this limit only those segments that contain some topic data.
    Previously the segments to download were precomputed from offset_meta keeping a total_size sum.
    Now sum is done while downloading segments, to account for segments that contain no data and that will be discarded.

--- src/v/cloud_storage/partition_recovery_manager.cc
+++ src/v/cloud_storage/partition_recovery_manager.cc
@@ -341,38 +341,6 @@ partition_downloader::download_log_with_capped_size(
   size_t max_size) {
     vlog(_ctxlog.info, "Starting log download with size limit at {}", max_size);
     gate_guard guard(_gate);
-    size_t total_size = 0;
-    std::deque<segment> staged_downloads;
-    model::offset start_offset{0};
-    model::offset start_delta{0};
-    for (auto it = offset_map.rbegin(); it != offset_map.rend(); it++) {
-        const auto& meta = it->second.meta;
-        if (total_size > max_size) {
-            vlog(
-              _ctxlog.debug,
-              "Max size {} reached, skipping {}",
-              total_size,
-              it->second.manifest_key);
-            break;
-        } else {
-            vlog(
-              _ctxlog.debug,
-              "Found {}, total log size {}",
-              it->second.manifest_key,
-              total_size);
-        }
-        staged_downloads.push_front(it->second);
-        total_size += meta.size_bytes;
-        start_offset = meta.base_offset;
-        start_delta = meta.delta_offset == model::offset() ? start_delta
-                                                           : meta.delta_offset;
-    }
-    vlog(
-      _ctxlog.info,
-      "Downloading {} bytes, start_offset: {}, start_delta: {}",
-      total_size,
-      start_offset,
-      start_delta);
 
     std::vector<offset_range> dloffsets;
     download_part dlpart{
@@ -384,13 +352,25 @@ partition_downloader::download_log_with_capped_size(
         .max_offset = model::offset::min(),
       }};
 
-    co_await ss::max_concurrent_for_each(
-      staged_downloads,
-      max_concurrency,
-      [this, _dlpart{&dlpart}, _dloffsets{&dloffsets}](
-        const segment& s) -> ss::future<> {
-          auto& dloffsets{*_dloffsets};
-          auto& dlpart{*_dlpart};
+    // process from most recent (last one)
+    auto offset_segment_it = offset_map.rbegin();
+    auto offset_end = offset_map.rend();
+    size_t total_size = 0;
+    auto data_found = false;
+
+    co_await ss::do_until(
+      [&] {
+          return offset_segment_it == offset_end
+                 || // no more segments to process
+                    // or processing this segment would exceed max_size
+                    // and some proper data is ready
+                 (data_found
+                  && (total_size + offset_segment_it->second.meta.size_bytes >= max_size));
+      },
+      [&]() -> ss::future<> {
+          auto [_, s] = *offset_segment_it;
+          ++offset_segment_it;
+
           retry_chain_node fib(&_rtcnode);
           retry_chain_logger dllog(cst_log, fib);
           vlog(
@@ -403,15 +383,18 @@ partition_downloader::download_log_with_capped_size(
             s.meta.size_bytes,
             dlpart.part_prefix,
             dlpart.dest_prefix);
-          return download_segment_file(s, dlpart).then(
-            [&dloffsets](auto offsets) {
-                if (offsets) {
-                    dloffsets.push_back({
-                      .min_offset = offsets->min_offset,
-                      .max_offset = offsets->max_offset,
-                    });
-                }
-            });
+          return download_segment_file(s, dlpart).then([&](auto offsets) {
+              if (!offsets || offsets->size_bytes == 0u) {
+                  return;
+              }
+              auto [min_offset, max_offset, size] = *offsets;
+              data_found = true;
+              total_size += size;
+              dloffsets.push_back({
+                .min_offset = min_offset,
+                .max_offset = max_offset,
+              });
+          });
       });
     update_downloaded_offsets(std::move(dloffsets), dlpart);
     if (dlpart.num_files == 0) {
