149e4fd0c9061fd959f523a486e2be828cef44fb
Andrew Wong <awong@redpanda.com>
2022-11-28 20:03:59 -0800

    cluster: ignore removed nodes for maintenance commands
    CONFLICT: recommissioning is not really supported in v22.1, so the test
    for recommissioning is removed. Also had to pull in some other test
    utilities used by the remaining test. Also swapped node_id() for idx()
    
    Issue #7370 is fixed on dev by a wide refactor that isn't ideal for
    backporting. This commit is a lighter-weight fix for the issue.
    
    This commit ignores removed nodes when considering whether nodes can be
    placed into maintenance mode, matching the behavior on dev.
    
    Fixes #7370

--- tests/rptest/tests/nodes_decommissioning_test.py
+++ tests/rptest/tests/nodes_decommissioning_test.py
@@ -8,6 +8,7 @@
 # by the Apache License, Version 2.0
 
 import random
+from requests.exceptions import HTTPError
 
 from rptest.services.cluster import cluster
 from ducktape.utils.util import wait_until
@@ -18,10 +19,99 @@ from rptest.services.admin import Admin
 from rptest.services.redpanda import CHAOS_LOG_ALLOW_LIST, RESTART_LOG_ALLOW_LIST
 
 
+def in_maintenance(node, admin):
+    """
+    Use the given Admin to check if the given node is in maintenance mode.
+    """
+    status = admin.maintenance_status(node)
+    if "finished" in status and status["finished"]:
+        return True
+    return status["draining"]
+
+
+def wait_for_maintenance(node, admin):
+    """
+    Use the given Admin to wait until the given node is in maintenance mode.
+    """
+    wait_until(lambda: in_maintenance(node, admin),
+               timeout_sec=10,
+               backoff_sec=1)
+
+
 class NodesDecommissioningTest(EndToEndTest):
     """
     Basic nodes decommissioning test.
     """
+    def _create_topics(self, replication_factors=[1, 3]):
+        topics = []
+        for i in range(10):
+            spec = TopicSpec(
+                partition_count=random.randint(1, 10),
+                replication_factor=random.choice(replication_factors))
+            topics.append(spec)
+
+        for spec in topics:
+            self.client().create_topic(spec)
+
+        self.topic = random.choice(topics).name
+
+    def _not_decommissioned_node(self, to_decommission):
+        return [
+            n for n in self.redpanda.nodes
+            if self.redpanda.idx(n) != to_decommission
+        ][0]
+
+    def _node_removed(self, removed_id, node_to_query):
+        admin = Admin(self.redpanda)
+        brokers = admin.get_brokers(node=node_to_query)
+        for b in brokers:
+            if b['node_id'] == removed_id:
+                return False
+        return True
+
+    def _wait_until_status(self, node_id, status, timeout_sec=15):
+        def requested_status():
+            brokers = Admin(self.redpanda).get_brokers()
+            for broker in brokers:
+                if broker['node_id'] == node_id:
+                    return broker['membership_status'] == status
+            return False
+
+        wait_until(requested_status, timeout_sec=timeout_sec, backoff_sec=1)
+
+    @cluster(num_nodes=4, log_allow_list=RESTART_LOG_ALLOW_LIST)
+    def test_decommissioning_maintenance_node(self):
+        self.start_redpanda(num_nodes=4)
+        self._create_topics()
+        admin = self.redpanda._admin
+
+        to_decommission = random.choice(self.redpanda.nodes)
+        to_decommission_id = self.redpanda.idx(to_decommission)
+        survivor_node = self._not_decommissioned_node(to_decommission_id)
+
+        admin.maintenance_start(to_decommission)
+        wait_for_maintenance(to_decommission, admin)
+
+        admin.decommission_broker(to_decommission_id)
+        wait_until(
+            lambda: self._node_removed(to_decommission_id, survivor_node),
+            timeout_sec=120,
+            backoff_sec=2)
+
+        # We should be unable to run further maintenance commands on the
+        # removed node.
+        try:
+            admin.maintenance_stop(to_decommission)
+            assert False, f"Excepted 404 for node {to_decommission_id}"
+        except HTTPError as e:
+            assert "404 Client Error" in repr(e)
+
+        # We should be able to start maintenance on another node, even if we
+        # didn't explicitly stop maintenance on 'to_decommission'.
+        admin.maintenance_start(survivor_node)
+        wait_for_maintenance(survivor_node, admin)
+        assert in_maintenance(to_decommission, admin)
+
     @cluster(
         num_nodes=6,
         # A decom can look like a restart in terms of logs from peers dropping
