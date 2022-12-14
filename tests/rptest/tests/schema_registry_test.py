2ad0a67fa5c8689452d51a69d4269c3b3a8c8cb2
Ben Pope <ben@redpanda.com>
2022-12-14 14:39:51 +0000

    schema_registry: Test restarts
    Signed-off-by: Ben Pope <ben@redpanda.com>
    (cherry picked from commit d55544f6cb167079d217386dcae7b03bc0eef829)

--- tests/rptest/tests/schema_registry_test.py
+++ tests/rptest/tests/schema_registry_test.py
@@ -22,6 +22,7 @@ from rptest.clients.types import TopicSpec
 from rptest.clients.kafka_cli_tools import KafkaCliTools
 from rptest.clients.python_librdkafka_serde_client import SerdeClient, SchemaType
 from rptest.tests.redpanda_test import RedpandaTest
+from rptest.services.admin import Admin
 from rptest.services.redpanda import ResourceSettings
 
 
@@ -1079,3 +1080,26 @@ class SchemaRegistryTest(RedpandaTest):
                 assert schema.json().get("schemaType") is None
             else:
                 assert schema.json()["schemaType"] == protocols[i].name
+
+    @cluster(num_nodes=3)
+    def test_restarts(self):
+        admin = Admin(self.redpanda)
+
+        def check_connection(hostname: str):
+            result_raw = self._get_subjects(hostname=hostname)
+            self.logger.info(result_raw.status_code)
+            self.logger.info(result_raw.json())
+            assert result_raw.status_code == requests.codes.ok
+            assert result_raw.json() == []
+
+        def restart_leader():
+            leader = admin.get_partition_leader(namespace="kafka",
+                                                topic="_schemas",
+                                                partition=0)
+            self.logger.info(f"Restarting node: {leader}")
+            self.redpanda.restart_nodes(self.redpanda.get_node(leader))
+
+        for _ in range(20):
+            for n in self.redpanda.nodes:
+                check_connection(n.account.hostname)
+            restart_leader()
