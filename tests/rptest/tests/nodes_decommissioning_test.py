b3dfcf3c2659c0a1befbcff59b76ba293b438729
Michal Maslanka <michal@redpanda.com>
2023-01-04 13:24:23 +0100

    tests: restart nodes after multiple decommissioning test
    Signed-off-by: Michal Maslanka <michal@redpanda.com>

--- tests/rptest/tests/nodes_decommissioning_test.py
+++ tests/rptest/tests/nodes_decommissioning_test.py
@@ -624,6 +624,10 @@ class NodesDecommissioningTest(EndToEndTest):
 
                 wait_until(has_partitions, 180, 2)
 
+        self.redpanda.restart_nodes(self.redpanda.nodes,
+                                    auto_assign_node_id=True,
+                                    omit_seeds_on_idx_one=False)
+
     @cluster(num_nodes=4, log_allow_list=RESTART_LOG_ALLOW_LIST)
     @parametrize(new_bootstrap=True)
     @parametrize(new_bootstrap=False)
