80f0e5a7bdcc4885039cf66f00460056bb632c32
Alexey Biryukov <alexey@redpanda.com>
2023-01-04 00:21:49 -0500

    utils: bottomless token bucket & ut
    Differences from the existing token bucket implementations:
    
    - token level can go negative - because tokens (bytes of traffic) can go
    negative even on the first call, but throttling is not enforced yet
    (classic token bucket only allows nonnegative token levels)
    
    - token level can go negative unrestricted - because there are multiple
    connections and a second connection could consume its initial portion
    of tokens before the token have restored to zero after the first
    connection has exhausted them below zero (token_bucket_rate_tracker
    does not allow that)
    
    - no built in throttling (as in utils/token_bucket.h)
    
    - quota and bucket width can be changed - to allow dynamic adjustment of
    the corresponding cluster settings (token_bucket_rate_tracker does not
    allow that)
    
    - millisecond precision for refill - to have token refill resolution
    at a scale comparable with how frequently the bucket can be used by
    different connections (token_bucket_rate_tracker has seconds resolution)

---
+++ src/v/utils/bottomless_token_bucket.cc
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2023 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#include "utils/bottomless_token_bucket.h"
+
+#include "likely.h"
+#include "vassert.h"
+
+void bottomless_token_bucket::use(
+  const tokens_t n, const clock::time_point now) noexcept {
+    vassert(n >= 0, "Cannot use negative number of tokens");
+    refill(now);
+    _tokens -= n;
+}
+
+void bottomless_token_bucket::set_quota_impl(const quota_t quota) noexcept {
+    vassert(quota > 0, "Token bucket quota must be positive ({})", quota);
+    vassert(
+      quota <= max_quota,
+      "Token bucket quota too large ({} > {})",
+      quota,
+      max_quota);
+    _quota = quota;
+}
+
+void bottomless_token_bucket::set_width_impl(const time_res_t width) noexcept {
+    vassert(
+      width.count() > 0,
+      "Token bucket width must be positive ({})",
+      width.count());
+    vassert(
+      width <= max_width,
+      "Token bucket width too large ({} > {})",
+      width.count(),
+      max_width.count());
+    _width = width;
+}
+
+void bottomless_token_bucket::update_burst_tokens() noexcept {
+    _burst = muldiv(_quota, _width.count(), time_res_t::period::den);
+    _tokens = std::min(_burst, _tokens);
+}
+
+void bottomless_token_bucket::refill(const clock::time_point now) noexcept {
+    const auto delta = std::chrono::duration_cast<time_res_t>(
+      now - _last_check);
+    _last_check = now;
+    if (unlikely(delta >= max_width)) {
+        // avoid overflow on multiplication - the product will be greater
+        // than _burst anyway
+        _tokens = _burst;
+    } else {
+        const tokens_t add_tokens = muldiv(
+          _quota, delta.count(), time_res_t::period::den);
+        if (add_tokens >= _burst) {
+            _tokens = _burst;
+        } else {
+            _tokens = std::min<tokens_t>(_burst, _tokens + add_tokens);
+        }
+    }
+}
---
+++ src/v/utils/bottomless_token_bucket.h
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2023 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#pragma once
+#include "seastar/core/lowres_clock.hh"
+#include "seastarx.h"
+
+#include <algorithm>
+#include <chrono>
+#include <limits>
+
+/// muldiv calculates \p x * \p mul / \p div in integer domain avoiding both
+/// integer overflow and loss of precision because of the intermediate result
+/// being the same integer type as the arguments.
+///
+/// There are still limits for the values where overflow can kick in:
+/// if \p x % \p div * \p mul fails to stay within type limits, the behaviour is
+/// undefined. That may happen when
+/// abs( \p mul * \p div ) >= numeric_limits<int64_t>::max()
+inline int64_t
+muldiv(const int64_t x, const int64_t mul, const int64_t div) noexcept {
+    const auto qr = std::div(x, div);
+    return qr.quot * mul + qr.rem * mul / div;
+}
+
+/// Variation of token bucket algorithm to allow bursty traffic.
+/// Tokens represent resource units (e.g. bytes of traffic) whose usage is
+/// controlled. Number of tokens in the bucket is capped by \ref quota *
+/// \ref width at the top, and can go negative without a bound.
+class bottomless_token_bucket {
+public:
+    /// Quota is measured in [tokens/s] and cannot be negative despite the
+    /// signed type
+    using quota_t = int64_t;  // [tokens/s]
+    using tokens_t = int64_t; // [tokens]
+    using clock = ss::lowres_clock;
+    using time_res_t = std::chrono::milliseconds;
+
+    // Algoritms rely on time_res_t to be subsecond
+    static_assert(time_res_t::period::num == 1);
+
+    // Maximum supported width of a bucket ~ 25 days
+    static constexpr time_res_t max_width{std::numeric_limits<int32_t>::max()};
+
+    // To avoid dynamic checking of acceptable width and quota maximums,
+    // quota is limited in the way to avoid integer overflows in any condition.
+    // This effectively maxes it to ~ 2 Ttokens/s. If that is ever found not
+    // enough, this maximum should be raised to full int64_t, and both
+    // set_quota() and set_width() should dynamically check that the calculated
+    // burst does not overflow.
+    static constexpr quota_t max_quota{
+      static_cast<quota_t>(std::numeric_limits<int32_t>::max())
+      * time_res_t::period::den};
+
+    /// Token bucket is initialized full of tokens
+    bottomless_token_bucket(
+      const quota_t quota, const time_res_t width) noexcept {
+        // do boundary checks on params and calculate _burst
+        set_quota_impl(quota);
+        set_width_impl(width);
+        update_burst_tokens();
+        refill(clock::time_point{});
+    }
+
+    void set_quota(const quota_t quota) noexcept {
+        set_quota_impl(quota);
+        update_burst_tokens();
+    }
+
+    /// @post 0 < return value <= max_quota
+    quota_t quota() const noexcept { return _quota; }
+
+    void set_width(const time_res_t width) noexcept {
+        set_width_impl(width);
+        update_burst_tokens();
+    }
+
+    /// Record usage of tokens
+    /// @param n Number of tokens used
+    /// @param now Time to record at
+    void use(tokens_t n, clock::time_point now) noexcept;
+
+    /// Current amount of tokens
+    tokens_t tokens() const noexcept { return _tokens; }
+
+private:
+    void set_quota_impl(const quota_t quota) noexcept;
+    void set_width_impl(const time_res_t width) noexcept;
+    void update_burst_tokens() noexcept;
+    void refill(const clock::time_point now) noexcept;
+
+private:
+    quota_t _quota{0};
+    time_res_t _width;
+    tokens_t _tokens{0};
+    tokens_t _burst{0}; // capacity of the bucket
+    clock::time_point _last_check{-max_width};
+};
