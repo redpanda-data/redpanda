0f347799c551c0d7680c8d7efb33c47bfdaa0878
Noah Watkins <noahwatkins@gmail.com>
2023-01-05 15:52:49 -0800

    kafka: consolidate offset fetch handler
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/kafka/server/server.cc
+++ src/v/kafka/server/server.cc
@@ -24,6 +24,7 @@
 #include "kafka/server/handlers/join_group.h"
 #include "kafka/server/handlers/leave_group.h"
 #include "kafka/server/handlers/list_groups.h"
+#include "kafka/server/handlers/offset_fetch.h"
 #include "kafka/server/handlers/sasl_authenticate.h"
 #include "kafka/server/handlers/sasl_handshake.h"
 #include "kafka/server/handlers/sync_group.h"
@@ -655,6 +656,80 @@ ss::future<response_ptr> add_partitions_to_txn_handler::handle(
               return ctx.respond(std::move(response));
           });
     });
+}
+
+template<>
+ss::future<response_ptr>
+offset_fetch_handler::handle(request_context ctx, ss::smp_service_group) {
+    offset_fetch_request request;
+    request.decode(ctx.reader(), ctx.header().version);
+    log_request(ctx.header(), request);
+    if (!ctx.authorized(
+          security::acl_operation::describe, request.data.group_id)) {
+        co_return co_await ctx.respond(
+          offset_fetch_response(error_code::group_authorization_failed));
+    }
+
+    /*
+     * request is for all group offsets
+     */
+    if (!request.data.topics) {
+        auto resp = co_await ctx.groups().offset_fetch(std::move(request));
+        if (resp.data.error_code != error_code::none) {
+            co_return co_await ctx.respond(std::move(resp));
+        }
+
+        // remove unauthorized topics from response
+        auto unauthorized = std::partition(
+          resp.data.topics.begin(),
+          resp.data.topics.end(),
+          [&ctx](const offset_fetch_response_topic& topic) {
+              /*
+               * quiet authz failures. this is checking for visibility across
+               * all topics not specifically requested topics.
+               */
+              return ctx.authorized(
+                security::acl_operation::describe,
+                topic.name,
+                authz_quiet{true});
+          });
+        resp.data.topics.erase(unauthorized, resp.data.topics.end());
+
+        co_return co_await ctx.respond(std::move(resp));
+    }
+
+    /*
+     * pre-filter authorized topics in request
+     */
+    auto unauthorized_it = std::partition(
+      request.data.topics->begin(),
+      request.data.topics->end(),
+      [&ctx](const offset_fetch_request_topic& topic) {
+          return ctx.authorized(security::acl_operation::describe, topic.name);
+      });
+
+    std::vector<offset_fetch_request_topic> unauthorized(
+      std::make_move_iterator(unauthorized_it),
+      std::make_move_iterator(request.data.topics->end()));
+
+    // remove unauthorized topics from request
+    request.data.topics->erase(unauthorized_it, request.data.topics->end());
+    auto resp = co_await ctx.groups().offset_fetch(std::move(request));
+
+    // add requested (but unauthorized) topics into response
+    for (auto& req_topic : unauthorized) {
+        auto& topic = resp.data.topics.emplace_back();
+        topic.name = std::move(req_topic.name);
+        topic.partitions.reserve(req_topic.partition_indexes.size());
+        for (auto partition_index : req_topic.partition_indexes) {
+            auto& partition = topic.partitions.emplace_back();
+            partition.partition_index = partition_index;
+            partition.error_code = error_code::group_authorization_failed;
+            topic.partitions.push_back(std::move(partition));
+        }
+    }
+
+    co_return co_await ctx.respond(std::move(resp));
 }
 
 } // namespace kafka
