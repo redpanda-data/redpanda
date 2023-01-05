19d91db46897903656e573217b0118e4a60d45bf
Noah Watkins <noahwatkins@gmail.com>
2023-01-05 15:30:32 -0800

    kafka: consolidate add offsets to txn handler
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/kafka/server/server.cc
+++ src/v/kafka/server/server.cc
@@ -16,6 +16,7 @@
 #include "kafka/server/connection_context.h"
 #include "kafka/server/coordinator_ntp_mapper.h"
 #include "kafka/server/group_router.h"
+#include "kafka/server/handlers/add_offsets_to_txn.h"
 #include "kafka/server/handlers/delete_groups.h"
 #include "kafka/server/handlers/end_txn.h"
 #include "kafka/server/handlers/heartbeat.h"
@@ -525,6 +526,58 @@ ss::future<response_ptr> end_txn_handler::handle(
               response.data = data;
               return ctx.respond(response);
           });
+    });
+}
+
+template<>
+ss::future<response_ptr>
+add_offsets_to_txn_handler::handle(request_context ctx, ss::smp_service_group) {
+    return ss::do_with(std::move(ctx), [](request_context& ctx) {
+        add_offsets_to_txn_request request;
+        request.decode(ctx.reader(), ctx.header().version);
+        log_request(ctx.header(), request);
+
+        cluster::add_offsets_tx_request tx_request{
+          .transactional_id = request.data.transactional_id,
+          .producer_id = request.data.producer_id,
+          .producer_epoch = request.data.producer_epoch,
+          .group_id = request.data.group_id};
+
+        auto f = ctx.tx_gateway_frontend().add_offsets_to_tx(
+          tx_request, config::shard_local_cfg().create_topic_timeout_ms());
+
+        return f.then([&ctx](cluster::add_offsets_tx_reply tx_response) {
+            add_offsets_to_txn_response_data data;
+            switch (tx_response.error_code) {
+            case cluster::tx_errc::none:
+                data.error_code = error_code::none;
+                break;
+            case cluster::tx_errc::not_coordinator:
+                data.error_code = error_code::not_coordinator;
+                break;
+            case cluster::tx_errc::coordinator_not_available:
+                data.error_code = error_code::coordinator_not_available;
+                break;
+            case cluster::tx_errc::coordinator_load_in_progress:
+                data.error_code = error_code::coordinator_load_in_progress;
+                break;
+            case cluster::tx_errc::invalid_producer_id_mapping:
+                data.error_code = error_code::invalid_producer_id_mapping;
+                break;
+            case cluster::tx_errc::fenced:
+                data.error_code = error_code::invalid_producer_epoch;
+                break;
+            case cluster::tx_errc::invalid_txn_state:
+                data.error_code = error_code::invalid_txn_state;
+                break;
+            default:
+                data.error_code = error_code::unknown_server_error;
+                break;
+            }
+            add_offsets_to_txn_response res;
+            res.data = data;
+            return ctx.respond(res);
+        });
     });
 }
 
