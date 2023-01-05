5ba54a16eeadb183b400567e0b263ea5ddbab040
Noah Watkins <noahwatkins@gmail.com>
2023-01-05 14:31:17 -0800

    kafka: consolidate sync group handler
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/kafka/server/server.cc
+++ src/v/kafka/server/server.cc
@@ -18,6 +18,7 @@
 #include "kafka/server/group_router.h"
 #include "kafka/server/handlers/heartbeat.h"
 #include "kafka/server/handlers/sasl_authenticate.h"
+#include "kafka/server/handlers/sync_group.h"
 #include "kafka/server/logger.h"
 #include "kafka/server/request_context.h"
 #include "kafka/server/response.h"
@@ -290,6 +291,30 @@ ss::future<response_ptr> sasl_authenticate_handler::handle(
         "SASL authentication failed: {}", result.error().message()),
     };
     return ctx.respond(sasl_authenticate_response(std::move(data)));
+}
+
+template<>
+process_result_stages sync_group_handler::handle(
+  request_context ctx, [[maybe_unused]] ss::smp_service_group g) {
+    sync_group_request request;
+    request.decode(ctx.reader(), ctx.header().version);
+    log_request(ctx.header(), request);
+
+    if (!ctx.authorized(security::acl_operation::read, request.data.group_id)) {
+        return process_result_stages::single_stage(ctx.respond(
+          sync_group_response(error_code::group_authorization_failed)));
+    }
+
+    auto stages = ctx.groups().sync_group(std::move(request));
+    auto res = ss::do_with(
+      std::move(ctx),
+      [f = std::move(stages.result)](request_context& ctx) mutable {
+          return f.then([&ctx](sync_group_response response) {
+              return ctx.respond(std::move(response));
+          });
+      });
+
+    return process_result_stages(std::move(stages.dispatched), std::move(res));
 }
 
 } // namespace kafka
