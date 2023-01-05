3d7381a76b198a23e835a34c07042f428b271801
Noah Watkins <noahwatkins@gmail.com>
2023-01-05 15:01:50 -0800

    kafka: consolidate join group handler
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/kafka/server/server.cc
+++ src/v/kafka/server/server.cc
@@ -17,6 +17,7 @@
 #include "kafka/server/coordinator_ntp_mapper.h"
 #include "kafka/server/group_router.h"
 #include "kafka/server/handlers/heartbeat.h"
+#include "kafka/server/handlers/join_group.h"
 #include "kafka/server/handlers/leave_group.h"
 #include "kafka/server/handlers/list_groups.h"
 #include "kafka/server/handlers/sasl_authenticate.h"
@@ -404,6 +405,37 @@ ss::future<response_ptr> sasl_handshake_handler::handle(
 
     return ctx.respond(
       sasl_handshake_response(error, supported_sasl_mechanisms));
+}
+
+template<>
+process_result_stages join_group_handler::handle(
+  request_context ctx, [[maybe_unused]] ss::smp_service_group g) {
+    join_group_request request;
+    request.decode(ctx.reader(), ctx.header().version);
+    request.version = ctx.header().version;
+    if (ctx.header().client_id) {
+        request.client_id = kafka::client_id(
+          ss::sstring(*ctx.header().client_id));
+    }
+    request.client_host = kafka::client_host(
+      fmt::format("{}", ctx.connection()->client_host()));
+    log_request(ctx.header(), request);
+
+    if (!ctx.authorized(security::acl_operation::read, request.data.group_id)) {
+        return process_result_stages::single_stage(ctx.respond(
+          join_group_response(error_code::group_authorization_failed)));
+    }
+
+    auto stages = ctx.groups().join_group(std::move(request));
+    auto res = ss::do_with(
+      std::move(ctx),
+      [f = std::move(stages.result)](request_context& ctx) mutable {
+          return f.then([&ctx](join_group_response response) {
+              return ctx.respond(std::move(response));
+          });
+      });
+
+    return process_result_stages(std::move(stages.dispatched), std::move(res));
 }
 
 } // namespace kafka
