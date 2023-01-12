a08cecc0f48bbf8e0e00b9f8c2fc3c095c1b2493
Michal Maslanka <michal@redpanda.com>
2023-01-12 14:57:17 +0100

    Revert "tests: mark unstable controller upgrade tests ok_to_fail"
    This reverts commit 23671c32f171bcc62d898f45cae523c67d32cd75.

--- tests/rptest/tests/controller_upgrade_test.py
+++ tests/rptest/tests/controller_upgrade_test.py
@@ -10,7 +10,6 @@
 import re
 
 from ducktape.utils.util import wait_until
-from ducktape.mark import ok_to_fail
 from rptest.clients.types import TopicSpec
 from rptest.clients.default import DefaultClient
 from rptest.services.admin import Admin
@@ -41,7 +40,6 @@ ALLOWED_LOGS = [
 
 
 class ControllerUpgradeTest(EndToEndTest):
-    @ok_to_fail  # https://github.com/redpanda-data/redpanda/issues/8102
     @cluster(num_nodes=7, log_allow_list=RESTART_LOG_ALLOW_LIST + ALLOWED_LOGS)
     def test_updating_cluster_when_executing_operations(self):
         '''
