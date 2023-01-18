15fd6e9c7ee35124e022f882f4925612e47d5213
Rob Blafford <rob@redpanda.com>
2023-01-18 13:44:53 -0500

    self_test: Unit tests for parsing / validation

--- src/v/cluster/self_test/tests/self_test_request_test.cc
+++ src/v/cluster/self_test/tests/self_test_request_test.cc
@@ -14,12 +14,17 @@
 
 SEASTAR_THREAD_TEST_CASE(test_make_netcheck_request) {
     static const auto check_frags =
-      [](iobuf::const_iterator begin, const iobuf::const_iterator& end) {
-          while (begin != end) {
-              if (begin->size() != 8192) {
+      [](iobuf::const_iterator begin, iobuf::const_iterator end, size_t sz) {
+          vassert(
+            std::distance(begin, end) >= 2,
+            "check frags verifies constant fragment legnth up until final "
+            "fragment in iobuf");
+          /// Its expected for the final fragment to have a size < 'sz'
+          const auto final_equiv_frag = --(--end);
+          for (; begin != final_equiv_frag; ++begin) {
+              if (begin->size() != sz) {
                   return false;
               }
-              begin++;
           }
           return true;
       };
@@ -27,20 +32,20 @@ SEASTAR_THREAD_TEST_CASE(test_make_netcheck_request) {
     auto req = cluster::make_netcheck_request(model::node_id{0}, 52).get0().buf;
     BOOST_CHECK_EQUAL(req.size_bytes(), 52);
     BOOST_CHECK_EQUAL(std::distance(req.cbegin(), req.cend()), 1);
-    BOOST_CHECK_EQUAL(req.cbegin()->size(), 52);
     req = cluster::make_netcheck_request(model::node_id{0}, 8192).get0().buf;
     BOOST_CHECK_EQUAL(req.size_bytes(), 8192);
     BOOST_CHECK_EQUAL(std::distance(req.cbegin(), req.cend()), 1);
-    BOOST_CHECK_EQUAL(req.cbegin()->size(), 8192);
     req = cluster::make_netcheck_request(model::node_id{0}, 8193).get0().buf;
     BOOST_CHECK_EQUAL(req.size_bytes(), 8193);
     BOOST_CHECK_EQUAL(std::distance(req.cbegin(), req.cend()), 2);
+    /// Verify the first of 2 fragments has the expected size of 8192 while
+    /// remainder leftover size must be in second fragment
     BOOST_CHECK_EQUAL(req.cbegin()->size(), 8192);
     BOOST_CHECK_EQUAL((++req.cbegin())->size(), 1);
     const auto size = ((8192 * 52) + 100);
     req = cluster::make_netcheck_request(model::node_id{0}, size).get0().buf;
     BOOST_CHECK_EQUAL(req.size_bytes(), size);
     BOOST_CHECK_EQUAL(std::distance(req.cbegin(), req.cend()), 53);
-    BOOST_CHECK(check_frags(req.cbegin(), (--(--req.cend()))));
+    BOOST_CHECK(check_frags(req.cbegin(), req.cend(), 8192));
     BOOST_CHECK_EQUAL((--req.cend())->size(), 100);
 }
