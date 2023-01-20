c40dd50289fea9102fc674d2d5deaab519afcb95
Vadim Plakhtinskiy <vadim@redpanda.com>
2023-01-20 14:59:04 +0100

    kafka: Handle for case if node is isolated
    If node isolated or decomissioned it can not handle kafka requests from
    client, so in this case we need to signal client comunicate with another
    broker. For this we need to exclude isolated node from brokers list and
    return -1 for controller_id, after it client will send metadata request to
    another broker and will comunicate with it
    
    Also we can not put isolated node like leader for partition.
    To prevent client stuck we gonna add fake leader to force
    client connect to another broker

--- src/v/features/feature_table.cc
+++ src/v/features/feature_table.cc
@@ -59,6 +59,8 @@ std::string_view to_string_view(feature f) {
         return "kafka_gssapi";
     case feature::partition_move_revert_cancel:
         return "partition_move_cancel_revert";
+    case feature::node_isolation:
+        return "node_isolation";
     case feature::test_alpha:
         return "__test_alpha";
     case feature::test_bravo:
