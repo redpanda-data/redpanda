def6d9c5490a85177123450105ae85a1108dd9e8
Bharath Vissapragada <bharathv@redpanda.com>
2023-01-23 23:26:54 -0800

    ducktape: Update log filter in CrashLoopChecksTest
    CDT runs into a different error log for the passed
    hostname.

--- tests/rptest/tests/crash_loop_checks_test.py
+++ tests/rptest/tests/crash_loop_checks_test.py
@@ -23,8 +23,10 @@ class CrashLoopChecksTest(RedpandaTest):
     ]
 
     # main - application.cc:348 - Failure during startup: std::__1::system_error (error C-Ares:4, unreachable_host.com: Not found)
-    HOSTNAME_ERROR = [
-        ".*Failure during startup: std::__1::system_error \(error C-Ares:4, unreachable_host.com: Not found\)"
+    # main - application.cc:363 - Failure during startup: std::__1::system_error (error C-Ares:11, unreachable_host.com: Connection refused)
+    HOSTNAME_ERRORS = [
+        ".*Failure during startup: std::__1::system_error \(error C-Ares:4, unreachable_host.com: Not found\)",
+        ".*Failure during startup: std::__1::system_error \(error C-Ares:11, unreachable_host.com: Connection refused\)"
     ]
 
     CRASH_LOOP_TRACKER_FILE = f"{RedpandaService.DATA_DIR}/startup_log"
@@ -65,7 +67,7 @@ class CrashLoopChecksTest(RedpandaTest):
         update = dict(kafka_api=dict(address="127.0.0.1", port=9099))
         self.redpanda.start_node(broker, override_cfg_params=update)
 
-    @cluster(num_nodes=1, log_allow_list=CRASH_LOOP_LOG + HOSTNAME_ERROR)
+    @cluster(num_nodes=1, log_allow_list=CRASH_LOOP_LOG + HOSTNAME_ERRORS)
     def test_crash_loop_with_misconfiguration(self):
         broker = self.redpanda.nodes[0]
         self.redpanda.signal_redpanda(broker)
