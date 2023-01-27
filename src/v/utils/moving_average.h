6f884654086874545edc20ed41b7e9b25378cf97
Evgeny Lazin <4lazin@gmail.com>
2023-01-27 10:57:55 -0500

    utils: Add timed_moving_average
    This is a moving average wich takes timestamps into account. Every
    update call accepts a timestamp. The width of the sliding window is
    defined as a time interval. There is also a resolution parameter which
    limits precision of time calculations. If the width of the sliding
    widnow is 10s and the precision is 100ms the object will use 100 buckets
    to store samples (10s / 100ms). This means that the time increments
    which are smaller than resolution won't be taken into account and that
    the memory requirements can be lowered by lowering the precision.

--- src/v/utils/moving_average.h
+++ src/v/utils/moving_average.h
@@ -10,10 +10,14 @@
  */
 #pragma once
 #include "likely.h"
+#include "named_type.h"
+#include "vassert.h"
 
 #include <array>
+#include <chrono>
 #include <iterator>
 #include <numeric>
+#include <ratio>
 
 /*
  * simple moving average with configurable sample count
@@ -44,4 +48,87 @@ private:
     T _value{0};
     size_t _idx{0};
     size_t _valid_samples{0};
+};
+
+template<typename T, typename Clock>
+class timed_moving_average {
+    /// Timestamp normalized by resolution
+    using normalized_timestamp_t
+      = named_type<size_t, struct normalized_time_tag>;
+    struct bucket {
+        T value{};
+        size_t num_samples{0};
+        normalized_timestamp_t ix;
+    };
+
+public:
+    using clock_t = Clock;
+    using duration_t = typename Clock::duration;
+    using timestamp_t = typename Clock::time_point;
+    timed_moving_average(
+      T initial_value,
+      std::chrono::nanoseconds depth,
+      std::chrono::nanoseconds resolution = std::chrono::milliseconds(100))
+      : _num_buckets(depth.count() / resolution.count())
+      , _resolution(resolution)
+      , _buckets(_num_buckets, bucket{})
+      , _end(_num_buckets) {
+        vassert(_num_buckets > 0, "Resolution is too small");
+        _buckets[index(_end)] = {
+          .value = initial_value, .num_samples = 1, .ix = _end};
+    }
+
+    // Update moving average
+    // \note time should never go back
+    void update(T v, timestamp_t ts) noexcept {
+        // NOTE: we have to add num_buckets to the value to avoid
+        // overflow.
+        auto end = normalize(ts) + normalized_timestamp_t(_num_buckets);
+        vassert(
+          end >= _end,
+          "Timestamp moved backward in time, current {}, previous {}",
+          _end,
+          end);
+        _end = end;
+        auto& bucket = _buckets[index(end)];
+        if (bucket.ix == _end) {
+            bucket.value += v;
+            bucket.num_samples += 1;
+        } else {
+            bucket.ix = _end;
+            bucket.value = v;
+            bucket.num_samples = 1;
+        }
+    }
+
+    T get() const noexcept {
+        T running_sum{};
+        size_t num_samples = 0;
+        for (size_t i = 0; i < _num_buckets; i++) {
+            auto ix = _end - normalized_timestamp_t(i);
+            auto& b = _buckets[index(ix)];
+            if (b.ix != ix) {
+                break;
+            }
+            running_sum += b.value;
+            num_samples += b.num_samples;
+        }
+        vassert(num_samples != 0, "timed_moving_average invariant broken");
+        return running_sum / num_samples;
+    }
+
+private:
+    size_t index(normalized_timestamp_t ts) const noexcept {
+        return ts() % _num_buckets;
+    }
+    normalized_timestamp_t normalize(timestamp_t ts) {
+        auto nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(
+          ts.time_since_epoch());
+        auto norm = nanos.count() / _resolution.count();
+        return normalized_timestamp_t(norm);
+    }
+    const size_t _num_buckets;
+    std::chrono::nanoseconds _resolution;
+    std::vector<bucket> _buckets;
+    normalized_timestamp_t _end;
 };
--- src/v/utils/tests/moving_average_test.cc
+++ src/v/utils/tests/moving_average_test.cc
@@ -1,7 +1,18 @@
+#include "seastarx.h"
 #include "utils/moving_average.h"
+#include "vassert.h"
+#include "vlog.h"
+
+#include <seastar/core/lowres_clock.hh>
+#include <seastar/util/log.hh>
 
 #include <boost/test/tools/old/interface.hpp>
 #include <boost/test/unit_test.hpp>
+
+#include <chrono>
+#include <ratio>
+
+using namespace std::chrono_literals;
 
 BOOST_AUTO_TEST_CASE(test_moving_average) {
     moving_average<int, 4> ma(0);
@@ -19,4 +30,61 @@ BOOST_AUTO_TEST_CASE(test_moving_average) {
     ma.update(9);
 
     BOOST_REQUIRE_EQUAL(ma.get(), 5);
+}
+
+BOOST_AUTO_TEST_CASE(test_timed_moving_average) {
+    const auto eps = 0.001;
+    constexpr auto depth = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      1s);
+    constexpr auto resolution
+      = std::chrono::duration_cast<std::chrono::nanoseconds>(100ms);
+    timed_moving_average<double, ss::lowres_clock> ma(0, depth, resolution);
+    BOOST_REQUIRE_CLOSE(ma.get(), 00, eps);
+    auto bt = ss::lowres_clock::now();
+    ma.update(1, bt + 100ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 0.5, eps);
+    ma.update(2, bt + 200ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 1.0, eps);
+    ma.update(3, bt + 300ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 1.5, eps);
+    ma.update(4, bt + 400ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 2.0, eps);
+    ma.update(5, bt + 500ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 2.5, eps);
+    ma.update(6, bt + 600ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 3.0, eps);
+    ma.update(7, bt + 700ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 3.5, eps);
+    ma.update(8, bt + 800ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 4.0, eps);
+    ma.update(9, bt + 900ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 4.5, eps);
+    // saturated, start evict elements
+    ma.update(10, bt + 1000ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 5.5, eps);
+    ma.update(11, bt + 1100ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 6.5, eps);
+    ma.update(12, bt + 1200ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 7.5, eps);
+    ma.update(13, bt + 1300ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 8.5, eps);
+    ma.update(14, bt + 1400ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 9.5, eps);
+    ma.update(15, bt + 1500ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 10.5, eps);
+    ma.update(16, bt + 1600ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 11.5, eps);
+    ma.update(17, bt + 1700ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 12.5, eps);
+    ma.update(18, bt + 1800ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 13.5, eps);
+    ma.update(19, bt + 1900ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 14.5, eps);
+    // evict everything
+    ma.update(0, bt + 5s);
+    BOOST_REQUIRE_CLOSE(ma.get(), 0, eps);
+    // check samples which are overlapping in time
+    ma.update(6, bt + 5s);
+    ma.update(3, bt + 5s + 10ms);
+    BOOST_REQUIRE_CLOSE(ma.get(), 3, eps);
 }
