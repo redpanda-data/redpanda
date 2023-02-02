a35c21e9fc3968513798feee01b761effefd1196
Andrea Barbadoro <andrea@redpanda.com>
2023-02-02 21:24:24 +0100

    comments on test

--- tests/rptest/tests/log_segment_ms_test.py
+++ tests/rptest/tests/log_segment_ms_test.py
@@ -1,4 +1,6 @@
-from ducktape.mark import matrix, parametrize, defaults
+import typing
+
+from ducktape.mark import parametrize, defaults
 from ducktape.utils.util import wait_until
 
 from rptest.clients.rpk import RpkTool
@@ -14,7 +16,6 @@ from rptest.util import segments_count
 # int(Admin(self.redpanda).get_cluster_config_schema()
 #                       ['properties']['log_segment_ms_min']['example'])
 SERVER_SEGMENT_MS = 60000
-SERVER_ROLLING_SECONDS = SERVER_SEGMENT_MS / 1000
 SERVER_HOUSEKEEPING_LOOP = 10
 
 
@@ -32,8 +33,15 @@ class SegmentMsTest(RedpandaTest):
     def _total_segments_count(self, topic_spec: TopicSpec, partition=0):
         return next(segments_count(self.redpanda, topic_spec.name, partition))
 
-    def generate_workload(self, server_cfg, topic_cfg, use_alter_cfg: bool,
-                          rolling_period, segments_to_produce):
+    def generate_workload(self, server_cfg: typing.Optional[int],
+                          topic_cfg: typing.Optional[int], use_alter_cfg: bool,
+                          segments_to_produce: int) -> tuple[int, int]:
+        """
+        Create some workload to trigger time-based segment rolling. setup cluster, create a topic with config
+        (either at creation time or with alter_config),
+        then starts a producer at 1hz of small messages, enough to generate segments_to_produce.
+        returns the number of segment before and after the workload
+        """
         self.redpanda.set_cluster_config({'log_segment_ms': server_cfg},
                                          expect_restart=False)
 
@@ -52,6 +60,11 @@ class SegmentMsTest(RedpandaTest):
             else:
                 rpk.delete_topic_config(topic.name,
                                         TopicSpec.PROPERTY_SEGMENT_MS)
+
+        # select first not None value
+        rolling_period = next(
+            (cfg for cfg in [topic_cfg, server_cfg, SERVER_SEGMENT_MS]
+             if cfg is not None))
 
         to_produce = messages_to_generate_segments(segments_to_produce,
                                                    rolling_period)
@@ -78,9 +91,9 @@ class SegmentMsTest(RedpandaTest):
                    err_msg=f"consumer failed to consume {to_produce=} msgs")
         consumer.stop()
         producer.stop()
+        stop_count = self._total_segments_count(topic)
         assert producer.num_acked == consumer.total_consumed(), \
             f"failed to preserve the messages across segment rolling"
-        stop_count = self._total_segments_count(topic)
 
         assert stop_count >= start_count
 
@@ -92,9 +105,13 @@ class SegmentMsTest(RedpandaTest):
     @parametrize(server_cfg=SERVER_SEGMENT_MS, topic_cfg=-1)
     def test_segment_not_rolling(self, server_cfg, topic_cfg,
                                  use_alter_cfg: bool):
+        """
+        Tests under several conditions that a segment will not roll,
+        either because there is no config or the topic property is set to disable.
+        The topic is created with the configuration or an alter config is issued to configure it
+        """
         start_count, stop_count = self.generate_workload(
-            server_cfg, topic_cfg, use_alter_cfg, SERVER_SEGMENT_MS * 2,
-            self.TEST_NUM_SEGMENTS)
+            server_cfg, topic_cfg, use_alter_cfg, self.TEST_NUM_SEGMENTS * 2)
         assert start_count == stop_count, f"{start_count=} != {stop_count=}"
 
     @cluster(num_nodes=3)
@@ -103,9 +120,13 @@ class SegmentMsTest(RedpandaTest):
     @parametrize(server_cfg=SERVER_SEGMENT_MS, topic_cfg=None)
     @parametrize(server_cfg=SERVER_SEGMENT_MS, topic_cfg=120000)
     def test_segment_rolling(self, server_cfg, topic_cfg, use_alter_cfg):
-        rolling_period = topic_cfg if topic_cfg is not None else server_cfg
+        """
+        Tests under several conditions that a segment will roll,
+        either for the topic config or if none, for the server config.
+        Tests also that the number of rolls is congruent (within an error margin) with the configuration.
+        The topic is created with the configuration or an alter config is issued to configure it
+        """
         start_count, stop_count = self.generate_workload(
-            server_cfg, topic_cfg, use_alter_cfg, rolling_period,
-            self.TEST_NUM_SEGMENTS)
+            server_cfg, topic_cfg, use_alter_cfg, self.TEST_NUM_SEGMENTS)
         assert abs((stop_count - start_count) - self.TEST_NUM_SEGMENTS) <= self.ERROR_MARGIN, \
                                        f"{stop_count=}-{start_count=} != {self.TEST_NUM_SEGMENTS=} +-{self.ERROR_MARGIN=}"
