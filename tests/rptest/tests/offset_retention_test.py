bc2757d150bda6324b0e6aa38b2f737d08cc452f
Noah Watkins <noahwatkins@gmail.com>
2023-02-04 15:11:54 -0800

    test: test for non-reclaimable offsets
    Updates the offset retention upgrade test to account for pre-upgrade
    offsets not being reclaimable by default. Pre-upgrade offsets that are
    updated/re-committed after the upgrade are subject to being reclaimed by
    the retention policy provided legacy support is enabled for the cluster.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- tests/rptest/tests/offset_retention_test.py
+++ tests/rptest/tests/offset_retention_test.py
@@ -23,8 +23,24 @@ class OffsetRetentionDisabledAfterUpgrade(RedpandaTest):
     When upgrading to Redpanda v23 or later offset retention should be disabled
     by default. Offset retention did not exist pre-v23, so existing clusters
     should have to opt-in after upgrade in order to avoid suprises.
+
+    When a cluster upgrades to v23 then only newly committed offsets are
+    reclaimed automatically. Offsets written prior to the upgrade must be
+    manually removed using the kafka delete offset api.
+
+    Offsets committed prior to v23 that are normally non-reclaimable become
+    reclaimable if they are updated following the upgrade.
     """
-    topics = (TopicSpec(), )
+    topics = (TopicSpec(), TopicSpec(), TopicSpec(), TopicSpec(), TopicSpec())
+
+    # pre-defined named topics used to simulate use of topics that are used only
+    # pre-v23, both pre and post upgrade, and only after upgrade to v23. the
+    # idle variants stop receiving updates prior to enabling legacy support.
+    pre_v23_topic = topics[0].name
+    prepost_v23_topic = topics[1].name
+    prepost_v23_topic_idle = topics[2].name
+    post_v23_topic = topics[3].name
+    post_v23_topic_idle = topics[4].name
 
     feature_config_timing = {
         "group_offset_retention_sec",
@@ -84,30 +100,92 @@ class OffsetRetentionDisabledAfterUpgrade(RedpandaTest):
         # after upgrade legacy support should be disabled
         assert config[self.feature_config_legacy] == False
 
-    def _offset_removal_occurred(self, period):
+    def _offset_removal_occurred(self, period, pre_upgrade,
+                                 pre_legacy_support):
         rpk = RpkTool(self.redpanda)
         group = "hey_group"
 
-        def offsets_exist():
+        def offsets_exist(include_idle):
+            """
+            Note to future ci-failure investigators. If you were to ever observe
+            a failure in which it appeared that prepost/post topic offsets were
+            not removed it could be because the following occurred:
+
+              1) the version fence was not written for some reason
+              2) so some of the offsets were incorrectly marked as legacy
+              3) something else happened like updates to commits also failing
+
+            it's unlikely that this would happen, but if it does, the proper fix
+            would be to update the test to check for this condition (may be
+            non-trivial to automate this check) and then retry the test.
+
+            in real-world scenarios if this were to happen then it is expected
+            that offset delete api is used to clean up any offsets that lose
+            this race condition that exists during an upgrade.
+            """
             desc = rpk.group_describe(group)
-            return len(desc.partitions) > 0
+            if len(desc.partitions) == 0:
+                return False
+
+            pre = any(p.topic == self.pre_v23_topic for p in desc.partitions)
+            prepost = any(p.topic == self.prepost_v23_topic
+                          for p in desc.partitions)
+            prepost_idle = any(p.topic == self.prepost_v23_topic_idle
+                               for p in desc.partitions)
+            post = any(p.topic == self.post_v23_topic for p in desc.partitions)
+            post_idle = any(p.topic == self.post_v23_topic_idle
+                            for p in desc.partitions)
+
+            assert pre, "pre-topic offsets should never be reclaimed"
+
+            # prior to upgrade, the only offsets that should exist are for the
+            # pre and prepost topics.
+            if pre_upgrade:
+                assert not post, "post-topic has not yet been written"
+                return prepost and prepost_idle
+
+            if include_idle:
+                return prepost and post and prepost_idle and post_idle
+            return prepost and post
 
         # consume from the group for twice as long as the retention period
         # setting and verify that group offsets exist for the entire time.
         start = time.time()
         while time.time() - start < (period * 2):
-            rpk.produce(self.topic, "k", "v")
-            rpk.consume(self.topic, n=1, group=group)
-            wait_until(offsets_exist, timeout_sec=1, backoff_sec=1)
+            if pre_upgrade:
+                # pre-v23
+                rpk.produce(self.pre_v23_topic, "k", "v")
+                rpk.consume(self.pre_v23_topic, n=1, group=group)
+            else:
+                # post-v23
+                rpk.produce(self.post_v23_topic, "k", "v")
+                rpk.consume(self.post_v23_topic, n=1, group=group)
+                if pre_legacy_support:
+                    rpk.produce(self.post_v23_topic_idle, "k", "v")
+                    rpk.consume(self.post_v23_topic_idle, n=1, group=group)
+
+            # pre/post-v23
+            rpk.produce(self.prepost_v23_topic, "k", "v")
+            rpk.consume(self.prepost_v23_topic, n=1, group=group)
+            if pre_legacy_support:
+                rpk.produce(self.prepost_v23_topic_idle, "k", "v")
+                rpk.consume(self.prepost_v23_topic_idle, n=1, group=group)
+
+            wait_until(lambda: offsets_exist(pre_legacy_support),
+                       timeout_sec=1,
+                       backoff_sec=1)
             time.sleep(1)
 
-        # after one half life the offset should still exist
+        # after one half life the offset should still exist. prior to legacy
+        # support being enabled include idle / be strict. after it is enabled
+        # then it's possible the idle topics get reclaimed so relax this check.
         time.sleep(period / 2)
-        assert offsets_exist()
+        assert offsets_exist(pre_legacy_support)
 
-        # after waiting for twice the retention period, it should be gone
+        # after waiting for twice the retention period, it should be gone.
+        # always be strict on this check and include idle topics.
         time.sleep(period * 2 - period / 2)
-        return not offsets_exist()
+        return not offsets_exist(True)
 
     @cluster(num_nodes=3, log_allow_list=RESTART_LOG_ALLOW_LIST)
     @parametrize(initial_version=(22, 2, 9))
@@ -123,21 +201,21 @@ class OffsetRetentionDisabledAfterUpgrade(RedpandaTest):
 
         # in old cluster offset retention should not be active
         self._validate_pre_upgrade(initial_version)
-        assert not self._offset_removal_occurred(period)
+        assert not self._offset_removal_occurred(period, True, True)
 
         # in cluster upgraded from pre-v23 retention should not be active
         self._perform_upgrade(initial_version, RedpandaInstaller.HEAD)
         rpk = RpkTool(self.redpanda)
         rpk.cluster_config_set("group_offset_retention_sec", str(period))
         rpk.cluster_config_set("group_offset_retention_check_ms", str(1000))
-        assert not self._offset_removal_occurred(period)
+        assert not self._offset_removal_occurred(period, False, True)
 
         # enable legacy. enablng legacy support takes affect at the next
         # retention check. since that is configured above to happen every second
         # then the response time should be adequate.
         rpk.cluster_config_set("legacy_group_offset_retention_enabled",
                                str(True))
-        assert self._offset_removal_occurred(period)
+        assert self._offset_removal_occurred(period, False, False)
 
 
 class OffsetRetentionTest(RedpandaTest):
