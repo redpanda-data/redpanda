55c897a0cc5a1ad138f7d275a13fb02ec85c28b9
ZeDRoman <ilgovskiy@mail.ru>
2023-02-06 16:40:02 +0300

    controller: clean up topic orphan files
    When redpanda is restarted while delete operation is not finish
    Partition files might be left on disk.
    We need to cleanup orphan partition files after bootstrap.

--- src/v/cluster/controller_backend.cc
+++ src/v/cluster/controller_backend.cc
@@ -292,6 +292,19 @@ void controller_backend::setup_metrics() {
 ss::future<> controller_backend::start() {
     setup_metrics();
     return bootstrap_controller_backend().then([this] {
+        if (ss::this_shard_id() == cluster::controller_stm_shard) {
+            auto bootstrap_revision = _topics.local().last_applied_revision();
+            ssx::spawn_with_gate(_gate, [this, bootstrap_revision] {
+                return clear_orphan_topic_files(bootstrap_revision)
+                  .handle_exception_type(
+                    [](std::filesystem::filesystem_error const& err) {
+                        vlog(
+                          clusterlog.error,
+                          "Exception while cleaning oprhan files {}",
+                          err);
+                    });
+            });
+        }
         start_topics_reconciliation_loop();
         _housekeeping_timer.set_callback([this] { housekeeping(); });
         _housekeeping_timer.arm(_housekeeping_timer_interval);
@@ -599,6 +612,70 @@ ss::future<> controller_backend::fetch_deltas() {
                     _topic_deltas[ntp].emplace_back(std::move(d));
                 }
             });
+      });
+}
+
+bool topic_files_are_orphan(
+  const model::ntp& ntp,
+  storage::partition_path::metadata ntp_directory_data,
+  ss::sharded<cluster::topic_table>& _topics,
+  model::revision_id last_applied_revision,
+  model::node_id current_node) {
+    if (ntp_directory_data.revision_id > last_applied_revision) {
+        return false;
+    }
+    auto ntp_view = model::topic_namespace_view(ntp);
+    if (!_topics.local().contains(ntp_view)) {
+        return true;
+    }
+    auto ntp_meta = _topics.local().get_topic_metadata(ntp_view);
+    if (ntp_meta && !ntp_meta->is_topic_replicable()) {
+        return false;
+    }
+    if (ntp_meta && ntp_meta->get_revision() > ntp_directory_data.revision_id) {
+        return true;
+    }
+
+    auto current_replica_set = _topics.local().get_partition_assignment(ntp);
+    if (cluster::contains_node(current_replica_set->replicas, current_node)) {
+        return false;
+    }
+
+    auto target_replica_set = _topics.local().get_target_replica_set(ntp);
+    if (
+      target_replica_set
+      && cluster::contains_node(*target_replica_set, current_node)) {
+        return false;
+    }
+
+    auto previous_replica_set = _topics.local().get_previous_replica_set(ntp);
+    if (
+      previous_replica_set
+      && cluster::contains_node(*previous_replica_set, current_node)) {
+        return false;
+    }
+    return true;
+}
+
+ss::future<> controller_backend::clear_orphan_topic_files(
+  model::revision_id bootstrap_revision) {
+    vlog(
+      clusterlog.info,
+      "Cleaning up orphan topic files. bootstrap_revision: {}",
+      bootstrap_revision);
+    // Init with default namespace to clean if there is no topics
+    absl::flat_hash_set<model::ns> namespaces = {{model::kafka_namespace}};
+    for (const auto& t : _topics.local().all_topics()) {
+        namespaces.emplace(t.ns);
+    }
+
+    return _storage.local().log_mgr().remove_orphan_files(
+      _data_directory,
+      std::move(namespaces),
+      [&, bootstrap_revision](
+        model::ntp ntp, storage::partition_path::metadata p) {
+          return topic_files_are_orphan(
+            ntp, p, _topics, bootstrap_revision, _self);
       });
 }
 
