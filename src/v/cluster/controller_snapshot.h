f683e50eccf32a90c027ea9cced7cc41063c4be4
Alexey Zatelepin <ztlpn@redpanda.com>
2023-02-16 18:00:16 +0300

    c/controller_snapshot: apply cluster_uuid from snapshot

--- src/v/cluster/controller_snapshot.h
+++ src/v/cluster/controller_snapshot.h
@@ -17,11 +17,27 @@
 
 namespace cluster {
 
+namespace controller_snapshot_parts {
+
+struct bootstrap_t
+  : public serde::
+      envelope<bootstrap_t, serde::version<0>, serde::compat_version<0>> {
+    std::optional<model::cluster_uuid> cluster_uuid;
+
+    friend bool operator==(const bootstrap_t&, const bootstrap_t&) = default;
+
+    auto serde_fields() { return std::tie(cluster_uuid); }
+};
+
+} // namespace controller_snapshot_parts
+
 struct controller_snapshot
   : public serde::checksum_envelope<
       controller_snapshot,
       serde::version<0>,
       serde::compat_version<0>> {
+    controller_snapshot_parts::bootstrap_t bootstrap;
+
     friend bool
     operator==(const controller_snapshot&, const controller_snapshot&)
       = default;
