57dec666c180e63487b3ea7e5501c65dc59f0a3f
Denis Rystsov <denis@vectorized.io>
2023-02-22 11:00:09 -0800

    cluster: keep leaders on local partition removal
    If the partition removal mode is local it means that the partition
    exists in the cluster and the current node will be receiving updates
    from the leadership changes so it will be recreated anyway. Moreover
    it might already received an update and in this case it's lost
    
    (cherry picked from commit 147e70e9c8ff9d0f628699bcea9c856b55be45fc)

--- src/v/cluster/controller_backend.cc
+++ src/v/cluster/controller_backend.cc
@@ -1213,7 +1213,11 @@ ss::future<std::error_code> controller_backend::dispatch_update_finished(
 
 ss::future<> controller_backend::finish_partition_update(
   model::ntp ntp, const partition_assignment& current, model::revision_id rev) {
-    vlog(clusterlog.trace, "[{}] processing finished update command", ntp);
+    vlog(
+      clusterlog.trace,
+      "[{}] processing finished update command, revision: {}",
+      ntp,
+      rev);
     // if there is no local replica in replica set but,
     // partition with requested ntp exists on this broker core
     // it has to be removed after new configuration is stable
@@ -1228,7 +1232,12 @@ ss::future<> controller_backend::finish_partition_update(
     if (!partition) {
         return ss::make_ready_future<>();
     }
-    vlog(clusterlog.trace, "[{}] removing partition replica", ntp);
+
+    vlog(
+      clusterlog.trace,
+      "[{}] removing partition replica, revision: {}",
+      ntp,
+      rev);
     return delete_partition(
       std::move(ntp), rev, partition_removal_mode::local_only);
 }
@@ -1564,25 +1573,21 @@ ss::future<> controller_backend::cleanup_orphan_files(
 
 ss::future<> controller_backend::delete_partition(
   model::ntp ntp, model::revision_id rev, partition_removal_mode mode) {
-    auto part = _partition_manager.local().get(ntp);
-    auto is_partition_replicated_locally = part.get() != nullptr;
-
     // The partition leaders table contains partition leaders for all
     // partitions accross the cluster. For this reason, when deleting a
     // partition (i.e. removal mode is global), we need to delete from the table
     // regardless of whether a replica of 'ntp' is present on the node.
-    if (
-      mode == partition_removal_mode::global
-      || is_partition_replicated_locally) {
+    if (mode == partition_removal_mode::global) {
         co_await _partition_leaders_table.invoke_on_all(
           [ntp, rev](partition_leaders_table& leaders) {
               leaders.remove_leader(ntp, rev);
           });
     }
 
+    auto part = _partition_manager.local().get(ntp);
     // partition is not replicated locally or it was already recreated with
     // greater rev, do nothing
-    if (!is_partition_replicated_locally || part->get_revision_id() > rev) {
+    if (part.get() == nullptr || part->get_revision_id() > rev) {
         co_return;
     }
 
