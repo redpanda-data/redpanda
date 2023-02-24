e11b9b2d7285b12e15619fc6edd9ffe47d2d75f0
Andrew Hsu <xuzuan@gmail.com>
2023-02-24 16:14:32 -0600

    redpanda_installer: update tgz url

--- tests/rptest/services/redpanda_installer.py
+++ tests/rptest/services/redpanda_installer.py
@@ -109,7 +109,7 @@ class RedpandaInstaller:
     # cluster, and that directories therein are only ever created (never
     # deleted) during the lifetime of the RedpandaInstaller.
     INSTALLER_ROOT = "/opt/redpanda_installs"
-    TGZ_URL_TEMPLATE = "https://dl.redpanda.com/qSZR7V26sJx7tCXe/redpanda/raw/names/redpanda-{arch}/versions/{version}/redpanda-{version}-{arch}.tar.gz"
+    TGZ_URL_TEMPLATE = "https://vectorized-public.s3.us-west-2.amazonaws.com/releases/redpanda/{version}/redpanda-{version}-{arch}.tar.gz"
 
     # File path to be used for locking to prevent multiple local test processes
     # from operating on the same volume mounts.
