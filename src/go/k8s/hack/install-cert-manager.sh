c0256c0b147b47d0d6c41b62939b3eb5e88cc822
Rafal Korepta <rafal@vectorized.io>
2023-03-01 11:04:18 +0100

    k8s: Move curl output to file
    The output of kuttl tests is not easy to read. The curl verbosity should be
    saved to file for later investigation if it fails. Currently it is very
    reliable.
    
    (cherry picked from commit e40f4c3ed62047145350279c56fd6a415ec72a78)

--- src/go/k8s/hack/install-cert-manager.sh
+++ src/go/k8s/hack/install-cert-manager.sh
@@ -14,10 +14,12 @@ fi
 
 mkdir -p ./bin
 
+CURL_OUTPUT="${1:-.}"
+
 if [ "$(uname)" == 'Darwin' ]; then
-  curl -Lv https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${VERSION}"/cert-manager-verifier_"${VERSION}"_Darwin_x86_64.tar.gz | tar -xvf - -C ./bin
+  curl -Lv https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${VERSION}"/cert-manager-verifier_"${VERSION}"_Darwin_x86_64.tar.gz 2>"${CURL_OUTPUT}"/cert-manager-verifier-download-output.txt | tar -xvf - -C ./bin
 else
-  curl -Lv https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${VERSION}"/cert-manager-verifier_"${VERSION}"_Linux_x86_64.tar.gz | tar -xzvf - -C ./bin
+  curl -Lv https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${VERSION}"/cert-manager-verifier_"${VERSION}"_Linux_x86_64.tar.gz 2>"${CURL_OUTPUT}"/cert-manager-verifier-download-output.txt | tar -xzvf - -C ./bin
 fi
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
