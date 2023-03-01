0e08c71c7452e9e50aaaf6db0c82d6a257f477ae
Joe Julian <me@joejulian.name>
2023-03-01 09:53:41 -0800

    operator: use newer cert-manager-verifier
    This is for local development use

--- src/go/k8s/hack/install-cert-manager.sh
+++ src/go/k8s/hack/install-cert-manager.sh
@@ -4,7 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-VERSION="0.2.0"
+CERT_MANAGER_VERSION="v1.11.0"
+CERT_MANAGER_VERIFIER_VERSION="0.3.0"
 
 # TODO: support more OS/architectures as they are needed
 if [ "$(kubectl get deploy --sort-by=.metadata.name --namespace cert-manager -o=jsonpath='{.items[*].metadata.name}')" = "cert-manager cert-manager-cainjector cert-manager-webhook" ]; then
@@ -17,10 +18,13 @@ mkdir -p ./bin
 CURL_OUTPUT="${1:-.}"
 
 if [ "$(uname)" == 'Darwin' ]; then
-  curl -Lv https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${VERSION}"/cert-manager-verifier_"${VERSION}"_Darwin_x86_64.tar.gz 2>"${CURL_OUTPUT}"/cert-manager-verifier-download-output.txt | tar -xvf - -C ./bin
+  curl -Lv https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${CERT_MANAGER_VERIFIER_VERSION}"/cert-manager-verifier_"${CERT_MANAGER_VERIFIER_VERSION}"_Darwin_x86_64.tar.gz 2>"${CURL_OUTPUT}"/cert-manager-verifier-download-output.txt | tar -xvf - -C ./bin
 else
-  curl -Lv https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${VERSION}"/cert-manager-verifier_"${VERSION}"_Linux_x86_64.tar.gz 2>"${CURL_OUTPUT}"/cert-manager-verifier-download-output.txt | tar -xzvf - -C ./bin
+  curl -Lv https://github.com/alenkacz/cert-manager-verifier/releases/download/v"${CERT_MANAGER_VERIFIER_VERSION}"/cert-manager-verifier_"${CERT_MANAGER_VERIFIER_VERSION}"_Linux_x86_64.tar.gz 2>"${CURL_OUTPUT}"/cert-manager-verifier-download-output.txt | tar -xzvf - -C ./bin
 fi
 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
+HOME=$(mktemp -d)
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install cert-manager jetstack/cert-manager --version "${CERT_MANAGER_VERSION}" --set installCRDs=true --namespace cert-manager --create-namespace
 ./bin/cm-verifier --timeout 5m
