94f2168cf73b2c76c6e4b236401de34f53466cdd
Michal Maslanka <michal@redpanda.com>
2023-03-03 13:58:47 +0100

    r/consensus: validate that the reply is received from the correct node
    If a node receiving a request have different id than the leader
    addresses we must discard the reply. Added validation of a reply node_id
    against the id of a node that the request was sent to.
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>
    (cherry picked from commit 63aa826c5a30a13e31ec02f4d2ab144ddfa630b0)

--- src/v/raft/consensus.cc
+++ src/v/raft/consensus.cc
@@ -309,6 +309,21 @@ consensus::success_reply consensus::update_follower_index(
     follower_index_metadata& idx = it->second;
     const append_entries_reply& reply = r.value();
     vlog(_ctxlog.trace, "Append entries response: {}", reply);
+
+    /**
+     * Do not update any of the follower state if a node that replied to
+     * append_entries_request is not the one that the request was addressed to.
+     */
+    if (unlikely(reply.node_id.id() != physical_node)) {
+        vlog(
+          _ctxlog.warn,
+          "Received append entries response node_id doesn't match expected "
+          "node_id (received: {}, expected: {})",
+          reply.node_id.id(),
+          node);
+        return success_reply::no;
+    }
+
     if (unlikely(reply.result == append_entries_reply::status::timeout)) {
         // ignore this response, timed out on the receiver node
         vlog(_ctxlog.trace, "Append entries request timedout at node {}", node);
