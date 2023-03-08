redpanda-data/redpanda

commit ece6f648ee5fecea6abd0f0b1bbe772e96e507d5

c/members_manager: added handling for node operations commands
Added handling of node addition, deletion and update commands to members
manager.

Signed-off-by: Michal Maslanka <michal@redpanda.com>

src/v/cluster/members_manager.cc
@@ -296,6 +296,15 @@ members_manager::apply_update(model::record_batch b) {
                 return f.then([error] { return error; });
             });
       },
+      [this, update_offset](add_node_cmd cmd) {
+          return do_apply_add_node(std::move(cmd), update_offset);
+      },
+      [this, update_offset](update_node_cfg_cmd cmd) {
+          return do_apply_update_node(std::move(cmd), update_offset);
+      },
+      [this, update_offset](remove_node_cmd cmd) {
+          return do_apply_remove_node(cmd, update_offset);
+      },
       [this](register_node_uuid_cmd cmd) {
           const auto& node_uuid = cmd.key;
           const auto& requested_node_id = cmd.value;
@@ -329,6 +338,108 @@ members_manager::apply_update(model::record_batch b) {
             clusterlog.info, "Node UUID {} has node ID {}", node_uuid, node_id);
           return ss::make_ready_future<std::error_code>(errc::success);
       });
+}
+
+ss::future<std::error_code> members_manager::do_apply_add_node(
+  add_node_cmd cmd, model::offset update_offset) {
+    vlog(
+      clusterlog.info,
+      "processing node add command - broker: {}, offset: {}",
+      cmd.value,
+      update_offset);
+    // update members table
+    auto ec = co_await dispatch_updates_to_cores(update_offset, cmd);
+    if (ec) {
+        co_return ec;
+    }
+    // update partition allocator
+    co_await _allocator.invoke_on(
+      partition_allocator::shard, [cmd](partition_allocator& allocator) {
+          allocator.upsert_allocation_node(cmd.value);
+      });
+
+    // update internode connections
+    if (
+      update_offset >= _last_connection_update_offset
+      && cmd.key != _self.id()) {
+        co_await update_broker_client(
+          _self.id(),
+          _connection_cache,
+          cmd.value.id(),
+          cmd.value.rpc_address(),
+          _rpc_tls_config);
+
+        _last_connection_update_offset = update_offset;
+    }
+
+    co_await _update_queue.push_eventually(node_update{
+      .id = cmd.value.id(),
+      .type = node_update_type::added,
+      .offset = update_offset,
+    });
+    co_return errc::success;
+}
+
+ss::future<std::error_code> members_manager::do_apply_update_node(
+  update_node_cfg_cmd cmd, model::offset update_offset) {
+    vlog(
+      clusterlog.info,
+      "processing node update command - broker: {}, offset: {}",
+      cmd.value,
+      update_offset);
+    // update members table
+    auto ec = co_await dispatch_updates_to_cores(update_offset, cmd);
+    if (ec) {
+        co_return ec;
+    }
+    // update partition allocator
+    co_await _allocator.invoke_on(
+      partition_allocator::shard, [cmd](partition_allocator& allocator) {
+          allocator.upsert_allocation_node(cmd.value);
+      });
+
+    // update internode connections
+    if (
+      update_offset >= _last_connection_update_offset
+      && cmd.key != _self.id()) {
+        co_await update_broker_client(
+          _self.id(),
+          _connection_cache,
+          cmd.value.id(),
+          cmd.value.rpc_address(),
+          _rpc_tls_config);
+
+        _last_connection_update_offset = update_offset;
+    }
+    co_return errc::success;
+}
+ss::future<std::error_code> members_manager::do_apply_remove_node(
+  remove_node_cmd cmd, model::offset update_offset) {
+    vlog(
+      clusterlog.info,
+      "processing node delete command - node: {}, offset: {}",
+      cmd.key,
+      update_offset);
+    // update members table
+    auto ec = co_await dispatch_updates_to_cores(update_offset, cmd);
+    if (ec) {
+        co_return ec;
+    }
+    // update partition allocator
+    co_await _allocator.invoke_on(
+      partition_allocator::shard, [cmd](partition_allocator& allocator) {
+          allocator.remove_allocation_node(cmd.key);
+      });
+
+    // update internode connections
+    if (
+      update_offset >= _last_connection_update_offset
+      && cmd.key != _self.id()) {
+        co_await remove_broker_client(_self.id(), _connection_cache, cmd.key);
+        _last_connection_update_offset = update_offset;
+    }
+
+    co_return errc::success;
 }
 ss::future<std::error_code>
 members_manager::apply_raft_configuration_batch(model::record_batch b) {

src/v/cluster/members_manager.h
@@ -71,7 +71,10 @@ public:
       recommission_node_cmd,
       finish_reallocations_cmd,
       maintenance_mode_cmd,
-      register_node_uuid_cmd>{};
+      register_node_uuid_cmd,
+      add_node_cmd,
+      remove_node_cmd,
+      update_node_cfg_cmd>{};
     static constexpr ss::shard_id shard = 0;
     static constexpr size_t max_updates_queue_size = 100;
 
@@ -229,6 +232,12 @@ private:
 
     ss::future<std::error_code>
       apply_raft_configuration_batch(model::record_batch);
+
+    ss::future<std::error_code> do_apply_add_node(add_node_cmd, model::offset);
+    ss::future<std::error_code>
+      do_apply_update_node(update_node_cfg_cmd, model::offset);
+    ss::future<std::error_code>
+      do_apply_remove_node(remove_node_cmd, model::offset);
 
     const std::vector<config::seed_server> _seed_servers;
     const model::broker _self;

src/v/cluster/members_table.cc
@@ -67,42 +67,57 @@ members_table::get_removed_node_metadata_ref(model::node_id id) const {
     return std::make_optional(std::cref(it->second));
 }
 
-void members_table::update_brokers(
-  model::offset version, const std::vector<model::broker>& new_brokers) {
-    _version = model::revision_id(version());
-
-    for (auto& br : new_brokers) {
-        auto it = _nodes.find(br.id());
-        if (it != _nodes.end()) {
-            // update configuration
-            it->second.broker = br;
-
-        } else {
-            _nodes.emplace(
-              br.id(),
-              node_metadata{
-                .broker = br,
-                .state = broker_state{},
-              });
-        }
-
-        _waiters.notify(br.id());
+std::error_code members_table::apply(model::offset o, add_node_cmd cmd) {
+    _version = model::revision_id(o);
+    auto it = _nodes.find(cmd.value.id());
+    if (it != _nodes.end()) {
+        return errc::invalid_node_operation;
     }
-    for (auto it = _nodes.begin(); it != _nodes.end();) {
-        auto new_it = std::find_if(
-          new_brokers.begin(),
-          new_brokers.end(),
-          [id = it->first](const model::broker& br) { return br.id() == id; });
-        if (new_it == new_brokers.end()) {
-            _removed_nodes.emplace(it->first, it->second);
-            _nodes.erase(it++);
-            continue;
-        }
-        ++it;
-    }
+    vlog(clusterlog.info, "adding node {}", cmd.value);
+    _nodes.emplace(cmd.value.id(), node_metadata{.broker = cmd.value});
+
+    _waiters.notify(cmd.value.id());
 
     notify_members_updated();
+    return errc::success;
 }
+
+std::error_code members_table::apply(model::offset o, update_node_cfg_cmd cmd) {
+    _version = model::revision_id(o);
+    auto it = _nodes.find(cmd.value.id());
+    if (it == _nodes.end()) {
+        return errc::node_does_not_exists;
+    }
+    vlog(clusterlog.info, "updating node configuration {}", cmd.value);
+    it->second.broker = std::move(cmd.value);
+
+    notify_members_updated();
+    return errc::success;
+}
+
+std::error_code members_table::apply(model::offset o, remove_node_cmd cmd) {
+    _version = model::revision_id(o);
+    auto it = _nodes.find(cmd.key);
+    if (it == _nodes.end()) {
+        return errc::node_does_not_exists;
+    }
+    if (
+      it->second.state.get_membership_state()
+      != model::membership_state::draining) {
+        return errc::invalid_node_operation;
+    }
+
+    vlog(clusterlog.info, "removing node {}", cmd.key);
+    auto handle = _nodes.extract(it);
+
+    handle.mapped().state.set_membership_state(
+      model::membership_state::removed);
+    _removed_nodes.insert(std::move(handle));
+
+    notify_members_updated();
+    return errc::success;
+}
+
 std::error_code
 members_table::apply(model::offset version, decommission_node_cmd cmd) {
     _version = model::revision_id(version());

src/v/cluster/members_table.h
@@ -51,6 +51,9 @@ public:
     std::error_code apply(model::offset, decommission_node_cmd);
     std::error_code apply(model::offset, recommission_node_cmd);
     std::error_code apply(model::offset, maintenance_mode_cmd);
+    std::error_code apply(model::offset, add_node_cmd);
+    std::error_code apply(model::offset, update_node_cfg_cmd);
+    std::error_code apply(model::offset, remove_node_cmd);
 
     model::revision_id version() const { return _version; }
 

commit 9eab8a9aa1841d982c86eb005b160bd1a1d0b859

c/members_mgr: fix buggy conn cache updates on node additions
Regression introduced by ece6f648ee5f (not in other release branches).

The check that compares whether the added node id is self is done on
a wrong class member that is always 0.

cmd.key is always 0, so the bug was manifesting as missing connections
on node with node_id 0 when new nodes are added as a part of
node_ops_fuzz. This was detected by stuck decommissions trying to move
partitions from a node with missing tcp client.

src/v/cluster/members_manager.cc
@@ -416,13 +416,14 @@ ss::future<std::error_code> members_manager::do_apply_add_node(
       });
 
     // update internode connections
+    auto target_id = cmd.value.id();
     if (
       update_offset >= _last_connection_update_offset
-      && cmd.key != _self.id()) {
+      && target_id != _self.id()) {
         co_await update_broker_client(
           _self.id(),
           _connection_cache,
-          cmd.value.id(),
+          target_id,
           cmd.value.rpc_address(),
           _rpc_tls_config);
 
@@ -457,13 +458,14 @@ ss::future<std::error_code> members_manager::do_apply_update_node(
       });
 
     // update internode connections
+    auto target_id = cmd.value.id();
     if (
       update_offset >= _last_connection_update_offset
-      && cmd.key != _self.id()) {
+      && target_id != _self.id()) {
         co_await update_broker_client(
           _self.id(),
           _connection_cache,
-          cmd.value.id(),
+          target_id,
           cmd.value.rpc_address(),
           _rpc_tls_config);
 