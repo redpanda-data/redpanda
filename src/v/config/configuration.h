fe084c8a2796cf6dd13362c9853b3a974215536d
Michael Boquard <michael@redpanda.com>
2023-03-18 15:43:46 -0400

    config: Added flag for unsafe log operation
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/config/configuration.h
+++ src/v/config/configuration.h
@@ -390,6 +390,9 @@ struct configuration final : public config_store {
     bounded_property<int64_t> node_isolation_heartbeat_timeout;
 
     property<std::chrono::seconds> controller_snapshot_max_age_sec;
+    // security controls
+    property<bool> legacy_permit_unsafe_log_operation;
+    property<std::chrono::seconds> legacy_unsafe_log_warning_interval_sec;
 
     configuration();
 
