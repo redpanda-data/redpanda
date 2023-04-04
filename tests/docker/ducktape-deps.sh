7a016378cf401fb85b4abfe72d6ec315e7e5d1c8
Michael Boquard <michael@redpanda.com>
2023-04-04 09:24:55 -0400

    tests/docker: Added building new golang client
    Signed-off-by: Michael Boquard <michael@redpanda.com>
    (cherry picked from commit a0215d6dff7b73da89bfbb07dd4557f0febb08fc)

--- tests/docker/ducktape-deps.sh
+++ tests/docker/ducktape-deps.sh
@@ -8,7 +8,11 @@ function install_java_client_deps() {
     build-essential \
     default-jdk \
     git \
-    maven
+    maven \
+    cmake \
+    curl
+
+  install_protobuf_compiler
 }
 
 function install_system_deps() {
@@ -17,9 +21,7 @@ function install_system_deps() {
     bind9-utils \
     bind9-dnsutils \
     bsdmainutils \
-    curl \
     dmidecode \
-    cmake \
     krb5-admin-server \
     krb5-kdc \
     krb5-user \
@@ -178,6 +180,18 @@ function install_go_test_clients() {
   cd /opt/redpanda-tests/go/sarama/produce_test
   go mod tidy
   go build
+
+  cd /opt/redpanda-tests/go/go-kafka-serde
+  GOPATH=${HOME}/go make clean all
+}
+
+function install_protobuf_compiler() {
+  mkdir /tmp/protobuf
+  curl -SL "https://vectorized-public.s3.amazonaws.com/dependencies/protobuf-cpp-3.21.8.tar.gz" | tar --no-same-owner -xz --strip-components=1 -C /tmp/protobuf
+  cd /tmp/protobuf
+  cmake -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=/usr
+  make -j$(nproc)
+  make install
 }
 
 $@
