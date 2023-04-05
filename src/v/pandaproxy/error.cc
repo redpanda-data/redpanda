3b8fbe483046888097d150608025ae6162cdb8b3
Michael Boquard <michael@redpanda.com>
2023-04-05 16:20:01 -0400

    pp/errors: Added serialization error
    Signed-off-by: Michael Boquard <michael@redpanda.com>
    (cherry picked from commit e098e2bf7e0b60ff4f99b7323f04cd2bd2127204)

--- src/v/pandaproxy/error.cc
+++ src/v/pandaproxy/error.cc
@@ -125,6 +125,8 @@ struct reply_error_category final : std::error_category {
             return "subject_version_soft_deleted";
         case reply_error_code::subject_version_not_deleted:
             return "subject_version_not_deleted";
+        case reply_error_code::serialization_error:
+            return "serialization_error";
         case reply_error_code::consumer_already_exists:
             return "Consumer with specified consumer ID already exists in the "
                    "specified consumer group.";
