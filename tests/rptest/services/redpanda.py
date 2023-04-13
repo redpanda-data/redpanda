9705473229376706cc996e229e4090869dfc57b9
John Spray <jcs@redpanda.com>
2023-04-13 09:51:30 +0100

    tests: add BogusTimestampTest
    (cherry picked from commit ba1b0ad59c72ea98d6e61dc3ead60c0bccdd12a1)

--- tests/rptest/services/redpanda.py
+++ tests/rptest/services/redpanda.py
@@ -2390,7 +2390,7 @@ class RedpandaService(Service):
 
         return None
 
-    def node_storage(self, node, sizes: bool = False):
+    def node_storage(self, node, sizes: bool = False) -> NodeStorage:
         """
         Retrieve a summary of storage on a node.
 
