886e1eae171862ede6879436f3ec206022d2bb59
Ben Pope <ben@redpanda.com>
2023-04-13 13:18:26 +0100

    c/tm_stm: Convert tm_snapshot to fragmented_vector
    An OOM was reported:
    ```
    seastar - Failed to allocate 18874368 bytes
    ```
    
    Backtrace:
    ```
    pthread_key_delete at ??:?
    raise at ??:?
    abort at ??:?
    seastar::memory::cpu_pages::free_large(void*) at /v/build/v_deps_build/seastar-prefix/src/seastar/src/core/memory.cc:880
     (inlined by) seastar::memory::free_large(void*) at /v/build/v_deps_build/seastar-prefix/src/seastar/src/core/memory.cc:1358
    seastar::memory::object_size(void*) at /v/build/v_deps_build/seastar-prefix/src/seastar/src/core/memory.cc:1362
     (inlined by) realloc at /v/build/v_deps_build/seastar-prefix/src/seastar/src/core/memory.cc:1939
    std::__1::vector<cluster::tm_transaction, std::__1::allocator<cluster::tm_transaction> >::push_back(cluster::tm_transaction const&) at /vectorized/llvm/bin/../include/c++/v1/vector:1529
    vector at /vectorized/llvm/bin/../include/c++/v1/vector:650
     (inlined by) cluster::tm_stm_cache::get_log_transactions() at /var/lib/buildkite-agent/builds/buildkite-amd64-builders-i-00b5d93b2049284be-1/redpanda/redpanda/src/v/cluster/tm_stm_cache.h:232
     (inlined by) cluster::tm_stm::do_take_snapshot() at /var/lib/buildkite-agent/builds/buildkite-amd64-builders-i-00b5d93b2049284be-1/redpanda/redpanda/src/v/cluster/tm_stm.cc:759
    tm_transaction at /var/lib/buildkite-agent/builds/buildkite-amd64-builders-i-00b5d93b2049284be-1/redpanda/redpanda/src/v/cluster/tm_stm_cache.h:44
     (inlined by) cluster::tm_stm::apply_snapshot(cluster::stm_snapshot_header, iobuf&&) at /var/lib/buildkite-agent/builds/buildkite-amd64-builders-i-00b5d93b2049284be-1/redpanda/redpanda/src/v/cluster/tm_stm.cc:743
    cluster::persisted_stm::do_make_snapshot() at /var/lib/buildkite-agent/builds/buildkite-amd64-builders-i-00b5d93b2049284be-1/redpanda/redpanda/src/v/cluster/persisted_stm.cc:139
    ?? ??:0
    seastar::futurize<std::__1::invoke_result<cluster::persisted_stm::make_snapshot()::$_2>::type>::type seastar::with_semaphore<seastar::named_semaphore_exception_factory, cluster::persisted_stm::make_snapshot()::$_2, std::__1::chrono::steady_clock>(seastar::basic_semaphore<seastar::named_semaphore_exception_factory, std::__1::chrono::steady_clock>&, unsigned long, cluster::persisted_stm::make_snapshot()::$_2&&) at /vectorized/include/seastar/core/semaphore.hh:741
     (inlined by) auto mutex::with<cluster::persisted_stm::make_snapshot()::$_2>(cluster::persisted_stm::make_snapshot()::$_2&&) at /var/lib/buildkite-agent/builds/buildkite-amd64-builders-i-00b5d93b2049284be-1/redpanda/redpanda/src/v/utils/mutex.h:42
     (inlined by) cluster::persisted_stm::make_snapshot() at /var/lib/buildkite-agent/builds/buildkite-amd64-builders-i-00b5d93b2049284be-1/redpanda/redpanda/src/v/cluster/persisted_stm.cc:152
    ```
    
    Convert to a `fragmented_vector` to alleviate the pressure on the allocator.
    
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/cluster/tx_gateway_frontend.h
+++ src/v/cluster/tx_gateway_frontend.h
@@ -70,7 +70,8 @@ public:
     ss::future<end_tx_reply>
       end_txn(end_tx_request, model::timeout_clock::duration);
 
-    using return_all_txs_res = result<std::vector<tm_transaction>, tx_errc>;
+    using return_all_txs_res
+      = result<fragmented_vector<tm_transaction>, tx_errc>;
     ss::future<return_all_txs_res> get_all_transactions();
     ss::future<result<tm_transaction, tx_errc>>
       describe_tx(kafka::transactional_id);
