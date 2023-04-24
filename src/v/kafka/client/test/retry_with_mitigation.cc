8ea45a679241e79cc1a55bbca95ff94b49c8f1b7
NyaliaLui <nyalia@redpanda.com>
2023-04-24 15:11:28 -0400

    k/client: rename retry_with_mitigation to utils
    (cherry picked from commit 217d25fc4ac5c72482444201154f62d1839ca464)

--- src/v/kafka/client/test/retry_with_mitigation.cc
+++ src/v/kafka/client/test/retry_with_mitigation.cc
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
-#include "kafka/client/retry_with_mitigation.h"
+#include "kafka/client/utils.h"
 
 #include <seastar/core/future.hh>
 #include <seastar/testing/thread_test_case.hh>
