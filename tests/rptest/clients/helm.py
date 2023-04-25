fe925844c9fcc0500f5187ad4efd0d5d6cdc25ca
Andrew Hsu <xuzuan@gmail.com>
2023-04-25 13:09:42 +0000

    tests: add upgrade_config_cluster to helm tool

--- tests/rptest/clients/helm.py
+++ tests/rptest/clients/helm.py
@@ -7,6 +7,7 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0
 
+import json
 import subprocess
 
 
@@ -16,11 +17,14 @@ class HelmTool:
     """
     def __init__(self, redpanda):
         self._redpanda = redpanda
+        self._release = 'redpanda'
+        self._chart = 'redpanda/redpanda'
+        self._namespace = 'redpanda'
 
     def install(self):
         cmd = [
-            'helm', 'install', 'redpanda', 'redpanda/redpanda', '--namespace',
-            'redpanda', '--create-namespace', '--set',
+            'helm', 'install', self._release, self._chart, '--namespace',
+            self._namespace, '--create-namespace', '--set',
             'external.domain=customredpandadomain.local', '--set',
             'statefulset.initContainers.setDataDirOwnership.enabled=true',
             '--wait'
@@ -34,8 +38,27 @@ class HelmTool:
 
     def uninstall(self):
         cmd = [
-            'helm', 'uninstall', '--namespace', 'redpanda', 'redpanda',
+            'helm', 'uninstall', self._release, '--namespace', self._namespace,
             '--wait'
+        ]
+        try:
+            subprocess.check_output(cmd)
+        except subprocess.CalledProcessError as e:
+            # log but ignore for now
+            self._redpanda.logger.info("helm error {}: {}".format(
+                e.returncode, e.output))
+
+    def upgrade_config_cluster(self, values: dict = {}, timeout: int = 300):
+        """
+        Changes the redpanda cluster config settings in 'values',
+        but leaves other values alone. Default timeout for helm is 5 minutes.
+        """
+
+        cmd = [
+            'helm', 'upgrade', self._release, self._chart, '--namespace',
+            self._namespace, '--wait', '--reuse-values', '--timeout',
+            '{}s'.format(timeout), '--set-json',
+            'config.cluster={}'.format(json.dumps(values))
         ]
         try:
             subprocess.check_output(cmd)
