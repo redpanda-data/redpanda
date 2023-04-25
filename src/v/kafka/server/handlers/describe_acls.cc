2b79e28d655191f48e85830224477cb7ddd2560c
Rob Blafford <rob@redpanda.com>
2023-04-25 16:14:57 -0400

    kafka/server: Call describe_acls on leader
    - Fixes: #9266

--- src/v/kafka/server/handlers/describe_acls.cc
+++ src/v/kafka/server/handlers/describe_acls.cc
@@ -10,7 +10,9 @@
  */
 #include "kafka/server/handlers/describe_acls.h"
 
+#include "cluster/topics_frontend.h"
 #include "kafka/protocol/errors.h"
+#include "kafka/server/errors.h"
 #include "kafka/server/handlers/details/security.h"
 #include "kafka/server/request_context.h"
 #include "kafka/server/response.h"
@@ -25,54 +27,39 @@
 namespace kafka {
 
 static void fill_response(
-  request_context& ctx,
-  security::acl_binding_filter& filter,
-  describe_acls_response_data& response) {
-    /*
-     * collapse common acls by pattern
-     */
-    absl::flat_hash_map<
-      security::resource_pattern,
-      std::vector<security::acl_entry>>
-      entries;
-
-    auto bindings = ctx.authorizer().acls(filter);
-
-    for (const auto& binding : bindings) {
-        entries[binding.pattern()].push_back(binding.entry());
-    }
-
+  const std::vector<cluster::acl_resource>& entries,
+  describe_acls_response_data& data) {
     for (const auto& entry : entries) {
         describe_acls_resource resource;
-
-        // resource pattern
-        resource.type = details::to_kafka_resource_type(entry.first.resource());
-        resource.name = entry.first.name();
+        resource.type = details::to_kafka_resource_type(entry.type);
+        resource.name = entry.name;
         resource.pattern_type = details::to_kafka_pattern_type(
-          entry.first.pattern());
+          entry.pattern_type);
 
         // acl entries
-        for (auto& acl : entry.second) {
+        for (const auto& acl : entry.acls) {
             // ignore ephemeral_users
             auto ephemeral_user = security::principal_type::ephemeral_user;
-            if (acl.principal().type() == ephemeral_user) {
+            if (acl.principal.type() == ephemeral_user) {
                 continue;
             }
             acl_description desc{
-              .principal = details::to_kafka_principal(acl.principal()),
-              .host = details::to_kafka_host(acl.host()),
-              .operation = details::to_kafka_operation(acl.operation()),
-              .permission_type = details::to_kafka_permission(acl.permission()),
+              .principal = details::to_kafka_principal(acl.principal),
+              .host = details::to_kafka_host(acl.host),
+              .operation = details::to_kafka_operation(acl.operation),
+              .permission_type = details::to_kafka_permission(
+                acl.permission_type),
             };
             resource.acls.push_back(std::move(desc));
         }
-        response.resources.push_back(std::move(resource));
+        data.resources.push_back(std::move(resource));
     }
 }
 
 template<>
 ss::future<response_ptr> describe_acls_handler::handle(
   request_context ctx, [[maybe_unused]] ss::smp_service_group ssg) {
+    static const auto default_describe_acls_timeout = 3s;
     describe_acls_request request;
     request.decode(ctx.reader(), ctx.header().version);
     log_request(ctx.header(), request);
@@ -88,7 +75,14 @@ ss::future<response_ptr> describe_acls_handler::handle(
 
     try {
         auto filter = details::to_acl_binding_filter(request.data);
-        fill_response(ctx, filter, data);
+        auto result = co_await ctx.security_frontend().describe_acls(
+          filter, default_describe_acls_timeout);
+        if (result.error != cluster::errc::success) {
+            vlog(klog.debug, "Error describing ACLs: {}", result.error);
+            data.error_code = map_topic_error_code(result.error);
+        } else {
+            fill_response(result.acl_resources, data);
+        }
     } catch (const details::acl_conversion_error& e) {
         vlog(klog.debug, "Error describing ACLs: {}", e.what());
         data.error_code = error_code::invalid_request;
