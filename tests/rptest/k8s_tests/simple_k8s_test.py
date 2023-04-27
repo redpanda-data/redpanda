d591998de74a046cd41c56a6db960852236e9c6a
Andrew Hsu <xuzuan@gmail.com>
2023-04-27 17:45:13 +0000

    tests: test get_node_cpu_count works on k8s

--- tests/rptest/k8s_tests/simple_k8s_test.py
+++ tests/rptest/k8s_tests/simple_k8s_test.py
@@ -29,6 +29,9 @@ class SimpleK8sTest(Test):
         node_memory = float(self.redpanda.get_node_memory_mb())
         assert node_memory > 1.0
 
+        node_cpu_count = self.redpanda.get_node_cpu_count()
+        assert node_cpu_count > 0
+
         node_disk_free = self.redpanda.get_node_disk_free()
         assert node_disk_free > 0
 
