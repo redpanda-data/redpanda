93b26ea45bb6d610d85869c4688b27346f696e8d
Vladimir Shtokman <vshtokman@gmail.com>
2023-04-28 16:30:52 -0400

    Adding the backport_on_merge script.

---
+++ .github/workflows/scripts/backport-command/backport_on_merge.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# type-branch job
+# Extract Required Backports from PR Body step
+
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR/gh_wrapper.sh"
+
+required_backport_branches=$(gh issue view $PR_NUMBER | grep "\[x\]" | sed 's/- \[x\] //')
+
+for branch in `echo $required_backport_branches`
+do
+	gh pr comment $PR_NUMBER -b "/backport $branch"
+done
