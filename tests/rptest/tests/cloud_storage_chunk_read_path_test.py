b1eae9fc5aa4e627572683c250a2ce97528429ef
Abhijat Malviya <abhijat@redpanda.com>
2023-04-28 14:53:01 +0530

    ducktape: Add test for fallback mode

--- tests/rptest/tests/cloud_storage_chunk_read_path_test.py
+++ tests/rptest/tests/cloud_storage_chunk_read_path_test.py
@@ -1,10 +1,13 @@
+import pprint
+
 from ducktape.utils.util import wait_until
 
 from rptest.clients.kafka_cli_tools import KafkaCliTools
 from rptest.clients.types import TopicSpec
 from rptest.services.cluster import cluster
 from rptest.services.kgo_verifier_services import KgoVerifierProducer, KgoVerifierSeqConsumer
-from rptest.services.redpanda import SISettings, RedpandaService
+from rptest.services.kgo_verifier_services import KgoVerifierRandomConsumer
+from rptest.services.redpanda import SISettings
 from rptest.tests.prealloc_nodes import PreallocNodesTest
 from rptest.util import Scale
 from rptest.utils.si_utils import nodes_report_cloud_segments
@@ -50,20 +53,75 @@ class CloudStorageChunkReadTest(PreallocNodesTest):
         self.redpanda.cloud_storage_client.empty_bucket(
             self.si_settings.cloud_storage_bucket)
 
-    @cluster(num_nodes=4)
-    def test_read_chunks(self):
+    def _produce_baseline(self, n_segments=20):
         producer = KgoVerifierProducer(self.test_context,
                                        self.redpanda,
                                        self.topic,
                                        msg_size=1024,
-                                       msg_count=1000 * 1000,
+                                       msg_count=1000 * 200,
                                        custom_node=self.preallocated_nodes)
         producer.start()
-        wait_until(lambda: nodes_report_cloud_segments(self.redpanda, 5),
-                   timeout_sec=120,
-                   backoff_sec=3)
+        wait_until(
+            lambda: nodes_report_cloud_segments(self.redpanda, n_segments),
+            timeout_sec=180,
+            backoff_sec=3)
         producer.stop()
         producer.wait()
+
+        return producer
+
+    def _remove_indices_from_cloud(self):
+        for obj in self.cloud_storage_client.list_objects(
+                self.si_settings.cloud_storage_bucket, self.topic):
+            if obj.key.endswith('.index'):
+                self.cloud_storage_client.delete_object(
+                    self.si_settings.cloud_storage_bucket, obj.key)
+
+    def _assert_files_in_cache(self, expr: str, must_be_absent=False):
+        """
+        Asserts that files matching a pattern are present in the cache directory on _any one_ of the started nodes.
+        If must_be_absent is True, then the files should be absent on every started node.
+        """
+        found_files = []
+        for node in self.redpanda.started_nodes():
+            files = [
+                l.strip() for l in node.account.ssh_capture(
+                    f"""find {self.redpanda.DATA_DIR}/cloud_storage_cache""")
+            ]
+            self.redpanda.logger.debug(f'files in cache: {files}')
+
+            found_files += [
+                l.strip() for l in node.account.ssh_capture(
+                    f"""find {self.redpanda.DATA_DIR}/cloud_storage_cache -regex '{expr}'"""
+                )
+            ]
+
+        if must_be_absent:
+            assert not found_files, 'unexpected files in cache dir: ' \
+                                    f'{pprint.pformat(found_files)} ' \
+                                    f'matching expression {expr}'
+        else:
+            assert found_files, f'no files in cache dir matching expression {expr}'
+
+    @cluster(num_nodes=4)
+    def test_read_chunks(self):
+        self._produce_baseline()
+
+        rand_cons = KgoVerifierRandomConsumer(self.test_context,
+                                              self.redpanda,
+                                              self.topic,
+                                              0,
+                                              100,
+                                              10,
+                                              nodes=self.preallocated_nodes)
+        rand_cons.start()
+        rand_cons.wait(timeout_sec=300)
+
+        # There should be no log files in cache
+        self._assert_files_in_cache(fr'.*kafka/{self.topic}/.*\.log\.[0-9]+$',
+                                    must_be_absent=True)
+        # There should be some chunk files in cache
+        self._assert_files_in_cache(f'.*kafka/{self.topic}/.*_chunks/[0-9]+')
 
         consumer = KgoVerifierSeqConsumer(self.test_context,
                                           self.redpanda,
@@ -72,3 +130,34 @@ class CloudStorageChunkReadTest(PreallocNodesTest):
                                           nodes=self.preallocated_nodes)
         consumer.start()
         consumer.wait(timeout_sec=120)
+        self._assert_files_in_cache(fr'.*kafka/{self.topic}/.*\.log\.[0-9]+$',
+                                    must_be_absent=True)
+
+    @cluster(num_nodes=4)
+    def test_fallback_mode(self):
+        """
+        In fallback mode, redpanda is not able to download the index from cloud storage.
+        This should result in fallback mode being engaged, which instructs the leader to
+        start downloading full segments. Additionally, the index is generated on the fly
+        during download.
+        """
+        self._produce_baseline(n_segments=5)
+        self._remove_indices_from_cloud()
+
+        consumer = KgoVerifierSeqConsumer(self.test_context,
+                                          self.redpanda,
+                                          self.topic,
+                                          0,
+                                          nodes=self.preallocated_nodes)
+        consumer.start()
+        consumer.wait(timeout_sec=60)
+
+        # There should be no chunk files in cache in fallback mode
+        self._assert_files_in_cache(f'kafka/{self.topic}/.*_chunks/[0-9]+$',
+                                    must_be_absent=True)
+
+        # There should be some log files in cache in fallback mode
+        self._assert_files_in_cache(fr'.*kafka/{self.topic}/.*\.log\.[0-9]+$')
+
+        # Index files should have been generated during full segment download.
+        self._assert_files_in_cache(f'.*kafka/{self.topic}/.*index$')
