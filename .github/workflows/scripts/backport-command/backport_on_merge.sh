4d79262a9250bf3bbef6b745fed6cac97d495e30
Vladimir Shtokman <vshtokman@gmail.com>
2023-05-02 15:48:15 -0400

    Make the github action case-insensitive, as both [x] and [X] appear to be used.

--- .github/workflows/scripts/backport-command/backport_on_merge.sh
+++ .github/workflows/scripts/backport-command/backport_on_merge.sh
@@ -6,7 +6,7 @@
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR/gh_wrapper.sh"
 
-required_backport_branches=$(gh issue view $PR_NUMBER | grep "\[x\]" | sed 's/- \[x\] //' | grep "^v")
+required_backport_branches=$(gh issue view $PR_NUMBER | grep -i "\[x\]" | sed 's/- \[x\] //i' | grep "^v")
 
 for branch in $(echo $required_backport_branches); do
   gh pr comment $PR_NUMBER -b "/backport $branch"
