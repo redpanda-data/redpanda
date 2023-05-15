d8f74b0c8c0eb95aba7e5264ec5bfcac687d2b5d
Alexey Biryukov <alexey@redpanda.com>
2023-05-15 02:08:17 -0400

    config: support for empty client_id in throughput_control_group
    Client can omit client_id specification on its side. This is different
    from empty client_id value, and for throughput_control_group it is
    also different from omitting client_id (which means match anything
    regardless of client_id). This commit introduces special selector tags for
    client_id value in throughput_control_group. The syntax for selector
    tags is "+name", this makes them distinct from any regex because regex
    cannot start with a "+".
    
    One selector name is introduced: "empty", it matches the omitted
    client_id values and only them. To support that, throughput_control_group
    does not store a regex directly anymore. Now it's a variant type
    with one option for "empty" and another for the regex.
    
    (cherry picked from commit b1fa02e1b41bc76ba57409c6106b295a67fcbdcc)

--- src/v/config/throughput_control_group.cc
+++ src/v/config/throughput_control_group.cc
@@ -13,6 +13,7 @@
 
 #include "config/convert.h"
 #include "ssx/sformat.h"
+#include "utils/functional.h"
 #include "utils/to_string.h"
 #include "utils/utf8.h"
 
@@ -21,11 +22,14 @@
 #include <fmt/core.h>
 #include <re2/re2.h>
 #include <re2/stringpiece.h>
+#include <yaml-cpp/node/node.h>
 
 #include <algorithm>
 #include <memory>
 #include <optional>
 #include <string>
+#include <type_traits>
+#include <variant>
 
 using namespace std::string_literals;
 
@@ -51,7 +55,41 @@ constexpr const char* tp_limit_node_in = "throughput_limit_node_in_bps";
 constexpr const char* tp_limit_node_out = "throughput_limit_node_out_bps";
 } // namespace ids
 
+constexpr char selector_prefix = '+';
+constexpr std::string_view selector_empty = "empty";
+
 static const ss::sstring noname(1, '\0');
+
+struct copyable_RE2 : re2::RE2 {
+    copyable_RE2(const copyable_RE2& other)
+      : RE2(other.pattern()) {}
+    copyable_RE2& operator=(const copyable_RE2&) = delete;
+    copyable_RE2(copyable_RE2&&) noexcept = delete;
+    copyable_RE2& operator=(copyable_RE2&&) noexcept = delete;
+    explicit copyable_RE2(const std::string& s)
+      : RE2(s) {}
+    friend std::ostream& operator<<(std::ostream& os, const copyable_RE2& re) {
+        fmt::print(os, "{}", re.pattern());
+        return os;
+    }
+};
+
+struct client_id_matcher_type {
+    // nullopt stands for the empty client_id value
+    std::optional<copyable_RE2> v;
+    client_id_matcher_type() = default;
+    explicit client_id_matcher_type(const copyable_RE2& d)
+      : v(d) {}
+    friend std::ostream& operator<<(
+      std::ostream& os, const std::unique_ptr<client_id_matcher_type>& mt) {
+        if (mt) {
+            fmt::print(os, "{{v: {}}}", mt->v);
+        } else {
+            fmt::print(os, "null");
+        }
+        return os;
+    }
+};
 
 throughput_control_group::throughput_control_group() = default;
 throughput_control_group::throughput_control_group(
@@ -63,10 +101,10 @@ throughput_control_group::~throughput_control_group() noexcept = default;
 throughput_control_group::throughput_control_group(
   const throughput_control_group& other)
   : name(other.name)
-  , client_id_re(
-      other.client_id_re
-        ? std::make_unique<re2::RE2>(other.client_id_re->pattern())
-        : std::unique_ptr<re2::RE2>{})
+  , client_id_matcher(
+      other.client_id_matcher
+        ? std::make_unique<client_id_matcher_type>(*other.client_id_matcher)
+        : std::unique_ptr<client_id_matcher_type>{})
   , throughput_limit_node_in_bps(other.throughput_limit_node_in_bps)
   , throughput_limit_node_out_bps(other.throughput_limit_node_out_bps) {}
 
@@ -82,20 +120,28 @@ operator<<(std::ostream& os, const throughput_control_group& tcg) {
       "{{group_name: {}, client_id: {}, throughput_limit_node_in_bps: {}, "
       "throughput_limit_node_out_bps: {}}}",
       tcg.name,
-      tcg.client_id_re ? tcg.client_id_re->pattern() : ""s,
+      tcg.client_id_matcher,
       tcg.throughput_limit_node_in_bps,
       tcg.throughput_limit_node_out_bps);
     return os;
 }
 
 bool throughput_control_group::match_client_id(
-  const ss::sstring& client_id) const {
-    if (!client_id_re) {
+  const std::optional<std::string_view> client_id) const {
+    if (!client_id_matcher) {
         // omitted match criterion means "always match"
         return true;
     }
-    return re2::RE2::FullMatch(
-      re2::StringPiece(client_id.c_str(), client_id.length()), *client_id_re);
+    if (!client_id_matcher->v) {
+        // empty client_id match
+        // only missing client_id matches the empty
+        return !client_id;
+    }
+    // regex match
+    // missing client_id never matches a re
+    return client_id
+           && re2::RE2::FullMatch(
+             re2::StringPiece(*client_id), *client_id_matcher->v);
 }
 
 bool throughput_control_group::is_noname() const noexcept {
@@ -106,10 +152,21 @@ ss::sstring throughput_control_group::validate() const {
     if (unlikely(name != noname && contains_control_characters(name))) {
         return "Group name contains invalid character";
     }
-    if (unlikely(client_id_re && !client_id_re->ok())) {
-        return ss::format("Invalid client_id regex. {}", client_id_re->error());
+    if (!client_id_matcher) {
+        // omitted value always ok
+        return {};
     }
-    return {};
+    if (!client_id_matcher->v) {
+        // empty match always ok
+        return {};
+    }
+    // regex match: check if the regex is valid
+    if (likely(client_id_matcher->v->ok())) {
+        return {};
+    } else {
+        return ss::format(
+          "Invalid client_id regex. {}", client_id_matcher->v->error());
+    }
 }
 
 } // namespace config
@@ -117,46 +174,79 @@ ss::sstring throughput_control_group::validate() const {
 namespace YAML {
 
 Node convert<config::throughput_control_group>::encode(const type& tcg) {
+    using namespace config;
     Node node;
-    if (tcg.name != config::noname) {
-        node[config::ids::name] = tcg.name;
+
+    if (tcg.name != noname) {
+        node[ids::name] = tcg.name;
     }
-    if (tcg.client_id_re) {
-        node[config::ids::client_id] = tcg.client_id_re->pattern();
+
+    if (tcg.client_id_matcher) {
+        YAML::Node client_id_node = node[ids::client_id];
+        if (!tcg.client_id_matcher->v) {
+            // the empty match
+            client_id_node = fmt::format(
+              "{}{}", selector_prefix, selector_empty);
+        } else {
+            // regex
+            client_id_node = tcg.client_id_matcher->v->pattern();
+        }
     }
+
     return node;
 }
 
 bool convert<config::throughput_control_group>::decode(
   const Node& node, type& tcg) {
-    config::throughput_control_group res;
-    if (const auto& n = node[config::ids::name]; n) {
+    using namespace config;
+    throughput_control_group res;
+
+    if (const auto& n = node[ids::name]; n) {
         res.name = n.as<ss::sstring>();
         if (contains_control_characters(res.name)) {
             return false;
         }
     } else {
-        res.name = config::noname;
+        res.name = noname;
     }
-    if (const auto& n = node[config::ids::client_id]; n) {
+
+    if (const auto& n = node[ids::client_id]; n) {
         const auto s = n.as<std::string>();
         if (contains_control_characters(s)) {
             return false;
         }
-        res.client_id_re = std::make_unique<re2::RE2>(s);
-        if (!res.client_id_re->ok()) {
-            return false;
+        if (!s.empty() && s[0] == selector_prefix) {
+            // an explicit selector
+            const std::string_view selector_name(
+              std::next(s.cbegin()), s.cend());
+            if (selector_name == selector_empty) {
+                res.client_id_matcher
+                  = std::make_unique<client_id_matcher_type>();
+            } else {
+                return false;
+            }
+        } else {
+            // a regex
+            const copyable_RE2 re{s};
+            if (!re.ok()) {
+                return false;
+            }
+            res.client_id_matcher = std::make_unique<client_id_matcher_type>(
+              re);
         }
     } else {
-        res.client_id_re.reset();
+        // nothing
+        res.client_id_matcher.reset();
     }
-    if (const auto& n = node[config::ids::tp_limit_node_in]; n) {
+
+    if (const auto& n = node[ids::tp_limit_node_in]; n) {
         // only the no-limit option is supported yet
         return false;
     } else {
         res.throughput_limit_node_in_bps = std::nullopt;
     }
-    if (const auto& n = node[config::ids::tp_limit_node_out]; n) {
+
+    if (const auto& n = node[ids::tp_limit_node_out]; n) {
         // only the no-limit option is supported yet
         return false;
     } else {
@@ -174,15 +264,25 @@ namespace json {
 void rjson_serialize(
   json::Writer<json::StringBuffer>& w,
   const config::throughput_control_group& tcg) {
+    using namespace config;
     w.StartObject();
-    if (tcg.name != config::noname) {
-        w.Key(config::ids::name);
+
+    if (tcg.name != noname) {
+        w.Key(ids::name);
         w.String(tcg.name);
     }
-    if (tcg.client_id_re) {
-        w.Key(config::ids::client_id);
-        w.String(tcg.client_id_re->pattern());
+
+    if (tcg.client_id_matcher) {
+        w.Key(ids::client_id);
+        if (!tcg.client_id_matcher->v) {
+            // empty match
+            w.String(fmt::format("{}{}", selector_prefix, selector_empty));
+        } else {
+            // regex match
+            w.String(tcg.client_id_matcher->v->pattern());
+        }
     }
+
     w.EndObject();
 }
 
