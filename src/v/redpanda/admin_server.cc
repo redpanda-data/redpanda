d96f27823d4b53ba6cc66f2d9876b1cb6f025685
Andrew Wong <awong@redpanda.com>
2023-05-15 15:30:33 -0700

    admin: interface to reset manifest
    CONFLICT:
    - surrounding debug endpoints are different in this version
    
    This adds an interface to the admin endpoint to reset the manifest to
    only include the segments represented by an input JSON.
    
    (cherry picked from commit 5c05b12de0cfe0af7341f59d47eab3138034fd45)

--- src/v/redpanda/admin_server.cc
+++ src/v/redpanda/admin_server.cc
@@ -3867,6 +3867,15 @@ void admin_server::register_debug_routes() {
         -> ss::future<ss::json::json_return_type> {
           return get_partition_state_handler(std::move(req));
       });
+
+    request_handler_fn unsafe_reset_metadata_handler = [this](
+                                                         auto req, auto reply) {
+        return unsafe_reset_metadata(std::move(req), std::move(reply));
+    };
+
+    register_route<superuser>(
+      ss::httpd::debug_json::unsafe_reset_metadata,
+      std::move(unsafe_reset_metadata_handler));
 }
 
 ss::future<ss::json::json_return_type>
@@ -4097,6 +4106,56 @@ ss::future<ss::json::json_return_type> admin_server::sync_local_state_handler(
         }
     }
     co_return ss::json::json_return_type(ss::json::json_void());
+}
+
+ss::future<std::unique_ptr<ss::reply>> admin_server::unsafe_reset_metadata(
+  std::unique_ptr<ss::request> request, std::unique_ptr<ss::reply> reply) {
+    reply->set_content_type("json");
+
+    auto ntp = parse_ntp_from_request(request->param, model::kafka_namespace);
+    if (need_redirect_to_leader(ntp, _metadata_cache)) {
+        vlog(logger.info, "Need to redirect unsafe reset metadata request");
+        throw co_await redirect_to_leader(*request, ntp);
+    }
+    if (request->content_length <= 0) {
+        throw ss::httpd::bad_request_exception("Empty request content");
+    }
+
+    ss::sstring content = request->content;
+
+    iobuf buf;
+    buf.append(request->content.data(), request->content.size());
+
+    content = {};
+
+    const auto shard = _shard_table.local().shard_for(ntp);
+    if (!shard) {
+        throw ss::httpd::not_found_exception(fmt::format(
+          "{} could not be found on the node. Perhaps it has been moved "
+          "during the redirect.",
+          ntp));
+    }
+
+    try {
+        co_await _partition_manager.invoke_on(
+          *shard,
+          [ntp = std::move(ntp), buf = std::move(buf), shard](
+            auto& pm) mutable {
+              auto partition = pm.get(ntp);
+              if (!partition) {
+                  throw ss::httpd::not_found_exception(
+                    fmt::format("Could not find {} on shard {}", ntp, *shard));
+              }
+
+              return partition->unsafe_reset_remote_partition_manifest(
+                std::move(buf));
+          });
+    } catch (const std::runtime_error& err) {
+        throw ss::httpd::server_error_exception(err.what());
+    }
+
+    reply->set_status(ss::httpd::reply::status_type::ok);
+    co_return reply;
 }
 
 ss::future<std::unique_ptr<ss::reply>>
