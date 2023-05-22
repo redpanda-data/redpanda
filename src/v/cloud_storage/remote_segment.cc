d59e641074d47ac85181cfc46db4477d13f441ae
Vlad Lazar <vlad@redpanda.com>
2023-05-22 12:39:53 +0100

    cloud_storage: tolerate expcetions in hydrate loop
    Previously, any unexpected exceptions thrown in the hydration loop
    caused it to stop, leaving the `remote_segment` in an undetermined
    state. We have seen this in the field when permissions for the cloud
    storage cache prevent writes.
    
    With this patch, these exceptions are tolerated and hydration can be
    retried.

--- src/v/cloud_storage/remote_segment.cc
+++ src/v/cloud_storage/remote_segment.cc
@@ -518,8 +518,8 @@ combine_statuses(cache_element_status segment, cache_element_status tx_range) {
 
 ss::future<> remote_segment::run_hydrate_bg() {
     ss::gate::holder guard(_gate);
-    try {
-        while (!_gate.is_closed()) {
+    while (!_gate.is_closed()) {
+        try {
             co_await _bg_cvar.wait(
               [this] { return !_wait_list.empty() || _gate.is_closed(); });
             vlog(
@@ -611,14 +611,15 @@ ss::future<> remote_segment::run_hydrate_bg() {
                 }
                 _wait_list.pop_front();
             }
+        } catch (const ss::broken_condition_variable&) {
+            vlog(_ctxlog.debug, "Hydration loop is stopped");
+            break;
+        } catch (...) {
+            vlog(
+              _ctxlog.error,
+              "Error in hydration loop: {}",
+              std::current_exception());
         }
-    } catch (const ss::broken_condition_variable&) {
-        vlog(_ctxlog.debug, "Hydraton loop is stopped");
-    } catch (...) {
-        vlog(
-          _ctxlog.error,
-          "Error in hydraton loop: {}",
-          std::current_exception());
     }
 
     _hydration_loop_running = false;
