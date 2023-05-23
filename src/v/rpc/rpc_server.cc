05c6c82a6ff5a4b40c14756bc41a1114d2d26286
Abhijat Malviya <abhijat@redpanda.com>
2023-05-23 17:59:56 +0530

    rpc: Log broken semaphore errors as timeout
    When a broken semaphore error is encountered during dispatching a
    method, it should be treated as a timeout instead of a service error.
    
    (cherry picked from commit b6ae7c4a1d541d5e6fbb4808b717535916157348)

--- src/v/rpc/rpc_server.cc
+++ src/v/rpc/rpc_server.cc
@@ -233,6 +233,10 @@ ss::future<> rpc_server::dispatch_method_once(
                           "Timing out request on abort_requested_exception "
                           "(shutting down)");
                         reply_buf.set_status(rpc::status::request_timeout);
+                    } catch (const ss::broken_semaphore& e) {
+                        rpclog.debug("Timing out request on broken_semaphore "
+                                     "(shutting down)");
+                        reply_buf.set_status(rpc::status::request_timeout);
                     } catch (...) {
                         rpclog.error(
                           "Service handler threw an exception: {}",
