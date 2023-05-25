6c2e3af78b6f2b0decf6054d55db26d46ec6952e
Rafal Korepta <rafal@vectorized.io>
2023-05-25 14:40:51 +0200

    k8s: Fix redundant imports

--- src/go/k8s/controllers/redpanda/cluster_controller_configuration_test.go
+++ src/go/k8s/controllers/redpanda/cluster_controller_configuration_test.go
@@ -20,7 +20,6 @@ import (
 	"github.com/moby/moby/pkg/namesgenerator"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/api/admin"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
