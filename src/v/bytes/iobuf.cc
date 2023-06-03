9fd64f1588c1f7e37f698c142c4621075d7eeeb6
Noah Watkins <noahwatkins@gmail.com>
2023-06-02 18:29:12 -0700

    bytes: conslidate bytes sources into iobuf
    Having a separate compilation unit for a dozen or so lines of code is
    wasteful in compilation cost and on-disk space, especially for our
    static build where on-disk object files are often >1MB when including
    seastar headers.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/bytes/iobuf.cc
+++ src/v/bytes/iobuf.cc
@@ -9,6 +9,7 @@
 
 #include "bytes/iobuf.h"
 
+#include "bytes/bytes.h"
 #include "bytes/details/io_allocation_size.h"
 #include "bytes/iostream.h"
 #include "bytes/scattered_message.h"
@@ -305,3 +306,35 @@ iobuf::placeholder iobuf::reserve(size_t sz) {
     it->reserve(sz);
     return p;
 }
+
+ss::sstring to_hex(bytes_view b) {
+    static constexpr std::string_view digits{"0123456789abcdef"};
+    ss::sstring out = ss::uninitialized_string(b.size() * 2);
+    const auto end = b.size();
+    for (size_t i = 0; i != end; ++i) {
+        uint8_t x = b[i];
+        out[2 * i] = digits[x >> uint8_t(4)];
+        out[2 * i + 1] = digits[x & uint8_t(0xf)];
+    }
+    return out;
+}
+
+ss::sstring to_hex(const bytes& b) { return to_hex(bytes_view(b)); }
+
+std::ostream& operator<<(std::ostream& os, const bytes& b) {
+    return os << bytes_view(b);
+}
+
+std::ostream& operator<<(std::ostream& os, const bytes_opt& b) {
+    if (b) {
+        return os << *b;
+    }
+    return os << "empty";
+}
+
+namespace std {
+std::ostream& operator<<(std::ostream& os, const bytes_view& b) {
+    fmt::print(os, "{{bytes:{}}}", b.size());
+    return os;
+}
+} // namespace std
