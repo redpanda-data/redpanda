7598cf351e61629603f3fbcd8dc4c25fbd049907
Rafal Korepta <rafal@vectorized.io>
2023-06-05 13:41:04 +0200

    k8s: Generate topic controller
    This code was generated using kubebuilder:
    ```
    kubebuilder version
    [Deprecation Notice] This version is deprecated.The `go/v3` cannot scaffold projects using kustomize versions v4x+ and cannot fully support Kubernetes 1.25+.It is recommended to upgrade your project to the latest versions available (go/v4).Please, check the migration guide to learn how to upgrade your project
    
    Version: main.version{KubeBuilderVersion:"3.10.0", KubernetesVendor:"1.26.1", GitCommit:"0fa57405d4a892efceec3c5a902f634277e30732", BuildDate:"2023-04-15T08:10:35Z", GoOs:"darwin", GoArch:"amd64"}
    ```
    
    ```
    kubebuilder create api --group cluster.redpanda.com --version v1alpha1 --kind Topic --namespaced --force
    [Deprecation Notice] This version is deprecated.The `go/v3` cannot scaffold projects using kustomize versions v4x+ and cannot fully support Kubernetes 1.25+.It is recommended to upgrade your project to the latest versions available (go/v4).Please, check the migration guide to learn how to upgrade your project
    
    Create Resource [y/n]
    y
    Create Controller [y/n]
    y
    Writing kustomize manifests for you to edit...
    Writing scaffold for you to edit...
    apis/cluster.redpanda.com/v1alpha1/topic_types.go
    controllers/cluster.redpanda.com/topic_controller.go
    Update dependencies:
    $ go mod tidy
    Running make:
    $ make generate
    test -s /Users/korepta/workspace/vectorized.io/redpanda/src/go/k8s/bin/controller-gen || GOBIN=/Users/korepta/workspace/vectorized.io/redpanda/src/go/k8s/bin go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1
    /Users/korepta/workspace/vectorized.io/redpanda/src/go/k8s/bin/controller-gen object:headerFile="../../../licenses/boilerplate.go.txt" paths="./..."
    Next: implement your new API and generate the manifests (e.g. CRDs,CRs) with:
    $ make manifests
    ```
    
    (cherry picked from commit 8113d71defd39ec3748eb8aa2699e31f51ce13f3)

---
+++ src/go/k8s/controllers/cluster.redpanda.com/topic_controller.go
@@ -0,0 +1,55 @@
+// Copyright 2021-2023 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package clusterredpandacom
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	clusterredpandacomv1alpha1 "github.com/redpanda-data/redpanda/src/go/k8s/apis/cluster.redpanda.com/v1alpha1"
+)
+
+// TopicReconciler reconciles a Topic object
+type TopicReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=cluster.redpanda.com,namespace=default,resources=topics,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=cluster.redpanda.com,namespace=default,resources=topics/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=cluster.redpanda.com,namespace=default,resources=topics/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Topic object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
+func (r *TopicReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *TopicReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&clusterredpandacomv1alpha1.Topic{}).
+		Complete(r)
+}
