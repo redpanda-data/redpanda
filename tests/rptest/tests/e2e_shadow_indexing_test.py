350434663cbacc71e3eac8f0c2adc8d5a9586f64
Andrew Wong <awong@redpanda.com>
2023-06-13 18:25:04 -0700

    e2e_shadow_indexing_test: bump workload
    The test could sometimes fail to upload a sufficient number of segments
    (e.g. 17k instead of ~25k). It seems to be caused by the fact that
    segment rolling will not always keep up with the produce rate, resulting
    in fewer, larger segments than would be calculated by the data and
    segment size.
    
    This commit bumps the total data size written by an order of magnitude
    -- the workload won't finish given the test stops the worklaod once a
    sufficient number of segments is reached.

--- tests/rptest/tests/e2e_shadow_indexing_test.py
+++ tests/rptest/tests/e2e_shadow_indexing_test.py
@@ -623,7 +623,7 @@ class ShadowIndexingManyPartitionsTest(PreallocNodesTest):
                                        self.redpanda,
                                        self.topic,
                                        msg_size=1024,
-                                       msg_count=1000 * 1000,
+                                       msg_count=10 * 1000 * 1000,
                                        custom_node=self.preallocated_nodes)
         producer.start()
         try:
@@ -655,7 +655,7 @@ class ShadowIndexingManyPartitionsTest(PreallocNodesTest):
                                        self.redpanda,
                                        self.topic,
                                        msg_size=1024,
-                                       msg_count=1000 * 1000,
+                                       msg_count=10 * 1000 * 1000,
                                        custom_node=self.preallocated_nodes)
         producer.start()
         try:
