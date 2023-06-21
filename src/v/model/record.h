52f6a4d540ccb28bb34c332de25066fb5951693f
Michal Maslanka <michal@redpanda.com>
2023-06-21 11:15:31 +0200

    m/record: use max timestamp in batch identity
    Max timestamp is a timestamp of last record in a record batch. To be
    consistent with Kafka and semantically correct changed usage of first to
    max timestamp in `batch_identity`. Batch identity timestamp should
    represent the timestamp of the most latest record contained in a batch.
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>
    (cherry picked from commit 76d5a63ffabb4a264af8a9aa52c18d1730e6a3a5)

--- src/v/model/record.h
+++ src/v/model/record.h
@@ -509,7 +509,7 @@ struct batch_identity {
           .last_seq = increment_sequence(
             hdr.base_sequence, hdr.last_offset_delta),
           .record_count = hdr.record_count,
-          .first_timestamp = hdr.first_timestamp,
+          .max_timestamp = hdr.max_timestamp,
           .is_transactional = hdr.attrs.is_transactional()};
     }
 
@@ -517,7 +517,7 @@ struct batch_identity {
     int32_t first_seq{0};
     int32_t last_seq{0};
     int32_t record_count;
-    timestamp first_timestamp;
+    timestamp max_timestamp;
     bool is_transactional{false};
 
     bool has_idempotent() { return pid.id >= 0; }
