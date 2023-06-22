c97f67f14fcff3371758db93490b5ad6c343d2df
Noah Watkins <noahwatkins@gmail.com>
2023-06-22 12:57:47 -0700

    rmgmt: remove space management control loop body
    The logging was a place holder. Removing the body to be replaced by the
    next commit makes the next commit significantly easier to parse.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/resource_mgmt/storage.cc
+++ src/v/resource_mgmt/storage.cc
@@ -89,60 +89,6 @@ ss::future<> disk_space_manager::run_loop() {
         if (!_enabled()) {
             continue;
         }
-
-        /*
-         * Collect cache and logs storage usage information. These accumulate
-         * across all shards (despite the local() accessor). If a failure occurs
-         * we wait rather than operate with a reduced set of information.
-         */
-        cloud_storage::cache_usage_target cache_usage_target;
-        try {
-            cache_usage_target
-              = co_await _pm->local().get_cloud_cache_disk_usage_target();
-        } catch (...) {
-            vlog(
-              rlog.info,
-              "Unable to collect cloud cache usage: {}",
-              std::current_exception());
-            continue;
-        }
-
-        storage::usage_report logs_usage;
-        try {
-            logs_usage = co_await _storage->local().disk_usage();
-        } catch (...) {
-            vlog(
-              rlog.info,
-              "Unable to collect log storage usage: {}",
-              std::current_exception());
-            continue;
-        }
-
-        vlog(
-          rlog.debug,
-          "Cloud storage cache target minimum size {} nice to have {}",
-          cache_usage_target.target_min_bytes,
-          cache_usage_target.target_bytes);
-
-        vlog(
-          rlog.debug,
-          "Log storage usage total {} - data {} index {} compaction {}",
-          logs_usage.usage.total(),
-          logs_usage.usage.data,
-          logs_usage.usage.index,
-          logs_usage.usage.compaction);
-
-        vlog(
-          rlog.debug,
-          "Log storage usage available for reclaim local {} total {}",
-          logs_usage.reclaim.retention,
-          logs_usage.reclaim.available);
-
-        vlog(
-          rlog.debug,
-          "Log storage usage target minimum size {} nice to have {}",
-          logs_usage.target.min_capacity,
-          logs_usage.target.min_capacity_wanted);
     }
 }
 
