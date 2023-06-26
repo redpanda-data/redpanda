acbdf9cf907a634596c23360fef9e3d1eff9f049
Noah Watkins <noahwatkins@gmail.com>
2023-06-26 16:36:57 -0700

    rmgmt: subscribe to data disk notifications
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/resource_mgmt/storage.cc
+++ src/v/resource_mgmt/storage.cc
@@ -51,6 +51,9 @@ ss::future<> disk_space_manager::start() {
         _cache_disk_nid = _storage_node->local().register_disk_notification(
           node::disk_type::cache,
           [this](node::disk_space_info info) { _cache_disk_info = info; });
+        _data_disk_nid = _storage_node->local().register_disk_notification(
+          node::disk_type::data,
+          [this](node::disk_space_info info) { _data_disk_info = info; });
     }
     co_return;
 }
@@ -60,6 +63,8 @@ ss::future<> disk_space_manager::stop() {
     if (ss::this_shard_id() == run_loop_core) {
         _storage_node->local().unregister_disk_notification(
           node::disk_type::cache, _cache_disk_nid);
+        _storage_node->local().unregister_disk_notification(
+          node::disk_type::data, _data_disk_nid);
     }
     _control_sem.broken();
     co_await _gate.close();
