3aa915c0db958c15ed5ffc112b71b2e9d0f5d06f
Andrew Wong <awong@redpanda.com>
2023-06-26 22:15:22 -0700

    cloud_storage: test utils for generating segments
    It has been useful in tests to generate a specific number of segments.
    This pulls out some utilities to be useful in more upcoming tests.

---
+++ src/v/cloud_storage/tests/produce_utils.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2023 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#pragma once
+
+#include "archival/ntp_archiver_service.h"
+#include "cloud_storage/remote_segment.h"
+#include "cluster/partition.h"
+#include "config/configuration.h"
+#include "kafka/server/tests/produce_consume_utils.h"
+#include "storage/disk_log_impl.h"
+
+namespace tests {
+
+class remote_segment_generator {
+public:
+    remote_segment_generator(
+      kafka::client::transport transport, cluster::partition& partition)
+      : _producer(std::move(transport))
+      , _partition(partition) {}
+
+    remote_segment_generator& num_segments(size_t n) {
+        _num_segs = n;
+        return *this;
+    }
+    remote_segment_generator& records_per_batch(size_t r) {
+        _records_per_batch = r;
+        return *this;
+    }
+    remote_segment_generator& batches_per_segment(size_t b) {
+        _batches_per_seg = b;
+        return *this;
+    }
+    kafka_produce_transport& producer() { return _producer; }
+
+    // Produces records, flushing and rolling the local log, and uploading
+    // according to the given parameters. Once the given segments are produced,
+    // the partition manifest is uploaded.
+    //
+    // The produced pattern "key<i>", "val<i>" for Kafka offset i.
+    // Expects to be the only one mutating the underlying partition state.
+    ss::future<int> produce() {
+        co_await _producer.start();
+        auto* log = dynamic_cast<storage::disk_log_impl*>(
+          _partition.log().get_impl());
+        auto& archiver = _partition.archiver().value().get();
+
+        size_t total_records = 0;
+        while (_partition.archival_meta_stm()->manifest().size() < _num_segs) {
+            for (size_t i = 0; i < _batches_per_seg; i++) {
+                std::vector<kv_t> records = kv_t::sequence(
+                  total_records, _records_per_batch);
+                total_records += _records_per_batch;
+                co_await _producer.produce_to_partition(
+                  _partition.ntp().tp.topic,
+                  _partition.ntp().tp.partition,
+                  std::move(records));
+            }
+            co_await log->flush();
+            co_await log->force_roll(ss::default_priority_class());
+            if (
+              config::shard_local_cfg()
+                .cloud_storage_disable_upload_loop_for_tests.value()
+              && !co_await archiver.sync_for_tests()) {
+                co_return -1;
+            }
+            if (
+              (co_await archiver.upload_next_candidates())
+                .non_compacted_upload_result.num_failed
+              > 0) {
+                co_return -1;
+            }
+        }
+        // Upload and flush the manifest to unpin the max collectable offset.
+        if (
+          config::shard_local_cfg()
+            .cloud_storage_disable_upload_loop_for_tests.value()
+          && !co_await archiver.sync_for_tests()) {
+            co_return -1;
+        }
+        auto manifest_res = co_await archiver.upload_manifest("test");
+        if (manifest_res != cloud_storage::upload_result::success) {
+            co_return -1;
+        }
+        co_await archiver.flush_manifest_clean_offset();
+        co_return total_records;
+    }
+
+private:
+    kafka_produce_transport _producer;
+    cluster::partition& _partition;
+    size_t _num_segs{1};
+    size_t _records_per_batch{1};
+    size_t _batches_per_seg{1};
+};
+
+} // namespace tests
