a7da373f1eef89210c3193b2f157b572c0fce804
Michal Maslanka <michal@redpanda.com>
2023-07-07 09:45:20 +0200

    r/consensus: request flush with linearizable barrier requests
    When linearizable barrier is set we want to move committed offset
    forward. In this case followers must flush their offsets to allow leader
    committing its entries.
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>
    (cherry picked from commit f936b6d5335d0e580b1e040aca711aede1b1d034)

--- src/v/raft/consensus.cc
+++ src/v/raft/consensus.cc
@@ -626,7 +626,7 @@ ss::future<result<model::offset>> consensus::linearizable_barrier() {
           meta(),
           model::make_memory_record_batch_reader(
             ss::circular_buffer<model::record_batch>{}),
-          append_entries_request::flush_after_append::no);
+          append_entries_request::flush_after_append::yes);
         auto seq = next_follower_sequence(target);
         sequences.emplace(target, seq);
 
