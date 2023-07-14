6d21c3ebb4150a2737ed219d57f494f77789e7e9
Michal Maslanka <michal@redpanda.com>
2023-07-14 10:14:46 +0200

    r/service: simplified heartbeats dispatch logic
    Previously the heartbeat dispatch used foreign pointers and eager
    conversion to append_entries_request. Replaced dispatching logic to
    defer creation of `append_entries_request` to the point where it is
    required. This way we are able to skip the foreign pointer wrapper.
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>

--- src/v/raft/service.h
+++ src/v/raft/service.h
@@ -70,46 +70,33 @@ public:
     [[gnu::always_inline]] ss::future<heartbeat_reply>
     heartbeat(heartbeat_request&& r, rpc::streaming_context&) final {
         using ret_t = std::vector<append_entries_reply>;
-        std::vector<append_entries_request> reqs;
-        reqs.reserve(r.heartbeats.size());
-        for (auto& m : r.heartbeats) {
-            append_entries_request append_req(
-              m.node_id,
-              m.target_node_id,
-              m.meta,
-              model::make_memory_record_batch_reader(
-                ss::circular_buffer<model::record_batch>{}),
-              flush_after_append::no);
-            reqs.push_back(std::move(append_req));
-        };
-
-        auto req_size = reqs.size();
-        auto groupped = group_hbeats_by_shard(std::move(reqs));
+        auto const req_sz = r.heartbeats.size();
+        auto grouped = group_hbeats_by_shard(std::move(r.heartbeats));
 
         std::vector<ss::future<std::vector<append_entries_reply>>> futures;
-        futures.reserve(groupped.shard_requests.size());
-        for (auto& [shard, req] : groupped.shard_requests) {
+        futures.reserve(grouped.shard_requests.size());
+        for (auto& [shard, req] : grouped.shard_requests) {
             // dispatch to each core in parallel
             futures.push_back(dispatch_hbeats_to_core(shard, std::move(req)));
         }
         // replies for groups that are not yet registered at this node
         std::vector<append_entries_reply> group_missing_replies;
-        group_missing_replies.reserve(groupped.group_missing_requests.size());
+        group_missing_replies.reserve(grouped.group_missing_requests.size());
         std::transform(
-          std::begin(groupped.group_missing_requests),
-          std::end(groupped.group_missing_requests),
+          std::begin(grouped.group_missing_requests),
+          std::end(grouped.group_missing_requests),
           std::back_inserter(group_missing_replies),
-          [](append_entries_request& r) {
+          [](heartbeat_metadata& r) {
               return append_entries_reply{
-                .group = r.metadata().group,
+                .group = r.meta.group,
                 .result = append_entries_reply::status::group_unavailable};
           });
 
         return ss::when_all_succeed(futures.begin(), futures.end())
-          .then([req_size, missing = std::move(group_missing_replies)](
+          .then([req_sz, missing = std::move(group_missing_replies)](
                   std::vector<ret_t> replies) mutable {
               ret_t ret;
-              ret.reserve(req_size);
+              ret.reserve(req_sz);
               // flatten responses
               for (auto& part : replies) {
                   std::move(part.begin(), part.end(), std::back_inserter(ret));
@@ -201,13 +188,11 @@ public:
 
 private:
     using consensus_ptr = seastar::lw_shared_ptr<consensus>;
-    using hbeats_t = std::vector<append_entries_request>;
-    using hbeats_ptr = ss::foreign_ptr<std::unique_ptr<hbeats_t>>;
-    using groupped_hbeats_ptr = ss::foreign_ptr<
-      std::unique_ptr<ss::chunked_fifo<append_entries_request>>>;
+
     struct shard_groupped_hbeat_requests {
-        absl::flat_hash_map<ss::shard_id, groupped_hbeats_ptr> shard_requests;
-        std::vector<append_entries_request> group_missing_requests;
+        absl::flat_hash_map<ss::shard_id, ss::chunked_fifo<heartbeat_metadata>>
+          shard_requests;
+        std::vector<heartbeat_metadata> group_missing_requests;
     };
 
     static ss::future<vote_reply> make_failed_vote_reply() {
@@ -269,11 +254,11 @@ private:
           });
     }
 
-    ss::future<std::vector<append_entries_reply>>
-    dispatch_hbeats_to_core(ss::shard_id shard, groupped_hbeats_ptr requests) {
+    ss::future<std::vector<append_entries_reply>> dispatch_hbeats_to_core(
+      ss::shard_id shard, ss::chunked_fifo<heartbeat_metadata> heartbeats) {
         return with_scheduling_group(
           get_scheduling_group(),
-          [this, shard, r = std::move(requests)]() mutable {
+          [this, shard, r = std::move(heartbeats)]() mutable {
               return _group_manager.invoke_on(
                 shard,
                 get_smp_service_group(),
@@ -283,19 +268,31 @@ private:
           });
     }
 
-    ss::future<std::vector<append_entries_reply>>
-    dispatch_hbeats_to_groups(ConsensusManager& m, groupped_hbeats_ptr reqs) {
+    static append_entries_request
+    make_append_entries_request(const heartbeat_metadata& hb) {
+        return {
+          hb.node_id,
+          hb.target_node_id,
+          hb.meta,
+          model::make_memory_record_batch_reader(
+            ss::circular_buffer<model::record_batch>{}),
+          flush_after_append::no};
+    }
+
+    ss::future<std::vector<append_entries_reply>> dispatch_hbeats_to_groups(
+      ConsensusManager& m, ss::chunked_fifo<heartbeat_metadata> reqs) {
         std::vector<ss::future<append_entries_reply>> futures;
-        futures.reserve(reqs->size());
+        futures.reserve(reqs.size());
         // dispatch requests in parallel
         auto timeout = clock_type::now() + _heartbeat_interval;
         std::transform(
-          reqs->begin(),
-          reqs->end(),
+          reqs.begin(),
+          reqs.end(),
           std::back_inserter(futures),
-          [this, &m, timeout](append_entries_request& req) mutable {
-              auto group = req.target_group();
-              auto f = dispatch_append_entries(m, std::move(req));
+          [this, &m, timeout](heartbeat_metadata& hb) mutable {
+              auto group = hb.meta.group;
+              auto f = dispatch_append_entries(
+                m, make_append_entries_request(hb));
               return ss::with_timeout(timeout, std::move(f))
                 .handle_exception_type([group](const ss::timed_out_error&) {
                     return append_entries_reply{
@@ -307,29 +304,21 @@ private:
         return ss::when_all_succeed(futures.begin(), futures.end());
     }
 
-    shard_groupped_hbeat_requests group_hbeats_by_shard(hbeats_t reqs) {
+    shard_groupped_hbeat_requests
+    group_hbeats_by_shard(std::vector<heartbeat_metadata> reqs) {
         shard_groupped_hbeat_requests ret;
 
         for (auto& r : reqs) {
-            if (unlikely(!_shard_table.contains(r.metadata().group))) {
-                ret.group_missing_requests.push_back(std::move(r));
+            if (unlikely(!_shard_table.contains(r.meta.group))) {
+                ret.group_missing_requests.push_back(r);
                 continue;
             }
 
-            auto shard = _shard_table.shard_for(r.metadata().group);
-            auto it = ret.shard_requests.find(shard);
-            if (it == ret.shard_requests.end()) {
-                auto result = ret.shard_requests.try_emplace(
-                  it,
-                  shard,
-                  ss::make_foreign(
-                    std::make_unique<
-                      ss::chunked_fifo<append_entries_request>>()));
-                it = result;
-            }
-
-            it->second->push_back(std::move(r));
+            auto shard = _shard_table.shard_for(r.meta.group);
+            auto [it, _] = ret.shard_requests.try_emplace(shard);
+            it->second.push_back(r);
         }
+
         return ret;
     }
 
