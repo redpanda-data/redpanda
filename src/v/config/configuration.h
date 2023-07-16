ba677b4838048ef95881cf458a4587997f53b425
Noah Watkins <noahwatkins@gmail.com>
2023-07-16 13:57:35 -0700

    config: add percent-based cloud cache size option
    Introduces a percent-based expression of cloud storage max size:
    
       cloud_storage_cache_size_percent
    
    along side the version expressed in number of bytes:
    
       cloud_storage_cache_size
    
    Additionally, this commit configures the upgrade behavior of the
    configuration options. On upgrade the defaults are:
    
       cloud_storage_cache_size           20_GiB
       cloud_storage_cache_size_percent   0%/nullopt
    
    And on new systems the default are:
    
       cloud_storage_cache_size           0
       cloud_storage_cache_size_percent   20%
    
    These changes mean that on upgraded systems no change in behavior or
    settings are made. But on new systems, a percentage based expression is
    the default.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>
    (cherry picked from commit ba64314b964472fba7f17b9fa4a36e6594bc0c6b)

--- src/v/config/configuration.h
+++ src/v/config/configuration.h
@@ -330,6 +330,7 @@ struct configuration final : public config_store {
 
     // Archival cache
     property<uint64_t> cloud_storage_cache_size;
+    property<std::optional<double>> cloud_storage_cache_size_percent;
     property<uint32_t> cloud_storage_cache_max_objects;
     property<std::chrono::milliseconds> cloud_storage_cache_check_interval_ms;
     property<std::optional<uint32_t>>
