432870268a73808081403139374c13b136776b54
Tyler Rockwood <rockwood@redpanda.com>
2023-07-19 16:18:51 -0500

    Initial Wasm transform SDK
    This is the initial SDK for Wasm transforms.
    
    We have an ABI that we expect from the broker, and the broker expects
    from this SDK. We mostly pass buffers back and forth across the ABI
    boundary, and in these buffers are Kafka wire format serialized records.
    
    We operate on single records at a time so that we use as little memory as
    possible within the Wasm runtime (there is very little overhead to the
    Wasm function calls).
    
    Lastly, the ABI is stubbed out on platforms other than Wasm so you can
    test with the SDK without running it in Wasm.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

---
+++ src/go/transform-sdk/abi.go
@@ -0,0 +1,66 @@
+// Copyright 2023 Redpanda Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build wasm
+
+package redpanda
+
+import (
+	"unsafe"
+)
+
+// These are the host functions that go allows for wasm functions that are imported.
+// See: https://github.com/golang/go/issues/59149
+
+// readRecordHeader reads all the data from the batch header into memory.
+//
+// Returns 0 upon success.
+//
+//go:wasmimport redpanda_transform read_batch_header
+func readRecordHeader(
+	h inputBatchHandle,
+	baseOffset unsafe.Pointer,
+	recordCount unsafe.Pointer,
+	partitionLeaderEpoch unsafe.Pointer,
+	attributes unsafe.Pointer,
+	lastOffsetDelta unsafe.Pointer,
+	baseTimestamp unsafe.Pointer,
+	maxTimestamp unsafe.Pointer,
+	producerId unsafe.Pointer,
+	producerEpoch unsafe.Pointer,
+	baseSequence unsafe.Pointer,
+) int32
+
+// readRecord reads the record specified using the handle. The format of the data
+// written into `buf` is a single record as specified by kafka protocol's serialized
+// wire format.
+//
+// See: https://kafka.apache.org/documentation/#record for more information.
+//
+// Returns the amount that was written into `buf` on success, otherwise
+// returns a negative number to indicate an error.
+//
+//go:wasmimport redpanda_transform read_record
+func readRecord(h inputRecordHandle, buf unsafe.Pointer, len int32) int32
+
+// writeRecord writes a new record by copying the data pointed to. The expected
+// format of `buf` is to be a record serialized according to the kafka protocol's
+// wire format.
+//
+// See: https://kafka.apache.org/documentation/#record for more information.
+//
+// Returns a negative number to indicate an error.
+//
+//go:wasmimport redpanda_transform write_record
+func writeRecord(buf unsafe.Pointer, len int32) int32
---
+++ src/go/transform-sdk/sdk.go
@@ -0,0 +1,91 @@
+// Copyright 2023 Redpanda Data, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package redpanda
+
+import (
+	"time"
+)
+
+// OnRecordWritten registers a callback to be fired when a record is written to the input topic.
+//
+// OnRecordWritten should be called in a package's `main` function to register the transform function that will be applied.
+func OnRecordWritten(fn OnRecordWrittenCallback) {
+	userTransformFunction = fn
+}
+
+// OnRecordWrittenCallback is a callback to transform records after a write event happens in the input topic.
+type OnRecordWrittenCallback func(e WriteEvent) ([]Record, error)
+
+var userTransformFunction OnRecordWrittenCallback = nil
+
+// WriteEvent contains information about the write that took place,
+// namely it contains the record that was written.
+type WriteEvent interface {
+	// Access the record associated with this event
+	Record() Record
+}
+
+// Look at options/builder pattern for making TransformEvent, and make it a "private" interface
+
+type writeEvent struct {
+	record Record
+}
+
+func (e *writeEvent) Record() Record {
+	return e.record
+}
+
+// Headers are optional key/value pairs that are passed along with
+// records.
+type RecordHeader struct {
+	Key   []byte
+	Value []byte
+}
+
+// Record is a record that has been written to Redpanda.
+type Record struct {
+	// Key is an optional field.
+	Key []byte
+	// Value is the blob of data that is written to Redpanda.
+	Value []byte
+	// Headers are client specified key/value pairs that are
+	// attached to a record.
+	Headers []RecordHeader
+	// Attrs is the attributes of a record.
+	//
+	// Output records should leave these unset.
+	Attrs RecordAttrs
+	// The timestamp associated with this record.
+	//
+	// For output records this can be left unset as it will
+	// always be the same value as the input record.
+	Timestamp time.Time
+	// The offset of this record in the partition.
+	//
+	// For output records this field is left unset,
+	// as it will be set by Redpanda.
+	Offset int64
+}
+
+type RecordAttrs struct {
+	attr uint8
+}
+
+func (a RecordAttrs) TimestampType() int8 {
+	if a.attr&0b1000_0000 != 0 {
+		return -1
+	}
+	return int8(a.attr&0b0000_1000) >> 3
+}
