a21e6865ec05404ce4ae17f8aa9e3b88a51d37b5
Tyler Rockwood <rockwood@redpanda.com>
2023-07-20 10:47:52 -0500

    ducktape: Make compute_storage.py robust to directories being deleted
    In
    https://buildkite.com/redpanda/redpanda/builds/33545#01896f0b-4206-4688-b578-4321e0069ea1
    there was a failure due to a directory being deleted so the `listdir`
    method threw. This fixes that by returning an empty list for those
    case where the directory has been deleted by the time we got to listing
    it.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- tests/rptest/remote_scripts/compute_storage.py
+++ tests/rptest/remote_scripts/compute_storage.py
@@ -7,23 +7,34 @@ Useful in tests if you want to know what files exist on a node or if they are a 
 from pathlib import Path
 import sys
 import json
+from collections.abc import Iterable
+
+
+def safe_listdir(p: Path) -> Iterable[Path]:
+    """
+    It's valid for directories to be deleted at any time, 
+    in that case that the directory is missing, just return
+    that there are no files.
+    """
+    try:
+        return p.iterdir()
+    except FileNotFoundError:
+        return []
 
 
 def compute_size(data_dir: Path, sizes: bool):
     output = {}
-    for ns in data_dir.iterdir():
+    for ns in safe_listdir(data_dir):
         if not ns.is_dir():
-            continue
-        if ns.name == ".coprocessor_offset_checkpoints":
             continue
         if ns.name == "cloud_storage_cache":
             continue
         ns_output = {}
-        for topic in ns.iterdir():
+        for topic in safe_listdir(ns):
             topic_output = {}
-            for partition in topic.iterdir():
+            for partition in safe_listdir(topic):
                 part_output = {}
-                for segment in partition.iterdir():
+                for segment in safe_listdir(partition):
                     seg_output = {}
                     if sizes:
                         try:
