dbfccd640c8a8b5ad33b00aa74c1204d7e0aab4b
Rob Blafford <rob@redpanda.com>
2023-07-24 14:14:12 -0400

    rptest: Update ducktape tests for usage_manager
    (cherry picked from commit 3311ee7f88d048dd50abc41975ad95c59ba4f466)

--- tests/rptest/tests/usage_test.py
+++ tests/rptest/tests/usage_test.py
@@ -9,6 +9,7 @@
 
 import time
 import random
+import operator
 from rptest.clients.rpk import RpkTool
 from requests.exceptions import HTTPError
 from rptest.services.cluster import cluster
@@ -27,6 +28,76 @@ from rptest.clients.types import TopicSpec
 from rptest.utils.functional import flat_map
 
 
+class UsageWindow:
+    def __init__(self, begin, end, is_open, bytes_sent, bytes_received,
+                 bytes_in_cloud_storage):
+        self.begin = begin
+        self.end = end
+        self.is_open = is_open
+        self.bytes_sent = bytes_sent
+        self.bytes_received = bytes_received
+        self.bytes_in_cloud_storage = bytes_in_cloud_storage
+
+    @property
+    def is_closed(self):
+        return not self.is_open
+
+    def __repr__(self):
+        r = {
+            'begin': self.begin,
+            'end': self.end,
+            'is_open': self.is_open,
+            'bytes_sent': self.bytes_sent,
+            'bytes_received': self.bytes_received,
+            'bytes_in_cloud_storage': self.bytes_in_cloud_storage
+        }
+        return str(r)
+
+
+def parse_usage_response(usage_response):
+    def parse_usage_window(e):
+        return UsageWindow(e['begin_timestamp'], e['end_timestamp'], e['open'],
+                           e['kafka_bytes_sent_count'],
+                           e['kafka_bytes_received_count'],
+                           e['cloud_storage_bytes_gauge'])
+
+    return [parse_usage_window(e) for e in usage_response]
+
+
+def assert_usage_consistency(node, a, b):
+    """
+    This method returns true if windows with matching timestamps contain the same values
+
+    node: String (For debugging if assertions fires)
+    a: List[UsageWindow]
+    b: List[UsageWindow]
+
+    Windows from list b are assumed to be from a newer response, so this method will verify
+    that all items from b, if they exist in a, are equivalent.
+
+    One excepton if the window is considered an 'open' window, there would be at max 1
+    per request, if there is a matching open window in both inputs, their values
+    are checked so that the value of b's open window would be >= the value in a's.
+    """
+    def group_windows(ws):
+        return {(x.begin, x.end): x for x in ws}
+
+    def compare(x, y, fn):
+        return fn(x.bytes_sent, y.bytes_sent) and fn(
+            x.bytes_received, y.bytes_received) and fn(
+                x.bytes_in_cloud_storage, y.bytes_in_cloud_storage)
+
+    # Windows from group 'a' are expected to be from a previous request
+    grp_a = group_windows(a)
+    for x in b:
+        key = (x.begin, x.end)
+        a_value = grp_a.get(key, None)
+        if a_value is not None:
+            fn = operator.le if a_value.is_open else operator.eq
+            assert compare(a_value, x,
+                           fn), f"node: {node} a: {str(a_value)} b:{str(x)}"
+
+
 class UsageTest(RedpandaTest):
     """
     Tests that the usage endpoint is tracking kafka metrics
@@ -34,49 +105,152 @@ class UsageTest(RedpandaTest):
     topics = (TopicSpec(), )
 
     def __init__(self, test_context):
-        extra_conf = {
-            'enable_usage': True,
-            'usage_num_windows': 30,
-            'usage_window_width_interval_sec': 1,
-            'usage_disk_persistance_interval_sec': 5
-        }
+        self._settings = self._usage_conf(enable_usage=True,
+                                          num_windows=30,
+                                          window_interval=3,
+                                          disk_write_interval=5)
         super(UsageTest, self).__init__(test_context=test_context,
-                                        extra_rp_conf=extra_conf)
+                                        extra_rp_conf=self._settings)
         self._ctx = test_context
         self._admin = Admin(self.redpanda)
+        # Dict[NodeName, v1/usage_response] cached history of responses
+        # used to validate future usage requests against historical ones to
+        # detect for any inconsistencies
+        self._previous_response = {}
 
-    def _get_all_usage(self, include_open=True):
+    @property
+    def usage_enabled(self):
+        return self._settings.get('enable_usage', False)
+
+    @property
+    def num_windows(self):
+        return self._settings.get('usage_num_windows', 0)
+
+    @property
+    def window_interval(self):
+        return self._settings.get('usage_window_width_interval_sec', 0)
+
+    @property
+    def disk_write_interval(self):
+        return self._settings.get('usage_disk_persistance_interval_sec', 0)
+
+    def _usage_conf(self,
+                    enable_usage=None,
+                    num_windows=None,
+                    window_interval=None,
+                    disk_write_interval=None):
+        changes = {}
+        if enable_usage is not None:
+            changes['enable_usage'] = enable_usage
+        if num_windows is not None:
+            changes['usage_num_windows'] = num_windows
+        if window_interval is not None:
+            changes['usage_window_width_interval_sec'] = window_interval
+        if disk_write_interval is not None:
+            changes[
+                'usage_disk_persistance_interval_sec'] = disk_write_interval
+
+        return changes
+
+    def _modify_settings(self,
+                         enable_usage=None,
+                         num_windows=None,
+                         window_interval=None,
+                         disk_write_interval=None):
+        conf = self._usage_conf(enable_usage=enable_usage,
+                                num_windows=num_windows,
+                                window_interval=window_interval,
+                                disk_write_interval=disk_write_interval)
+        self._admin.patch_cluster_config(upsert=conf)
+        self._settings = self._settings | conf
+        return conf
+
+    def _get_usage(self, node, include_open, retries=3):
+        exc = None
+        while retries > 0:
+            try:
+                response = self._admin.get_usage(node, include_open)
+                if response == []:
+                    raise RuntimeError("Empty response received")
+                return response
+            except Exception as e:
+                self.redpanda.logger.error(
+                    f"Error making v1/usage request: {e}")
+                retries -= 1
+                time.sleep(1)
+                exc = e
+        raise exc
+
+    def _get_all_usage(self,
+                       include_open=True,
+                       allow_gaps=False,
+                       only_nodes=None):
         """
         Performs an additional check that results are correctly ordered
-        """
-        def validate(node_response):
-            prev_begin = datetime.now()
-            prev_end = datetime.now()
-            for e in node_response:
-                begin = datetime.fromtimestamp(e['begin_timestamp'])
-                if e['open'] is True:
-                    # Open windows have a value of 0 for end timestamp
-                    prev_begin = begin
-                    continue
-                end = datetime.fromtimestamp(e['end_timestamp'])
-                assert begin < end, f"Begin: {begin}, End: {end}"
-                assert begin < prev_begin, f"Begin: {begin}, PrevBegin: {prev_begin}"
-                assert end < prev_end, f"End: {end}, PrevEnd: {prev_end}"
-                prev_begin = begin
-                prev_end = end
 
+        include_open: Make usage requests with the include_open_window option
+        allow_gaps: Assert all closed windows end timestamps match with proceeding begin
+        only_nodes: Make request to a subset of rp nodes
+        """
+        def assert_history(node, node_response):
+            # Ensure no historical results were lost or modified
+            prev = self._previous_response.get(node, None)
+            if prev is not None:
+                assert_usage_consistency(node, prev, node_response)
+            self._previous_response[node] = node_response
+
+        def validate_ts(ts):
+            # All timestamps must be a multiple of the interval in seconds
+            return ts % self.window_interval == 0
+
+        def validate_no_duplicates(response):
+            # Ensure no two entires contain matching begin & end timestamps
+            to_tss = [(x.begin, x.end) for x in response]
+            return len(to_tss) == len(set(to_tss))
+
+        def validate(node, node_response):
+            # Ensure all windows in in chronological order with end timestamp
+            # matching next beginning
+            node_response = parse_usage_response(node_response)
+            assert validate_no_duplicates(node_response), f"{node_response}"
+            prev_begin = float('inf')
+            prev_end = float('inf')
+            for e in node_response:
+                assert validate_ts(e.begin), e.begin
+                if e.is_open is True:
+                    # Open windows have a value of time::now() for end
+                    prev_begin = e.begin
+                    continue
+                assert validate_ts(e.end), e.end
+                # A window may be dropped which would mean this difference
+                # being a multiple of the interval but not less than it
+                assert e.end - e.begin >= self.window_interval, f"Expected interval: {self.window_interval} observed {e.end - e.begin}"
+                assert e.begin < e.end, f"Begin: {e.begin}, End: {e.end}"
+                assert e.begin < prev_begin, f"Begin: {e.begin}, PrevBegin: {prev_begin}"
+                assert e.end < prev_end, f"End: {e.end}, PrevEnd: {prev_end}"
+                if prev_begin != float('inf'):
+                    if not allow_gaps:
+                        assert e.end == prev_begin, f"End: {e.end} PrevEnd: {prev_begin}"
+                    else:
+                        assert e.end <= prev_begin, f"End: {e.end}, PrevBegin: {prev_begin}"
+                prev_begin = e.begin
+                prev_end = e.end
+
+            assert_history(node, node_response)
             return node_response
 
-        # validate() checks results are ordered newest to oldest
+        # validate() checks results are ordered newest to oldest and the timestamps
+        # are multiples of the window interval
+        nodes = self.redpanda.nodes if only_nodes is None else only_nodes
         return flat_map(
-            lambda x: validate(self._admin.get_usage(x, include_open)),
-            self.redpanda.nodes)
+            lambda x: validate(x.name, self._get_usage(x, include_open)),
+            nodes)
 
     def _calculate_total_usage(self, results=None):
         # Total number of ingress/egress bytes across entire cluster
         def all_bytes(x):
-            kafka_ingress = x['kafka_bytes_sent_count']
-            kafka_egress = x['kafka_bytes_received_count']
+            kafka_ingress = x.bytes_sent
+            kafka_egress = x.bytes_received
             return kafka_ingress + kafka_egress
 
         if results is None:
@@ -121,7 +295,7 @@ class UsageTest(RedpandaTest):
         iterations = 1
         prev_usage = self._get_all_usage()
         producer = KafkaCliTools(self.redpanda)
-        while iterations < 37:
+        while iterations < (self.num_windows + 7):
             producer.produce(self.topic, (512 * iterations), 512, acks=1)
             time.sleep(1)
 
@@ -137,43 +311,44 @@ class UsageTest(RedpandaTest):
                 # and the initial non 0 recorded data are also included in the total recorded amt
                 total_data = self._calculate_total_usage(usage)
                 total_prev = self._calculate_total_usage(prev_usage)
-                assert total_data > total_prev, f"Expected {total_data} > {total_prev} itr: {iterations}"
+                assert total_data > 0, "no data observed"
+                assert total_data >= total_prev, f"Expected {total_data} >= {total_prev} itr: {iterations}"
 
             prev_usage = usage
             iterations += 1
 
     @cluster(num_nodes=4)
     def test_usage_collection_restart(self):
-        self._admin.patch_cluster_config(
-            upsert={'usage_disk_persistance_interval_sec': 1})
-        # Ensure the restarted accounting fiber is up before data begins to be produced
-        time.sleep(2)
-
         # Produce / consume test data, should observe usage numbers increase
         _ = self._produce_and_consume_data()
         time.sleep(3)
 
         # Query usage of node to restart before restart
         usage_pre_restart = self._calculate_total_usage(
-            self._admin.get_usage(node=self.redpanda.nodes[0]))
+            self._get_all_usage(include_open=True,
+                                allow_gaps=True,
+                                only_nodes=[self.redpanda.nodes[0]]))
 
         self.redpanda.restart_nodes([self.redpanda.nodes[0]])
 
         # Compare values pre/post restart to ensure data was persisted to disk
         usage_post_restart = self._calculate_total_usage(
-            self._admin.get_usage(node=self.redpanda.nodes[0]))
+            self._get_all_usage(include_open=True,
+                                allow_gaps=True,
+                                only_nodes=[self.redpanda.nodes[0]]))
         assert usage_post_restart >= usage_pre_restart, f"Usage post restart: {usage_post_restart} Usage pre restart: {usage_pre_restart}"
 
     @cluster(num_nodes=3)
     def test_usage_settings_changed(self):
-        # Should expect maximum of 2 windows per broker
-        self._admin.patch_cluster_config(upsert={'usage_num_windows': 2})
-        time.sleep(2)
+        # Should expect maximum of 2 windows per broker (including open)
+        self._modify_settings(num_windows=2)
+        # Wait 3 intervals
+        time.sleep(self.window_interval * 3)
         response = self._get_all_usage()
-        assert len(response) == 6
+        assert len(response) == 6, f"{len(response)}"
 
         # Should expect a 500 from the cluster
-        self._admin.patch_cluster_config(upsert={'enable_usage': False})
+        self._modify_settings(enable_usage=False)
         try:
             _ = self._get_all_usage()
             assert False, "Expecting v1/usage to return 400"
@@ -181,19 +356,15 @@ class UsageTest(RedpandaTest):
             assert e.response.status_code == 400
 
         # Should expect windows to have been resized correctly
-        self._admin.patch_cluster_config(
-            upsert={
-                'enable_usage': True,
-                'usage_num_windows': 3,
-                'usage_window_width_interval_sec': 2
-            })
-        time.sleep(3)
-        response = self._get_all_usage(False)
+        self._modify_settings(enable_usage=True,
+                              num_windows=3,
+                              window_interval=2,
+                              disk_write_interval=5)
+        time.sleep(self.window_interval * 5)
+        response = self._get_all_usage(include_open=False, allow_gaps=True)
         for r in response:
-            begin = datetime.fromtimestamp(r['begin_timestamp'])
-            end = datetime.fromtimestamp(r['end_timestamp'])
-            total = end - begin
-            assert total.seconds == 2 or total.seconds == 3, total.seconds
+            if r.is_closed:
+                assert (r.end - r.begin) == self.window_interval
 
 
 class UsageTestCloudStorageMetrics(RedpandaTest):
@@ -279,10 +450,11 @@ class UsageTestCloudStorageMetrics(RedpandaTest):
         def check_usage():
             # Check that the usage reporting system has reported correct values
             manifest_usage = self.admin.cloud_storage_usage()
-            reported_usage = self.admin.get_usage(
-                random.choice(self.redpanda.nodes))
+            # Only active leader will have latest value of cs_bytes
+            reported_usages = flat_map(lambda node: self.admin.get_usage(node),
+                                       self.redpanda.nodes)
             reported_usages = [
-                x['cloud_storage_bytes_gauge'] for x in reported_usage
+                x['cloud_storage_bytes_gauge'] for x in reported_usages
             ]
 
             self.logger.info(
