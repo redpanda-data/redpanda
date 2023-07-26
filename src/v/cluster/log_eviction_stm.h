b17e4800cd6989809642844c92790cdd245d9cd9
Michal Maslanka <michal@redpanda.com>
2023-07-26 17:11:57 +0200

    c/log_eviction_stm: refactored log eviction stm to use queue
    Log eviction STM previously was using a condition variable and raft
    offset monitor to synchronize processing of two different eviction
    events types i.e. storage driven eviction and delete record related
    eviction. The coordination between the fiber executing eviction and the
    the one requesting it was complicated. Changed the coordination logic to
    use a queue where all the eviction requests are enqueued and the fibre
    simply executes them in sequence.
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>
    (cherry picked from commit 9fa0752c8f75a2258ff8eadda14dc19745963ec0)

--- src/v/cluster/log_eviction_stm.h
+++ src/v/cluster/log_eviction_stm.h
@@ -21,6 +21,7 @@
 
 #include <seastar/core/abort_source.hh>
 #include <seastar/core/gate.hh>
+#include <seastar/core/queue.hh>
 #include <seastar/util/log.hh>
 
 namespace cluster {
@@ -124,14 +125,36 @@ private:
       std::optional<std::reference_wrapper<ss::abort_source>> as);
 
 private:
+    using retry_event_until_success = ss::bool_class<struct wait_for_tag>;
+    /**
+     * Eviction event represents an eviction request coming either from storage
+     * offset monitor notification or the delete records special batch.
+     *
+     * The event stores the trucation point offset and information if truncation
+     * related should be retried. It may be the case that the truncation
+     * may not yet be executed as max_collectible offsets of the partition
+     * related STMs hasn't advanced yet.
+     */
+    struct eviction_event {
+        model::offset prefix_truncate_offset;
+        retry_event_until_success wait_for_success;
+    };
+
+    ss::future<>
+      enqueue_eviction_event(model::offset, retry_event_until_success);
+
+    void maybe_pop_queue();
+
     ss::abort_source& _as;
     model::offset _storage_eviction_offset;
     model::offset _delete_records_eviction_offset;
-
-    /// Signaled when a snapshot should be taken, and data deleted
-    ss::condition_variable _reap_condition;
-    /// To maintain backpressure on snapshot writes from storage
-    raft::offset_monitor _last_snapshot_monitor;
+    /**
+     * Since the ss::queue is a SPSC type of queue we use additional mutex to
+     * hold the two producers i.e. apply fiber and log eviction monitoring fiber
+     */
+    static constexpr size_t max_event_queue_size = 4;
+    mutex _queue_mutex;
+    ss::queue<eviction_event> _queue;
 };
 
 } // namespace cluster
