868befba2caafecf036e526d31127239c3a2ad7c
Andrew Wong <awong@redpanda.com>
2023-07-27 10:06:42 -0700

    [v23.1.x] rpk: add common command args to storage
    NOTE: This patch only exists on v23.1.x, as this is handled generically
    already on later branches.
    
    Adds some common arguments to allow setting --api-urls

--- src/go/rpk/pkg/cli/cmd/cluster/storage/storage.go
+++ src/go/rpk/pkg/cli/cmd/cluster/storage/storage.go
@@ -11,18 +11,41 @@ package storage
 
 import (
 	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/cli/cmd/cluster/storage/recovery"
+	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/cli/cmd/common"
+	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/config"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 )
 
 func NewCommand(fs afero.Fs) *cobra.Command {
+	var (
+		adminURL       string
+		adminEnableTLS bool
+		adminCertFile  string
+		adminKeyFile   string
+		adminCAFile    string
+	)
 	cmd := &cobra.Command{
 		Use:   "storage",
 		Short: "Manage the cluster storage",
 	}
 
+	common.AddAdminAPITLSFlags(cmd,
+		&adminEnableTLS,
+		&adminCertFile,
+		&adminKeyFile,
+		&adminCAFile,
+	)
+
 	cmd.AddCommand(
 		recovery.NewCommand(fs),
 	)
+
+	cmd.PersistentFlags().StringVar(
+		&adminURL,
+		config.FlagAdminHosts2,
+		"",
+		"Comma-separated list of admin API addresses (<IP>:<port>)")
+
 	return cmd
 }
