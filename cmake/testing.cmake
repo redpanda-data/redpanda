4dc307ffc82209cce278da0b6560f7d01d9a661a
Noah Watkins <noahwatkins@gmail.com>
2023-08-06 15:21:13 -0700

    ctest: add a gtest option to control cli param protocol
    Google test doesn't expect the `--` protocol for extra command line
    arguments. We can remove it for gtest in which case gtest will filter
    out gtest specific options and pass the rest to Seastar engine.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- cmake/testing.cmake
+++ cmake/testing.cmake
@@ -21,7 +21,7 @@ message(STATUS "RP_ENABLE_BENCHMARK_TESTS=${RP_ENABLE_BENCHMARK_TESTS}")
 
 function (rp_test)
   set(options
-    FIXTURE_TEST UNIT_TEST BENCHMARK_TEST)
+    FIXTURE_TEST UNIT_TEST BENCHMARK_TEST GTEST)
   set(oneValueArgs BINARY_NAME TIMEOUT PREPARE_COMMAND POST_COMMAND)
   set(multiValueArgs
     INCLUDES
@@ -117,10 +117,15 @@ function (rp_test)
     endif()
   endif()
 
+  set(gtest_option "")
+  if (RP_TEST_GTEST)
+      set(gtest_option "--gtest")
+  endif()
+
   if(NOT skip_test)
     add_test (
       NAME ${RP_TEST_BINARY_NAME}
-      COMMAND bash -c "${RUNNER} --binary=$<TARGET_FILE:${RP_TEST_BINARY_NAME}> ${prepare_command} ${post_command} ${files_to_copy} ${RP_TEST_ARGS} "
+      COMMAND bash -c "${RUNNER} --binary=$<TARGET_FILE:${RP_TEST_BINARY_NAME}> ${gtest_option} ${prepare_command} ${post_command} ${files_to_copy} ${RP_TEST_ARGS} "
       )
     set_tests_properties(${RP_TEST_BINARY_NAME} PROPERTIES LABELS "${RP_TEST_LABELS}")
     if(RP_TEST_TIMEOUT)
