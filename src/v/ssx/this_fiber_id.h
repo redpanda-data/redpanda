a662f7b9776f799760541a8bec040e4123a5f53e
Evgeny Lazin <evgeny@vectorized.io>
2023-08-08 11:43:34 +0000

    Fixup this fiber id

--- src/v/ssx/this_fiber_id.h
+++ src/v/ssx/this_fiber_id.h
@@ -2,6 +2,7 @@
 #include "vassert.h"
 
 #include <seastar/core/reactor.hh>
+#include <seastar/core/task.hh>
 #include <seastar/core/thread.hh>
 
 namespace ssx {
@@ -12,11 +13,7 @@ namespace ssx {
 /// If one coroutine awaits another one they both will have the same fiber id.
 inline uint64_t this_fiber_id() {
     auto task = ss::engine().current_task();
-    if (task == nullptr) {
-        task = seastar::thread_impl::get()->waiting_task();
-    }
-    vassert(task != nullptr, "this_fiber_id can only be called from coroutine");
-    while (true) {
+    while (task != nullptr) {
         auto child = task->waiting_task();
         if (child == nullptr) {
             break;
