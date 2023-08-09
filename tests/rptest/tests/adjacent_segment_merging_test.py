470765abb2331f3d2ba62d6d9e87b1de97eae9ea
Evgeny Lazin <4lazin@gmail.com>
2023-08-09 07:47:46 -0400

    rptest: Fix adjacent segment merger test
    Disable acks==0 case since it's not guaranteed that the data will be
    uploaded and segments are merged in 60 seconds.
    
    (cherry picked from commit 7f0304257b64bc640583343b2fced8a36e1037cf)

--- tests/rptest/tests/adjacent_segment_merging_test.py
+++ tests/rptest/tests/adjacent_segment_merging_test.py
@@ -68,7 +68,7 @@ class AdjacentSegmentMergingTest(RedpandaTest):
         super().tearDown()
 
     @cluster(num_nodes=3)
-    @matrix(acks=[-1, 0, 1], cloud_storage_type=get_cloud_storage_type())
+    @matrix(acks=[-1, 1], cloud_storage_type=get_cloud_storage_type())
     def test_reupload_of_local_segments(self, acks, cloud_storage_type):
         """Test adjacent segment merging using using local data.
         The test starts by uploading large number of very small segments.
