fb8657bb1c6612f612b56baeb2bc4be98e84b336
Noah Watkins <noahwatkins@gmail.com>
2023-08-09 16:48:23 -0700

    cluster: add reclaimable size to partition health report
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/cluster/health_monitor_types.cc
+++ src/v/cluster/health_monitor_types.cc
@@ -140,12 +140,13 @@ std::ostream& operator<<(std::ostream& o, const partition_status& ps) {
     fmt::print(
       o,
       "{{id: {}, term: {}, leader_id: {}, revision_id: {}, size_bytes: {}, "
-      "under_replicated: {}}}",
+      "reclaimable_size_bytes: {}, under_replicated: {}}}",
       ps.id,
       ps.term,
       ps.leader_id,
       ps.revision_id,
       ps.size_bytes,
+      ps.reclaimable_size_bytes,
       ps.under_replicated_replicas);
     return o;
 }
--- src/v/cluster/health_monitor_types.h
+++ src/v/cluster/health_monitor_types.h
@@ -57,7 +57,7 @@ struct node_state
 
 struct partition_status
   : serde::
-      envelope<partition_status, serde::version<1>, serde::compat_version<0>> {
+      envelope<partition_status, serde::version<2>, serde::compat_version<0>> {
     static constexpr size_t invalid_size_bytes = size_t(-1);
 
     model::partition_id id;
@@ -67,6 +67,21 @@ struct partition_status
     size_t size_bytes;
     std::optional<uint8_t> under_replicated_replicas;
 
+    /*
+     * estimated amount of data above local retention that is subject to
+     * reclaim under disk pressure. this is useful for the partition balancer
+     * which is interested in free space on a node. a node may have very little
+     * physical free space, but have effective free space represented by
+     * reclaimable size bytes.
+     *
+     * an intuitive relationship between size_bytes and reclaimable_size_bytes
+     * would have the former being >= than the later. however due to the way
+     * that data is collected it is conceivable that this inequality doesn't
+     * hold. callers should check for this condition and normalize the values or
+     * ignore the update.
+     */
+    std::optional<size_t> reclaimable_size_bytes;
+
     auto serde_fields() {
         return std::tie(
           id,
@@ -74,7 +89,8 @@ struct partition_status
           leader_id,
           revision_id,
           size_bytes,
-          under_replicated_replicas);
+          under_replicated_replicas,
+          reclaimable_size_bytes);
     }
 
     friend std::ostream& operator<<(std::ostream&, const partition_status&);
