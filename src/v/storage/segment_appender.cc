815306c88fd3cef1490ae8946e233e7be8e00558
Travis Downs <travis.downs@vectorized.io>
2023-08-15 22:58:33 -0700

    SQUASH: use dma_size in one place

--- src/v/storage/segment_appender.cc
+++ src/v/storage/segment_appender.cc
@@ -549,7 +549,7 @@ void segment_appender::dispatch_background_head_write() {
                   .dma_write(
                     w->file_start_offset,
                     w->chunk->data() + w->chunk_begin,
-                    w->chunk_end - w->chunk_begin,
+                    dma_size,
                     _opts.priority)
 #pragma clang diagnostic pop
                   .then([this, w](size_t got) {
