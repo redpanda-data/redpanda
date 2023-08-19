a9581e23b7a5247e4f8e6e663a2cb8177b572dc2
Alex Savatieiev <a.savex@gmail.com>
2023-08-18 20:09:17 -0500

    rptest: Support BYOC in VPC Peering (part 3)
        BYOC cloud is created in Operator's provider account. Due
        to this, CloudV2 API can't be used for Peering creation.
        I.e. we will not be able to put routes and acccess data. So,
        BYOC required separate workflow.
    
        Functions broken down to steps and each workflow uses
        common set of routines as well as own specific ones.

--- tests/rptest/services/provider_clients/__init__.py
+++ tests/rptest/services/provider_clients/__init__.py
@@ -2,10 +2,10 @@ from rptest.services.provider_clients.ec2_client import EC2Client
 from rptest.services.provider_clients.gcp_client import GCPClient
 
 
-def make_provider_client(provider, logger, provider_config=None):
+def make_provider_client(provider, logger, region, key, secret):
     _client = None
     if provider == 'AWS':
-        _client = EC2Client(provider_config, logger)
+        _client = EC2Client(region, key, secret, logger)
     elif provider == 'GCP':
-        _client = GCPClient(provider_config, logger)
+        _client = GCPClient(region, key, secret, logger)
     return _client
