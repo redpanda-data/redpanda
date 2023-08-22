9e130bd7bf35ca89d9f55cd098233d8df487e3cb
Tyler Rockwood <rockwood@redpanda.com>
2023-08-21 19:59:32 -0500

    Add a utility to check if a string is valid UTF8
    A boolean check for `is_valid_utf8` is an easier interface to work with
    when doing some error_code based validation (as opposed to just letting
    exceptions be thrown willy-nilly)
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/utils/utf8.cc
+++ src/v/utils/utf8.cc
@@ -12,3 +12,19 @@
 #include "utils/utf8.h"
 
 thread_local bool permit_unsafe_log_operation::_flag = false;
+
+bool is_valid_utf8(std::string_view s) {
+    bool is_valid = true;
+    class default_utf8_checker {
+    public:
+        explicit default_utf8_checker(bool* is_valid)
+          : _is_valid(is_valid) {}
+
+        void conversion_error() { *_is_valid = false; }
+
+    private:
+        bool* _is_valid;
+    };
+    validate_utf8(s, default_utf8_checker{&is_valid});
+    return is_valid;
+}
