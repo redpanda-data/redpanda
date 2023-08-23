a4275884daf757e3f0fe056a3899d72ae58018d7
Rob Blafford <rob@redpanda.com>
2023-08-23 11:12:31 -0400

    rptest: Marking test_consume_miss_cache @ignore

--- tests/rptest/scale_tests/high_throughput_test.py
+++ tests/rptest/scale_tests/high_throughput_test.py
@@ -793,7 +793,7 @@ class HighThroughputTest(PreallocNodesTest):
     # The testcase occasionally fails on various parts:
     # - toing on `_consume_from_offset(self.topic_name, 1, p_id, "newest", 30)`
     # - failing to ensure all manifests are in the cloud in `stop_and_scrub_object_storage`
-    @ok_to_fail
+    @ignore
     @cluster(num_nodes=7, log_allow_list=RESTART_LOG_ALLOW_LIST)
     def test_consume_miss_cache(self):
         self.setup_cluster(segment_bytes=self.small_segment_size,
