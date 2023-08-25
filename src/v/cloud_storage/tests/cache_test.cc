14c5767fa620b85fac22d841ec297f964357e4f3
Abhijat Malviya <abhijat@redpanda.com>
2023-08-25 19:09:10 +0530

    cloud_storage: Force exhaustive trim when fast trim fails
    In the case where the cache dir consists solely of index+tx files, the
    current code path in fast trim does not remove anything. This should be
    followed up by an exhaustive trim to free up slots in cache, but due to
    an adjustment of the objects to delete counter, this ends up not
    happening.
    
    The change here makes sure that if we have a certain count of objects to
    delete, and we were not able to delete the count, and there is a set of
    filtered out files, we proceed to do an exhaustive trim.
    
    (cherry picked from commit 2cf23e86ea1fdf38d1a6b014764dbbd53277e991)

--- src/v/cloud_storage/tests/cache_test.cc
+++ src/v/cloud_storage/tests/cache_test.cc
@@ -26,6 +26,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <chrono>
+#include <fstream>
 #include <stdexcept>
 
 using namespace cloud_storage;
@@ -465,4 +466,37 @@ FIXTURE_TEST(test_clean_up_on_start_empty, cache_test_fixture) {
     clean_up_at_start().get();
 
     BOOST_CHECK(ss::file_exists(CACHE_DIR.native()).get());
+}
+
+/**
+ * Given a cache dir populated with files which are filtered out by fast trim,
+ * validate that a failing fast trim should be followed up by an exhaustive trim
+ * and clean up the required object count.
+ */
+FIXTURE_TEST(test_exhaustive_trim_runs_after_fast_trim, cache_test_fixture) {
+    std::vector<std::filesystem::path> indices;
+    const auto count_indices = 5;
+    indices.reserve(count_indices);
+
+    for (auto i = 0; i < count_indices; ++i) {
+        indices.emplace_back(CACHE_DIR / fmt::format("{}.index", i, i, i));
+        std::ofstream f{indices.back()};
+        f.flush();
+    }
+
+    BOOST_REQUIRE(
+      std::all_of(indices.cbegin(), indices.cend(), [](const auto& path) {
+          return std::filesystem::exists(path);
+      }));
+
+    // Make cache service scan the disk for objects
+    clean_up_at_start().get();
+
+    // Only allow the access time tracker to remain on disk.
+    trim_cache(std::nullopt, 1);
+
+    BOOST_REQUIRE(
+      std::all_of(indices.cbegin(), indices.cend(), [](const auto& path) {
+          return !std::filesystem::exists(path);
+      }));
 }
