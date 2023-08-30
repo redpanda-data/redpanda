133187dd625ef70e0bd3de52cf472fec1446a1ed
Tyler Rockwood <rockwood@redpanda.com>
2023-08-30 11:11:13 -0500

    Add a schema registry client to the transform-sdk
    This wraps the ABI exposed in previous commits in a user friendly
    fashion. By default all data from schema registry is cached forever
    in the client, since we expect schemas to change infrequently this seems
    to be the right tradeoff. This also is the default in most kafka clients.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/go/transform-sdk/sr/client.go
+++ src/go/transform-sdk/sr/client.go
@@ -14,6 +14,15 @@
 
 package sr
 
+import (
+	"encoding/binary"
+	"errors"
+	"strconv"
+	"unsafe"
+
+	"github.com/redpanda-data/redpanda/src/go/transform-sdk/internal/rwbuf"
+)
+
 // schemaId is an ID of a schema registered with schema registry
 type schemaId int32
 
@@ -53,4 +62,182 @@ type SubjectSchema struct {
 	Subject string
 	Version int
 	ID      int
+}
+
+// SchemaRegistryClient is a client for interacting with the schema registry within Redpanda.
+//
+// The client provides caching out of the box, which can be configured with options.
+type SchemaRegistryClient interface {
+	// LookupSchemaById looks up a schema via it's global ID.
+	LookupSchemaById(id int) (s *Schema, err error)
+	// LookupSchemaByVersion looks up a schema via a subject for a specific version.
+	//
+	// Use version -1 to get the latest version.
+	LookupSchemaByVersion(subject string, version int) (s *SubjectSchema, err error)
+	// CreateSchema creates a schema under the given subject, returning the version and ID.
+	//
+	// If an equivalent schema already exists globally, that schema ID will be reused.
+	// If an equivalent schema already exists within that subject, this will be a noop and the
+	// existing schema will be returned.
+	CreateSchema(subject string, schema Schema) (s *SubjectSchema, err error)
+}
+
+type (
+	clientOpts struct {
+		disableCaches bool
+	}
+	// ClientOpt is an option to configure a SchemaRegistryClient
+	ClientOpt     interface{ apply(*clientOpts) }
+	clientOptFunc func(*clientOpts)
+)
+
+func (f clientOptFunc) apply(opts *clientOpts) {
+	f(opts)
+}
+
+type subjectVersion struct {
+	subject string
+	version int
+}
+
+type (
+	clientImpl        struct{}
+	cachingClientImpl struct {
+		underlying                  SchemaRegistryClient
+		schemaByIdCache             map[schemaId]*Schema
+		schemaBySubjectVersionCache map[subjectVersion]*SubjectSchema
+	}
+)
+
+// WithCachesDisabled disables any caching done by the client
+func WithCachesDisabled() ClientOpt {
+	return clientOptFunc(func(o *clientOpts) {
+		o.disableCaches = true
+	})
+}
+
+// NewClient creates a new SchemaRegistryClient with the specified options applied.
+func NewClient(opts ...ClientOpt) (c SchemaRegistryClient) {
+	o := clientOpts{
+		disableCaches: false,
+	}
+	for _, opt := range opts {
+		opt.apply(&o)
+	}
+	c = &clientImpl{}
+	if o.disableCaches {
+		return c
+	}
+	return &cachingClientImpl{
+		underlying:                  c,
+		schemaByIdCache:             make(map[schemaId]*Schema),
+		schemaBySubjectVersionCache: make(map[subjectVersion]*SubjectSchema),
+	}
+}
+
+func (sr *cachingClientImpl) LookupSchemaById(id int) (s *Schema, err error) {
+	cached, ok := sr.schemaByIdCache[schemaId(id)]
+	if ok {
+		return cached, nil
+	}
+	s, err = sr.underlying.LookupSchemaById(id)
+	if err != nil {
+		sr.schemaByIdCache[schemaId(id)] = s
+	}
+	return
+}
+
+func (sr *clientImpl) LookupSchemaById(id int) (*Schema, error) {
+	var length int32
+	errno := getSchemaDefinitionLen(schemaId(id), unsafe.Pointer(&length))
+	if errno != 0 {
+		return nil, errors.New("unable to find a schema definition with id " + strconv.Itoa(int(id)))
+	}
+	buf := rwbuf.New(int(length))
+	result := getSchemaDefinition(
+		schemaId(id),
+		unsafe.Pointer(buf.WriterBufPtr()),
+		int32(buf.WriterLen()),
+	)
+	if result < 0 {
+		return nil, errors.New("unable to find a schema definition with id " + strconv.Itoa(int(id)))
+	}
+	buf.AdvanceWriter(int(result))
+	schema, err := decodeSchemaDef(buf)
+	if err != nil {
+		return nil, err
+	}
+	return &schema, nil
+}
+
+func (sr *cachingClientImpl) LookupSchemaByVersion(subject string, version int) (s *SubjectSchema, err error) {
+	cached, ok := sr.schemaBySubjectVersionCache[subjectVersion{subject, version}]
+	if ok {
+		return cached, nil
+	}
+	s, err = sr.underlying.LookupSchemaByVersion(subject, version)
+	if err != nil {
+		sr.schemaBySubjectVersionCache[subjectVersion{subject, version}] = s
+	}
+	return
+}
+
+func (sr *clientImpl) LookupSchemaByVersion(subject string, version int) (s *SubjectSchema, err error) {
+	var length int32
+	errno := getSchemaSubjectLen(
+		unsafe.Pointer(unsafe.StringData(subject)),
+		int32(len(subject)),
+		int32(version),
+		unsafe.Pointer(&length),
+	)
+	if errno != 0 {
+		return nil, errors.New("unable to find a schema " + subject + " with version " + strconv.Itoa(int(version)))
+	}
+	buf := rwbuf.New(int(length))
+	result := getSchemaSubject(
+		unsafe.Pointer(unsafe.StringData(subject)),
+		int32(len(subject)),
+		int32(version),
+		unsafe.Pointer(buf.WriterBufPtr()),
+		int32(buf.WriterLen()),
+	)
+	if result < 0 {
+		return nil, errors.New("unable to find a schema " + subject + " and version " + strconv.Itoa(version))
+	}
+	buf.AdvanceWriter(int(result))
+	schema, err := decodeSchema(subject, buf)
+	if err != nil {
+		return nil, err
+	}
+	return &schema, nil
+}
+
+func (sr *cachingClientImpl) CreateSchema(subject string, schema Schema) (s *SubjectSchema, err error) {
+	s, err = sr.underlying.CreateSchema(subject, schema)
+	if err == nil {
+		sr.schemaBySubjectVersionCache[subjectVersion{subject, s.Version}] = s
+	}
+	return
+}
+
+func (sr *clientImpl) CreateSchema(subject string, schema Schema) (s *SubjectSchema, err error) {
+	buf := rwbuf.New(binary.MaxVarintLen32 + len(schema.Schema) + binary.MaxVarintLen32)
+	encodeSchemaDef(buf, schema)
+	b := buf.ReadAll()
+	var id schemaId
+	errno := createSubjectSchema(
+		unsafe.Pointer(unsafe.StringData(subject)),
+		int32(len(subject)),
+		unsafe.Pointer(&b[0]),
+		int32(len(b)),
+		unsafe.Pointer(&id),
+	)
+	if errno != 0 {
+		return nil, errors.New("unable to create new schema")
+	}
+	return &SubjectSchema{
+		Subject: subject,
+		ID:      int(id),
+		Schema:  schema,
+	}, nil
 }
