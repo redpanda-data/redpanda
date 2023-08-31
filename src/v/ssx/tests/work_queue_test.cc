d1e12491545c886e5ce8454d8426b5f174b7d3db
Tyler Rockwood <rockwood@redpanda.com>
2023-08-31 16:37:22 -0500

    Add tests for delayed tasks in work_queue
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/ssx/tests/work_queue_test.cc
+++ src/v/ssx/tests/work_queue_test.cc
@@ -11,6 +11,7 @@
 #include "ssx/work_queue.h"
 
 #include <seastar/core/future.hh>
+#include <seastar/core/manual_clock.hh>
 #include <seastar/core/sleep.hh>
 
 #include <gmock/gmock.h>
@@ -22,14 +23,38 @@
 
 namespace ssx {
 
+using namespace std::chrono_literals;
 using ::testing::ElementsAre;
 using ::testing::IsEmpty;
+
+namespace {
+ss::future<> submit_delayed(
+  work_queue* q,
+  ss::manual_clock::duration d,
+  ss::noncopyable_function<void()> fn) {
+    ss::promise<> p;
+    q->submit_delayed<ss::manual_clock>(d, [&] {
+        fn();
+        p.set_value();
+        return ss::now();
+    });
+    co_await p.get_future();
+}
+ss::future<> submit(work_queue* q, ss::noncopyable_function<void()> fn) {
+    ss::promise<> p;
+    q->submit([&] {
+        fn();
+        p.set_value();
+        return ss::now();
+    });
+    co_await p.get_future();
+}
+} // namespace
 
 TEST(WorkQueue, Order) {
     work_queue queue([](auto ex) { std::rethrow_exception(ex); });
 
     std::vector<int> values;
-    ss::promise<> p;
 
     queue.submit([&] {
         values.push_back(1);
@@ -39,14 +64,10 @@ TEST(WorkQueue, Order) {
         values.push_back(2);
         return ss::now();
     });
-    queue.submit([&] {
-        values.push_back(3);
-        p.set_value();
-        return ss::now();
-    });
+    auto done = submit(&queue, [&] { values.push_back(3); });
 
     EXPECT_THAT(values, IsEmpty());
-    p.get_future().get();
+    done.get();
     EXPECT_THAT(values, ElementsAre(1, 2, 3));
     queue.shutdown().get();
 }
@@ -56,7 +77,6 @@ TEST(WorkQueue, FailuresDoNotStopTheQueue) {
     work_queue queue([&error_count](auto) { ++error_count; });
 
     std::vector<int> values;
-    ss::promise<> p;
 
     queue.submit([&] {
         values.push_back(1);
@@ -64,15 +84,11 @@ TEST(WorkQueue, FailuresDoNotStopTheQueue) {
     });
     queue.submit(
       [&]() -> ss::future<> { throw std::runtime_error("oh noes!"); });
-    queue.submit([&] {
-        values.push_back(3);
-        p.set_value();
-        return ss::now();
-    });
+    auto done = submit(&queue, [&] { values.push_back(3); });
 
     EXPECT_THAT(values, IsEmpty());
     EXPECT_EQ(error_count, 0);
-    p.get_future().get();
+    done.get();
     EXPECT_EQ(error_count, 1);
     EXPECT_THAT(values, ElementsAre(1, 3));
     queue.shutdown().get();
@@ -87,6 +103,40 @@ TEST(WorkQueue, ShutdownStopsTheQueueImmediately) {
     });
     queue.shutdown().get();
     EXPECT_EQ(a, 1);
+}
+
+TEST(WorkQueue, CanSubmitDelayedTasks) {
+    work_queue queue([](auto ex) { std::rethrow_exception(ex); });
+    int a = 1;
+    ss::promise<> p1;
+    auto f1 = submit_delayed(&queue, 1s, [&] { a = 2; });
+    auto f2 = submit(&queue, [&] { a = 3; });
+    f2.get();
+    EXPECT_FALSE(f1.available());
+    EXPECT_EQ(a, 3);
+    ss::manual_clock::advance(1s);
+    f1.get();
+    EXPECT_EQ(a, 2);
+    queue.shutdown().get();
+}
+
+TEST(WorkQueue, CanShutdownWithDelayedTasks) {
+    work_queue queue([](auto ex) { std::rethrow_exception(ex); });
+    auto f1 = submit_delayed(&queue, 1s, [&] {});
+    bool a2 = false;
+    queue.submit_delayed<ss::manual_clock>(2s, [&] {
+        a2 = true;
+        return ss::now();
+    });
+    EXPECT_FALSE(f1.available());
+    EXPECT_FALSE(a2);
+    ss::manual_clock::advance(1s);
+    f1.get();
+    EXPECT_FALSE(a2);
+    queue.shutdown().get();
+    ss::manual_clock::advance(3s);
+    ss::now().get();
+    EXPECT_FALSE(a2);
 }
 
 } // namespace ssx
