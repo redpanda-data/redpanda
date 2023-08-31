ee8d12488a16820f6971ffdda1bbcf741e0fad0d
Tyler Rockwood <rockwood@redpanda.com>
2023-08-31 16:37:04 -0500

    Support delayed tasks in the work_queue
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/ssx/work_queue.cc
+++ src/v/ssx/work_queue.cc
@@ -9,6 +9,9 @@
 
 #include "ssx/work_queue.h"
 
+#include <seastar/core/lowres_clock.hh>
+#include <seastar/core/sleep.hh>
+
 #include <exception>
 
 namespace ssx {
@@ -32,6 +35,29 @@ void work_queue::submit(ss::noncopyable_function<ss::future<>()> fn) {
 
 ss::future<> work_queue::shutdown() {
     _as.request_abort();
-    return std::exchange(_tail, ss::now());
+    for (auto& [_, fut] : _delayed_tasks) {
+        co_await std::move(fut);
+    }
+    _delayed_tasks.clear();
+    co_await std::exchange(_tail, ss::now());
+}
+
+void work_queue::submit_after(
+  ss::future<> fut, ss::noncopyable_function<ss::future<>()> fn) {
+    uint64_t my_id = _delayed_id++;
+    _delayed_tasks.emplace(
+      my_id,
+      std::move(fut).then_wrapped(
+        [this, my_id, fn = std::move(fn)](auto fut) mutable {
+            fut.ignore_ready_future();
+            if (_as.abort_requested()) {
+                // when an abort is requested, stop is
+                // responsible for deleting the future,
+                // as it needs to wait for them.
+                return;
+            }
+            _delayed_tasks.erase(my_id);
+            submit(std::move(fn));
+        }));
 }
 } // namespace ssx
--- src/v/ssx/work_queue.h
+++ src/v/ssx/work_queue.h
@@ -17,7 +17,10 @@
 #include <seastar/core/abort_source.hh>
 #include <seastar/core/future-util.hh>
 #include <seastar/core/future.hh>
+#include <seastar/core/lowres_clock.hh>
 #include <seastar/util/noncopyable_function.hh>
+
+#include <absl/container/flat_hash_map.h>
 
 namespace ssx {
 
@@ -32,13 +35,26 @@ public:
     explicit work_queue(error_reporter_fn);
     // Add a task to the queue to be processed.
     void submit(ss::noncopyable_function<ss::future<>()>);
+    // Add a task to the queue to be processed after some timeout.
+    template<typename Clock = ss::lowres_clock>
+    void
+      submit_delayed(Clock::duration, ss::noncopyable_function<ss::future<>()>);
     // Shutdown the queue, waiting for the currently executing task to finish.
     ss::future<> shutdown();
 
 private:
+    void submit_after(ss::future<>, ss::noncopyable_function<ss::future<>()>);
+
     error_reporter_fn _error_reporter;
     ss::future<> _tail = ss::now();
     ss::abort_source _as;
+    uint64_t _delayed_id = 0;
+    absl::flat_hash_map<uint64_t, ss::future<>> _delayed_tasks;
 };
 
+template<typename Clock>
+void work_queue::submit_delayed(
+  Clock::duration delay, ss::noncopyable_function<ss::future<>()> fn) {
+    submit_after(ss::sleep_abortable<Clock>(delay, _as), std::move(fn));
+}
 } // namespace ssx
