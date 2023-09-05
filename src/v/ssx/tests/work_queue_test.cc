f5df616eb87667f5144397cf90b20c6176776819
Tyler Rockwood <rockwood@redpanda.com>
2023-09-05 16:01:25 -0500

    Fix work queue tests in release mode
    In release mode seastar can execute tasks inline (when created using `ss::future::then`) instead of
    submitting the task to the scheduler, which cause these tests to fail,
    as we require control over when the callbacks are executed.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/ssx/tests/work_queue_test.cc
+++ src/v/ssx/tests/work_queue_test.cc
@@ -13,6 +13,7 @@
 #include <seastar/core/future.hh>
 #include <seastar/core/manual_clock.hh>
 #include <seastar/core/sleep.hh>
+#include <seastar/util/later.hh>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -53,6 +54,10 @@ ss::future<> submit(work_queue* q, ss::noncopyable_function<void()> fn) {
 
 TEST(WorkQueue, Order) {
     work_queue queue([](auto ex) { std::rethrow_exception(ex); });
+    // Add a task that isn't ready to the queue, so that in release mode,
+    // seastar doesn't run it inline without adding it to the scheduler. These
+    // tests rely on the callbacks not executing immedately.
+    queue.submit([] { return ss::yield(); });
 
     std::vector<int> values;
 
@@ -75,6 +80,7 @@ TEST(WorkQueue, Order) {
 TEST(WorkQueue, FailuresDoNotStopTheQueue) {
     int error_count = 0;
     work_queue queue([&error_count](auto) { ++error_count; });
+    queue.submit([] { return ss::yield(); });
 
     std::vector<int> values;
 
@@ -96,6 +102,7 @@ TEST(WorkQueue, FailuresDoNotStopTheQueue) {
 
 TEST(WorkQueue, ShutdownStopsTheQueueImmediately) {
     work_queue queue([](auto ex) { std::rethrow_exception(ex); });
+    queue.submit([] { return ss::yield(); });
     int a = 1;
     queue.submit([&] {
         a = 2;
