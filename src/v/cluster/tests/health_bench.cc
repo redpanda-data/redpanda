a700f0531bb909855c4ad7bdf6d8f7a23c87bc8b
Travis Downs <travis.downs@vectorized.io>
2023-09-06 17:13:29 -0300

    Bench for health report aggregation
    In order to solve issue #11378, we will produce not-truncated counts
    of leaderless and under-represented partitions. To do this, we need
    to keep adding partitions to a set/map where we would not
    previously have done so, in order to get a correct de-duplicated count
    of these elements.
    
    For very large numbers of partitions, this could be a performance
    problem, so I wrote this benchmark.
    
    Results for 100k partitions and 10 nodes (the config in the
    benchmark):
    
    test                         iterations      median          inst
    original                            134     1.380ms     6400321.7
    original_unlimited                   34    25.006ms   141876040.5
    current                              66     7.363ms    42025980.6
    
    Original is the code before the change, which stops adding elements
    to the set when their size reaches 128. original_unlimited is the same
    code but with the limit removed: the easiest way to get the counts,
    while current is the actual code checked in the immediately prior
    changes which uses the `collector` to collect per-topic lists of
    partition IDs.
    
    So the new code is about 5x slower than the old approach which didn't
    count all partitions, but at least 3x fast than the direct approach to
    counting the partitions. Importantly even for this very large test case
    we are well below the reactor stall threshold (though well above the
    default task quota). Overall the time of less than 10 ms seems fine
    for a command which seems to take ~35ms even on a totally unloaded
    single-node system over localhost.
    
    (cherry picked from commit ced4a02474fee04e6a08ba3dfd3f98e2116f84ec)

---
+++ src/v/cluster/tests/health_bench.cc
@@ -0,0 +1,115 @@
+// Copyright 2020 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+#include "cluster/health_monitor_backend.h"
+#include "cluster/health_monitor_types.h"
+#include "cluster/tests/health_monitor_test_utils.h"
+#include "model/namespace.h"
+#include "random/generators.h"
+#include "vassert.h"
+
+#include <seastar/testing/perf_tests.hh>
+
+#include <limits>
+#include <optional>
+
+namespace cluster {
+
+struct health_bench : health_report_accessor {
+    using health_report_accessor::aggregated_report;
+    /**
+     * @brief The original aggregate function prior to optimization.
+     */
+    template<size_t max_partitions_report>
+    static aggregated_report original_aggregate(report_cache_t& reports) {
+        aggregated_report ret;
+
+        absl::node_hash_map<
+          model::topic_namespace,
+          std::vector<model::partition_id>>
+          leaderless, urp;
+
+        for (const auto& [_, report] : reports) {
+            for (const auto& [tp_ns, partitions] : report.topics) {
+                for (const auto& partition : partitions) {
+                    if (
+                      !partition.leader_id.has_value()
+                      && ret.leaderless.size() < max_partitions_report) {
+                        ret.leaderless.emplace(
+                          tp_ns.ns, tp_ns.tp, partition.id);
+                    }
+                    if (
+                      partition.under_replicated_replicas.value_or(0) > 0
+                      && ret.under_replicated.size() < max_partitions_report) {
+                        ret.under_replicated.emplace(
+                          tp_ns.ns, tp_ns.tp, partition.id);
+                    }
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    void bench(auto aggr_fn) {
+        using namespace cluster;
+
+        constexpr int topic_count = 10;
+        constexpr int parts_per_topic = 10000;
+        constexpr int rf = 3;
+        constexpr int nodes = 32;
+
+        // genreate a random health report
+        absl::node_hash_map<model::node_id, cluster::node_health_report>
+          reports;
+
+        for (int topic = 0; topic < topic_count; topic++) {
+            std::vector<topic_status> statuses;
+            for (model::node_id node{0}; node < nodes; node++) {
+                model::topic_namespace tns{
+                  model::kafka_namespace,
+                  model::topic(fmt::format("topic_{}", topic))};
+
+                statuses.emplace_back(topic_status{tns, {}});
+            }
+
+            for (int pid = 0; pid < parts_per_topic; pid++) {
+                for (int r = 0; r < rf; r++) {
+                    auto nid = model::node_id(
+                      random_generators::get_int(nodes - 1));
+                    partition_status status{
+                      .id{pid},
+                      .leader_id = std::nullopt,
+                      .under_replicated_replicas = 1};
+                    statuses.at(nid).partitions.emplace_back(std::move(status));
+                }
+            }
+
+            for (model::node_id node{0}; node < nodes; node++) {
+                reports[node].topics.emplace_back(statuses.at(node));
+            }
+        }
+
+        perf_tests::start_measuring_time();
+        auto res = aggr_fn(reports);
+        perf_tests::stop_measuring_time();
+    }
+};
+
+PERF_TEST_F(health_bench, original) {
+    bench(original_aggregate<original_limit>);
+}
+
+PERF_TEST_F(health_bench, original_unlimited) {
+    bench(original_aggregate<std::numeric_limits<size_t>::max()>);
+}
+
+PERF_TEST_F(health_bench, current) { bench(aggregate); }
+
+} // namespace cluster
