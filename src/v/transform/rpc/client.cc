908c27f76b5ab8ed8139afc35b68b3c007bfd1d4
Tyler Rockwood <rockwood@redpanda.com>
2023-09-05 20:03:39 -0500

    transform/rpc: Add internal RPC client
    A simple as possible client, plenty of opportunities for
    batching/retries/etc
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

---
+++ src/v/transform/rpc/client.cc
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2023 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#include "transform/rpc/client.h"
+
+#include "cluster/errc.h"
+#include "cluster/partition_leaders_table.h"
+#include "cluster/scheduling/constraints.h"
+#include "model/fundamental.h"
+#include "model/metadata.h"
+#include "model/namespace.h"
+#include "model/record.h"
+#include "model/timeout_clock.h"
+#include "raft/errc.h"
+#include "rpc/errc.h"
+#include "rpc/types.h"
+#include "ssx/semaphore.h"
+#include "transform/rpc/logger.h"
+#include "transform/rpc/rpc_service.h"
+#include "transform/rpc/serde.h"
+
+#include <seastar/core/abort_source.hh>
+#include <seastar/core/chunked_fifo.hh>
+#include <seastar/core/future.hh>
+#include <seastar/core/loop.hh>
+#include <seastar/core/semaphore.hh>
+#include <seastar/core/smp.hh>
+#include <seastar/core/when_all.hh>
+#include <seastar/util/noncopyable_function.hh>
+
+#include <absl/container/btree_map.h>
+#include <absl/container/flat_hash_map.h>
+#include <boost/fusion/sequence/intrinsic/back.hpp>
+
+#include <algorithm>
+#include <chrono>
+#include <exception>
+#include <iterator>
+#include <stdexcept>
+#include <system_error>
+#include <utility>
+#include <vector>
+
+namespace transform::rpc {
+
+namespace {
+constexpr auto timeout = std::chrono::seconds(1);
+
+cluster::errc map_errc(std::error_code ec) {
+    if (ec.category() == cluster::error_category()) {
+        return static_cast<cluster::errc>(ec.value());
+    } else if (ec.category() == raft::error_category()) {
+        auto raft_ec = static_cast<raft::errc>(ec.value());
+        switch (raft_ec) {
+        case raft::errc::not_leader:
+        case raft::errc::leadership_transfer_in_progress:
+            return cluster::errc::not_leader;
+        default:
+            vlog(log.error, "unexpected transform produce error: {}", raft_ec);
+            break;
+        }
+    } else if (ec.category() == ::rpc::error_category()) {
+        auto rpc_ec = static_cast<::rpc::errc>(ec.value());
+        switch (rpc_ec) {
+        case ::rpc::errc::client_request_timeout:
+        case ::rpc::errc::connection_timeout:
+        case ::rpc::errc::disconnected_endpoint:
+            return cluster::errc::timeout;
+        default:
+            vlog(log.error, "unexpected transform produce error: {}", rpc_ec);
+            break;
+        }
+    } else {
+        vlog(log.error, "unexpected transform produce error: {}", ec);
+    }
+    return cluster::errc::timeout;
+}
+} // namespace
+
+client::client(
+  model::node_id self,
+  ss::sharded<cluster::partition_leaders_table>* l,
+  ss::sharded<::rpc::connection_cache>* c,
+  ss::sharded<local_service>* s)
+  : _self(self)
+  , _leaders(l)
+  , _connections(c)
+  , _local_service(s) {}
+
+ss::future<cluster::errc> client::produce(
+  model::topic_partition tp, ss::chunked_fifo<model::record_batch> batches) {
+    vlog(log.trace, "producing {} batches to {}", batches.size(), tp);
+    auto leader = _leaders->local().get_leader(
+      model::topic_namespace_view(model::kafka_namespace, tp.topic),
+      tp.partition);
+    if (!leader) {
+        co_return cluster::errc::not_leader;
+    }
+    produce_request req;
+    req.topic_data.emplace_back(std::move(tp), std::move(batches));
+    req.timeout = timeout;
+    auto reply = co_await (
+      *leader == _self ? do_local_produce(std::move(req))
+                       : do_remote_produce(*leader, std::move(req)));
+    vassert(
+      reply.results.size() == 1,
+      "expected a single result: {}",
+      reply.results.size());
+
+    co_return reply.results.front().err;
+}
+
+ss::future<> client::stop() { return ss::now(); }
+
+ss::future<produce_reply> client::do_local_produce(produce_request req) {
+    auto r = co_await _local_service->local().produce(
+      std::move(req.topic_data), req.timeout);
+    co_return produce_reply(std::move(r));
+}
+
+ss::future<produce_reply>
+client::do_remote_produce(model::node_id node, produce_request req) {
+    auto resp = co_await _connections->local()
+                  .with_node_client<impl::transform_rpc_client_protocol>(
+                    _self,
+                    ss::this_shard_id(),
+                    node,
+                    timeout,
+                    [&req](impl::transform_rpc_client_protocol proto) mutable {
+                        return proto.produce(
+                          req.share(),
+                          ::rpc::client_opts(
+                            model::timeout_clock::now() + timeout));
+                    })
+                  .then(&::rpc::get_ctx_data<produce_reply>);
+    if (resp.has_error()) {
+        cluster::errc ec = map_errc(resp.assume_error());
+        produce_reply reply;
+        for (const auto& data : req.topic_data) {
+            reply.results.emplace_back(data.tp, ec);
+        }
+        co_return reply;
+    }
+    co_return std::move(resp).value();
+}
+} // namespace transform::rpc
---
+++ src/v/transform/rpc/client.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "cluster/errc.h"
+#include "cluster/fwd.h"
+#include "model/fundamental.h"
+#include "model/metadata.h"
+#include "model/record.h"
+#include "model/timeout_clock.h"
+#include "rpc/connection_cache.h"
+#include "transform/rpc/service.h"
+
+#include <seastar/core/chunked_fifo.hh>
+#include <seastar/core/sharded.hh>
+
+#include <absl/container/flat_hash_map.h>
+
+namespace transform::rpc {
+
+/**
+ * A client for the transform rpc service.
+ *
+ * This is a sharded service that exists on every core, requests that can be
+ * serviced locally will not go through the rpc boundary but will directly go to
+ * the local service.
+ */
+class client {
+public:
+    client(
+      model::node_id self,
+      ss::sharded<cluster::partition_leaders_table>*,
+      ss::sharded<::rpc::connection_cache>*,
+      ss::sharded<local_service>*);
+    client(client&&) = delete;
+    client& operator=(client&&) = delete;
+    client(const client&) = delete;
+    client& operator=(const client&) = delete;
+    ~client() = default;
+
+    ss::future<cluster::errc>
+      produce(model::topic_partition, ss::chunked_fifo<model::record_batch>);
+
+    ss::future<> stop();
+
+private:
+    ss::future<produce_reply> do_local_produce(produce_request);
+    ss::future<produce_reply>
+      do_remote_produce(model::node_id, produce_request);
+
+    model::node_id _self;
+    // need partition_leaders_table to know which node owns the partitions
+    ss::sharded<cluster::partition_leaders_table>* _leaders;
+    ss::sharded<::rpc::connection_cache>* _connections;
+    ss::sharded<local_service>* _local_service;
+};
+
+} // namespace transform::rpc
