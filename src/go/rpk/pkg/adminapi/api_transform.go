21e82c93d06f6cf3c07b06f6ca3ad63c02040032
Tyler Rockwood <rockwood@redpanda.com>
2023-09-14 16:07:34 -0500

    rpk/transform: Add the API + structs for listing transforms
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/go/rpk/pkg/adminapi/api_transform.go
+++ src/go/rpk/pkg/adminapi/api_transform.go
@@ -27,3 +27,34 @@ type transformDeleteRequest struct {
 func (a *AdminAPI) DeleteWasmTransform(ctx context.Context, name string) error {
 	return a.sendToLeader(ctx, http.MethodPost, baseTransformEndpoint+deleteSuffix, transformDeleteRequest{name}, nil)
 }
+
+// PartitionTransformStatus is the status of a single transform that is running on an input partition.
+type PartitionTransformStatus struct {
+	NodeID    int `json:"node_id"`
+	Partition int `json:"partition"`
+	Core      int `json:"core"`
+	// Status is an enum of: ["running", "inactive", "errored", "unknown"].
+	Status string `json:"status"`
+}
+
+// EnvironmentVariable is a configuration key/value that can be injected into to a data transform.
+type EnvironmentVariable struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+// TransformMetadata is the metadata for a live running transform on a cluster.
+type TransformMetadata struct {
+	Name         string                     `json:"name"`
+	InputTopic   string                     `json:"input_topic"`
+	OutputTopics []string                   `json:"output_topics"`
+	Status       []PartitionTransformStatus `json:"status"`
+	Environment  []EnvironmentVariable      `json:"environment"`
+}
+
+// ListWasmTransforms lists the transforms that are running on a cluster.
+func (a *AdminAPI) ListWasmTransforms(ctx context.Context) ([]TransformMetadata, error) {
+	resp := []TransformMetadata{}
+	err := a.sendAny(ctx, http.MethodGet, baseTransformEndpoint, nil, &resp)
+	return resp, err
+}
