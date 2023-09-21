ae17bdaae4e8eda9e1d78e8fbecbab015cbce145
Alexey Zatelepin <ztlpn@redpanda.com>
2023-09-21 14:12:13 +0200

    tests: don't log rpk stderr twice in case of timeout
    (cherry picked from commit b373b54bb0805fec48e862556b1924e9a2d9fb76)

--- tests/rptest/clients/rpk.py
+++ tests/rptest/clients/rpk.py
@@ -35,21 +35,26 @@ class ClusterAuthorizationError(Exception):
 
 
 class RpkException(Exception):
-    def __init__(self, msg, stderr="", returncode=None):
+    def __init__(self, msg, stderr="", returncode=None, stdout=""):
         self.msg = msg
+        self.stdout = stdout
         self.stderr = stderr
         self.returncode = returncode
 
     def __str__(self):
         if self.stderr:
-            err = f" error: {self.stderr}"
+            err = f"; stderr: {self.stderr}"
         else:
             err = ""
         if self.returncode:
-            retcode = f" returncode: {self.returncode}"
+            retcode = f"; returncode: {self.returncode}"
         else:
             retcode = ""
-        return f"RpkException<{self.msg}{err}{retcode}>"
+        if self.stdout:
+            stdout = f"; stdout: {self.stdout}"
+        else:
+            stdout = ""
+        return f"RpkException<{self.msg}{err}{stdout}{retcode}>"
 
 
 class RpkPartition:
@@ -852,7 +857,10 @@ class RpkTool:
             output, error = p.communicate(input=stdin, timeout=timeout)
         except subprocess.TimeoutExpired:
             p.kill()
-            raise RpkException(f"command {' '.join(cmd)} timed out")
+            output, stderr = p.communicate()
+            raise RpkException(f"command `{' '.join(cmd)}' timed out",
+                               stdout=output,
+                               stderr=stderr)
 
         self._redpanda.logger.debug(f'\n{output}')
 
