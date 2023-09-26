8288ae8275eb7f305f3aa5bf9151c58cba3f3d44
Andrew Wong <awong@redpanda.com>
2023-09-25 19:23:29 -0700

    cloud_metadata: RPC endpoint for offsets lookup
    Wraps the offsets lookup logic in an RPC endpoint. This will be hit
    during cluster recovery as the offsets topic is recovered, to know if
    there is a need to adjust groups with any missing offsets.

--- src/v/redpanda/application.cc
+++ src/v/redpanda/application.cc
@@ -1398,6 +1398,12 @@ void application::wire_up_redpanda_services(
       std::ref(producer_manager))
       .get();
     vlog(_log.info, "Partition manager started");
+    construct_service(
+      offsets_lookup,
+      node_id,
+      std::ref(partition_manager),
+      std::ref(shard_table))
+      .get();
 
     construct_service(node_status_table, node_id).get();
     // controller
@@ -2418,6 +2424,7 @@ void application::start_runtime_services(
               cluster::cloud_metadata::offsets_recovery_rpc_service>(
               sched_groups.archival_upload(),
               smp_service_groups.cluster_smp_sg(),
+              std::ref(offsets_lookup),
               std::ref(offsets_upload_router)));
           runtime_services.push_back(std::make_unique<cluster::id_allocator>(
             sched_groups.raft_sg(),
