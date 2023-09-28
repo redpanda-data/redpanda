70e87083e4bf05114e87a3fbd799c9ec47241f26
Ben Pope <ben@redpanda.com>
2023-09-28 17:28:33 +0100

    security/oidc: Introduce rs256_verifier
    Also adds some boilerplate for type-erased extensibility.
    
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/security/jwt.h
+++ src/v/security/jwt.h
@@ -14,6 +14,15 @@
 #include "outcome.h"
 #include "utils/utf8.h"
 
+#include <seastar/core/sstring.hh>
+#include <seastar/util/variant_utils.hh>
+
+#include <cryptopp/base64.h>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/integer.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/rsa.h>
+
 #include <optional>
 #include <string_view>
 #include <system_error>
@@ -24,6 +33,7 @@ enum errc {
     success = 0,
     metadata_invalid,
     jwks_invalid,
+    jwk_invalid,
 };
 
 struct errc_category final : public std::error_category {
@@ -37,6 +47,8 @@ struct errc_category final : public std::error_category {
             return "Invalid metadata";
         case errc::jwks_invalid:
             return "Invalid jwks";
+        case errc::jwk_invalid:
+            return "Invalid jwk";
         }
     }
 };
@@ -62,6 +74,19 @@ namespace security::oidc {
 
 namespace detail {
 
+template<typename T>
+struct is_string_viewable : std::false_type {};
+template<typename CharT, typename Size, Size max_size, bool NulTerminate>
+struct is_string_viewable<
+  ss::basic_sstring<CharT, Size, max_size, NulTerminate>> : std::true_type {};
+template<typename CharT>
+struct is_string_viewable<std::basic_string_view<CharT>> : std::true_type {};
+template<typename T>
+concept string_viewable = detail::is_string_viewable<T>::value;
+
+using cryptopp_bytes = ss::basic_sstring<CryptoPP::byte, uint32_t, 31, false>;
+using cryptopp_byte_view = std::basic_string_view<cryptopp_bytes::value_type>;
+
 template<typename CharT = std::string_view::value_type>
 std::basic_string_view<CharT> as_string_view(json::Value const& v) {
     expression_in_debug_mode(
@@ -78,6 +103,33 @@ string_view(json::Value const& doc, std::string_view field) {
         return std::nullopt;
     }
     return as_string_view<CharT>(it->value);
+}
+
+template<string_viewable StringT = cryptopp_bytes>
+auto base64_url_decode(cryptopp_byte_view sv) {
+    CryptoPP::Base64URLDecoder decoder;
+
+    decoder.Put(sv.data(), sv.size());
+    decoder.MessageEnd();
+
+    StringT decoded;
+    if (auto size = decoder.MaxRetrievable(); size != 0) {
+        decoded.resize(size);
+        decoder.Get(
+          reinterpret_cast<CryptoPP::byte*>(decoded.data()), decoded.size());
+    }
+    return decoded;
+};
+
+inline std::optional<CryptoPP::Integer>
+as_cryptopp_integer(json::Value const& v, std::string_view field) {
+    CryptoPP::Integer integer;
+    auto b64 = string_view<CryptoPP::byte>(v, field);
+    if (!b64.has_value()) {
+        return std::nullopt;
+    }
+    auto chars = base64_url_decode(b64.value());
+    return CryptoPP::Integer{chars.data(), chars.size()};
 }
 
 } // namespace detail
@@ -147,5 +199,96 @@ private:
 
     json::Document _impl;
 };
+
+namespace detail {
+
+// Base class for verifying and signing tokens
+template<typename Algo, const char* Alg, const char* Kty>
+class signature_base {
+public:
+    using algorithm = Algo;
+    using verifier = algorithm::Verifier;
+    using signer = algorithm::Signer;
+
+    constexpr std::string_view alg() const { return Alg; }
+    constexpr std::string_view kty() const { return Kty; }
+    constexpr std::string_view use() const { return "sig"; }
+};
+
+template<typename Algo, const char* Alg, const char* Kty>
+class verifier_impl : public signature_base<Algo, Alg, Kty> {
+    using base = signature_base<Algo, Alg, Kty>;
+
+public:
+    using verifier = base::verifier;
+    using public_key = base::algorithm::PublicKey;
+
+    explicit verifier_impl(verifier&& v)
+      : _verifier(std::move(v)) {}
+
+    explicit verifier_impl(public_key&& k)
+      : _verifier(std::move(k)) {}
+
+    bool verify(cryptopp_byte_view msg, cryptopp_byte_view sig) const {
+        return _verifier.VerifyMessage(
+          msg.data(), msg.length(), sig.data(), sig.length());
+    }
+
+private:
+    verifier _verifier;
+};
+
+constexpr const char rsa_str[] = "RSA";
+constexpr const char rs256_str[] = "RS256";
+using rs256_verifier = verifier_impl<
+  CryptoPP::RSASS<CryptoPP::PKCS1v15, CryptoPP::SHA256>,
+  rs256_str,
+  rsa_str>;
+
+// Verify the signature of a message
+class verifier {
+public:
+    template<typename Algo, const char* Alg, const char* Kty>
+    explicit verifier(verifier_impl<Algo, Alg, Kty> v)
+      : _impl(std::move(v)) {}
+
+    auto alg() const {
+        return ss::visit(_impl, [](auto const& impl) { return impl.alg(); });
+    }
+
+    auto kty() const {
+        return ss::visit(_impl, [](auto const& impl) { return impl.kty(); });
+    }
+
+    bool verify(cryptopp_byte_view msg, cryptopp_byte_view sig) const {
+        return ss::visit(
+          _impl, [=](auto const& impl) { return impl.verify(msg, sig); });
+    }
+
+private:
+    using verifier_impls = std::variant<rs256_verifier>;
+    verifier_impls _impl;
+};
+
+inline result<verifier> make_rs256_verifier(
+  json::Value const& jwk, CryptoPP::AutoSeededRandomPool& rng) {
+    CryptoPP::RSA::PublicKey key;
+    try {
+        auto n = detail::as_cryptopp_integer(jwk, "n");
+        auto e = detail::as_cryptopp_integer(jwk, "e");
+        if (!n.has_value() || !e.has_value()) {
+            return errc::jwk_invalid;
+        }
+        key.Initialize(n.value(), e.value());
+        if (!key.Validate(rng, 3)) {
+            return errc::jwk_invalid;
+        }
+    } catch (CryptoPP::Exception const& ex) {
+        return errc::jwk_invalid;
+    }
+    return verifier{rs256_verifier{key}};
+}
+
+} // namespace detail
 
 } // namespace security::oidc
