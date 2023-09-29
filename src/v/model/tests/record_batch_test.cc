5fbb9d4eace1e3ad7fe373657beea29235d70ee3
Tyler Rockwood <rockwood@redpanda.com>
2023-09-29 14:37:02 -0500

    model/record_batch_test: Add test for extra bytes at the end of a batch
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/model/tests/record_batch_test.cc
+++ src/v/model/tests/record_batch_test.cc
@@ -90,3 +90,19 @@ SEASTAR_THREAD_TEST_CASE(iterator) {
     }
     BOOST_TEST(!it.has_next());
 }
+
+SEASTAR_THREAD_TEST_CASE(extra_bytes_iterator) {
+    auto b = model::test::make_random_batch(model::offset(0), 1, false);
+    auto buf = b.data().copy();
+    // If there are extra bytes at the end of the batch we should throw.
+    constexpr std::string_view extra_data = "foobar";
+    buf.append(extra_data.data(), extra_data.size());
+    auto header = b.header();
+    header.size_bytes = static_cast<int32_t>(
+      model::packed_record_batch_header_size + buf.size_bytes());
+    b = model::record_batch(
+      header, std::move(buf), model::record_batch::tag_ctor_ng{});
+    auto it = model::record_batch_iterator::create(b);
+    BOOST_TEST(it.has_next());
+    BOOST_REQUIRE_THROW(it.next(), std::out_of_range);
+}
