298965dfe140a2d1f54d12003e279d646a57ab20
Tyler Rockwood <rockwood@redpanda.com>
2023-10-04 04:25:56 -0500

    wasm/cache: Add an initial caching wasm::runtime
    This runtime caches factories so there is only a single copy of
    executable memory for a given module in a process.
    
    A GC process will cleanup old factories that are no longer in use.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/wasm/cache.cc
+++ src/v/wasm/cache.cc
@@ -17,6 +17,10 @@
 
 namespace wasm {
 
+struct cached_factory {
+    ss::shared_ptr<factory> factory;
+};
+
 namespace {
 
 /**
@@ -74,7 +78,107 @@ private:
     size_t _ref_count = 0;
     std::unique_ptr<engine> _underlying;
 };
+
+/**
+ * A RAII scoped lock that ensures factory locks are deleted when there are no
+ * waiters.
+ */
+class factory_creation_lock_guard {
+public:
+    factory_creation_lock_guard(const factory_creation_lock_guard&) = delete;
+    factory_creation_lock_guard& operator=(const factory_creation_lock_guard&)
+      = delete;
+    factory_creation_lock_guard(factory_creation_lock_guard&&) noexcept
+      = default;
+    factory_creation_lock_guard&
+    operator=(factory_creation_lock_guard&&) noexcept
+      = default;
+
+    static ss::future<factory_creation_lock_guard> acquire(
+      absl::btree_map<model::offset, std::unique_ptr<mutex>>* mu_map,
+      model::offset offset) {
+        auto it = mu_map->find(offset);
+        mutex* mu = nullptr;
+        if (it == mu_map->end()) {
+            auto inserted = mu_map->emplace(offset, std::make_unique<mutex>());
+            vassert(inserted.second, "expected mutex to be inserted");
+            mu = inserted.first->second.get();
+        } else {
+            mu = it->second.get();
+        }
+        mutex::units units = co_await mu->get_units();
+        co_return factory_creation_lock_guard(
+          offset, mu_map, mu, std::move(units));
+    }
+
+    ~factory_creation_lock_guard() {
+        _underlying.return_all();
+        // If nothing is waiting on or holding the mutex, we can remove the lock
+        // from the map.
+        if (_mu->ready()) {
+            _mu_map->erase(_offset);
+        }
+    }
+
+private:
+    factory_creation_lock_guard(
+      model::offset offset,
+      absl::btree_map<model::offset, std::unique_ptr<mutex>>* mu_map,
+      mutex* mu,
+      mutex::units underlying)
+      : _offset(offset)
+      , _mu_map(mu_map)
+      , _mu(mu)
+      , _underlying(std::move(underlying)) {}
+
+    model::offset _offset;
+    absl::btree_map<model::offset, std::unique_ptr<mutex>>* _mu_map;
+    mutex* _mu;
+    mutex::units _underlying;
+};
+} // namespace
+
+caching_runtime::caching_runtime(std::unique_ptr<runtime> u)
+  : _underlying(std::move(u)) {}
+
+caching_runtime::~caching_runtime() = default;
+
+ss::future<> caching_runtime::start() {
+    co_await _underlying->start();
+    // TODO: Start a periodic timer to GC unused factories
 }
 
+ss::future<> caching_runtime::stop() { co_await _underlying->stop(); }
 
+ss::future<ss::shared_ptr<factory>> caching_runtime::make_factory(
+  model::transform_metadata meta, iobuf binary, ss::logger* logger) {
+    model::offset offset = meta.source_ptr;
+    // Look in the cache outside the lock
+    auto it = _factory_cache.find(offset);
+    if (it != _factory_cache.end()) {
+        // TODO: Use a factory that caches engines
+        co_return it->second->factory;
+    }
+    auto lock = co_await factory_creation_lock_guard::acquire(
+      &_factory_creation_mu_map, offset);
+    // Look again in the cache with the lock
+    it = _factory_cache.find(offset);
+    if (it != _factory_cache.end()) {
+        // TODO: Use a factory that caches engines
+        co_return it->second->factory;
+    }
+    // There is no factory and we're holding the lock,
+    // time to create a new one.
+    auto factory = co_await _underlying->make_factory(
+      std::move(meta), std::move(binary), logger);
+
+    // Now cache the factory and return the result.
+    auto cached = ss::make_shared<cached_factory>(std::move(factory));
+    auto [_, inserted] = _factory_cache.emplace(offset, cached);
+    vassert(inserted, "expected factory to be inserted");
+
+    // TODO: Use a factory that caches engines
+    co_return cached->factory;
 }
+
+} // namespace wasm
--- src/v/wasm/cache.h
+++ src/v/wasm/cache.h
@@ -11,6 +11,59 @@
 
 #pragma once
 
+#include "utils/mutex.h"
 #include "wasm/api.h"
 
-namespace wasm {}
+#include <absl/container/btree_map.h>
+
+namespace wasm {
+
+struct cached_factory;
+
+/**
+ * A runtime that reuses factories and caches them per process as to share the
+ * executable memory.
+ *
+ * To enable this, this runtime can only create factories on a single shard
+ * (the same shard that it is created on, which is probably shard zero).
+ * However, factories that are created by this runtime can be used to create
+ * engines for any shard.
+ *
+ * Additionally, engines from this runtime's factories are reused within a
+ * single shard. Ramifications of this is that failures to a single engine cause
+ * the engine to be restarted and all users of a given engine must wait until
+ * it's restarted to use the engine.
+ */
+class caching_runtime : public runtime {
+public:
+    explicit caching_runtime(std::unique_ptr<runtime>);
+    caching_runtime(const caching_runtime&) = delete;
+    caching_runtime(caching_runtime&&) = delete;
+    caching_runtime& operator=(const caching_runtime&) = delete;
+    caching_runtime& operator=(caching_runtime&&) = delete;
+    ~caching_runtime() override;
+
+    ss::future<> start() override;
+    ss::future<> stop() override;
+
+    /**
+     * Create a factory, must be called only on a single shard.
+     */
+    ss::future<ss::shared_ptr<factory>>
+    make_factory(model::transform_metadata, iobuf, ss::logger*) override;
+
+private:
+    /*
+     * This map holds locks for creating factories.
+     *
+     * These mutexes are shortlived and should only live during the creation of
+     * factories.
+     */
+    absl::btree_map<model::offset, std::unique_ptr<mutex>>
+      _factory_creation_mu_map;
+    std::unique_ptr<runtime> _underlying;
+    absl::btree_map<model::offset, ss::shared_ptr<cached_factory>>
+      _factory_cache;
+};
+
+} // namespace wasm
