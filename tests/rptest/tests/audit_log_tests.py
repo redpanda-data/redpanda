53c220b4f8115f9644860acddd8f2684b29e940d
Rob Blafford <rob@redpanda.com>
2023-10-10 16:42:54 -0400

    rptest: Ducktape tests for audit logging

---
+++ tests/rptest/tests/audit_log_tests.py
@@ -0,0 +1,176 @@
+# Copyright 2023 Redpanda Data, Inc.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.md
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0
+
+import re
+import time
+import json
+from functools import partial, reduce
+from rptest.services.rpk_consumer import RpkConsumer
+from ducktape.utils.util import wait_until
+from rptest.tests.redpanda_test import RedpandaTest
+from rptest.services.cluster import cluster
+from rptest.services.admin import Admin
+from rptest.clients.rpk import RpkTool
+from rptest.tests.cluster_config_test import wait_for_version_sync
+from rptest.utils.audit_schemas import validate_audit_schema
+from rptest.services.redpanda import LoggingConfig
+
+
+class AuditLogTests(RedpandaTest):
+    audit_log = "__audit_log"
+
+    def __init__(self, test_context):
+        self._extra_conf = {
+            'audit_enabled': True,
+            'audit_log_num_partitions': 8,
+            'audit_enabled_event_types': ['management'],
+        }
+        super(AuditLogTests,
+              self).__init__(test_context=test_context,
+                             extra_rp_conf=self._extra_conf,
+                             log_config=LoggingConfig('info',
+                                                      logger_levels={
+                                                          'auditing':
+                                                          'trace',
+                                                          'admin_api_server':
+                                                          'trace'
+                                                      }))
+        self._ctx = test_context
+        self.admin = Admin(self.redpanda)
+        self.rpk = RpkTool(self.redpanda)
+
+    def _wait_for_audit_log(self, timeout_sec):
+        wait_until(lambda: self.audit_log in self.rpk.list_topics(),
+                   timeout_sec=timeout_sec,
+                   backoff_sec=2)
+
+    def _audit_log_total_number_records(self):
+        audit_partitions = self.rpk.describe_topic(self.audit_log)
+        partition_totals = [
+            partition.high_watermark for partition in audit_partitions
+        ]
+        assert len(partition_totals) == self._extra_conf[
+            'audit_log_num_partitions'], \
+                f"Expected: {self._extra_conf['audit_log_num_partitions']}, Result: {len(partition_totals)}"
+        return sum(partition_totals)
+
+    def _read_all_from_audit_log(self,
+                                 filter_fn,
+                                 stop_cond,
+                                 timeout_sec=30,
+                                 backoff_sec=1):
+        class MessageMapper():
+            def __init__(self, logger, filter_fn, stop_cond):
+                self.logger = logger
+                self.records = []
+                self.filter_fn = filter_fn
+                self.stop_cond = stop_cond
+                self.next_offset_ingest = 0
+
+            def ingest(self, records):
+                new_records = records[self.next_offset_ingest:]
+                self.next_offset_ingest = len(records)
+                new_records = [json.loads(msg['value']) for msg in new_records]
+                self.logger.info(f"Ingested: {len(new_records)} records")
+                self.records += [r for r in new_records if self.filter_fn(r)]
+
+            def is_finished(self):
+                return stop_cond(self.records)
+
+        mapper = MessageMapper(self.redpanda.logger, filter_fn, stop_cond)
+        n = self._audit_log_total_number_records()
+
+        consumer = RpkConsumer(self._ctx, self.redpanda, self.audit_log)
+        consumer.start()
+
+        def predicate():
+            mapper.ingest(consumer.messages)
+            return consumer.message_count >= n and mapper.is_finished()
+
+        wait_until(predicate, timeout_sec=timeout_sec, backoff_sec=backoff_sec)
+        consumer.stop()
+        consumer.free()
+        return mapper.records
+
+    @cluster(num_nodes=4)
+    def test_audit_log_functioning(self):
+        """
+        Ensures that the audit log can be produced to when the audit_enabled()
+        configuration option is set, and that the same actions do nothing
+        when the option is unset. Furthermore verifies that the internal duplicate
+        aggregation feature is working.
+        """
+        def is_api_match(matches, record):
+            self.logger.debug(f'{record}')
+            if record['class_uid'] == 6003:
+                regex = re.compile(
+                    "http:\/\/(?P<address>.*):(?P<port>\d+)\/v1\/(?P<handler>.*)"
+                )
+                match = regex.match(
+                        record['http_request']['url']['url_string'])
+                if match is None:
+                    raise RuntimeError(f'Record out of spec: {record}')
+                return match.group('handler') in matches
+            else:
+                return False
+
+        def number_of_records_matching(filter_by, n_expected):
+            filter_fn = partial(is_api_match, filter_by)
+
+            def aggregate_count(records):
+                # Duplicate records are combined with the 'count' field
+                def combine(acc, x):
+                    return acc + (1 if 'count' not in x else x['count'])
+
+                return reduce(combine, records, 0)
+
+            stop_cond = lambda records: aggregate_count(records) >= n_expected
+            records = self._read_all_from_audit_log(filter_fn, stop_cond)
+            assert aggregate_count(
+                records
+            ) == n_expected, f"Expected: {n_expected}, Actual: {aggregate_count(records)}"
+            return records
+
+        self._wait_for_audit_log(timeout_sec=10)
+
+        # The test override the default event type to 'heartbeat', therefore
+        # any actions on the admin server should not result in audit msgs
+        api_calls = {
+            'features/license': self.admin.get_license,
+            'cluster/health_overview': self.admin.get_cluster_health_overview
+        }
+        api_keys = api_calls.keys()
+        call_apis = lambda: [fn() for fn in api_calls.values()]
+        self.logger.debug("Starting 500 api calls with management enabled")
+        for _ in range(0, 500):
+            call_apis()
+        self.logger.debug("Finished 500 api calls with management enabled")
+
+        # Wait 1 second because asserting that 0 records of a type occur
+        # will not continuously loop for the records we are expected not to
+        # see. 1 second is long enough for the audit fibers to run and push
+        # data to the audit topic.
+        time.sleep(1)
+        records = number_of_records_matching(api_keys, 1000)
+        self.redpanda.logger.debug(f"records: {records}")
+
+        # Raises if validation fails on any records
+        [validate_audit_schema(record) for record in records]
+
+        # Remove management setting
+        patch_result = self.admin.patch_cluster_config(
+            upsert={'audit_enabled_event_types': ['heartbeat']})
+        wait_for_version_sync(self.admin, self.redpanda,
+                              patch_result['config_version'])
+
+        self.logger.debug("Started 500 api calls with management disabled")
+        for _ in range(0, 500):
+            call_apis()
+        self.logger.debug("Finished 500 api calls with management disabled")
+        _ = number_of_records_matching(api_keys, 1000)
---
+++ tests/rptest/utils/audit_schemas.py
@@ -0,0 +1,400 @@
+# Copyright 2023 Redpanda Data, Inc.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.md
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0
+
+import jsonschema
+import jsonschema.exceptions
+
+PRODUCT_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'name': {
+            'type': 'string'
+        },
+        'vendor_name': {
+            'type': 'string'
+        },
+        'version': {
+            'type': 'string'
+        }
+    },
+    'required': ['name', 'vendor_name', 'version'],
+    'additionalProperties': False
+}
+
+OCSF_BASE_SCHEMA = {
+    'type':
+    'object',
+    'properties': {
+        'category_uid': {
+            'type': 'number',
+            'enum': [1, 2, 3, 4, 5, 6]
+        },
+        'class_uid': {
+            'type':
+            'number',
+            'enum': [
+                1001, 1002, 1003, 1004, 1005, 1006, 1007, 2001, 3001, 3002,
+                3003, 3004, 3005, 3006, 4001, 4002, 4003, 4004, 4005, 4006,
+                4007, 4008, 4009, 4010, 4011, 4012, 5001, 5002, 6001, 6002,
+                6003, 6004
+            ]
+        },
+        'count': {
+            'type': 'number'
+        },
+        'end_time': {
+            'type': 'number'
+        },
+        'metadata': {
+            'type': 'object',
+            'properties': {
+                'version': {
+                    'type': 'string'
+                },
+                'product': PRODUCT_SCHEMA
+            },
+            'required': ['product', 'version'],
+            'additionalProperties': False
+        },
+        'severity_id': {
+            'type': 'number',
+            'enum': [0, 1, 2, 3, 4, 5, 6, 99]
+        },
+        'start_time': {
+            'type': 'number'
+        },
+        'time': {
+            'type': 'number'
+        },
+        'type_uid': {
+            'type': 'number'
+        }
+    },
+    'required': [
+        'category_uid', 'class_uid', 'metadata', 'severity_id', 'time',
+        'type_uid'
+    ],
+    'additionalProperties':
+    False
+}
+
+ENDPOINT_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'intermediate_ips': {
+            'type': 'array'
+        },
+        'ip': {
+            'type': 'string'
+        },
+        'name': {
+            'type': 'string'
+        },
+        'port': {
+            'type': 'number'
+        },
+        'svc_name': {
+            'type': 'string'
+        },
+        'uid': {
+            'type': 'string'
+        }
+    },
+    'required': ['ip', 'port'],
+    'additionalProperties': False
+}
+
+AUTH_METADATA_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'acl_authorization': {
+            'type': 'object',
+            'properties': {
+                'host': {
+                    'type': 'string'
+                },
+                'op': {
+                    'type': 'string'
+                },
+                'permission_type': {
+                    'type': 'string'
+                },
+                'principal': {
+                    'type': 'string'
+                }
+            },
+            'required': ['host', 'op', 'permission_type', 'principal'],
+            'additionalProperties': False
+        },
+        'resource': {
+            'type': 'object',
+            'properties': {
+                'name': {
+                    'type': 'string'
+                },
+                'pattern': {
+                    'type': 'string'
+                },
+                'type': {
+                    'type': 'string'
+                }
+            },
+            'required': ['name', 'pattern', 'type'],
+            'additionalProperties': False
+        }
+    },
+    'required': ['acl_authorization', 'resource'],
+    'additionalProperties': False
+}
+
+API_ACTIVITY_SCHEMA = {
+    'type':
+    'object',
+    'properties': {
+        'activity_id': {
+            'type': 'number',
+            'enum': [0, 1, 2, 3, 4, 99]
+        },
+        'actor': {
+            'type': 'object',
+            'properties': {
+                'authorizations': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'object',
+                        'properties': {
+                            'decision': {
+                                'type': 'string'
+                            },
+                            'policy': {
+                                'type': 'object',
+                                'properties': {
+                                    'desc': {
+                                        'type': 'string'
+                                    },
+                                    'name': {
+                                        'type': 'string'
+                                    }
+                                },
+                                'required': ['desc', 'name'],
+                                'additionalProperties': False
+                            },
+                        },
+                        'required': ['decision'],
+                        'additionalProperties': False
+                    }
+                },
+                'user': {
+                    'type': 'object',
+                    'properties': {
+                        'credential_uid': {
+                            'type': 'string'
+                        },
+                        'domain': {
+                            'type': 'string'
+                        },
+                        'name': {
+                            'type': 'string'
+                        },
+                        'type_id': {
+                            'type': 'number',
+                            'enum': [0, 1, 2, 3, 99]
+                        }
+                    },
+                    'required': ['name', 'type_id'],
+                    'additionalProperties': False
+                }
+            },
+            'required': ['authorizations', 'user'],
+            'additionalProperties': False
+        },
+        'api': {
+            'type': 'object',
+            'properties': {
+                'operation': {
+                    'type': 'string'
+                }
+            },
+            'required': ['operation'],
+            'additionalProperties': False
+        },
+        'dst_endpoint': ENDPOINT_SCHEMA,
+        'http_request': {
+            'type':
+            'object',
+            'properties': {
+                'http_headers': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'object',
+                        'properties': {
+                            'name': {
+                                'type': 'string'
+                            },
+                            'value': {
+                                'type': 'string'
+                            }
+                        },
+                        'required': ['name', 'value'],
+                        'additionalProperties': False
+                    }
+                },
+                'http_method': {
+                    'type': 'string'
+                },
+                'url': {
+                    'type':
+                    'object',
+                    'properties': {
+                        'hostname': {
+                            'type': 'string'
+                        },
+                        'path': {
+                            'type': 'string'
+                        },
+                        'port': {
+                            'type': 'number'
+                        },
+                        'scheme': {
+                            'type': 'string'
+                        },
+                        'url_string': {
+                            'type': 'string'
+                        }
+                    },
+                    'required':
+                    ['hostname', 'path', 'port', 'scheme', 'url_string'],
+                    'additionalProperties':
+                    False
+                },
+                'user_agent': {
+                    'type': 'string'
+                },
+                'version': {
+                    'type': 'string'
+                }
+            },
+            'required':
+            ['http_headers', 'http_method', 'url', 'user_agent', 'version'],
+            'additionalProperties':
+            False
+        },
+        'resources': {
+            'type': 'object',
+            'properties': {
+                'name': {
+                    'type': 'string'
+                },
+                'type': {
+                    'type': 'string'
+                }
+            },
+            'required': ['name', 'type'],
+            'additionalProperties': False
+        },
+        'src_endpoint': ENDPOINT_SCHEMA,
+        'status_id': {
+            'type': 'number',
+            'enum': [0, 1, 2, 99]
+        },
+        'unmapped': {
+            'type': 'object',
+            'properties': {
+                'authorization_metadata': AUTH_METADATA_SCHEMA
+            },
+            'required': [],
+            'additionalProperties': False
+        }
+    },
+    'required': [
+        'activity_id', 'actor', 'api', 'dst_endpoint', 'src_endpoint',
+        'status_id', 'unmapped'
+    ],
+    'additionalProperties':
+    False
+}
+
+APPLICATION_ACTIVITY_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'activity_id': {
+            'type': 'number',
+            'enum': [0, 1, 2, 3, 4, 99]
+        },
+        'app': PRODUCT_SCHEMA
+    },
+    'required': ['activity_id', 'app'],
+    'additionalProperties': False
+}
+
+
+# A proper schema is an application based schema + the fields that exist
+# in the OCSF base schema.
+def _create_schema(schema):
+    return {
+        'type': 'object',
+        'properties': schema['properties'] | OCSF_BASE_SCHEMA['properties'],
+        'required': schema['required'] + OCSF_BASE_SCHEMA['required'],
+        'additionalProperties': False
+    }
+
+
+# All schemas outside of this list are nested within the supported structures
+# and are not expected to be observed as-is in the audit log
+VALID_AUDIT_SCHEMAS = [
+    _create_schema(schema)
+    for schema in [API_ACTIVITY_SCHEMA, APPLICATION_ACTIVITY_SCHEMA]
+]
+
+
+class AuditSchemeValidationException(Exception):
+    def __init__(self, excs):
+        failures = [str(exc) for exc in excs]
+        self.message = '\n'.join(failures)
+
+
+def validate_audit_contents(message):
+    """
+    This method verifies the validity of the contents of a message that
+    has already passed the schema check.
+    """
+    if 'count' not in message:
+        return 'start_time' not in message and 'end_time' not in message
+
+    if message['count'] <= 1:
+        return False
+    if 'start_time' not in message:
+        return False
+    if 'end_time' not in message:
+        return False
+    return True
+
+
+def validate_audit_schema(message):
+    """
+    Verify that a given message adheres to one of the supported audit log
+    record formats and if one of those checks pass, that the fields within
+    the records are themselves valid.
+    """
+    excs = []
+    for schema in VALID_AUDIT_SCHEMAS:
+        try:
+            jsonschema.validate(instance=message, schema=schema)
+            if not validate_audit_contents(message):
+                excs = [
+                    RuntimeError(
+                        f'Message {message} failed to pass content validation, check "count", "start_time" and "end_time keys/values"'
+                    )
+                ]
+                break
+            return
+        except Exception as e:
+            excs.append(e)
+    assert len(excs) > 0
+    raise AuditSchemeValidationException(excs)
