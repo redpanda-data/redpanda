05de8841ced510b2f6032301c5e2a593e5b2320d
Tyler Rockwood <rockwood@redpanda.com>
2023-10-17 22:25:56 -0500

    ducktape/transform: add a ducktape test for full lifecycle
    Add the boilerplate for a ducktape test for Transforms. I've tried to be
    careful about being robust with retries, adding good error messages
    (mostly by throwing execeptions from wait until with extra context) and
    keeping things simple.
    
    More tests will follow actually verifying the transforms processor
    records correctly. This just ensures the lifecycle is working.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

---
+++ tests/rptest/tests/data_transforms_test.py
@@ -0,0 +1,121 @@
+# Copyright 2023 Redpanda Data, Inc.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.md
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0
+
+from rptest.clients.rpk import RpkTool
+from rptest.services.cluster import cluster
+from ducktape.utils.util import wait_until
+
+from rptest.tests.redpanda_test import RedpandaTest
+from rptest.clients.types import TopicSpec
+
+
+class WasmException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+
+class DataTransformsTest(RedpandaTest):
+    """
+    Tests related to WebAssembly powered data transforms
+    """
+    topics = [TopicSpec(partition_count=9), TopicSpec(partition_count=9)]
+
+    def __init__(self, test_context):
+        super(DataTransformsTest,
+              self).__init__(test_context=test_context,
+                             extra_rp_conf={'data_transforms_enabled': True})
+        self._ctx = test_context
+        self._rpk = RpkTool(self.redpanda)
+
+    def _deploy_wasm(self, name: str):
+        """
+        Deploy a wasm transform and wait for all processors to be running.
+        """
+        [input_topic, output_topic] = self.topics
+
+        def do_deploy():
+            self._rpk.deploy_wasm(name, input_topic.name, output_topic.name)
+            return True
+
+        wait_until(
+            do_deploy,
+            timeout_sec=30,
+            backoff_sec=5,
+            err_msg=f"unable to deploy wasm transform {name}",
+            retry_on_exc=True,
+        )
+
+        def is_all_running():
+            transforms = self._rpk.list_wasm()
+            transform = next((x for x in transforms if x.name == name), None)
+            if not transform:
+                raise WasmException(f"missing transform {name} from report")
+            for partition_id in range(0, input_topic.partition_count):
+                processor = next(
+                    (p
+                     for p in transform.status if p.partition == partition_id),
+                    None)
+                if not processor:
+                    raise WasmException(
+                        f"missing processor {name}/{partition_id} from report")
+                if processor.status != "running":
+                    raise WasmException(
+                        f"processor {name}/{partition_id} is not running: {processor.status}"
+                    )
+            return True
+
+        wait_until(
+            is_all_running,
+            timeout_sec=30,
+            backoff_sec=1,
+            err_msg=f"wasm transform {name} did not reach steady state",
+            retry_on_exc=True,
+        )
+
+    def _delete_wasm(self, name: str):
+        """
+        Delete a wasm transform and wait for all processors to stop.
+        """
+        def do_deploy():
+            self._rpk.delete_wasm(name)
+            return True
+
+        wait_until(
+            do_deploy,
+            timeout_sec=30,
+            backoff_sec=5,
+            err_msg=f"unable to delete wasm transform {name}",
+            retry_on_exc=True,
+        )
+
+        def transform_is_gone():
+            transforms = self._rpk.list_wasm()
+            transform = next((x for x in transforms if x.name == name), None)
+            if transform:
+                raise WasmException(f"transform {name} still in report")
+            return True
+
+        wait_until(
+            transform_is_gone,
+            timeout_sec=30,
+            backoff_sec=1,
+            err_msg=f"wasm transform {name} was not fully shutdown",
+            retry_on_exc=True,
+        )
+
+    @cluster(num_nodes=3)
+    def test_lifecycle(self):
+        """
+        Test that a Wasm binary can be deployed, reach steady state and then be deleted.
+        """
+        self._deploy_wasm("identity-xform")
+        self._delete_wasm("identity-xform")
