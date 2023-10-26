2bb6e3c3da8d48199ef3e77b798ad0149439f2f8
Tyler Rockwood <rockwood@redpanda.com>
2023-10-26 14:49:12 -0500

    resource_mgmt/memory: allocate memory after user code
    If wasm is enabled, we need to account for the memory that a user has
    carved out for their code when accounting for the rest of the system's
    memory.
    
    By default the wasm per core memory configuration value is 20MB. It's
    only used if transforms are enabled.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/resource_mgmt/memory_groups.h
+++ src/v/resource_mgmt/memory_groups.h
@@ -11,20 +11,24 @@
 
 #pragma once
 
+#include "config/configuration.h"
+#include "config/node_config.h"
 #include "seastarx.h"
 
 #include <seastar/core/memory.hh>
 
 // centralized unit for memory management
-struct memory_groups {
+class memory_groups {
+public:
     static size_t kafka_total_memory() {
         // 30%
-        return ss::memory::stats().total_memory() * .30;
+        return total_memory() * .30; // NOLINT
     }
+
     /// \brief includes raft & all services
     static size_t rpc_total_memory() {
         // 20%
-        return ss::memory::stats().total_memory() * .20;
+        return total_memory() * .20; // NOLINT
     }
 
     /**
@@ -35,7 +39,7 @@ struct memory_groups {
      * add upper bound of 30 pct of memory as a hard outstanding limit.
      */
     static size_t chunk_cache_min_memory() {
-        return ss::memory::stats().total_memory() * .10; // NOLINT
+        return total_memory() * .10; // NOLINT
     }
 
     /**
@@ -44,15 +48,32 @@ struct memory_groups {
      * returned to the cache.
      */
     static size_t chunk_cache_max_memory() {
-        return ss::memory::stats().total_memory() * .30; // NOLINT
+        return total_memory() * .30; // NOLINT
     }
 
     static size_t recovery_max_memory() {
-        return ss::memory::stats().total_memory() * .10;
+        return total_memory() * .10; // NOLINT
     }
 
     /// Max memory that both cloud storage uploads and read-path could use
     static size_t tiered_storage_max_memory() {
-        return ss::memory::stats().total_memory() * .10;
+        return total_memory() * .10; // NOLINT
+    }
+
+private:
+    /**
+     * Total memory for a core after the user's Wasm reservation.
+     */
+    static size_t total_memory() {
+        size_t total = ss::memory::stats().total_memory();
+        if (
+          config::shard_local_cfg().data_transforms_enabled.value()
+          && !config::node().emergency_disable_data_transforms.value()) {
+            size_t wasm_memory_reservation
+              = config::shard_local_cfg()
+                  .wasm_per_core_memory_reservation.value();
+            total -= wasm_memory_reservation;
+        }
+        return total;
     }
 };
