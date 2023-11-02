c97fe3e92cc2b3a0ede9ba3f1f7ef24ac4b5314c
Michael Boquard <michael@redpanda.com>
2023-11-01 20:55:25 -0400

    audit: Updated license headers
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/security/audit/audit_log_manager.cc
+++ src/v/security/audit/audit_log_manager.cc
@@ -1,11 +1,12 @@
-// Copyright 2023 Redpanda Data, Inc.
-//
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.md
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0
+/*
+ * Copyright 2023 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
 
 #include "security/audit/audit_log_manager.h"
 
