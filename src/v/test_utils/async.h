f2ac706cebdb981d41b250e4e152d09b8d3ae5f4
Tyler Rockwood <rockwood@redpanda.com>
2023-11-02 20:35:10 -0500

    tests: introduce seastar task queue drain function
    This drain function is the pattern used in transform_manager_test
    hacking around idle_cpu_handler to tell us when the seastar queue is
    empty.
    
    There is already a function here to do this, but it crudely sleeps to
    get around debug build task reordering, while this approach is more
    robust and does not require the sleep hack.
    
    Ideally `drain_task_queue` can replace all usages of `flush_tasks` but
    I'll save that for another day.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/test_utils/async.h
+++ src/v/test_utils/async.h
@@ -17,6 +17,7 @@
 #include "vassert.h"
 
 #include <seastar/core/future-util.hh>
+#include <seastar/core/idle_cpu_handler.hh>
 #include <seastar/core/lowres_clock.hh>
 #include <seastar/core/reactor.hh>
 #include <seastar/core/sleep.hh>
@@ -113,6 +114,41 @@ inline void flush_tasks() {
 #ifndef NDEBUG
     ss::sleep(10ms).get();
 #endif
+}
+
+/**
+ * This allows us to wait for the seastar queue to be drained.
+ *
+ * This can used when we need to wait for manual_clock tasks to
+ * execute to ensure that task execution is deterministic. Because in
+ * debug mode seastar randomizes task order, so there is no way to wait
+ * for those tasks to be executed outside of draining the seastar queue.
+ *
+ * The above function's caveats are valid for this function, but it does not
+ * require the sleep hack.
+ */
+inline ss::future<> drain_task_queue() {
+    return ss::smp::invoke_on_all([] {
+        // Using an optional ensures that this is a noop on subsequent idle CPU
+        // callbacks.
+        std::optional<ss::promise<>> p;
+        p.emplace();
+        auto fut = p->get_future();
+        ss::set_idle_cpu_handler(
+          [p = std::move(p)](ss::work_waiting_on_reactor) mutable {
+              if (!p) {
+                  return ss::idle_cpu_handler_result::no_more_work;
+              }
+              p->set_value();
+              p.reset();
+              // this tells the reactor loop to go back and check
+              // for more work, which we should have just enqueued because of
+              // completing the promise above.
+              return ss::idle_cpu_handler_result::
+                interrupted_by_higher_priority_task;
+          });
+        return fut;
+    });
 }
 
 }; // namespace tests
