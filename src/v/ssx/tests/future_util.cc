8cdf1dbb87985e9bedf545847594a07e0531bfa3
Tyler Rockwood <rockwood@redpanda.com>
2023-11-05 20:36:30 -0600

    ssx/with_timeout_abortable: support custom exceptions
    It's possible to throw custom exceptions in ss::abort_source, and the
    transform subsystem uses a custom exception here to ensure that all
    aborts are explicitly accounted for and the shutdown sequence is clean.
    
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/ssx/tests/future_util.cc
+++ src/v/ssx/tests/future_util.cc
@@ -9,6 +9,7 @@
 
 #include "ssx/future-util.h"
 
+#include <seastar/core/lowres_clock.hh>
 #include <seastar/core/manual_clock.hh>
 #include <seastar/testing/thread_test_case.hh>
 
@@ -111,5 +112,38 @@ SEASTAR_THREAD_TEST_CASE(with_timeout_abortable_test) {
 
         seastar::manual_clock::advance(100ms);
         BOOST_CHECK_THROW(res.get(), seastar::abort_requested_exception);
+    }
+}
+
+SEASTAR_THREAD_TEST_CASE(with_timeout_abortable_custom_ex_test) {
+    // It's possible to throw custom exceptions with abort_source, make sure
+    // we propagate them correctly.
+    class custom_abort_exception : public std::exception {};
+    {
+        // Already aborted source
+        seastar::abort_source as;
+        as.request_abort_ex(custom_abort_exception{});
+
+        auto f = seastar::sleep<seastar::manual_clock>(50ms).then(
+          [] { return seastar::make_ready_future<int>(123); });
+        auto res = ssx::with_timeout_abortable(
+          std::move(f), seastar::manual_clock::time_point::max(), as);
+        BOOST_CHECK_THROW(res.get(), custom_abort_exception);
+        // f has to resolve or LSAN thinks there are leaks
+        seastar::manual_clock::advance(50ms);
+    }
+    {
+        seastar::abort_source as;
+
+        auto f = seastar::sleep<seastar::manual_clock>(50ms).then(
+          [] { return seastar::make_ready_future<int>(123); });
+        auto res = ssx::with_timeout_abortable(
+          std::move(f), seastar::manual_clock::time_point::max(), as);
+
+        seastar::manual_clock::advance(10ms);
+        as.request_abort_ex(custom_abort_exception{});
+        BOOST_CHECK_THROW(res.get(), custom_abort_exception);
+        // f has to resolve or LSAN thinks there are leaks
+        seastar::manual_clock::advance(40ms);
     }
 }
