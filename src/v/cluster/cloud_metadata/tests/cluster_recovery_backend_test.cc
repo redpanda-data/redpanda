6448f8104bd5e5c642a917e6683b995510f4524d
Andrew Wong <awong@redpanda.com>
2023-11-07 18:01:31 -0800

    cluster_recovery_backend_test: switch over to gtest
    I intend on parameterizing a test, and gtest makes that easier.

--- src/v/cluster/cloud_metadata/tests/cluster_recovery_backend_test.cc
+++ src/v/cluster/cloud_metadata/tests/cluster_recovery_backend_test.cc
@@ -28,6 +28,7 @@
 #include "security/scram_credential.h"
 #include "security/types.h"
 #include "test_utils/scoped_config.h"
+#include "test_utils/test.h"
 #include "utils/retry_chain_node.h"
 
 #include <seastar/core/lowres_clock.hh>
@@ -38,21 +39,23 @@ ss::logger logger("backend_test");
 static ss::abort_source never_abort;
 } // anonymous namespace
 
-class cluster_recovery_backend_test
-  : public s3_imposter_fixture
+class ClusterRecoveryBackendTest
+  : public seastar_test
+  , public s3_imposter_fixture
   , public redpanda_thread_fixture
   , public enable_cloud_storage_fixture {
 public:
-    cluster_recovery_backend_test()
+    ClusterRecoveryBackendTest()
       : redpanda_thread_fixture(
         redpanda_thread_fixture::init_cloud_storage_tag{}, httpd_port_number())
       , raft0(app.partition_manager.local().get(model::controller_ntp)->raft())
       , controller_stm(app.controller->get_controller_stm().local())
       , remote(app.cloud_storage_api.local())
-      , bucket(cloud_storage_clients::bucket_name("test-bucket")) {
-        set_expectations_and_listen({});
-        wait_for_controller_leadership().get();
-        RPTEST_REQUIRE_EVENTUALLY(5s, [this] {
+      , bucket(cloud_storage_clients::bucket_name("test-bucket")) {}
+    ss::future<> SetUpAsync() override {
+        co_await ss::async([&] { set_expectations_and_listen({}); });
+        co_await wait_for_controller_leadership();
+        RPTEST_REQUIRE_EVENTUALLY_CORO(5s, [this] {
             return app.storage.local().get_cluster_uuid().has_value();
         });
         cluster_uuid = app.storage.local().get_cluster_uuid().value();
@@ -66,14 +69,14 @@ protected:
     model::cluster_uuid cluster_uuid;
 };
 
-FIXTURE_TEST(test_recover_controller_state, cluster_recovery_backend_test) {
+TEST_F(ClusterRecoveryBackendTest, TestRecoveryControllerState) {
     // Create a license.
     auto license = get_test_license();
     auto err = app.controller->get_feature_manager()
                  .local()
                  .update_license(std::move(license))
                  .get();
-    BOOST_REQUIRE(!err);
+    ASSERT_TRUE(!err);
 
     // Update the cluster config (via the controller, rather than shard local).
     cluster::config_update_request req;
@@ -133,8 +136,8 @@ FIXTURE_TEST(test_recover_controller_state, cluster_recovery_backend_test) {
     manifest.cluster_uuid = cluster_uuid;
     uploader.upload_next_metadata(raft0->confirmed_term(), manifest, retry_node)
       .get();
-    BOOST_REQUIRE_EQUAL(manifest.metadata_id, cluster_metadata_id(0));
-    BOOST_REQUIRE(!manifest.controller_snapshot_path.empty());
+    ASSERT_EQ(manifest.metadata_id, cluster_metadata_id(0));
+    ASSERT_TRUE(!manifest.controller_snapshot_path.empty());
 
     // Create a new cluster.
     raft0 = nullptr;
@@ -145,24 +148,23 @@ FIXTURE_TEST(test_recover_controller_state, cluster_recovery_backend_test) {
     });
 
     // Sanity check we have a different cluster.
-    BOOST_REQUIRE(
+    ASSERT_TRUE(
       !app.controller->get_feature_table().local().get_license().has_value());
-    BOOST_REQUIRE_NE(
+    ASSERT_NE(
       1, config::shard_local_cfg().log_segment_size_jitter_percent.value());
-    BOOST_REQUIRE(!app.controller->get_credential_store().local().contains(
+    ASSERT_TRUE(!app.controller->get_credential_store().local().contains(
       security::credential_user{"userguy"}));
-    BOOST_REQUIRE_EQUAL(
+    ASSERT_EQ(
       0, app.controller->get_authorizer().local().all_bindings().get().size());
-    BOOST_REQUIRE_EQUAL(
-      0, app.controller->get_topics_state().local().all_topics_count());
+    ASSERT_EQ(0, app.controller->get_topics_state().local().all_topics_count());
 
     // Perform recovery.
     auto recover_err = app.controller->get_cluster_recovery_manager()
                          .local()
                          .initialize_recovery(bucket)
                          .get();
-    BOOST_REQUIRE(recover_err.has_value());
-    BOOST_REQUIRE_EQUAL(recover_err.value(), cluster::errc::success);
+    ASSERT_TRUE(recover_err.has_value());
+    ASSERT_EQ(recover_err.value(), cluster::errc::success);
     RPTEST_REQUIRE_EVENTUALLY(10s, [&] {
         return !app.controller->get_cluster_recovery_table()
                   .local()
@@ -171,18 +173,18 @@ FIXTURE_TEST(test_recover_controller_state, cluster_recovery_backend_test) {
 
     // Validate the controller state is restored.
     auto validate_post_recovery = [&] {
-        BOOST_REQUIRE(app.controller->get_feature_table()
-                        .local()
-                        .get_license()
-                        .has_value());
-        BOOST_REQUIRE_EQUAL(
+        ASSERT_TRUE(app.controller->get_feature_table()
+                      .local()
+                      .get_license()
+                      .has_value());
+        ASSERT_EQ(
           1, config::shard_local_cfg().log_segment_size_jitter_percent.value());
-        BOOST_REQUIRE(app.controller->get_credential_store().local().contains(
+        ASSERT_TRUE(app.controller->get_credential_store().local().contains(
           security::credential_user{"userguy"}));
-        BOOST_REQUIRE_EQUAL(
+        ASSERT_EQ(
           1,
           app.controller->get_authorizer().local().all_bindings().get().size());
-        BOOST_REQUIRE_EQUAL(
+        ASSERT_EQ(
           2, app.controller->get_topics_state().local().all_topics_count());
         for (const auto& [ntp, p] :
              app.partition_manager.local().partitions()) {
@@ -190,9 +192,9 @@ FIXTURE_TEST(test_recover_controller_state, cluster_recovery_backend_test) {
                 continue;
             }
             if (p->archiver().has_value()) {
-                BOOST_REQUIRE_EQUAL(p->ntp().tp.topic(), "remote_foo");
+                ASSERT_EQ(p->ntp().tp.topic(), "remote_foo");
             } else {
-                BOOST_REQUIRE_EQUAL(p->ntp().tp.topic(), "foo");
+                ASSERT_EQ(p->ntp().tp.topic(), "foo");
             }
         }
     };
@@ -211,7 +213,7 @@ FIXTURE_TEST(test_recover_controller_state, cluster_recovery_backend_test) {
     validate_post_recovery();
 }
 
-FIXTURE_TEST(test_recover_failed_action, cluster_recovery_backend_test) {
+TEST_F(ClusterRecoveryBackendTest, TestRecoverFailedAction) {
     // Create a remote topic, but don't upload any of its manifests or anything.
     scoped_config task_local_cfg;
     task_local_cfg.get("cloud_storage_disable_upload_loop_for_tests")
@@ -229,8 +231,8 @@ FIXTURE_TEST(test_recover_failed_action, cluster_recovery_backend_test) {
     manifest.cluster_uuid = cluster_uuid;
     uploader.upload_next_metadata(raft0->confirmed_term(), manifest, retry_node)
       .get();
-    BOOST_REQUIRE_EQUAL(manifest.metadata_id, cluster_metadata_id(0));
-    BOOST_REQUIRE(!manifest.controller_snapshot_path.empty());
+    ASSERT_EQ(manifest.metadata_id, cluster_metadata_id(0));
+    ASSERT_TRUE(!manifest.controller_snapshot_path.empty());
 
     // Create a new cluster.
     raft0 = nullptr;
@@ -256,14 +258,14 @@ FIXTURE_TEST(test_recover_failed_action, cluster_recovery_backend_test) {
                               .current_recovery()
                               .value()
                               .get();
-    BOOST_REQUIRE_EQUAL(latest_recovery.stage, cluster::recovery_stage::failed);
-    BOOST_REQUIRE(latest_recovery.error_msg.has_value());
-    BOOST_REQUIRE(latest_recovery.error_msg.value().contains(
+    ASSERT_EQ(latest_recovery.stage, cluster::recovery_stage::failed);
+    ASSERT_TRUE(latest_recovery.error_msg.has_value());
+    ASSERT_TRUE(latest_recovery.error_msg.value().contains(
       "Failed to apply action for "
       "recovery_stage::recovered_remote_topic_data"));
 }
 
-FIXTURE_TEST(test_recover_failed_download, cluster_recovery_backend_test) {
+TEST_F(ClusterRecoveryBackendTest, TestRecoverFailedDownload) {
     // Create a some topic to be able to upload a snapshot.
     model::topic_namespace remote_tp_ns{
       model::kafka_namespace, model::topic{"remote_foo"}};
@@ -278,8 +280,8 @@ FIXTURE_TEST(test_recover_failed_download, cluster_recovery_backend_test) {
     manifest.cluster_uuid = cluster_uuid;
     uploader.upload_next_metadata(raft0->confirmed_term(), manifest, retry_node)
       .get();
-    BOOST_REQUIRE_EQUAL(manifest.metadata_id, cluster_metadata_id(0));
-    BOOST_REQUIRE(!manifest.controller_snapshot_path.empty());
+    ASSERT_EQ(manifest.metadata_id, cluster_metadata_id(0));
+    ASSERT_TRUE(!manifest.controller_snapshot_path.empty());
 
     // Create a new cluster.
     raft0 = nullptr;
@@ -295,7 +297,7 @@ FIXTURE_TEST(test_recover_failed_download, cluster_recovery_backend_test) {
                      manifest.controller_snapshot_path},
                    retry_node)
                  .get();
-    BOOST_REQUIRE_EQUAL(res, cloud_storage::upload_result::success);
+    ASSERT_EQ(res, cloud_storage::upload_result::success);
 
     // Attempt a recovery.
     auto recover_err = app.controller->get_cluster_recovery_manager()
@@ -314,8 +316,8 @@ FIXTURE_TEST(test_recover_failed_download, cluster_recovery_backend_test) {
                               .current_recovery()
                               .value()
                               .get();
-    BOOST_REQUIRE_EQUAL(latest_recovery.stage, cluster::recovery_stage::failed);
-    BOOST_REQUIRE(latest_recovery.error_msg.has_value());
-    BOOST_REQUIRE(latest_recovery.error_msg.value().contains(
+    ASSERT_EQ(latest_recovery.stage, cluster::recovery_stage::failed);
+    ASSERT_TRUE(latest_recovery.error_msg.has_value());
+    ASSERT_TRUE(latest_recovery.error_msg.value().contains(
       "Failed to download controller snapshot"));
 }
