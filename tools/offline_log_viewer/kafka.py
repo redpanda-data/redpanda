26f15060ee285404d182c865090938b7f6b8ba8e
Noah Watkins <noahwatkins@gmail.com>
2023-11-08 20:01:16 -0800

    tools: decode archival metadata stm commands
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- tools/offline_log_viewer/kafka.py
+++ tools/offline_log_viewer/kafka.py
@@ -1,5 +1,5 @@
-from storage import Segment
-from enum import Enum
+from storage import Segment, BatchType
+from enum import Enum, IntEnum
 from io import BytesIO
 from reader import Reader
 import struct
@@ -15,6 +15,65 @@ class KafkaControlRecordType(Enum):
     unknown = -1
 
 
+class ArchivalMetadataCommand(IntEnum):
+    add_segment = 0
+    cleanup_metadata = 3
+    mark_clean = 4
+    replace_manifest = 10
+
+
+def read_cloud_storage_segment_meta(rdr):
+    def spec(rdr, version):
+        assert version >= 2, f"Version {version} < 2 unsupported"
+        o = {}
+        o["is_compacted"] = rdr.read_bool()
+        o["size_bytes"] = rdr.read_int64()
+        o["base_offset"] = rdr.read_int64()
+        o["committed_offset"] = rdr.read_int64()
+        o["base_timestamp"] = rdr.read_int64()
+        o["max_timestamp"] = rdr.read_int64()
+        o["delta_offset"] = rdr.read_int64()
+        o["ntp_revision"] = rdr.read_int64()
+        o["archiver_term"] = rdr.read_int64()
+        o["segment_term"] = rdr.read_int64()
+        o["delta_offset_end"] = rdr.read_int64()
+        o["sname_format"] = rdr.read_int16()
+        if version >= 3:
+            o["metadata_size_hint"] = rdr.read_int64()
+        else:
+            o["metadata_size_hint"] = 0
+        return o
+
+    return rdr.read_envelope(spec, max_version=3)
+
+
+def decode_archival_metadata_command(kr, vr):
+    key = kr.read_int8()
+    if key == ArchivalMetadataCommand.add_segment:
+
+        def spec(rdr, version):
+            o = {}
+            o['ntp_revision_deprecated'] = rdr.read_int64(),
+            o['name'] = rdr.read_string(),
+            o['meta'] = read_cloud_storage_segment_meta(rdr),
+            if version >= 1:
+                o['is_validated'] = rdr.read_bool(),
+            else:
+                o['is_validated'] = False
+            return o
+
+        return vr.read_envelope(spec, max_version=1)
+
+    elif key == ArchivalMetadataCommand.mark_clean:
+        return dict(offset=vr.read_int64())
+
+    elif key == ArchivalMetadataCommand.cleanup_metadata:
+        return dict()
+
+    elif key == ArchivalMetadataCommand.replace_manifest:
+        return dict(size=len(vr.read_iobuf()))
+
+
 def decode_record(batch, header, record):
     attrs = header["expanded_attrs"]
     record_dict = record.kv_dict()
@@ -24,6 +83,16 @@ def decode_record(batch, header, record):
     is_tx_ctrl = is_txn and is_ctrl
     if is_tx_ctrl:
         record_dict["type"] = self.get_control_record_type(record.key)
+
+    kr = Reader(BytesIO(record.key))
+    vr = Reader(BytesIO(record.value))
+
+    decoded = None
+    if batch.type == BatchType.archival_metadata:
+        decoded = decode_archival_metadata_command(kr, vr)
+
+    if decoded is not None:
+        return decoded
 
     return record_dict
 
