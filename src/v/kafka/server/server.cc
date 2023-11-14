6196b0775df01d58f88908bae9b1adecfa562780
Oren Leiman <oren.leiman@redpanda.com>
2023-11-14 13:23:09 -0800

    kafka/server: Add authZ to add_offsets_to_txn_handler
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/kafka/server/server.cc
+++ src/v/kafka/server/server.cc
@@ -874,6 +874,19 @@ add_offsets_to_txn_handler::handle(request_context ctx, ss::smp_service_group) {
             add_offsets_to_txn_response response;
             response.data.error_code = error_code::policy_violation;
             return ctx.respond(response);
+        } else if (!ctx.authorized(
+                     security::acl_operation::write,
+                     transactional_id{request.data.transactional_id})) {
+            add_offsets_to_txn_response response;
+            response.data.error_code
+              = error_code::transactional_id_authorization_failed;
+            return ctx.respond(response);
+        } else if (!ctx.authorized(
+                     security::acl_operation::read,
+                     group_id{request.data.group_id})) {
+            add_offsets_to_txn_response response;
+            response.data.error_code = error_code::group_authorization_failed;
+            return ctx.respond(response);
         }
 
         cluster::add_offsets_tx_request tx_request{
