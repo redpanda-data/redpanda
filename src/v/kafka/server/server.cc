d9531aaa534ad542e3adba4d452c3a325de1460c
Oren Leiman <oren.leiman@redpanda.com>
2023-11-20 11:52:36 -0800

    kafka/server: Add audit logging to end_txn_handler
    Also adds a convenience constructor for errored `end_txn_response`
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/kafka/server/server.cc
+++ src/v/kafka/server/server.cc
@@ -814,11 +814,17 @@ ss::future<response_ptr> end_txn_handler::handle(
         } else if (!ctx.authorized(
                      security::acl_operation::write,
                      transactional_id{request.data.transactional_id})) {
-            end_txn_response response;
-            response.data.error_code
-              = error_code::transactional_id_authorization_failed;
-            return ctx.respond(response);
+            auto ec = !ctx.audit()
+                        ? error_code::broker_not_available
+                        : error_code::transactional_id_authorization_failed;
+            return ctx.respond(end_txn_response{ec});
         }
+
+        if (!ctx.audit()) {
+            return ctx.respond(
+              end_txn_response{error_code::broker_not_available});
+        }
+
         cluster::end_tx_request tx_request{
           .transactional_id = request.data.transactional_id,
           .producer_id = request.data.producer_id,
