8abbedd0440c1a7a29da4c2243536845d96326e4
Andrea Barbadoro <andij.cr@gmail.com>
2023-11-22 14:58:06 +0100

    cloud_storage_fuzzy_rpfixture: remove potential issues with unit test
    see issue/11271

--- src/v/cloud_storage/tests/remote_partition_fuzz_test.cc
+++ src/v/cloud_storage/tests/remote_partition_fuzz_test.cc
@@ -372,13 +372,13 @@ FIXTURE_TEST(
 namespace {
 
 ss::future<> scan_until_close(
-  remote_partition& partition,
-  const storage::log_reader_config& reader_config,
+  ss::shared_ptr<remote_partition> partition,
+  storage::log_reader_config reader_config,
   ss::gate& g) {
     auto guard = g.hold();
     while (!g.is_closed()) {
         try {
-            auto translating_reader = co_await partition.make_reader(
+            auto translating_reader = co_await partition->make_reader(
               reader_config);
             auto reader = std::move(translating_reader.reader);
             auto headers_read = co_await reader.consume(
@@ -418,7 +418,11 @@ FIXTURE_TEST(test_scan_while_shutting_down, cloud_storage_fixture) {
     auto partition_stop = ss::defer([&partition] { partition->stop().get(); });
 
     ss::gate g;
-    ssx::background = scan_until_close(*partition, reader_config, g);
+    auto scan_future = scan_until_close(
+      partition,
+      storage::log_reader_config(
+        base, model::offset::max(), ss::default_priority_class()),
+      g);
     auto close_fut = ss::maybe_yield()
                        .then([] { return ss::maybe_yield(); })
                        .then([] { return ss::maybe_yield(); })
@@ -431,9 +435,22 @@ FIXTURE_TEST(test_scan_while_shutting_down, cloud_storage_fixture) {
                        });
 
     // NOTE: see issues/11271
-    BOOST_TEST_INFO("scan_unit_close should terminate in a finite amount of "
-                    "time at shutdown");
-    auto timeout_fut = ss::with_timeout(
-      model::timeout_clock::now() + 60s, std::move(close_fut));
-    BOOST_REQUIRE_NO_THROW(timeout_fut.get());
+    BOOST_TEST_CONTEXT("scan_unit_close should terminate in a finite amount of "
+                       "time at shutdown") {
+        auto timeout_fut = ss::with_timeout(
+          model::timeout_clock::now() + 60s, std::move(close_fut));
+        try {
+            BOOST_TEST_INFO("waiting on close future with timeout");
+            timeout_fut.get();
+            BOOST_TEST_INFO("waiting on scan_future");
+            BOOST_CHECK(scan_future.available());
+            scan_future.get();
+        } catch (...) {
+            // BOOST_REQUIRE_NOTHROW can't print std::current_exception, sadly
+            BOOST_CHECK_MESSAGE(
+              false,
+              fmt::format(
+                "failed with exception {}", std::current_exception()));
+        }
+    }
 }
