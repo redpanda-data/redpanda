58a623357a4e2fac50269e020b4203f7a40de9c5
Andrea Barbadoro <andij.cr@gmail.com>
2023-11-22 12:25:54 +0100

    test_utils/fixture: add support for decorators in FIXTURE_TEST macro

--- src/v/test_utils/fixture.h
+++ src/v/test_utils/fixture.h
@@ -22,7 +22,7 @@
 #define __FIXTURE_JOIN_(a, b) a ##_## b
 #define __FIXTURE_JOIN(a, b) __FIXTURE_JOIN_(a, b)
 // clang-format on
-#define FIXTURE_TEST(method, klass)                                            \
+#define FIXTURE_TEST_WITH_DECO(method, klass, decorators)                      \
     class __FIXTURE_JOIN(klass, method) final : klass {                        \
     public:                                                                    \
         void fixture_test();                                                   \
@@ -38,10 +38,21 @@
     private:                                                                   \
         static inline ss::logger g_seastar_test_log{"" #klass "::" #method};   \
     };                                                                         \
-    SEASTAR_THREAD_TEST_CASE(method) {                                         \
+    SEASTAR_THREAD_TEST_CASE(method, decorators) {                             \
         BOOST_TEST_CHECKPOINT("" << #klass << "::" << #method << "()");        \
         __FIXTURE_JOIN(klass, method) _fixture_driver;                         \
         _fixture_driver.fixture_test();                                        \
         BOOST_TEST_CHECKPOINT("~" << #klass << "::" << #method << "()");       \
     }                                                                          \
     void ::__FIXTURE_JOIN(klass, method)::fixture_test()
+
+#define FIXTURE_TEST_WITHOUT_DECO(method, klass)                               \
+    FIXTURE_TEST_WITH_DECO(                                                    \
+      method, klass, boost::unit_test::decorator::collector_t::instance())
+
+#define FIXTURE_TEST(...)                                                      \
+    BOOST_PP_IIF(                                                              \
+      BOOST_PP_EQUAL(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), 2),                  \
+      FIXTURE_TEST_WITHOUT_DECO,                                               \
+      FIXTURE_TEST_WITH_DECO)                                                  \
+    (__VA_ARGS__)
