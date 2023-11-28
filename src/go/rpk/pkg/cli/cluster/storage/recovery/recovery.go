b2180d66f01215b6867b231838cf904900bc509b
Rogger Vasquez <rvasque3@gmail.com>
2023-11-28 10:52:00 -0800

    rpk: rename storage recovery to restore.
    We are introducing a new recovery mode and this
    can lead to confusion.

--- src/go/rpk/pkg/cli/cluster/storage/recovery/recovery.go
+++ src/go/rpk/pkg/cli/cluster/storage/recovery/recovery.go
@@ -17,19 +17,20 @@ import (
 
 func NewCommand(fs afero.Fs, p *config.Params) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "recovery",
-		Short: "Interact with the topic recovery process",
-		Long: `Interact with the topic recovery process.
+		Use:     "restore",
+		Aliases: []string{"recovery"},
+		Short:   "Interact with the topic restoration process",
+		Long: `Interact with the topic restoration process.
 		
 This command is used to restore topics from the archival bucket, which can be 
 useful for disaster recovery or if a topic was accidentally deleted.
 
-To begin the recovery process, use the "recovery start" command. Note that this 
+To begin the recovery process, use the "restore start" command. Note that this 
 process can take a while to complete, so the command will exit after starting 
 it. If you want the command to wait for the process to finish, use the "--wait"
 or "-w" flag.
 
-You can check the status of the recovery process with the "recovery status" 
+You can check the status of the recovery process with the "restore status" 
 command after it has been started.
 `,
 	}
