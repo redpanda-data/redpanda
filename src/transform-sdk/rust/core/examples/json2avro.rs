ac7494c3dbcaab504e3f07acad9b3d7e3c4ed6e4
Tyler Rockwood <rockwood@redpanda.com>
2023-11-28 13:43:37 -0600

    transform-sdk/rust: simplify avro example
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/transform-sdk/rust/core/examples/json2avro.rs
+++ src/transform-sdk/rust/core/examples/json2avro.rs
@@ -14,40 +14,29 @@
 
 extern crate redpanda_transform_sdk as redpanda;
 
-use std::collections::HashMap;
-
 use anyhow::Result;
 use redpanda::*;
 
 fn main() -> Result<()> {
     let schema = apache_avro::Schema::parse_str(
-        r#"
-        {"type": "record", "name":"Interop", "namespace": "org.apache.avro",
+        r#"{
+          "type": "record",
+          "name":"Example",
+          "namespace": "org.apache.avro",
           "fields": [
-              {"name": "intField", "type": "int"},
-              {"name": "longField", "type": "long"},
-              {"name": "stringField", "type": "string"},
-              {"name": "boolField", "type": "boolean"},
-              {"name": "floatField", "type": "float"},
-              {"name": "doubleField", "type": "double"},
-              {"name": "bytesField", "type": "bytes"},
-              {"name": "nullField", "type": "null"},
-              {"name": "arrayField", "type": {"type": "array", "items": "double"}},
-              {"name": "mapField", "type":
-               {"type": "map", "values":
-                {"type": "record", "name": "Foo",
-                 "fields": [{"name": "label", "type": "string"}]}}},
-              {"name": "unionField", "type":
-               ["boolean", "double", {"type": "array", "items": "bytes"}]},
-              {"name": "enumField", "type":
-               {"type": "enum", "name": "Kind", "symbols": ["A","B","C"]}},
-              {"name": "fixedField", "type":
-               {"type": "fixed", "name": "MD5", "size": 16}},
-              {"name": "recordField", "type":
-               {"type": "record", "name": "Node",
-                "fields": [
-                    {"name": "label", "type": "string"},
-                    {"name": "children", "type": {"type": "array", "items": "Node"}}]}}
+              {"name": "a", "type": "int"},
+              {"name": "b", "type": "string"},
+              {
+                "name": "nested", 
+                "type": {
+                  "type": "record",
+                  "name": "Node",
+                  "fields": [
+                      {"name": "label", "type": "string"},
+                      {"name": "children", "type": {"type": "array", "items": "Node"}}
+                  ]
+                }
+              }
           ]
         }"#,
     )?;
@@ -99,10 +88,10 @@ fn json2avro(v: JsonValue) -> Result<AvroValue> {
         JsonValue::Array(a) => {
             AvroValue::Array(a.into_iter().map(json2avro).collect::<Result<Vec<_>>>()?)
         }
-        JsonValue::Object(o) => AvroValue::Map(
+        JsonValue::Object(o) => AvroValue::Record(
             o.into_iter()
                 .map(|(k, v)| json2avro(v).map(|v| (k, v)))
-                .collect::<Result<HashMap<_, _>>>()?,
+                .collect::<Result<Vec<_>>>()?,
         ),
     })
 }
