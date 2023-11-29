f27fc62144f81a038195c0e24af5bd781238ccc9
Noah Watkins <noahwatkins@gmail.com>
2023-11-29 15:55:10 -0800

    io: add clang-tidy helper for headers
    Enable stricter clang-tidy checks for headers that aren't included
    except for in tests where checks are less strict.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ src/v/io/clang-tidy-helper.cc
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2023 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#include "io/cache.h"
+#include "io/interval_map.h"
+#include "io/page.h"
+#include "io/page_set.h"
+#include "io/persistence.h"
