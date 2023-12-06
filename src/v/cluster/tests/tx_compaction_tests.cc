d9d398a8bc6833e79bba8e63751b8c85c97e4ba4
Andrea Barbadoro <andij.cr@gmail.com>
2023-12-06 16:59:57 +0100

    tx_compaction_test setup ntp for short segment.ms rolling

--- src/v/cluster/tests/tx_compaction_tests.cc
+++ src/v/cluster/tests/tx_compaction_tests.cc
@@ -12,7 +12,9 @@ static ss::logger test_logger{"tx_compaction_tests"};
 using cluster::random_tx_generator;
 
 #define STM_BOOTSTRAP()                                                        \
-    storage::ntp_config::default_overrides o;                                  \
+    storage::ntp_config::default_overrides o{                                  \
+      .retention_time = tristate<std::chrono::milliseconds>(10s),              \
+      .segment_ms = tristate<std::chrono::milliseconds>(1s)};                  \
     o.cleanup_policy_bitflags = model::cleanup_policy_bitflags::compaction;    \
     start_raft(o);                                                             \
     ss::sharded<cluster::tx_gateway_frontend> tx_gateway_frontend;             \
@@ -54,6 +56,7 @@ FIXTURE_TEST(test_tx_compaction_combinations, mux_state_machine_fixture) {
     // batches and tx control batches removed.
     // Each workload execution can fully be backtracked from the test log
     // (in case of failures) and re-executed manually.
+    config::shard_local_cfg().log_segment_ms_min.set_value(1ms);
     for (auto num_tx : {10, 20, 30}) {
         for (auto num_rolls : {0, 1, 2, 3, 5}) {
             for (auto type :
