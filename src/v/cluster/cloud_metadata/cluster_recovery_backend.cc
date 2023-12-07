b9dbae05021db66f527239648b79f071e50f6a86
Andrew Wong <awong@redpanda.com>
2023-12-06 18:57:40 -0800

    cloud_metadata: be more strict about recovery stage order
    We could previously see recovery stages go "backwards" in a benign way,
    if we previously had replicated the stage change, changed leadership,
    and then reconciled the controller snapshot only to discover we need to
    reapply the cluster config again (we don't diff the config against the
    current config, we just apply it whole).
    
    This updates the reconciliation code to consider the current stage when
    determining what steps need to be performed, allowing us to more easily
    reason about the contents in the controller log.
    
    A couple of test cases are updated to perform such validation.

--- src/v/cluster/cloud_metadata/cluster_recovery_backend.cc
+++ src/v/cluster/cloud_metadata/cluster_recovery_backend.cc
@@ -138,10 +138,10 @@ cluster_recovery_backend::apply_controller_actions_in_term(
   ss::abort_source& term_as,
   model::term_id term,
   cloud_metadata::controller_snapshot_reconciler::controller_actions actions) {
-    if (actions.empty()) {
-        co_return cluster::errc::success;
-    }
     for (const auto next_stage : actions.stages) {
+        if (!co_await sync_in_term(term_as, term)) {
+            co_return cluster::errc::not_leader_controller;
+        }
         auto errc = co_await do_action(term_as, next_stage, actions);
         if (errc != cluster::errc::success) {
             co_return co_await _recovery_manager.replicate_update(
@@ -496,6 +496,9 @@ ss::future<> cluster_recovery_backend::recover_until_term_change() {
               "Cluster has reached {}/{} nodes, proceeding...",
               _members_table.node_count(),
               nodes.size());
+        }
+        if (!co_await sync_in_term(term_as, synced_term)) {
+            co_return;
         }
 
         // We may need to restore state from the controller snapshot.
--- src/v/cluster/cluster_recovery_reconciler.cc
+++ src/v/cluster/cluster_recovery_reconciler.cc
@@ -55,14 +55,18 @@ controller_snapshot_reconciler::controller_actions
 controller_snapshot_reconciler::get_actions(
   cluster::controller_snapshot& snap) const {
     // If recovery isn't in progress, return empty.
-    if (!may_require_controller_recovery(_recovery_table.current_status())) {
+    auto cur_stage = _recovery_table.current_status().value_or(
+      recovery_stage::initialized);
+    if (!may_require_controller_recovery(cur_stage)) {
         return {};
     }
 
     controller_actions actions;
     const auto& snap_license = snap.features.snap.license;
     auto existing_license = _feature_table.get_license();
-    if (!existing_license && snap_license.has_value()) {
+    if (
+      cur_stage < recovery_stage::recovered_license && !existing_license
+      && snap_license.has_value()) {
         // If there is already a license, it's presumably more up-to-date than
         // whatever is in a snapshot. Otherwise, try using the license.
         actions.license = snap.features.snap.license.value();
@@ -70,72 +74,84 @@ controller_snapshot_reconciler::get_actions(
     }
 
     const auto& snap_config = snap.config.values;
-    for (const auto& [snap_k, snap_v] : snap_config) {
-        if (properties_ignore_list.contains(snap_k)) {
-            continue;
-        }
-        if (!config::shard_local_cfg().contains(snap_k)) {
-            continue;
-        }
-        actions.config.upsert.emplace_back(cluster_property_kv{snap_k, snap_v});
-    }
-    if (!actions.config.upsert.empty()) {
-        actions.stages.emplace_back(recovery_stage::recovered_cluster_config);
-    }
-
-    const auto& snap_user_creds = snap.security.user_credentials;
-    for (const auto& user : snap_user_creds) {
-        if (!_creds.contains(user.username)) {
-            actions.users.emplace_back(user.username, user.credential);
-        }
-    }
-    if (!actions.users.empty()) {
-        actions.stages.emplace_back(recovery_stage::recovered_users);
-    }
-
-    const auto& snap_acls = snap.security.acls;
-    for (const auto& binding : snap_acls) {
-        // TODO: filter those that exist. For now, just pass in everything since
-        // this is idempotent.
-        actions.acls.emplace_back(binding);
-    }
-    if (!actions.acls.empty()) {
-        actions.stages.emplace_back(recovery_stage::recovered_acls);
-    }
-
-    const auto& snap_tables = snap.topics.topics;
-    for (const auto& [tp_ns, meta] : snap_tables) {
-        const auto& tp_config = meta.metadata.configuration;
-        auto& si_props = tp_config.properties.shadow_indexing;
-        if (_topic_table.contains(tp_ns)) {
-            continue;
-        }
-        if (
-          si_props.has_value()
-          && model::is_archival_enabled(si_props.value())) {
-            // We expect to create the topic with tiered storage data.
-            auto new_config = tp_config;
-            if (!new_config.properties.remote_topic_properties.has_value()) {
-                auto& remote_props
-                  = new_config.properties.remote_topic_properties.emplace();
-                remote_props.remote_revision = model::initial_revision_id{
-                  meta.metadata.revision};
-                remote_props.remote_partition_count = tp_config.partition_count;
+    if (cur_stage < recovery_stage::recovered_cluster_config) {
+        for (const auto& [snap_k, snap_v] : snap_config) {
+            if (properties_ignore_list.contains(snap_k)) {
+                continue;
             }
-            new_config.properties.recovery = true;
-            actions.remote_topics.emplace_back(std::move(new_config));
-            continue;
-        };
-        // Either this is a read replica or no metadata is expected to exist in
-        // tiered storage. Just create the topic.
-        actions.local_topics.emplace_back(tp_config);
+            if (!config::shard_local_cfg().contains(snap_k)) {
+                continue;
+            }
+            actions.config.upsert.emplace_back(
+              cluster_property_kv{snap_k, snap_v});
+        }
+        if (!actions.config.upsert.empty()) {
+            actions.stages.emplace_back(
+              recovery_stage::recovered_cluster_config);
+        }
     }
-    if (!actions.remote_topics.empty()) {
-        actions.stages.emplace_back(
-          recovery_stage::recovered_remote_topic_data);
+
+    if (cur_stage < recovery_stage::recovered_users) {
+        const auto& snap_user_creds = snap.security.user_credentials;
+        for (const auto& user : snap_user_creds) {
+            if (!_creds.contains(user.username)) {
+                actions.users.emplace_back(user.username, user.credential);
+            }
+        }
+        if (!actions.users.empty()) {
+            actions.stages.emplace_back(recovery_stage::recovered_users);
+        }
     }
-    if (!actions.local_topics.empty()) {
-        actions.stages.emplace_back(recovery_stage::recovered_topic_data);
+
+    if (cur_stage < recovery_stage::recovered_acls) {
+        const auto& snap_acls = snap.security.acls;
+        for (const auto& binding : snap_acls) {
+            // TODO: filter those that exist. For now, just pass in everything
+            // since this is idempotent.
+            actions.acls.emplace_back(binding);
+        }
+        if (!actions.acls.empty()) {
+            actions.stages.emplace_back(recovery_stage::recovered_acls);
+        }
+    }
+
+    if (cur_stage < recovery_stage::recovered_topic_data) {
+        const auto& snap_tables = snap.topics.topics;
+        for (const auto& [tp_ns, meta] : snap_tables) {
+            const auto& tp_config = meta.metadata.configuration;
+            auto& si_props = tp_config.properties.shadow_indexing;
+            if (_topic_table.contains(tp_ns)) {
+                continue;
+            }
+            if (
+              si_props.has_value()
+              && model::is_archival_enabled(si_props.value())) {
+                // We expect to create the topic with tiered storage data.
+                auto new_config = tp_config;
+                if (!new_config.properties.remote_topic_properties
+                       .has_value()) {
+                    auto& remote_props
+                      = new_config.properties.remote_topic_properties.emplace();
+                    remote_props.remote_revision = model::initial_revision_id{
+                      meta.metadata.revision};
+                    remote_props.remote_partition_count
+                      = tp_config.partition_count;
+                }
+                new_config.properties.recovery = true;
+                actions.remote_topics.emplace_back(std::move(new_config));
+                continue;
+            };
+            // Either this is a read replica or no metadata is expected to exist
+            // in tiered storage. Just create the topic.
+            actions.local_topics.emplace_back(tp_config);
+        }
+        if (!actions.remote_topics.empty()) {
+            actions.stages.emplace_back(
+              recovery_stage::recovered_remote_topic_data);
+        }
+        if (!actions.local_topics.empty()) {
+            actions.stages.emplace_back(recovery_stage::recovered_topic_data);
+        }
     }
 
     // Always include this final stage, indicating the end of the recovering of
