4dc2cc8bb164b40274ca313521be492b68e4633a
Andrew Wong <awong@redpanda.com>
2023-12-06 18:24:44 -0800

    tests: utility to read recovery_stages from controller log
    This will be useful to validate the state changes in the log.

--- src/v/cluster/cloud_metadata/tests/cluster_metadata_utils.h
+++ src/v/cluster/cloud_metadata/tests/cluster_metadata_utils.h
@@ -8,10 +8,14 @@
  * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
  */
 
+#include "cluster/commands.h"
+#include "cluster/partition.h"
 #include "cluster/tests/topic_properties_generator.h"
 #include "cluster/types.h"
 #include "model/fundamental.h"
+#include "raft/consensus.h"
 #include "security/acl.h"
+#include "storage/types.h"
 
 // Test utilities to facilitate creating metadata.
 
@@ -62,6 +66,42 @@ inline topic_properties non_remote_topic_properties() {
     props.read_replica = false;
     props.cleanup_policy_bitflags = model::cleanup_policy_bitflags::deletion;
     return props;
+}
+
+// Reads the given partition, returning the sequence of recovery stages
+// recorded in its log.
+inline ss::future<std::vector<cluster::recovery_stage>>
+read_recovery_stages(cluster::partition& controller_prt) {
+    storage::log_reader_config reader_config(
+      model::offset(0),
+      controller_prt.raft()->committed_offset(),
+      0,
+      std::numeric_limits<size_t>::max(),
+      ss::default_priority_class(),
+      model::record_batch_type::cluster_recovery_cmd,
+      std::nullopt,
+      std::nullopt,
+      std::nullopt);
+    auto reader = controller_prt.make_reader(reader_config).get();
+    auto batches = co_await model::consume_reader_to_memory(
+      std::move(reader), model::timeout_clock::time_point::max());
+    std::vector<cluster::recovery_stage> stages;
+    for (auto& b : batches) {
+        auto cmd = co_await cluster::deserialize(
+          std::move(b),
+          cluster::make_commands_list<
+            cluster::cluster_recovery_update_cmd,
+            cluster::cluster_recovery_init_cmd>());
+        ss::visit(
+          cmd,
+          [&](const cluster::cluster_recovery_update_cmd& cmd) {
+              stages.emplace_back(cmd.value.stage);
+          },
+          [&](const cluster::cluster_recovery_init_cmd&) {
+              stages.emplace_back(cluster::recovery_stage::initialized);
+          });
+    }
+    co_return stages;
 }
 
 } // namespace cluster::cloud_metadata
