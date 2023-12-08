b08b4083ce78752a290891ed17d1717f14f4a027
Rogger Vasquez <rvasque3@gmail.com>
2023-12-08 14:11:45 -0800

    rpk: add long help text to 'rpk redpanda mode'

--- src/go/rpk/pkg/cli/redpanda/mode.go
+++ src/go/rpk/pkg/cli/redpanda/mode.go
@@ -26,7 +26,7 @@ func NewModeCommand(fs afero.Fs, p *config.Params) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "mode [MODE]",
 		Short: "Enable a default configuration mode",
-		Long:  "",
+		Long:  modeHelpText,
 		Args:  cobra.ExactArgs(1),
 		ValidArgsFunction: func(_ *cobra.Command, _ []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 			// We complete "dev" and "prod", but if the user is typing
@@ -63,3 +63,27 @@ func executeMode(fs afero.Fs, p *config.Params, mode string) error {
 	}
 	return cfg.SetMode(fs, mode)
 }
+
+const modeHelpText = `Enable a default configuration mode
+
+This command allows you to set one of the following modes: Development,
+Production, or Recovery.
+
+PRODUCTION
+
+  - Enforces optimal runtime checks.
+  - Enables all rpk tuners, for you to run 'rpk redpanda tune'.
+
+DEVELOPMENT
+
+  - Disables optimal checks and bypasses fsync, which results in unrealistically
+    fast clusters and may result in data loss.
+  - Disables all rpk tuners. The tuners are intended to run only for production 
+    mode.
+
+RECOVERY
+
+Sets the broker configuration property 'redpanda.recovery_mode_enabled=true' in 
+your redpanda.yaml. This provides a stable environment for troubleshooting and 
+restoring a failed cluster. Restart required.
+`
