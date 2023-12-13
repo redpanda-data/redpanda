6f25f76c3fa1ae3f3d03d67a4f80ba2a8d0be5ae
Ben Pope <ben@redpanda.com>
2023-12-13 14:56:10 +0000

    kafka/client: Allow configuration of consumer min.bytes
    * Add a configuration parameter and wire it up.
    * The default is 1
    * This changes client::fetch_partition from min of 0 to 1
    * switch max_bytes to a bounded_property with min 0
    
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/kafka/client/configuration.h
+++ src/v/kafka/client/configuration.h
@@ -10,6 +10,7 @@
  */
 
 #pragma once
+#include "config/bounded_property.h"
 #include "config/config_store.h"
 #include "config/tls_config.h"
 
@@ -37,7 +38,8 @@ struct configuration final : public config::config_store {
     config::property<std::chrono::milliseconds> produce_shutdown_delay;
     config::property<int16_t> produce_ack_level;
     config::property<std::chrono::milliseconds> consumer_request_timeout;
-    config::property<int32_t> consumer_request_max_bytes;
+    config::bounded_property<int32_t> consumer_request_min_bytes;
+    config::bounded_property<int32_t> consumer_request_max_bytes;
     config::property<std::chrono::milliseconds> consumer_session_timeout;
     config::property<std::chrono::milliseconds> consumer_rebalance_timeout;
     config::property<std::chrono::milliseconds> consumer_heartbeat_interval;
