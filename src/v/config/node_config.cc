b025e3478b05c96c827e58d2b28a62edbbd5923a
Andrea Barbadoro <andij.cr@gmail.com>
2023-12-15 17:59:40 +0100

    config: use property_names_and_aliases()
    this allows to include prop aliases when validating external input

--- src/v/config/node_config.cc
+++ src/v/config/node_config.cc
@@ -245,9 +245,10 @@ node_config::error_map_t node_config::load(const YAML::Node& root_node) {
         throw std::invalid_argument("'redpanda' root is required");
     }
 
-    const auto& ignore = shard_local_cfg().property_names();
+    auto ignore = shard_local_cfg().property_names_and_aliases();
 
-    auto errors = config_store::read_yaml(root_node["redpanda"], ignore);
+    auto errors = config_store::read_yaml(
+      root_node["redpanda"], std::move(ignore));
     validate_multi_node_property_config(errors);
     return errors;
 }
