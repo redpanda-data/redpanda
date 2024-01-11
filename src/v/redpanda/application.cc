56cb178a2f530d5a79791d123a5befe6629bb366
Michal Maslanka <michal@redpanda.com>
2024-01-11 13:17:33 +0100

    c/partition_manager: introduced partition shutdown watchdog timer
    Introduced a watchdog that is tracking partition shutdown state. The
    watchdog is intended to provide a mechanism that will ease debugging of
    partition shutdown issues. The watchdog tracks state of partitions that
    were requested to be stopped or removed. When there was no state update
    for the time longer than the configurable threshold (by default 30
    seconds) the watchdog will emit error log entry which will inform user
    about the problem.
    
    Signed-off-by: Michal Maslanka <michal@redpanda.com>
    (cherry picked from commit 3adfdfc8fd5ac1dd52a09f6e4617e0208dd46a07)

--- src/v/redpanda/application.cc
+++ src/v/redpanda/application.cc
@@ -1443,7 +1443,11 @@ void application::wire_up_redpanda_services(
       std::ref(feature_table),
       std::ref(tm_stm_cache_manager),
       std::ref(_archival_upload_housekeeping),
-      std::ref(producer_manager))
+      std::ref(producer_manager),
+      ss::sharded_parameter([] {
+          return config::shard_local_cfg()
+            .partition_manager_shutdown_watchdog_timeout.bind();
+      }))
       .get();
     vlog(_log.info, "Partition manager started");
     construct_service(
