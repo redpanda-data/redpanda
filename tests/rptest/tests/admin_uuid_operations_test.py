706fed5ecd39ab5eb72b3f308bbc1f3503e1ee0a
Michal Maslanka <michal@redpanda.com>
2024-01-24 11:42:24 +0100

    tests: added admin_api node uuid override test
    Signed-off-by: Michal Maslanka <michal@redpanda.com>
    (cherry picked from commit e3220d710018abd47836f33bd53a1a8a0561dd29)

--- tests/rptest/tests/admin_uuid_operations_test.py
+++ tests/rptest/tests/admin_uuid_operations_test.py
@@ -18,10 +18,17 @@ from rptest.services.admin import Admin
 from rptest.services.cluster import cluster
 from ducktape.cluster.cluster import ClusterNode
 
+from rptest.util import wait_until_result
+
 
 class AdminUUIDOperationsTest(RedpandaTest):
     def __init__(self, ctx):
         super().__init__(test_context=ctx, num_brokers=3)
+
+    def setUp(self):
+        self.redpanda.start(auto_assign_node_id=True,
+                            omit_seeds_on_idx_one=False)
+        self._create_initial_topics()
 
     @cluster(num_nodes=3)
     def test_getting_node_id_to_uuid_map(self):
@@ -37,3 +44,56 @@ class AdminUUIDOperationsTest(RedpandaTest):
         brokers = admin.get_brokers()
         for b in brokers:
             assert b['node_id'] in all_ids
+
+    @cluster(num_nodes=3)
+    def test_overriding_node_id(self):
+        admin = Admin(self.redpanda)
+        to_stop = self.redpanda.nodes[0]
+        initial_to_stop_id = self.redpanda.node_id(to_stop)
+        # Stop node and clear its data directory
+        self.redpanda.stop_node(to_stop)
+        self.redpanda.clean_node(to_stop,
+                                 preserve_current_install=True,
+                                 preserve_logs=False)
+
+        self.redpanda.start_node(to_stop,
+                                 auto_assign_node_id=True,
+                                 omit_seeds_on_idx_one=False)
+
+        def _uuids_updated():
+            uuids = admin.get_broker_uuids()
+            if len(uuids) != 4:
+                return False, None
+
+            return True, uuids
+
+        # wait for the node to join with new ID
+        uuids = wait_until_result(
+            _uuids_updated,
+            timeout_sec=30,
+            err_msg="Node was unable to join the cluster")
+
+        uuids = admin.get_broker_uuids()
+        old_uuid = None
+
+        for n in uuids:
+            id = n['node_id']
+            if id == initial_to_stop_id:
+                old_uuid = n['uuid']
+
+        # get current node id and UUID
+        current = admin.get_broker_uuid(to_stop)
+
+        admin.override_node_id(to_stop,
+                               current_uuid=current['node_uuid'],
+                               new_node_id=initial_to_stop_id,
+                               new_node_uuid=old_uuid)
+
+        self.redpanda.restart_nodes(to_stop,
+                                    auto_assign_node_id=True,
+                                    omit_seeds_on_idx_one=False)
+
+        after_restart = admin.get_broker_uuid(to_stop)
+
+        assert after_restart['node_id'] == initial_to_stop_id
+        assert after_restart['node_uuid'] == old_uuid
