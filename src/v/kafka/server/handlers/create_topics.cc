dcd1261632e6da61522fbd62220d4f72eafe03bc
Oren Leiman <oren.leiman@redpanda.com>
2024-02-01 13:48:32 -0800

    kafka/create_topics: validate_only improvements
    - If the topic already exists, use that return code (36)
    - If the topic does not exist, return the partition count and replication
      factor from the request.
    - Responses now match Apache Kafka
    
    (cherry picked from commit b48d3ac5244b2060f92be4a1ad9dc5e3944f5ef2)

--- src/v/kafka/server/handlers/create_topics.cc
+++ src/v/kafka/server/handlers/create_topics.cc
@@ -221,6 +221,13 @@ ss::future<response_ptr> create_topics_handler::handle(
           std::back_inserter(response.data.topics),
           [&ctx](const creatable_topic& t) {
               auto result = generate_successfull_result(t);
+              if (ctx.metadata_cache().contains(model::topic_namespace_view{
+                    model::kafka_namespace, t.name})) {
+                  result.error_code = error_code::topic_already_exists;
+                  return result;
+              }
+              result.num_partitions = t.num_partitions;
+              result.replication_factor = t.replication_factor;
               if (ctx.header().version >= api_version(5)) {
                   auto default_properties
                     = ctx.metadata_cache().get_default_properties();
