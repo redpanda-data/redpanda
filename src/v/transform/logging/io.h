d59c9283593eb11f95667a16a1f5df27ccb97e3a
Oren Leiman <oren.leiman@redpanda.com>
2024-02-04 22:51:46 -0800

    xform/logging/io: Add client::initialize and update log_manager
    To create the transform logs topic on demand. Also adds code in log_manager.cc
    to perform this operation lazily at flush time.
    
    Additionally, update logging::client to return transform::logging::errc
    (or a result<> thereof). This becomes increasingly useful for error
    reporting as we start hooking things up to the rest of the cluster.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/transform/logging/io.h
+++ src/v/transform/logging/io.h
@@ -9,9 +9,11 @@
  * by the Apache License, Version 2.0
  */
 
+#include "base/outcome.h"
 #include "cluster/errc.h"
 #include "model/record.h"
 #include "ssx/semaphore.h"
+#include "transform/logging/errc.h"
 #include "transform/logging/event.h"
 
 #pragma once
@@ -53,16 +55,17 @@ public:
     client& operator=(client&&) = delete;
     virtual ~client() = default;
 
+    virtual ss::future<errc> initialize() = 0;
+
     /*
      * Collect io::json_batches into model::record_batch(es) and publish
      *
      * Implementations should aim to batch outgoing records maximally and
      * respect cluster-wide batch size limits.
      */
-    virtual ss::future<> write(model::partition_id, io::json_batches) = 0;
+    virtual ss::future<errc> write(model::partition_id, io::json_batches) = 0;
 
-    virtual model::partition_id
-    compute_output_partition(model::transform_name_view name)
-      = 0;
+    virtual result<model::partition_id, errc>
+    compute_output_partition(model::transform_name_view name) = 0;
 };
 } // namespace transform::logging
