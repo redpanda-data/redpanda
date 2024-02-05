38f99f9c9dbdda98041f1f81dbb95d2e1e3ebead
Oren Leiman <oren.leiman@redpanda.com>
2024-02-04 23:18:28 -0800

    xform/api: Wire up transform::logging::manager
    Also Add sharded<metadata_cache> field to transform::service
    in service of transform::logging::rpc_client.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/redpanda/application.cc
+++ src/v/redpanda/application.cc
@@ -1260,6 +1260,7 @@ void application::wire_up_runtime_services(
           &controller->get_topics_state(),
           &partition_manager,
           &_transform_rpc_client,
+          &metadata_cache,
           sched_groups.transforms_sg())
           .get();
     }
