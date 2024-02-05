dd91e9b3dbd8abdc7d5f3c8aa3992e74ddbb0609
Oren Leiman <oren.leiman@redpanda.com>
2024-02-04 23:32:25 -0800

    dt/utils: generate_string_with_control_characters to utf8 module
    Previously a static method on a SCRAM test.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>
    (cherry picked from commit ad2c4e6b2dc94e2763e6018da2388a95d46c62bc)

--- tests/rptest/utils/utf8.py
+++ tests/rptest/utils/utf8.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 # List of every control character except the null character
 CONTROL_CHARS = ''.join((chr(x) for x in range(1, 32))) + '\x7f'
 # List of control character numerical value
@@ -10,3 +13,12 @@ CONTROL_CHARS_MAP = dict(
         [(lambda c: b'\xe2\x90\xa1'.decode('utf-8')
           if c == 0x7f else bytes([0xe2, 0x90, 0x80 + c]).decode('utf-8'))(c)
          for c in CONTROL_CHARS_VALS]))
+
+
+def generate_string_with_control_character(length: int) -> str:
+    rv = ''.join(random.choices(string.ascii_letters + CONTROL_CHARS,
+                                k=length))
+    while not any(char in rv for char in CONTROL_CHARS):
+        rv = ''.join(
+            random.choices(string.ascii_letters + CONTROL_CHARS, k=length))
+    return rv
