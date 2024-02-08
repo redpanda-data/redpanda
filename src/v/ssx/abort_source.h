53f0cc690db6990716c535c738fefd070c36b7ba
Brandon Allard <brandon@redpanda.com>
2024-02-07 19:09:46 -0500

    ssx: implement `ssx::sharded_abort_source` using `std::shared_ptr`
    The previous implementation of `ssx::sharded_abort_source` had two
    potential race conditions;
    
    - If it is stopped then `.local()` is no longer safe to call since
      `.stop()` will delete all local objects. However, other shards do not
    have a way of knowing if the abort_source was stopped, hence no way of
    knowing if `.local()` is safe to call or not. The new implementation
    avoids this issue by using a shared_ptr to delete the local objects
    rather than `.stop()`.
    
    - If the parent abort_source aborts then the subscription will make
      cross shard calls to every shard-local abort_source. However, on the
    original shard the `ssx::sharded_abort_source` may be stopped and the
    shard-local abort_source's deleted before the cross shard calls finish.
    Resulting in a use-after-free violation. The new implementation fixes
    this in the same way as it did the last issue.

--- src/v/ssx/abort_source.h
+++ src/v/ssx/abort_source.h
@@ -12,6 +12,7 @@
 #pragma once
 
 #include "base/oncore.h"
+#include "ssx/future-util.h"
 
 #include <seastar/core/abort_source.hh>
 #include <seastar/core/sharded.hh>
@@ -20,61 +21,99 @@
 
 namespace ssx {
 
+/**
+ * A sharded version of `ss::abort_source` that allows any shard to request an
+ * abort on all other shards.
+ *
+ * Sharing one instance of this class across threads raises difficult lifetime
+ * and thread safety issues, so the intended usage pattern for this class is to
+ * create it on one shard, then make a copy of this original object on every
+ * shard where it will be used. All internal state is wrapped in a shared
+ * pointer so these copies reference the same underlying state.
+ */
 class sharded_abort_source {
 public:
-    ss::future<> start(ss::abort_source& parent) {
-        return _as.start().then([this, &parent]() {
-            auto dex = parent.get_default_exception();
-            auto sub = parent.subscribe(
-              [this, dex](
-                std::optional<std::exception_ptr> const& ex) mutable noexcept {
-                  dex = ex.value_or(dex);
-                  return _as.invoke_on_all(
-                    [dex](auto& as) { return as.request_abort_ex(dex); });
-              });
-            if (sub) {
-                _sub.emplace(std::move(*sub));
-            }
-        });
+    explicit sharded_abort_source(ss::abort_source& parent)
+      : _internal(std::make_shared<sharded_abort_source_internal>(
+        ss::this_shard_id(),
+        std::vector<ss::abort_source>(ss::smp::count),
+        std::nullopt)) {
+        auto dex = parent.get_default_exception();
+        auto sub = parent.subscribe(
+          [as = *this,
+           dex](std::optional<std::exception_ptr> const& ex) mutable noexcept {
+              dex = ex.value_or(dex);
+              ssx::background = as.request_abort_ex(dex);
+          });
+        if (sub) {
+            _internal->sub.emplace(std::move(*sub));
+        }
     }
 
-    ss::future<> stop() noexcept {
-        _sub.reset();
-        return request_abort().then([this]() { return _as.stop(); });
+    // Returns a reference to an abort_source local to the calling shard
+    //
+    // Note: the lifetime of `sharded_abort_source` must exceed the lifetime of
+    // this reference.
+    auto& local() noexcept { return _internal->as[ss::this_shard_id()]; }
+
+    // Returns a const reference to an abort_source local to the calling shard
+    //
+    // Note: the lifetime of `sharded_abort_source` must exceed the lifetime of
+    // this reference.
+    auto const& local() const noexcept {
+        return _internal->as[ss::this_shard_id()];
     }
 
-    auto& local() noexcept { return _as.local(); }
-    auto const& local() const noexcept { return _as.local(); }
-
-    template<typename Func>
-    [[nodiscard]] auto subscribe(Func&& func) {
-        return local().subscribe(std::forward<Func>(func));
-    }
-
+    // Requests an abort with exception_ptr `ex` on every shard's abort_source.
     ss::future<> request_abort_ex(std::exception_ptr ex) noexcept {
-        return _as.invoke_on_all(
-          [ex](auto& s) { return s.request_abort_ex(ex); });
+        return ss::smp::invoke_on_all(
+          [ex, as = *this]() mutable { as.local().request_abort_ex(ex); });
     }
 
+    // Requests an abort with exception `e` on every shard's abort_source.
     template<typename Exception>
     ss::future<> request_abort_ex(Exception&& e) noexcept {
         return request_abort_ex(std::make_exception_ptr(e));
     }
 
+    // Requests an abort with the default_exception type on every shard's
+    // abort_source.
     ss::future<> request_abort() noexcept {
         return request_abort_ex(local().get_default_exception());
     }
 
+    // Subscribes to the calling shard's local abort_source.
+    template<typename Func>
+    [[nodiscard]] auto subscribe(Func&& func) {
+        return local().subscribe(std::forward<Func>(func));
+    }
+
+    // Checks the calling shard's abort_source to see if an abort was requested.
     auto abort_requested() const noexcept { return local().abort_requested(); }
+
+    // Throws the abort exception of the calling shard's abort_source if its
+    // been aborted.
     auto check() const { return local().check(); }
 
-    bool local_is_initialized() const noexcept {
-        return _as.local_is_initialized();
+    // Removes subscription to parent abort source and requests an abort on
+    // every shard's abort_source.
+    ss::future<> stop() noexcept {
+        vassert(
+          ss::this_shard_id() == _internal->orig_shard,
+          "sharded_abort_source must be stopped on its original shard");
+
+        _internal->sub.reset();
+        return request_abort();
     }
 
 private:
-    ss::sharded<ss::abort_source> _as;
-    std::optional<ss::abort_source::subscription> _sub;
+    struct sharded_abort_source_internal {
+        ss::shard_id orig_shard;
+        std::vector<ss::abort_source> as;
+        std::optional<ss::abort_source::subscription> sub;
+    };
+
+    std::shared_ptr<sharded_abort_source_internal> _internal;
 };
 
 struct shutdown_requested_exception : ss::abort_requested_exception {};
