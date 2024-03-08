1d2e410054657bd14157904edb30d94f43299237
Michal Maslanka <michal@redpanda.com>
2024-03-08 12:50:52 +0100

    c/tests: added contiguous range benchmark
    Signed-off-by: Michal Maslanka <michal@redpanda.com>

--- src/v/container/tests/vector_bench.cc
+++ src/v/container/tests/vector_bench.cc
@@ -10,6 +10,7 @@
  */
 
 #include "container/fragmented_vector.h"
+#include "container/tests/bench_utils.h"
 #include "random/generators.h"
 #include "utils/functional.h"
 #include "utils/type_traits.h"
@@ -20,54 +21,10 @@
 #include <iterator>
 #include <vector>
 
-struct large_struct {
-    size_t foo;
-    size_t bar;
-    std::string qux;
-    std::string baz;
-    std::string more;
-    std::string data;
-    size_t okdone;
-
-    bool operator==(const large_struct&) const = default;
-    auto operator<=>(const large_struct&) const = default;
-};
-
 template<typename Vector, size_t Size>
 class VectorBenchTest {
     static auto make_value() {
-        if constexpr (std::is_same_v<typename Vector::value_type, int64_t>) {
-            return static_cast<int64_t>(random_generators::get_int<int64_t>());
-
-        } else if constexpr (std::is_same_v<
-                               typename Vector::value_type,
-                               ss::sstring>) {
-            constexpr size_t max_str_len = 64;
-            return random_generators::gen_alphanum_string(
-              random_generators::get_int<size_t>(0, max_str_len));
-        } else if constexpr (std::is_same_v<
-                               typename Vector::value_type,
-                               large_struct>) {
-            constexpr size_t max_str_len = 64;
-            constexpr size_t max_num_cardinality = 16;
-            return large_struct{
-              .foo = random_generators::get_int<size_t>(0, max_num_cardinality),
-              .bar = random_generators::get_int<size_t>(0, max_num_cardinality),
-              .qux = random_generators::gen_alphanum_string(
-                random_generators::get_int<size_t>(0, max_str_len)),
-              .baz = random_generators::gen_alphanum_string(
-                random_generators::get_int<size_t>(0, max_str_len)),
-              .more = random_generators::gen_alphanum_string(
-                random_generators::get_int<size_t>(0, max_str_len)),
-              .data = random_generators::gen_alphanum_string(
-                random_generators::get_int<size_t>(0, max_str_len)),
-              .okdone = random_generators::get_int<size_t>(),
-            };
-        } else {
-            static_assert(
-              utils::unsupported_type<typename Vector::value_type>::value,
-              "unsupported");
-        }
+        return ::make_value<typename Vector::value_type>();
     }
     static auto make_filled() {
         Vector v;
