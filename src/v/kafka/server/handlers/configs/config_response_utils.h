7354df72147a9bd66e38728b5e10eb68cc069a91
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2024-03-12 11:08:59 +0000

    kafka: extract topic configs code
    This extracts the topic config specific code in describe_configs and
    moves it into a separate file so that it can later be reused in the
    CreateTopic handler.
    
    (cherry picked from commit 47cf65f252cdc42f8c66fad732c8f55f10785375)

---
+++ src/v/kafka/server/handlers/configs/config_response_utils.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include "cluster/types.h"
+#include "kafka/protocol/describe_configs.h"
+
+namespace kafka {
+
+void report_topic_config(
+  const describe_configs_resource& resource,
+  describe_configs_result& result,
+  const cluster::metadata_cache& metadata_cache,
+  const cluster::topic_properties& topic_properties,
+  bool include_synonyms,
+  bool include_documentation);
+
+void report_broker_config(
+  const describe_configs_resource& resource,
+  describe_configs_result& result,
+  bool include_synonyms,
+  bool include_documentation);
+
+} // namespace kafka
