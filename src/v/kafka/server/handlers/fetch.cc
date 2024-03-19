d80f7704ef25da3e8f06e0096fd35550ec4c004d
Brandon Allard <brandon@redpanda.com>
2024-03-19 15:27:03 -0400

    kafka: avoid disconnecting on storage errors in fetch handler
    The storage layer can thow a few uncaught exceptions that are
    caught in the fetch handler. I.e,
     - cloud_storage::download_exception
     - seastar::gate_closed_exception
    
    When the fetch handler catches these it will simply close the client
    connection and log the exception it caught. This isn't the most optimal
    behavior as the errors caught from the storage layer are retriable. And
    any data read from other partitions in the fetch request is lost when
    the connection is closed.
    
    This commit changes the behavior to instead return a retriable error for
    the partition the storage exception occurs in.

--- src/v/kafka/server/handlers/fetch.cc
+++ src/v/kafka/server/handlers/fetch.cc
@@ -10,6 +10,7 @@
 #include "kafka/server/handlers/fetch.h"
 
 #include "base/likely.h"
+#include "cloud_storage/download_exception.h"
 #include "cluster/metadata_cache.h"
 #include "cluster/partition_manager.h"
 #include "cluster/shard_table.h"
@@ -39,6 +40,7 @@
 
 #include <seastar/core/do_with.hh>
 #include <seastar/core/future.hh>
+#include <seastar/core/gate.hh>
 #include <seastar/core/scheduling.hh>
 #include <seastar/core/sleep.hh>
 #include <seastar/core/thread.hh>
@@ -106,6 +108,7 @@ static ss::future<read_result> read_from_partition(
 
     reader_config.strict_max_bytes = config.strict_max_bytes;
     auto rdr = co_await part.make_reader(reader_config);
+    bool known_exception = false;
     std::exception_ptr e;
     std::unique_ptr<iobuf> data;
     std::vector<cluster::rm_stm::tx_range> aborted_transactions;
@@ -139,12 +142,27 @@ static ss::future<read_result> read_from_partition(
                   part.high_watermark());
             }
         }
-
+    } catch (const cloud_storage::download_exception&) {
+        e = std::current_exception();
+        known_exception = true;
+    } catch (const ss::gate_closed_exception&) {
+        e = std::current_exception();
+        known_exception = true;
     } catch (...) {
         e = std::current_exception();
+        known_exception = false;
     }
 
     co_await std::move(rdr.reader).release()->finally();
+
+    if (e && known_exception) {
+        vlog(
+          klog.info,
+          "exception while reading topic_partition: {} exception: {}",
+          part.ntp().tp,
+          e);
+        co_return read_result(error_code::kafka_storage_error, start_o, hw);
+    }
 
     if (e) {
         std::rethrow_exception(e);
