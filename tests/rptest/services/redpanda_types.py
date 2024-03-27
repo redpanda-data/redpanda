f7ecc4731f8ab19f5b21583fe68e29ccfbd8b884
Travis Downs <travis.downs@vectorized.io>
2024-03-27 17:27:14 -0300

    wip: sasl creds to dataclass

--- tests/rptest/services/redpanda_types.py
+++ tests/rptest/services/redpanda_types.py
@@ -1,6 +1,23 @@
 import collections
+from dataclasses import astuple, dataclass
+from typing import Iterator
 
 # Basic types shared between various redpanda service and test modes.
 
-SaslCredentials = collections.namedtuple("SaslCredentials",
-                                         ["username", "password", "algorithm"])
+
+@dataclass
+class SaslCredentials:
+    """Credentials and algorithm for SASL authentication."""
+
+    username: str
+    """The username for SASL authentication."""
+
+    password: str
+    """The password for SASL authentication."""
+
+    algorithm: str
+    """The SASL authentication mechanism."""
+
+    # allow credentials to be unpacked for backwards compat
+    def __iter__(self) -> Iterator[str]:
+        return iter(astuple(self))
