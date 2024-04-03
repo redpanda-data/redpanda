74cd8aeccbac9dac9fdf10c3ce016dc856737862
Michael Boquard <michael@redpanda.com>
2024-04-03 12:43:09 -0400

    app: FIPS flag now contolling OpenSSL startup
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/redpanda/application.cc
+++ src/v/redpanda/application.cc
@@ -2131,9 +2131,11 @@ void application::wire_up_and_start_crypto_services() {
     construct_service(
       ossl_context_service,
       std::ref(*thread_worker),
-      ss::sstring{},
-      ss::sstring{},
-      crypto::is_fips_mode::no)
+      ss::sstring{config::node_config().openssl_config_file().value_or("")},
+      ss::sstring{
+        config::node_config().openssl_module_directory().value_or("")},
+      config::node_config().fips_mode() ? crypto::is_fips_mode::yes
+                                        : crypto::is_fips_mode::no)
       .get();
     ossl_context_service.invoke_on_all(&crypto::ossl_context_service::start)
       .get();
