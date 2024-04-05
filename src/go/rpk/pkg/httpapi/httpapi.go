61a8de9fbcce0e0f5d9e13a1c54b1324efa616bb
Rogger Vasquez <rvasque3@gmail.com>
2024-04-05 15:48:58 -0700

    rpk httapi: log request method and response status

--- src/go/rpk/pkg/httpapi/httpapi.go
+++ src/go/rpk/pkg/httpapi/httpapi.go
@@ -310,9 +310,13 @@ func (cl *Client) do(ctx context.Context, method, path string, qps url.Values, b
 		for k, v := range cl.headers {
 			req.Header.Set(k, v)
 		}
-		zap.L().Sugar().Debugf("requesting %s", req.URL)
+		zap.L().Sugar().Debugf("requesting %s %s", req.Method, req.URL)
 		resp, err = cl.httpCl.Do(req)
-		zap.L().Sugar().Debugf("got response for %s", req.URL)
+		if resp != nil {
+			zap.L().Sugar().Debugf("got response for %s %s: %v", req.Method, req.URL, resp.Status)
+		} else {
+			zap.L().Sugar().Debugf("got response for %s", req.URL)
+		}
 
 		if isRetryable, err := backoff.maybeDo(ctx, resp, err); err != nil {
 			return err
