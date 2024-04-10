155799f962616dc568088660cbc1bf13fbe540e2
Noah Watkins <noahwatkins@gmail.com>
2024-04-10 08:00:04 -0700

    test: do not start seastar for --gtest_list_tests
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/test_utils/gtest_main.cc
+++ src/v/test_utils/gtest_main.cc
@@ -13,6 +13,18 @@
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     GTEST_FLAG_SET(death_test_style, "threadsafe");
+
+    /*
+     * if the test is being passed --gtest_list_tests then do not start Seastar.
+     * this is an important optimization for using `gtest_discover_tests`
+     * because every test exectuable is queried in parallel, and the result is
+     * that listing would timeout or seastar would exhaust some kind of resource
+     * like aio-max-nr.
+     */
+    if (GTEST_FLAG_GET(list_tests)) {
+        return RUN_ALL_TESTS();
+    }
+
     seastar::testing::global_test_runner().start(argc, argv);
 
     int ret = 0;
