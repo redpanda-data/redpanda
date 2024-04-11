aa6eebbf81a6e01faae876a5338e91c2568982cf
Michał Maślanka <michal@redpanda.com>
2024-04-11 10:39:55 +0000

    k/replicated_partition: fixed querying end offset of an empty log
    When querying offset translator we are relying on the property of the
    end offset or high watermark being exclusive. Changed the behavior of
    log end offset to increment offset in Raft offset space and then
    translate to achieve correct behavior when log is ended with a batch
    subjected to offset translation.
    
    Fixes: #16612
    
    Signed-off-by: Michał Maślanka <michal@redpanda.com>
    (cherry picked from commit fd04fd72c8f0d37e1e480c6f3a32b6ad53ae270f)

--- src/v/kafka/server/replicated_partition.h
+++ src/v/kafka/server/replicated_partition.h
@@ -108,8 +108,8 @@ public:
         /**
          * By default we return a dirty_offset + 1
          */
-        return model::next_offset(
-          _translator->from_log_offset(_partition->dirty_offset()));
+        return _translator->from_log_offset(
+          model::next_offset(_partition->dirty_offset()));
     }
 
     model::offset leader_high_watermark() const {
