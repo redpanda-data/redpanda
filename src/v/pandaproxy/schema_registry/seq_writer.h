0f93e1e13834593447ed1af366a7733cb2ffa491
Ben Pope <ben@redpanda.com>
2024-04-16 13:27:27 +0100

    schema_registry: Refactor seq_writer
    * Remove unusual "this" parameter
    
    Signed-off-by: Ben Pope <ben@redpanda.com>
    (cherry picked from commit 60ceea9177158ef1e2a145eb70274cb9f961391b)

--- src/v/pandaproxy/schema_registry/seq_writer.h
+++ src/v/pandaproxy/schema_registry/seq_writer.h
@@ -69,27 +69,22 @@ private:
 
     void advance_offset_inner(model::offset offset);
 
-    ss::future<std::optional<schema_id>> do_write_subject_version(
-      subject_schema schema, model::offset write_at, seq_writer& seq);
+    ss::future<std::optional<schema_id>>
+    do_write_subject_version(subject_schema schema, model::offset write_at);
 
     ss::future<std::optional<bool>> do_write_config(
       std::optional<subject> sub,
       compatibility_level compat,
-      model::offset write_at,
-      seq_writer& seq);
+      model::offset write_at);
 
     ss::future<std::optional<bool>>
-    do_delete_config(subject sub, model::offset write_at, seq_writer& seq);
+    do_delete_config(subject sub, model::offset write_at);
 
     ss::future<std::optional<bool>> do_delete_subject_version(
-      subject sub,
-      schema_version version,
-      model::offset write_at,
-      seq_writer& seq);
+      subject sub, schema_version version, model::offset write_at);
 
     ss::future<std::optional<std::vector<schema_version>>>
-    do_delete_subject_impermanent(
-      subject sub, model::offset write_at, seq_writer& seq);
+    do_delete_subject_impermanent(subject sub, model::offset write_at);
 
     ss::future<std::vector<schema_version>> delete_subject_permanent_inner(
       subject sub, std::optional<schema_version> version);
