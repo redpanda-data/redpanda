e0ba45ee2a76571a8ac9fe3e7020c36643b4d223
Ben Pope <ben@redpanda.com>
2024-04-16 22:45:45 +0100

    kafka/server: Disable quota balancer
    v2 was enabled by default in:
    https://github.com/redpanda-data/redpanda/pull/16441
    
    Running the balancer doesn't do anything other
    than waste cpu cycles.
    
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/config/configuration.cc
+++ src/v/config/configuration.cc
@@ -2894,7 +2894,7 @@ configuration::configuration()
       "milliseconds. Value of 0 disables the balancer and makes all the "
       "throughput quotas immutable.",
       {.needs_restart = needs_restart::no, .visibility = visibility::user},
-      750ms,
+      0ms,
       {.min = 0ms})
   , kafka_quota_balancer_min_shard_throughput_ratio(
       *this,
