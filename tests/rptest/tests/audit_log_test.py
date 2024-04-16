2d6059ac3a2d1b1ac1745fb4645cebfb2d34d34e
Rob Blafford <rob@redpanda.com>
2024-04-16 12:02:26 -0400

    tests: Marking test_drain_audit* as ok_to_fail
    - After investigation its been determined that the failure isn't really
    an indication of auditing not working properly.
    
    - The test verifies that on shutdown all stop application_lifecycle messages
    are observed, and during shutdown there is a short grace period in which
    shutdown will be held until these messages can be successfully produced,
    however there is no guarantee that the produce will succeed as shutdown
    is random and it may be the case that nodes that host the audit
    partition to produce to have already been shutdown.
    
    - Therefore the produce fails after the grace period and the message
    fails to make it to the audit log, making the test fail.
    
    - A potential fix is to have the audit log subsystem be partition
    agnostic when producing messages, but this means it must have the
    ability to  reacting to failures in a smarter way then just retrying,
    currently our kafka client does not support this. Although attempts have
    been made to implement this: https://github.com/redpanda-data/redpanda/pull/15202

--- tests/rptest/tests/audit_log_test.py
+++ tests/rptest/tests/audit_log_test.py
@@ -23,7 +23,7 @@ from typing import Any, Optional
 
 from ducktape.cluster.cluster import ClusterNode
 from ducktape.errors import TimeoutError
-from ducktape.mark import matrix, ok_to_fail_fips
+from ducktape.mark import matrix, ok_to_fail, ok_to_fail_fips
 from keycloak import KeycloakOpenID
 from rptest.clients.default import DefaultClient
 from rptest.clients.kcl import KCL
@@ -675,6 +675,7 @@ class AuditLogTestsAppLifecycle(AuditLogTestBase):
                     True), lambda record_count: record_count == 3,
             "Single redpanda start event per node")
 
+    @ok_to_fail  # https://github.com/redpanda-data/redpanda/issues/16198
     @ok_to_fail_fips
     @cluster(num_nodes=5)
     def test_drain_on_audit_disabled(self):
