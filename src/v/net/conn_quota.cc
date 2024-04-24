3376178fd09e03da40e5ad124050f1a20f7ad646
Noah Watkins <noahwatkins@gmail.com>
2024-04-24 08:17:29 -0700

    net: make net a new-style module
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/net/conn_quota.cc
+++ src/v/net/conn_quota.cc
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
-#include "conn_quota.h"
+#include "net/conn_quota.h"
 
 #include "base/vlog.h"
 #include "config/configuration.h"
