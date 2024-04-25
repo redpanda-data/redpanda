b2e31f02ede14c162ba5545295947d19c93571b3
Alex Savatieiev <a.savex@gmail.com>
2024-04-25 18:00:51 -0500

    PESDLC-1113 Fix bash liter errors

--- tests/rptest/remote_scripts/cloud/pod_log_extract.sh
+++ tests/rptest/remote_scripts/cloud/pod_log_extract.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 files=($(find /var/log/pods/ -type f | grep "$1" | grep -v "configurator"))
 
-for item in ${files[*]};
-do
-    echo "# $item"
-    tail --lines=+$(cat $item | grep -n -m 1 "$2" | cut -d":" -f1) $item
-    echo "# End of log"
+for item in ${files[*]}; do
+  echo "# $item"
+  tail --lines=+$(cat $item | grep -n -m 1 "$2" | cut -d":" -f1) $item
+  echo "# End of log"
 done
