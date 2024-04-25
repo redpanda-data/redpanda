bdbde60597af1d108cbec6b86bc98ed87b26dce0
Alex Savatieiev <a.savex@gmail.com>
2024-04-25 17:32:38 -0500

    PESDLC-1113 Add log extraction script
       Implement simple script that extracts specific log lines from rp log
       based on broker name and time.

---
+++ tests/rptest/remote_scripts/cloud/pod_log_extract.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+files=($(find /var/log/pods/ -type f | grep "$1" | grep -v "configurator"))
+
+for item in ${files[*]};
+do
+    echo "# $item"
+    tail --lines=+$(cat $item | grep -n -m 1 "$2" | cut -d":" -f1) $item
+    echo "# End of log"
+done
