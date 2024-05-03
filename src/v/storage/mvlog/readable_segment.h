0a2191be9dc7750b30d4d17268eef714fd0db75f
Andrew Wong <awong@redpanda.com>
2024-05-02 23:45:16 -0700

    mvlog: add gaps to segment reader
    Adds a gap set to the readable segment so that readers can iterate as
    of a specific version.
    
    The full set of gaps in the segment is long-lived, so this is owned by
    the readable segment. Segment readers make of copy of this set (expected
    to be small) when constructed, before iterating.
    
    Longer term, multiple segments may have gaps added to them at a time,
    but this commit focuses only on introducing the per-segment gaps.

--- src/v/storage/mvlog/readable_segment.h
+++ src/v/storage/mvlog/readable_segment.h
@@ -8,7 +8,12 @@
 // by the Apache License, Version 2.0
 #pragma once
 
+#include "storage/mvlog/file_gap.h"
+
 #include <memory>
+
+template<std::integral>
+class interval_set;
 
 namespace storage::experimental::mvlog {
 
@@ -19,16 +24,35 @@ class segment_reader;
 // passing out short-lived readers.
 class readable_segment {
 public:
-    explicit readable_segment(file* f)
-      : file_(f) {}
+    readable_segment(readable_segment&) = delete;
+    readable_segment(readable_segment&&) = delete;
+    readable_segment& operator=(readable_segment&) = delete;
+    readable_segment& operator=(readable_segment&&) = delete;
 
+    explicit readable_segment(file* f);
+    ~readable_segment();
+
+    // Returns a segment reader that reads data as of the time of calling. I.e.
+    // honors only gaps that were added before calling, and only reads up to the
+    // segment's end as of calling.
     std::unique_ptr<segment_reader> make_reader();
+
+    // Adds the given gap, such that subsequent readers of the file skip the
+    // specified range and all those previously added.
+    void add_gap(file_gap gap);
+
+    // Returns the number of readers returned created by this segment that have
+    // yet to be destructed.
     size_t num_readers() const { return num_readers_; }
 
 private:
     friend class segment_reader;
 
     file* file_;
+
+    // Set of gaps that have been added to the file. May be null if no gaps
+    // have been added yet.
+    std::unique_ptr<interval_set<size_t>> gaps_{nullptr};
 
     size_t num_readers_{0};
 };
