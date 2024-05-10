bc9c4f04c07041d303bd5c438d64400778a16c85
alexey bashtanov <alexey.bashtanov@redpanda.com>
2024-05-10 11:23:48 +0100

    tests: create a raft stm fixture
    Create a raft stm fixture, make distributed_kv_stm_tests
    and id_allocator_stm_test use it

--- src/v/cluster/tests/id_allocator_stm_test.cc
+++ src/v/cluster/tests/id_allocator_stm_test.cc
@@ -14,6 +14,7 @@
 #include "model/record.h"
 #include "model/timeout_clock.h"
 #include "raft/fundamental.h"
+#include "raft/tests/raft_fixture.h"
 #include "random/generators.h"
 #include "reflection/adl.h"
 #include "storage/record_batch_builder.h"
@@ -21,11 +22,10 @@
 #include "test_utils/async.h"
 #include "test_utils/fixture.h"
 #include "test_utils/scoped_config.h"
-#include "test_utils/test.h"
-#include "tests/stm_test_fixture.h"
 
 #include <seastar/core/abort_source.hh>
 #include <seastar/core/future.hh>
+#include <seastar/core/shared_ptr.hh>
 #include <seastar/core/sleep.hh>
 #include <seastar/util/defer.hh>
 #include <seastar/util/log.hh>
@@ -33,70 +33,37 @@
 #include <thread>
 
 using namespace std::chrono_literals;
+using namespace raft;
 
 namespace cluster {
 namespace {
 
 ss::logger idstmlog{"idstm-test"};
+using stm_ssshptr_t = const ss::shared_ptr<cluster::id_allocator_stm>&;
 
-struct id_allocator_stm_fixture : state_machine_fixture {
-    ss::future<> initialize_state_machines() {
-        // set configuration parameters
+struct id_allocator_stm_fixture
+  : stm_raft_fixture<id_allocator_stm_fixture, cluster::id_allocator_stm> {
+    //
+    id_allocator_stm_fixture() {
         test_local_cfg.get("id_allocator_batch_size").set_value(int16_t(1));
         test_local_cfg.get("id_allocator_log_capacity").set_value(int16_t(2));
-
-        create_nodes();
-        co_await start_nodes();
     }
 
-    ss::future<> start_node(raft_node_instance& node) {
-        co_await node.initialise(all_vnodes());
-        raft::state_machine_manager_builder builder;
-        auto stm = builder.create_stm<cluster::id_allocator_stm>(
+    stm_shptrs_t create_stms(
+      state_machine_manager_builder& builder, raft_node_instance& node) {
+        return builder.create_stm<cluster::id_allocator_stm>(
           idstmlog, node.raft().get(), config::shard_local_cfg());
-        co_await node.start(std::move(builder));
-        node_stms.emplace(node.get_vnode(), std::move(stm));
-    }
-
-    ss::future<> start_nodes() {
-        co_await parallel_for_each_node(
-          [this](raft_node_instance& node) { return start_node(node); });
-    }
-
-    ss::future<> stop_and_recreate_nodes() {
-        absl::flat_hash_map<model::node_id, ss::sstring> data_directories;
-        for (auto& [id, node] : nodes()) {
-            data_directories[id]
-              = node->raft()->log()->config().base_directory();
-            node_stms.erase(node->get_vnode());
-        }
-
-        co_await ss::parallel_for_each(
-          std::views::keys(data_directories),
-          [this](model::node_id id) { return stop_node(id); });
-
-        for (auto& [id, data_dir] : data_directories) {
-            add_node(id, model::revision_id(0), std::move(data_dir));
-        }
     }
 
     ss::future<> reset(int64_t id) {
-        auto result = co_await retry_with_leader(
-          model::timeout_clock::now() + 30s,
-          [this, id](raft_node_instance& leader_node) {
-              auto stm = node_stms[leader_node.get_vnode()];
-              return stm->reset_next_id(id, 1s);
-          });
+        auto result = co_await stm_retry_with_leader<0>(
+          30s, [id](stm_ssshptr_t stm) { return stm->reset_next_id(id, 1s); });
         ASSERT_RESULT_EQ_CORO(result, id);
     }
 
     ss::future<> allocate1(int64_t& cur_last_id) {
-        auto result = co_await retry_with_leader(
-          model::timeout_clock::now() + 30s,
-          [this](raft_node_instance& leader_node) {
-              auto stm = node_stms[leader_node.get_vnode()];
-              return stm->allocate_id(1s);
-          });
+        auto result = co_await stm_retry_with_leader<0>(
+          30s, [](stm_ssshptr_t stm) { return stm->allocate_id(1s); });
         ASSERT_RESULT_GT_CORO(result, cur_last_id);
         cur_last_id = result.assume_value();
     }
@@ -112,8 +79,6 @@ struct id_allocator_stm_fixture : state_machine_fixture {
         co_return cur_last_id;
     }
 
-    absl::flat_hash_map<raft::vnode, ss::shared_ptr<cluster::id_allocator_stm>>
-      node_stms;
     scoped_config test_local_cfg;
 };
 
@@ -132,8 +97,7 @@ TEST_F_CORO(id_allocator_stm_fixture, stm_restart_test) {
     int64_t last_id = -1;
 
     last_id = co_await allocate_n(last_id, 5);
-    co_await stop_and_recreate_nodes();
-    co_await start_nodes();
+    co_await restart_nodes();
     co_await allocate_n(last_id, 5);
 }
 
@@ -150,8 +114,7 @@ TEST_F_CORO(id_allocator_stm_fixture, stm_reset_id_test) {
     ASSERT_EQ_CORO(last_id, 105);
 
     // Even after restarting, the starting point should be where we left off.
-    co_await stop_and_recreate_nodes();
-    co_await start_nodes();
+    co_await restart_nodes();
 
     last_id = co_await allocate_n(last_id, 5);
     ASSERT_EQ_CORO(last_id, 110);
