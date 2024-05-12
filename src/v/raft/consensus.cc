27d9ae37b50a615a3ed43926c61b2042f8918ac3
Michał Maślanka <michal@redpanda.com>
2024-05-12 13:41:49 +0000

    r/consensus: added logging around pre-vote election phase
    Added more logging about the start and result of pre-vote election.
    
    Signed-off-by: Michał Maślanka <michal@redpanda.com>
    (cherry picked from commit 1f0203fd915d4313614575a690a0a871cf06ff86)

--- src/v/raft/consensus.cc
+++ src/v/raft/consensus.cc
@@ -919,6 +919,12 @@ consensus::dispatch_prevote(bool leadership_transfer) {
 
     election_success success = election_success::no;
     try {
+        vlog(
+          _ctxlog.info,
+          "starting pre-vote leader election, current term: {}, leadership "
+          "transfer: {}",
+          _term,
+          leadership_transfer);
         success = co_await pv_stm->vote(leadership_transfer);
     } catch (...) {
         vlog(
@@ -994,6 +1000,13 @@ void consensus::dispatch_vote(bool leadership_transfer) {
             return dispatch_prevote(leadership_transfer)
               .then([this, leadership_transfer](
                       election_success prevote_success) mutable {
+                  vlog(
+                    _ctxlog.debug,
+                    "pre-vote phase success: {}, current term: {}, "
+                    "leadership transfer: {}",
+                    prevote_success,
+                    _term,
+                    leadership_transfer);
                   // if a current node is not longer candidate we should skip
                   // proceeding to actual vote phase
                   if (!prevote_success || _vstate != vote_state::candidate) {
