0f3d21ca0f8b99c25403083d4f0c7897efa83014
Michał Maślanka <michal@redpanda.com>
2024-05-27 07:11:24 +0000

    c/migrations: introduced data migrated resources
    Introduced a class that is going to be instantiated on every shard and
    will contain information about migrated resources. The class is
    intended to be used by a validation logic in hot path where migration
    information will be queried to block writes and properties updates.
    
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- src/v/cluster/fwd.h
+++ src/v/cluster/fwd.h
@@ -81,6 +81,9 @@ class shard_balancer;
 class id_allocator_stm;
 class tm_stm;
 class rm_stm;
+namespace data_migrations {
+class migrated_resources;
+} // namespace data_migrations
 
 namespace tx {
 class producer_state_manager;
