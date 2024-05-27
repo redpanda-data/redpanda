d0c863bf8dd97bbcd44fc2946170c218f7b57503
Michał Maślanka <michal@redpanda.com>
2024-05-27 06:53:48 +0000

    c/logger: introduced data migrations logger
    Introduced separate logger for data migrations to easily separate all
    log entries related with migrating data across the clusters.
    
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- src/v/cluster/logger.cc
+++ src/v/cluster/logger.cc
@@ -12,4 +12,5 @@
 namespace cluster {
 ss::logger clusterlog{"cluster"};
 ss::logger txlog{"tx"};
+ss::logger dm_log{"d-migrations"};
 } // namespace cluster
--- src/v/cluster/logger.h
+++ src/v/cluster/logger.h
@@ -18,4 +18,5 @@
 namespace cluster {
 extern ss::logger clusterlog;
 extern ss::logger txlog;
+extern ss::logger dm_log;
 } // namespace cluster
