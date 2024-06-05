878c4f75d264d9031f681def3bdf3787c324fb2f
Ben Pope <ben@redpanda.com>
2024-06-05 11:32:56 +0100

    kafka/quotas: combine alter and describe
    This will allow sharing code between the implementations.

--- src/v/kafka/server/handlers/client_quotas.cc
+++ src/v/kafka/server/handlers/client_quotas.cc
@@ -9,14 +9,21 @@
  * by the Apache License, Version 2.0
  */
 
-#include "kafka/server/handlers/alter_client_quotas.h"
-
 #include "kafka/protocol/schemata/alter_client_quotas_request.h"
 #include "kafka/protocol/schemata/alter_client_quotas_response.h"
+#include "kafka/protocol/schemata/describe_client_quotas_request.h"
+#include "kafka/protocol/schemata/describe_client_quotas_response.h"
+#include "kafka/server/handlers/alter_client_quotas.h"
+#include "kafka/server/handlers/describe_client_quotas.h"
 
 namespace kafka {
 
 namespace {
+
+describe_client_quotas_response
+make_response(describe_client_quotas_response_data&& resp_data) {
+    return describe_client_quotas_response{.data = std::move(resp_data)};
+}
 
 void make_error_response(
   alter_client_quotas_request& req, alter_client_quotas_response& resp) {
@@ -30,6 +37,22 @@ void make_error_response(
 }
 
 } // namespace
+
+template<>
+ss::future<response_ptr> describe_client_quotas_handler::handle(
+  request_context ctx, ss::smp_service_group) {
+    describe_client_quotas_request request;
+    request.decode(ctx.reader(), ctx.header().version);
+    log_request(ctx.header(), request);
+
+    // TODO: implement the DescribeClientQuotas API
+    // ctx.quota_store().get_quota(...);
+
+    co_return co_await ctx.respond(make_response({
+      .error_code = error_code::unsupported_version,
+      .error_message = "Unsupported version - not yet implemented",
+    }));
+}
 
 template<>
 ss::future<response_ptr> alter_client_quotas_handler::handle(
