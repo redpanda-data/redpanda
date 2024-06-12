588e7b45085371562e968ca8ca2c2fb3af152cb7
Ben Pope <ben@redpanda.com>
2024-06-12 15:15:26 +0100

    schema_registry: Create json_schema_definitions
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/pandaproxy/schema_registry/json.cc
+++ src/v/pandaproxy/schema_registry/json.cc
@@ -9,9 +9,13 @@
  * by the Apache License, Version 2.0
  */
 
+#include "pandaproxy/schema_registry/json.h"
+
 #include "json/document.h"
 #include "json/stringbuffer.h"
 #include "json/writer.h"
+#include "pandaproxy/schema_registry/error.h"
+#include "pandaproxy/schema_registry/errors.h"
 #include "pandaproxy/schema_registry/types.h"
 
 #include <seastar/core/coroutine.hh>
@@ -66,5 +70,41 @@ canonical_schema_definition::raw_string json_schema_definition::raw() const {
 }
 
 ss::sstring json_schema_definition::name() const { return {_impl->name}; };
+
+namespace {
+
+result<json::Document> parse_json(std::string_view v) {
+    json::Document doc;
+    doc.Parse(v.data(), v.size());
+    if (doc.HasParseError()) {
+        return error_info{
+          error_code::schema_invalid,
+          fmt::format(
+            "Invalid schema: {} at offset {}",
+            rapidjson::GetParseError_En(doc.GetParseError()),
+            doc.GetErrorOffset())};
+    }
+    return {std::move(doc)};
+}
+
+} // namespace
+
+ss::future<json_schema_definition>
+make_json_schema_definition(sharded_store&, canonical_schema schema) {
+    auto doc = parse_json(schema.def().raw()()).value(); // throws on error
+    std::string_view name = schema.sub()();
+    auto refs = std::move(schema).def().refs();
+    co_return json_schema_definition{
+      ss::make_shared<json_schema_definition::impl>(std::move(doc), name),
+      std::move(refs)};
+}
+
+ss::future<canonical_schema>
+make_canonical_json_schema(sharded_store&, unparsed_schema def) {
+    // TODO BP: More validation and normalisation
+    parse_json(def.def().raw()()).value(); // throws on error
+    co_return canonical_schema{
+      def.sub(), canonical_schema_definition{def.def().raw(), def.type()}};
+}
 
 } // namespace pandaproxy::schema_registry
