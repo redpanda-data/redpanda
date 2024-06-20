2d7e19a14f4a3c891070313eb507492a492cdfa5
Andrew Wong <awong@redpanda.com>
2024-06-20 01:28:45 -0700

    config: property for disabling remote labels for tests

--- src/v/config/configuration.cc
+++ src/v/config/configuration.cc
@@ -2161,6 +2161,16 @@ configuration::configuration()
       "timeout error.",
       {.needs_restart = needs_restart::no, .visibility = visibility::tunable},
       600s)
+  , cloud_storage_disable_remote_labels_for_tests(
+      *this,
+      "cloud_storage_disable_remote_labels_for_tests",
+      "If 'true', Redpanda disables remote labels and falls back on the "
+      "hash-based object naming scheme for new topics. This property exists to "
+      "simplify testing "
+      "and shouldn't be set in production.",
+      {.needs_restart = needs_restart::no, .visibility = visibility::tunable},
+      // TODO(awong): enable once stable
+      true)
   , cloud_storage_azure_storage_account(
       *this,
       "cloud_storage_azure_storage_account",
