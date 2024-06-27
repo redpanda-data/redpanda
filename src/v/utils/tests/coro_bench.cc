2db3849e606d239683f224d196f920ce4e4f73af
Travis Downs <travis.downs@vectorized.io>
2024-06-27 12:58:17 -0400

    coro_bench: updates with more patterns
    Test a few more call patterns such as chaining vs nesting when the first
    future is non-ready.

--- src/v/utils/tests/coro_bench.cc
+++ src/v/utils/tests/coro_bench.cc
@@ -1,15 +1,26 @@
 #include "base/seastarx.h"
+#include "ssx/future-util.h"
 
 #include <seastar/core/coroutine.hh>
 #include <seastar/core/future.hh>
 #include <seastar/testing/perf_tests.hh>
 #include <seastar/util/later.hh>
 
-bool always_false_coro = false;
+#include <string>
+#include <utility>
 
 namespace {
 
 static constexpr size_t ITERS = 10000;
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+static bool always_false_global = false;
+
+// always return false, but the compiler can't prove it
+bool always_false() {
+    perf_tests::do_not_optimize(always_false_global);
+    return always_false_global;
+}
 
 template<typename F>
 ss::future<size_t> co_await_in_loop(F f) {
@@ -45,26 +56,151 @@ ss::future<size_t> collect_futures(F f) {
     return ss::make_ready_future<int>(1);
 }
 
-ss::future<int> empty_coro() {
+ss::future<int> always_ready_coro() {
     int x = 0;
-    if (always_false_coro) {
+    if (always_false()) {
         x = co_await seastar::coroutine::without_preemption_check(
           ready_future());
     }
     co_return x;
 }
 
+/**
+ * @brief Returns always ready future, but across an optimization barrier.
+ */
+inline ss::future<> always_ready() {
+    if (always_false()) [[unlikely]] {
+        return ss::yield();
+    }
+    return ss::now();
+}
+
+template<typename T>
+inline ss::future<T> always_ready(T&& t) {
+    if (always_false()) [[unlikely]] {
+        return ss::yield().then([=]() mutable { return std::move(t); });
+    }
+    return ssx::now(std::forward<T>(t));
+}
+
+/**
+ * @brief Returns an always ready future after co_awaiting it.
+ */
 ss::future<> never_awaits() {
-    if (always_false_coro) {
+    if (always_false()) {
         co_await ss::make_ready_future<>();
     }
 }
 
 struct coro_bench {};
 
-inline ss::future<> co_await_ready() { co_await ss::make_ready_future<>(); }
-inline ss::future<> co_await_ready_nest2() { co_await co_await_ready(); }
-inline ss::future<> co_await_ready_nest3() { co_await co_await_ready_nest2(); }
+volatile int some_int;
+
+void do_work() { some_int = 1; }
+
+template<typename T>
+auto do_work(T& t) {
+    return always_ready(std::move(t));
+}
+
+inline ss::future<> co_await_ready() {
+    co_await always_ready();
+    do_work();
+}
+
+template<size_t depth>
+inline ss::future<> co_await_ready_nested() {
+    static_assert(depth > 0);
+
+    if constexpr (depth == 1) {
+        co_await always_ready();
+    } else {
+        co_await co_await_ready_nested<depth - 1>();
+    }
+    do_work();
+}
+
+struct small_object {
+    char x;
+};
+
+struct large_object {
+    char x[33];
+};
+
+/**
+ * Does an unconditional yield, followed by series of
+ * non-yielding then continuations, but which are all tested
+ * inside the single then() chained off of the yield.
+ *
+ * Compared to the chained_after_yield immediately after, where
+ * all the continuations are directly chained, it shows the
+ * efficiency of the nesting here, because the yield causes
+ * each continuation in the attached chain to become a separate
+ * task, even if they would never yield themselves. The nesting
+ * avoids this as it isn't even evaluated until after the yield
+ * finishes.
+ */
+ss::future<> nested_after_yield() {
+    using T = small_object;
+    T t{};
+    return yield().then([t = t]() mutable {
+        return do_work(t)
+          .then([](T t) { return do_work(t); })
+          .then([](T t) { return do_work(t); })
+          .then([](T t) { return do_work(t); })
+          .then([](T t) { return do_work(t); })
+          .then([](T) { return always_ready(); });
+    });
+}
+
+ss::future<> chained_after_yield() {
+    using T = small_object;
+    T t{};
+    return yield()
+      .then([t = t]() mutable { return do_work(t); })
+      .then([](T t) { return do_work(t); })
+      .then([](T t) { return do_work(t); })
+      .then([](T t) { return do_work(t); })
+      .then([](T t) { return do_work(t); })
+      .then([](T) { return always_ready(); })
+      .finally([] {})
+      .finally([] {});
+}
+
+ss::future<> coro_after_yield() {
+    small_object t{};
+    co_await yield();
+
+    t = co_await do_work(t);
+    t = co_await do_work(t);
+    t = co_await do_work(t);
+    t = co_await do_work(t);
+    t = co_await do_work(t);
+}
+
+ss::future<> chain_then5() {
+    using T = std::string;
+    T t{};
+    return do_work(t)
+      .then([](T t) { return t; })
+      .then([](T t) { return t; })
+      .then([](T t) { return t; })
+      .then([](T t) { return t; })
+      .then([](T t) { return t; })
+      .then([](T) { return always_ready(); });
+}
+
+inline ss::future<> nested_then5() {
+    return always_ready().then([] {
+        return always_ready().then([] {
+            return always_ready().then([] {
+                return always_ready().then(
+                  [] { return always_ready().then([] { do_work(); }); });
+            });
+        });
+    });
+}
 
 } // namespace
 
@@ -82,25 +218,33 @@ PERF_TEST_F(coro_bench, co_await_ready_collect) {
     return collect_futures(co_await_ready);
 }
 
-PERF_TEST_F(coro_bench, co_await_ready_nest2) {
-    return co_await_in_loop(co_await_ready_nest2);
+PERF_TEST_F(coro_bench, nested_then5) { return co_await_in_loop(nested_then5); }
+
+PERF_TEST_F(coro_bench, chain_then5) { return co_await_in_loop(chain_then5); }
+
+PERF_TEST_F(coro_bench, nested_after_yield) {
+    return co_await_in_loop(nested_after_yield);
 }
 
-PERF_TEST_F(coro_bench, co_await_ready_nest2_collect) {
-    return collect_futures(co_await_ready_nest2);
+PERF_TEST_F(coro_bench, chained_after_yield) {
+    return co_await_in_loop(chained_after_yield);
 }
 
-PERF_TEST_F(coro_bench, co_await_ready_nest3) {
-    return co_await_in_loop(co_await_ready_nest3);
+PERF_TEST_F(coro_bench, coro_after_yield) {
+    return co_await_in_loop(coro_after_yield);
 }
 
-PERF_TEST_F(coro_bench, co_await_ready_nest3_collect) {
-    return collect_futures(co_await_ready_nest3);
+PERF_TEST_F(coro_bench, co_await_ready_nested3) {
+    return co_await_in_loop(co_await_ready_nested<3>);
 }
 
-PERF_TEST_F(coro_bench, empty_coro) { return co_await_in_loop(empty_coro); }
+PERF_TEST_F(coro_bench, co_await_ready_nested5) {
+    return co_await_in_loop(co_await_ready_nested<5>);
+}
 
-PERF_TEST_F(coro_bench, never_awaits) { return co_await_in_loop(never_awaits); }
+PERF_TEST_F(coro_bench, empty_coro) {
+    return co_await_in_loop(always_ready_coro);
+}
 
 PERF_TEST_F(coro_bench, never_awaits_collect) {
     return collect_futures(never_awaits);
