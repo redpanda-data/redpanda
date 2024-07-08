660011d72d9360176d198dc54213ed4e7559cd82
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2024-07-08 14:38:02 +0100

    k/alter: reorder property updates
    Pure refactor to pull out non-default configs and order the rest in the
    same way as further down in the for loop.

--- src/v/kafka/server/handlers/alter_configs.cc
+++ src/v/kafka/server/handlers/alter_configs.cc
@@ -76,20 +76,8 @@ create_topic_properties_update(
     update.properties.segment_size.op = op_t::remove;
     update.properties.timestamp_type.op = op_t::remove;
     update.properties.retention_bytes.op = op_t::remove;
-    update.properties.retention_duration.op = op_t::remove;
     update.properties.shadow_indexing.op = op_t::remove;
-
-    update.custom_properties.replication_factor.op = op_t::none;
-    update.custom_properties.data_policy.op = op_t::none;
-
-    /**
-     * Since 'cleanup.policy' is always defaulted to 'delete' at topic creation,
-     * we must special case the handling to preserve this default.
-     */
-    update.properties.cleanup_policy_bitflags.op = op_t::set;
-    update.properties.cleanup_policy_bitflags.value
-      = ctx.metadata_cache().get_default_cleanup_policy_bitflags();
-
+    update.properties.retention_duration.op = op_t::remove;
     update.properties.record_key_schema_id_validation.op = op_t::remove;
     update.properties.record_key_schema_id_validation_compat.op = op_t::remove;
     update.properties.record_key_subject_name_strategy.op = op_t::remove;
@@ -100,6 +88,17 @@ create_topic_properties_update(
     update.properties.record_value_subject_name_strategy.op = op_t::remove;
     update.properties.record_value_subject_name_strategy_compat.op
       = op_t::remove;
+
+    /**
+     * Since 'cleanup.policy' is always defaulted to 'delete' at topic creation,
+     * we must special case the handling to preserve this default.
+     */
+    update.properties.cleanup_policy_bitflags.op = op_t::set;
+    update.properties.cleanup_policy_bitflags.value
+      = ctx.metadata_cache().get_default_cleanup_policy_bitflags();
+
+    update.custom_properties.replication_factor.op = op_t::none;
+    update.custom_properties.data_policy.op = op_t::none;
 
     schema_id_validation_config_parser schema_id_validation_config_parser{
       update.properties};
