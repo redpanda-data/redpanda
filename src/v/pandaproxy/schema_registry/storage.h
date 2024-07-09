020050ff74d5aca94e63c61f4cbc4e6ec9702927
Ben Pope <ben@redpanda.com>
2024-07-09 22:29:00 +0100

    schema_registry: Use iobuf_writer
    Signed-off-by: Ben Pope <ben@redpanda.com>
    (cherry picked from commit 609db2ee5e688eab510877999b0232514f2c2db4)

--- src/v/pandaproxy/schema_registry/storage.h
+++ src/v/pandaproxy/schema_registry/storage.h
@@ -12,6 +12,7 @@
 #pragma once
 
 #include "bytes/iobuf_parser.h"
+#include "json/iobuf_writer.h"
 #include "json/json.h"
 #include "json/types.h"
 #include "json/writer.h"
@@ -325,7 +326,8 @@ using unparsed_schema_value = schema_value<unparsed_schema_defnition_tag>;
 using canonical_schema_value = schema_value<canonical_schema_definition_tag>;
 
 template<typename Buffer, typename Tag>
-void rjson_serialize(::json::Writer<Buffer>& w, const schema_value<Tag>& val) {
+void rjson_serialize(
+  ::json::iobuf_writer<Buffer>& w, const schema_value<Tag>& val) {
     w.StartObject();
     w.Key("subject");
     ::json::rjson_serialize(w, val.schema.sub());
