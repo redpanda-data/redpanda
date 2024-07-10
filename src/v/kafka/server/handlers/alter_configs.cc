8fc8ed94abece18dfd12176f0881141952759529
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2024-07-10 11:45:42 +0100

    k/alter_configs: refactor topic property updates
    Pure refactor.
    
    * Reorder the configs to pull out non-default configs and order the rest
      in the same way as further down in the for loop.
    * Alias the `opt_t` for better formatting.
    
    (cherry picked from commit c8b7d92236f6ee0d2a85a90d8e559305492b2960)

--- src/v/kafka/server/handlers/alter_configs.cc
+++ src/v/kafka/server/handlers/alter_configs.cc
@@ -56,6 +56,8 @@ static void parse_and_set_shadow_indexing_mode(
 
 checked<cluster::topic_properties_update, alter_configs_resource_response>
 create_topic_properties_update(alter_configs_resource& resource) {
+    using op_t = cluster::incremental_update_operation;
+
     model::topic_namespace tp_ns(
       model::kafka_namespace, model::topic(resource.resource_name));
     cluster::topic_properties_update update(tp_ns);
@@ -67,41 +69,24 @@ create_topic_properties_update(alter_configs_resource& resource) {
      * configuration in topic table, the only difference is the replication
      * factor, if not set in the request explicitly it will not be overriden.
      */
-    update.properties.compaction_strategy.op
-      = cluster::incremental_update_operation::set;
-    update.properties.compression.op
-      = cluster::incremental_update_operation::set;
-    update.properties.segment_size.op
-      = cluster::incremental_update_operation::set;
-    update.properties.timestamp_type.op
-      = cluster::incremental_update_operation::set;
-    update.properties.retention_bytes.op
-      = cluster::incremental_update_operation::set;
-    update.properties.retention_duration.op
-      = cluster::incremental_update_operation::set;
-    update.properties.shadow_indexing.op
-      = cluster::incremental_update_operation::set;
-    update.custom_properties.replication_factor.op
-      = cluster::incremental_update_operation::none;
-    update.custom_properties.data_policy.op
-      = cluster::incremental_update_operation::none;
+    update.properties.compaction_strategy.op = op_t::set;
+    update.properties.compression.op = op_t::set;
+    update.properties.segment_size.op = op_t::set;
+    update.properties.timestamp_type.op = op_t::set;
+    update.properties.retention_bytes.op = op_t::set;
+    update.properties.shadow_indexing.op = op_t::set;
+    update.properties.retention_duration.op = op_t::set;
+    update.properties.record_key_schema_id_validation.op = op_t::set;
+    update.properties.record_key_schema_id_validation_compat.op = op_t::set;
+    update.properties.record_key_subject_name_strategy.op = op_t::set;
+    update.properties.record_key_subject_name_strategy_compat.op = op_t::set;
+    update.properties.record_value_schema_id_validation.op = op_t::set;
+    update.properties.record_value_schema_id_validation_compat.op = op_t::set;
+    update.properties.record_value_subject_name_strategy.op = op_t::set;
+    update.properties.record_value_subject_name_strategy_compat.op = op_t::set;
 
-    update.properties.record_key_schema_id_validation.op
-      = cluster::incremental_update_operation::set;
-    update.properties.record_key_schema_id_validation_compat.op
-      = cluster::incremental_update_operation::set;
-    update.properties.record_key_subject_name_strategy.op
-      = cluster::incremental_update_operation::set;
-    update.properties.record_key_subject_name_strategy_compat.op
-      = cluster::incremental_update_operation::set;
-    update.properties.record_value_schema_id_validation.op
-      = cluster::incremental_update_operation::set;
-    update.properties.record_value_schema_id_validation_compat.op
-      = cluster::incremental_update_operation::set;
-    update.properties.record_value_subject_name_strategy.op
-      = cluster::incremental_update_operation::set;
-    update.properties.record_value_subject_name_strategy_compat.op
-      = cluster::incremental_update_operation::set;
+    update.custom_properties.replication_factor.op = op_t::none;
+    update.custom_properties.data_policy.op = op_t::none;
 
     schema_id_validation_config_parser schema_id_validation_config_parser{
       update.properties};
