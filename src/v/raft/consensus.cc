9b912481b47c322cc31c2f14209711099f142fbf
alexey bashtanov <alexey.bashtanov@redpanda.com>
2024-07-11 11:44:38 +0100

    r/consensus: remove dead code
    vstm has already been moved-from, it makes no sense to capture it here
    
    (cherry picked from commit cafb4458f7fe29071ddc2d4e9ffa1a4224b6cfae)

--- src/v/raft/consensus.cc
+++ src/v/raft/consensus.cc
@@ -1039,9 +1039,7 @@ void consensus::dispatch_vote(bool leadership_transfer) {
                           }
                           // background
                           ssx::spawn_with_gate(
-                            _bg,
-                            [vstm = std::move(vstm),
-                             f = std::move(f)]() mutable {
+                            _bg, [f = std::move(f)]() mutable {
                                 return std::move(f);
                             });
 
