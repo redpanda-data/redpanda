f4f231a8430a985a0ff3dacf6ce20809ff2bfcf6
Andrew Wong <awong@redpanda.com>
2024-07-15 02:04:08 -0700

    iceberg: json serde for data types
    Adds JSON serialization for newly added Iceberg data types.
    
    A test is added to demonstrate the roundtrip with a type comprised of
    multiple nested fields.

---
+++ src/v/iceberg/datatypes_json.h
@@ -0,0 +1,42 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+#pragma once
+
+#include "iceberg/datatypes.h"
+#include "json/_include_first.h"
+#include "json/document.h"
+#include "json/stringbuffer.h"
+#include "json/writer.h"
+
+namespace iceberg {
+
+struct_type parse_struct(const json::Value&);
+list_type parse_list(const json::Value&);
+map_type parse_map(const json::Value&);
+field_type parse_type(const json::Value&);
+nested_field_ptr parse_field(const json::Value&);
+
+} // namespace iceberg
+
+namespace json {
+
+void rjson_serialize(
+  json::Writer<json::StringBuffer>& w, const iceberg::nested_field& f);
+void rjson_serialize(
+  json::Writer<json::StringBuffer>& w, const iceberg::primitive_type& t);
+void rjson_serialize(
+  json::Writer<json::StringBuffer>& w, const iceberg::struct_type& t);
+void rjson_serialize(
+  json::Writer<json::StringBuffer>& w, const iceberg::list_type& t);
+void rjson_serialize(
+  json::Writer<json::StringBuffer>& w, const iceberg::map_type& t);
+void rjson_serialize(
+  json::Writer<json::StringBuffer>& w, const iceberg::field_type& t);
+
+} // namespace json
