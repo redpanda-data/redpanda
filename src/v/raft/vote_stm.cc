ea9627fbda7662da1296b15af86215de600af6ca
alexey bashtanov <alexey.bashtanov@redpanda.com>
2024-07-17 09:24:30 +0100

    r/vote_stm: guard from concurrent elections where we are the candidate
    Use a lock held by critical part of the voting process,
    released when decision is made
    
    (cherry picked from commit 97e52de555701a0ee5ce6129bca8bef82920cceb)

--- src/v/raft/vote_stm.cc
+++ src/v/raft/vote_stm.cc
@@ -93,53 +93,56 @@ ss::future<> vote_stm::dispatch_one(vnode n) {
 
 ss::future<> vote_stm::vote(bool leadership_transfer) {
     using skip_vote = ss::bool_class<struct skip_vote_tag>;
-    return _ptr->_op_lock
-      .with([this, leadership_transfer] {
-          _config = _ptr->config();
-          // check again while under op_sem
-          if (_ptr->should_skip_vote(leadership_transfer)) {
-              return ss::make_ready_future<skip_vote>(skip_vote::yes);
-          }
-          // 5.2.1 mark node as candidate, and update leader id
-          _ptr->_vstate = consensus::vote_state::candidate;
-          //  only trigger notification when we had a leader previously
-          if (_ptr->_leader_id) {
-              _ptr->_leader_id = std::nullopt;
-              _ptr->trigger_leadership_notification();
-          }
+    return _ptr->_election_lock.with([this, leadership_transfer] {
+        return _ptr->_op_lock
+          .with([this, leadership_transfer] {
+              _config = _ptr->config();
+              // check again while under op_sem
+              if (_ptr->should_skip_vote(leadership_transfer)) {
+                  return ss::make_ready_future<skip_vote>(skip_vote::yes);
+              }
+              // 5.2.1 mark node as candidate, and update leader id
+              _ptr->_vstate = consensus::vote_state::candidate;
+              //  only trigger notification when we had a leader previously
+              if (_ptr->_leader_id) {
+                  _ptr->_leader_id = std::nullopt;
+                  _ptr->trigger_leadership_notification();
+              }
 
-          // 5.2.1.2
-          _ptr->_term += model::term_id(1);
-          _ptr->_voted_for = {};
+              // 5.2.1.2
+              _ptr->_term += model::term_id(1);
+              _ptr->_voted_for = {};
 
-          // special case, it may happen that node requesting votes is not a
-          // voter, it may happen if it is a learner in previous configuration
-          _replies.emplace(_ptr->_self, vmeta{});
+              // special case, it may happen that node requesting votes is not a
+              // voter, it may happen if it is a learner in previous
+              // configuration
+              _replies.emplace(_ptr->_self, vmeta{});
 
-          // vote is the only method under _op_sem
-          _config->for_each_voter(
-            [this](vnode id) { _replies.emplace(id, vmeta{}); });
-          auto lstats = _ptr->_log->offsets();
-          auto last_entry_term = _ptr->get_last_entry_term(lstats);
+              // vote is the only method under _op_sem
+              _config->for_each_voter(
+                [this](vnode id) { _replies.emplace(id, vmeta{}); });
+              auto lstats = _ptr->_log->offsets();
+              auto last_entry_term = _ptr->get_last_entry_term(lstats);
 
-          _req = vote_request{
-            .node_id = _ptr->_self,
-            .group = _ptr->group(),
-            .term = _ptr->term(),
-            .prev_log_index = lstats.dirty_offset,
-            .prev_log_term = last_entry_term,
-            .leadership_transfer = leadership_transfer};
-          // we have to self vote before dispatching vote request to
-          // other nodes, this vote has to be done under op semaphore as
-          // it changes voted_for state
-          return self_vote().then([] { return skip_vote::no; });
-      })
-      .then([this](skip_vote skip) {
-          if (skip) {
-              return ss::make_ready_future<>();
-          }
-          return do_vote();
-      });
+              _req = vote_request{
+                .node_id = _ptr->_self,
+                .group = _ptr->group(),
+                .term = _ptr->term(),
+                .prev_log_index = lstats.dirty_offset,
+                .prev_log_term = last_entry_term,
+                .leadership_transfer = leadership_transfer};
+              // we have to self vote before dispatching vote request to
+              // other nodes, this vote has to be done under op semaphore as
+              // it changes voted_for state
+              return self_vote().then([] { return skip_vote::no; });
+          })
+          .then([this](skip_vote skip) {
+              if (skip) {
+                  return ss::make_ready_future<>();
+              }
+              return do_vote();
+          });
+    });
 }
 
 ss::future<> vote_stm::do_vote() {
