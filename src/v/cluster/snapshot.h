fb7731c83d9306b8152207cb070cfc9d38a87160
Noah Watkins <noahwatkins@gmail.com>
2024-07-19 22:46:10 -0700

    cluster: factor out snapshot file names
    This is shared with raft, but we don't want raft to depend on all of the
    cluster library.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ src/v/cluster/snapshot.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#pragma once
+
+namespace cluster {
+
+/// Names of snapshot files used by stm's
+static const ss::sstring archival_stm_snapshot = "archival_metadata.snapshot";
+static const ss::sstring rm_stm_snapshot = "tx.snapshot";
+static const ss::sstring tm_stm_snapshot = "tx.coordinator.snapshot";
+static const ss::sstring id_allocator_snapshot = "id.snapshot";
+static const ss::sstring tx_registry_snapshot = "tx_registry.snapshot";
+
+} // namespace cluster
