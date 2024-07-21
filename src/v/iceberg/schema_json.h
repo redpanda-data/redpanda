b037d01f7818e8117bc6982527509b7ba312cde1
Andrew Wong <awong@redpanda.com>
2024-07-20 17:22:34 -0700

    iceberg: add schema json serialization
    Adds JSON serialization for the newly added Iceberg schema struct.
    
    A test is added to demonstrate the roundtrip with a nested schema.

---
+++ src/v/iceberg/schema_json.h
@@ -0,0 +1,28 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+#pragma once
+
+#include "iceberg/schema.h"
+#include "json/_include_first.h"
+#include "json/document.h"
+#include "json/stringbuffer.h"
+#include "json/writer.h"
+
+namespace iceberg {
+
+schema parse_schema(const json::Value&);
+
+} // namespace iceberg
+
+namespace json {
+
+void rjson_serialize(
+  json::Writer<json::StringBuffer>& w, const iceberg::schema& s);
+
+} // namespace json
