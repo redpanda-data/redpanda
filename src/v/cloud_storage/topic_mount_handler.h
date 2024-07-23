78c52e5cf9f1cb7c78cdf7387aa121b5da3b4f9a
Willem Kaufmann <willem.kaufmann@redpanda.com>
2024-07-23 09:00:43 -0400

    cloud_storage: add `topic_mount_handler`
    The `topic_mount_handler` class is a thin wrapper around `topic_mount_manifest`
    operations in cloud storage.
    
    Most notably, it provides the `mount_topic()` and `unmount_topic()` functions,
    which construct a `topic_mount_manifest` and `remote_path_provider`, and
    perform the necessary cloud storage checks/operations for mounting a topic
    from a provided `topic_configuration`, and returns the outcome in form of a
    `topic_mount/unmount_result` enum.
    
    The cloud storage operations used for mounting are currently non-atomic,
    with individual calls used to 1. check for the existence of a
    `topic_mount_manifest` and to 2. delete it. This could lead to race
    conditions involving multiple clusters and topic mounting in the future, as
    `DeleteObject` requests will return a success outcome if a key to be deleted
    is not found in cloud storage.
    
    (cherry picked from commit bbd86d4767de76839922f89747b7c28c7e9fc572)

---
+++ src/v/cloud_storage/topic_mount_handler.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+#pragma once
+
+#include "cloud_storage/fwd.h"
+#include "cloud_storage/remote_label.h"
+#include "cloud_storage_clients/types.h"
+#include "cluster/topic_configuration.h"
+#include "model/fundamental.h"
+#include "utils/retry_chain_node.h"
+
+namespace cloud_storage {
+
+enum class topic_mount_result {
+    mount_manifest_does_not_exist,
+    mount_manifest_not_deleted,
+    success
+};
+
+std::ostream& operator<<(std::ostream& o, const topic_mount_result& r);
+
+enum class topic_unmount_result { mount_manifest_not_created, success };
+
+std::ostream& operator<<(std::ostream& o, const topic_unmount_result& r);
+
+class topic_mount_handler {
+public:
+    topic_mount_handler(
+      const cloud_storage_clients::bucket_name& bucket, remote& remote);
+
+    // Perform the mounting process by deleting the topic mount manifest.
+    // topic_cfg should be the recovered topic configuration from a topic
+    // manifest in cloud storage. If it has a value, the remote_label stored in
+    // the topic properties is used as the "source" label. Otherwise, the
+    // default uuid (all zeros) is used.
+    ss::future<topic_mount_result> mount_topic(
+      const cluster::topic_configuration& topic_cfg, retry_chain_node& parent);
+
+    // Perform the mounting process by deleting the topic mount manifest.
+    // topic_cfg should be the recovered topic configuration from a topic
+    // manifest in cloud storage. If it has a value, the remote_label stored in
+    // the topic properties is used as the "source" label. Otherwise, the
+    // default uuid (all zeros) is used.
+    ss::future<topic_unmount_result> unmount_topic(
+      const cluster::topic_configuration& topic_cfg, retry_chain_node& parent);
+
+private:
+    // Check for the existence of a topic mount manifest in tiered storage.
+    // If it exists, then the topic can be mounted.
+    ss::future<topic_mount_result> check_mount(
+      const topic_mount_manifest& manifest,
+      const remote_path_provider& path_provider,
+      retry_chain_node& parent);
+
+    // Commits to the mount of the topic by deleting the topic mount manifest in
+    // tiered storage.
+    ss::future<topic_mount_result> commit_mount(
+      const topic_mount_manifest& manifest,
+      const remote_path_provider& path_provider,
+      retry_chain_node& parent);
+
+    cloud_storage_clients::bucket_name _bucket;
+    remote& _remote;
+};
+
+} // namespace cloud_storage
