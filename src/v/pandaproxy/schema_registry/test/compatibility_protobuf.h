4cf17fc33168c9274e3a3abd676b362c0bd0ddc6
Ben Pope <ben@redpanda.com>
2024-07-29 18:35:02 +0100

    schema_registry: Translate base64_decoder_exception
    This is a regression introduced in https://github.com/redpanda-data/redpanda/commit/e7fab4a27489a8862e2bce36589e5770ec089882
    
    Signed-off-by: Ben Pope <ben@redpanda.com>
    (cherry picked from commit e4b64db113e6991ca40f9bd1da4e2fa735f41271)

--- src/v/pandaproxy/schema_registry/test/compatibility_protobuf.h
+++ src/v/pandaproxy/schema_registry/test/compatibility_protobuf.h
@@ -88,3 +88,70 @@ message A1 {
   }
 })",
   pps::schema_type::protobuf};
+
+// Binary encoded protobuf descriptor:
+//
+// syntax = "proto3";
+//
+// package com.redpanda;
+//
+// option go_package = "./;main";
+// option java_multiple_files = true;
+//
+// import "google/protobuf/timestamp.proto";
+//
+// message Payload {
+//     int32 val = 1;
+//     google.protobuf.Timestamp timestamp = 2;
+// }
+//
+// message A {
+//     message B {
+//         message C {
+//             message D {
+//                 message M00 {}
+//                 message M01 {}
+//                 message M02 {}
+//                 message M03 {}
+//                 message M04 {}
+//                 message M05 {}
+//                 message M06 {}
+//                 message M07 {}
+//                 message M08 {}
+//                 message M09 {}
+//                 message M10 {}
+//                 message M11 {}
+//                 message M12 {}
+//                 message M13 {}
+//                 message M14 {}
+//                 message M15 {}
+//                 message M16 {}
+//                 message M17 {}
+//                 message NestedPayload {
+//                     int32 val = 1;
+//                     google.protobuf.Timestamp timestamp = 2;
+//                 }
+//             }
+//         }
+//     }
+// }
+//
+// message CompressiblePayload {
+//     int32 val = 1;
+//     google.protobuf.Timestamp timestamp = 2;
+//     string message = 3;
+// }
+constexpr std::string_view base64_raw_proto{
+  "Cg1wYXlsb2FkLnByb3RvEgxjb20ucmVkcGFuZGEaH2dvb2dsZS9wcm90b2J1Zi90aW1lc3RhbXAu"
+  "cHJvdG8iRQoHUGF5bG9hZBILCgN2YWwYASABKAUSLQoJdGltZXN0YW1wGAIgASgLMhouZ29vZ2xl"
+  "LnByb3RvYnVmLlRpbWVzdGFtcCLgAQoBQRraAQoBQhrUAQoBQxrOAQoBRBoFCgNNMDAaBQoDTTAx"
+  "GgUKA00wMhoFCgNNMDMaBQoDTTA0GgUKA00wNRoFCgNNMDYaBQoDTTA3GgUKA00wOBoFCgNNMDka"
+  "BQoDTTEwGgUKA00xMRoFCgNNMTIaBQoDTTEzGgUKA00xNBoFCgNNMTUaBQoDTTE2GgUKA00xNxpL"
+  "Cg1OZXN0ZWRQYXlsb2FkEgsKA3ZhbBgBIAEoBRItCgl0aW1lc3RhbXAYAiABKAsyGi5nb29nbGUu"
+  "cHJvdG9idWYuVGltZXN0YW1wImIKE0NvbXByZXNzaWJsZVBheWxvYWQSCwoDdmFsGAEgASgFEi0K"
+  "CXRpbWVzdGFtcBgCIAEoCzIaLmdvb2dsZS5wcm90b2J1Zi5UaW1lc3RhbXASDwoHbWVzc2FnZRgD"
+  "IAEoCUILUAFaBy4vO21haW5iBnByb3RvMw=="};
+
+const pps::canonical_schema_definition base64_proto{
+  pps::canonical_schema_definition{
+    base64_raw_proto, pps::schema_type::protobuf}};
