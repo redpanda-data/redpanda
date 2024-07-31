bc3d761b87b42b719ad9fab2172760abd4ac46d0
Bharath Vissapragada <bharathv@redpanda.com>
2024-07-31 14:52:43 -0700

    idempotency: skip sequence checks for forgotten producers
    For producers the broker no longer tracks, we now skip sequence
    checks and allow any non zero sequence. This can happen if the producer
    produced after the producer got evicted from the broker's memory (eg:
    log got prefix truncated, producer hit expiration thresholds etc)
    
    While kip-360 suggests that the broker should throw unknown_producer_id
    error in this case, Apache Kafka no longer does that. Adding to the
    complication not every client implements unknown_producer_id logic
    similiarly, this can result in different behaviors on different clients.
    
    With this patch, we just mimic what Apache Kafka does to be consistent.
    
    Apache Kafka code for future reference.
    https://github.com/apache/kafka/pull/7115/files#diff-5482b26d93c5d36f272f65e628c1692622b69f8ba4a2df04ba74fad23623828dR239

--- src/v/cluster/rm_stm.h
+++ src/v/cluster/rm_stm.h
@@ -233,7 +233,13 @@ private:
     ss::future<> do_remove_persistent_state();
     ss::future<fragmented_vector<tx::tx_range>>
       do_aborted_transactions(model::offset, model::offset);
-    tx::producer_ptr maybe_create_producer(model::producer_identity);
+
+    // Tells whether the producer is already known or is created
+    // for the first time from the incoming request.
+    using producer_previously_known
+      = ss::bool_class<struct new_producer_created_tag>;
+    std::pair<tx::producer_ptr, producer_previously_known>
+      maybe_create_producer(model::producer_identity);
     void cleanup_producer_state(model::producer_identity);
     ss::future<> reset_producers();
     ss::future<checked<model::term_id, tx::errc>> do_begin_tx(
@@ -295,7 +301,8 @@ private:
       model::record_batch_reader,
       raft::replicate_options,
       ss::lw_shared_ptr<available_promise<>>,
-      ssx::semaphore_units&);
+      ssx::semaphore_units&,
+      producer_previously_known);
 
     ss::future<result<kafka_result>> idempotent_replicate(
       model::term_id,
@@ -304,7 +311,8 @@ private:
       model::record_batch_reader,
       raft::replicate_options,
       ss::lw_shared_ptr<available_promise<>>,
-      ssx::semaphore_units);
+      ssx::semaphore_units,
+      producer_previously_known);
 
     ss::future<result<kafka_result>> replicate_msg(
       model::record_batch_reader,
