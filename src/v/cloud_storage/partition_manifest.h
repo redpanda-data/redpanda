69606c39cd6e3c70a5665d2cdedb44fb437fe868
Oren Leiman <oren.leiman@redpanda.com>
2024-08-07 10:03:30 -0700

    cs: serde_fields for partition_manifest::lw_segment_meta
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/cloud_storage/partition_manifest.h
+++ src/v/cloud_storage/partition_manifest.h
@@ -90,6 +90,17 @@ public:
 
         segment_name_format sname_format{segment_name_format::v1};
 
+        auto serde_fields() {
+            return std::tie(
+              ntp_revision,
+              base_offset,
+              committed_offset,
+              archiver_term,
+              segment_term,
+              size_bytes,
+              sname_format);
+        }
+
         auto operator<=>(const lw_segment_meta&) const = default;
 
         static lw_segment_meta convert(const segment_meta& m);
