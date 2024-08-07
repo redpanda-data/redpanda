dc7a9651d031afc2965d603c43b44aaff6a38567
Willem Kaufmann <willem.kaufmann@redpanda.com>
2024-08-07 18:46:03 -0400

    `storage`: add `test_concurrent_log_eviction_and_append`
    To test that we do not encounter data races or other issues with concurrent
    threads appending to and evicting from a `log`.
    
    (cherry picked from commit fd552885c945057f479a6bb025f8840f18db13f8)

--- src/v/storage/tests/storage_e2e_fixture_test.cc
+++ src/v/storage/tests/storage_e2e_fixture_test.cc
@@ -8,6 +8,8 @@
 // by the Apache License, Version 2.0
 
 #include "kafka/server/tests/produce_consume_utils.h"
+#include "model/fundamental.h"
+#include "random/generators.h"
 #include "redpanda/tests/fixture.h"
 #include "test_utils/fixture.h"
 #include "test_utils/scoped_config.h"
@@ -16,10 +18,10 @@
 
 #include <boost/test/tools/old/interface.hpp>
 
+#include <chrono>
 #include <vector>
 
 using namespace std::chrono_literals;
-using std::vector;
 
 struct storage_e2e_fixture : public redpanda_thread_fixture {
     scoped_config test_local_cfg;
@@ -82,4 +84,95 @@ FIXTURE_TEST(test_compaction_segment_ms, storage_e2e_fixture) {
         std::move(p).get();
     }
     app.stress_fiber_manager.local().stop().get();
+}
+
+FIXTURE_TEST(test_concurrent_log_eviction_and_append, storage_e2e_fixture) {
+    const auto topic_name = model::topic("tapioca");
+    const auto ntp = model::ntp(model::kafka_namespace, topic_name, 0);
+    // We will be running housekeeping locally in a loop.
+    test_local_cfg.get("log_disable_housekeeping_for_tests").set_value(true);
+    cluster::topic_properties props;
+    // Try to ensure we stay at 1 segment during our eviction/appending loop.
+    props.segment_size = 1_MiB;
+    props.cleanup_policy_bitflags = model::cleanup_policy_bitflags::deletion;
+    add_topic({model::kafka_namespace, topic_name}, 1, props).get();
+    wait_for_leader(ntp).get();
+
+    auto partition = app.partition_manager.local().get(ntp);
+    auto* log = dynamic_cast<storage::disk_log_impl*>(partition->log().get());
+
+    ss::abort_source as;
+
+    // Config is set up to always garbage collect as much as possible.
+    storage::housekeeping_config cfg(
+      /*gc_upper=*/model::timestamp::max(),
+      /*max_bytes_in_log=*/1,
+      /*max_collect_offset=*/model::offset::min(),
+      ss::default_priority_class(),
+      as);
+
+    const auto num_records = 100;
+    model::offset produce_base_offset{0};
+    model::offset stop_after{num_records * 100};
+    tests::kafka_produce_transport producer(make_kafka_client().get());
+    producer.start().get();
+    auto produce = [&] {
+        return producer
+          .produce_to_partition(
+            topic_name,
+            model::partition_id(0),
+            tests::kv_t::sequence(produce_base_offset(), num_records))
+          .then([&](model::offset base_offset) {
+              BOOST_REQUIRE_EQUAL(base_offset, produce_base_offset);
+              produce_base_offset += model::offset{num_records};
+              if (produce_base_offset >= stop_after) {
+                  as.request_abort();
+              }
+          })
+          .then([] {
+              return ss::sleep(
+                std::chrono::milliseconds(random_generators::get_int(10, 100)));
+          });
+    };
+
+    auto read = [&] {
+        auto lstats = log->offsets();
+        return log
+          ->make_reader(storage::log_reader_config(
+            lstats.start_offset,
+            model::offset::max(),
+            ss::default_priority_class()))
+          .then([](auto reader) {
+              return ss::sleep(std::chrono::milliseconds(
+                                 random_generators::get_int(15, 30)))
+                .then([r = std::move(reader)]() mutable {
+                    return model::consume_reader_to_memory(
+                      std::move(r), model::no_timeout);
+                })
+                .then([](ss::circular_buffer<model::record_batch> batches) {
+                    model::offset prev_base_offset{model::offset::min()};
+                    for (const auto& batch : batches) {
+                        BOOST_REQUIRE_GT(batch.base_offset(), prev_base_offset);
+                        prev_base_offset = batch.base_offset();
+                    }
+                });
+          });
+    };
+
+    auto gc = [&] { return log->housekeeping(cfg); };
+
+    // Concurrent operations
+    auto produce_fut = ss::do_until(
+      [&] { return as.abort_requested(); }, [&] { return produce(); });
+    auto read_fut = ss::do_until(
+      [&] { return as.abort_requested(); }, [&] { return read(); });
+    auto gc_fut = ss::do_until(
+      [&] { return as.abort_requested(); }, [&] { return gc(); });
+
+    ss::when_all(std::move(produce_fut), std::move(read_fut), std::move(gc_fut))
+      .get();
+
+    // log_eviction_stm may have removed the active segment after we finished
+    // final round of eviction.
+    BOOST_REQUIRE_LE(log->segment_count(), 1);
 }
