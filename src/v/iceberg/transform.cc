0d28b17f00728f63ba5a618761dd783ae6ab2945
Andrew Wong <awong@redpanda.com>
2024-08-15 22:40:45 -0700

    iceberg: separate transforms from partitioning
    Transforms were previously in partition.cc because they get serialized
    as a part of the partition spec. But longer term there will be more
    sophisticated logic that is will be needed for transforms specifically
    (e.g. applying transforms) so this pulls them out into their own
    library.
    
    Also adds day_transform to the transform variant (was defined previously
    but not added to the variant).

--- src/v/iceberg/transform.cc
+++ src/v/iceberg/transform.cc
@@ -6,7 +6,7 @@
 // As of the Change Date specified in that file, in accordance with
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
-#include "iceberg/partition.h"
+#include "iceberg/transform.h"
 
 namespace iceberg {
 
