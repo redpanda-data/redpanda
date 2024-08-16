f489f338e988b834362d0dec36caab40bf6583b8
Andrew Wong <awong@redpanda.com>
2024-08-15 23:27:23 -0700

    iceberg: add a logger

---
+++ src/v/iceberg/logger.cc
@@ -0,0 +1,13 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+#include "iceberg/logger.h"
+
+namespace iceberg {
+ss::logger log("iceberg");
+} // namespace iceberg
---
+++ src/v/iceberg/logger.h
@@ -0,0 +1,17 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+#pragma once
+
+#include "base/seastarx.h"
+
+#include <seastar/util/log.hh>
+
+namespace iceberg {
+extern ss::logger log;
+} // namespace iceberg
