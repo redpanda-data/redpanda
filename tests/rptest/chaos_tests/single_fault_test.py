c26f04229972cce108b75f9e1b36276657fa512e
Alexey Zatelepin <ztlpn@redpanda.com>
2024-08-20 11:49:02 +0200

    tests/chaos: port tx_subscribe scenario

--- tests/rptest/chaos_tests/single_fault_test.py
+++ tests/rptest/chaos_tests/single_fault_test.py
@@ -104,6 +104,42 @@ class SingleFaultTestBase(RedpandaTest):
                    err_msg="failed to transfer leadership",
                    retry_on_exc=True)
 
+    def _reconfigure(self,
+                     topic,
+                     partition,
+                     replica_ids,
+                     namespace="kafka",
+                     timeout_s=20):
+        self.logger.info(
+            f"reconfiguring {namespace}/{topic}/{partition} to {replica_ids}")
+        assignment = [{"node_id": rid, "core": 0} for rid in replica_ids]
+        admin = Admin(self.redpanda,
+                      retry_codes=[404, 503, 504],
+                      retries_amount=10)
+        admin.set_partition_replicas(namespace=namespace,
+                                     topic=topic,
+                                     partition=partition,
+                                     replicas=assignment)
+
+        def converged():
+            results = []
+            for n in self.redpanda._started:
+                info = admin.get_partitions(namespace=namespace,
+                                            topic=topic,
+                                            partition=partition,
+                                            node=n)
+                info_replicas = set(r["node_id"] for r in info["replicas"])
+                self.logger.debug(
+                    f"replicas reported by node {n.name}: {info_replicas}, "
+                    f"partition status: {info['status']}")
+                results.append(
+                    set(replica_ids) == info_replicas
+                    and info["status"] == "done")
+
+            return all(results)
+
+        wait_until(converged, timeout_sec=timeout_s, backoff_sec=2)
+
     def _do_run(self, workload: workloads.WorkloadServiceBase,
                 fault: faults.FaultBase | None, timings: TimingConfig,
                 check_progress_during_fault: CheckProgressDuringFaultConfig):
@@ -281,6 +317,171 @@ class SingleTopicTest(SingleFaultTestBase):
         fault = None
         if case.make_fault:
             fault = case.make_fault(self)
+        timings = TimingConfig()
+
+        self.prepare(workload, timings)
+        self.run(workload, fault, timings, case.check_progress_during_fault)
+
+
+class TxSubscribeTest(SingleFaultTestBase):
+    WORKLOAD_SETUP = workloads.TxSubscribeWorkload.Setup(source="source1",
+                                                         source_partitions=3,
+                                                         target="target1",
+                                                         group_id="group1")
+
+    @dataclasses.dataclass
+    class Case:
+        make_fault: Callable[[SingleFaultTestBase],
+                             faults.FaultBase] | None = None
+        check_progress_during_fault: CheckProgressDuringFaultConfig | None = None
+        fail_consistency_on_interruption: bool = False
+
+    CASES = {
+        "baseline":
+        Case(fail_consistency_on_interruption=True),
+        "isolate_source_leader":
+        Case(
+            lambda test: faults.IsolateLeaderFault(
+                test.redpanda,
+                topic=test.WORKLOAD_SETUP.source,
+                partition=0,
+                name="isolate_source_leader",
+            ),
+            CheckProgressDuringFaultConfig(selector="any"),
+        ),
+        "isolate_target_leader":
+        Case(
+            lambda test: faults.IsolateLeaderFault(
+                test.redpanda,
+                topic=test.WORKLOAD_SETUP.target,
+                name="isolate_target_leader",
+            ),
+            CheckProgressDuringFaultConfig(selector="any"),
+        ),
+        "isolate_group_leader":
+        Case(
+            lambda test: faults.IsolateLeaderFault(
+                test.redpanda,
+                topic="__consumer_offsets",
+                partition=0,
+                name="isolate_group_leader",
+            ),
+            CheckProgressDuringFaultConfig(selector="any"),
+        ),
+        "isolate_tx_leader":
+        Case(
+            lambda test: faults.IsolateLeaderFault(
+                test.redpanda,
+                namespace="kafka_internal",
+                topic="tx",
+                partition=0,
+                name="isolate_tx_leader",
+            ),
+            CheckProgressDuringFaultConfig(selector="any"),
+        ),
+        "hijack_tx_ids":
+        Case(lambda test: faults.HijackTxIDsFault(
+            test.redpanda, ["tx-consume-0"], test.logger)),
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, num_brokers=6, **kwargs)
+        self.topics = [
+            TopicSpec(name=self.WORKLOAD_SETUP.source,
+                      replication_factor=3,
+                      partition_count=self.WORKLOAD_SETUP.source_partitions),
+            TopicSpec(name=self.WORKLOAD_SETUP.target,
+                      replication_factor=3,
+                      partition_count=1),
+        ]
+
+    def prepare(self, workload: workloads.WorkloadServiceBase,
+                timings: TimingConfig):
+        workload.start()
+        workload.start_workload()
+
+        workload.wait_progress(timeout_sec=timings.wait_progress_timeout_s)
+
+        internal_node_ids = [
+            self.redpanda.node_id(n) for n in self.redpanda.nodes[0:3]
+        ]
+        data_node_ids = [
+            self.redpanda.node_id(n) for n in self.redpanda.nodes[3:6]
+        ]
+
+        self._reconfigure(namespace="kafka_internal",
+                          topic="tx",
+                          partition=0,
+                          replica_ids=internal_node_ids)
+        self._reconfigure(namespace="kafka_internal",
+                          topic="id_allocator",
+                          partition=0,
+                          replica_ids=internal_node_ids)
+        self._reconfigure(namespace="kafka",
+                          topic="__consumer_offsets",
+                          partition=0,
+                          replica_ids=internal_node_ids)
+
+        for partition in range(0, self.WORKLOAD_SETUP.source_partitions):
+            self._reconfigure(namespace="kafka",
+                              topic=self.WORKLOAD_SETUP.source,
+                              partition=partition,
+                              replica_ids=data_node_ids)
+        self._reconfigure(namespace="kafka",
+                          topic=self.WORKLOAD_SETUP.target,
+                          partition=0,
+                          replica_ids=data_node_ids)
+
+        self.logger.info(f"waiting for post-reconfigure progress")
+        workload.wait_progress(timeout_sec=timings.wait_progress_timeout_s)
+
+        self._transfer_leadership(namespace="redpanda",
+                                  topic="controller",
+                                  partition=0,
+                                  target_id=internal_node_ids[0])
+        self._transfer_leadership(namespace="kafka_internal",
+                                  topic="id_allocator",
+                                  partition=0,
+                                  target_id=internal_node_ids[1])
+        self._transfer_leadership(namespace="kafka",
+                                  topic="__consumer_offsets",
+                                  partition=0,
+                                  target_id=internal_node_ids[1])
+        self._transfer_leadership(namespace="kafka_internal",
+                                  topic="tx",
+                                  partition=0,
+                                  target_id=internal_node_ids[2])
+
+        self._transfer_leadership(namespace="kafka",
+                                  topic=self.WORKLOAD_SETUP.target,
+                                  partition=0,
+                                  target_id=data_node_ids[0])
+        for partition in range(0, self.WORKLOAD_SETUP.source_partitions):
+            self._transfer_leadership(
+                namespace="kafka",
+                topic=self.WORKLOAD_SETUP.source,
+                partition=partition,
+                target_id=data_node_ids[partition % len(data_node_ids)])
+
+        self.logger.info(f"waiting for post-transfer progress")
+        workload.wait_progress(timeout_sec=timings.wait_progress_timeout_s)
+
+    @cluster(num_nodes=8)
+    @matrix(case_id=CASES.keys())
+    def test(self, case_id):
+        case = self.CASES[case_id]
+        workload = workloads.TxSubscribeWorkload(
+            self.test_context,
+            self.redpanda.brokers(),
+            setup=self.WORKLOAD_SETUP,
+            fail_consistency_on_interruption=case.
+            fail_consistency_on_interruption,
+        )
+
+        fault = None
+        if case.make_fault is not None:
+            fault = case.make_fault(self)
+
         timings = TimingConfig()
 
         self.prepare(workload, timings)
