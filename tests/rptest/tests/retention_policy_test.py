32ab540c5d41fa10c571410fcbc4efe4045edc8f
Willem Kaufmann <willem.kaufmann@redpanda.com>
2024-08-19 21:44:02 -0400

    `rptest`: adjust `retention_ms` value in `test_bogus_timestamps`
    The previous value of `retention_ms` of 10 seconds was not permissive
    enough for this test. Raise it to 20 seconds (20,000ms).

--- tests/rptest/tests/retention_policy_test.py
+++ tests/rptest/tests/retention_policy_test.py
@@ -704,9 +704,9 @@ class BogusTimestampTest(RedpandaTest):
             cleanup_policy=TopicSpec.CLEANUP_DELETE,
             # 1 megabyte segments
             segment_bytes=segment_size,
-            # 10 second retention: in the case of mixed timestamps,
+            # 20 second retention: in the case of mixed timestamps,
             # give active segment time to help adjust retention timestamps and show up in log monitor.
-            retention_ms=10000), )
+            retention_ms=20000), )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args,
