ec7ba4dcf2ba362265d841c20a4b5f87aa06375f
Alexey Zatelepin <ztlpn@redpanda.com>
2024-08-28 13:58:13 +0200

    tests/chaos: port list_offsets test scenario

--- tests/rptest/chaos_tests/single_fault_test.py
+++ tests/rptest/chaos_tests/single_fault_test.py
@@ -10,9 +10,17 @@
 import dataclasses
 import sys
 from time import sleep
+import random
+from collections.abc import Callable
 
+from ducktape.mark import matrix
+
+from rptest.util import wait_until
+from rptest.clients.types import TopicSpec
 from rptest.tests.redpanda_test import RedpandaTest
+from rptest.services.admin import Admin
 from rptest.services.failure_injector import FailureInjector
+from rptest.services.cluster import cluster
 from rptest.services.chaos.types import NoProgressError
 import rptest.services.chaos.workloads.all as workloads
 import rptest.services.chaos.faults.all as faults
@@ -59,6 +67,42 @@ class SingleFaultTestBase(RedpandaTest):
                 # check crashes only if the test passed,
                 # if it failed, the check will be performed automatically
                 self.redpanda.raise_on_crash()
+
+    def _transfer_leadership(self,
+                             topic,
+                             target_id,
+                             partition=0,
+                             namespace="kafka",
+                             current_leader_id=None,
+                             admin=None,
+                             timeout_s=10):
+        self.logger.info(
+            f"transferring {namespace}/{topic}/{partition} leader to {target_id}"
+        )
+
+        if admin is None:
+            admin = Admin(self.redpanda)
+
+        def transfer_successful():
+            nonlocal current_leader_id
+            if current_leader_id is None:
+                current_leader_id = admin.await_stable_leader(
+                    namespace=namespace, topic=topic, partition=partition)
+            if current_leader_id == target_id:
+                return True
+
+            admin.transfer_leadership_to(namespace=namespace,
+                                         topic=topic,
+                                         partition=partition,
+                                         leader_id=current_leader_id,
+                                         target_id=target_id)
+            current_leader_id = None
+
+        wait_until(transfer_successful,
+                   timeout_sec=timeout_s,
+                   backoff_sec=1,
+                   err_msg="failed to transfer leadership",
+                   retry_on_exc=True)
 
     def _do_run(self, workload: workloads.WorkloadServiceBase,
                 fault: faults.FaultBase | None, timings: TimingConfig,
@@ -169,3 +213,75 @@ class SingleFaultTestBase(RedpandaTest):
     def tearDown(self):
         FailureInjector(self.redpanda)._heal_all()
         super().tearDown()
+
+
+class SingleTopicTest(SingleFaultTestBase):
+    @dataclasses.dataclass
+    class ListOffsetsCase:
+        make_fault: Callable[[SingleFaultTestBase],
+                             faults.FaultBase] | None = None
+        check_progress_during_fault: CheckProgressDuringFaultConfig | None = None
+
+    LIST_OFFSETS_CASES = {
+        "baseline":
+        ListOffsetsCase(),
+        "isolate_leader":
+        ListOffsetsCase(
+            lambda test: faults.IsolateLeaderFault(test.redpanda,
+                                                   topic=test.topic),
+            CheckProgressDuringFaultConfig(),
+        )
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, num_brokers=3, **kwargs)
+        self.topics = [
+            TopicSpec(name="topic1", replication_factor=3, partition_count=1)
+        ]
+
+    def prepare(self, workload: workloads.WorkloadServiceBase,
+                timings: TimingConfig):
+        admin = Admin(self.redpanda)
+
+        # wait for the topic to come online
+        admin.await_stable_leader(topic=self.topic, timeout_s=20)
+
+        workload.start()
+        workload.start_workload()
+
+        topic_leader_id = admin.await_stable_leader(self.topic)
+        self.logger.debug(f"leader id of '{self.topic}': {topic_leader_id}")
+
+        controller_leader_id = admin.await_stable_leader("controller",
+                                                         namespace="redpanda")
+        self.logger.debug(f"controller leader id: {controller_leader_id}")
+
+        if topic_leader_id == controller_leader_id:
+            other_ids = [
+                self.redpanda.node_id(n) for n in self.redpanda.nodes
+                if self.redpanda.node_id(n) != controller_leader_id
+            ]
+            self._transfer_leadership(namespace="redpanda",
+                                      topic="controller",
+                                      partition=0,
+                                      current_leader_id=controller_leader_id,
+                                      target_id=random.choice(other_ids))
+
+        self.logger.info(f"waiting for progress")
+
+        workload.wait_progress(timeout_sec=timings.wait_progress_timeout_s)
+
+    @cluster(num_nodes=4)
+    @matrix(case_id=LIST_OFFSETS_CASES.keys())
+    def test_list_offsets(self, case_id):
+        case = self.LIST_OFFSETS_CASES[case_id]
+        workload = workloads.ListOffsetsWorkload(self.test_context,
+                                                 self.redpanda.brokers(),
+                                                 self.topic)
+        fault = None
+        if case.make_fault:
+            fault = case.make_fault(self)
+        timings = TimingConfig()
+
+        self.prepare(workload, timings)
+        self.run(workload, fault, timings, case.check_progress_during_fault)
