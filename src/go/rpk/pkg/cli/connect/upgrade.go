b87e1a559abdcebe7a8ba77f92e684e5f0cfed43
r-vasquez <rvasque3@gmail.com>
2024-08-28 15:53:44 -0700

    rpk: add 'rpk connect' as a managed plugin
    This change means that from now on, connect will
    be managed by rpk, so, if a user tries to run
    `rpk connect <...>` for the first time, rpk will
    download, install and execute the plugin (connect).
    
    It also introduces 3 new commands to `rpk conenct`
    that live in rpk: install, uninstall, and upgrade.
    
    (cherry picked from commit b3e92117eb72e6bc694aa27ce2c30a57c3e6a2c0)

---
+++ src/go/rpk/pkg/cli/connect/upgrade.go
@@ -0,0 +1,56 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
+package connect
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/out"
+	"github.com/redpanda-data/redpanda/src/go/rpk/pkg/plugin"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+)
+
+func upgradeCommand(fs afero.Fs) *cobra.Command {
+	return &cobra.Command{
+		Use:   "upgrade",
+		Short: "Upgrade to the latest Redpanda Connect version",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, _ []string) {
+			pluginDir, err := plugin.DefaultBinPath()
+			out.MaybeDie(err, "unable to determine managed plugin path: %w", err)
+			connect, pluginExists := plugin.ListPlugins(fs, plugin.UserPaths()).Find("connect")
+			if !pluginExists {
+				out.Die("Redpanda Connect plugin not found. You may install it running 'rpk connect install'")
+			}
+			// A user may have installed version of 'rpk connect' either
+			// manually or using a package manager (air-gapped). An attempt to
+			// upgrade can result in having multiple copies of connect. Instead,
+			// we kindly ask to re-install.
+			if !connect.Managed {
+				out.Die("Found an unmanaged Redpanda Connect plugin; unfortunately we cannot upgrade it with this install. Run 'rpk connect uninstall' first, and download a new one with 'rpk connect install' or use your package manager.")
+			}
+			art, version, err := getConnectArtifact(cmd.Context(), "latest")
+			out.MaybeDieErr(err)
+
+			currentSha, err := plugin.Sha256Path(fs, connect.Path)
+			out.MaybeDie(err, "unable to determine the sha256sum of current Redpanda Connect %q: %v", connect.Path, err)
+
+			if strings.HasPrefix(currentSha, art.Sha256) {
+				out.Exit("Redpanda Connect already up-to-date")
+			}
+			_, err = downloadAndInstallConnect(cmd.Context(), fs, pluginDir, art.Path, art.Sha256)
+			out.MaybeDieErr(err)
+
+			fmt.Printf("Redpanda Connect successfully upgraded to the latest version (%v).\n", version)
+		},
+	}
+}
