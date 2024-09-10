3a423107dc0afca33e238b8c2d038057a8a73b14
Willem Kaufmann <willem.kaufmann@redpanda.com>
2024-09-10 16:03:16 -0400

    `rpk`: disambiguate `null` values of consumed tombstone records
    To differentiate tombstone records with null values (`null`) from records
    with empty string values (`""`), use a `*string` on the `rpk` side.
    
    The output from `rpk topic consume` will show an empty string for records,
    while tombstone records will have their values omitted.

--- src/go/rpk/pkg/cli/topic/consume.go
+++ src/go/rpk/pkg/cli/topic/consume.go
@@ -271,10 +271,16 @@ func (c *consumer) writeRecordJSON(r *kgo.Record, toStdErr bool) {
 		Value string `json:"value"`
 	}
 
+	var valuePtr *string
+	if r.Value != nil {
+		valueStr := string(r.Value)
+		valuePtr = &valueStr
+	}
+
 	m := struct {
 		Topic     string   `json:"topic"`
 		Key       string   `json:"key,omitempty"`
-		Value     string   `json:"value,omitempty"`
+		Value     *string  `json:"value,omitempty"`
 		ValueSize *int     `json:"value_size,omitempty"` // non-nil if --meta-only
 		Headers   []Header `json:"headers,omitempty"`
 		Timestamp int64    `json:"timestamp"` // millis
@@ -284,7 +290,7 @@ func (c *consumer) writeRecordJSON(r *kgo.Record, toStdErr bool) {
 	}{
 		Topic:     r.Topic,
 		Key:       string(r.Key),
-		Value:     string(r.Value),
+		Value:     valuePtr,
 		Headers:   make([]Header, 0, len(r.Headers)),
 		Timestamp: r.Timestamp.UnixNano() / 1e6,
 
@@ -293,8 +299,8 @@ func (c *consumer) writeRecordJSON(r *kgo.Record, toStdErr bool) {
 	}
 
 	if c.metaOnly {
-		size := len(m.Value)
-		m.Value = ""
+		size := len(r.Value)
+		m.Value = nil
 		m.ValueSize = &size
 	}
 
