dc702a767bda304d4a1264a9c33b7e0ab9c36ca9
Oren Leiman <oren.leiman@redpanda.com>
2024-09-12 16:24:20 -0700

    c/feature_manager: Generate an enterprise features report
    Also refactor feature_manager::license_required_feature_enabled to
    use the report rather than calculating feature status on its own.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>
    (cherry picked from commit 067df42f1df3d30b50230d1c3df34efaafc276ae)
    
    Conflicts:
      - feature_manager.cc - nonexistent features (fips, shard balancing)
      - feature_manager.cc - didn't need node config as a result

--- src/v/cluster/feature_manager.cc
+++ src/v/cluster/feature_manager.cc
@@ -193,6 +193,11 @@ ss::future<> feature_manager::stop() {
 }
 
 bool feature_manager::license_required_feature_enabled() const {
+    return report_enterprise_features().any();
+}
+
+features::enterprise_feature_report
+feature_manager::report_enterprise_features() const {
     const auto& cfg = config::shard_local_cfg();
     auto has_gssapi = [&cfg]() {
         return absl::c_any_of(
@@ -202,7 +207,8 @@ bool feature_manager::license_required_feature_enabled() const {
         return config::oidc_is_enabled_kafka()
                || config::oidc_is_enabled_http();
     };
-    auto has_schma_id_validation = [&cfg]() {
+
+    auto has_schema_id_validation = [&cfg]() {
         return cfg.enable_schema_id_validation()
                != pandaproxy::schema_registry::schema_id_validation_mode::none;
     };
@@ -211,11 +217,23 @@ bool feature_manager::license_required_feature_enabled() const {
       = n_roles >= 2
         || (n_roles == 1 && !_role_store.local().contains(security::default_role));
 
-    return cfg.audit_enabled || cfg.cloud_storage_enabled
-           || cfg.partition_autobalancing_mode
-                == model::partition_autobalancing_mode::continuous
-           || has_gssapi() || has_oidc() || has_schma_id_validation()
-           || has_non_default_roles;
+    features::enterprise_feature_report report;
+    report.set(
+      features::license_required_feature::audit_logging, cfg.audit_enabled());
+    report.set(
+      features::license_required_feature::cloud_storage,
+      cfg.cloud_storage_enabled());
+    report.set(
+      features::license_required_feature::partition_auto_balancing_continuous,
+      cfg.partition_autobalancing_mode()
+        == model::partition_autobalancing_mode::continuous);
+    report.set(features::license_required_feature::gssapi, has_gssapi());
+    report.set(features::license_required_feature::oidc, has_oidc());
+    report.set(
+      features::license_required_feature::schema_id_validation,
+      has_schema_id_validation());
+    report.set(features::license_required_feature::rbac, has_non_default_roles);
+    return report;
 }
 
 ss::future<> feature_manager::maybe_log_license_check_info() {
