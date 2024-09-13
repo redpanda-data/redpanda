75c00b7dc6b669cd27679f0dbaa8779337877cd3
Michael Boquard <michael@redpanda.com>
2024-09-13 10:30:51 -0400

    debug-bundle: Added tests to start and get status
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/debug_bundle/tests/debug_bundle_service_test.cc
+++ src/v/debug_bundle/tests/debug_bundle_service_test.cc
@@ -12,10 +12,12 @@
 #include "config/configuration.h"
 #include "debug_bundle/debug_bundle_service.h"
 #include "debug_bundle/error.h"
+#include "debug_bundle/types.h"
 #include "random/generators.h"
 #include "test_utils/test.h"
 
 #include <seastar/core/seastar.hh>
+#include <seastar/core/sleep.hh>
 #include <seastar/util/defer.hh>
 
 #include <gtest/gtest.h>
@@ -45,6 +47,17 @@ struct debug_bundle_service_fixture : public seastar_test {
     ss::sharded<debug_bundle::service> _service;
 };
 
+struct debug_bundle_service_started_fixture
+  : public debug_bundle_service_fixture {
+    ss::future<> SetUpAsync() override {
+        co_await debug_bundle_service_fixture::SetUpAsync();
+        ASSERT_NO_THROW_CORO(co_await _service.invoke_on_all(
+          [](debug_bundle::service& s) -> ss::future<> {
+              co_await s.start();
+          }));
+    }
+};
+
 TEST_F_CORO(debug_bundle_service_fixture, basic_start_stop) {
     auto debug_bundle_dir = _data_dir
                             / debug_bundle::service::debug_bundle_dir_name;
@@ -63,4 +76,213 @@ TEST_F_CORO(debug_bundle_service_fixture, bad_rpk_path) {
     ASSERT_NO_THROW_CORO(co_await _service.invoke_on_all(
       [](debug_bundle::service& s) -> ss::future<> { co_await s.start(); }));
     EXPECT_TRUE(co_await ss::file_exists(debug_bundle_dir.native()));
+}
+
+ss::future<debug_bundle::result<debug_bundle::debug_bundle_status_data>>
+wait_for_process_to_finish(
+  ss::sharded<debug_bundle::service>& service,
+  const std::chrono::seconds timeout) {
+    const auto start_time = debug_bundle::clock::now();
+    while (debug_bundle::clock::now() - start_time <= timeout) {
+        auto status = co_await service.local().rpk_debug_bundle_status();
+        if (status.has_failure()) {
+            throw std::runtime_error("status contains error");
+        }
+        if (
+          status.assume_value().status
+          != debug_bundle::debug_bundle_status::running) {
+            co_return status;
+        }
+    }
+    throw std::runtime_error("Timed out waiting for process to exit");
+}
+
+TEST_F_CORO(debug_bundle_service_started_fixture, run_process) {
+    using namespace std::chrono_literals;
+
+    debug_bundle::job_id_t job_id(uuid_t::create());
+
+    auto res = co_await _service.local().initiate_rpk_debug_bundle_collection(
+      job_id, {});
+    ASSERT_FALSE_CORO(res.has_failure()) << res.as_failure().error().message();
+
+    auto status = co_await _service.local().rpk_debug_bundle_status();
+    ASSERT_FALSE_CORO(status.has_failure())
+      << res.as_failure().error().message();
+
+    EXPECT_EQ(
+      status.assume_value().status, debug_bundle::debug_bundle_status::running);
+    EXPECT_EQ(status.assume_value().job_id, job_id);
+    EXPECT_EQ(status.assume_value().file_name, fmt::format("{}.zip", job_id));
+
+    ASSERT_NO_THROW_CORO(
+      status = co_await wait_for_process_to_finish(_service, 10s));
+
+    ASSERT_FALSE_CORO(status.has_failure())
+      << res.as_failure().error().message();
+
+    EXPECT_EQ(
+      status.assume_value().status, debug_bundle::debug_bundle_status::success);
+    const auto& cout = status.assume_value().cout;
+    EXPECT_FALSE(cout.empty());
+    auto expected_args = fmt::format(
+      "debug bundle --output {}/{}.zip --verbose\n",
+      (_data_dir / debug_bundle::service::debug_bundle_dir_name).native(),
+      job_id);
+    EXPECT_EQ(cout[0], expected_args) << cout[0] << " != " << expected_args;
+}
+
+TEST_F_CORO(debug_bundle_service_started_fixture, test_all_parameters) {
+    using namespace std::chrono_literals;
+
+    debug_bundle::job_id_t job_id(uuid_t::create());
+
+    ss::sstring username = "test";
+    ss::sstring password = "test12345";
+    ss::sstring mechanism = "SCRAM-SHA-256";
+    uint64_t controller_logs_size_limit_bytes = 123456;
+    std::chrono::seconds cpu_profiler_wait_seconds{60};
+    debug_bundle::special_date logs_since
+      = debug_bundle::special_date::yesterday;
+    uint64_t logs_size_limit = 987654;
+    const std::string_view logs_until = "2024-09-05T14:34:02";
+    debug_bundle::clock::time_point logs_until_tp;
+    {
+        std::istringstream ss(logs_until.data());
+        std::tm tmp{};
+        ASSERT_NO_THROW_CORO(ss >> std::get_time(&tmp, "%FT%T"));
+        ASSERT_FALSE_CORO(ss.fail());
+        tmp.tm_isdst = -1;
+        std::time_t tt = std::mktime(&tmp);
+        logs_until_tp = debug_bundle::clock::from_time_t(tt);
+    }
+    std::chrono::seconds metrics_interval_seconds{54};
+    model::topic_namespace tn1(model::ns{"kafka"}, model::topic{"t1"});
+    model::topic_namespace tn2(model::ns{"internal"}, model::topic{"t2"});
+    std::vector<debug_bundle::partition_selection> partition{
+      debug_bundle::partition_selection{
+        .tn = tn1,
+        .partitions
+        = {model::partition_id{1}, model::partition_id{2}, model::partition_id{3}}},
+      debug_bundle::partition_selection{
+        .tn = tn2,
+        .partitions = {
+          model::partition_id{4},
+          model::partition_id{5},
+          model::partition_id{6}}}};
+
+    debug_bundle::debug_bundle_parameters params{
+      .authn_options = debug_bundle::
+        scram_creds{security::credential_user{username}, security::credential_password{password}, mechanism},
+      .controller_logs_size_limit_bytes = controller_logs_size_limit_bytes,
+      .cpu_profiler_wait_seconds = cpu_profiler_wait_seconds,
+      .logs_since = logs_since,
+      .logs_size_limit_bytes = logs_size_limit,
+      .logs_until = logs_until_tp,
+      .metrics_interval_seconds = metrics_interval_seconds,
+      .partition = partition};
+
+    ss::sstring expected_params(fmt::format(
+      "debug bundle --output {}/{}.zip --verbose -Xuser={} -Xpass={} "
+      "-Xsasl.mechanism={} --controller-logs-size-limit {}B "
+      "--cpu-profiler-wait {}s --logs-since {} --logs-size-limit {}B "
+      "--logs-until {} --metrics-interval {}s --partition {}/{}/1,2,3 "
+      "{}/{}/4,5,6\n",
+      (_data_dir / debug_bundle::service::debug_bundle_dir_name).native(),
+      job_id,
+      username,
+      password,
+      mechanism,
+      controller_logs_size_limit_bytes,
+      cpu_profiler_wait_seconds.count(),
+      logs_since,
+      logs_size_limit,
+      logs_until,
+      metrics_interval_seconds.count(),
+      tn1.ns,
+      tn1.tp,
+      tn2.ns,
+      tn2.tp));
+
+    auto res = co_await _service.local().initiate_rpk_debug_bundle_collection(
+      job_id, std::move(params));
+    ASSERT_FALSE_CORO(res.has_failure()) << res.assume_error().message();
+
+    const auto max_retries = 5;
+    const auto timeout_time = 1s;
+
+    auto num_retries = 0;
+    while (num_retries < max_retries) {
+        auto status = co_await _service.local().rpk_debug_bundle_status();
+        ASSERT_FALSE_CORO(status.has_failure()) << res.assume_error().message();
+
+        EXPECT_EQ(
+          status.assume_value().status,
+          debug_bundle::debug_bundle_status::running);
+
+        if (status.assume_value().cout.empty()) {
+            // Sleep a second to allow the cout to be captured
+            co_await ss::sleep(timeout_time);
+        } else {
+            EXPECT_EQ(status.assume_value().cout[0], expected_params)
+              << status.assume_value().cout[0] << " != " << expected_params;
+            break;
+        }
+
+        ASSERT_NE_CORO(++num_retries, max_retries)
+          << "Maximum number of retries reached!";
+    }
+}
+
+TEST_F_CORO(debug_bundle_service_started_fixture, try_running_multiple) {
+    auto res = co_await _service.invoke_on(
+      debug_bundle::service::service_shard, [](debug_bundle::service& s) {
+          return s.initiate_rpk_debug_bundle_collection(
+            debug_bundle::job_id_t(uuid_t::create()), {});
+      });
+    ASSERT_FALSE_CORO(res.has_failure()) << res.as_failure().error().message();
+
+    auto res2 = co_await _service.invoke_on(
+      (debug_bundle::service::service_shard + 1) % ss::smp::count,
+      [](debug_bundle::service& s) {
+          return s.initiate_rpk_debug_bundle_collection(
+            debug_bundle::job_id_t(uuid_t::create()), {});
+      });
+
+    ASSERT_TRUE_CORO(res2.has_failure());
+    EXPECT_EQ(
+      res2.assume_error().code(),
+      debug_bundle::error_code::debug_bundle_process_running);
+}
+
+TEST_F_CORO(debug_bundle_service_started_fixture, run_no_rpk_binary) {
+    config::shard_local_cfg().rpk_path.set_value("/no/such/bin");
+
+    auto res = co_await _service.local().initiate_rpk_debug_bundle_collection(
+      debug_bundle::job_id_t(uuid_t::create()), {});
+
+    ASSERT_TRUE_CORO(res.has_failure());
+    EXPECT_EQ(
+      res.assume_error().code(),
+      debug_bundle::error_code::rpk_binary_not_present);
+}
+
+TEST_F_CORO(debug_bundle_service_started_fixture, rpk_binary_no_exec) {
+    // This will use /proc/cmdline as the rpk binary, which is unable to be
+    // executed since it's executable flag is not set
+    config::shard_local_cfg().rpk_path.set_value("/proc/cmdline");
+
+    auto res = co_await _service.local().initiate_rpk_debug_bundle_collection(
+      debug_bundle::job_id_t(uuid_t::create()), {});
+    ASSERT_TRUE_CORO(res.has_failure());
+    EXPECT_EQ(
+      res.assume_error().code(), debug_bundle::error_code::internal_error);
+}
+
+TEST_F_CORO(debug_bundle_service_started_fixture, status_no_run) {
+    auto res = co_await _service.local().rpk_debug_bundle_status();
+    ASSERT_TRUE_CORO(res.has_failure());
+    EXPECT_EQ(
+      res.assume_error().code(),
+      debug_bundle::error_code::debug_bundle_process_never_started);
 }
--- src/v/debug_bundle/tests/rpk-shim.sh
+++ src/v/debug_bundle/tests/rpk-shim.sh
@@ -1,1 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+sleep_time="${RPK_SHIM_SLEEP_TIME:-5}"
+
+echo "$@" | tee results.txt
+
+sleep "${sleep_time}"
