1cd4d1e9a07a8c4b558b95234a69515a793a713e
Michał Maślanka <michal@redpanda.com>
2024-09-30 10:31:48 +0000

    datalake/tests: use code generated messages descriptors
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- src/v/datalake/tests/datalake_protobuf_tests.cc
+++ src/v/datalake/tests/datalake_protobuf_tests.cc
@@ -8,11 +8,10 @@
 // by the Apache License, Version 2.0
 
 #include "datalake/schema_protobuf.h"
+#include "datalake/tests/proto_definitions.h"
 #include "gtest/gtest.h"
 #include "iceberg/datatypes.h"
-#include "test_utils/runfiles.h"
 #include "test_utils/test.h"
-#include "utils/file_io.h"
 
 #include <seastar/core/sstring.hh>
 
@@ -25,81 +24,10 @@
 #include <google/protobuf/io/tokenizer.h>
 #include <gtest/gtest.h>
 
-#include <filesystem>
 #include <memory>
 
 using namespace iceberg;
 using namespace testing;
-namespace {
-
-const char* basic_proto_str = R"(
-syntax = "proto3";
-
-message Department {
-    int32 id = 1;
-    string name = 2;
-}
-
-message Person {
-    string name = 1;
-    int32 id = 2;
-    string email = 3;
-    Department dept = 4;
-    string test_coverage = 5;
-}
-)";
-const char* complex_proto_str = R"(
-syntax = "proto3";
-
-enum State {
-  ACTIVE = 0;
-  DISABLED = 1;
-}
-
-message BrokerShard {
-  int64 id = 1;
-  int32 shard = 2;
-}
-
-message Partition {
-    int32 id = 1;
-    repeated BrokerShard replicas = 2;
-    State state = 3;
-}
-
-message Topic {
-    string name = 1;
-    repeated Partition partitions = 2;
-}
-
-message Metadata {
-    map<string,Topic> topics = 1;
-    repeated int64 nodes = 2;
-}
-
-)";
-static constexpr auto file_descriptor_name = "test_schema_file";
-AssertionResult parse_proto_definition_string(
-  const ss::sstring& proto_str, google::protobuf::DescriptorPool& pool) {
-    google::protobuf::compiler::Parser parser;
-
-    google::protobuf::io::ArrayInputStream input_stream(
-      proto_str.c_str(), proto_str.size());
-
-    google::protobuf::io::Tokenizer tokenizer(&input_stream, nullptr);
-    google::protobuf::FileDescriptorProto fdp;
-    fdp.set_name(file_descriptor_name);
-
-    auto parse_result = parser.Parse(&tokenizer, &fdp);
-    if (!parse_result) {
-        return AssertionFailure(
-          Message("Unable to parse protocol buffer defintion string"));
-    }
-    pool.BuildFile(fdp);
-    return AssertionSuccess();
-}
-
-}; // namespace
 
 MATCHER_P3(IsField, id, name, type, "") {
     *result_listener << fmt::format(
@@ -116,11 +44,9 @@ MATCHER_P3(IsField, id, name, type, "") {
 }
 
 TEST(SchemaProtobuf, TestSimpleSchemaConversion) {
-    google::protobuf::DescriptorPool d_pool;
-    ASSERT_TRUE(parse_proto_definition_string(basic_proto_str, d_pool));
-    auto person_descriptor = d_pool.FindMessageTypeByName("Person");
+    auto descriptor = Person::GetDescriptor();
 
-    auto result = datalake::type_to_iceberg(*person_descriptor);
+    auto result = datalake::type_to_iceberg(*descriptor);
     ASSERT_FALSE(result.has_error());
     auto& top_level = result.value();
     // string name = 1;
@@ -180,11 +106,9 @@ TEST(SchemaProtobuf, TestSimpleSchemaConversion) {
 // static_assert(
 // SchemaConverter<protobuf_schema_converter, google::protobuf::Descriptor>);
 TEST(SchemaProtobuf, TestComplexSchema) {
-    google::protobuf::DescriptorPool d_pool;
-    ASSERT_TRUE(parse_proto_definition_string(complex_proto_str, d_pool));
-    auto md = d_pool.FindMessageTypeByName("Metadata");
+    auto d = Metadata::GetDescriptor();
 
-    auto result = datalake::type_to_iceberg(*md);
+    auto result = datalake::type_to_iceberg(*d);
     ASSERT_FALSE(result.has_exception());
 
     auto& top_level = result.value();
@@ -227,64 +151,19 @@ TEST(SchemaProtobuf, TestComplexSchema) {
 }
 
 TEST_CORO(SchemaProtobuf, TestProtoTestMessages) {
-    auto desc_path = test_utils::get_runfile_path(
-      "src/v/datalake/tests/testdata/"
-      "iceberg_ready_test_messages_edition2023.proto");
-
-    if (!desc_path.has_value()) {
-        desc_path = "iceberg_ready_test_messages_edition2023.proto";
-    }
-
-    auto proto_string = co_await read_fully_to_string(desc_path.value());
-
-    google::protobuf::DescriptorPool d_pool;
-    ASSERT_TRUE_CORO(parse_proto_definition_string(proto_string, d_pool));
-
-    auto message_desc = d_pool.FindMessageTypeByName(
-      "protobuf_test_messages.editions.TestAllTypesEdition2023");
-    auto result = datalake::type_to_iceberg(*message_desc);
+    auto d = protobuf_test_messages::editions::TestAllTypesEdition2023::
+      GetDescriptor();
+    auto result = datalake::type_to_iceberg(*d);
     ASSERT_FALSE_CORO(result.has_error());
 }
 
-const char* not_supported_messages = R"(
-  syntax = "proto3";
-  message StructWithUnsignedInt {
-      int64 valid = 1;
-      uint64 invalid = 2;
-  }
-
-  message StructWithUnsignedFixed {
-      int64 valid = 1;
-      fixed64 invalid = 2;
-  }
-  
-  message RecursiveMessage {
-      int32 field = 1;
-      RecursiveMessage recursive = 2;
-  }
-
-  message RecursiveMessageNested {
-    
-    message Nested {
-      repeated RecursiveMessageNested foo = 1;
-    }
-
-
-    int32 field = 1;
-    Nested corecursive = 2;
-  }
-)";
-
 TEST(SchemaProtobuf, TestInvalidSchema) {
-    google::protobuf::DescriptorPool d_pool;
-    ASSERT_TRUE(parse_proto_definition_string(not_supported_messages, d_pool));
-    for (auto& m :
-         {"StructWithUnsignedInt",
-          "StructWithUnsignedFixed",
-          "RecursiveMessage",
-          "RecursiveMessageNested"}) {
-        auto md = d_pool.FindMessageTypeByName(m);
-        auto result = datalake::type_to_iceberg(*md);
+    for (auto desc :
+         {StructWithUnsignedInt::GetDescriptor(),
+          StructWithUnsignedFixed::GetDescriptor(),
+          RecursiveMessage::GetDescriptor(),
+          RecursiveMessageNested::GetDescriptor()}) {
+        auto result = datalake::type_to_iceberg(*desc);
         ASSERT_TRUE(result.has_error());
     }
 }
