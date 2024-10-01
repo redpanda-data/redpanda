5db7d87ded2c4fa935909fe2335670dcf5ec2824
Tyler Rockwood <rockwood@redpanda.com>
2024-10-01 19:28:41 +0000

    wasm/tests: split up tests
    For Bazel, the existing debug tests are quite slow, so split up the
    sharding for these tests that take forever to compile because of
    wasmtime's debug mode is amazingly slow.

--- src/v/wasm/tests/wasm_transform_test.cc
+++ src/v/wasm/tests/wasm_transform_test.cc
@@ -9,23 +9,12 @@
  * by the Apache License, Version 2.0
  */
 
-#include "bytes/bytes.h"
-#include "bytes/streambuf.h"
-#include "pandaproxy/schema_registry/types.h"
 #include "wasm/errc.h"
 #include "wasm/tests/wasm_fixture.h"
 
 #include <seastar/core/internal/cpu_profiler.hh>
 #include <seastar/core/reactor.hh>
 
-#include <absl/strings/str_cat.h>
-#include <avro/Compiler.hh>
-#include <avro/Encoder.hh>
-#include <avro/Generic.hh>
-#include <avro/GenericDatum.hh>
-#include <avro/Specific.hh>
-#include <avro/Stream.hh>
-#include <avro/ValidSchema.hh>
 #include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 
@@ -36,94 +25,6 @@ TEST_F(WasmTestFixture, IdentityFunction) {
     auto batch = make_tiny_batch();
     auto transformed = transform(batch);
     ASSERT_EQ(transformed.copy_records(), batch.copy_records());
-}
-
-TEST_F(WasmTestFixture, CanRestartEngine) {
-    load_wasm("identity");
-    engine()->stop().get();
-    // It still works after being restarted
-    engine()->start().get();
-    auto batch = make_tiny_batch();
-    auto transformed = transform(batch);
-    ASSERT_EQ(transformed.copy_records(), batch.copy_records());
-}
-
-TEST_F(WasmTestFixture, HandlesSetupPanic) {
-    EXPECT_THROW(load_wasm("setup-panic"), wasm::wasm_exception);
-}
-
-TEST_F(WasmTestFixture, HandlesTransformPanic) {
-    load_wasm("transform-panic");
-    EXPECT_THROW(transform(make_tiny_batch()), wasm::wasm_exception);
-}
-
-TEST_F(WasmTestFixture, HandlesTransformErrors) {
-    load_wasm("transform-error");
-    EXPECT_THROW(transform(make_tiny_batch()), wasm::wasm_exception);
-    engine()->stop().get();
-    engine()->start().get();
-    EXPECT_THROW(transform(make_tiny_batch()), wasm::wasm_exception);
-}
-
-namespace {
-std::string generate_example_avro_record(
-  const pandaproxy::schema_registry::canonical_schema_definition& def) {
-    // Generate a simple avro record that looks like this (as json):
-    // {"a":5,"b":"foo"}
-    iobuf_istream bis{def.shared_raw()};
-    auto is = avro::istreamInputStream(bis.istream());
-    auto schema = avro::compileJsonSchemaFromStream(*is);
-    avro::GenericRecord r(schema.root());
-    r.setFieldAt(r.fieldIndex("a"), int64_t(4));
-    r.setFieldAt(r.fieldIndex("b"), std::string("foo"));
-
-    std::unique_ptr<avro::OutputStream> out = avro::memoryOutputStream();
-    avro::EncoderPtr e = avro::binaryEncoder();
-    e->init(*out);
-    avro::encode(*e, avro::GenericDatum(schema.root(), r));
-    e->flush();
-    auto snap = avro::snapshot(*out);
-    return {snap->begin(), snap->end()};
-}
-} // namespace
-
-TEST_F(WasmTestFixture, SchemaRegistry) {
-    // Test an example schema registry encoded avro value -> JSON transform
-    load_wasm("schema-registry");
-    const auto& schemas = registered_schemas();
-    ASSERT_EQ(schemas.size(), 1);
-    ASSERT_EQ(schemas[0].id, 1);
-    iobuf record_value;
-    // Prepend the "magic" nul byte then the schema id in big endian
-    record_value.append("\0\0\0\0\1", 5);
-    auto avro = generate_example_avro_record(schemas[0].schema.def());
-    record_value.append(avro.data(), avro.size());
-    auto batch = make_tiny_batch(std::move(record_value));
-    auto transformed = transform(batch);
-    auto records = transformed.copy_records();
-    ASSERT_EQ(records.size(), 1);
-    EXPECT_EQ(
-      iobuf_to_bytes(records[0].value()),
-      bytes::from_string(R"JSON({"a":4,"b":"foo"})JSON"));
-}
-
-TEST_F(WasmTestFixture, MemoryIsLimited) {
-    load_wasm("dynamic");
-    EXPECT_NO_THROW(execute_command("allocate", 5_KiB));
-    EXPECT_THROW(execute_command("allocate", MAX_MEMORY), wasm::wasm_exception);
-}
-
-TEST_F(WasmTestFixture, CpuIsLimited) {
-    load_wasm("dynamic");
-    // In reality, we don't want this to be over a few milliseconds, but in an
-    // effort to prevent test flakiness on hosts with lots of concurrent tests
-    // on shared vCPUs, we make this much higher.
-    ss::engine().update_blocked_reactor_notify_ms(50ms);
-    bool stalled = false;
-    ss::reactor::test::set_stall_detector_report_function(
-      [&stalled] { stalled = true; });
-    EXPECT_THROW(execute_command("loop", 0), wasm::wasm_exception);
-    EXPECT_FALSE(stalled);
 }
 
 using ::testing::ElementsAre;
