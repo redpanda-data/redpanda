5642b5bb1b4baf054662c7f6cb31b16b1c225ec0
Evgeny Lazin <4lazin@gmail.com>
2024-10-02 05:15:20 -0400

    ct: Add write request timeout test
    The test validates that the enqueued write requests are expiring
    properly.
    
    Signed-off-by: Evgeny Lazin <4lazin@gmail.com>

--- src/v/cloud_topics/batcher/tests/batcher_test.cc
+++ src/v/cloud_topics/batcher/tests/batcher_test.cc
@@ -224,7 +224,6 @@ TEST_CORO(batcher_test, many_write_requests) {
     auto id = mock.keys.back();
 
     ASSERT_TRUE_CORO(res.has_value());
-    // TODO: validate placeholders
 
     auto results = co_await ss::when_all_succeed(std::move(futures));
     size_t ix = 0;
@@ -246,4 +245,80 @@ TEST_CORO(batcher_test, many_write_requests) {
     }
 }
 
+TEST_CORO(batcher_test, expired_write_request) {
+    // The test starts two write request but one of which is expected to
+    // timeout. The expectation is that uploaded L0 object will not contain any
+    // data from the expired write request.
+    remote_mock mock;
+    cloud_storage_clients::bucket_name bucket("foo");
+    cloud_topics::batcher<ss::manual_clock> batcher(bucket, mock);
+    cloud_topics::batcher_accessor accessor{
+      .batcher = &batcher,
+    };
+
+    int expected_num_batches = 33;
+    int expected_num_records = 33;
+    auto [_1, included_records, included_reader] = get_random_reader(
+      expected_num_batches, expected_num_records);
+    auto [_2, timedout_records, timedout_reader] = get_random_reader(1, 1);
+
+    chunked_vector<bytes> all_records;
+    std::copy(
+      std::make_move_iterator(included_records.begin()),
+      std::make_move_iterator(included_records.end()),
+      std::back_inserter(all_records));
+    std::copy(
+      std::make_move_iterator(timedout_records.begin()),
+      std::make_move_iterator(timedout_records.end()),
+      std::back_inserter(all_records));
+
+    // Expect single upload to be made
+    mock.expect_upload_object(all_records);
+
+    const auto timeout = 1s;
+    auto expect_fail_fut = batcher.write_and_debounce(
+      model::controller_ntp, std::move(timedout_reader), timeout);
+
+    // Let time pass to invalidate the first enqueued write request
+    co_await sleep_until(
+      10ms, [&] { return accessor.write_requests_pending(1); });
+    ss::manual_clock::advance(timeout);
+
+    auto expect_pass_fut = batcher.write_and_debounce(
+      model::controller_ntp, std::move(included_reader), timeout);
+
+    // Make sure that both write requests are pending
+    co_await sleep_until(
+      10ms, [&] { return accessor.write_requests_pending(2); });
+
+    // Single L0 object that contains data from all write requests
+    // should be "uploaded".
+    auto res = co_await accessor.run_once();
+
+    // Expect single L0 upload
+    ASSERT_EQ_CORO(mock.keys.size(), 1);
+    auto id = mock.keys.back();
+
+    ASSERT_TRUE_CORO(res.has_value());
+
+    auto [pass_result, fail_result] = co_await ss::when_all_succeed(
+      std::move(expect_pass_fut), std::move(expect_fail_fut));
+
+    ASSERT_TRUE_CORO(fail_result.has_error());
+
+    ASSERT_TRUE_CORO(pass_result.has_value());
+    auto placeholder_batches = co_await model::consume_reader_to_memory(
+      std::move(pass_result.value()), model::no_timeout);
+
+    ASSERT_EQ_CORO(placeholder_batches.size(), expected_num_batches);
+    for (const model::record_batch& batch : placeholder_batches) {
+        auto placeholder = parse_placeholder_batch(batch.copy());
+        // TODO: revisit this code when the object path format will change
+        auto sid = ssx::sformat("{}", placeholder.id);
+        ASSERT_EQ_CORO(sid, id());
+    }
+}
+
 // TODO: add more tests
+// - behaviour in case if pending write request sizes exceed L0 object size
+// limit
