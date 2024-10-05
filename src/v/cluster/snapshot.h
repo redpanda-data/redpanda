c94d5be344aaf83c9ff9f146f76cd251f770cb1c
Noah Watkins <noahwatkins@gmail.com>
2024-10-05 12:01:50 -0700

    cluster: fix unused variable,function warnings
    /home/nwatkins/.cache/bazel/_bazel_nwatkins/31f1aa7012681f681e8d49a1818a8564/sandbox/linux-sandbox/10047/execroot/_main/src/v/cluster/tests/leader_balancer_test_utils.h:20:54: error: unused function 'make_cluster_index' [clang-diagnostic-unused-function]
       20 | static cluster::leader_balancer_strategy::index_type make_cluster_index(
          |                                                      ^~~~~~~~~~~~~~~~~~
    /home/nwatkins/.cache/bazel/_bazel_nwatkins/31f1aa7012681f681e8d49a1818a8564/sandbox/linux-sandbox/10047/execroot/_main/src/v/cluster/tests/leader_balancer_test_utils.h:54:61: error: unused function 'copy_cluster_index' [clang-diagnostic-unused-function]
       54 | static inline cluster::leader_balancer_strategy::index_type copy_cluster_index(
          |                                                             ^~~~~~~~~~~~~~~~~~
    /home/nwatkins/.cache/bazel/_bazel_nwatkins/31f1aa7012681f681e8d49a1818a8564/sandbox/linux-sandbox/41/execroot/_main/src/v/cluster/tests/leader_balancer_constraints_utils.h:116:13: error: unused function 'group_to_topic_from_spec' [clang-diagnostic-unused-function]
      116 | static auto group_to_topic_from_spec(const group_to_ntp_spec& spec) {
          |             ^~~~~~~~~~~~~~~~~~~~~~~~
    /home/nwatkins/.cache/bazel/_bazel_nwatkins/31f1aa7012681f681e8d49a1818a8564/sandbox/linux-sandbox/41/execroot/_main/src/v/cluster/tests/leader_balancer_constraints_utils.h:147:13: error: unused function 'no_movement' [clang-diagnostic-unused-function]
      147 | static bool no_movement(
          |             ^~~~~~~~~~~
    /home/nwatkins/.cache/bazel/_bazel_nwatkins/31f1aa7012681f681e8d49a1818a8564/sandbox/linux-sandbox/41/execroot/_main/src/v/cluster/tests/leader_balancer_constraints_utils.h:174:20: error: unused function 'expect_movement' [clang-diagnostic-unused-function]
      174 | static ss::sstring expect_movement(
          |                    ^~~~~~~~~~~~~~~
    /home/nwatkins/.cache/bazel/_bazel_nwatkins/31f1aa7012681f681e8d49a1818a8564/sandbox/linux-sandbox/41/execroot/_main/src/v/cluster/tests/leader_balancer_constraints_utils.h:211:21: error: unused function 're' [clang-diagnostic-unused-function]
      211 | static reassignment re(unsigned group, int from, int to) {
    
    /home/nwatkins/.cache/bazel/_bazel_nwatkins/31f1aa7012681f681e8d49a1818a8564/sandbox/linux-sandbox/1475/execroot/_main/src/v/cluster/distributed_kv_stm_types.h:70:1: error: unused function 'make_repartitioning_batch' [clang-diagnostic-unused-function]
       70 | make_repartitioning_batch(size_t num_partitions) {
    
    /home/nwatkins/.cache/bazel/_bazel_nwatkins/31f1aa7012681f681e8d49a1818a8564/sandbox/linux-sandbox/1953/execroot/_main/src/v/cluster/archival/types.h:169:22: error: unused variable 'max_reuploads_per_run' [clang-diagnostic-unused-const-variable]
      169 | static constexpr int max_reuploads_per_run = 4;
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/cluster/snapshot.h
+++ src/v/cluster/snapshot.h
@@ -10,14 +10,18 @@
  */
 #pragma once
 
+#include "base/seastarx.h"
+
+#include <seastar/core/sstring.hh>
+
 namespace cluster {
 
 /// Names of snapshot files used by stm's
-static const ss::sstring archival_stm_snapshot = "archival_metadata.snapshot";
-static const ss::sstring rm_stm_snapshot = "tx.snapshot";
-static const ss::sstring tm_stm_snapshot = "tx.coordinator.snapshot";
-static const ss::sstring id_allocator_snapshot = "id.snapshot";
-static const ss::sstring partition_properties_stm_snapshot
+inline const ss::sstring archival_stm_snapshot = "archival_metadata.snapshot";
+inline const ss::sstring rm_stm_snapshot = "tx.snapshot";
+inline const ss::sstring tm_stm_snapshot = "tx.coordinator.snapshot";
+inline const ss::sstring id_allocator_snapshot = "id.snapshot";
+inline const ss::sstring partition_properties_stm_snapshot
   = "partition_properties.snapshot";
 
 } // namespace cluster
