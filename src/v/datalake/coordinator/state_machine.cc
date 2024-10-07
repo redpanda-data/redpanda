a143badce9cfd8f6b99dd9d3ddd3452030166715
Andrew Wong <awong@redpanda.com>
2024-10-07 00:26:56 -0700

    dl/coordinator: apply datalake updates to stm
    Adds topics_state to the STM, and updates the apply method to
    appropriately update it.

--- src/v/datalake/coordinator/state_machine.cc
+++ src/v/datalake/coordinator/state_machine.cc
@@ -10,15 +10,68 @@
 
 #include "datalake/coordinator/state_machine.h"
 
+#include "datalake/coordinator/state.h"
+#include "datalake/coordinator/state_update.h"
 #include "datalake/logger.h"
+#include "model/record_batch_types.h"
 
 namespace datalake::coordinator {
+
+namespace {
+void maybe_log_update_error(
+  update_key key,
+  model::offset o,
+  const checked<std::nullopt_t, stm_update_error>& r) {
+    if (r.has_value()) {
+        return;
+    }
+    // NOTE: inability to update the STM is not necessarily a bug! It indicates
+    // that this update's construction raced with another update that broke an
+    // invariant required to apply update. Expectation is that this update's
+    // caller constructs a new update and tries again if needed.
+    vlog(
+      datalake_log.debug,
+      "Coordinator STM {} update at offset {} didn't apply: {}",
+      key,
+      o,
+      r.error());
+}
+} // namespace
 
 coordinator_stm::coordinator_stm(ss::logger& logger, raft::consensus* raft)
   : raft::persisted_stm<>("datalake_coordinator_stm.snapshot", logger, raft) {}
 
-ss::future<> coordinator_stm::do_apply(const model::record_batch&) {
-    co_return;
+ss::future<> coordinator_stm::do_apply(const model::record_batch& b) {
+    if (b.header().type != model::record_batch_type::datalake_coordinator) {
+        return ss::now();
+    }
+    auto iter = model::record_batch_iterator::create(b);
+    while (iter.has_next()) {
+        auto r = iter.next();
+        auto key = serde::from_iobuf<update_key>(r.release_key());
+        auto offset = b.base_offset() + model::offset_delta{r.offset_delta()};
+        switch (key) {
+        case update_key::add_files: {
+            auto update = serde::from_iobuf<add_files_update>(
+              r.release_value());
+            auto res = update.apply(state_);
+            maybe_log_update_error(key, offset, res);
+            continue;
+        }
+        case update_key::mark_files_committed: {
+            auto update = serde::from_iobuf<mark_files_committed_update>(
+              r.release_value());
+            auto res = update.apply(state_);
+            maybe_log_update_error(key, offset, res);
+            continue;
+        }
+        }
+        vlog(
+          datalake_log.error,
+          "Unknown datalake coordinator STM record type: {}",
+          key);
+    }
+    return ss::now();
 }
 
 model::offset coordinator_stm::max_collectible_offset() { return {}; }
