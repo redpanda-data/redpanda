4df2c37371b46a1e600b6790864c9525a707fbe6
Andrew Wong <awong@redpanda.com>
2024-10-08 16:10:00 -0700

    dl/coordinator: scope down STM public interface
    In some upcoming changes, we'll expect the STM to be an implementation
    detail, and for callers to go through a separate coordinator
    abstraction. To that end, this moves most STM methods out of the public
    interface and exposes some internals for use in the upcoming
    coordinator.

--- src/v/datalake/coordinator/state_machine.cc
+++ src/v/datalake/coordinator/state_machine.cc
@@ -14,6 +14,10 @@
 #include "datalake/coordinator/state_update.h"
 #include "datalake/logger.h"
 #include "model/record_batch_types.h"
+#include "serde/async.h"
+#include "ssx/future-util.h"
+
+#include <seastar/coroutine/as_future.hh>
 
 namespace datalake::coordinator {
 
@@ -41,26 +45,68 @@ void maybe_log_update_error(
 coordinator_stm::coordinator_stm(ss::logger& logger, raft::consensus* raft)
   : raft::persisted_stm<>("datalake_coordinator_stm.snapshot", logger, raft) {}
 
+ss::future<checked<model::term_id, coordinator_stm::errc>>
+coordinator_stm::sync(model::timeout_clock::duration timeout) {
+    auto sync_res = co_await ss::coroutine::as_future(
+      raft::persisted_stm<>::sync(timeout));
+    if (sync_res.failed()) {
+        auto eptr = sync_res.get_exception();
+        auto msg = fmt::format("Exception caught while syncing: {}", eptr);
+        if (ssx::is_shutdown_exception(eptr)) {
+            vlog(datalake_log.debug, "Ignoring shutdown error: {}", msg);
+            co_return errc::shutting_down;
+        }
+        vlog(datalake_log.warn, "{}", msg);
+        co_return errc::raft_error;
+    }
+
+    if (!sync_res.get()) {
+        co_return errc::not_leader;
+    }
+    // At this point we're guaranteed that this node is the leader and that the
+    // STM has been caught up in the current term before (this doesn't mean the
+    // STM is currently caught up right now though!)
+    co_return _insync_term;
+}
+
+ss::future<checked<std::nullopt_t, coordinator_stm::errc>>
+coordinator_stm::replicate_and_wait(
+  model::term_id term, model::record_batch batch, ss::abort_source& as) {
+    auto opts = raft::replicate_options{raft::consistency_level::quorum_ack};
+    opts.set_force_flush();
+    auto res = co_await _raft->replicate(
+      term, model::make_memory_record_batch_reader(std::move(batch)), opts);
+    if (res.has_error()) {
+        co_return errc::raft_error;
+    }
+    auto replicated_offset = res.value().last_offset;
+    co_await wait_no_throw(
+      replicated_offset, ss::lowres_clock::now() + 30s, as);
+    co_return std::nullopt;
+}
+
 ss::future<> coordinator_stm::do_apply(const model::record_batch& b) {
     if (b.header().type != model::record_batch_type::datalake_coordinator) {
-        return ss::now();
+        co_return;
     }
     auto iter = model::record_batch_iterator::create(b);
     while (iter.has_next()) {
         auto r = iter.next();
-        auto key = serde::from_iobuf<update_key>(r.release_key());
+        iobuf_parser key_p{r.release_key()};
+        auto key = co_await serde::read_async<update_key>(key_p);
+
+        iobuf_parser val_p{r.release_value()};
         auto offset = b.base_offset() + model::offset_delta{r.offset_delta()};
         switch (key) {
         case update_key::add_files: {
-            auto update = serde::from_iobuf<add_files_update>(
-              r.release_value());
+            auto update = co_await serde::read_async<add_files_update>(val_p);
             auto res = update.apply(state_);
             maybe_log_update_error(key, offset, res);
             continue;
         }
         case update_key::mark_files_committed: {
-            auto update = serde::from_iobuf<mark_files_committed_update>(
-              r.release_value());
+            auto update
+              = co_await serde::read_async<mark_files_committed_update>(val_p);
             auto res = update.apply(state_);
             maybe_log_update_error(key, offset, res);
             continue;
@@ -69,9 +115,9 @@ ss::future<> coordinator_stm::do_apply(const model::record_batch& b) {
         vlog(
           datalake_log.error,
           "Unknown datalake coordinator STM record type: {}",
-          key);
+          key,
+          b.header());
     }
-    return ss::now();
 }
 
 model::offset coordinator_stm::max_collectible_offset() { return {}; }
