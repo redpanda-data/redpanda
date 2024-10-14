cbb011bcd2f5b2168470492541ffeadedbe4f1b7
Abhijat Malviya <abhijat@redpanda.com>
2024-10-14 12:55:25 +0530

    iceberg/rest-client: Add table rest entity
    A table entity exposes common operations on iceberg tables

---
+++ src/v/iceberg/rest_client/entities.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
+#pragma once
+
+#include "container/fragmented_vector.h"
+#include "http/rest_client/rest_entity.h"
+
+namespace iceberg::rest_client {
+
+struct table : public http::rest_client::rest_entity {
+    table(
+      std::string_view root_url,
+      const chunked_vector<ss::sstring>& namespace_parts);
+
+    ss::sstring resource_name() const final;
+
+private:
+    ss::sstring _namespace;
+};
+
+} // namespace iceberg::rest_client
