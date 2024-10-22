88c194ac938a2ecf2d70aa51c616b61fc5216f11
Michał Maślanka <michal@redpanda.com>
2024-10-22 16:51:04 +0200

    iceberg/rc: added strict validation of oauth_token.token_type
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- src/v/iceberg/rest_client/json.cc
+++ src/v/iceberg/rest_client/json.cc
@@ -23,7 +23,17 @@ oauth_token parse_oauth_token(const json::Document& doc) {
     } else {
         ret.expires_at = ss::lowres_clock::time_point::max();
     }
-    ret.token_type = parse_required_str(doc, "token_type");
+    // token type is defined as enum with the following possible values:
+    // "bearer", "mac", "N_A"
+    auto token_type = parse_required_str(doc, "token_type");
+    if (!(token_type == "bearer" || token_type == "mac"
+          || token_type == "N_A")) {
+        throw std::invalid_argument(fmt::format(
+          "Unexpected oauth_token.token_type value {}. It must be one of the "
+          "bearer, mac or N_A",
+          token_type));
+    }
+    ret.token_type = std::move(token_type);
     ret.scope = parse_optional_str(doc, "scope");
     ret.refresh_token = parse_optional_str(doc, "refresh_token");
     return ret;
