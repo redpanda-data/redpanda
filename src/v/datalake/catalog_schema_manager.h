8b4308003f38a1a98d3bdd0ef1b947a5c9161d05
Andrew Wong <awong@redpanda.com>
2024-10-25 17:30:34 -0700

    datalake: add catalog schema manager
    Adds a new component the catalog_schema_manager to get field IDs from
    the Iceberg catalog, potentially registering a new schema if there are
    missing fields in the catalog.
    
    This will be the place we can make decisions about schema evolution. As
    is, the implemented policy is that as long as fields are added to the
    end of the table, the schema can be added. This is so that we can
    support going from the default schema to the default schema + user
    defined columns.
    
    In the short term, for simplicity, I expect this to live along side the
    datalake workers, where each worker will call into the schema manager
    when constructing a parquet writer with a given schema to get the
    correct field IDs. Longer term, for scale, perhaps this ought to live in
    the coordinator and workers may interact with it via RPC.

---
+++ src/v/datalake/catalog_schema_manager.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+#pragma once
+
+#include "iceberg/catalog.h"
+#include "iceberg/datatypes.h"
+#include "iceberg/table_identifier.h"
+
+namespace datalake {
+
+// Manages interactions with the catalog when reconciling the current schema of
+// a given table. This is where Redpanda should make decisions about schema
+// evolution.
+class catalog_schema_manager {
+public:
+    enum class errc {
+        // The requested operation is not supported (e.g. unsupported schema
+        // evolution).
+        not_supported,
+        // The operation failed because of a subsystem failure.
+        failed,
+        // The system is shutting down.
+        shutting_down,
+    };
+    explicit catalog_schema_manager(iceberg::catalog& catalog)
+      : catalog_(catalog) {}
+
+    // Loads the table metadata for the given topic and fills the field IDs of
+    // the given type with those in the current schema.
+    //
+    // If the table's current schema doesn't include all of the fields (e.g. we
+    // are going from the schemaless schema to a schema containing user
+    // fields), the table's schema is updated to the desired type, and then the
+    // fill is attempted again.
+    ss::future<checked<std::nullopt_t, errc>>
+    get_registered_ids(const model::topic&, iceberg::struct_type& desired_type);
+
+private:
+    iceberg::table_identifier table_id_for_topic(const model::topic& t) const;
+
+    // Attempts to fill the field ids in the given type with those from the
+    // current schema of the given table metadata.
+    //
+    // Returns true if successful, false if the fill is incomplete because the
+    // table schema does not have all the necessary fields. The latter is a
+    // signal that the caller needs to add the schema to the table.
+    checked<bool, errc> get_ids_from_table_meta(
+      const iceberg::table_identifier&,
+      const iceberg::table_metadata&,
+      iceberg::struct_type&);
+
+    iceberg::catalog& catalog_;
+};
+
+} // namespace datalake
