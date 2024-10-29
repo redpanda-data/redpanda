559d43eb27428ad4ef1d1857748825640a8ad906
Nicolae Vartolomei <nv@redpanda.com>
2024-10-29 14:21:14 +0000

    ct: add snapshotting to dl_stm_state

--- src/v/cloud_topics/types.cc
+++ src/v/cloud_topics/types.cc
@@ -17,6 +17,8 @@ auto fmt::formatter<experimental::cloud_topics::dl_stm_key>::format(
     switch (key) {
     case experimental::cloud_topics::dl_stm_key::push_overlay:
         return fmt::format_to(ctx.out(), "push_overlay");
+    case experimental::cloud_topics::dl_stm_key::start_snapshot:
+        return fmt::format_to(ctx.out(), "start_snapshot");
     }
     return fmt::format_to(
       ctx.out(), "unknown dl_stm_key({})", static_cast<int>(key));
