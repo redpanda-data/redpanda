0c818644aa4004e383e5bd7522aec8d419a25449
Andrew Wong <awong@redpanda.com>
2024-10-30 17:32:31 -0700

    datalake: schema identifier requirements for hashing

--- src/v/datalake/schema_identifier.h
+++ src/v/datalake/schema_identifier.h
@@ -12,6 +12,8 @@
 #include "pandaproxy/schema_registry/types.h"
 #include "serde/rw/envelope.h"
 
+#include <boost/container_hash/hash.hpp>
+
 #include <optional>
 
 namespace datalake {
@@ -26,6 +28,7 @@ struct schema_identifier
 
     pandaproxy::schema_registry::schema_id schema_id;
     std::optional<std::vector<int32_t>> protobuf_offsets;
+    bool operator==(const schema_identifier&) const = default;
 };
 
 // The components required to build the Iceberg schema of a record.
@@ -39,6 +42,39 @@ struct record_schema_components
 
     std::optional<schema_identifier> key_identifier;
     std::optional<schema_identifier> val_identifier;
+    bool operator==(const record_schema_components&) const = default;
 };
 
 } // namespace datalake
+
+namespace std {
+template<>
+struct hash<datalake::schema_identifier> {
+    size_t operator()(const datalake::schema_identifier& id) const {
+        namespace ppsr = pandaproxy::schema_registry;
+        size_t h = 0;
+        boost::hash_combine(h, hash<ppsr::schema_id>()(id.schema_id));
+        if (id.protobuf_offsets.has_value()) {
+            for (auto i : id.protobuf_offsets.value()) {
+                boost::hash_combine(h, hash<int32_t>()(i));
+            }
+        }
+        return h;
+    }
+};
+template<>
+struct hash<datalake::record_schema_components> {
+    size_t operator()(const datalake::record_schema_components& c) const {
+        size_t h = 0;
+        if (c.key_identifier.has_value()) {
+            boost::hash_combine(
+              h, hash<datalake::schema_identifier>()(*c.key_identifier));
+        }
+        if (c.val_identifier.has_value()) {
+            boost::hash_combine(
+              h, hash<datalake::schema_identifier>()(*c.val_identifier));
+        }
+        return h;
+    }
+};
+} // namespace std
