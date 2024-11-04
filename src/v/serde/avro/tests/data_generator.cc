a0fe4a2b2fb529664a08790089fe88e1358ae94d
Michał Maślanka <michal@redpanda.com>
2024-11-04 19:06:41 +0100

    serde/avro/tests: added logical types support in avro data generator
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- src/v/serde/avro/tests/data_generator.cc
+++ src/v/serde/avro/tests/data_generator.cc
@@ -12,10 +12,21 @@
 
 #include "base/seastarx.h"
 #include "random/generators.h"
+#include "utils/uuid.h"
 
 #include <seastar/util/defer.hh>
 
 namespace testing {
+static constexpr auto seconds_in_day = 24 * 60 * 60;
+
+std::vector<uint8_t> generate_decimal(size_t max_size = 16) {
+    std::vector<uint8_t> bytes;
+    for (size_t i = 0; i < max_size; ++i) {
+        bytes.push_back(random_generators::get_int<uint8_t>());
+    }
+
+    return bytes;
+}
 
 ::avro::GenericDatum generate_datum(
   const avro::NodePtr& node,
@@ -34,38 +45,69 @@ namespace testing {
         return elements_in_collection.value_or(
           random_generators::get_int<size_t>(10));
     };
+    ::avro::GenericDatum datum{node};
     switch (node->type()) {
     case avro::AVRO_STRING: {
+        if (node->logicalType().type() == avro::LogicalType::UUID) {
+            datum.value<std::string>() = fmt::to_string(uuid_t::create());
+            return datum;
+        }
         auto v = random_generators::gen_alphanum_string(
           random_generators::get_int(32));
-        return {std::string(v)};
+        datum.value<std::string>() = v;
+        return datum;
     }
     case avro::AVRO_BYTES: {
+        if (node->logicalType().type() == avro::LogicalType::DECIMAL) {
+            datum.value<std::vector<uint8_t>>() = generate_decimal();
+            return datum;
+        }
         auto sz = random_generators::get_int(512);
         std::vector<uint8_t> bytes;
         bytes.reserve(sz);
         for (int i = 0; i < sz; ++i) {
             bytes.push_back(random_generators::get_int<uint8_t>());
         }
-
-        return {bytes};
+        datum.value<std::vector<uint8_t>>() = bytes;
+        return datum;
     }
-    case avro::AVRO_INT:
-        return {random_generators::get_int<int32_t>()};
+    case avro::AVRO_INT: {
+        auto max = std::numeric_limits<int32_t>::max();
+        if (node->logicalType().type() == avro::LogicalType::TIME_MILLIS) {
+            max = seconds_in_day * 1000;
+        } else if (node->logicalType().type() == avro::LogicalType::DATE) {
+            max = 365;
+        }
+        datum.value<int32_t>() = random_generators::get_int<int32_t>(max);
+        return datum;
+    }
     case avro::AVRO_ENUM: {
         ::avro::GenericEnum e{node};
-        e.set(random_generators::get_int<int64_t>(node->names() - 1));
 
-        return {node, e};
+        datum.value<::avro::GenericEnum>().set(
+          random_generators::get_int<int64_t>(node->names() - 1));
+        return datum;
     }
-    case avro::AVRO_LONG:
-        return {random_generators::get_int<int64_t>()};
+    case avro::AVRO_LONG: {
+        auto max = std::numeric_limits<int64_t>::max();
+        if (node->logicalType().type() == avro::LogicalType::TIME_MICROS) {
+            max = seconds_in_day * 1000000L;
+        } else if (
+          node->logicalType().type() == avro::LogicalType::TIMESTAMP_MILLIS) {
+            max = max / 1000;
+        }
+        datum.value<int64_t>() = random_generators::get_int<int64_t>(max);
+        return datum;
+    }
     case avro::AVRO_FLOAT:
-        return {random_generators::get_real<float>()};
+        datum.value<float>() = random_generators::get_real<float>();
+        return datum;
     case avro::AVRO_DOUBLE:
-        return {random_generators::get_real<double>()};
+        datum.value<double>() = random_generators::get_real<double>();
+        return datum;
     case avro::AVRO_BOOL:
-        return {random_generators::random_choice({true, false})};
+        datum.value<bool>() = random_generators::random_choice({true, false});
+        return datum;
     case avro::AVRO_NULL:
         return {};
     case avro::AVRO_RECORD: {
@@ -140,12 +182,18 @@ namespace testing {
     }
     case avro::AVRO_FIXED: {
         ::avro::GenericFixed fixed{node};
+        if (node->logicalType().type() == avro::LogicalType::DECIMAL) {
+            fixed.value() = generate_decimal(node->fixedSize());
+            datum.value<::avro::GenericFixed>() = fixed;
+            return datum;
+        }
         fixed.value().reserve(node->fixedSize());
         for (size_t i = 0; i < node->fixedSize(); ++i) {
             fixed.value()[i] = random_generators::get_int<uint8_t>();
         }
         fixed.value().shrink_to_fit();
-        return {node, fixed};
+        datum.value<::avro::GenericFixed>() = fixed;
+        return datum;
     }
     case avro::AVRO_UNKNOWN:
         throw std::runtime_error("unsupported avro type");
