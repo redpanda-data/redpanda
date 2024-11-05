dcffd2015f2c4d5b58f4e5f379ee94eb621e8ce2
Michael Boquard <michael@redpanda.com>
2024-11-05 10:40:48 -0500

    Revert "dt: install license in fips environment"
    This reverts commit f8c04ec15312e785b5429d1d516c598bb6a5a631 except for
    the changes in license_enforcement_test.py
    
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- tests/rptest/transactions/transactions_test.py
+++ tests/rptest/transactions/transactions_test.py
@@ -38,7 +38,7 @@ from rptest.services.admin import Admin
 from rptest.services.redpanda_installer import RedpandaInstaller, wait_for_num_versions
 from rptest.clients.rpk import RpkTool, AclList
 
-from rptest.utils.mode_checks import skip_debug_mode, skip_fips_mode
+from rptest.utils.mode_checks import skip_debug_mode
 
 
 class TransactionsMixin:
@@ -1422,7 +1422,6 @@ class GATransaction_v22_1_UpgradeTest(RedpandaTest):
 
         self.check_consume(2)
 
-    @skip_fips_mode
     @cluster(num_nodes=3, log_allow_list=RESTART_LOG_ALLOW_LIST)
     def upgrade_coordinator_test(self):
         def get_tx_coordinator():
@@ -1434,7 +1433,6 @@ class GATransaction_v22_1_UpgradeTest(RedpandaTest):
 
         self.do_upgrade_with_tx(get_tx_coordinator)
 
-    @skip_fips_mode
     @cluster(num_nodes=3, log_allow_list=RESTART_LOG_ALLOW_LIST)
     def upgrade_topic_test(self):
         topic_name = self.topics[0].name
