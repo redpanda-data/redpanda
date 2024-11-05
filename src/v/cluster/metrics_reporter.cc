5cbf1b2d0d9e3568677d964733139a5a286bac8c
Oren Leiman <oren.leiman@redpanda.com>
2024-11-04 16:02:12 -0800

    metrics: Add list of enterprise features to call-home POST
    Flat list of feature names, only those which are currently enabled
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>
    (cherry picked from commit 4e64125268f1afbacbc0baa842ddd5a531da28ce)
    
    Conflicts:
      - metrics_reporter_test.py - slightly different code _adjacent_ to diff
        no change to new code.

--- src/v/cluster/metrics_reporter.cc
+++ src/v/cluster/metrics_reporter.cc
@@ -274,6 +274,8 @@ metrics_reporter::build_metrics_snapshot() {
                                  && !license.value().is_expired();
     snapshot.has_enterprise_features = feature_report.any();
 
+    snapshot.enterprise_features.emplace(std::move(feature_report));
+
     co_return snapshot;
 }
 
@@ -547,6 +549,15 @@ void rjson_serialize(
 
     w.Key("has_enterprise_features");
     w.Bool(snapshot.has_enterprise_features);
+
+    if (snapshot.enterprise_features.has_value()) {
+        w.Key("enterprise_features");
+        w.StartArray();
+        for (const auto& f : snapshot.enterprise_features.value().enabled()) {
+            w.String(fmt::format("{}", f));
+        }
+        w.EndArray();
+    }
 
     w.EndObject();
 }
