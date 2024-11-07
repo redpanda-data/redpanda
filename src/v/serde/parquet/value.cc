c70d8cf31f5bc37fb5bdb20418aeb346f496b377
Tyler Rockwood <rockwood@redpanda.com>
2024-11-07 13:44:32 +0000

    parquet: add method to copy a value
    For tests

--- src/v/serde/parquet/value.cc
+++ src/v/serde/parquet/value.cc
@@ -15,6 +15,38 @@
 
 #include <absl/strings/escaping.h>
 
+namespace serde::parquet {
+
+value copy(const value& val) {
+    return ss::visit(
+      val,
+      [](const byte_array_value& v) {
+          return value(byte_array_value(v.val.copy()));
+      },
+      [](const fixed_byte_array_value& v) {
+          return value(fixed_byte_array_value(v.val.copy()));
+      },
+      [](const group_value& v) {
+          group_value clone;
+          clone.reserve(v.size());
+          for (const auto& member : v) {
+              clone.emplace_back(copy(member.field));
+          }
+          return value(std::move(clone));
+      },
+      [](const repeated_value& v) {
+          repeated_value clone;
+          clone.reserve(v.size());
+          for (const auto& member : v) {
+              clone.emplace_back(copy(member.element));
+          }
+          return value(std::move(clone));
+      },
+      [](const auto& v) { return value(v); });
+}
+
+} // namespace serde::parquet
+
 auto fmt::formatter<serde::parquet::value>::format(
   const serde::parquet::value& val,
   fmt::format_context& ctx) const -> decltype(ctx.out()) {
