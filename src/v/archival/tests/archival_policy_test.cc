4aa22ad61db61fe72af61f233ce274db2a1879eb
Eugene Lazin <4lazin@gmail.com>
2024-11-13 12:35:27 +0100

    cs: Fix include in  "archival_policy_test.cc"

--- src/v/archival/tests/archival_policy_test.cc
+++ src/v/archival/tests/archival_policy_test.cc
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
-#include "cluster/archival/archival_policy.h"
+#include "archival/archival_policy.h"
 #include "storage/tests/utils/disk_log_builder.h"
 #include "test_utils/tmp_dir.h"
 
