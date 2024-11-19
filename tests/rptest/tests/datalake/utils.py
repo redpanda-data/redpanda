068859499d049b77c96e6c0ff7c2d5ce06cdf012
Nicolae Vartolomei <nv@redpanda.com>
2024-11-19 10:32:43 +0000

    rptest/datalake: fix cloud_storage_type injected arg
    It must be a list of cloud storage types
    
    (cherry picked from commit 9902203b88e2c4d3a45da8763b6993a9adb99823)

--- tests/rptest/tests/datalake/utils.py
+++ tests/rptest/tests/datalake/utils.py
@@ -5,22 +5,25 @@
 #
 # As of the Change Date specified in that file, in accordance with
 # the Business Source License, use of this s
-from rptest.services.redpanda import CloudStorageType, get_cloud_provider, get_cloud_storage_type
-from itertools import product
+from rptest.services.redpanda import CloudStorageType, get_cloud_provider
 
+# For docker we run only s3 tests because azurite doesn't support
+# Data Lake Storage Gen2 which seems to be necessary for iceberg catalog and alike.
 # gcp also uses S3 as storage type
 # Currently we only run tests in ec2 + S3 combination
 # as the query engine deployment was tested only in ec2.
 # The credential plumbing (IAM roles, credential providers) probably
 # needs some fixing in GCP environment before enabling it here, otherwise
 # there no fundamental reason it shouldn't work.
-ACCEPTED_CLOUD_PROVIDERS = {"docker", "aws"}
-ACCEPTED_STORAGE_TYPES = [CloudStorageType.S3]
+ACCEPTED_VARIANTS = {("docker", CloudStorageType.S3),
+                     ("aws", CloudStorageType.S3),
+                     ("azure", CloudStorageType.ABS)}
 
 
 def supported_storage_types():
-    cloud_provider = ACCEPTED_CLOUD_PROVIDERS.intersection(
-        {get_cloud_provider()})
-    storage_type = get_cloud_storage_type(
-        applies_only_on=ACCEPTED_STORAGE_TYPES)
-    return list(product(cloud_provider, storage_type))
+    cloud_provider = get_cloud_provider()
+    supported_variants = filter(lambda x: x[0] == cloud_provider,
+                                ACCEPTED_VARIANTS)
+    supported_storage = map(lambda x: x[1], supported_variants)
+    unique = list(set(supported_storage))
+    return unique
