920530cf1837759d808cf00d7000f2cc645d0662
Nicolae Vartolomei <nv@redpanda.com>
2024-11-19 16:32:21 +0000

    rptest/s: additional refactoring to spark service to support azure

--- tests/rptest/services/spark_service.py
+++ tests/rptest/services/spark_service.py
@@ -8,73 +8,89 @@
 # by the Apache License, Version 2.0
 
 from typing import Optional
+
 from ducktape.services.service import Service
-from ducktape.cluster.cluster import ClusterNode
 from ducktape.utils.util import wait_until
 from pyhive import hive
+
 from rptest.context import cloud_storage
-from rptest.services.redpanda import SISettings
-from rptest.tests.datalake.query_engine_base import QueryEngineType, QueryEngineBase
-import jinja2
+from rptest.tests.datalake.query_engine_base import QueryEngineBase, QueryEngineType
 
 
 class SparkService(Service, QueryEngineBase):
     """Spark service for querying data generated in datalake."""
 
     SPARK_HOME = "/opt/spark"
+    SPARK_SERVER_EXEC = "/opt/spark/sbin/start-thriftserver.sh"
     LOGS_DIR = f"{SPARK_HOME}/logs"
-    logs = {"spark_sql_logs": {"path": LOGS_DIR, "collect_default": True}}
 
-    SPARK_SERVER_CMD = jinja2.Template(
-        """ /opt/spark/sbin/start-thriftserver.sh \
-        --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \
-        --conf spark.sql.defaultCatalog=redpanda-iceberg-catalog \
-        --conf spark.sql.catalog.redpanda-iceberg-catalog=org.apache.iceberg.spark.SparkCatalog \
-        --conf spark.sql.catalog.redpanda-iceberg-catalog.type=rest \
-        --conf spark.sql.catalog.redpanda-iceberg-catalog.uri={{ catalog }} \
-        --conf spark.sql.catalog.redpanda-iceberg-catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO \
-        --conf spark.sql.catalog.redpanda-iceberg-catalog.cache-enabled=false \
-        {% if s3 -%}
-        --conf spark.sql.catalog.redpanda-iceberg-catalog.s3.endpoint={{ s3 }}
-        {% endif %}""")
+    logs = {"spark_sql_logs": {"path": LOGS_DIR, "collect_default": True}}
 
     def __init__(self, ctx, iceberg_catalog_rest_uri: str):
         super(SparkService, self).__init__(ctx, num_nodes=1)
         self.iceberg_catalog_rest_uri = iceberg_catalog_rest_uri
+
         self.credentials = cloud_storage.Credentials.from_context(ctx)
+        self.spark_host: Optional[SparkService] = None
+        self.spark_port = 10000
+
+    def make_env(self):
+        env = dict()
+
         if isinstance(self.credentials, cloud_storage.S3Credentials):
-            self.cloud_storage_access_key = self.credentials.access_key
-            self.cloud_storage_secret_key = self.credentials.secret_key
-            self.cloud_storage_region = self.credentials.region
-            self.cloud_storage_api_endpoint = self.credentials.endpoint
+            env["AWS_ACCESS_KEY_ID"] = self.credentials.access_key
+            env["AWS_SECRET_ACCESS_KEY"] = self.credentials.secret_key
+            env["AWS_REGION"] = self.credentials.region
         elif isinstance(self.credentials,
                         cloud_storage.AWSInstanceMetadataCredentials):
             pass
         else:
             raise ValueError(
                 f"Unsupported cloud storage type {type(self.credentials)}")
-        self.spark_host: Optional[SparkService] = None
-        self.spark_port = 10000
 
-    def make_env(self):
-        env = dict()
-        if self.cloud_storage_access_key:
-            env["AWS_ACCESS_KEY_ID"] = self.cloud_storage_access_key
-        if self.cloud_storage_secret_key:
-            env["AWS_SECRET_ACCESS_KEY"] = self.cloud_storage_secret_key
-        if self.cloud_storage_region:
-            env["AWS_REGION"] = self.cloud_storage_region
         return " ".join([f"{k}={v}" for k, v in env.items()])
 
     def start_cmd(self):
-        cmd = SparkService.SPARK_SERVER_CMD.render(
-            catalog=self.iceberg_catalog_rest_uri,
-            s3=self.cloud_storage_api_endpoint)
+        conf_args: dict[str, Optional[str | bool]] = {
+            "spark.sql.extensions":
+            "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+            "spark.sql.defaultCatalog":
+            "redpanda-iceberg-catalog",
+            "spark.sql.catalog.redpanda-iceberg-catalog":
+            "org.apache.iceberg.spark.SparkCatalog",
+            "spark.sql.catalog.redpanda-iceberg-catalog.type":
+            "rest",
+            "spark.sql.catalog.redpanda-iceberg-catalog.cache-enabled":
+            False,
+            "spark.sql.catalog.redpanda-iceberg-catalog.uri":
+            self.iceberg_catalog_rest_uri,
+        }
+
+        if isinstance(self.credentials, cloud_storage.S3Credentials):
+            conf_args.update({
+                "spark.sql.catalog.redpanda-iceberg-catalog.io-impl":
+                "org.apache.iceberg.aws.s3.S3FileIO",
+                "spark.sql.catalog.redpanda-iceberg-catalog.s3.endpoint":
+                self.credentials.endpoint,
+            })
+        elif isinstance(self.credentials,
+                        cloud_storage.AWSInstanceMetadataCredentials):
+            conf_args.update({
+                "spark.sql.catalog.redpanda-iceberg-catalog.io-impl":
+                "org.apache.iceberg.aws.s3.S3FileIO",
+            })
+        else:
+            raise ValueError(
+                f"Unsupported cloud storage type {type(self.credentials)}")
+
         env = self.make_env()
-        return f"{env} {cmd}"
+        return f"{env} {SparkService.SPARK_SERVER_EXEC} {SparkService.dict_to_conf_args(conf_args)}"
 
     def start_node(self, node, timeout_sec=120, **kwargs):
-        node.account.ssh(self.start_cmd(), allow_fail=False)
+        start_cmd = self.start_cmd()
+        self.logger.info(
+            f"Starting Spark SQL server with command: {start_cmd}")
+        node.account.ssh(start_cmd, allow_fail=False)
         self.spark_host = node.account.hostname
         self.wait(timeout_sec=timeout_sec)
 
@@ -111,3 +127,15 @@ class SparkService(Service, QueryEngineBase):
     def make_client(self):
         assert self.spark_host
         return hive.connect(host=self.spark_host, port=self.spark_port)
+
+    @staticmethod
+    def dict_to_conf_args(conf: dict[str, Optional[str | bool]]) -> str:
+        def transform_value(v: str | bool):
+            if isinstance(v, bool):
+                return str(v).lower()
+            return v
+
+        return " ".join([
+            f"--conf {k}={transform_value(v)}" for k, v in conf.items()
+            if v is not None
+        ])
