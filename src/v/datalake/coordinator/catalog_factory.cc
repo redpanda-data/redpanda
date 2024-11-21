995df0c9f2f21468fc861d66ee3c1b69577c9b01
Michał Maślanka <michal@redpanda.com>
2024-11-21 17:48:42 +0100

    iceberg/rest: added tls support to iceberg rest catalog factory
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- src/v/datalake/coordinator/catalog_factory.cc
+++ src/v/datalake/coordinator/catalog_factory.cc
@@ -15,6 +15,8 @@
 #include "iceberg/filesystem_catalog.h"
 #include "iceberg/rest_catalog.h"
 #include "iceberg/rest_client/catalog_client.h"
+#include "net/tls.h"
+#include "net/tls_certificate_probe.h"
 #include "thirdparty/ada/ada.h"
 
 #include <absl/strings/numbers.h>
@@ -30,14 +32,61 @@ void throw_if_not_present(const config::property<std::optional<T>>& property) {
     }
 }
 
-net::unresolved_address endpoint_to_address(const ss::sstring& url_str) {
+ss::future<ss::shared_ptr<ss::tls::certificate_credentials>>
+build_tls_credentials(config::configuration& cfg) {
+    ss::tls::credentials_builder cred_builder;
+    cred_builder.set_cipher_string(
+      {config::tlsv1_2_cipher_string.data(),
+       config::tlsv1_2_cipher_string.size()});
+    cred_builder.set_ciphersuites(
+      {config::tlsv1_3_ciphersuites.data(),
+       config::tlsv1_3_ciphersuites.size()});
+    cred_builder.set_minimum_tls_version(from_config(cfg.tls_min_version()));
+    auto trust_file = cfg.iceberg_rest_catalog_trust_file();
+    if (trust_file.has_value()) {
+        auto file = trust_file.value();
+        vlog(datalake_log.info, "Using non-default trust file {}", file);
+        co_await cred_builder.set_x509_trust_file(
+          file, ss::tls::x509_crt_format::PEM);
+    } else {
+        // Use system defaults, might not work on all systems
+        auto ca_file = co_await net::find_ca_file();
+        if (ca_file) {
+            vlog(
+              datalake_log.info,
+              "Using automatically discovered trust file {}",
+              ca_file.value());
+            co_await cred_builder.set_x509_trust_file(
+              ca_file.value(), ss::tls::x509_crt_format::PEM);
+        } else {
+            vlog(
+              datalake_log.info,
+              "Trust file can't be detected automatically, using system "
+              "default");
+            co_await cred_builder.set_system_trust();
+        }
+    }
+    if (auto crl_file = cfg.iceberg_rest_catalog_crl_file();
+        crl_file.has_value()) {
+        co_await cred_builder.set_x509_crl_file(
+          *crl_file, ss::tls::x509_crt_format::PEM);
+    }
+    co_return co_await cred_builder.build_reloadable_certificate_credentials();
+};
+struct endpoint_information {
+    net::unresolved_address address;
+    bool needs_tls;
+    std::optional<iceberg::rest_client::base_path> base_path;
+};
+
+endpoint_information endpoint_to_address(const ss::sstring& url_str) {
     auto url = ada::parse(url_str);
     if (!url) {
         throw std::invalid_argument(fmt::format(
           "Malformed Iceberg REST catalog endpoint url: {}", url_str));
     }
     // Default port as used by the Iceberg catalogs
-    uint16_t port = 8181;
+    uint16_t port = url->type == ada::scheme::HTTPS ? 443 : 8181;
     if (url->has_port()) {
         int32_t port_from_uri{0};
         auto parsed = absl::SimpleAtoi(url->get_port(), &port_from_uri);
@@ -51,7 +100,17 @@ net::unresolved_address endpoint_to_address(const ss::sstring& url_str) {
         }
         port = static_cast<uint16_t>(port_from_uri);
     }
-    return {ss::sstring(url->get_hostname()), port};
+    std::optional<iceberg::rest_client::base_path> path
+      = url->get_pathname() != ""
+          ? std::make_optional<iceberg::rest_client::base_path>(
+              url->get_pathname())
+          : std::nullopt;
+
+    return {
+      .address
+      = net::unresolved_address{ss::sstring(url->get_hostname()), port},
+      .needs_tls = url->type == ada::scheme::HTTPS,
+      .base_path = std::move(path)};
 }
 
 } // namespace
@@ -62,7 +121,7 @@ filesystem_catalog_factory::filesystem_catalog_factory(
   const cloud_storage_clients::bucket_name& bucket)
   : config_(&config)
   , remote_(&remote)
-  , bucket_(std::move(bucket)) {}
+  , bucket_(bucket) {}
 
 ss::future<std::unique_ptr<iceberg::catalog>>
 filesystem_catalog_factory::create_catalog() {
@@ -85,17 +144,34 @@ rest_catalog_factory::create_catalog() {
     throw_if_not_present(config_->iceberg_rest_catalog_client_secret);
     throw_if_not_present(config_->iceberg_rest_catalog_client_id);
 
-    vlog(
-      datalake_log.info,
-      "Creating rest Iceberg catalog connected to ",
+    auto endpoint_information = endpoint_to_address(
       config_->iceberg_rest_catalog_endpoint().value());
 
+    net::base_transport::configuration transport_config{
+      .server_addr = endpoint_information.address,
+    };
+    if (endpoint_information.needs_tls) {
+        transport_config.credentials = co_await build_tls_credentials(*config_);
+    }
     std::unique_ptr<http::abstract_client> http_client
       = static_cast<std::unique_ptr<http::abstract_client>>(
-        std::make_unique<http::client>(net::base_transport::configuration{
-          .server_addr = endpoint_to_address(
-            config_->iceberg_rest_catalog_endpoint().value())}));
-
+        std::make_unique<http::client>(std::move(transport_config)));
+    auto prefix_path
+      = config_->iceberg_rest_catalog_prefix()
+          ? std::make_optional<iceberg::rest_client::prefix_path>(
+              config_->iceberg_rest_catalog_prefix().value())
+          : std::nullopt;
+    auto token = config_->iceberg_rest_catalog_token()
+                   ? std::make_optional<iceberg::rest_client::oauth_token>(
+                       config_->iceberg_rest_catalog_token().value())
+                   : std::nullopt;
+    vlog(
+      datalake_log.info,
+      "Creating rest Iceberg catalog connected to: {}, with base path: {} and "
+      "prefix: {}",
+      endpoint_information.address,
+      endpoint_information.base_path,
+      prefix_path);
     // TODO: support OAuth token here
     auto client = std::make_unique<iceberg::rest_client::catalog_client>(
       std::move(http_client),
@@ -105,10 +181,10 @@ rest_catalog_factory::create_catalog() {
       iceberg::rest_client::credentials{
         .client_id = config_->iceberg_rest_catalog_client_id().value(),
         .client_secret = config_->iceberg_rest_catalog_client_secret().value()},
-      std::nullopt, // base_path
-      std::nullopt, // prefix
-      std::nullopt, // api_version
-      std::nullopt  // token
+      std::move(endpoint_information.base_path), // base_path
+      std::move(prefix_path),                    // prefix
+      std::nullopt,                              // api_version
+      std::move(token)                           // token
     );
 
     co_return std::make_unique<iceberg::rest_catalog>(
