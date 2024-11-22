b187703a06cd02ab5288eb2001bc4aeddfd4c88f
Brandon Allard <brandon@redpanda.com>
2024-11-22 00:41:18 -0500

    datalake: add test_serde_parquet_data_writer
    This data writer is for tests. It doesn't write to any files, however,
    it does go through the process of converting the ostream to parqueat.

--- src/v/datalake/tests/test_data_writer.h
+++ src/v/datalake/tests/test_data_writer.h
@@ -9,8 +9,10 @@
 #pragma once
 
 #include "datalake/data_writer_interface.h"
+#include "datalake/serde_parquet_writer.h"
 #include "iceberg/datatypes.h"
 #include "iceberg/values.h"
+#include "utils/null_output_stream.h"
 
 #include <seastar/core/future.hh>
 #include <seastar/core/shared_ptr.hh>
@@ -60,6 +62,46 @@ public:
 private:
     iceberg::struct_type _schema;
     bool _return_error;
+};
+
+class test_serde_parquet_data_writer : public parquet_file_writer {
+public:
+    explicit test_serde_parquet_data_writer(
+      std::unique_ptr<parquet_ostream> writer)
+      : _writer(std::move(writer))
+      , _result{} {}
+
+    ss::future<writer_error>
+    add_data_struct(iceberg::struct_value data, int64_t sz) override {
+        auto write_result = co_await _writer->add_data_struct(
+          std::move(data), sz);
+        _result.row_count++;
+        co_return write_result;
+    }
+
+    ss::future<result<local_file_metadata, writer_error>> finish() override {
+        return ss::make_ready_future<result<local_file_metadata, writer_error>>(
+          _result);
+    }
+
+private:
+    std::unique_ptr<parquet_ostream> _writer;
+    local_file_metadata _result;
+};
+
+class test_serde_parquet_writer_factory : public parquet_file_writer_factory {
+public:
+    ss::future<result<std::unique_ptr<parquet_file_writer>, writer_error>>
+    create_writer(const iceberg::struct_type& schema) override {
+        auto ostream_writer = co_await _serde_parquet_factory.create_writer(
+          schema, utils::make_null_output_stream());
+
+        co_return std::make_unique<test_serde_parquet_data_writer>(
+          std::move(ostream_writer));
+    }
+
+private:
+    serde_parquet_writer_factory _serde_parquet_factory;
 };
 
 } // namespace datalake
