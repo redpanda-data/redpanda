2cc256d589bb6264886a95abc15d15b925a78759
Stephan Dollberg <stephan@redpanda.com>
2024-11-28 09:38:08 +0000

    feature_table: Add v25.1 version
    Prepares the feature table for the next release of v25.1

--- src/v/features/feature_table.h
+++ src/v/features/feature_table.h
@@ -134,7 +134,8 @@ enum class release_version : int64_t {
     v24_1_1 = 12,
     v24_2_1 = 13,
     v24_3_1 = 14,
-    MAX = v24_3_1, // affects the latest_version
+    v25_1_1 = 15,
+    MAX = v25_1_1, // affects the latest_version
 };
 
 constexpr cluster::cluster_version to_cluster_version(release_version rv) {
@@ -151,6 +152,7 @@ constexpr cluster::cluster_version to_cluster_version(release_version rv) {
     case release_version::v24_1_1:
     case release_version::v24_2_1:
     case release_version::v24_3_1:
+    case release_version::v25_1_1:
         return cluster::cluster_version{static_cast<int64_t>(rv)};
     }
     vassert(false, "Invalid release_version");
