1ac19195fd5c5deabdba3fd406cfe37b578a47e9
Michał Maślanka <michal@redpanda.com>
2024-12-06 08:56:28 +0100

    tests: added iceberg verifiers to random_node_operations_test
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- tests/rptest/tests/random_node_operations_test.py
+++ tests/rptest/tests/random_node_operations_test.py
@@ -14,12 +14,15 @@ import threading
 from rptest.clients.rpk import RpkTool
 from rptest.services.admin import Admin
 from rptest.services.apache_iceberg_catalog import IcebergRESTCatalog
+from rptest.services.catalog_service import CatalogType
+from rptest.services.spark_service import SparkService
 from rptest.tests.prealloc_nodes import PreallocNodesTest
 
 from ducktape.mark import matrix, ignore
 from ducktape.utils.util import wait_until
 from rptest.services.admin_ops_fuzzer import AdminOperationsFuzzer
 from rptest.services.cluster import cluster
+from rptest.tests.datalake.datalake_verifier import DatalakeVerifier
 from rptest.clients.types import TopicSpec
 from rptest.clients.default import DefaultClient
 from rptest.services.kgo_verifier_services import KgoVerifierConsumerGroupConsumer, KgoVerifierProducer
@@ -88,7 +91,8 @@ class RandomNodeOperationsTest(PreallocNodesTest):
         self.catalog_service = IcebergRESTCatalog(
             test_context,
             cloud_storage_bucket=self._si_settings.cloud_storage_bucket,
-            filesystem_wrapper_mode=False)
+            filesystem_wrapper_mode=False,
+            log_level="INFO")
 
     def min_producer_records(self):
         return 20 * self.producer_throughput
@@ -120,6 +124,12 @@ class RandomNodeOperationsTest(PreallocNodesTest):
 
     def setUp(self):
         self.catalog_service.start()
+        self.spark = SparkService(
+            ctx=self.test_context,
+            iceberg_catalog_uri=self.catalog_service.iceberg_rest_url,
+            default_warehouse_dir=self.catalog_service.DEFAULT_WAREHOUSE_NAME,
+            catalog_type=CatalogType.REST_JDBC)
+        self.spark.start()
 
     def _setup_test_scale(self):
         # test setup
@@ -332,13 +342,19 @@ class RandomNodeOperationsTest(PreallocNodesTest):
     # before v24.2, dns query to s3 endpoint do not include the bucketname, which is required for AWS S3 fips endpoints
     @skip_fips_mode
     @skip_debug_mode
-    @cluster(num_nodes=9,
+    @cluster(num_nodes=10,
              log_allow_list=CHAOS_LOG_ALLOW_LIST +
              PREV_VERSION_LOG_ALLOW_LIST + TS_LOG_ALLOW_LIST)
     @matrix(enable_failures=[True, False],
             mixed_versions=[True, False],
             with_tiered_storage=[True, False],
-            with_iceberg=[True, False],
+            with_iceberg=[True],
+            with_chunked_compaction=[True, False],
+            cloud_storage_type=get_cloud_storage_type())
+    @cluster(num_nodes=8)
+    @matrix(enable_failures=[True, False],
+            mixed_versions=[True, False],
+            with_iceberg=[False],
             with_chunked_compaction=[True, False],
             cloud_storage_type=get_cloud_storage_type())
     def test_node_operations(self, enable_failures, mixed_versions,
@@ -526,6 +542,20 @@ class RandomNodeOperationsTest(PreallocNodesTest):
                                     is not TopicSpec.CLEANUP_DELETE),
                 tolerate_data_loss=True)
             write_caching_producer_consumer.start()
+        dl_verifiers = []
+        # when Iceberg is enabled, start a verifier for the two topics:
+        # - regular topic
+        # - topic with fast partition movements enabled
+        if with_iceberg:
+            dl_verifiers.append(
+                DatalakeVerifier(self.redpanda, regular_topic.name,
+                                 self.spark))
+            if enable_fast_partition_movement():
+                dl_verifiers.append(
+                    DatalakeVerifier(self.redpanda, fast_topic.name,
+                                     self.spark))
+            for verifier in dl_verifiers:
+                verifier.start()
 
         # start admin operations fuzzer, it will provide a stream of
         # admin day 2 operations executed during the test
@@ -611,6 +641,9 @@ class RandomNodeOperationsTest(PreallocNodesTest):
             )
 
         verify_offset_translator_state_consistent(self.redpanda)
+        for verifier in dl_verifiers:
+            verifier.wait(progress_timeout_sec=60)
+
         # Validate that the controller log written during the test is readable by offline log viewer
         log_viewer = OfflineLogViewer(self.redpanda)
         for node in self.redpanda.started_nodes():
