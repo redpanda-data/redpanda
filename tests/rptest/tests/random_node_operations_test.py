a74138a2a40d0393f8265bc4a997912e1d0ead12
Michał Maślanka <michal@redpanda.com>
2024-12-18 18:03:55 +0100

    tests: validate offset translator state in random_node_operations_test
    Signed-off-by: Michał Maślanka <michal@redpanda.com>
    (cherry picked from commit 59eba45a567add9689a2bf5a4d8bbab56660b3e7)

--- tests/rptest/tests/random_node_operations_test.py
+++ tests/rptest/tests/random_node_operations_test.py
@@ -26,7 +26,7 @@ from rptest.services.kgo_verifier_services import KgoVerifierConsumerGroupConsum
 from rptest.services.redpanda import CHAOS_LOG_ALLOW_LIST, PREV_VERSION_LOG_ALLOW_LIST, CloudStorageType, PandaproxyConfig, SISettings, SchemaRegistryConfig
 from rptest.services.redpanda_installer import RedpandaInstaller
 from rptest.utils.mode_checks import cleanup_on_early_exit, skip_debug_mode, skip_fips_mode
-from rptest.utils.node_operations import FailureInjectorBackgroundThread, NodeOpsExecutor, generate_random_workload
+from rptest.utils.node_operations import FailureInjectorBackgroundThread, NodeOpsExecutor, generate_random_workload, verify_offset_translator_state_consistent
 
 from rptest.clients.offline_log_viewer import OfflineLogViewer
 
@@ -577,6 +577,7 @@ class RandomNodeOperationsTest(PreallocNodesTest):
                 err_msg="Error waiting for cluster to report consistent version"
             )
 
+        verify_offset_translator_state_consistent(self.redpanda)
         # Validate that the controller log written during the test is readable by offline log viewer
         log_viewer = OfflineLogViewer(self.redpanda)
         for node in self.redpanda.started_nodes():
