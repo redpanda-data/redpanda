948b6ae0b58d684932552b9db6a70c3e5ef91c3e
Noah Watkins <noahwatkins@gmail.com>
2025-02-06 21:29:27 -0800

    storage: remove chunk cache for resource utils
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/storage/storage_resources.cc
+++ src/v/storage/storage_resources.cc
@@ -14,7 +14,6 @@
 #include "base/seastarx.h"
 #include "base/vlog.h"
 #include "config/configuration.h"
-#include "storage/chunk_cache.h"
 #include "storage/logger.h"
 
 #include <seastar/core/smp.hh>
@@ -36,7 +35,7 @@ storage_resources::storage_resources(
   , _global_target_replay_bytes(target_replay_bytes)
   , _max_concurrent_replay(max_concurrent_replay)
   , _compaction_index_mem_limit(compaction_index_memory)
-  , _append_chunk_size(internal::chunks().chunk_size())
+  , _append_chunk_size(config::shard_local_cfg().append_chunk_size())
   , _offset_translator_dirty_bytes(
       _global_target_replay_bytes() / ss::smp::count)
   , _configuration_manager_dirty_bytes(
