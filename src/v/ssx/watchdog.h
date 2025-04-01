4767dc6a49e264a0f9232ad90dc834cab2039f81
Nicolae Vartolomei <nv@redpanda.com>
2025-04-01 18:07:43 +0100

    ssx/watchdog: add a periodic watchdog constructor

--- src/v/ssx/watchdog.h
+++ src/v/ssx/watchdog.h
@@ -67,6 +67,39 @@ public:
       seastar::noncopyable_function<void()> callback) {
         start_waiting(timeout, std::move(callback));
     }
+
+    /// Create watchdog instance
+    ///
+    /// \param timeout defines time interval after which the watchdog will be
+    ///                triggered for the first time
+    /// \param repeat_interval defines the time interval after which the
+    ///                        watchdog will be trigger subsequently
+    /// \param callback is a callback that will be called when
+    ///                 the watchdog is triggered
+    /// \note The callback may outlive the watchdog instance. See the full note
+    /// on the previous constructor.
+    watchdog(
+      seastar::lowres_clock::duration timeout,
+      seastar::lowres_clock::duration repeat_interval,
+      seastar::noncopyable_function<void()> callback) {
+        ssx::background = ssx::ignore_shutdown_exceptions(seastar::do_with(
+          std::move(callback),
+          [this, timeout, repeat_interval](
+            seastar::noncopyable_function<void()>& cb) {
+              return seastar::sleep_abortable(timeout, _as)
+                .then([&cb] { cb(); })
+                .then([this, repeat_interval, &cb] {
+                    return seastar::repeat([this, repeat_interval, &cb] {
+                        return seastar::sleep_abortable(repeat_interval, _as)
+                          .then([&cb] {
+                              cb();
+                              return seastar::stop_iteration::no;
+                          });
+                    });
+                });
+          }));
+    }
+
     // D-tor defuses the watchdog. The callback won't be called after this.
     ~watchdog() {
         // Cancellation happens asynchronously but its guaranteed that
