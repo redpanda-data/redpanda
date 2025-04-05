9ccadb4112ef03c89f9bc401860efb944fa55878
Evgeny Lazin <4lazin@gmail.com>
2025-04-05 05:32:46 -0400

    archival: Remove dead code
    Signed-off-by: Evgeny Lazin <4lazin@gmail.com>

--- src/v/cluster/archival/archival_policy.h
+++ src/v/cluster/archival/archival_policy.h
@@ -90,19 +90,6 @@ public:
       std::optional<segment_time_limit> limit = std::nullopt,
       ss::io_priority_class io_priority = ss::default_priority_class());
 
-    /// \brief regurn next upload candidate
-    ///
-    /// \param begin_inclusive is an inclusive begining of the range
-    /// \param end_exclusive is an exclusive end of the range
-    /// \param lm is a log manager
-    /// \return initializd struct on success, empty struct on failure
-    ss::future<candidate_creation_result> get_next_candidate(
-      model::offset begin_inclusive,
-      model::offset end_exclusive,
-      std::optional<model::offset> flush_offset,
-      ss::shared_ptr<storage::log>,
-      ss::lowres_clock::duration segment_lock_duration);
-
     ss::future<candidate_creation_result> get_next_compacted_segment(
       model::offset begin_inclusive,
       ss::shared_ptr<storage::log> log,
@@ -127,33 +114,10 @@ private:
     /// result in partial upload.
     bool upload_deadline_reached();
 
-    struct lookup_result {
-        ss::lw_shared_ptr<storage::segment> segment;
-        const storage::ntp_config* ntp_conf;
-        bool forced;
-    };
-
-    lookup_result find_segment(
-      model::offset last_offset,
-      model::offset adjusted_lso,
-      std::optional<model::offset> flush_offset,
-      ss::shared_ptr<storage::log>);
-
     model::ntp _ntp;
     std::optional<segment_time_limit> _upload_limit;
     std::optional<ss::lowres_clock::time_point> _upload_deadline;
     ss::io_priority_class _io_priority;
 };
-
-/// This function computes offsets for the upload (inc. file offets)
-/// If the full segment is uploaded the segment is not scanned.
-/// If the upload is partial, the partial scan will be performed if
-/// the segment has the index and full scan otherwise.
-ss::future<std::optional<std::error_code>> get_file_range(
-  model::offset begin_inclusive,
-  std::optional<model::offset> end_inclusive,
-  ss::lw_shared_ptr<storage::segment> segment,
-  ss::lw_shared_ptr<upload_candidate> upl,
-  ss::io_priority_class io_priority);
 
 } // namespace archival
