04400447ad157b31b1ca364e25c397dcc91719bc
Alexey Bashtanov <alexey.bashtanov@redpanda.com>
2025-04-10 09:47:40 +0100

    debug3

--- src/v/raft/state_machine_manager.cc
+++ src/v/raft/state_machine_manager.cc
@@ -210,7 +210,9 @@ ss::future<> state_machine_manager::stop() {
     auto gate_f = _gate.close();
     co_await ss::coroutine::parallel_for_each(_machines, [this](auto p) {
         vlog(_log.debug, "stopping state machine {}", p.first);
-        return p.second->stm->stop();
+        return p.second->stm->stop().then([this, name = p.first] {
+            vlog(_log.debug, "stopped state machine {}", name);
+        });
     });
     co_await std::move(gate_f);
 }
