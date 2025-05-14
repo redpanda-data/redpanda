0f28ace4d7ff85c4d2ae6bdff919e2d9ab5fb7fe
Michael Boquard <michael@redpanda.com>
2025-05-14 19:10:13 -0400

    cluster/snapshot: Added panda link to snapshot
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/cluster/controller_snapshot.h
+++ src/v/cluster/controller_snapshot.h
@@ -18,6 +18,7 @@
 #include "container/chunked_hash_map.h"
 #include "container/fragmented_vector.h"
 #include "features/feature_table_snapshot.h"
+#include "model/panda_link.h"
 #include "security/role.h"
 #include "security/types.h"
 #include "serde/envelope.h"
@@ -296,6 +297,16 @@ struct data_migrations_t
     auto serde_fields() { return std::tie(next_id, migrations); }
 };
 
+struct panda_link_t
+  : public serde::
+      envelope<panda_link_t, serde::version<0>, serde::compat_version<0>> {
+    absl::btree_map<model::panda_link_id, model::panda_link_metadata> links;
+
+    friend bool operator==(const panda_link_t&, const panda_link_t&) = default;
+
+    auto serde_fields() { return std::tie(links); }
+};
+
 } // namespace controller_snapshot_parts
 
 struct controller_snapshot
@@ -314,6 +325,7 @@ struct controller_snapshot
     controller_snapshot_parts::cluster_recovery_t cluster_recovery;
     controller_snapshot_parts::client_quotas_t client_quotas;
     controller_snapshot_parts::data_migrations_t data_migrations;
+    controller_snapshot_parts::panda_link_t panda_links;
 
     friend bool
     operator==(const controller_snapshot&, const controller_snapshot&)
