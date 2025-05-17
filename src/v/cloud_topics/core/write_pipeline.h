9f622f5f94b00b187286c895018649ec19fda455
Evgeny Lazin <4lazin@gmail.com>
2025-05-17 09:17:14 -0400

    ct: Refactor write_pipeline
    Update the signature of the write_and_debounce and add trigger_event
    method.
    
    Signed-off-by: Evgeny Lazin <4lazin@gmail.com>

--- src/v/cloud_topics/core/write_pipeline.h
+++ src/v/cloud_topics/core/write_pipeline.h
@@ -51,7 +51,8 @@ public:
     ss::sstring pipeline_name() const { return "write_pipeline"; }
 
     /// Add write request to the pipeline
-    ss::future<result<model::record_batch_reader>> write_and_debounce(
+    ss::future<result<ss::circular_buffer<model::record_batch>>>
+    write_and_debounce(
       model::ntp ntp,
       model::record_batch_reader r,
       std::chrono::milliseconds timeout);
@@ -155,6 +156,8 @@ public:
     stage register_write_pipeline_stage() noexcept;
 
     void signal(pipeline_stage stage);
+
+    event trigger_event(pipeline_stage stage);
 
 private:
     /// Get write requests atomically.
