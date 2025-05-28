e661696b6472d20153220ce8151b419bf0186411
Willem Kaufmann <willem.kaufmann@gmail.com>
2025-05-28 09:04:13 -0400

    test

--- src/v/storage/disk_log_impl.cc
+++ src/v/storage/disk_log_impl.cc
@@ -7,6 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
+// Testing
 #include "storage/disk_log_impl.h"
 
 #include "base/vassert.h"
