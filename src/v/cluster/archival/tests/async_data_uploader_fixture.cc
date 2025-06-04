afa61358bdcac3821ac2fa5374da04a27f89ac9f
Oren Leiman <oren.leiman@redpanda.com>
2025-06-03 21:58:02 -0700

    c/a/tests: Extract async_data_uploader_fixture

---
+++ src/v/cluster/archival/tests/async_data_uploader_fixture.cc
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ *
+ */
+
+#include "cluster/archival/tests/async_data_uploader_fixture.h"
+
+namespace archival {
+
+ss::logger adu_fixture_log("async-uploader-fixture");
+
+} // namespace archival
