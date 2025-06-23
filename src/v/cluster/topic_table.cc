f9c9f95daf9a392e20dee88d6b0ab35d545d3b5c
Bharath Vissapragada <bharathv@redpanda.com>
2025-06-23 14:30:39 -0700

    c/topic_table: remove unusued variable
    (cherry picked from commit dad7bbba900a9abfb7de28ace5d82752fbd9e9ab)

--- src/v/cluster/topic_table.cc
+++ src/v/cluster/topic_table.cc
@@ -1990,7 +1990,6 @@ void topic_table::change_partition_replicas(
         update_revision,
         policy,
         &_probe));
-    auto previous_assignment = current_assignment.replicas;
     // replace partition replica set
     current_assignment.replicas = new_assignment;
     _topics_map_revision++;
