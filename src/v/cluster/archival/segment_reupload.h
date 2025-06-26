25ae4559209ba49763ab0dc94e558e1640e3f9f9
Oren Leiman <oren.leiman@redpanda.com>
2025-06-26 14:41:22 -0700

    c/a/upload: Purify segment_collector::align_end_offset_to_manifest
    Assign result instead of modifying internal state
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/cluster/archival/segment_reupload.h
+++ src/v/cluster/archival/segment_reupload.h
@@ -235,7 +235,8 @@ private:
     /// segment, we decrement the offset enough to the end of the previous
     /// manifest segment, so that when we re-upload segments there is no
     /// overlap.
-    void align_end_offset_to_manifest(model::offset compacted_segment_end);
+    std::optional<model::offset>
+    align_end_offset_to_manifest(model::offset compacted_segment_end);
 
     /// Finds the offset which the collection needs to progress upto in order to
     /// replace at least one manifest segment. The collection is valid if it
