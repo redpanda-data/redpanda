e3f88463aac44e028bae18c5d2be4e28256e8bdd
Oren Leiman <oren.leiman@redpanda.com>
2025-06-30 13:26:24 -0700

    s/log_reader: Add config to skip batch cache
    Sometimes we want the convenience of a batch reader without the possibility
    of reading from a stale batch cache. For example, when computing the size of
    an offset range on disk.
    
    This commit adds 'force_ignore_batch_cache' to 'log_reader_config'. If set,
    'log_segment_batch_reader' will completely skip over any batch cache access
    during reads, instead grabbing a data stream directly from the target segment.
    
    This commit also updates disk_log_impl::offset_range_size to use this config
    when constructing readers.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/storage/disk_log_impl.cc
+++ src/v/storage/disk_log_impl.cc
@@ -2411,6 +2411,7 @@ auto disk_log_impl::get_file_offset(
 
     reader_cfg.skip_batch_cache = true;
     reader_cfg.skip_readers_cache = true;
+    reader_cfg.force_ignore_batch_cache = true;
 
     auto lock = co_await s->try_read_lock();
 
