3dd8314d2776b9e555dd0c421f50be6594955b7d
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2025-07-01 15:43:55 +0100

    sr/authorization: refactor handle_authz
    Pure refactor. Introduce some helpers for reuse in the next commit.

--- src/v/pandaproxy/schema_registry/authorization.cc
+++ src/v/pandaproxy/schema_registry/authorization.cc
@@ -29,15 +29,23 @@ concept no_auth = std::is_same_v<T, auth::none>
 template<typename T>
 concept requires_auth = !no_auth<T>;
 
+struct auth_params {
+    security::acl_principal principal;
+    security::acl_host host;
+    security::acl_operation op;
+
+    auth_params(const server::request_t& rq, const auth& auth)
+      : principal{security::principal_type::user, rq.user.name}
+      , host{rq.req->get_client_address().addr()}
+      , op{auth.get_op().value_or(security::acl_operation::all)} {}
+};
+
 } // namespace detail
 
-void handle_authz(
-  const server::request_t& rq,
-  const auth& auth,
-  request_auth_result& auth_result) {
-    auth_result.pass();
+namespace {
 
-    // Extract the resource from the request
+auth::resource
+extract_resource_from_request(const server::request_t& rq, const auth& auth) {
     auto resource = auth.get_resource();
     ss::visit(
       resource,
@@ -45,34 +53,45 @@ void handle_authz(
           sub = parse::request_param<subject>(*rq.req, "subject");
       },
       [](const auto&) {});
+    return resource;
+}
 
-    security::acl_principal principal{
-      security::principal_type::user, rq.user.name};
-    security::acl_host host{rq.req->get_client_address().addr()};
-    security::acl_operation op = auth.get_op().value_or(
-      security::acl_operation::all);
+void handle_authz_result(const security::auth_result& authz_result) {
+    if (authz_result.is_authorized()) {
+        // TODO(CORE-12275): audit success
+    } else {
+        // TODO(CORE-12275): audit failure
+        throw ss::httpd::base_exception(
+          "Forbidden (missing required ACLs)",
+          ss::http::reply::status_type::forbidden);
+    }
+}
+
+} // namespace
+
+void handle_authz(
+  const server::request_t& rq,
+  const auth& auth,
+  request_auth_result& auth_result) {
+    auth_result.pass();
+
+    detail::auth_params params{rq, auth};
+
+    auto resource = extract_resource_from_request(rq, auth);
 
     // Check Authorization
     auto authz_result = ss::visit(
       resource,
       [&](const detail::requires_auth auto& resource_name) {
           return rq.service().authorizor().authorized(
-            resource_name, op, principal, host);
+            resource_name, params.op, params.principal, params.host);
       },
       [&](const detail::no_auth auto&) {
           return security::auth_result::authz_disabled(
-            principal, host, op, registry_resource{});
+            params.principal, params.host, params.op, registry_resource{});
       });
 
-    if (authz_result.is_authorized()) {
-        // TODO(CORE-12275): audit success
-    } else {
-        // TODO(CORE-12275): audit failure
-
-        throw ss::httpd::base_exception(
-          "Forbidden (missing required ACLs)",
-          ss::http::reply::status_type::forbidden);
-    }
+    handle_authz_result(authz_result);
 }
 
 } // namespace pandaproxy::schema_registry::enterprise
