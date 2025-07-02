bf97e513a7882271557a54d6a8e5f211dd6a4ae6
Ioannis Kavvadias <ioannis.kavvadias@redpanda.com>
2025-07-02 18:45:09 +0100

    k/s/group: address inconsistency in group subscription
    Compared to the reference implementation, the redpanda
    logic on whether a group is subscribed is too restrictive.
    Specifically in the fallback path, if the _subscriptions member
    is not set. We consider all such groups as subscribed.
    
    Updated fallback logic to consider the group subscribed if the
    protocol type is consumer group. Otherwise, it falls back to
    considering the group unsubscribed. This logic matches the
    reference implementation.
    
    For the full discussion see CORE-12338
    
    (cherry picked from commit 8a32acbefb61262da261cc5b882134c5e7559196)

--- tests/rptest/tests/consumer_group_test.py
+++ tests/rptest/tests/consumer_group_test.py
@@ -1628,3 +1628,70 @@ class ConsumerGroupOffsetResetTest(RedpandaTest):
             last_total = self.total_committed()
             self.logger.debug(f"Total offsets committed: {last_total}")
             time.sleep(5)
+
+    @cluster(num_nodes=3)
+    def test_offset_delete_manual_consumers(self):
+        """
+        Verify that OffsetDelete requests work as expected when consumers
+        are being manually managed.
+        """
+        # Magic value returned by confluent_kafka when it can't find commited offsets
+        INVALID_OFFSET = -1001
+
+        rpk = RpkTool(self.redpanda)
+
+        topic = TopicSpec(name="cg-test-topic-1", partition_count=1)
+        DefaultClient(self.redpanda).create_topic(topic)
+        self.group_id = "test-group-1"
+
+        self.consumer_count = 1
+        self.admin_client = AdminClient(
+            {"bootstrap.servers": self.redpanda.brokers()})
+
+        self.consumers = [
+            OffsetCommitter(
+                bootstrap_servers=self.redpanda.brokers(),
+                group=self.group_id,
+                topic=topic.name,
+                id=0,
+                logger=self.logger,
+                partition_id=0,
+            )
+        ]
+
+        self.wait_for_total_commits(1000)
+
+        # No consumers have subscribed. Group should be in empty state
+        desc = rpk.group_describe(self.group_id)
+        assert desc.state == "Empty", \
+                f"Expected group.state 'Empty' but got '{desc.state}', instead\n" \
+                f"Group description: {desc}"
+
+        # Try to delete offsets while there is an assigned consumer
+        # who keeps commiting offsets
+        res = rpk.offset_delete(self.group_id, {topic.name: [0]})[0]
+        assert res.status == "OK", \
+                f"Expected res.status 'OK' but got '{res.status}', instead\n" \
+                f"Response:  {res}"
+
+        def wait_for_new_commits():
+            tp = self.list_consumer_group_offsets(
+                topic.name).topic_partitions[0]
+            return tp.offset != INVALID_OFFSET
+
+        wait_until(
+            wait_for_new_commits,
+            timeout_sec=10,
+            backoff_sec=1,
+            err_msg="Timeout while waiting for consumer to commit more offsets"
+        )
+
+        self.consumers[0].stop()
+
+        res = rpk.offset_delete(self.group_id, {topic.name: [0]})
+        assert res[0].status == "OK", \
+                f"Expected res.status 'OK' but got '{res[0].status}', instead\n" \
+                f"Response:  {res[0]}"
+
+        tp = self.list_consumer_group_offsets(topic.name).topic_partitions[0]
+        assert tp.offset == INVALID_OFFSET, f"Expected offset '{INVALID_OFFSET}' but got '{tp.offset}', instead"
