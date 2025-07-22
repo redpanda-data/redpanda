57f04fcc55560ab578586d86f73fdc5cc1a754be
Travis Downs <travis.downs@redpanda.com>
2025-07-22 10:56:31 -0400

    treewide: remove small/large fragment_vector
    and used chunked_vector instead.
    
    chunked_vector just does it better using a dynamic fragment (first) size
    so adjusts itself to the actual number of elements, with a worst-case
    50% waste, unlike the older non-dynamic frag vector uses which always
    had a “full sized” first fragment so could have very large waste
    fractions, which have been fingered in multiple OOMs.
    
    Fixes CORE-12882.
    
    (cherry picked from commit 53bfa960266fc156ef37b982b198c913b22d9c1b)
    (cherry picked from commit 646126163b3061019bda9abd883b5587bcbb7cc3)

--- src/v/kafka/protocol/schemata/generator.py
+++ src/v/kafka/protocol/schemata/generator.py
@@ -484,10 +484,10 @@ extra_headers = {
 # These types, when they appear as the member type of an array, will override
 # the container type from std::vector
 override_member_container = {
-    'metadata_response_partition': 'large_fragment_vector',
-    'metadata_response_topic': 'small_fragment_vector',
-    'fetchable_partition_response': 'small_fragment_vector',
-    'offset_fetch_response_partition': 'small_fragment_vector',
+    'metadata_response_partition': 'chunked_vector',
+    'metadata_response_topic': 'chunked_vector',
+    'fetchable_partition_response': 'chunked_vector',
+    'offset_fetch_response_partition': 'chunked_vector',
     'int32_t': 'std::vector',
     'model::node_id': 'std::vector',
     'model::partition_id': 'std::vector',
