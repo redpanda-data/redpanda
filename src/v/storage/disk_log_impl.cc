3ddbb7aaa35e0d6b15f3b48b3f11656cd8e14497
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-07-31 10:27:58 -0400

    `storage`: bump window compaction log lines to `INFO`
    A similar log line level bump was recently done for adjacent merge compaction.
    These log lines will also be more useful at the `INFO` level while not adding
    a lot of new logging to the system.
    
    (cherry picked from commit 7740e538f846f998d863b19114d6b8030e8eeafd)

--- src/v/storage/disk_log_impl.cc
+++ src/v/storage/disk_log_impl.cc
@@ -721,7 +721,7 @@ ss::future<bool> disk_log_impl::sliding_window_compact(
         co_return has_self_compacted;
     }
     vlog(
-      gclog.debug,
+      gclog.info,
       "[{}] window compacting {} segments in interval [{}, {}]",
       config().ntp(),
       segs.size(),
@@ -1382,7 +1382,7 @@ ss::future<> disk_log_impl::do_compact(
         compact_fut = sliding_window_compact(compact_cfg, new_start_offset)
                         .then([&](bool compacted) {
                             vlog(
-                              gclog.debug,
+                              gclog.info,
                               "Sliding compaction of {} did {}compact "
                               "data, proceeding to adjacent "
                               "segment compaction",
