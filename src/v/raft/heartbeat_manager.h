e6eb604181cbe7e6dc213ebbb5da77f315836b80
joe-redpanda <joe.miller@redpanda.com>
2025-08-01 10:49:16 -0700

    raft/heartbeat_manager: race condition on _fstats
    hearbeat manager accesses two sets of collections in gathering
    heartbeat data for a given round of heartbeat RPCs.
    
    1. _consensus_groups which is owned by hbm, and protected by a lock for
       the duration of gathering heartbeats
    2. consensus::_fstats which is NOT owned by hbm and not protected by any
       locking
    
    heartbeat manager uses ssx::async_for_each_counter to gather heartbeat
    data because there are feasibly enough raft groups, and followers in
    those raft groups to either stall the reactor and or meaningfully
    increase e2e latency if all of these were computed in one synchronous
    block.
    
    As previously written, hbm may suspend in the middle of an iteration
    over an _fstats object. If consensus processes a reconfiguration
    while hbm's iteration is suspended, fstats will be mutated and the
    in-use iterator will be invalidated.
    
    This PR changes the loop over _fstats to be synchronous s.t. _fstats may
    no longer mutate while in use by hbm. To avoid reactor stalls and
    latency concerns, the outer loop is now asynchronous and set to offer a
    yield every 30 iterations. We make the assumption that each raft group
    will have approximately four followers, thus approximately preserving
    the prior logic of one yield every 100 followers.
    
    (cherry picked from commit 188ff988198f89fd013db367a0c188c03e69b061)

--- src/v/raft/heartbeat_manager.h
+++ src/v/raft/heartbeat_manager.h
@@ -150,6 +150,20 @@ private:
         absl::flat_hash_set<model::node_id> reconnect_nodes;
     };
 
+    using hb_pair
+      = std::pair<group_heartbeat, heartbeat_manager::follower_request_meta>;
+
+    // registers heartbeats for a singular raft group
+    // this is synchronous because hb_m doesn't own nor lock _fstats
+    // don't yield the cpu while iterating fstats s.t. our iterator doesn't get
+    // invalidated
+    void fetch_heartbeats_for_raft_group(
+      ss::lw_shared_ptr<consensus>& raft_group,
+      absl::node_hash_map<model::node_id, ss::chunked_fifo<hb_pair>>&
+        /*in and out*/ pending_beats,
+      absl::flat_hash_set<model::node_id>& /*in and out*/ reconnect_nodes,
+      const ss::lowres_clock::time_point& last_heartbeat);
+
     void dispatch_heartbeats();
 
     clock_type::time_point next_heartbeat_timeout();
