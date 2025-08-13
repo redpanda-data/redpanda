f9c8aba8291423781e6e679fc08b9f6bfe755e3b
Oren Leiman <oren.leiman@redpanda.com>
2025-08-13 14:53:03 -0700

    cloud_io: Wire up client_lease_timeout_ms
    Replace timeout computed from the incoming rtc node (high variance leading to
    frequent timeouts) w/ the cluster config.
    
    The overall goal is to avoid lease timeouts in the common case. We are generally
    fine with uploads taking a long time, and the knock-on effects of frequent
    timeout errors are not well understood, not to mention creating a lot of noise
    in production cloud alerts.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>
    
    cloud_io: Use lease timeout cluster config
    
    Replacing static factor of segment_upload_timeout_ms
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>
    (cherry picked from commit 81092a80a85615f58b033fd17eb0bb1ddabf5eba)

--- src/v/cloud_io/remote.cc
+++ src/v/cloud_io/remote.cc
@@ -115,7 +115,9 @@ remote::remote(
   , _cloud_storage_backend{cloud_storage_clients::
                              infer_backend_from_configuration(
                                conf, cloud_credentials_source)}
-  , _provider(infer_provider(_cloud_storage_backend, conf)) {
+  , _provider(infer_provider(_cloud_storage_backend, conf))
+  , _lease_timeout(
+      config::shard_local_cfg().cloud_storage_client_lease_timeout_ms.bind()) {
     vlog(
       log.info, "remote initialized with backend {}", _cloud_storage_backend);
     // If the credentials source is from config file, bypass the background
@@ -256,7 +258,7 @@ ss::future<upload_result> remote::upload_stream(
         }
         auto fut = co_await ss::coroutine::as_future(
           _pool.local().acquire_with_timeout(
-            fib.root_abort_source(), fib.get_timeout(), fib()));
+            fib.root_abort_source(), _lease_timeout(), fib()));
         if (fut.failed()) {
             co_return throw_if_not_timeout(
               fut.get_exception(), upload_result::timedout);
@@ -369,7 +371,7 @@ ss::future<download_result> remote::download_stream(
     auto fut = co_await [this, &fib, &transfer_details] {
         transfer_details.on_client_acquire();
         return ss::coroutine::as_future(_pool.local().acquire_with_timeout(
-          fib.root_abort_source(), fib.get_timeout(), fib()));
+          fib.root_abort_source(), _lease_timeout(), fib()));
     }();
     if (fut.failed()) {
         co_return throw_if_not_timeout(
@@ -492,7 +494,7 @@ remote::download_object(download_request download_request) {
 
     auto fut = co_await ss::coroutine::as_future(
       _pool.local().acquire_with_timeout(
-        fib.root_abort_source(), fib.get_timeout(), fib()));
+        fib.root_abort_source(), _lease_timeout(), fib()));
     if (fut.failed()) {
         co_return throw_if_not_timeout(
           fut.get_exception(), download_result::timedout);
@@ -583,7 +585,7 @@ ss::future<download_result> remote::object_exists(
     retry_chain_logger ctxlog(log, fib);
     auto fut = co_await ss::coroutine::as_future(
       _pool.local().acquire_with_timeout(
-        fib.root_abort_source(), fib.get_timeout(), fib()));
+        fib.root_abort_source(), _lease_timeout(), fib()));
     if (fut.failed()) {
         co_return throw_if_not_timeout(
           fut.get_exception(), download_result::timedout);
@@ -660,7 +662,7 @@ remote::delete_object(transfer_details transfer_details) {
     retry_chain_logger ctxlog(log, fib);
     auto fut = co_await ss::coroutine::as_future(
       _pool.local().acquire_with_timeout(
-        fib.root_abort_source(), fib.get_timeout(), fib()));
+        fib.root_abort_source(), _lease_timeout(), fib()));
     if (fut.failed()) {
         co_return throw_if_not_timeout(
           fut.get_exception(), upload_result::timedout);
@@ -820,7 +822,7 @@ ss::future<upload_result> remote::delete_object_batch(
     retry_chain_logger ctxlog(log, fib);
     auto fut = co_await ss::coroutine::as_future(
       _pool.local().acquire_with_timeout(
-        fib.root_abort_source(), fib.get_timeout(), fib()));
+        fib.root_abort_source(), _lease_timeout(), fib()));
     if (fut.failed()) {
         co_return throw_if_not_timeout(
           fut.get_exception(), upload_result::timedout);
@@ -1011,7 +1013,7 @@ ss::future<list_result> remote::list_objects(
     retry_chain_logger ctxlog(log, fib);
     auto fut = co_await ss::coroutine::as_future(
       _pool.local().acquire_with_timeout(
-        fib.root_abort_source(), fib.get_timeout(), fib()));
+        fib.root_abort_source(), _lease_timeout(), fib()));
     if (fut.failed()) {
         co_return throw_if_not_timeout(
           fut.get_exception(), cloud_storage_clients::error_outcome::retry);
@@ -1141,7 +1143,7 @@ ss::future<upload_result> remote::upload_object(upload_request upload_request) {
     while (!_gate.is_closed() && permit.is_allowed && !result) {
         auto fut = co_await ss::coroutine::as_future(
           _pool.local().acquire_with_timeout(
-            fib.root_abort_source(), fib.get_timeout(), fib()));
+            fib.root_abort_source(), _lease_timeout(), fib()));
         if (fut.failed()) {
             co_return throw_if_not_timeout(
               fut.get_exception(), upload_result::timedout);
