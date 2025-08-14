4dfd494f258824359fa0019bdf0b0fab0b06d58e
Tyler Rockwood <rockwood@redpanda.com>
2025-08-14 03:15:37 +0000

    rptest: bump protobuf to non yanked version

--- tests/setup.py
+++ tests/setup.py
@@ -20,7 +20,7 @@ setup(
         'confluent-kafka==2.6.1',
         'zstandard==0.23.0',
         'xxhash==3.5.0',
-        'protobuf==5.29.0',
+        'protobuf==5.29.5',
         'fastavro==1.11.1',
         'falcon==4.0.2',
         'psutil==5.9.0',
