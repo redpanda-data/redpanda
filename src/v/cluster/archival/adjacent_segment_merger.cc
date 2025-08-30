5edb5ce8339427928d3066922ea0b731cf826198
Oren Leiman <oren.leiman@redpanda.com>
2025-08-29 17:15:43 -0700

    archival: adjacent_segment_merger jobs should scan from the last upload
    Fixes a bug where in all runs but the very first, the adjacent segment merger
    job would override the '_last' offset (end offset of the last successful upload)
    with the manifest start offset, mistakenly treating every run like a remote
    segment merge run, which we never do anyway.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>
    (cherry picked from commit e359b0373e56b43930f0a0f8d457510eda5ac6b9)

--- src/v/cluster/archival/adjacent_segment_merger.cc
+++ src/v/cluster/archival/adjacent_segment_merger.cc
@@ -89,7 +89,7 @@ std::optional<adjacent_segment_run> adjacent_segment_merger::scan_manifest(
         so = std::max(
           manifest.get_start_offset().value_or(local_start_offset),
           local_start_offset);
-    } else {
+    } else if (!_is_local) {
         // Remote lookup, start from start offset in the manifest (or 0)
         so = _archiver.manifest().get_start_offset().value_or(model::offset{0});
     }
