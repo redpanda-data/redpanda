3644b7f75a0b1017e322258750907a6b8bbb943d
Brandon Allard <brandon@redpanda.com>
2025-09-13 17:05:41 -0400

    kafka/server: add response_size method to op_context::response_placeholder

--- src/v/kafka/server/handlers/fetch.h
+++ src/v/kafka/server/handlers/fetch.h
@@ -68,6 +68,12 @@ struct op_context {
         }
         intrusive_list_hook _hook;
 
+        size_t response_size() {
+            return _it->partition_response->records
+              .transform(&kafka::batch_reader::size_bytes)
+              .value_or(0);
+        }
+
     private:
         fetch_response::iterator _it;
         op_context* _ctx;
