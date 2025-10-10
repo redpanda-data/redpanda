4c5db5a4d8ec79c0d63d1ec1f9ca41070ac04d3f
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-10-10 13:01:54 -0400

    `compaction`: add `expired_tombstones_discarded` to `stats`
    To improve visibility into tombstone removal due to `delete.retention.ms`.

--- src/v/compaction/types.h
+++ src/v/compaction/types.h
@@ -123,6 +123,9 @@ struct stats {
     // Number of transactional control batches that were removed.
     // This is only relevant for local storage compaction.
     size_t control_batches_discarded{0};
+    // Number of tombstone records that were removed due to expiration (not
+    // including those removed by de-duplication)
+    size_t expired_tombstones_discarded{0};
 
     // The starting offset of the range processed by this reducer.
     std::optional<model::offset> start_offset{std::nullopt};
@@ -132,7 +135,8 @@ struct stats {
     // Returns whether any data was removed by this reducer.
     bool has_removed_data() const {
         return batches_discarded > 0 || records_discarded > 0
-               || control_batches_discarded > 0;
+               || control_batches_discarded > 0
+               || expired_tombstones_discarded > 0;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const stats& s);
