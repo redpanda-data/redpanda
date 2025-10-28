355fe7651df7df8ea363c5a40777d913bae36643
Andrew Wong <awong@redpanda.com>
2025-10-28 16:30:12 -0700

    ct: make L1 GC interval configurable

--- src/v/cloud_topics/level_one/domain/domain_manager.cc
+++ src/v/cloud_topics/level_one/domain/domain_manager.cc
@@ -13,6 +13,7 @@
 #include "cloud_topics/level_one/metastore/rpc_types.h"
 #include "cloud_topics/level_one/metastore/simple_metastore.h"
 #include "cloud_topics/logger.h"
+#include "config/configuration.h"
 #include "model/batch_builder.h"
 #include "ssx/future-util.h"
 #include "ssx/sleep_abortable.h"
@@ -521,13 +522,17 @@ domain_manager::remove_topics(rpc::remove_topics_request req) {
     };
 }
 
+ss::lowres_clock::duration domain_manager::gc_interval() const {
+    return config::shard_local_cfg()
+      .cloud_topics_level_one_garbage_collection_interval();
+}
+
 ss::future<> domain_manager::gc_loop() {
     auto gate = maybe_gate();
     if (!gate.has_value()) {
         co_return;
     }
     // TODO: make configurable.
-    auto gc_interval = 5min;
     garbage_collector gc(stm_.get(), object_io_);
     while (!as_.abort_requested()) {
         vlog(cd_log.debug, "Running garbage collection now...");
@@ -535,10 +540,13 @@ ss::future<> domain_manager::gc_loop() {
         if (!gc_res.has_value()) {
             vlog(cd_log.warn, "Garbage collection failed: {}", gc_res.error());
         }
+        auto sleep_interval = gc_interval();
         vlog(
-          cd_log.debug, "Re-running garbage collection in {}...", gc_interval);
+          cd_log.debug,
+          "Re-running garbage collection in {}...",
+          sleep_interval);
         auto sleep_res = co_await ss::coroutine::as_future(
-          ssx::sleep_abortable(gc_interval, as_));
+          ssx::sleep_abortable(sleep_interval, as_));
         if (sleep_res.failed()) {
             auto eptr = sleep_res.get_exception();
             auto log_lvl = ssx::is_shutdown_exception(eptr)
--- src/v/config/configuration.h
+++ src/v/config/configuration.h
@@ -815,6 +815,8 @@ public:
     property<size_t> cloud_topics_produce_cardinality_threshold;
     property<bool> cloud_topics_disable_reconciliation_loop;
     property<std::chrono::milliseconds> cloud_topics_reconciliation_interval;
+    property<std::chrono::milliseconds>
+      cloud_topics_level_one_garbage_collection_interval;
 
     development_feature_property<int> development_feature_property_testing_only;
 
