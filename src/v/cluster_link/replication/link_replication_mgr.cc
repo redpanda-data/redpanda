bb71bae740323600f6bf4aba21e968d20c5e0f86
Bharath Vissapragada <bharathv@redpanda.com>
2025-10-30 10:30:20 -0700

    Revert "attempt one"
    This reverts commit 89a86772987956c2a279bdcaa086e2e42203c452.

--- src/v/cluster_link/replication/link_replication_mgr.cc
+++ src/v/cluster_link/replication/link_replication_mgr.cc
@@ -30,6 +30,14 @@ link_replication_manager::link_replication_manager(
   , _config_provider(std::move(config_provider))
   , _source_factory(std::move(source_factory))
   , _sink_factory(std::move(sink_factory))
+  , _queue(
+      _sg,
+      [](const std::exception_ptr& ex) {
+          vlog(
+            cllog.error,
+            "unexpected error in processing notifications: {}",
+            ex);
+      })
   , _cfg_probe{std::move(cfg_probe)}
   , _link_data_probe{nullptr} {}
 
@@ -57,19 +65,19 @@ ss::future<> link_replication_manager::stop() {
     vlog(cllog.trace, "Stopping link replication manager");
     // to avoid further submissions to the queue.
     _as.request_abort();
-    _pending_cv.broken();
+    _pending_changes_cv.broken();
     auto gate_f = _gate.close();
-    // no new replicators can be added after gate is closed
+    // no new replicators can be added / removed past this point.
+    chunked_vector<ss::future<>> stop_futures;
+    stop_futures.reserve(_replicators.size());
+    // stop any pending replicators
     for (auto& [_, replicator] : _replicators) {
-        replicator->initiate_shutdown();
+        stop_futures.push_back(replicator->stop());
     }
+    co_await _queue.shutdown();
+    co_await ss::when_all_succeed(stop_futures.begin(), stop_futures.end());
+    _replicators.clear();
     co_await std::move(gate_f);
-    vlog(cllog.debug, "Gate closed");
-    // wait for all replicators to stop
-    for (auto& [ntp, replicator] : _replicators) {
-        vlog(cllog.debug, "Stopping replicator for {}", ntp);
-        co_await replicator->stop();
-    }
     co_await _source_factory->stop();
     unset_data_probe();
     vlog(cllog.trace, "Link replication manager stopped");
@@ -90,45 +98,44 @@ void link_replication_manager::unset_data_probe() {
     }
 }
 
-std::ostream&
-operator<<(std::ostream& os, link_replication_manager::op_type op) {
-    switch (op) {
-    case link_replication_manager::op_type::start:
-        return os << "start";
-    case link_replication_manager::op_type::stop:
-        return os << "stop";
-    default:
-        return os << "unknown";
+ss::future<> link_replication_manager::do_start_replicator(
+  model::ntp ntp, model::term_id term) {
+    if (_as.abort_requested()) {
+        co_return;
     }
-}
-
-fmt::iterator
-link_replication_manager::ntp_target_state::format_to(fmt::iterator out) const {
-    return fmt::format_to(out, "{{op={}, term: {}}}", op, term);
-}
-
-bool link_replication_manager::ntp_target_state::merge(
-  const ::model::ntp& ntp, ntp_target_state new_desired) {
-    auto can_merge = !new_desired.term || !term || new_desired.term >= term;
-    if (!can_merge) {
-        vlog(
-          cllog.debug,
-          "Ignoring attempt to change desired state for {} from {} to {} as "
-          "it has a lower term",
+    auto holder = _gate.hold();
+    vlog(cllog.debug, "Starting replicator for {}", ntp);
+    auto it = _replicators.find(ntp);
+    if (it != _replicators.end()) {
+        if (term == it->second->term()) {
+            // duplicate start requests are possible when starting replicators
+            // outside of leader notifications (eg: unpausing a link)
+            co_return;
+        }
+        // Mismatching terms for the same ntp should never happen
+        // it means we are trying to start a replicator for an ntp that
+        // that has not been stopped yet.
+        vassert(
+          false,
+          "Attempting to start replicator for {} in term {} but it "
+          "already exists in term {}",
           ntp,
-          *this,
-          new_desired);
-        return false;
+          term,
+          it->second->term());
     }
-    vlog(
-      cllog.debug,
-      "Merging desired state for {} from {} to {}",
+    auto source = _source_factory->make_source(ntp);
+    auto sink = _sink_factory->make_sink(ntp);
+    auto replicator = std::make_unique<partition_replicator>(
       ntp,
-      *this,
-      new_desired);
-    op = new_desired.op;
-    term = new_desired.term;
-    return true;
+      term,
+      *_config_provider,
+      std::move(source),
+      std::move(sink),
+      _sg,
+      _cfg_probe,
+      _link_data_probe);
+    auto [r_it, _] = _replicators.emplace(ntp, std::move(replicator));
+    co_await r_it->second->start();
 }
 
 void link_replication_manager::start_replicator(
@@ -136,10 +143,63 @@ void link_replication_manager::start_replicator(
     if (_gate.is_closed()) {
         return;
     }
-    vlog(cllog.debug, "Enqueuing start action for {} term {}", ntp, term);
-    auto& rec_state = _pending[ntp];
-    rec_state.set_desired(ntp, {.op = op_type::start, .term = term});
-    _pending_cv.signal();
+
+    // If we are starting replication, check to see if there is a pending stop.
+    // If one exists and its term is higher than ours, then do not enqueue the
+    // start action, otherwise remove the stop action
+    auto stop_it = _pending_stops.find(ntp);
+    if (stop_it != _pending_stops.end()) {
+        if (stop_it->second.has_value() && term <= *stop_it->second) {
+            vlog(
+              cllog.debug,
+              "Not enqueueing start action for {} at term {}, a stop is "
+              "pending with term {}",
+              ntp,
+              term,
+              stop_it->second);
+            return;
+        }
+    }
+
+    // Now check if there are any pending starts.  If there are, replace the
+    // start only if the term is higher
+    auto start_it = _pending_starts.find(ntp);
+    if (start_it != _pending_starts.end()) {
+        if (term <= start_it->second) {
+            vlog(
+              cllog.debug,
+              "Not enqueueing start action for {} at term {}, a start is "
+              "pending with term {}",
+              ntp,
+              term,
+              start_it->second);
+            return;
+        }
+    }
+    vlog(cllog.debug, "Enqueueing start action for {} at term {}", ntp, term);
+    _pending_starts.insert_or_assign(ntp, term);
+    _pending_changes_cv.signal();
+}
+
+ss::future<> link_replication_manager::do_stop_replicator(
+  model::ntp ntp, std::optional<model::term_id> term) {
+    if (_as.abort_requested()) {
+        co_return;
+    }
+    auto holder = _gate.hold();
+    vlog(cllog.debug, "Stopping replicator for {}", ntp);
+    auto it = _replicators.find(ntp);
+    if (it == _replicators.end() || (term && (*term < it->second->term()))) {
+        vlog(
+          cllog.trace,
+          "No replicator found for {} at term {}, skipping stop",
+          ntp,
+          term);
+        co_return;
+    }
+    auto replicator = std::move(it->second);
+    _replicators.erase(it);
+    co_await replicator->stop();
 }
 
 void link_replication_manager::stop_replicator(
@@ -147,22 +207,42 @@ void link_replication_manager::stop_replicator(
     if (_gate.is_closed()) {
         return;
     }
-    vlog(cllog.debug, "Enqueuing stop action for {} term {}", ntp, term);
-    auto& rec_state = _pending[ntp];
-    auto result = rec_state.set_desired(
-      ntp, {.op = op_type::stop, .term = term});
-    if (result) {
-        vlog(
-          cllog.debug,
-          "Desired state for {} set to stop with term {}, initiating shutdown",
-          ntp,
-          term);
-        auto it = _replicators.find(ntp);
-        if (it != _replicators.end()) {
-            it->second->initiate_shutdown();
+
+    // If we are stopping replication, we need to check if there are any pending
+    // starting or stopping operations.  If there are pending starts and the
+    // term is higher or not present, enqueue the stop and remove the start
+    auto start_it = _pending_starts.find(ntp);
+    if (start_it != _pending_starts.end()) {
+        if (term.has_value() && *term < start_it->second) {
+            vlog(
+              cllog.debug,
+              "Not enqueueing stop action for {} at term {}",
+              ntp,
+              term);
+            return;
+        }
+        vlog(cllog.debug, "Removing pending start action for {}", ntp);
+        _pending_starts.erase(start_it);
+    }
+
+    auto stop_it = _pending_stops.find(ntp);
+    if (stop_it != _pending_stops.end()) {
+        if (
+          term.has_value() && stop_it->second.has_value()
+          && *term <= *stop_it->second) {
+            vlog(
+              cllog.debug,
+              "Not enqueueing stop action for {} at term {}, a stop is "
+              "pending with term {}",
+              ntp,
+              term,
+              stop_it->second);
+            return;
         }
     }
-    _pending_cv.signal();
+    vlog(cllog.debug, "Enqueuing stop action for {} at term {}", ntp, term);
+    _pending_stops.insert_or_assign(ntp, term);
+    _pending_changes_cv.signal();
 }
 
 void link_replication_manager::stop_replicators(
@@ -198,152 +278,82 @@ link_replication_manager::get_partition_offsets_report(
     return it->second->get_partition_offsets_report();
 }
 
-bool link_replication_manager::can_reconcile_now() {
-    auto has_pending_actions = std::ranges::any_of(
-      _pending, [](const auto& pair) {
-          const auto& [_, rec_state] = pair;
-          return rec_state.needs_reconciliation()
-                 || rec_state.reconciliation_complete();
-      });
-    vlog(
-      cllog.debug,
-      "Checking for pending actions: {}, available units: {}",
-      has_pending_actions ? "found" : "none found",
-      _max_reconciliations.available_units());
-    return has_pending_actions && _max_reconciliations.available_units() > 0;
+bool link_replication_manager::has_pending_actions() {
+    return !_pending_starts.empty() || !_pending_stops.empty();
 }
 
 ss::future<> link_replication_manager::reconcile() {
-    co_await _pending_cv.wait([this] { return can_reconcile_now(); });
+    co_await _pending_changes_cv.wait([this] { return has_pending_actions(); });
     if (_gate.is_closed()) {
         co_return;
     }
-    vlog(
-      cllog.debug,
-      "Starting reconciliation loop, pending actions: {}",
-      _pending.size());
-    // cleanup completed reconciliations
-    for (auto it = _pending.begin(); it != _pending.end();) {
-        if (it->second.reconciliation_complete()) {
-            it = _pending.erase(it);
-        } else {
-            ++it;
-        }
-    }
-    // launch new reconciliations
-    for (auto& [ntp, rec_state] : _pending) {
-        if (!rec_state.needs_reconciliation()) {
-            continue;
-        }
-        auto units = ss::try_get_units(_max_reconciliations, 1);
-        if (!units) {
-            // try when available.
-            co_return;
-        }
-        ssx::spawn_with_gate(
-          _gate, [this, ntp, u = std::move(units.value())]() mutable {
-              return reconcile_ntp_once(std::move(ntp), std::move(u));
-          });
-    }
+    run_stop_actions();
+    run_start_actions();
 }
 
-ss::future<> link_replication_manager::reconcile_ntp_once(
-  ::model::ntp ntp, ssx::semaphore_units units) {
-    auto it = _pending.find(ntp);
-    if (it == _pending.end()) {
-        co_return;
+void link_replication_manager::run_start_actions() {
+    for (auto& [ntp, term] : _pending_starts) {
+        vlog(cllog.trace, "Starting {} term {}", ntp, term);
+        _queue.submit([this, ntp = std::move(ntp), term]() mutable {
+            return do_start_replicator(ntp, term).handle_exception(
+              [ntp = std::move(ntp),
+               term](const std::exception_ptr& e) mutable {
+                  vlog(
+                    cllog.warn,
+                    "Failed to start replicator for {} at term {}: {}",
+                    ntp,
+                    term,
+                    e);
+              });
+        });
     }
-    auto& state = it->second;
-    vlog(cllog.debug, "Reconciling ntp {}", ntp);
-    vassert(
-      state.needs_reconciliation(),
-      "Attempting to reconcile ntp {} which is already in progress",
-      ntp);
-    // mark it in progress
-    // this is the target state for this iteration of reconciliation
-    state.in_progress = std::exchange(state.desired, std::nullopt);
-    auto target_state = state.in_progress.value();
-    auto replicator_it = _replicators.find(ntp);
-    auto has_running_replicator = replicator_it != _replicators.end();
-    if (has_running_replicator) {
-        // check if it needs to be stopped first
-        auto replicator_term = replicator_it->second->term();
-        auto needs_stop = false;
-        switch (target_state.op) {
-        case op_type::start:
-            // if there is a start from higher term, we nee to restart
-            needs_stop = target_state.term
-                         && (replicator_term < *target_state.term);
-            break;
-        case op_type::stop:
-            // If the stop is from higher or equal term, we need to stop
-            needs_stop = !target_state.term
-                         || (replicator_term <= *target_state.term);
-            break;
-        }
-        if (needs_stop) {
-            auto stop_it = _replicators.extract(replicator_it);
-            try {
-                co_await stop_it.second->stop();
-            } catch (const std::exception& e) {
+    _pending_starts.clear();
+}
+
+void link_replication_manager::run_stop_actions() {
+    for (auto& [ntp, term] : _pending_stops) {
+        vlog(cllog.trace, "Stopping {} term {}", ntp, term);
+        _queue.submit([this, ntp = std::move(ntp), term]() mutable {
+            auto it = _replicators.find(ntp);
+            if (it != _replicators.end()) {
+                auto replicator_term = it->second->term();
+                if (!term || term == replicator_term) {
+                    vlog(
+                      cllog.trace,
+                      "Initiating stop for {} at term {}",
+                      ntp,
+                      term);
+                    it->second->initiate_shutdown();
+                } else {
+                    vlog(
+                      cllog.trace,
+                      "Not initiating stop for {} at term {} as replicator is "
+                      "at term {}, enqueuing stop request",
+                      ntp,
+                      term,
+                      replicator_term);
+                }
+            } else {
                 vlog(
-                  cllog.warn,
-                  "Error stopping replicator for {}: {}, moving on",
+                  cllog.trace,
+                  "No replicator found for {} at term {}, enqueing stop "
+                  "request",
                   ntp,
-                  e);
+                  term);
             }
-            has_running_replicator = false;
-        }
+            return do_stop_replicator(ntp, term).handle_exception(
+              [ntp = std::move(ntp),
+               term](const std::exception_ptr& e) mutable {
+                  vlog(
+                    cllog.error,
+                    "Failed to stop replicator for {} at term {}: {}",
+                    ntp,
+                    term,
+                    e);
+              });
+        });
     }
-    // Check if we need to start a replicator
-    if (
-      !has_running_replicator && target_state.op == op_type::start
-      && !_gate.is_closed()) {
-        vassert(
-          !_replicators.contains(ntp), "Replicator for {} already exists", ntp);
-        vassert(
-          target_state.term.has_value(),
-          "Cannot start replicator for {} without a term",
-          ntp);
-        auto term = target_state.term.value();
-        auto source = _source_factory->make_source(ntp);
-        auto sink = _sink_factory->make_sink(ntp);
-        auto replicator = std::make_unique<partition_replicator>(
-          ntp,
-          term,
-          *_config_provider,
-          std::move(source),
-          std::move(sink),
-          _sg,
-          _cfg_probe,
-          _link_data_probe);
-        auto [r_it, _] = _replicators.emplace(ntp, std::move(replicator));
-        try {
-            co_await r_it->second->start();
-        } catch (const std::exception& e) {
-            vlog(cllog.warn, "Error starting replicator for {}: {}", ntp, e);
-            auto r_it = _replicators.find(ntp);
-            if (r_it != _replicators.end()) {
-                // this initiates a step down and a subsequent stop
-                r_it->second->notify_sink_on_failure(term);
-            }
-        }
-    }
-    it = _pending.find(ntp);
-    // State should not have been removed during reconciliation
-    vassert(it != _pending.end(), "Pending state for {} missing", ntp);
-    // Only place that can modify in_progress is this function
-    vassert(
-      it->second.in_progress == target_state,
-      "In-progress state for {} changed during reconciliation, expected {}, "
-      "got {}",
-      ntp,
-      target_state,
-      it->second.in_progress);
-    it->second.in_progress.reset();
-    units.return_all();
-    _pending_cv.signal();
-    vlog(cllog.debug, "Completed reconciliation for ntp {}", ntp);
+    _pending_stops.clear();
 }
 
 ss::future<> link_replication_manager::maybe_sync_start_offsets() {
--- src/v/cluster_link/replication/link_replication_mgr.h
+++ src/v/cluster_link/replication/link_replication_mgr.h
@@ -60,48 +60,23 @@ private:
     ss::future<> do_start_replicator(::model::ntp, ::model::term_id);
     ss::future<>
       do_stop_replicator(::model::ntp, std::optional<::model::term_id>);
-    bool can_reconcile_now();
+    bool has_pending_actions();
     ss::future<> reconcile();
-    ss::future<> reconcile_ntp_once(::model::ntp ntp, ssx::semaphore_units);
 
-    enum class op_type : uint8_t { start, stop };
-    friend std::ostream& operator<<(std::ostream& os, op_type);
-    struct ntp_target_state {
-        op_type op;
-        std::optional<::model::term_id> term;
-        fmt::iterator format_to(fmt::iterator) const;
-        bool merge(const ::model::ntp&, ntp_target_state new_desired);
-        bool operator==(const ntp_target_state& other) const = default;
-    };
+    void run_start_actions();
+    void run_stop_actions();
 
-    struct ntp_reconciliation_state {
-        std::optional<ntp_target_state> in_progress;
-        std::optional<ntp_target_state> desired;
-
-        bool
-        set_desired(const ::model::ntp& ntp, ntp_target_state new_desired) {
-            if (desired) {
-                return desired->merge(ntp, new_desired);
-            }
-            desired = new_desired;
-            return true;
-        }
-
-        bool needs_reconciliation() const { return desired && !in_progress; }
-        bool reconciliation_complete() const {
-            return !desired && !in_progress;
-        }
-    };
-
-    chunked_hash_map<::model::ntp, ntp_reconciliation_state> _pending;
-    ss::condition_variable _pending_cv;
-    ssx::semaphore _max_reconciliations{32, "link-replicator-mgr"};
-
+private:
     ss::scheduling_group _sg;
     std::unique_ptr<link_configuration_provider> _config_provider;
     std::unique_ptr<data_source_factory> _source_factory;
     std::unique_ptr<data_sink_factory> _sink_factory;
     ss::future<> maybe_sync_start_offsets();
+    ssx::work_queue _queue;
+    chunked_hash_map<::model::ntp, ::model::term_id> _pending_starts;
+    chunked_hash_map<::model::ntp, std::optional<::model::term_id>>
+      _pending_stops;
+    ss::condition_variable _pending_changes_cv;
     chunked_hash_map<::model::ntp, std::unique_ptr<partition_replicator>>
       _replicators;
     std::optional<replication_probe::configuration> _cfg_probe;
