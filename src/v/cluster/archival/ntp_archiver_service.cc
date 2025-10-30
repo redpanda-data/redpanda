ac75d89f5b774ee23312dfd233983ccde070ad47
Evgeny Lazin <4lazin@gmail.com>
2025-10-30 07:32:58 -0400

    ts: Fail housekeeping if we failed to GC
    If the metadata is not replicated or applied in time we shouldn't
    proceed to spillover.
    
    Signed-off-by: Evgeny Lazin <4lazin@gmail.com>
    (cherry picked from commit a6058cc31ea17174a8a1e70c26bb401e3858af83)

--- src/v/cluster/archival/ntp_archiver_service.cc
+++ src/v/cluster/archival/ntp_archiver_service.cc
@@ -2816,6 +2816,10 @@ ss::future<> ntp_archiver::garbage_collect_archive() {
               _rtclog.info,
               "Failed to clean up metadata after garbage collection: {}",
               error);
+            throw std::runtime_error(fmt_with_ctx(
+              fmt::format,
+              "Failed to clean up metadata after archive GC: {}",
+              error));
         } else {
             std::ignore = co_await _remote.delete_objects(
               get_bucket_name(), manifests_to_remove, fib);
@@ -3268,6 +3272,9 @@ ss::future<> ntp_archiver::garbage_collect() {
               _rtclog.info,
               "Failed to clean up metadata after garbage collection: {}",
               error);
+
+            throw std::runtime_error(fmt_with_ctx(
+              fmt::format, "Failed to clean up metadata after GC: {}", error));
         }
     } else {
         vlog(
