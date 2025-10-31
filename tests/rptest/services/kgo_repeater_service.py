5d11e2958f92b14483adabd8f07a67f20a45487e
Andrew Wong <awong@redpanda.com>
2025-10-31 09:12:57 -0700

    kgo_repeater_services: bump start timeout
    I saw it take 7 seconds to start up with CDT. Bumps it to a conservative
    30 seconds.

--- tests/rptest/services/kgo_repeater_service.py
+++ tests/rptest/services/kgo_repeater_service.py
@@ -194,7 +194,7 @@ class KgoRepeaterService(Service):
 
         wait_until(
             lambda: self._is_ready(node),
-            timeout_sec=10,
+            timeout_sec=30,
             backoff_sec=0.5,
             err_msg=f"Timed out waiting for status endpoint {self.who_am_i()} to be available",
         )
