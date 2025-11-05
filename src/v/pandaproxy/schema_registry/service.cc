577be50588c3b5adf1f11830e7718fb3beb400e2
Michael Boquard <michael@redpanda.com>
2025-11-05 16:59:52 -0500

    sr: Create acls on unknown topic error
    If an unknown_topic_or_partition error is received and ephemeral
    credentials are in use, then it is likely that it is a permission issue
    and not that the topic does not actually exist.  Perform ACL creation in
    this situation.
    
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- src/v/pandaproxy/schema_registry/service.cc
+++ src/v/pandaproxy/schema_registry/service.cc
@@ -451,9 +451,21 @@ ss::future<> service::mitigate_error(std::exception_ptr eptr) {
             return ss::make_exception_future<>(eptr);
         })
       .handle_exception_type([this,
+                              eptr](const kafka::client::partition_error& ex) {
+          if (
+            (ex.error == kafka::error_code::topic_authorization_failed
+             || ex.error == kafka::error_code::unknown_topic_or_partition)
+            && _has_ephemeral_credentials) {
+              return create_acls(_controller->get_security_frontend().local());
+          }
+
+          return ss::make_exception_future<>(eptr);
+      })
+      .handle_exception_type([this,
                               eptr](const kafka::client::topic_error& ex) {
           if (
-            ex.error == kafka::error_code::topic_authorization_failed
+            (ex.error == kafka::error_code::topic_authorization_failed
+             || ex.error == kafka::error_code::unknown_topic_or_partition)
             && _has_ephemeral_credentials) {
               return create_acls(_controller->get_security_frontend().local());
           }
