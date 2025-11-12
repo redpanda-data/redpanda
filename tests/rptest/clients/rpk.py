ec22e735358f184f15b285cf030db5b86040b32c
Andrew Nguyen <andrew.nguyen@redpanda.com>
2025-11-12 10:52:41 -0600

    rptest: Backporting rpk.py updates
    Backporting one of the updates made to rpk.py in the following commit:
    https://github.com/redpanda-data/redpanda/commit/a273f54b22ec8530826f1760437c32a956bd488a

--- tests/rptest/clients/rpk.py
+++ tests/rptest/clients/rpk.py
@@ -661,7 +661,7 @@ class RpkTool:
         lines = output.splitlines()
         for i, line in enumerate(lines):
             if line.split() == ["NAME", "PARTITIONS", "REPLICAS"]:
-                return map(topic_line, lines[i + 1 :])
+                return list(map(topic_line, lines[i + 1 :]))
 
         assert False, "Unexpected output format"
 
