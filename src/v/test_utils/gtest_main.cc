d3f585ea9e8765771d59cce0e69d7a352f677f0c
Tyler Rockwood <rockwood@redpanda.com>
2025-11-14 03:36:32 +0000

    gtest: give stacktraces on sigill
    Use absl's signal handler in gtest in order to give proper backtraces.
    We don't use crash_tracker because it's undesirable to bring in a bunch
    of dependencies for such a low level thing. If someone wanted to extract
    out just the backtrace stuff for tests from crash_tracker, I'd accept
    it, but for now this will do.

--- src/v/test_utils/gtest_main.cc
+++ src/v/test_utils/gtest_main.cc
@@ -8,14 +8,35 @@
  * the Business Source License, use of this software will be governed
  * by the Apache License, Version 2.0
  */
+#include "absl/debugging/failure_signal_handler.h"
 #include "test_utils/gtest_utils.h"
 
+#include <seastar/core/smp.hh>
 #include <seastar/core/thread.hh>
 #include <seastar/testing/test_runner.hh>
+
+#include <csignal>
+
+namespace {
+
+seastar::future<> unblock_sigill() {
+    // A workaround until https://github.com/scylladb/seastar/pull/3102 lands
+    return seastar::smp::invoke_on_all([] {
+        ::sigset_t set;
+        ::sigemptyset(&set);
+        ::sigaddset(&set, SIGILL);
+        auto r = ::pthread_sigmask(SIG_UNBLOCK, &set, nullptr);
+        ss::throw_pthread_error(r);
+    });
+}
+
+} // namespace
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     GTEST_FLAG_SET(death_test_style, "threadsafe");
+    // Fall back signal handlers if our crash tracker isn't registered
+    absl::InstallFailureSignalHandler({});
 
     auto& listeners = ::testing::UnitTest::GetInstance()->listeners();
     listeners.Append(new rp_test_listener());
@@ -34,8 +55,10 @@ int main(int argc, char** argv) {
     seastar::testing::global_test_runner().start(argc, argv);
 
     int ret = 0;
-    seastar::testing::global_test_runner().run_sync(
-      [&ret] { return seastar::async([&ret] { ret = RUN_ALL_TESTS(); }); });
+    seastar::testing::global_test_runner().run_sync([&ret] {
+        return unblock_sigill().then(
+          [&ret] { return seastar::async([&ret] { ret = RUN_ALL_TESTS(); }); });
+    });
 
     int ss_ret = seastar::testing::global_test_runner().finalize();
     if (ret) {
