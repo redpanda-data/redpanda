288bbf20ef0df5539be91c2340a65ad506aac3df
joe-redpanda <joe.miller@redpanda.com>
2025-11-14 13:56:13 -0800

    shard_placement_table: unchecked optional fix
    shard placement table test will fail periodically on bad variant access.
    assigned may be set to nullopt concurrently just as with the other
    waiting_for_shard_placement_update returning checks.
    
    This commit adds an optional validity check before using the optional
    value with the appropriate error code
    
    (cherry picked from commit d958a1ba99d5ae3a9e8a74afc0d58929e483df63)

--- src/v/cluster/shard_placement_table.cc
+++ src/v/cluster/shard_placement_table.cc
@@ -959,6 +959,11 @@ ss::future<std::error_code> shard_placement_table::prepare_create(
     }
     auto& state = state_it->second;
 
+    if (!state.assigned()) {
+        // assignments got updated while we were waiting for the lock
+        co_return errc::waiting_for_shard_placement_update;
+    }
+
     if (state.assigned()->log_revision != expected_log_rev) {
         // assignments got updated while we were waiting for the lock
         co_return errc::waiting_for_shard_placement_update;
@@ -1404,6 +1409,11 @@ ss::future<std::error_code> shard_placement_table::set_remake_state(
 
     auto& state = it->second;
     auto& current = it->second._current.value();
+
+    if (!state.assigned()) {
+        // assignments got updated while we were waiting for the lock
+        co_return errc::waiting_for_shard_placement_update;
+    }
 
     if (state.assigned()->log_revision != expected_log_rev) {
         // assignments got updated while we were waiting for the lock
