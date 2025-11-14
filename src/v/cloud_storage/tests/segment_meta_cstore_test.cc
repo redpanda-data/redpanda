e76231b1981b33c243babe2f0f6171e73e45d5ab
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-11-13 23:07:44 -0500

    `cloud_storage`: tweak `segment_meta_cstore_test` parameters
    This test was often timing out in CI. Tweak test parameters (such as
    test size and what was potentially a very large random value for `size_bytes`)
    and bump its timeout to `moderate` to avoid flakes.

--- src/v/cloud_storage/tests/segment_meta_cstore_test.cc
+++ src/v/cloud_storage/tests/segment_meta_cstore_test.cc
@@ -39,8 +39,8 @@ static ss::logger test("test-logger-s");
 // have to use smaller dataset. It's important to actually run the tests in
 // debug to detect potential memory bugs using ASan.
 #ifdef NDEBUG
-static constexpr size_t short_test_size = 10000;
-static constexpr size_t long_test_size = 100000;
+static constexpr size_t short_test_size = 3000;
+static constexpr size_t long_test_size = 20'000;
 #else
 static constexpr size_t short_test_size = 1500;
 static constexpr size_t long_test_size = 10'000;
@@ -91,7 +91,7 @@ std::vector<segment_meta> generate_metadata(size_t sz) {
             curr.base_offset = model::next_offset(curr.committed_offset);
             curr.committed_offset = curr.committed_offset
                                     + model::offset(rg::get_int(1, 1000));
-            curr.size_bytes = rg::get_int(1, 200000);
+            curr.size_bytes = rg::get_int(1, 20000);
             curr.base_timestamp = curr.max_timestamp;
             curr.max_timestamp = model::timestamp(
               curr.max_timestamp.value()
