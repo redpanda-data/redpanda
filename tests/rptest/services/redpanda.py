56f27ef3e2107c411221ca3cd207fb2a1535b895
Oren Leiman <oren.leiman@redpanda.com>
2025-11-25 19:49:51 -0800

    dt/rp: Filter .cannotrecover files from storage usage post-check
    Unrecoverable segments aren't included in the disk usage report.
    Since we don't remove them automatically and they don't get
    cleaned up automatically, we can ignore them here for now.
    
    Fixes:
    
    - [CORE-14754]
    - [CORE-14763]
    - [CORE-14784]
    - [CORE-14790]
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>
    (cherry picked from commit 8511a8e1b745022fc20c03ab7c33c0b4eb54beea)

--- tests/rptest/services/redpanda.py
+++ tests/rptest/services/redpanda.py
@@ -4251,6 +4251,11 @@ class RedpandaService(RedpandaServiceBase):
                 # happen without placing a lot of restrictions on shutdown. for
                 # the time being just ignore these.
                 return False
+            if file.suffix == ".cannotrecover":
+                # Unrecoverable segments aren't included in the disk usage report.
+                # Since we don't remove them automatically and they don't get
+                # cleaned up automatically, we can ignore them here for now.
+                return False
             return True
 
         def inspect_node(node):
