22dddcdac91a078633af160198db1b0fb276083f
Michael Boquard <michael@redpanda.com>
2026-01-09 12:12:22 -0500

    sr/json: Bump resolve reference loop limit
    Signed-off-by: Michael Boquard <michael@redpanda.com>
    (cherry picked from commit be65804e31be71456948e1460efbb3e4bafc9270)

--- src/v/pandaproxy/schema_registry/json.cc
+++ src/v/pandaproxy/schema_registry/json.cc
@@ -284,7 +284,7 @@ public:
 
 private:
     const json_schema_definition::impl& _schema;
-    static constexpr int max_recursion_depth{5};
+    static constexpr int max_recursion_depth{8};
     int _ref_units{max_recursion_depth};
 };
 
