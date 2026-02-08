70dba622c0f8afd7830e742d0a95e1ebfd69dfa9
Oren Leiman <oren.leiman@redpanda.com>
2026-02-07 16:50:31 -0800

    ct/data_plane: inflight write tracking
    data plane
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/cloud_topics/data_plane_impl.cc
+++ src/v/cloud_topics/data_plane_impl.cc
@@ -236,6 +236,14 @@ public:
         }
         co_return epoch_fut.get();
     }
+    std::unique_ptr<inflight_write_token> track_inflight_write() override {
+        return _batcher.local().track_write();
+    }
+
+    ss::future<> drain_inflight_writes() override {
+        co_await _batcher.invoke_on_all(
+          [](auto& b) { return b.drain_writes(); });
+    }
 
 private:
     ss::sharded<l0::cluster_services> _cluster_services;
