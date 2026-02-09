805ef889733a2ea5550dab1621547ad0c9ea45cc
Ioannis Kavvadias <ioannis.kavvadias@redpanda.com>
2026-02-09 18:41:56 +0000

    kafka/client: replace circular buffer with chunked equivalent
    (cherry picked from commit 355d5a1bede8a0741b54c0ea0fad4d630b7cbfab)

--- src/v/kafka/client/produce_batcher.h
+++ src/v/kafka/client/produce_batcher.h
@@ -13,11 +13,10 @@
 
 #include "absl/container/flat_hash_map.h"
 #include "base/seastarx.h"
+#include "container/chunked_circular_buffer.h"
 #include "kafka/protocol/produce.h"
 #include "model/fundamental.h"
 #include "storage/record_batch_builder.h"
-
-#include <seastar/core/circular_buffer.hh>
 
 namespace kafka::client {
 
@@ -122,8 +121,8 @@ private:
     model::compression _c;
     storage::record_batch_builder _builder;
     // TODO(Ben): Maybe these should be a queue for backpressure
-    ss::circular_buffer<client_context> _client_reqs;
-    ss::circular_buffer<broker_context> _broker_reqs;
+    chunked_circular_buffer<client_context> _client_reqs;
+    chunked_circular_buffer<broker_context> _broker_reqs;
 };
 
 } // namespace kafka::client
