ed36548a834d23edc65434cb3698975e0ad969d3
Oren Leiman <oren.leiman@redpanda.com>
2026-02-24 10:46:00 -0800

    sr/test: Add no-op transport to utils
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/pandaproxy/schema_registry/test/utils.h
+++ src/v/pandaproxy/schema_registry/test/utils.h
@@ -9,6 +9,9 @@
 
 #pragma once
 #include "pandaproxy/schema_registry/seq_writer.h"
+#include "pandaproxy/schema_registry/transport.h"
+
+#include <stdexcept>
 
 class sequence_state_checker_test
   : public pandaproxy::schema_registry::sequence_state_checker {
@@ -20,4 +23,35 @@ public:
 
 private:
     writes_disabled_t _wd;
+};
+
+/// No-op transport used in tests where seq_writer is only instantiated to
+/// receive consume_to_store offset updates.
+class noop_transport final : public pandaproxy::schema_registry::transport {
+public:
+    ss::future<> stop() final { return ss::now(); }
+    ss::future<pandaproxy::schema_registry::produce_result>
+    produce(model::record_batch) override {
+        throw std::runtime_error("noop_transport::produce not implemented");
+    }
+    ss::future<model::offset> get_high_watermark() override {
+        throw std::runtime_error(
+          "noop_transport::get_high_watermark not implemented");
+    }
+    ss::future<> consume_range(
+      model::offset,
+      model::offset,
+      ss::noncopyable_function<
+        ss::future<ss::stop_iteration>(model::record_batch)>) override {
+        throw std::runtime_error(
+          "noop_transport::consume_range not implemented");
+    }
+    ss::future<cluster::errc> create_topic(
+      model::topic_namespace_view,
+      int32_t,
+      cluster::topic_properties,
+      int16_t) final {
+        throw std::runtime_error(
+          "noop_transport::create_topic not implemented");
+    }
 };
