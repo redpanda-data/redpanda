5c3007729d37098b258cf1613cddf24b3c6bf3b5
Andrew Nguyen <andrew.nguyen@redpanda.com>
2026-03-18 16:53:54 +0000

    kafka/tests: align unlicensed_permit_if_config_disabled with other tests
    Switch from lconf().set_value() to update_cluster_config() and add
    wait_for_license_init() before revoke_license() for consistency with
    the sibling unlicensed enterprise tests, not due to observed flakiness.
    
    (cherry picked from commit aedd7d6ff8d6202ab76fcb5bdd58c9f2541a2722)

--- src/v/kafka/server/tests/create_topics_test.cc
+++ src/v/kafka/server/tests/create_topics_test.cc
@@ -495,10 +495,10 @@ FIXTURE_TEST(case_insensitive_boolean_property, create_topic_fixture) {
 }
 
 FIXTURE_TEST(unlicensed_permit_if_config_disabled, create_topic_fixture) {
-    lconf().enable_schema_id_validation.set_value(
-      pandaproxy::schema_registry::schema_id_validation_mode::none);
-    lconf().cloud_storage_enabled.set_value(false);
+    update_cluster_config(lconf().enable_schema_id_validation.name(), "none");
+    update_cluster_config(lconf().cloud_storage_enabled.name(), "false");
 
+    wait_for_license_init();
     revoke_license();
 
     using prop_t = std::map<ss::sstring, ss::sstring>;
