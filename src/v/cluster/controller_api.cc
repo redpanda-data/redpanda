7b739d58b2780758a889a42b6deb1fcf0ae4d1b7
Willem Kaufmann <willem.kaufmann@redpanda.com>
2026-03-31 22:11:28 -0400

    `cluster`: reduce `parallel_for_each()` batch size to 512
    The concurrent limit of `4096` in `all_reconciliations_done()` is high enough
    to trigger `Too long queue accumulated` warnings from seastar within scale
    tests and high partition scenarios.
    
    Reduce `batch_size` to 512 to reduce the amount of concurrency in the system
    and avoid flooding the reactor.
    
    (cherry picked from commit 0de828b93cbf7cc39f6947f5e54a2ffaa1312f22)

--- src/v/cluster/controller_api.cc
+++ src/v/cluster/controller_api.cc
@@ -76,7 +76,7 @@ controller_api::get_reconciliation_state(chunked_vector<model::ntp> ntps) {
 
 ss::future<result<bool>>
 controller_api::all_reconciliations_done(std::deque<model::ntp> ntps) {
-    const size_t batch_size = 4096;
+    const size_t batch_size = 512;
     // For a huge topic with e.g. 100k partitions, this will be a huge loop:
     // that means we need parallelism, but not so much that we totally
     // saturate inter-core queues.
