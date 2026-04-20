4ea0abb0313b2b5243598f91164a5b26d377e9d2
Nicolae Vartolomei <nv@redpanda.com>
2026-04-20 12:14:27 +0100

    cloud_io: serialize concurrent throughput updates
    Both config bindings (`cloud_storage_max_throughput_per_shard`,
    `cloud_storage_throughput_limit_percent`) and `start()` trigger
    `update_throughput()`. Since it contains a `co_await` inside
    `set_disk_max_bandwidth`, concurrent invocations can interleave and
    leave the final throughput reflecting a stale config instead of the
    latest.
    
    Serialize with a per-shard mutex so updates run to completion in order.
    
    (cherry picked from commit 1c397242a1a5b50af51e6172589bfee9cdc8134e)

--- src/v/cloud_io/io_resources.h
+++ src/v/cloud_io/io_resources.h
@@ -11,8 +11,8 @@
 
 #include "base/seastarx.h"
 #include "config/property.h"
-#include "ssx/semaphore.h"
 #include "utils/adjustable_semaphore.h"
+#include "utils/mutex.h"
 #include "utils/token_bucket.h"
 
 #include <seastar/core/abort_source.hh>
@@ -75,6 +75,7 @@ private:
     bool _throttling_disabled{false};
     std::optional<size_t> _device_throughput;
     seastar::scheduling_group _scheduling_group;
+    mutex _update_lock{"cloud_io/io_resources/update"};
 };
 
 } // namespace cloud_io
