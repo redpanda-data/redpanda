a751778d1f99410cacce06585a6d76d5ae3f7761
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2026-05-01 13:23:10 +0100

    cluster/tests: roundtrip create_acls_reply with populated results
    (cherry picked from commit e9c470a16d9675f57c77875655687a27bb77d925)

--- src/v/cluster/tests/serialization_rt_test.cc
+++ src/v/cluster/tests/serialization_rt_test.cc
@@ -1492,12 +1492,11 @@ SEASTAR_THREAD_TEST_CASE(serde_reflection_roundtrip) {
         roundtrip_test(std::move(data));
     }
     {
-        std::vector<cluster::errc> results;
-        for (int i = 0, mi = random_generators::get_int(20); i < mi; i++) {
-            results.push_back(cluster::errc::invalid_node_operation);
-        }
         cluster::create_acls_reply data;
-        roundtrip_test(data);
+        for (int i = 0, mi = random_generators::get_int(20); i < mi; i++) {
+            data.results.push_back(cluster::errc::invalid_node_operation);
+        }
+        roundtrip_test(std::move(data));
     }
     {
         cluster::delete_acls_cmd_data delete_acls_data{};
