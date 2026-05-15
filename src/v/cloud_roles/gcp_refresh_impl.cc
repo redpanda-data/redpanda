1012214b8a39fffaeca6a7b37e001a1a7009cb02
Willem Kaufmann <willem.kaufmann@redpanda.com>
2026-05-14 22:27:09 -0400

    `cloud_roles`: plumb `metrics_tag` through `refresh_credentials` impls
    Previously when using `sts` credentials for both `tiered_storage` and
    `datalake` subsystems, we would accidentally register the same metric
    twice.
    
    Prevent this from happening by plumbing a `metrics_tag` through the
    `refresh_credentials` impls and use this to distinguish when registering
    metrics. E.g., `aws_sts` versus `aws_sts_datalake`.
    
    (cherry picked from commit 4f0f029c50cccf73070649d802031dbc19d9a5fd)

--- src/v/cloud_roles/gcp_refresh_impl.cc
+++ src/v/cloud_roles/gcp_refresh_impl.cc
@@ -40,9 +40,14 @@ gcp_refresh_impl::gcp_refresh_impl(
   aws_service_name, // Ignored for GCP
   aws_region_name region,
   ss::abort_source& as,
-  retry_params retry_params)
+  retry_params retry_params,
+  ss::sstring metrics_tag)
   : refresh_credentials::impl(
-      std::move(address), std::move(region), as, retry_params) {}
+      std::move(address),
+      std::move(region),
+      as,
+      retry_params,
+      std::move(metrics_tag)) {}
 
 ss::future<api_response> gcp_refresh_impl::fetch_credentials() {
     http::client::request_header oauth_req;
