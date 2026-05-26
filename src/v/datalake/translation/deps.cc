1309e7682cb6a57727368f4d185ea4ce979f678f
Nicolae Vartolomei <nv@redpanda.com>
2026-05-26 14:19:22 +0100

    datalake: stop bucketing unknown_error as file_io_error
    Avoid misleading error logs like:
    
      Error ensuring table schema for record 42: table_creator::errc::failed
      Translation attempt ran into an unexpected exception: std::runtime_error
      (Error flushing in-progress translation: local file IO error)
    
    where the underlying cause has nothing to do with local file IO.
    
    (cherry picked from commit 75e1fada8b034d70b1f2129ab75c45c2714aa9b7)

--- src/v/datalake/translation/deps.cc
+++ src/v/datalake/translation/deps.cc
@@ -50,6 +50,8 @@ map_error_code(datalake::translation_task::errc errc) {
         return translation_errc::out_of_disk;
     case datalake::translation_task::errc::type_resolution_error:
         return translation_errc::type_resolution_error;
+    case datalake::translation_task::errc::unknown_error:
+        return translation_errc::unknown_error;
     }
 }
 } // namespace
@@ -454,6 +456,8 @@ std::ostream& operator<<(std::ostream& o, translation_errc ec) {
         return o << "translation_errc::out_of_disk";
     case type_resolution_error:
         return o << "translation_errc::type_resolution_error";
+    case unknown_error:
+        return o << "translation_errc::unknown_error";
     }
 }
 
--- src/v/datalake/translation_task.cc
+++ src/v/datalake/translation_task.cc
@@ -55,7 +55,7 @@ translation_task::errc map_error_code(writer_error errc) {
     case writer_error::out_of_disk:
         return translation_task::errc::out_of_disk;
     case writer_error::unknown_error:
-        return translation_task::errc::file_io_error;
+        return translation_task::errc::unknown_error;
     case writer_error::retryable_type_resolution_error:
         return translation_task::errc::type_resolution_error;
     }
@@ -428,6 +428,8 @@ std::ostream& operator<<(std::ostream& o, translation_task::errc ec) {
         return o << "disk exhausted";
     case translation_task::errc::type_resolution_error:
         return o << "type resolution error";
+    case translation_task::errc::unknown_error:
+        return o << "unknown error";
     }
 }
 } // namespace datalake
