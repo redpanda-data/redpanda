7864c2e90389136e0c848321e0ea17df32678e70
Willem Kaufmann <willem.kaufmann@redpanda.com>
2026-07-22 12:05:58 -0400

    Merge pull request #31215 from WillemKauf/json-sink-staging
    `json`: reduce per-character costs in chunked buffer and input stream
    
    (cherry picked from commit 2c81a74cb086d4d5e03d776c6e0251c20b584ac3)

--- src/v/json/chunked_buffer.h
+++ src/v/json/chunked_buffer.h
@@ -12,6 +12,10 @@
 #include "bytes/iobuf.h"
 #include "json/encodings.h"
 
+#include <array>
+#include <optional>
+#include <string_view>
+
 namespace json {
 
 template<
@@ -25,6 +29,10 @@ namespace impl {
 
 /**
  * \brief An in-memory output stream with non-contiguous memory allocation.
+ *
+ * Single characters are staged in a small inline buffer and flushed to the
+ * underlying iobuf in blocks, so that per-character Put() calls don't pay
+ * for an iobuf::append() each.
  */
 template<typename Encoding>
 struct generic_chunked_buffer {
@@ -35,18 +43,26 @@ struct generic_chunked_buffer {
      */
     /**@{*/
 
-    void Put(Ch c) { _impl.append(&c, sizeof(Ch)); }
-    void Flush() {}
+    void Put(Ch c) {
+        if (_staged == stage_capacity) [[unlikely]] {
+            flush_stage();
+        }
+        _stage[_staged++] = c;
+    }
+    void Flush() { flush_stage(); }
 
     //! Get the size of string in bytes in the string buffer.
-    size_t GetSize() const { return _impl.size_bytes(); }
+    size_t GetSize() const { return _impl.size_bytes() + _staged * sizeof(Ch); }
 
     //! Get the length of string in Ch in the string buffer.
-    size_t GetLength() const { return _impl.size_bytes() / sizeof(Ch); }
+    size_t GetLength() const { return GetSize() / sizeof(Ch); }
 
     void Reserve(size_t s) { _impl.reserve_memory(s); }
 
-    void Clear() { _impl.clear(); }
+    void Clear() {
+        _impl.clear();
+        _staged = 0;
+    }
 
     /**@}*/
 
@@ -55,15 +71,42 @@ struct generic_chunked_buffer {
      * fragment and is a zero-copy operation.
      */
     void append(std::unique_ptr<iobuf::fragment> frag) {
+        flush_stage();
         _impl.append(std::move(frag));
+    }
+
+    /**
+     * If the buffered data is entirely contiguous in memory (i.e. it never
+     * spilled out of the stage buffer), return a view over it. The view is
+     * invalidated by any subsequent modification of this buffer.
+     */
+    std::optional<std::basic_string_view<Ch>> contiguous_view() const {
+        if (!_impl.empty()) {
+            return std::nullopt;
+        }
+        return std::basic_string_view<Ch>{_stage.data(), _staged};
     }
 
     /**
      * Return the underlying iobuf, this is destructive and zero-copy.
      */
-    iobuf as_iobuf() && { return std::move(_impl); }
+    iobuf as_iobuf() && {
+        flush_stage();
+        return std::move(_impl);
+    }
 
 private:
+    void flush_stage() {
+        if (_staged > 0) {
+            _impl.append(
+              reinterpret_cast<const char*>(_stage.data()),
+              _staged * sizeof(Ch));
+            _staged = 0;
+        }
+    }
+
+    static constexpr size_t stage_capacity = 1024;
+
     template<
       typename OutputStream,
       typename SourceEncoding,
@@ -71,6 +114,8 @@ private:
       unsigned writeFlags>
     friend class json::generic_iobuf_writer;
     iobuf _impl;
+    std::array<Ch, stage_capacity> _stage;
+    size_t _staged{0};
 };
 
 } // namespace impl
