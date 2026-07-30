99c269e5e062b754326ff3fb501e63584838a9f0
Oren Leiman <oren.leiman@redpanda.com>
2026-07-30 16:14:55 -0700

    k/s/tests: fix group_test segfault by using shard_local_cfg
    group_test built its own config::configuration on the stack of the
    ss::thread that SEASTAR_THREAD_TEST_CASE runs the test body in. That
    constructor initializes every property in a single function body, so
    its frame is ~143KiB unoptimized, while an ss::thread only gets a
    128KiB stack. It ran off the end of the stack and died on the guard
    page in the very first test case.
    
    Only fastbuild is affected. release optimizes the frame down to
    ~107KiB, which fits, and debug enables asan, which raises the
    ss::thread stack to 256KiB.
    
    config::shard_local_cfg() already constructs its instance on a
    dedicated 512KiB thread for this exact reason, and is also the config
    group_manager hands to group in production.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>
    (cherry picked from commit 0db2a7ad299023d5af3b97d5e0d7360ec383d93d)

--- src/v/kafka/server/tests/group_test.cc
+++ src/v/kafka/server/tests/group_test.cc
@@ -49,7 +49,7 @@ static bool is_uuid(const ss::sstring& uuid) {
  *  good covering set of scenarios.
  */
 static group get() {
-    static config::configuration conf;
+    auto& conf = config::shard_local_cfg();
     conf.enable_consumer_group_metrics.set_value(std::vector<ss::sstring>{});
     ss::sharded<cluster::tx_gateway_frontend> fr;
     ss::sharded<features::feature_table> feature_table;
