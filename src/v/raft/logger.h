55c40ae6a59847c5bf2c288ece25be90395c8868
Stephan Dollberg <stephan@redpanda.com>
2026-07-31 19:59:22 +0100

    raft: Don't use c-string format strings in raft logger

--- src/v/raft/logger.h
+++ src/v/raft/logger.h
@@ -28,39 +28,50 @@ public:
       , _ntp(std::move(ntp)) {}
 
     template<typename... Args>
-    void error(const char* format, Args&&... args) {
-        log(ss::log_level::error, format, std::forward<Args>(args)...);
+    void error(ss::logger::format_info_t<Args...> format, Args&&... args) {
+        log(
+          ss::log_level::error, std::move(format), std::forward<Args>(args)...);
     }
     template<typename... Args>
-    void warn(const char* format, Args&&... args) {
-        log(ss::log_level::warn, format, std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void info(const char* format, Args&&... args) {
-        log(ss::log_level::info, format, std::forward<Args>(args)...);
+    void warn(ss::logger::format_info_t<Args...> format, Args&&... args) {
+        log(
+          ss::log_level::warn, std::move(format), std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    void debug(const char* format, Args&&... args) {
-        log(ss::log_level::debug, format, std::forward<Args>(args)...);
+    void info(ss::logger::format_info_t<Args...> format, Args&&... args) {
+        log(
+          ss::log_level::info, std::move(format), std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    void trace(const char* format, Args&&... args) {
-        log(ss::log_level::trace, format, std::forward<Args>(args)...);
+    void debug(ss::logger::format_info_t<Args...> format, Args&&... args) {
+        log(
+          ss::log_level::debug, std::move(format), std::forward<Args>(args)...);
     }
 
     template<typename... Args>
-    void log(ss::log_level lvl, const char* format, Args&&... args) {
+    void trace(ss::logger::format_info_t<Args...> format, Args&&... args) {
+        log(
+          ss::log_level::trace, std::move(format), std::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    void log(
+      ss::log_level lvl,
+      ss::logger::format_info_t<Args...> format,
+      Args&&... args) {
         if (raftlog.is_enabled(lvl)) {
-            auto line_fmt = ss::sstring("[group_id:{}, {}] ") + format;
-            raftlog.log(
-              lvl,
-              line_fmt.c_str(),
-              _group_id,
-              _ntp,
-              std::forward<Args>(args)...);
+            ss::logger::lambda_log_writer writer(
+              [&](ss::internal::log_buf::inserter_iterator it) {
+                  it = fmt::format_to(
+                    it, "[group_id:{}, {}] ", _group_id, _ntp);
+                  return fmt::format_to(
+                    it,
+                    fmt::runtime(format.format),
+                    std::forward<Args>(args)...);
+              });
+            raftlog.log(lvl, writer);
         }
     }
 
