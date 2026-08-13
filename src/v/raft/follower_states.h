4e1c97008263c5cd985c99dc22acb37dbb95e444
Brandon Allard <brandon@redpanda.com>
2026-08-12 21:14:04 -0400

    raft: track per-follower nudge deadline
    Preparation for out of band metadata pushes to followers: the
    deadline debounces how often a single follower can be nudged.

--- src/v/raft/follower_states.h
+++ src/v/raft/follower_states.h
@@ -60,6 +60,9 @@ struct follower_index_metadata {
     // timestamp of last append_entries_rpc call
     clock_type::time_point last_sent_append_entries_req_timestamp;
     clock_type::time_point last_received_reply_timestamp;
+    // earliest time the next out of band metadata push (nudge) may be sent
+    // to this follower.
+    clock_type::time_point next_follower_nudge;
     /// Heartbeat failures observed on the current cached transport.
     /// Reset on a successful reply or when we force a reconnect.
     uint32_t heartbeats_failed{0};
